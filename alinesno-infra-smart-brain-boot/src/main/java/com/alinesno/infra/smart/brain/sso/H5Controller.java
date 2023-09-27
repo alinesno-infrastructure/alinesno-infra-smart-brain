677f05882493e5aa918d808ddba741ce2545aaa4
LuoXiaoDong <luoandon@gmail.com>
2023-09-28 06:43:15 +0800

    fix:处理代码安全问题

--- alinesno-infra-smart-brain-boot/src/main/java/com/alinesno/infra/smart/brain/sso/H5Controller.java
+++ alinesno-infra-smart-brain-boot/src/main/java/com/alinesno/infra/smart/brain/sso/H5Controller.java
@@ -1,6 +1,7 @@
-package com.alinesno.infra.smart.brain.sso;
+package com.alinesno.infra.plat.console.sso;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,29 +13,29 @@ import cn.dev33.satoken.util.SaResult;
 import java.util.UUID;
 
 /**
- * 前后台分离架构下集成SSO所需的代码 （SSO-Client端）
- * <p>（注：如果不需要前后端分离架构下集成SSO，可删除此包下所有代码）</p>
- * @author click33
+ * 集成Sa-Token前后端分离单点登陆代码
  *
+ * @author luoxiaodong
+ * @version 1.0.0
  */
 @RestController
 public class H5Controller {
 
 	// 当前是否登录 
-	@RequestMapping("/sso/isLogin")
+	@GetMapping("/sso/isLogin")
 	public Object isLogin() {
 		return SaResult.data(StpUtil.isLogin());
 	}
 	
 	// 返回SSO认证中心登录地址 
-	@RequestMapping("/sso/getSsoAuthUrl")
+	@GetMapping("/sso/getSsoAuthUrl")
 	public SaResult getSsoAuthUrl(String clientLoginUrl) {
 		String serverAuthUrl = SaSsoUtil.buildServerAuthUrl(clientLoginUrl, "");
 		return SaResult.data(serverAuthUrl);
 	}
 	
 	// 根据ticket进行登录 
-	@RequestMapping("/sso/doLoginByTicket")
+	@GetMapping("/sso/doLoginByTicket")
 	public SaResult doLoginByTicket(String ticket) {
 		Object loginId = SaSsoProcessor.instance.checkTicket(ticket, "/sso/doLoginByTicket");
 		if(loginId != null) {
@@ -53,7 +54,6 @@ public class H5Controller {
 	// 全局异常拦截 
 	@ExceptionHandler
 	public SaResult handlerException(Exception e) {
-		e.printStackTrace(); 
 		return SaResult.error(e.getMessage());
 	}
 	
