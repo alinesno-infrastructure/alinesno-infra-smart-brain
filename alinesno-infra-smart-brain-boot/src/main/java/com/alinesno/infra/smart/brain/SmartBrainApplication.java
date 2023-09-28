ddcff055e94ec119b234248828e4d0d0d37b6ea5
switchII <luoandon@gmail.com>
2023-09-29 06:19:26 +0800

    feat:优化单点登陆

--- alinesno-infra-smart-brain-boot/src/main/java/com/alinesno/infra/smart/brain/SmartBrainApplication.java
+++ alinesno-infra-smart-brain-boot/src/main/java/com/alinesno/infra/smart/brain/SmartBrainApplication.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.brain;
 
+import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  * @author LuoAnDong
  * @since 2023年8月3日 上午6:23:43
  */
+@EnableInfraSsoApi
 @ServletComponentScan
 @MapperScan("com.alinesno.infra.smart.brain.mapper")
 @SpringBootApplication
--- alinesno-infra-smart-brain-boot/src/main/java/com/alinesno/infra/smart/brain/console/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 临时的common-web列表，后期会删除掉
- */
-package com.alinesno.infra.smart.brain.console;
