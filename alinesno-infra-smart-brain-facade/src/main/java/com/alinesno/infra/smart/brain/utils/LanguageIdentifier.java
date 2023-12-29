262df8e2ac03dd8766c2b9cddadd54485bc8b110
switchII <luoandon@gmail.com>
2023-12-30 01:09:13 +0800

    fix:调整项目配置

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/utils/LanguageIdentifier.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/utils/LanguageIdentifier.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.brain.utils;
 
+import org.springframework.util.StringUtils;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.parser.ParserException;
 
@@ -27,32 +28,33 @@ public class LanguageIdentifier {
     }
 
     private static boolean isJava(String content) {
-        // 判断是否包含Java关键字
-        String javaKeywords = "\\b(package|import|class|public|private|protected|static|void|if|else|for|while|do|break|continue|return)\\b";
-        if (Pattern.compile(javaKeywords).matcher(content).find()) {
-            return true;
-        }
 
-        // 判断是否包含Java注释
-        String javaComment = "//.*|/\\*(.|[\\n\\r])*?\\*/";
-        if (Pattern.compile(javaComment).matcher(content).find()) {
-            return true;
+        if(StringUtils.hasLength(content)){
+            // 判断是否包含Java关键字
+            String javaKeywords = "\\b(package|import|class|public|private|protected|static|void|if|else|for|while|do|break|continue|return)\\b";
+            if (Pattern.compile(javaKeywords).matcher(content).find()) {
+                return true;
+            }
+
+            // 判断是否包含Java注释
+            String javaComment = "//.*|/\\*(.|[\\n\\r])*?\\*/";
+            return Pattern.compile(javaComment).matcher(content).find();
         }
 
         return false;
     }
 
     private static boolean isPython(String content) {
-        // 判断是否包含Python关键字
-        String pythonKeywords = "\\b(and|as|assert|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)\\b";
-        if (Pattern.compile(pythonKeywords).matcher(content).find()) {
-            return true;
-        }
+        if(StringUtils.hasLength(content)) {
+            // 判断是否包含Python关键字
+            String pythonKeywords = "\\b(and|as|assert|break|class|continue|def|del|elif|else|except|finally|for|from|global|if|import|in|is|lambda|nonlocal|not|or|pass|raise|return|try|while|with|yield)\\b";
+            if (Pattern.compile(pythonKeywords).matcher(content).find()) {
+                return true;
+            }
 
-        // 判断是否包含Python注释
-        String pythonComment = "#.*";
-        if (Pattern.compile(pythonComment).matcher(content).find()) {
-            return true;
+            // 判断是否包含Python注释
+            String pythonComment = "#.*";
+            return Pattern.compile(pythonComment).matcher(content).find();
         }
 
         return false;
@@ -69,32 +71,32 @@ public class LanguageIdentifier {
     }
 
     private static boolean isMarkdown(String content) {
-        // 判断是否包含Markdown标题
-        String markdownHeader = "^#+\\s.*";
-        if (Pattern.compile(markdownHeader, Pattern.MULTILINE).matcher(content).find()) {
-            return true;
-        }
+        if(StringUtils.hasLength(content)) {
+            // 判断是否包含Markdown标题
+            String markdownHeader = "^#+\\s.*";
+            if (Pattern.compile(markdownHeader, Pattern.MULTILINE).matcher(content).find()) {
+                return true;
+            }
 
-        // 判断是否包含Markdown列表
-        String markdownList = "^[\\-\\*\\+]\\s.*";
-        if (Pattern.compile(markdownList, Pattern.MULTILINE).matcher(content).find()) {
-            return true;
+            // 判断是否包含Markdown列表
+            String markdownList = "^[\\-\\*\\+]\\s.*";
+            return Pattern.compile(markdownList, Pattern.MULTILINE).matcher(content).find();
         }
 
         return false;
     }
 
     private static boolean isSQL(String content) {
-        // 判断是否包含SQL关键字
-        String sqlKeywords = "\\b(select|insert|update|delete|create|drop|alter|table|from|where|order by|group by|having|join|on|and|or)\\b";
-        if (Pattern.compile(sqlKeywords, Pattern.CASE_INSENSITIVE).matcher(content).find()) {
-            return true;
-        }
+        if(StringUtils.hasLength(content)) {
+            // 判断是否包含SQL关键字
+            String sqlKeywords = "\\b(select|insert|update|delete|create|drop|alter|table|from|where|order by|group by|having|join|on|and|or)\\b";
+            if (Pattern.compile(sqlKeywords, Pattern.CASE_INSENSITIVE).matcher(content).find()) {
+                return true;
+            }
 
-        // 判断是否包含SQL注释
-        String sqlComment = "--.*";
-        if (Pattern.compile(sqlComment).matcher(content).find()) {
-            return true;
+            // 判断是否包含SQL注释
+            String sqlComment = "--.*";
+            return Pattern.compile(sqlComment).matcher(content).find();
         }
 
         return false;
