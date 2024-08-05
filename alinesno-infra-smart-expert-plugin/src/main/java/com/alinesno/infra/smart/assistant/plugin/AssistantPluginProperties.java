27149c6e8c7e0572234d5515b65e4142720b0dad
陈伟(Victor) <victorAip@gmail.com>
2024-08-05 04:37:00 -0400

    新增AssistantPluginProperties类，包含插件存放路径与插件中心地址属性。

--- alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/AssistantPluginProperties.java
+++ alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/AssistantPluginProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+@ToString
 @Configuration
 @Data
 @ConfigurationProperties(prefix = "alinesno.infra.smart.assistant.plugin")
