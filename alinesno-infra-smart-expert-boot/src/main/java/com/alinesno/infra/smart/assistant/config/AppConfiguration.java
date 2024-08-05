1bd5ad32b337fc7b940cf001d079e038ae92ae74
陈伟(Victor) <victorAip@gmail.com>
2024-08-05 05:47:00 -0400

    优化配置中心类，整合日志、异常重试、SSO认证和命令行初始化功能。

--- alinesno-infra-smart-expert-boot/src/main/java/com/alinesno/infra/smart/assistant/config/AppConfiguration.java
+++ alinesno-infra-smart-expert-boot/src/main/java/com/alinesno/infra/smart/assistant/config/AppConfiguration.java
@@ -3,9 +3,12 @@ package com.alinesno.infra.smart.assistant.config;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
+import com.alinesno.infra.smart.assistant.initialize.IAssistantInitService;
 import com.dtflys.forest.springboot.annotation.ForestScan;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -26,9 +29,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ForestScan(basePackages = "com.alinesno.infra.smart.assistant.adapter")
 @MapperScan({"com.alinesno.infra.smart.assistant.mapper" , "com.alinesno.infra.smart.brain.mapper"})
 @ComponentScan({"com.alinesno.infra.smart.assistant","com.alinesno.infra.smart.brain"})
-public class AppConfiguration implements CommandLineRunner {{
+public class AppConfiguration implements CommandLineRunner {
 
-}
+    @Autowired
+    private IAssistantInitService assistantInitService ;
+
     @Bean
     public LogAspect getLogAspect() {
         return new LogAspect();
@@ -37,6 +42,18 @@ public class AppConfiguration implements CommandLineRunner {{
     @Override
     public void run(String... args) throws Exception {
         log.debug("项目启动初始化");
+
+        // 初始化提示
+        assistantInitService.promptCatalog();
+
+        // 初始化分类
+        assistantInitService.expertCatalog();
+
+        // 初始化专家
+        assistantInitService.expert();
+
+        // 初始化插件
+        assistantInitService.initPlugin();
     }
 
 }
