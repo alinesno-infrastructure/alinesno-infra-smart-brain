9caff6769b23d819343523ceccb3cf7b1911c70e
王磊(Levi) <leviAip@gmail.com>
2024-08-05 00:55:00 -0400

    修改IAssistantInitService接口，新增初始化用户信息、指令分类、指令、角色分类、角色和插件的方法。

---
+++ alinesno-infra-smart-expert-initialize/src/main/java/com/alinesno/infra/smart/assistant/initialize/IAssistantInitService.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.smart.assistant.initialize;
+
+/**
+ * 初始化用户信息
+ */
+public interface IAssistantInitService {
+
+    /**
+     * 初始化指令分类
+     */
+    void promptCatalog() ;
+
+    /**
+     * 初始化指令
+     */
+    void prompt() ;
+
+    /**
+     * 初始化角色分类
+     */
+    void expertCatalog() ;
+
+    /**
+     * 初始化角色
+     */
+    void expert() ;
+
+    /**
+     * 初始化插件
+     */
+    void initPlugin() ;
+
+}
