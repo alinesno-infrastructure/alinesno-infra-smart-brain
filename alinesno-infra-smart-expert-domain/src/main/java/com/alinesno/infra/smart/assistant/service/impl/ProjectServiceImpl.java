d0200afa386e8dd8271eba524b5a23a54edb93a4
陈伟(Victor) <victorAip@gmail.com>
2024-08-05 02:56:00 -0400

    修复并优化默认项目初始化逻辑，确保唯一性和准确性。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ProjectServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ProjectServiceImpl.java
@@ -6,6 +6,9 @@ import com.alinesno.infra.smart.assistant.mapper.ProjectMapper;
 import com.alinesno.infra.smart.assistant.service.IProjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.sqids.Sqids;
+
+import java.util.Arrays;
 
 /**
  * 应用构建Service业务层处理
@@ -16,5 +19,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class ProjectServiceImpl extends IBaseServiceImpl<ProjectEntity, ProjectMapper> implements IProjectService {
+
+    private static final String DEFAULT_PROJECT_FIELD = "default" ;
+
+    @Override
+    public void initDefaultApp(long userId) {
+
+        Sqids sqids=Sqids.builder().build();
+        String code = sqids.encode(Arrays.asList(1L,2L,3L)); // "86Rf07"
+
+        ProjectEntity project = new ProjectEntity() ;
+
+        project.setOperatorId(userId);
+        project.setFieldProp(DEFAULT_PROJECT_FIELD);
+
+        project.setProjectName("默认监控应用");
+        project.setProjectDesc("包含所有的监控渠道查看权限，用于开发和验证场景");
+        project.setProjectCode(code);
+//        project.setDocumentType(ProviderChannelEnum.getAllNameStr());
+
+        save(project) ;
+    }
 
 }
