3e4d4d5009e070c710fec7c66220ea70167d96d7
王磊(Levi) <leviAip@gmail.com>
2024-08-05 06:28:00 -0400

    更新:alinesno-infra-smart-expert-sample/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java

--- alinesno-infra-smart-expert-sample/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.smart.assistant.enums;
-
-public interface AssistantConstants {
-
-    String BUSINESS_ID = "businessId" ;
-    String BUSINESS_ASSISTANT_YAML_CONTENT = "assistantYamlContent" ;
-
-}
