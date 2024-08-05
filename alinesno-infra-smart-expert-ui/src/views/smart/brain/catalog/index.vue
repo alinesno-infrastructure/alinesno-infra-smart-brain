ffc96e1532b8c9ed5172a6efe626650633d33236
陈伟(Victor) <victorAip@gmail.com>
2024-08-05 04:18:00 -0400

    优化搜索与操作功能，添加新增按钮与状态筛选。

--- alinesno-infra-smart-expert-ui/src/views/smart/brain/catalog/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/brain/catalog/index.vue
@@ -68,7 +68,7 @@
          </el-table-column>
          <el-table-column label="创建时间" align="center" prop="createTime" width="200">
             <template #default="scope">
-               <span>{{ parseTime(scope.row.createTime) }}</span>
+               <span>{{ parseTime(scope.row.addTime ) }}</span>
             </template>
          </el-table-column>
          <el-table-column label="操作" align="center" width="200" class-name="small-padding fixed-width">
@@ -162,7 +162,7 @@ const loading = ref(true);
 const showSearch = ref(true);
 const title = ref("");
 const deptOptions = ref([]);
-const isExpandAll = ref(true);
+const isExpandAll = ref(false);
 const refreshTable = ref(true);
 
 const data = reactive({
