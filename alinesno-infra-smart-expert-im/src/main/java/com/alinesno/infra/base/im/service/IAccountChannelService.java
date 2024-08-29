8d2cabf2c852e0ae88588b7947d260325af3167f
陈伟(Victor) <victorAip@gmail.com>
2024-08-29 18:05:00 -0400

    新增 IAccountChannelService 接口及实体类服务接口实现。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IAccountChannelService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.im.service;
+
+import com.alinesno.infra.base.im.entity.AccountChannelEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+public interface IAccountChannelService extends IBaseService<AccountChannelEntity> {
+}
