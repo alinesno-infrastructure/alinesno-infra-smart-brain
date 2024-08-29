c771c7758bc357459951fdbe55fb78999929f275
张婷(Tina) <tinaAip@gmail.com>
2024-08-29 18:43:00 -0400

    fix:优化SignUtil类，添加签名验证功能。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/smart/assistant/im/utils/SignUtil.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/smart/assistant/im/utils/SignUtil.java
@@ -1,82 +1,82 @@
-package com.alinesno.infra.smart.assistant.im.utils;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-
-/**
- * 签名工具类
- * 用于验证微信消息的签名
- */
-@Component
-public class SignUtil {
-
-    // 与接口配置信息中的 Token 要一致
-    @Value("${alinesno.infra.smart.wechat.token}")
-    private String token ;
-
-    /**
-     * 验证签名
-     *
-     * @param signature 签名
-     * @param timestamp 时间戳
-     * @param nonce     随机字符串
-     * @return 验证结果，true表示签名有效，false表示签名无效
-     */
-    public  boolean checkSignature(String signature, String timestamp, String nonce) {
-        String[] arr = new String[]{token, timestamp, nonce};
-        // 将 token、timestamp、nonce 三个参数进行字典序排序
-        Arrays.sort(arr);
-        StringBuilder content = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            content.append(arr[i]);
-        }
-        MessageDigest md = null;
-        String tmpStr = null;
-
-        try {
-            md = MessageDigest.getInstance("SHA-1");
-            // 将三个参数字符串拼接成一个字符串进行 sha1 加密
-            byte[] digest = md.digest(content.toString().getBytes());
-            tmpStr = byteToStr(digest);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        content = null;
-        // 将 sha1 加密后的字符串可与 signature 对比，标识该请求来源于微信
-        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
-    }
-
-    /**
-     * 将字节数组转换为十六进制字符串
-     *
-     * @param byteArray 字节数组
-     * @return 十六进制字符串
-     */
-    private  String byteToStr(byte[] byteArray) {
-        String strDigest = "";
-        for (int i = 0; i < byteArray.length; i++) {
-            strDigest += byteToHexStr(byteArray[i]);
-        }
-        return strDigest;
-    }
-
-    /**
-     * 将字节转换为十六进制字符串
-     *
-     * @param mByte 字节
-     * @return 十六进制字符串
-     */
-    private  String byteToHexStr(byte mByte) {
-        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        char[] tempArr = new char[2];
-        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
-        tempArr[1] = Digit[mByte & 0X0F];
-        String s = new String(tempArr);
-        return s;
-    }
-}
+//package com.alinesno.infra.smart.assistant.im.utils;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.security.MessageDigest;
+//import java.security.NoSuchAlgorithmException;
+//import java.util.Arrays;
+//
+///**
+// * 签名工具类
+// * 用于验证微信消息的签名
+// */
+//@Component
+//public class SignUtil {
+//
+//    // 与接口配置信息中的 Token 要一致
+//    @Value("${alinesno.infra.smart.wechat.token}")
+//    private String token ;
+//
+//    /**
+//     * 验证签名
+//     *
+//     * @param signature 签名
+//     * @param timestamp 时间戳
+//     * @param nonce     随机字符串
+//     * @return 验证结果，true表示签名有效，false表示签名无效
+//     */
+//    public  boolean checkSignature(String signature, String timestamp, String nonce) {
+//        String[] arr = new String[]{token, timestamp, nonce};
+//        // 将 token、timestamp、nonce 三个参数进行字典序排序
+//        Arrays.sort(arr);
+//        StringBuilder content = new StringBuilder();
+//        for (int i = 0; i < arr.length; i++) {
+//            content.append(arr[i]);
+//        }
+//        MessageDigest md = null;
+//        String tmpStr = null;
+//
+//        try {
+//            md = MessageDigest.getInstance("SHA-1");
+//            // 将三个参数字符串拼接成一个字符串进行 sha1 加密
+//            byte[] digest = md.digest(content.toString().getBytes());
+//            tmpStr = byteToStr(digest);
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//
+//        content = null;
+//        // 将 sha1 加密后的字符串可与 signature 对比，标识该请求来源于微信
+//        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
+//    }
+//
+//    /**
+//     * 将字节数组转换为十六进制字符串
+//     *
+//     * @param byteArray 字节数组
+//     * @return 十六进制字符串
+//     */
+//    private  String byteToStr(byte[] byteArray) {
+//        String strDigest = "";
+//        for (int i = 0; i < byteArray.length; i++) {
+//            strDigest += byteToHexStr(byteArray[i]);
+//        }
+//        return strDigest;
+//    }
+//
+//    /**
+//     * 将字节转换为十六进制字符串
+//     *
+//     * @param mByte 字节
+//     * @return 十六进制字符串
+//     */
+//    private  String byteToHexStr(byte mByte) {
+//        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+//        char[] tempArr = new char[2];
+//        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+//        tempArr[1] = Digit[mByte & 0X0F];
+//        String s = new String(tempArr);
+//        return s;
+//    }
+//}
