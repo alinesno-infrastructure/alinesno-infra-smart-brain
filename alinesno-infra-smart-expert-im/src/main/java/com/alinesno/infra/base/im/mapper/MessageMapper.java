0f93693798c9f8e39b5c5992e6b715399c9bc2dd
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 01:29:00 -0400

    新增MessageMapper接口文件，对应MessageEntity实体类。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/MessageMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.MessageEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface MessageMapper extends IBaseMapper<MessageEntity> {
+    
+}
