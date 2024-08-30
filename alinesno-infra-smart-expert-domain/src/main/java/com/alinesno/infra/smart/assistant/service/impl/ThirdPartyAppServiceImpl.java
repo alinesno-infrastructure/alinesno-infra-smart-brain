3491ba6d9f7884bf14d444a8a04269c0729e37fc
陈伟(Victor) <victorAip@gmail.com>
2024-08-29 20:37:00 -0400

    修复了 ThirdPartyAppServiceImpl 类中导入包路径的问题，并进行了必要的日志记录和组件注解更新。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ThirdPartyAppServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ThirdPartyAppServiceImpl.java
@@ -1,13 +1,12 @@
 package com.alinesno.infra.smart.assistant.service.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.entity.ThirdPartyAppEntity;
 import com.alinesno.infra.smart.assistant.mapper.ThirdPartyAppMapper;
 import com.alinesno.infra.smart.assistant.service.IThirdPartyAppService;
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * <p>
