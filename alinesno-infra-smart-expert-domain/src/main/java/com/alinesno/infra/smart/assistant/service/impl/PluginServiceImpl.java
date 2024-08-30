529c63809bd26f7dbab10557b9cfd8b2eea948ca
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 05:34:00 -0400

    fix:调整PluginServiceImpl服务实现，替换ChannelMapper为PluginMapper。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/PluginServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/PluginServiceImpl.java
@@ -2,12 +2,10 @@ package com.alinesno.infra.smart.assistant.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.entity.PluginEntity;
-import com.alinesno.infra.smart.assistant.mapper.ChannelMapper;
+import com.alinesno.infra.smart.assistant.mapper.PluginMapper;
 import com.alinesno.infra.smart.assistant.service.IPluginService;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 /**
  * 应用构建Service业务层处理
@@ -17,7 +15,7 @@ import org.springframework.util.Assert;
  */
 @Slf4j
 @Service
-public class PluginServiceImpl extends IBaseServiceImpl<PluginEntity, ChannelMapper> implements IPluginService {
+public class PluginServiceImpl extends IBaseServiceImpl<PluginEntity, PluginMapper> implements IPluginService {
 
 
 }
