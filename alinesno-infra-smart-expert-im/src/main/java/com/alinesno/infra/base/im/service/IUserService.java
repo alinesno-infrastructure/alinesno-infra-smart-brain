e12ebd62c9e36e4a4c83c0ba4afea995d0e684e1
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 07:03:00 -0400

    新增IUserService接口，提供用户服务的基本操作。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.im.service;
+
+import com.alinesno.infra.base.im.entity.UserEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+public interface IUserService extends IBaseService<UserEntity> {
+}
