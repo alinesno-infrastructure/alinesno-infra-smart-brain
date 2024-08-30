e607022286b3bdfd3d0033ecbc20097b247db0be
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 09:15:00 -0400

    新增 ImChatController 控制器类，包含消息处理相关方法。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/ImChatController.java
@@ -0,0 +1,230 @@
+package com.alinesno.infra.base.im.gateway.provider;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import com.alinesno.infra.base.im.dto.RobotMessageDto;
+import com.alinesno.infra.base.im.dto.WebMessageDto;
+import com.alinesno.infra.base.im.entity.UserEntity;
+import com.alinesno.infra.base.im.gateway.utils.AgentUtils;
+import com.alinesno.infra.base.im.service.IChannelUserService;
+import com.alinesno.infra.base.im.service.IMessageService;
+import com.alinesno.infra.base.im.service.ITaskService;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.SuperController;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.entity.MessageQueueEntity;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleCatalogService;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.assistant.service.IMessageQueueService;
+import com.alinesno.infra.smart.brain.api.reponse.TaskContentDto;
+import com.alinesno.infra.smart.brain.service.IGenerateTaskService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/api/infra/base/im/chat/")
+public class ImChatController extends SuperController {
+
+    @Autowired
+    private IMessageQueueService messageQueueService; ;
+
+    @Autowired
+    private IMessageService messageService ;
+
+    @Autowired
+    private IChannelUserService channelUserService ;
+
+    @Autowired
+    private IGenerateTaskService generateTaskService ;
+
+    @Autowired
+    private ITaskService taskService ;
+
+    @Autowired
+    private IIndustryRoleService roleService ;
+
+    @Autowired
+    private IIndustryRoleCatalogService industryRoleCatalogService;
+
+    /**
+     * 获取到消息信息
+     * @return
+     */
+    @GetMapping("/chatAssistantContent")
+    public AjaxResult chatAssistantContent(long businessId){
+
+        MessageQueueEntity messageQueueDto = messageQueueService.queryMessage(businessId+"") ; //  smartBrainConsumer.chatContent(businessId) ;
+        log.debug("chatContent result = {}" , messageQueueDto);
+
+        return AjaxResult.success(messageQueueDto) ;
+    }
+
+    /**
+     * 前端用户发送消息
+     * @return
+     */
+    @PostMapping("/sendUserMessage")
+    public AjaxResult sendUserMessage(@RequestBody List<WebMessageDto> dtoList , long channelId , String type){
+
+        log.debug("dtoList = {}" , JSONObject.toJSONString(dtoList));
+
+        String text = AgentUtils.getText(dtoList)  ;
+        long roleId = AgentUtils.getRoleId(dtoList) ;
+        String preBusinessId = AgentUtils.getPreBusinessId(dtoList)  ;
+        long businessId = IdUtil.getSnowflakeNextId(); // 生成一个唯一的业务ID
+
+        IndustryRoleEntity roleDto = roleService.getById(roleId)  ;
+
+        // 提交任务给处理服务，让它后台执行处理
+        taskService.addTask(channelId , businessId , roleId , text , preBusinessId , roleDto);
+
+        ChatMessageDto personDto = AgentUtils.getChatMessageDto(dtoList, roleDto , businessId , text);
+
+        // 保存消息实体
+//        long receiverId = 1L ; // 当前用户的id
+
+        messageService.saveChatMessage(dtoList , roleDto , personDto , channelId , businessId) ;
+        return AjaxResult.success(personDto) ;
+    }
+
+
+    /**
+     * 获取到消息信息
+     * @return
+     */
+    @PostMapping("/updateAssistantContent")
+    public AjaxResult updateAssistantContent(@RequestBody TaskContentDto dto){
+
+        generateTaskService.modifyContent(dto);
+
+        return ok() ;
+    }
+
+    /**
+     * 执行下一个节点的任务
+     * @return
+     */
+    @GetMapping("/runChainAgent")
+    public AjaxResult runChainAgent(String businessId , String roleId){
+
+        log.debug("businessId = {} , roleId = {}" , businessId , roleId);
+        TaskContentDto dto = generateTaskService.getContentByBusinessId(businessId) ;
+
+        log.debug("chatContent result = {}" , dto);
+
+//        String resultData = result.get("data").toString() ;
+//        TaskContentDto ta = null ;
+//        if(resultData != null) {
+//            ta = JSONObject.parseObject(resultData, TaskContentDto.class);
+//        }
+//        smartAssistantConsumer.runChainAgent(ta , roleId) ;
+
+        return ok() ;
+    }
+
+    /**
+     * 轮训任务结果，这里假设并发不高的情况下执行
+     * TODO 后续再进一步优化
+     * @return
+     */
+    @GetMapping("/getTaskNotice")
+    public AjaxResult getTaskNotice(){
+        return AjaxResult.success("查询成功" , taskService.getTaskMessage()) ;
+    }
+
+    /**
+     * 轮训任务运行状态，这里假设并发不高的情况下执行
+     * TODO 后续再进一步优化
+     * @return
+     */
+    @GetMapping("/getFlowTaskNotice")
+    public AjaxResult getFlowTaskNotice(){
+        return AjaxResult.success("查询成功" , taskService.getFlowTaskNotice()) ;
+    }
+
+    /**
+     * 获取到消息信息
+     * @return
+     */
+    @GetMapping("/chatMessage")
+    public AjaxResult chatMessage(String channelId){
+
+        List<ChatMessageDto> chatMessageDtos = messageService.listByChannelId(channelId) ;
+
+        if(chatMessageDtos == null || chatMessageDtos.isEmpty()){
+            chatMessageDtos = new ArrayList<>()  ;
+
+            // 完成之后发送消息给前端
+            ChatMessageDto agentInfo = new ChatMessageDto() ;
+
+            agentInfo.setChatText("你好，你可以查看一下使用教程<a target='_blank' href='http://portal.infra.linesno.com'>教程</a>或者@你想咨询的Agent.");
+            agentInfo.setName("Agent小助理");
+            agentInfo.setRoleType("agent");
+            agentInfo.setReaderType("html");
+            agentInfo.setBusinessId(IdUtil.getSnowflakeNextId());
+            agentInfo.setDateTime(DateUtil.formatDateTime(new Date()));
+            agentInfo.setIcon("1746437666131714049");
+            agentInfo.setDateTime(DateUtil.formatDateTime(new Date()));
+
+            chatMessageDtos.add(agentInfo) ;
+        }
+
+        return AjaxResult.success(chatMessageDtos) ;
+    }
+
+    /**
+     * 获取到频道的Agent列表
+     * @return
+     */
+    @GetMapping("/getChannelAgent")
+    public AjaxResult getChannelAgent(String channelId){
+
+        List<UserEntity> userEntities = channelUserService.getChannelAgent(channelId) ;
+
+        return AjaxResult.success(userEntities) ;
+    }
+
+
+    /**
+     * 获取所有频道列表
+     * @return
+     */
+    @GetMapping("/getAllCatalog")
+    public AjaxResult getAllCatalog(){
+        return AjaxResult.success(industryRoleCatalogService.allCatalog()) ;
+    }
+
+
+    /**
+     * 发送消息给钉钉
+     * @return
+     */
+    @SneakyThrows
+    @GetMapping("/send")
+    public AjaxResult robotSend() {
+
+        RobotMessageDto dto = new RobotMessageDto() ;
+
+        dto.setAtAll(false);
+        dto.setMessageType("markdown");
+        dto.setMessageContent("你要求生成的Java计算机考核题目已经生成，请查看连接.");
+
+        // smartAssistantConsumer.sendMessageWebhook(dto, null) ;
+
+        return ok() ;
+    }
+
+
+
+}
