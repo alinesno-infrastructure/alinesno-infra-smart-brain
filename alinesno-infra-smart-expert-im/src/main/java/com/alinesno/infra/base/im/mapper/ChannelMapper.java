5600a3260447251bfaad75641f07806b55474dfb
陈伟(Victor) <victorAip@gmail.com>
2024-08-29 23:15:00 -0400

    新增 ChannelMapper 接口文件，用于 ChannelEntity 的数据操作。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.ChannelEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface ChannelMapper extends IBaseMapper<ChannelEntity> {
+    
+}
