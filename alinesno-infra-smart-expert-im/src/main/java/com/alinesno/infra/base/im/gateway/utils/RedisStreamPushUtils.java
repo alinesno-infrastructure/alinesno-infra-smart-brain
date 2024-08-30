5b24f90631ad3096152090cdb45aa20829a2ba23
黄涛(Leo) <leoAip@gmail.com>
2024-08-29 22:59:00 -0400

    新增 RedisStreamPushUtils 类，用于处理 Redis 流推送任务和聊天消息。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/utils/RedisStreamPushUtils.java
@@ -0,0 +1,35 @@
+//package com.alinesno.infra.base.im.gateway.utils;
+//
+//import com.alinesno.infra.base.im.gateway.stream.StreamConstants;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.connection.stream.Record;
+//import org.springframework.data.redis.core.StringRedisTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Slf4j
+//public class RedisStreamPushUtils {
+//
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
+//
+//    /**
+//     * 发送任务消息
+//     * @param msgContext
+//     */
+//    public void pushTask(String msgContext){
+//        stringRedisTemplate.opsForStream().add(Record.of(msgContext).withStreamKey(StreamConstants.IM_TASK_LOG));
+//        log.info("{}已发送消息:{}", StreamConstants.IM_TASK_LOG,msgContext);
+//    }
+//
+//    /**
+//     * 发送聊天消息
+//     * @param msgContext
+//     */
+//    public void pushMessage(String msgContext){
+//        stringRedisTemplate.opsForStream().add(Record.of(msgContext).withStreamKey(StreamConstants.IM_MESSAGE_LOG));
+//        log.info("{}已发送消息:{}", StreamConstants.IM_MESSAGE_LOG,msgContext);
+//    }
+//
+//}
