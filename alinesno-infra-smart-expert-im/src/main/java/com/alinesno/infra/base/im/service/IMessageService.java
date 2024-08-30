db3411a0f6ae32b8cc2940be6da182cacc323d4f
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 05:15:00 -0400

    新增IMessageService接口，包含处理消息相关的方法。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IMessageService.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.base.im.service;
+
+import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import com.alinesno.infra.base.im.dto.WebMessageDto;
+import com.alinesno.infra.base.im.entity.MessageEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+
+import java.util.List;
+
+public interface IMessageService extends IBaseService<MessageEntity> {
+
+    /**
+     * 保存用户所属频道消息
+     * @param parsedMessages
+     * @param channelId
+     */
+    void saveUserMessage(List<WebMessageDto> parsedMessages, Long channelId);
+
+    /**
+     * 查询出频道当前所有的消息并转换返回
+     * @param channelId
+     * @return
+     */
+    List<ChatMessageDto> listByChannelId(String channelId);
+
+    /**
+     * 保存用户的返回信息
+     *
+     * @param dtoList
+     * @param personDto
+     * @param channelId
+     */
+    void saveChatMessage(List<WebMessageDto> dtoList, IndustryRoleEntity roleDto , ChatMessageDto personDto, long channelId , long businessId);
+
+    /**
+     * 保存消息实体
+     * @param personDto
+     * @param channelId
+     */
+    void saveChatMessage(ChatMessageDto personDto, Long channelId);
+}
