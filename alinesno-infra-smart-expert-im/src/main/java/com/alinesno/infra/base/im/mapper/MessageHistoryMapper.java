951224f19d95d75d14cf877c72bdb19a711e5447
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 08:50:00 -0400

    新增MessageHistoryMapper接口文件。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/MessageHistoryMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.MessageHistoryEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface MessageHistoryMapper extends IBaseMapper<MessageHistoryEntity> {
+    
+}
