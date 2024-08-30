7910d724396a8e476a54a732fea2f30935f8fd88
林晓(Lily) <lilyAip@gmail.com>
2024-08-30 03:05:00 -0400

    fix: 更新src/main.js文件，添加全局truncateString方法。

--- alinesno-infra-smart-expert-ui/src/main.js
+++ alinesno-infra-smart-expert-ui/src/main.js
@@ -24,7 +24,7 @@ import elementIcons from '@/components/SvgIcon/svgicon'
 import './permission' // permission control
 
 import { useDict } from '@/utils/dict'
-import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
+import { parseTime, truncateString , resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
 
 // 分页组件
 import Pagination from '@/components/Pagination'
@@ -51,6 +51,7 @@ app.config.globalProperties.$echarts = echarts
 
 // 全局方法挂载
 app.config.globalProperties.useDict = useDict
+app.config.globalProperties.truncateString = truncateString 
 app.config.globalProperties.download = download
 app.config.globalProperties.parseTime = parseTime
 app.config.globalProperties.resetForm = resetForm
