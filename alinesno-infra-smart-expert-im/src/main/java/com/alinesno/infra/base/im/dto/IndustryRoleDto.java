a238d85df64870c284ff5bbd9464b2215b114a84
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 10:23:00 -0400

    新增 IndustryRoleDto 实体类，用于表示不同行业中的角色信息。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/dto/IndustryRoleDto.java
@@ -0,0 +1,37 @@
+//package com.alinesno.infra.base.im.dto;
+//
+//import lombok.Data;
+//
+///**
+// * 行业角色实体类，用于表示不同行业中的角色信息
+// */
+//@Data
+//public class IndustryRoleDto {
+//
+//    private Long id ;
+//
+//    private String roleAvatar; // 角色头像
+//
+//    private String roleName; // 角色名称
+//
+//    private String industry; // 所属行业
+//
+//    private String responsibilities; // 角色职责描述
+//
+//    private String skills; // 所需技能描述
+//
+//    private String experience; // 经验要求描述
+//
+//    private String roleLevel; // 角色等级
+//
+//    private String salaryRange; // 薪资范围
+//
+//    private String education; // 教育背景要求
+//
+//    private String otherAttributes; // 其他角色相关字段
+//
+//    private Long chainId; // 其他角色相关字段
+//
+//    private String promptId ; // 其他角色相关字段
+//
+//}
