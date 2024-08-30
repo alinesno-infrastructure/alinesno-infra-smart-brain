c0d88a86758e89cca6125c5505748f739adc5bb2
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 00:30:00 -0400

    fix: 更新 IScheduleService 接口引用至 ScheduleEntity 类

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IScheduleService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IScheduleService.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.service;
 
-import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
 
 /**
  * <p>
