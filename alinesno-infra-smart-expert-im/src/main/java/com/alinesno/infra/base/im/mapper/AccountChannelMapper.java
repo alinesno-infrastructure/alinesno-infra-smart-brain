225c86b4f2f356adabc6ea5fb94e85eeda0e6624
陈伟(Victor) <victorAip@gmail.com>
2024-08-29 22:25:00 -0400

    新增AccountChannelMapper接口文件。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/AccountChannelMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.AccountChannelEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface AccountChannelMapper extends IBaseMapper<AccountChannelEntity> {
+    
+}
