7336f81b9943c9533cb626e1c0ec0189ef67a9e6
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 02:53:00 -0400

    修复了ExecutorGPTTest类中的测试用例执行逻辑

--- alinesno-infra-smart-brain-inference/src/test/java/com/alinesno/infra/smart/brain/inference/executor/ExecutorGPTTest.java
+++ alinesno-infra-smart-brain-inference/src/test/java/com/alinesno/infra/smart/brain/inference/executor/ExecutorGPTTest.java
@@ -1,15 +1,8 @@
 package com.alinesno.infra.smart.brain.inference.executor;
 
-import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.xml.transform.stream.StreamSource;
-import java.io.IOException;
-import java.io.InputStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ExecutorGPTTest {
 
