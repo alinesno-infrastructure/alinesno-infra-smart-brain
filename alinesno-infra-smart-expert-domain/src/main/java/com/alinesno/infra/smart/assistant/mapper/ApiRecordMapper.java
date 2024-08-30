37ee2939fd09b32039998678c7b29986ff747c16
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 03:52:00 -0400

    fix:优化ApiRecordMapper接口注释

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/ApiRecordMapper.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/ApiRecordMapper.java
@@ -3,8 +3,6 @@ package com.alinesno.infra.smart.assistant.mapper;
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.assistant.entity.ApiRecordEntity;
 
-import java.util.List;
-
 /**
  * <p>
  * 持久层接口
