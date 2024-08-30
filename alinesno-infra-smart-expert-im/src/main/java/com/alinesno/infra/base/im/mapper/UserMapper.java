a9f9ec43f332c5efd21f01f9a509dc9e07329082
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 05:18:00 -0400

    新增UserMapper接口文件，用于操作UserEntity实体类。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.UserEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface UserMapper extends IBaseMapper<UserEntity> {
+
+}
