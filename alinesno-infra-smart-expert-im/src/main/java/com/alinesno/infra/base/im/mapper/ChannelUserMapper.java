962024d2f951c49940b1f931de3dc2425529f42f
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 07:55:00 -0400

    新增 ChannelUserMapper 接口及实体映射文件。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelUserMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.ChannelUserEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface ChannelUserMapper extends IBaseMapper<ChannelUserEntity> {
+    
+}
