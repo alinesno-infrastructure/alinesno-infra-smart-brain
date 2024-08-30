68458852f7f6233fcd65a4551014d75d527877dd
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 01:58:00 -0400

    新增TableItem类，包含任务状态、业务ID等属性及构造方法。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/dto/TableItem.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.base.im.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+@Data
+public class TableItem {
+    private String taskStatus;
+    private String businessId;
+    private String channel ;
+    private String taskType;
+    private String taskName;
+    private String assistantContent;
+    private String usageTime;
+
+    public TableItem(String taskStatus, String businessId, String taskType, String taskName, String assistantContent, String usageTime) {
+        this.taskStatus = taskStatus;
+        this.businessId = businessId;
+        this.taskType = taskType;
+        this.taskName = taskName;
+        this.assistantContent = assistantContent;
+        this.usageTime = usageTime;
+    }
+}
