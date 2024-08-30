528d9db1ea3e4bede1a769d7b3e1d7a6eb1bfb56
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 00:52:00 -0400

    fix:修复RequestRecordServiceImpl类中引用的实体类ApiKeyEntity路径问题

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RequestRecordServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RequestRecordServiceImpl.java
@@ -1,9 +1,7 @@
 package com.alinesno.infra.smart.assistant.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.entity.ApiKeyEntity;
 import com.alinesno.infra.smart.assistant.entity.RequestRecordEntity;
-import com.alinesno.infra.smart.assistant.mapper.ApiKeyMapper;
 import com.alinesno.infra.smart.assistant.mapper.RequestRecordMapper;
 import com.alinesno.infra.smart.assistant.service.IRequestRecordService;
 import lombok.extern.slf4j.Slf4j;
