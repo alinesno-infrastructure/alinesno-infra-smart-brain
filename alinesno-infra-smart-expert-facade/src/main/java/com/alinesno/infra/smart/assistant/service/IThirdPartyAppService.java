e8c3eae515b989e4cc4f8c852ef520a19580600c
陈伟(Victor) <victorAip@gmail.com>
2024-08-30 06:05:00 -0400

    fix: 更新 ThirdPartyAppService 接口引用至正确实体类

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IThirdPartyAppService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IThirdPartyAppService.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.service;
 
-import com.alinesno.infra.smart.assistant.entity.ThirdPartyAppEntity;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ThirdPartyAppEntity;
 
 /**
  * <p>
