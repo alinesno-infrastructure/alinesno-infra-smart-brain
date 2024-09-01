38d8c830389c75810788ca4de8f1a815f8b9966b
林晓(Lily) <lilyAip@gmail.com>
2024-08-31 21:52:00 -0400

    新增 TopHeader 组件 banner.vue 文件，包含导航链接与企业logo展示功能。

---
+++ alinesno-infra-smart-im-ui/src/layout/components/TopHeader/banner.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <router-link tag="div" class="header-logo-bar" to="/index">
+      <div class="header-logo" v-if="enableLogo" @click="enterDomain">
+        <img :src="saasLogoUrl" alt="" />
+      </div>
+      <a :title="saasTitle" target="_self" class="header-logo-label">
+        <span>{{ saasTitle }} </span>
+      </a>
+      <div v-if="saasUrl" class="dashboard-home" @click="dashboardHome()">
+        <i class="fa-solid fa-house"></i> 工作台
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TopHeader',
+  components: {
+  },
+  computed: {
+  },
+  data() {
+
+    let saasTitle = 'AIP智能体平台'
+    let enableLogo = true;
+    let saasUrl = 'http://portal.infra.linesno.com/' ; // http://alinesno-infra-plat-console-ui.beta.plat.infra.linesno.com' ;
+    let saasLogoUrl = 'http://portal.infra.linesno.com/logo.png' ;
+    let displayUrl = ''; 
+
+
+    return {
+      saasTitle,
+      saasUrl,
+      saasLogoUrl,
+      enableLogo,
+      displayUrl,
+      domainName: null,
+    }
+  },
+  created() {
+  },
+  methods: {
+    dashboardHome() {
+      window.open(this.saasUrl, '_blank')
+    },
+    // 进入企业官网
+    enterDomain() {
+      if (this.domainName) { // 跳转进入官网
+        window.open(this.domainName)
+      }
+    }
+  }
+}
+</script>
