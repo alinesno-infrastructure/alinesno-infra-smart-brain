de95a64106714f395d161f1762fedb9dd86114cd
林晓(Lily) <lilyAip@gmail.com>
2024-08-31 20:53:00 -0400

    创建 application API 接口文件

---
+++ alinesno-infra-smart-im-ui/src/api/base/im/application.js
@@ -0,0 +1,96 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/application/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'save' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  downloadfile: prefix + "downloadfile"
+}
+
+// 查询应用列表
+export function listApplication(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询应用详细
+export function getApplication(ApplicationId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(ApplicationId),
+    method: 'get'
+  })
+}
+
+// 新增应用
+export function addApplication(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改应用
+export function updateApplication(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除应用
+export function delApplication(ApplicationId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(ApplicationId),
+    method: 'delete'
+  })
+}
+
+// 应用密码重置
+export function resetApplicationPwd(ApplicationId, password) {
+  const data = {
+    ApplicationId,
+    password
+  }
+  return request({
+    url: '/api/infra/base/starter/application/resetPwd',
+    method: 'put',
+    data: data
+  })
+}
+
+// 应用状态修改
+export function changeApplicationStatus(ApplicationId, status) {
+  const data = {
+    ApplicationId,
+    status
+  }
+  return request({
+    url: '/api/infra/base/starter/application/changeStatus',
+    method: 'put',
+    data: data
+  })
+}
+
+// 查询部门下拉树结构
+export function deptTreeSelect() {
+  return request({
+    url: '/api/infra/base/starter/application/deptTree',
+    method: 'get'
+  })
+}
