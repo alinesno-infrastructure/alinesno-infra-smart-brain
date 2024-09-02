7c8520a3aa44d48f84f777d6a85ba7406ee1314a
张婷(Tina) <tinaAip@gmail.com>
2024-09-02 05:49:00 -0400

    修复并扩展了src/main.js文件中的公用工具函数与组件引用。

--- alinesno-infra-smart-im-ui/src/main.js
+++ alinesno-infra-smart-im-ui/src/main.js
@@ -24,7 +24,7 @@ import elementIcons from '@/components/SvgIcon/svgicon'
 import './permission' // permission control
 
 import { useDict } from '@/utils/dict'
-import { parseTime, resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
+import { parseTime, truncateString ,resetForm, addDateRange, handleTree, selectDictLabel, selectDictLabels } from '@/utils/ruoyi'
 
 // 分页组件
 import Pagination from '@/components/Pagination'
@@ -56,6 +56,7 @@ app.config.globalProperties.GLOBAL_FUN = GLOBAL_FUN
 app.config.globalProperties.useDict = useDict
 app.config.globalProperties.download = download
 app.config.globalProperties.parseTime = parseTime
+app.config.globalProperties.truncateString = truncateString 
 app.config.globalProperties.resetForm = resetForm
 app.config.globalProperties.handleTree = handleTree
 app.config.globalProperties.addDateRange = addDateRange
