7d1a138258739c02cd89aafe59aa0546c9d7c0e4
李娜(Allen) <allenAip@gmail.com>
2024-09-10 00:23:22 +0800

    新增智能体知识库实体类 `AgentKnowledgeEntity`。

---
+++ alinesno-infra-smart-inferen-facade/src/main/java/com/alinesno/infra/smart/inference/agent/entity/AgentKnowledgeEntity.java
@@ -0,0 +1,34 @@
+package com.alinesno.infra.smart.inference.agent.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 智能体知识库，每个智能体有多个智能库
+ */
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@TableName("life_agent_knowledge")
+@Data
+@NoArgsConstructor
+public class AgentKnowledgeEntity extends InfraBaseEntity {
+
+    @TableField
+    private long agentId ; // 智能体ID
+
+    @TableField
+    private long knowledgeId ; // 知识库ID
+
+    @TableField
+    private String indexName ;   // 索引名称
+
+}
