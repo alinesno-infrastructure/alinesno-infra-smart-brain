3e2625b7ef686b796ff890e6ea6a33aebbcad155
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:24 +0800

    新增测试类 MessageParserTest，用于测试 MessageParser 工具类的功能。

---
+++ alinesno-infra-smart-expert-im/src/test/java/com/alinesno/infra/base/im/gateway/utils/MessageParserTest.java
@@ -0,0 +1,34 @@
+package com.alinesno.infra.base.im.gateway.utils;
+
+import com.alinesno.infra.base.im.utils.MessageParser;
+import org.junit.Before;
+
+public class MessageParserTest {
+
+    private String message;
+
+    @Before
+    public void setUp() {
+        message = "This is a test message @user1 @user2 and #project1.0 #project2.0";
+    }
+
+
+    public static void main(String[] args) {
+        String message = "This is a test message @user1 @user2 and #1.0 #project2.0";
+
+        MessageParser.ParsedData parsedData = MessageParser.parseMessage(message);
+
+        System.out.println("提到的用户名:");
+        for (String username : parsedData.getUsernames()) {
+            System.out.println(username);
+        }
+
+        System.out.println("项目代号:");
+        for (String projectCode : parsedData.getProjectCodes()) {
+            System.out.println(projectCode);
+        }
+
+        System.out.println("用户名数量: " + parsedData.getUserCount());
+        System.out.println("项目代号数量: " + parsedData.getProjectCount());
+    }
+}
