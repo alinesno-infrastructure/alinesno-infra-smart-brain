36c11049459f55700dd60ff240ff4ce82e964b84
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:24 +0800

    feat:实现消息格式化工具类，包含提及、业务订单号和文本内容的处理。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/utils/MessageFormatter.java
@@ -0,0 +1,61 @@
+package com.alinesno.infra.base.im.utils;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageFormatter {
+
+
+
+    public static List<MsgMessageContent> formatMessage(String message) {
+        String[] words = message.split(" ");
+        List<MsgMessageContent> formattedMessage = new ArrayList<>();
+
+        for (String word : words) {
+            if (word.startsWith("@")) {
+                formattedMessage.add(new MsgMessageContent("mention", word)) ;
+            } else if (word.startsWith("#")) {
+                String orderId = word.substring(1);
+                formattedMessage.add(new MsgMessageContent("business", orderId));
+            } else {
+                formattedMessage.add(new MsgMessageContent("text", word));
+            }
+        }
+
+        return formattedMessage;
+    }
+
+    /**
+     * 消息处理工具类
+     * @param message
+     * @return
+     */
+    public static String getMessage(String message) {
+        List<MsgMessageContent> formattedMessage = formatMessage(message);
+
+        StringBuilder messageContent = new StringBuilder();
+        for (MsgMessageContent content : formattedMessage) {
+            if ("mention".equals(content.getType())) {
+                messageContent.append("<span class=\"mention\">").append(content.getValue()).append("</span>");
+            } else if ("business".equals(content.getType())) {
+                messageContent.append("<span class=\"mention-business\">").append(content.getValue()).append("</span>");
+            } else if ("text".equals(content.getType())){
+                messageContent.append(content.getValue()).append(" ");
+            }
+        }
+        return messageContent.toString();
+    }
+
+    @Data
+    public static class MsgMessageContent {
+        private String type;
+        private String value;
+
+        public MsgMessageContent(String type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+    }
+}
