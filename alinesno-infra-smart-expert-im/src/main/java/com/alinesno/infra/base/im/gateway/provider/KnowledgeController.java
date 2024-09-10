791cf765fc599e860db027260942106177e23328
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:25 +0800

    优化导入文件处理逻辑，并引入文件类型检测功能。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/KnowledgeController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/KnowledgeController.java
@@ -1,15 +1,16 @@
 package com.alinesno.infra.base.im.gateway.provider;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.io.FileTypeUtil;
 import com.alinesno.infra.base.im.dto.ChatMessageDto;
 import com.alinesno.infra.base.im.service.IMessageService;
 import com.alinesno.infra.base.im.service.ITaskService;
+import com.alinesno.infra.base.im.utils.AgentUtils;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.smart.assistant.adapter.BaseSearchController;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.util.Date;
+import java.util.Objects;
 
 /**
  * 文档控制层
@@ -37,6 +38,9 @@ public class KnowledgeController {
     @Autowired
     private IMessageService messageService ;
 
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
+
     /**
      * 导入数据
      *
@@ -45,36 +49,28 @@ public class KnowledgeController {
     @PostMapping(value = "/importData", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public AjaxResult importData(@RequestPart("file") MultipartFile file, Long channelId) throws Exception {
 
+        File tmpFile = new File(localPath , Objects.requireNonNull(file.getOriginalFilename())) ;
+        file.transferTo(tmpFile);
+
+        String fileType = FileTypeUtil.getType(tmpFile) ;
+
         // 获取原始文件名
         String fileName = file.getOriginalFilename();
-
-        // 完成之后发送消息给前端
-        ChatMessageDto personDto = new ChatMessageDto() ;
-
-        personDto.setChatText("<div style='color: #409EFF;padding: 3px;'><i class='fa-solid fa-file-word icon-word'></i> "+fileName+" </div> 已上传文件成功.");
-        personDto.setName("考核题目生成Agent");
-        personDto.setRoleType("person");
-        personDto.setReaderType("html");
-        personDto.setBusinessId(IdUtil.getSnowflakeNextId());
-        personDto.setDateTime(DateUtil.formatDateTime(new Date()));
-        personDto.setIcon("1746465675916124161") ;
-        personDto.setDateTime(DateUtil.formatDateTime(new Date()));
+        ChatMessageDto personDto = AgentUtils.getUploadChatMessageDto(fileName , fileType);
 
         // 上传到知识库角色
         String datasetId = "1735980565715537922" ;
-        File tmpFile = new File("/tmp/" + file.getOriginalFilename()) ;
-        file.transferTo(tmpFile);
 
-        AjaxResult result = searchController.datasetUpload(tmpFile.getAbsolutePath() , datasetId , progress -> {
-            log.debug("total bytes: " + progress.getTotalBytes());   // 文件大小
-            log.debug("current bytes: " + progress.getCurrentBytes());   // 已上传字节数
-            log.debug("progress: " + Math.round(progress.getRate() * 100) + "%");  // 已上传百分比
-            if (progress.isDone()) {   // 是否上传完成
-                log.debug("--------   Upload Completed!   --------");
-            }
-        }) ;
+//        AjaxResult result = searchController.datasetUpload(tmpFile.getAbsolutePath() , datasetId , progress -> {
+//            log.debug("total bytes: " + progress.getTotalBytes());   // 文件大小
+//            log.debug("current bytes: " + progress.getCurrentBytes());   // 已上传字节数
+//            log.debug("progress: " + Math.round(progress.getRate() * 100) + "%");  // 已上传百分比
+//            if (progress.isDone()) {   // 是否上传完成
+//                log.debug("--------   Upload Completed!   --------");
+//            }
+//        }) ;
+//        log.debug("upload file result = {}" , result);
 
-        log.debug("upload file result = {}" , result);
         FileUtils.forceDelete(tmpFile);
 
         // 保存消息实体
@@ -82,5 +78,7 @@ public class KnowledgeController {
 
         return AjaxResult.success(personDto) ;
     }
+
+
 
 }
