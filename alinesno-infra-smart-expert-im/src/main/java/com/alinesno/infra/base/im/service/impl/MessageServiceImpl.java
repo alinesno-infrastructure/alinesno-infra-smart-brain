4b19f5e77b80e4755bdc68c6d977be04632bf8d9
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:25 +0800

    feat:优化消息服务，增加消息格式化处理并支持行业角色服务集成。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/MessageServiceImpl.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/MessageServiceImpl.java
@@ -3,24 +3,33 @@ package com.alinesno.infra.base.im.service.impl;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import com.alinesno.infra.base.im.dto.ChatSendMessageDto;
 import com.alinesno.infra.base.im.dto.WebMessageDto;
 import com.alinesno.infra.base.im.entity.MessageEntity;
-import com.alinesno.infra.smart.im.enums.MessageType;
 import com.alinesno.infra.base.im.mapper.MessageMapper;
 import com.alinesno.infra.base.im.service.IMessageService;
+import com.alinesno.infra.base.im.utils.MessageFormatter;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.enums.MessageType;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class MessageServiceImpl extends IBaseServiceImpl<MessageEntity , MessageMapper> implements IMessageService {
+
+    @Autowired
+    private IIndustryRoleService industryRoleService ;
 
     @Override
     public void saveUserMessage(List<WebMessageDto> parsedMessages, Long channelId) {
@@ -77,7 +86,7 @@ public class MessageServiceImpl extends IBaseServiceImpl<MessageEntity , Message
             for(MessageEntity e : entityList){
                 ChatMessageDto dto = new ChatMessageDto() ;
 
-                dto.setChatText(e.getContent());
+                dto.setChatText(StringUtils.isBlank(e.getFormatContent())?e.getContent():e.getFormatContent());
 
                 dto.setName(e.getName());
                 dto.setIcon(e.getIcon());
@@ -94,53 +103,54 @@ public class MessageServiceImpl extends IBaseServiceImpl<MessageEntity , Message
         return list ;
     }
 
+    @Deprecated
     @Override
     public void saveChatMessage(List<WebMessageDto> parsedMessages, IndustryRoleEntity roleDto , ChatMessageDto personDto, long channelId , long businessId) {
-        // 处理解析后的消息对象
-        StringBuilder receiverId = new StringBuilder();
-        for (WebMessageDto message : parsedMessages) {
-            if (MessageType.MENTION.getValue().equals(message.getType())) {
-                receiverId.append(message.getId());
-                receiverId.append("\\|");
-            }
-        }
-        StringBuilder chatTextBuilder = new StringBuilder();
-
-        // 假设Content是一个自定义类，包含type、text、username和businessId属性
-        for (WebMessageDto content : parsedMessages) {
-            if ("text".equals(content.getType())) {
-                chatTextBuilder.append("<span class=\"mention-text\">").append(content.getText()).append("</span>");
-            } else if ("mention".equals(content.getType())) {
-                chatTextBuilder.append("<span class=\"mention\">@").append(content.getUsername()).append("</span>");
-            } else if ("business".equals(content.getType())) {
-                chatTextBuilder.append("<span class=\"mention-business\">#").append(content.getBusinessId()).append("</span>");
-            }
-        }
-        MessageEntity msg = getMessageEntity(channelId, chatTextBuilder, receiverId);
-
-        save(msg) ;
-
-
-        MessageEntity entity = new MessageEntity() ;
-
-        entity.setContent(personDto.getChatText().toString()) ;
-
-        entity.setIcon(personDto.getIcon()) ;
-        entity.setName(personDto.getName());
-
-        entity.setRoleType(personDto.getRoleType());
-        entity.setReaderType(personDto.getReaderType());
-        entity.setBusinessId(businessId);
-        entity.setAddTime(new Date()) ;
-
-        entity.setMessageId(IdUtil.getSnowflakeNextId());
-        entity.setChannelId(channelId);
-
-        entity.setSenderId(roleDto.getId()); // 发送者ID
-
-        entity.setReceiverId(receiverId.toString());
-
-        save(entity) ;
+//        // 处理解析后的消息对象
+//        StringBuilder receiverId = new StringBuilder();
+//        for (WebMessageDto message : parsedMessages) {
+//            if (MessageType.MENTION.getValue().equals(message.getType())) {
+//                receiverId.append(message.getId());
+//                receiverId.append("\\|");
+//            }
+//        }
+//        StringBuilder chatTextBuilder = new StringBuilder();
+//
+//        // 假设Content是一个自定义类，包含type、text、username和businessId属性
+//        for (WebMessageDto content : parsedMessages) {
+//            if ("text".equals(content.getType())) {
+//                chatTextBuilder.append("<span class=\"mention-text\">").append(content.getText()).append("</span>");
+//            } else if ("mention".equals(content.getType())) {
+//                chatTextBuilder.append("<span class=\"mention\">@").append(content.getUsername()).append("</span>");
+//            } else if ("business".equals(content.getType())) {
+//                chatTextBuilder.append("<span class=\"mention-business\">#").append(content.getBusinessId()).append("</span>");
+//            }
+//        }
+//        MessageEntity msg = getMessageEntity(channelId, chatTextBuilder, receiverId);
+//
+//        save(msg) ;
+//
+//
+//        MessageEntity entity = new MessageEntity() ;
+//
+//        entity.setContent(personDto.getChatText().toString()) ;
+//
+//        entity.setIcon(personDto.getIcon()) ;
+//        entity.setName(personDto.getName());
+//
+//        entity.setRoleType(personDto.getRoleType());
+//        entity.setReaderType(personDto.getReaderType());
+//        entity.setBusinessId(businessId);
+//        entity.setAddTime(new Date()) ;
+//
+//        entity.setMessageId(IdUtil.getSnowflakeNextId());
+//        entity.setChannelId(channelId);
+//
+//        entity.setSenderId(roleDto.getId()); // 发送者ID
+//
+//        entity.setReceiverId(receiverId.toString());
+//
+//        save(entity) ;
     }
 
     @Override
@@ -165,22 +175,77 @@ public class MessageServiceImpl extends IBaseServiceImpl<MessageEntity , Message
         save(entity) ;
     }
 
-    private static MessageEntity getMessageEntity(Long channelId, StringBuilder chatTextBuilder, StringBuilder receiverId) {
-        String chatText = chatTextBuilder.toString();
+    @Override
+    public void sendUserMessage(ChatSendMessageDto message, List<ChatMessageDto> personDto) {
 
+        // 生成消息记录
+        // 处理解析后的消息对象
+        String ids = message.getUsers().stream().map(String::valueOf).collect(Collectors.joining(","));
+
+        MessageEntity msg = getMessageEntity(
+                message.getChannelId(),
+                message.getMessage(),
+                MessageFormatter.getMessage(message.getMessage()),
+                ids
+        );
+
+        save(msg) ;
+
+        // 保存返回消息
+       for(ChatMessageDto dto : personDto) {
+           saveChatMessage(dto , message.getChannelId()) ;
+       }
+
+       // 触发执行任务
+
+    }
+
+    @Override
+    public void initChannelHelp(String channelId) {
+
+        long count = count(new LambdaQueryWrapper<MessageEntity>().eq(MessageEntity::getChannelId , channelId)) ;
+        if(count == 0){
+            // 完成之后发送消息给前端
+            MessageEntity agentInfo = new MessageEntity() ;
+
+            IndustryRoleEntity defaultHelpAgent = industryRoleService.getDefaultHelpAgent() ;
+
+            agentInfo.setFormatContent("你好，你可以查看一下使用教程<a target='_blank' href='http://portal.infra.linesno.com'>教程</a>或者@你想咨询的Agent.");
+            agentInfo.setName(defaultHelpAgent.getRoleName());
+            agentInfo.setIcon(defaultHelpAgent.getRoleAvatar());
+
+            agentInfo.setMessageId(IdUtil.getSnowflakeNextId());
+            agentInfo.setRoleType("agent");
+            agentInfo.setReaderType("html");
+
+            agentInfo.setAddTime(new Date());
+
+            agentInfo.setSenderId(defaultHelpAgent.getId());
+            agentInfo.setChannelId(Long.valueOf(channelId));
+            agentInfo.setReceiverId(IdUtil.getSnowflakeNextIdStr());
+            agentInfo.setBusinessId(IdUtil.getSnowflakeNextId());
+
+            save(agentInfo);
+        }
+
+    }
+
+    private static MessageEntity getMessageEntity(Long channelId, String content ,  String chatText , String receiverId) {
         MessageEntity msg = new MessageEntity() ;
 
         msg.setMessageId(IdUtil.getSnowflakeNextId());
         msg.setChannelId(channelId);
         msg.setSenderId(IdUtil.getSnowflakeNextId());
 
-        msg.setIcon("1746465675916124161") ;
+        msg.setIcon("1808349839242747906") ;
         msg.setReaderType("html");
         msg.setRoleType("person");
         msg.setAddTime(new Date());
         msg.setName("软件工程师罗小东");
-        msg.setReceiverId(receiverId.toString());
-        msg.setContent(chatText)  ; // JSONObject.toJSONString(parsedMessages));
+        msg.setReceiverId(receiverId);
+        msg.setContent(content)  ;
+        msg.setFormatContent(chatText);
+
         return msg;
     }
 }
