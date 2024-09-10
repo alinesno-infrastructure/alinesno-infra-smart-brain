36625b5dc0c3e9471c5b9ad544782819e867cf18
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:24 +0800

    新增SSEMessageEventService类，用于触发自定义事件。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/event/SSEMessageEventService.java
@@ -0,0 +1,23 @@
+package com.alinesno.infra.base.im.event;
+
+import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SSEMessageEventService {
+
+    private final ApplicationEventPublisher publisher;
+
+    @Autowired
+    public SSEMessageEventService(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public void triggerCustomEvent(ChatMessageDto message) {
+        // 触发自定义事件
+        SSEMessageEvent customEvent = new SSEMessageEvent(this, message);
+        this.publisher.publishEvent(customEvent);
+    }
+}
