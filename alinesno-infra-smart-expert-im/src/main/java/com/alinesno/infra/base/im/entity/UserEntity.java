98472cd37252ca126fc5bcebdd24ef5e488a21fd
李伟(Michael) <michaelAip@gmail.com>
2024-09-11 00:16:24 +0800

    修复并优化了UserEntity类的实现，包括引入AccountType枚举和调整相关注解。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/entity/UserEntity.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/entity/UserEntity.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.im.entity;
 
-import com.alinesno.infra.smart.im.enums.AccountType;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.alinesno.infra.smart.im.enums.AccountType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
