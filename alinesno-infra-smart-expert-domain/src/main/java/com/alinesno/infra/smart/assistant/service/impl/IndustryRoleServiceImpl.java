4b3f95b3087bb3b04c406de291cbe6628b10eb01
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    修复并优化IndustryRoleServiceImpl类的功能实现。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/IndustryRoleServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/IndustryRoleServiceImpl.java
@@ -2,28 +2,16 @@ package com.alinesno.infra.smart.assistant.service.impl;
 
 import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.chain.IBaseExpertService;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.entity.NoticeEntity;
-import com.alinesno.infra.smart.assistant.entity.RoleChainEntity;
-import com.alinesno.infra.smart.assistant.im.dto.NoticeDto;
 import com.alinesno.infra.smart.assistant.mapper.IndustryRoleMapper;
-import com.alinesno.infra.smart.assistant.redis.MessageConstants;
-import com.alinesno.infra.smart.assistant.redis.PublishRedisService;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
-import com.alinesno.infra.smart.assistant.service.INoticeService;
-import com.alinesno.infra.smart.assistant.service.IRoleChainService;
 import com.alinesno.infra.smart.brain.api.reponse.TaskContentDto;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,50 +26,50 @@ import java.util.Map;
 @Service
 public class IndustryRoleServiceImpl extends IBaseServiceImpl<IndustryRoleEntity, IndustryRoleMapper> implements IIndustryRoleService {
 
-    @Autowired
-    private IRoleChainService roleChainService ;
+//    @Autowired
+//    private IRoleChainService roleChainService ;
 
     @Value("${spring.application.name}")
     private String applicationName;
 
-    @Autowired
-    private PublishRedisService publishService ;
+//    @Autowired
+//    private PublishRedisService publishService ;
 
-    @Resource(name="chainRunner")
-    private IBaseExpertService baseExpert ;
+//    @Resource(name="chainRunner")
+//    private IBaseExpertService baseExpert ;
 
-    @Autowired
-    private INoticeService noticeService ;
+//    @Autowired
+//    private INoticeService noticeService ;
 
     @Override
-    public void saveRoleChainInfo(RoleChainEntity chain , String roleId) {
+    public void saveRoleChainInfo(String roleId) {
 
-        chain.setEnable("1");
-        chain.setChainApplicationName(applicationName);
-        chain.setCreateTime(new Date());
-
-        roleChainService.save(chain) ;
-
-        IndustryRoleEntity role = this.getById(roleId) ;
-        role.setChainId(chain.getId());
-        this.update(role) ;
+//        chain.setEnable("1");
+//        chain.setChainApplicationName(applicationName);
+//        chain.setCreateTime(new Date());
+//
+//        roleChainService.save(chain) ;
+//
+//        IndustryRoleEntity role = this.getById(roleId) ;
+//        role.setChainId(chain.getId());
+//        this.update(role) ;
 
         // 发送消息用于规则的热更新
-        publishService.sendMsg(MessageConstants.RELOAD_RULE);
+//        publishService.sendMsg(MessageConstants.RELOAD_RULE);
     }
 
     @Override
-    public void runRoleChainByRoleId(Map<String , Object> params , long roleId , NoticeDto noticeDto) {
+    public void runRoleChainByRoleId(Map<String , Object> params , long roleId) {
 
         Assert.notNull(params , "请求参数为空.");
 
-        IndustryRoleEntity role = getById(roleId) ;
-        RoleChainEntity roleChain = roleChainService.getById(role.getChainId()) ;
+//        IndustryRoleEntity role = getById(roleId) ;
+//        RoleChainEntity roleChain = roleChainService.getById(role.getChainId()) ;
+//
+//        String chainName = roleChain.getChainName() ;
+//        Long chainId = roleChain.getId() ;
 
-        String chainName = roleChain.getChainName() ;
-        Long chainId = roleChain.getId() ;
-
-        baseExpert.processExpert(params , chainName , chainId , noticeDto);
+//        baseExpert.processExpert(params , chainName , chainId);
     }
 
     @Override
@@ -90,19 +78,19 @@ public class IndustryRoleServiceImpl extends IBaseServiceImpl<IndustryRoleEntity
         // TODO 待优化获取多个脚本的问题
         String msg = dto.getCodeContent().get(0).getContent();
 
-        NoticeEntity noticeEntity = noticeService.getByBusinessId(dto.getBusinessId()) ;
-        NoticeDto noticeDto = new NoticeDto() ;
-
-        if(noticeEntity != null){
-            BeanUtils.copyProperties(noticeEntity , dto);
-            noticeDto.setTaskName(dto.getGenContent());
-        }
+//        NoticeEntity noticeEntity = noticeService.getByBusinessId(dto.getBusinessId()) ;
+//        NoticeDto noticeDto = new NoticeDto() ;
+//
+//        if(noticeEntity != null){
+//            BeanUtils.copyProperties(noticeEntity , dto);
+//            noticeDto.setTaskName(dto.getGenContent());
+//        }
 
         // 发送培训相关的要求到任务中
         Map<String , Object> params = new HashMap<>() ;
         params.put("label1" , msg) ;
 
-        runRoleChainByRoleId(params , roleId , noticeDto);
+        runRoleChainByRoleId(params , roleId);
     }
 
     @Override
