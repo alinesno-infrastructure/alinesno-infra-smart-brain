72d2aa678bae55db44aab162553550758e13f5ee
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复并优化SSE消息事件监听器，处理自定义事件时根据频道ID发送消息，并捕获异常进行日志记录。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/event/SSEMessageEventListener.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/event/SSEMessageEventListener.java
@@ -3,10 +3,12 @@ package com.alinesno.infra.base.im.event;
 import com.alinesno.infra.base.im.dto.ChatMessageDto;
 import com.alinesno.infra.base.im.service.ISSEService;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class SSEMessageEventListener {
 
@@ -18,6 +20,12 @@ public class SSEMessageEventListener {
     public void handleCustomEvent(SSEMessageEvent event) {
 
         ChatMessageDto message = event.getMessage();
-        sseService.send("20" , message);
+        String channelId = String.valueOf(message.getChannelId());
+        try {
+            sseService.send(channelId , message);
+        }catch (Exception e){
+            log.error("send message error:{}",e.getMessage());
+        }
+
     }
 }
