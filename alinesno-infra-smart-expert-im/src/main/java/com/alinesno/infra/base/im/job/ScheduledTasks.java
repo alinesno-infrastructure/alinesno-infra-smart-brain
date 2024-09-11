fb78cad6a8843d711710b8feff07bd86c2879335
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复并优化定时任务调度逻辑。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/job/ScheduledTasks.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/job/ScheduledTasks.java
@@ -1,13 +1,9 @@
 package com.alinesno.infra.base.im.job;
 
 import com.alinesno.infra.base.im.event.SSEMessageEventService;
-import com.alinesno.infra.base.im.utils.AgentUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Component
@@ -17,9 +13,10 @@ public class ScheduledTasks {
     private SSEMessageEventService eventService;
 
     // 每隔 5 秒执行一次
-    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.info("现在的时间是: {}", LocalDateTime.now());
-        eventService.triggerCustomEvent(AgentUtils.genSSEChatMessageDto());
-    }
+//    @Scheduled(fixedRate = 60*1000)
+//    public void reportCurrentTime() {
+//        log.info("现在的时间是: {}", LocalDateTime.now());
+//        eventService.triggerCustomEvent(AgentUtils.genSSEChatMessageDto());
+//    }
+
 }
