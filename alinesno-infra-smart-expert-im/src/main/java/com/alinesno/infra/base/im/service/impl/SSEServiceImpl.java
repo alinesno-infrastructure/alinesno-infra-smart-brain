4b0fd792a5fb9f1bf7490b1a78c897e94f8a0a74
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    修复并增强SSE服务实现，加入客户端检查与消息队列处理功能。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
@@ -1,15 +1,19 @@
 package com.alinesno.infra.base.im.service.impl;
 
+import cn.hutool.core.lang.Assert;
 import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import com.alinesno.infra.base.im.dto.MessageTaskInfo;
 import com.alinesno.infra.base.im.service.ISSEService;
 import com.alinesno.infra.base.im.utils.AgentUtils;
 import jakarta.validation.constraints.NotBlank;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -69,26 +73,10 @@ public class SSEServiceImpl implements ISSEService {
     public void send(@NotBlank String clientId , ChatMessageDto message) throws IOException {
         final SseEmitter emitter = SSE_CACHE.get(clientId);      // 推流内容到客户端
 
-        if(emitter == null){
-            log.warn("客户端不存在，clientId = {}", clientId);
-            return ;
-        }
+        Assert.notNull(emitter, "客户端不存在，clientId = " + clientId);
+        Assert.notNull(message, "推送消息为空");
 
-        if(message == null){
-            message = AgentUtils.genSSEChatMessageDto() ;
-        }
-
-        int count = 2 ;
-        while (count-- > 0){
-            emitter.send(message) ;
-            try {
-                Thread.sleep(100);
-                count -- ;
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
+        emitter.send(message) ;
         emitter.send("[DONE]");
 
         // 结束推流
@@ -106,6 +94,26 @@ public class SSEServiceImpl implements ISSEService {
     @Override
     public void closeAllConn() {
         SSE_CACHE.forEach((clientId, sseEmitter) -> sseEmitter.complete());
+    }
+
+    @SneakyThrows
+    @Override
+    public void sendQueueMessage(String clientId , List<MessageTaskInfo> messageList) {
+        final SseEmitter emitter = SSE_CACHE.get(clientId);      // 推流内容到客户端
+
+      if(messageList != null)  {
+          for (MessageTaskInfo message : messageList) {
+              ChatMessageDto chatMessage = AgentUtils.genSSEChatMessageDto() ;
+
+              chatMessage.setName(message.getRoleDto().getRoleName());
+              chatMessage.setIcon(message.getRoleDto().getRoleAvatar());
+              chatMessage.setChatText(message.getText());
+
+              emitter.send(chatMessage);
+          }
+      }
+
+        emitter.send("[DONE]");
     }
 
 }
