db7c2978f3ba3d971129b40c99e119c8f1c22e3e
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复并整理了ScheduleServiceImpl类的导入和文档注释。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ScheduleServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ScheduleServiceImpl.java
@@ -1,26 +1,26 @@
-package com.alinesno.infra.smart.assistant.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
-import com.alinesno.infra.smart.assistant.mapper.ScheduleMapper;
-import com.alinesno.infra.smart.assistant.service.IScheduleService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-/**
- * <p>
- * 服务实现类
- * </p>
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Service
-public class ScheduleServiceImpl extends IBaseServiceImpl<ScheduleEntity, ScheduleMapper> implements IScheduleService {
-
-	// 日志记录
-	@SuppressWarnings("unused")
-	private static final Logger log = LoggerFactory.getLogger(ScheduleServiceImpl.class);
-
-}
+//package com.alinesno.infra.smart.assistant.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
+//import com.alinesno.infra.smart.assistant.mapper.ScheduleMapper;
+//import com.alinesno.infra.smart.assistant.service.IScheduleService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * <p>
+// * 服务实现类
+// * </p>
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Service
+//public class ScheduleServiceImpl extends IBaseServiceImpl<ScheduleEntity, ScheduleMapper> implements IScheduleService {
+//
+//	// 日志记录
+//	@SuppressWarnings("unused")
+//	private static final Logger log = LoggerFactory.getLogger(ScheduleServiceImpl.class);
+//
+//}
