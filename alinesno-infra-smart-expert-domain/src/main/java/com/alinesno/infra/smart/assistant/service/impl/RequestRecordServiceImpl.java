796528ac8b63ab62cae1a4cc69d6da7abc9be527
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复代码格式，确保与团队编码规范一致。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RequestRecordServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RequestRecordServiceImpl.java
@@ -1,20 +1,20 @@
-package com.alinesno.infra.smart.assistant.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.entity.RequestRecordEntity;
-import com.alinesno.infra.smart.assistant.mapper.RequestRecordMapper;
-import com.alinesno.infra.smart.assistant.service.IRequestRecordService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-/**
- * 应用构建Service业务层处理
- * 
- * @version 1.0.0
- * @author luoxiaodong
- */
-@Slf4j
-@Service
-public class RequestRecordServiceImpl extends IBaseServiceImpl<RequestRecordEntity, RequestRecordMapper> implements IRequestRecordService {
-
-}
+//package com.alinesno.infra.smart.assistant.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.assistant.entity.RequestRecordEntity;
+//import com.alinesno.infra.smart.assistant.mapper.RequestRecordMapper;
+//import com.alinesno.infra.smart.assistant.service.IRequestRecordService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 应用构建Service业务层处理
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//@Slf4j
+//@Service
+//public class RequestRecordServiceImpl extends IBaseServiceImpl<RequestRecordEntity, RequestRecordMapper> implements IRequestRecordService {
+//
+//}
