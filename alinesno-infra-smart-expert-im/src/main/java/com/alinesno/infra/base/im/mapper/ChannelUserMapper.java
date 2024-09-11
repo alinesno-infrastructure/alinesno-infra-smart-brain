05fcaa9fa99769153a88e219c0d3d750bf3b8d54
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    更新:alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelUserMapper.java

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.base.im.mapper;
-
-import com.alinesno.infra.base.im.entity.ChannelUserEntity;
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-
-public interface ChannelUserMapper extends IBaseMapper<ChannelUserEntity> {
-    
-}
