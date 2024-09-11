1e5a79a989d73ed0d0d19831e5339a84c5f45fd2
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复了IWorkflowExecutionService接口中的代码缩进问题。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IWorkflowExecutionService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IWorkflowExecutionService.java
@@ -1,21 +1,21 @@
-package com.alinesno.infra.smart.assistant.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.WorkflowExecutionEntity;
-
-/**
- * 应用构建Service接口
- * 
- * @version 1.0.0
- * @author luoxiaodong
- */
-public interface IWorkflowExecutionService extends IBaseService<WorkflowExecutionEntity> {
-
-    /**
-     * 获取名称获取到构建次数
-     * @param chainName
-     * @return
-     */
-    int getByChainName(String chainName);
-
-}
+//package com.alinesno.infra.smart.assistant.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.assistant.entity.WorkflowExecutionEntity;
+//
+///**
+// * 应用构建Service接口
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//public interface IWorkflowExecutionService extends IBaseService<WorkflowExecutionEntity> {
+//
+//    /**
+//     * 获取名称获取到构建次数
+//     * @param chainName
+//     * @return
+//     */
+//    int getByChainName(String chainName);
+//
+//}
