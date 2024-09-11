b72e0a30cf414d3b146de04f99cea5fc5a679047
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    新增MessageTaskInfo类，用于封装消息任务相关信息。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/dto/MessageTaskInfo.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.base.im.dto;
+
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 表示消息任务的信息的数据传输对象
+ * 该类封装了消息任务的相关信息，包括渠道ID、业务ID、角色ID、消息文本等
+ */
+@NoArgsConstructor
+@ToString
+@Data
+public class MessageTaskInfo implements Serializable {
+
+    // 消息渠道的唯一标识
+    private long channelId ;
+    // 业务的唯一标识
+    private String businessId ;
+    // 角色的唯一标识
+    private long roleId ;
+    // 消息的文本内容
+    private String text ;
+    // 前置业务的标识，用于关联消息任务与之前的业务
+    private String preBusinessId ;
+    // 角色的详细信息，使用实体类封装
+    private IndustryRoleEntity roleDto ;
+    // 消息任务的执行时间
+    private String usageTime ;
+
+    public MessageTaskInfo(long channelId, String businessId, long roleId, String text, String preBusinessId, IndustryRoleEntity roleDto) {
+        this.channelId = channelId;
+        this.businessId = businessId;
+        this.roleId = roleId;
+        this.text = text;
+        this.preBusinessId = preBusinessId;
+        this.roleDto = roleDto;
+    }
+}
