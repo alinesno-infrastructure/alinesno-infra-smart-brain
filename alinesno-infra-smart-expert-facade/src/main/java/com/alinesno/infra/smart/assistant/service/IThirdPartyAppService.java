73b5888957f0d3fb8e7c882d9790cc2a22a959b4
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:45 +0800

    修复代码格式化并统一注释风格。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IThirdPartyAppService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IThirdPartyAppService.java
@@ -1,17 +1,17 @@
-package com.alinesno.infra.smart.assistant.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.ThirdPartyAppEntity;
-
-/**
- * <p>
- * 服务类
- * </p>
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-
-public interface IThirdPartyAppService extends IBaseService<ThirdPartyAppEntity> {
-
-}
+//package com.alinesno.infra.smart.assistant.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.assistant.entity.ThirdPartyAppEntity;
+//
+///**
+// * <p>
+// * 服务类
+// * </p>
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//
+//public interface IThirdPartyAppService extends IBaseService<ThirdPartyAppEntity> {
+//
+//}
