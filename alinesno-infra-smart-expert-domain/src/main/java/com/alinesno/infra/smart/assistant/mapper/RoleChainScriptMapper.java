8d69412fb60c8c5da867b422977edac60d896543
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    修复了RoleChainScriptMapper类的注释和导入包问题。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/RoleChainScriptMapper.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/RoleChainScriptMapper.java
@@ -1,16 +1,16 @@
-package com.alinesno.infra.smart.assistant.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.entity.RoleChainScriptEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-/**
- * 应用构建Mapper接口
- * 
- * @version 1.0.0
- * @author luoxiaodong
- */
-@Mapper
-public interface RoleChainScriptMapper extends IBaseMapper<RoleChainScriptEntity> {
-
-}
+//package com.alinesno.infra.smart.assistant.mapper;
+//
+//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+//import com.alinesno.infra.smart.assistant.entity.RoleChainScriptEntity;
+//import org.apache.ibatis.annotations.Mapper;
+//
+///**
+// * 应用构建Mapper接口
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//@Mapper
+//public interface RoleChainScriptMapper extends IBaseMapper<RoleChainScriptEntity> {
+//
+//}
