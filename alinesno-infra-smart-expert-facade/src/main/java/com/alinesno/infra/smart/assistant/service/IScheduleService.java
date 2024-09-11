20ade3404d9391a0842a957b09459151a979041b
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    修复并更新了IScheduleService接口文档与导入包。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IScheduleService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IScheduleService.java
@@ -1,17 +1,17 @@
-package com.alinesno.infra.smart.assistant.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
-
-/**
- * <p>
- * 服务类
- * </p>
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-
-public interface IScheduleService extends IBaseService<ScheduleEntity> {
-
-}
+//package com.alinesno.infra.smart.assistant.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.assistant.entity.ScheduleEntity;
+//
+///**
+// * <p>
+// * 服务类
+// * </p>
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//
+//public interface IScheduleService extends IBaseService<ScheduleEntity> {
+//
+//}
