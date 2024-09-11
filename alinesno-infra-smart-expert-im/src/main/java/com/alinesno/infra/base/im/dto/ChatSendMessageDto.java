46458f11e090982552a065ed47b50dfa1c5c8738
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:45 +0800

    修复: 更新 ChatSendMessageDto 类，添加 businessIds 属性。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/dto/ChatSendMessageDto.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/dto/ChatSendMessageDto.java
@@ -10,6 +10,7 @@ public class ChatSendMessageDto implements Serializable {
 
     private String message;
     private List<Long> users;
+    private List<Long> businessIds ;
     private Long channelId;
     private String type;
 
