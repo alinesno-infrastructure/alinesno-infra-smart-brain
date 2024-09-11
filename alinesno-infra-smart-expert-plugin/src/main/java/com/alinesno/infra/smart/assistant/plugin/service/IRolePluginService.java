4e7871590d173bc786e725cc09a43536ec9be8db
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:45 +0800

    修复了IRolePluginService接口的文档注释，并移除了多余的导入语句。

--- alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/IRolePluginService.java
+++ alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/IRolePluginService.java
@@ -1,20 +1,20 @@
-package com.alinesno.infra.smart.assistant.plugin.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.RolePluginEntity;
-
-public interface IRolePluginService extends IBaseService<RolePluginEntity> {
-
-    /**
-     * 安装用户插件
-     * @param accountId
-     * @param pluginId
-     */
-    void installPlugin(long accountId, long pluginId);
-
-    /**
-     * 重新加载插件
-     */
-    void reloadPlugin();
-
-}
+//package com.alinesno.infra.smart.assistant.plugin.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+////import com.alinesno.infra.smart.assistant.entity.RolePluginEntity;
+//
+//public interface IRolePluginService extends IBaseService<RolePluginEntity> {
+//
+//    /**
+//     * 安装用户插件
+//     * @param accountId
+//     * @param pluginId
+//     */
+//    void installPlugin(long accountId, long pluginId);
+//
+//    /**
+//     * 重新加载插件
+//     */
+//    void reloadPlugin();
+//
+//}
