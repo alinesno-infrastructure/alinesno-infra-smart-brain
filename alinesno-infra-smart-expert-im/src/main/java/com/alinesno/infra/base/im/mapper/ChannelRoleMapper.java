6d19e22082411e9be0c73d46b9cac8aeab9cbe3c
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:43 +0800

    新增 ChannelRoleMapper 接口，用于操作 ChannelRoleEntity 实体类。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelRoleMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.base.im.entity.ChannelRoleEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+public interface ChannelRoleMapper extends IBaseMapper<ChannelRoleEntity> {
+    
+}
