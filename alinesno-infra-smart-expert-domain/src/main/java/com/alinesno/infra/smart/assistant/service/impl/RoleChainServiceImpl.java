6b38b5cb5e2de74a79eb56a0c14064e70a148a13
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:44 +0800

    feat: 更新RoleChainServiceImpl类，添加Redis消息发送功能以支持规则热更新。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RoleChainServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/RoleChainServiceImpl.java
@@ -1,53 +1,53 @@
-package com.alinesno.infra.smart.assistant.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.chain.IChainService;
-import com.alinesno.infra.smart.assistant.entity.RoleChainEntity;
-import com.alinesno.infra.smart.assistant.mapper.RoleChainMapper;
-import com.alinesno.infra.smart.assistant.redis.MessageConstants;
-import com.alinesno.infra.smart.assistant.redis.PublishRedisService;
-import com.alinesno.infra.smart.assistant.service.IRoleChainService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-/**
- * 应用构建Service业务层处理
- * 
- * @version 1.0.0
- * @author luoxiaodong
- */
-@Slf4j
-@Service
-public class RoleChainServiceImpl extends IBaseServiceImpl<RoleChainEntity, RoleChainMapper> implements IRoleChainService {
-
-    @Autowired
-    private PublishRedisService publishService ;
-
-    @Autowired
-    private IChainService chainService ;
-
-    @Override
-    public void runById(Long chainId) {
-
-        RoleChainEntity roleChain = getById(chainId) ;
-
-        chainService.executeRule(roleChain) ;
-    }
-
-    @Override
-    public void saveRoleChain(RoleChainEntity entity) {
-        this.save(entity) ;
-
-        // 发送消息用于规则的热更新
-        publishService.sendMsg(MessageConstants.RELOAD_RULE);
-    }
-
-    @Override
-    public void updateRoleChain(RoleChainEntity entity) {
-        this.update(entity) ;
-
-        // 发送消息用于规则的热更新
-        publishService.sendMsg(MessageConstants.RELOAD_RULE);
-    }
-}
+//package com.alinesno.infra.smart.assistant.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.assistant.chain.IChainService;
+//import com.alinesno.infra.smart.assistant.entity.RoleChainEntity;
+//import com.alinesno.infra.smart.assistant.mapper.RoleChainMapper;
+//import com.alinesno.infra.smart.assistant.redis.MessageConstants;
+//import com.alinesno.infra.smart.assistant.redis.PublishRedisService;
+//import com.alinesno.infra.smart.assistant.service.IRoleChainService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 应用构建Service业务层处理
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//@Slf4j
+//@Service
+//public class RoleChainServiceImpl extends IBaseServiceImpl<RoleChainEntity, RoleChainMapper> implements IRoleChainService {
+//
+//    @Autowired
+//    private PublishRedisService publishService ;
+//
+//    @Autowired
+//    private IChainService chainService ;
+//
+//    @Override
+//    public void runById(Long chainId) {
+//
+//        RoleChainEntity roleChain = getById(chainId) ;
+//
+//        chainService.executeRule(roleChain) ;
+//    }
+//
+//    @Override
+//    public void saveRoleChain(RoleChainEntity entity) {
+//        this.save(entity) ;
+//
+//        // 发送消息用于规则的热更新
+//        publishService.sendMsg(MessageConstants.RELOAD_RULE);
+//    }
+//
+//    @Override
+//    public void updateRoleChain(RoleChainEntity entity) {
+//        this.update(entity) ;
+//
+//        // 发送消息用于规则的热更新
+//        publishService.sendMsg(MessageConstants.RELOAD_RULE);
+//    }
+//}
