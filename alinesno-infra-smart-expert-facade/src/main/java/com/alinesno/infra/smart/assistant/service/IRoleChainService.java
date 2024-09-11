1b188dd49848d0b45b028e59572021c04fad2679
张婷(Tina) <tinaAip@gmail.com>
2024-09-12 00:17:45 +0800

    修复并优化IRoleChainService接口文档及代码格式。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IRoleChainService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IRoleChainService.java
@@ -1,31 +1,31 @@
-package com.alinesno.infra.smart.assistant.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.RoleChainEntity;
-
-/**
- * 应用构建Service接口
- * 
- * @version 1.0.0
- * @author luoxiaodong
- */
-public interface IRoleChainService extends IBaseService<RoleChainEntity> {
-
-    /**
-     * 运行工作流
-     * @param chainId
-     */
-    void runById(Long chainId);
-
-    /**
-     * 保存流程节点
-     * @param entity
-     */
-    void saveRoleChain(RoleChainEntity entity);
-
-    /**
-     * 更新流程节点
-     * @param entity
-     */
-    void updateRoleChain(RoleChainEntity entity);
-}
+//package com.alinesno.infra.smart.assistant.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.assistant.entity.RoleChainEntity;
+//
+///**
+// * 应用构建Service接口
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//public interface IRoleChainService extends IBaseService<RoleChainEntity> {
+//
+//    /**
+//     * 运行工作流
+//     * @param chainId
+//     */
+//    void runById(Long chainId);
+//
+//    /**
+//     * 保存流程节点
+//     * @param entity
+//     */
+//    void saveRoleChain(RoleChainEntity entity);
+//
+//    /**
+//     * 更新流程节点
+//     * @param entity
+//     */
+//    void updateRoleChain(RoleChainEntity entity);
+//}
