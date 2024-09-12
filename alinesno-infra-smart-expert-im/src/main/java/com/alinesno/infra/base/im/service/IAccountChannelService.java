8ec811d4e8ab5b0e5566a88cf53ce0b51c09ac44
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:49 +0800

    更新:alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IAccountChannelService.java

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IAccountChannelService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.base.im.service;
-
-import com.alinesno.infra.base.im.entity.AccountChannelEntity;
-import com.alinesno.infra.common.facade.services.IBaseService;
-
-public interface IAccountChannelService extends IBaseService<AccountChannelEntity> {
-}
