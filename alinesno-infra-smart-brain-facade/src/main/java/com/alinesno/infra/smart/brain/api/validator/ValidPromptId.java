b03b6005de57624e83da29ac2fcfeb6f4a8cc48d
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修改了`ValidPromptId`注解的包路径和导入语句格式，以遵循代码风格一致性。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/api/validator/ValidPromptId.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/api/validator/ValidPromptId.java
@@ -1,23 +1,23 @@
-package com.alinesno.infra.smart.brain.api.validator;
-
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
-import java.lang.annotation.*;
-
-/**
- * 自定义验证器注解
- * 用于验证PromptId的有效性
- */
-@Documented
-@Constraint(validatedBy = PromptIdValidator.class)
-@Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ValidPromptId {
-
-    String message() default "Invalid promptId, systemContent or userContent";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
-}
+//package com.alinesno.infra.smart.brain.api.validator;
+//
+//import jakarta.validation.Constraint;
+//import jakarta.validation.Payload;
+//
+//import java.lang.annotation.*;
+//
+///**
+// * 自定义验证器注解
+// * 用于验证PromptId的有效性
+// */
+//@Documented
+//@Constraint(validatedBy = PromptIdValidator.class)
+//@Target({ElementType.TYPE})
+//@Retention(RetentionPolicy.RUNTIME)
+//public @interface ValidPromptId {
+//
+//    String message() default "Invalid promptId, systemContent or userContent";
+//
+//    Class<?>[] groups() default {};
+//
+//    Class<? extends Payload>[] payload() default {};
+//}
