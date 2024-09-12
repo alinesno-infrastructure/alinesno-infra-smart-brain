1aba1dd5ff05d179a93ac83e25de940f70f1b07f
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修改SSEMessageEvent类，替换IM模块相关DTO引用为智能专家IM模块的ChatMessageDto。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/event/SSEMessageEvent.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/event/SSEMessageEvent.java
@@ -1,6 +1,6 @@
 package com.alinesno.infra.base.im.event;
 
-import com.alinesno.infra.base.im.dto.ChatMessageDto;
+import com.alinesno.infra.smart.im.dto.ChatMessageDto;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
