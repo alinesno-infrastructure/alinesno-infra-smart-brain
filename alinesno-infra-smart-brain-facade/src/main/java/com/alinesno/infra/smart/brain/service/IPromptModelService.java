d535050a18a20a2bc98eeb47469f8eb265f5c044
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修复了IPromptModelService接口的导入及注释格式问题。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptModelService.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptModelService.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.brain.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.brain.entity.PromptModelEntity;
-
-/**
- * 模型管理Service接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface IPromptModelService extends IBaseService<PromptModelEntity> {
-}
+//package com.alinesno.infra.smart.brain.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.brain.entity.PromptModelEntity;
+//
+///**
+// * 模型管理Service接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface IPromptModelService extends IBaseService<PromptModelEntity> {
+//}
