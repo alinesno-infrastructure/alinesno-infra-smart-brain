75955d553e3e6df6ff99bb49bf3db0899bdded37
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复了导入注释与类注释的格式问题。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/AccountRecordServiceImpl.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/AccountRecordServiceImpl.java
@@ -1,20 +1,20 @@
-package com.alinesno.infra.smart.brain.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
-import com.alinesno.infra.smart.brain.mapper.AccountRecordMapper;
-import com.alinesno.infra.smart.brain.service.IAccountRecordService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-/**
- * 【请填写功能名称】Service业务层处理
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Slf4j
-@Service
-public class AccountRecordServiceImpl extends IBaseServiceImpl<AccountRecordEntity, AccountRecordMapper> implements IAccountRecordService {
-
-}
+//package com.alinesno.infra.smart.brain.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
+//import com.alinesno.infra.smart.brain.mapper.AccountRecordMapper;
+//import com.alinesno.infra.smart.brain.service.IAccountRecordService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 【请填写功能名称】Service业务层处理
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Slf4j
+//@Service
+//public class AccountRecordServiceImpl extends IBaseServiceImpl<AccountRecordEntity, AccountRecordMapper> implements IAccountRecordService {
+//
+//}
