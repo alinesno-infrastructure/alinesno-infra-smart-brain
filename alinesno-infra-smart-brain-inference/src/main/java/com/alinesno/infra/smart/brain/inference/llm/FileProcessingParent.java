8dbbfd259100827de1ac2135340b43f8018983df
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复并优化了FileProcessingParent类的导入和依赖结构。

--- alinesno-infra-smart-brain-inference/src/main/java/com/alinesno/infra/smart/brain/inference/llm/FileProcessingParent.java
+++ alinesno-infra-smart-brain-inference/src/main/java/com/alinesno/infra/smart/brain/inference/llm/FileProcessingParent.java
@@ -1,17 +1,13 @@
 package com.alinesno.infra.smart.brain.inference.llm;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.context.SpringContext;
-import com.alinesno.infra.smart.brain.api.dto.PromptMessageDto;
-import com.alinesno.infra.smart.brain.api.reponse.TaskContentDto;
 import com.alinesno.infra.smart.brain.entity.GenerateTaskEntity;
 import com.alinesno.infra.smart.brain.entity.GenerateTaskHistoryEntity;
-import com.alinesno.infra.smart.brain.entity.PromptPostsEntity;
 import com.alinesno.infra.smart.brain.enums.TaskStatus;
-import com.alinesno.infra.smart.brain.inference.tools.TemplateParser;
-import com.alinesno.infra.smart.brain.service.*;
-import com.alinesno.infra.smart.brain.utils.CodeBlockParser;
+import com.alinesno.infra.smart.brain.service.IFileProcessingService;
+import com.alinesno.infra.smart.brain.service.IGenerateTaskHistoryService;
+import com.alinesno.infra.smart.brain.service.IGenerateTaskService;
+import com.alinesno.infra.smart.brain.service.ILLMApiService;
 import com.plexpt.chatgpt.entity.chat.ChatCompletion;
 import com.plexpt.chatgpt.entity.chat.Message;
 import lombok.extern.slf4j.Slf4j;
@@ -22,13 +18,12 @@ import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 public abstract class FileProcessingParent implements IFileProcessingService {
 
-    @Autowired
-    protected IPromptPostsService promptPostsService ;
+//    @Autowired
+//    protected IPromptPostsService promptPostsService ;
 
     @Autowired
     protected ILLMApiService llmApiService ;
@@ -51,21 +46,26 @@ public abstract class FileProcessingParent implements IFileProcessingService {
             log.debug("方法执行时间：" + usedTime + " 秒");
 
             // 判断是否获取到代码内容(这里返回的结果里面一定要有代码)
-            List<TaskContentDto.CodeContent> codeContents =  CodeBlockParser.parseCodeBlocks(sb.toString()) ;
+            // List<TaskContentDto.CodeContent> codeContents =  CodeBlockParser.parseCodeBlocks(sb.toString()) ;
 
-            if(codeContents.isEmpty()){ // 未完成，则重新生成内容
+//            if(codeContents.isEmpty()){ // 未完成，则重新生成内容
+//
+//                dto.setAssistantContent(sb.toString());
+//                dto.setTaskStatus(TaskStatus.FAILED.getValue());
+//                dto.setUsageTime(usedTime) ;
+//
+//                generateTaskService.resetRetryTask(dto);
+//            }else{
+//                dto.setAssistantContent(sb.toString());
+//                dto.setUsageTime(usedTime) ;
+//                dto.setTaskStatus(TaskStatus.COMPLETED.getValue());
+//                generateTaskService.update(dto);
+//            }
 
-                dto.setAssistantContent(sb.toString());
-                dto.setTaskStatus(TaskStatus.FAILED.getValue());
-                dto.setUsageTime(usedTime) ;
-
-                generateTaskService.resetRetryTask(dto);
-            }else{
-                dto.setAssistantContent(sb.toString());
-                dto.setUsageTime(usedTime) ;
-                dto.setTaskStatus(TaskStatus.COMPLETED.getValue());
-                generateTaskService.update(dto);
-            }
+            dto.setAssistantContent(sb.toString());
+            dto.setUsageTime(usedTime) ;
+            dto.setTaskStatus(TaskStatus.COMPLETED.getValue());
+            generateTaskService.update(dto);
 
             // 记录历史记录
             recordHistory(dto , sb.toString()) ;
@@ -105,33 +105,33 @@ public abstract class FileProcessingParent implements IFileProcessingService {
     }
 
     protected void parseMessage(GenerateTaskEntity dto, String promptId, List<Message> messages) {
-        PromptPostsEntity postsEntity = promptPostsService.getByPromptId(promptId) ;
+//        PromptPostsEntity postsEntity = promptPostsService.getByPromptId(promptId) ;
 
-        List<PromptMessageDto> promptMessageList = JSONArray.parseArray(postsEntity.getPromptContent() , PromptMessageDto.class) ;
-        Map<?,?> params = JSONObject.parseObject(dto.getParams() , Map.class) ;
+//        List<PromptMessageDto> promptMessageList = JSONArray.parseArray(postsEntity.getPromptContent() , PromptMessageDto.class) ;
+//        Map<?,?> params = JSONObject.parseObject(dto.getParams() , Map.class) ;
+//
+//
+//        for(PromptMessageDto msg : promptMessageList){
+//            Message message = null ;
+//
+//            // 模板解析处理
+//            String contentTemplate = msg.getContent().trim() ;
+//            if(params != null){
+//                contentTemplate = TemplateParser.parserTemplate(contentTemplate , params) ;
+//            }
+//
+//            if(Message.Role.SYSTEM.getValue().equals(msg.getRole())){
+//                message = Message.ofSystem(contentTemplate);
+//            }else if(Message.Role.ASSISTANT.getValue().equals(msg.getRole())){
+//                message = Message.ofAssistant(contentTemplate);
+//            }else if(Message.Role.USER.getValue().equals(msg.getRole())){
+//                message = Message.of(contentTemplate);
+//            }
+//
+//            if(message != null){
+//                messages.add(message) ;
+//            }
 
-
-        for(PromptMessageDto msg : promptMessageList){
-            Message message = null ;
-
-            // 模板解析处理
-            String contentTemplate = msg.getContent().trim() ;
-            if(params != null){
-                contentTemplate = TemplateParser.parserTemplate(contentTemplate , params) ;
-            }
-
-            if(Message.Role.SYSTEM.getValue().equals(msg.getRole())){
-                message = Message.ofSystem(contentTemplate);
-            }else if(Message.Role.ASSISTANT.getValue().equals(msg.getRole())){
-                message = Message.ofAssistant(contentTemplate);
-            }else if(Message.Role.USER.getValue().equals(msg.getRole())){
-                message = Message.of(contentTemplate);
-            }
-
-            if(message != null){
-                messages.add(message) ;
-            }
-
-        }
+//        }
     }
 }
