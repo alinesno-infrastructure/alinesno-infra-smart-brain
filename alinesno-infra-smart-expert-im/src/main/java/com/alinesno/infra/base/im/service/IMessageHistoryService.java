86480bd984086fc600cde1d583e2e0471175251e
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:49 +0800

    更新:alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IMessageHistoryService.java

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/IMessageHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.base.im.service;
-
-import com.alinesno.infra.base.im.entity.MessageHistoryEntity;
-import com.alinesno.infra.common.facade.services.IBaseService;
-
-public interface IMessageHistoryService extends IBaseService<MessageHistoryEntity> {
-
-}
