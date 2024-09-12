af600cc77211824c78ec16ca3451896674442f0c
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:50 +0800

    新增IMessageHistoryService接口文件。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IMessageHistoryService.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.im.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.im.entity.MessageHistoryEntity;
+
+public interface IMessageHistoryService extends IBaseService<MessageHistoryEntity> {
+
+}
