a8abee473fc31ab7f2463135a9d836a5ea1d2615
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修改了AccountChannelServiceImpl类，将依赖的实体类和接口更新为智能专家模块的相关实体类和接口。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/AccountChannelServiceImpl.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/AccountChannelServiceImpl.java
@@ -1,9 +1,9 @@
 package com.alinesno.infra.base.im.service.impl;
 
-import com.alinesno.infra.base.im.entity.AccountChannelEntity;
 import com.alinesno.infra.base.im.mapper.AccountChannelMapper;
-import com.alinesno.infra.base.im.service.IAccountChannelService;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.im.entity.AccountChannelEntity;
+import com.alinesno.infra.smart.im.service.IAccountChannelService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
