69ce9ab69bfadf4e0931b80cb37e72991d8395e4
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复并整理BrainConstants接口中的文件注释和空行。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/constants/BrainConstants.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/constants/BrainConstants.java
@@ -1,12 +1,12 @@
-package com.alinesno.infra.smart.brain.constants;
-
-public interface BrainConstants {
-
-    String PDF = "pdf" ;
-
-    String DOCX = "docx";
-
-    String DOC = "doc";
-
-    String XMIND = "xmind";
-}
+//package com.alinesno.infra.smart.brain.constants;
+//
+//public interface BrainConstants {
+//
+//    String PDF = "pdf" ;
+//
+//    String DOCX = "docx";
+//
+//    String DOC = "doc";
+//
+//    String XMIND = "xmind";
+//}
