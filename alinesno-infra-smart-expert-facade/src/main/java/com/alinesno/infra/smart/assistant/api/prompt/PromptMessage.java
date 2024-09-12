34baee1aeb3a649271298b7f88857edd6f98ac8b
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    优化PromptMessage类结构并添加构造方法与枚举。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/prompt/PromptMessage.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/prompt/PromptMessage.java
@@ -1,16 +1,49 @@
 package com.alinesno.infra.smart.assistant.api.prompt;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class PromptMessage {
 
-    private String role ;
-    private String content ;
+    private String role;
+    private String content;
+    private String name;
 
-    public PromptMessage(String role , String content){
-        this.role = role ;
-        this.content = content ;
+    public PromptMessage(String role, String content) {
+        this.role = role;
+        this.content = content;
     }
 
+    public static PromptMessage of(String content) {
+        return new PromptMessage(Role.USER.getValue(), content);
+    }
+
+    public static PromptMessage ofSystem(String content) {
+        return new PromptMessage(Role.SYSTEM.getValue(), content);
+    }
+
+    public static PromptMessage ofAssistant(String content) {
+        return new PromptMessage(Role.ASSISTANT.getValue(), content);
+    }
+
+    public static PromptMessage ofFunction(String function) {
+        return new PromptMessage(Role.FUNCTION.getValue(), function);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static enum Role {
+
+        SYSTEM("system"),
+        USER("user"),
+        ASSISTANT("assistant"),
+        FUNCTION("function");
+
+        private final String value;
+    }
 }
