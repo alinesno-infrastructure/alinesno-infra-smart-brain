95953487b93ffe08f391a7ffa3376c0fc22c97b8
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修复了IAccountRecordService接口注释与代码格式的一致性问题。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IAccountRecordService.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IAccountRecordService.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.brain.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
-
-/**
- * 【请填写功能名称】Service接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface IAccountRecordService extends IBaseService<AccountRecordEntity> {
-}
+//package com.alinesno.infra.smart.brain.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
+//
+///**
+// * 【请填写功能名称】Service接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface IAccountRecordService extends IBaseService<AccountRecordEntity> {
+//}
