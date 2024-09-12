5c91bc629fbf6e134f7f0595c170ebe119908904
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    新增 AssistantPromptDto 类，用于封装发送Agent角色消息内容。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/AssistantPromptDto.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.im.dto;
+
+import lombok.Data;
+
+/**
+ * 发送Agent角色消息内容
+ */
+@Data
+public class AssistantPromptDto {
+
+    private Long businessId;
+    private long agentId;
+    private String content;
+    private long channelId ;
+    private String assistantContent ;
+
+}
