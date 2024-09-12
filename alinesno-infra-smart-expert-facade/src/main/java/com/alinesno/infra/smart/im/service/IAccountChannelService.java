4f07a8bfd5fedefcd5c35c51bb45e518c78d6c69
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    新增IAccountChannelService接口文件。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IAccountChannelService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.im.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.im.entity.AccountChannelEntity;
+
+public interface IAccountChannelService extends IBaseService<AccountChannelEntity> {
+}
