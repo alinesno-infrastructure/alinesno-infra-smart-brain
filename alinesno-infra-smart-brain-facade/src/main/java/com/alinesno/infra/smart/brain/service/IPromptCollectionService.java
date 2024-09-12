f1022d6179c65fcf1dd1fd91486e5041032bf710
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复了文件格式问题，确保了文件末尾有换行符。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptCollectionService.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptCollectionService.java
@@ -1,14 +1,14 @@
-package com.alinesno.infra.smart.brain.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
-
-/**
- * 资产收藏Service接口
- *
- * @version 1.0.0
- * @date 2023-03-28 22:25:58
- * @author luoxiaodong
- */
-public interface IPromptCollectionService extends IBaseService<PromptCollectionEntity> {
-}
+//package com.alinesno.infra.smart.brain.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
+//
+///**
+// * 资产收藏Service接口
+// *
+// * @version 1.0.0
+// * @date 2023-03-28 22:25:58
+// * @author luoxiaodong
+// */
+//public interface IPromptCollectionService extends IBaseService<PromptCollectionEntity> {
+//}
