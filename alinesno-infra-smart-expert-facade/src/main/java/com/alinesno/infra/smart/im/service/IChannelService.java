f495f1cbcc7db743139a83a097fb4881633474cc
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    新增 IChannelService 接口及其实现方法。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IChannelService.java
@@ -0,0 +1,54 @@
+package com.alinesno.infra.smart.im.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.im.entity.ChannelEntity;
+
+import java.util.List;
+
+public interface IChannelService extends IBaseService<ChannelEntity> {
+
+    /**
+     * 保存和创建Channel
+     *
+     * @param entity
+     * @return 最新保存的ID
+     */
+    String createChannel(ChannelEntity entity);
+
+    /**
+     * 逻辑删除频道
+     * @param channelId
+     */
+    void removeChannel(Long channelId);
+
+    /**
+     * 查询用户所有的渠道
+     * @return
+     */
+    List<ChannelEntity> allMyChannel();
+
+    /**
+     * 查询出所有公共频道
+     * @return
+     */
+    List<ChannelEntity> allPublicChannel();
+
+    /**
+     * 用户加入频道
+     * @param userId
+     * @param channelId
+     */
+    void jobChannel(long userId, String channelId);
+
+    /**
+     * 获取到默认的渠道
+     * @return
+     */
+    Long getDefaultChannelId();
+
+    /**
+     * 获取到推荐频道
+     * @return
+     */
+    List<ChannelEntity> getRecommendChannel();
+}
