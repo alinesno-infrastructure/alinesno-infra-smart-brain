3c714b0d7afcba9bb502fd6292002d35c2f344de
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复: 更新XmindUtil类，实现将Xmind文件内容转换为列表的功能。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/utils/parse/XmindUtil.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/utils/parse/XmindUtil.java
@@ -1,15 +1,15 @@
-package com.alinesno.infra.smart.brain.utils.parse;
-
-import java.io.InputStream;
-import java.util.List;
-
-/**
- * 用于解析Xmind文件内容
- */
-public class XmindUtil {
-
-    public static List<String> xmindToList(InputStream inputStream) {
-        // 实现将Xmind文件内容转换成列表的逻辑
-        return null;
-    }
-}
+//package com.alinesno.infra.smart.brain.utils.parse;
+//
+//import java.io.InputStream;
+//import java.util.List;
+//
+///**
+// * 用于解析Xmind文件内容
+// */
+//public class XmindUtil {
+//
+//    public static List<String> xmindToList(InputStream inputStream) {
+//        // 实现将Xmind文件内容转换成列表的逻辑
+//        return null;
+//    }
+//}
