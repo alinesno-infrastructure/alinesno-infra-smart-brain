2b24bfad143a2b76945113087daba824cb4d67ac
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修改 ChannelRoleMapper 接口，将实体类从 ChannelRoleEntity 引入。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelRoleMapper.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelRoleMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.im.mapper;
 
-import com.alinesno.infra.base.im.entity.ChannelRoleEntity;
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.ChannelRoleEntity;
 
 public interface ChannelRoleMapper extends IBaseMapper<ChannelRoleEntity> {
     
