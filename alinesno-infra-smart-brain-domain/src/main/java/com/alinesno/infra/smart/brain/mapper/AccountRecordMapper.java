2b1a0c50f4bab3fee6e2beb31343f77e018eec04
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:53 +0800

    修复: 更新AccountRecordMapper接口注释与导入路径

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/AccountRecordMapper.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/AccountRecordMapper.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.brain.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
-
-/**
- * 【请填写功能名称】Mapper接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface AccountRecordMapper extends IBaseMapper<AccountRecordEntity> {
-}
+//package com.alinesno.infra.smart.brain.mapper;
+//
+//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+//import com.alinesno.infra.smart.brain.entity.AccountRecordEntity;
+//
+///**
+// * 【请填写功能名称】Mapper接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface AccountRecordMapper extends IBaseMapper<AccountRecordEntity> {
+//}
