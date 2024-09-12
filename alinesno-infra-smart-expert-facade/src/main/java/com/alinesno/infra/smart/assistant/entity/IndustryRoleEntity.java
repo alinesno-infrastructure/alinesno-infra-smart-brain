1081ee844123b089b3f76dfb783794f62912aa85
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修改 IndustryRoleEntity 类，增加与 ChainID 关联的字段及其他角色相关属性。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/IndustryRoleEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/IndustryRoleEntity.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.assistant.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -27,10 +28,10 @@ public class IndustryRoleEntity extends InfraBaseEntity {
     @ColumnComment("角色名称")
     private String roleName; // 角色名称
 
-    @TableField("data_source_api") // MyBatis-Plus 字段注解
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 512)
-    @ColumnComment("数据源API")
-    private String dataSourceApi ; // 数据源API
+    @ColumnType(value = MySqlTypeConstant.VARCHAR , length = 32)
+    @ColumnComment("关联的ChainID")
+    @TableField("chain_id") // MyBatis-Plus 字段注解
+    private String chainId; // 其他角色相关字段
 
     @TableField("industry_catalog") // MyBatis-Plus 字段注解
     @ColumnType(value = MySqlTypeConstant.BIGINT, length = 32)
@@ -42,44 +43,52 @@ public class IndustryRoleEntity extends InfraBaseEntity {
     @ColumnComment("角色职责描述")
     private String responsibilities; // 角色职责描述
 
-    @TableField("skills") // MyBatis-Plus 字段注解
-    @ColumnType(value = MySqlTypeConstant.TEXT)
-    @ColumnComment("所需技能描述")
-    private String skills; // 所需技能描述
-
-    @TableField("experience") // MyBatis-Plus 字段注解
-    @ColumnType(value = MySqlTypeConstant.TEXT)
-    @ColumnComment("经验要求描述")
-    private String experience; // 经验要求描述
-
     @TableField("role_level") // MyBatis-Plus 字段注解
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
     @ColumnComment("角色等级")
     private String roleLevel; // 角色等级
-
-    @TableField("salary_range") // MyBatis-Plus 字段注解
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 100)
-    @ColumnComment("薪资范围")
-    private String salaryRange; // 薪资范围
-
-    @TableField("education") // MyBatis-Plus 字段注解
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 100)
-    @ColumnComment("教育背景要求")
-    private String education; // 教育背景要求
 
     @TableField("other_attributes") // MyBatis-Plus 字段注解
     @ColumnType(value = MySqlTypeConstant.TEXT)
     @ColumnComment("其他角色相关字段")
     private String otherAttributes; // 其他角色相关字段
 
-    @ColumnType(value = MySqlTypeConstant.BIGINT , length = 32)
-    @ColumnComment("关联的ChainID")
-    @TableField("chain_id") // MyBatis-Plus 字段注解
-    private Long chainId; // 其他角色相关字段
+	@Excel(name="指令内容")
+	@TableField("prompt_content")
+	@ColumnType(value = MySqlTypeConstant.LONGTEXT)
+	@ColumnComment("指令内容")
+	private String promptContent;
 
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 32)
-    @ColumnComment("关联的PromptId")
-    @TableField("prompt_id") // MyBatis-Plus 字段注解
-    private String promptId ; // 其他角色相关字段
+
+//    @TableField("data_source_api") // MyBatis-Plus 字段注解
+//    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 512)
+//    @ColumnComment("数据源API")
+//    private String dataSourceApi ; // 数据源API
+
+//    @TableField("skills") // MyBatis-Plus 字段注解
+//    @ColumnType(value = MySqlTypeConstant.TEXT)
+//    @ColumnComment("所需技能描述")
+//    private String skills; // 所需技能描述
+//
+//    @TableField("experience") // MyBatis-Plus 字段注解
+//    @ColumnType(value = MySqlTypeConstant.TEXT)
+//    @ColumnComment("经验要求描述")
+//    private String experience; // 经验要求描述
+
+
+//    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 32)
+//    @ColumnComment("关联的PromptId")
+//    @TableField("prompt_id") // MyBatis-Plus 字段注解
+//    private String promptId ; // 其他角色相关字段
+
+//    @TableField("salary_range") // MyBatis-Plus 字段注解
+//    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 100)
+//    @ColumnComment("薪资范围")
+//    private String salaryRange; // 薪资范围
+//
+//    @TableField("education") // MyBatis-Plus 字段注解
+//    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 100)
+//    @ColumnComment("教育背景要求")
+//    private String education; // 教育背景要求
 
 }
