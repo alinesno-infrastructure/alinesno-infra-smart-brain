7a53cb30a2f60d77c2f994753d44a37ec154f61a
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    feat: 更新AccountEntity类，增加用户状态、描述、名称、头像、登录账号、密码、手机号、加密、询问次数、总次数、支付次数字段注释及属性。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/entity/AccountEntity.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/entity/AccountEntity.java
@@ -1,117 +1,117 @@
-package com.alinesno.infra.smart.brain.entity;
-
-import cn.afterturn.easypoi.excel.annotation.Excel;
-import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-/**
- * 用户账户实体类
- *
- * @version 1.0.0
- * @author luoxiaodong
- */
-@EqualsAndHashCode(callSuper = true)
-@Data
-@TableName("user_account")
-public class AccountEntity extends InfraBaseEntity {
-
-    // fields
-
-    /**
-     * 用户状态
-     */
-    @Excel(name="用户状态")
-    @TableField("account_status")
-	@ColumnType(length=255)
-	@ColumnComment("用户状态")
-    private Long accountStatus;
-
-    /**
-     * 用户描述
-     */
-    @TableField
-	@ColumnType(length=255)
-	@ColumnComment("用户描述")
-    private String description; // 用户描述
-
-    /**
-     * 用户名称
-     */
-    @TableField
-	@ColumnType(length=255)
-	@ColumnComment("用户名称")
-    private String username; // 用户名称
-
-    /**
-     * 用户头像
-     */
-    @TableField
-	@ColumnType(length=255)
-	@ColumnComment("用户头像")
-    private String avatar ; // 用户头像
-
-    /**
-     * 登陆账号
-     */
-    @Excel(name="登陆账号")
-    @TableField("login_account")
-	@ColumnType(length=20)
-	@ColumnComment("登陆账号")
-    private String loginAccount;
-
-    /**
-     * 登陆授权码
-     */
-    @Excel(name="登陆密码")
-    @TableField("login_password")
-	@ColumnType(length=255)
-	@ColumnComment("登陆授权码")
-    private String loginPassword;
-
-    /**
-     * 手机号
-     */
-    @Excel(name="手机号")
-    @TableField("phone_code")
-	@ColumnType(length=4)
-	@ColumnComment("手机号")
-    private String phoneCode;
-
-    /**
-     * 加密
-     */
-    @Excel(name="加密")
-    @TableField("salt")
-	@ColumnType(length=64)
-	@ColumnComment("加密")
-    private String salt;
-
-    /**
-     * 询问次数
-     */
-    @TableField
-	@ColumnType(length=10)
-	@ColumnComment("询问次数")
-    private int askCount ; // 询问次数
-
-    /**
-     * 总的次数
-     */
-    @TableField
-	@ColumnType(length=10)
-	@ColumnComment("总的次数")
-    private int totalCount ; // 总的次数
-
-    /**
-     * 支付次数
-     */
-    @TableField
-	@ColumnType(length=2)
-	@ColumnComment("支付次数")
-    private int payCount ; // 支付次数
-}
+//package com.alinesno.infra.smart.brain.entity;
+//
+//import cn.afterturn.easypoi.excel.annotation.Excel;
+//import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+//import com.baomidou.mybatisplus.annotation.TableField;
+//import com.baomidou.mybatisplus.annotation.TableName;
+//import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+//import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//
+///**
+// * 用户账户实体类
+// *
+// * @version 1.0.0
+// * @author luoxiaodong
+// */
+//@EqualsAndHashCode(callSuper = true)
+//@Data
+//@TableName("user_account")
+//public class AccountEntity extends InfraBaseEntity {
+//
+//    // fields
+//
+//    /**
+//     * 用户状态
+//     */
+//    @Excel(name="用户状态")
+//    @TableField("account_status")
+//	@ColumnType(length=255)
+//	@ColumnComment("用户状态")
+//    private Long accountStatus;
+//
+//    /**
+//     * 用户描述
+//     */
+//    @TableField
+//	@ColumnType(length=255)
+//	@ColumnComment("用户描述")
+//    private String description; // 用户描述
+//
+//    /**
+//     * 用户名称
+//     */
+//    @TableField
+//	@ColumnType(length=255)
+//	@ColumnComment("用户名称")
+//    private String username; // 用户名称
+//
+//    /**
+//     * 用户头像
+//     */
+//    @TableField
+//	@ColumnType(length=255)
+//	@ColumnComment("用户头像")
+//    private String avatar ; // 用户头像
+//
+//    /**
+//     * 登陆账号
+//     */
+//    @Excel(name="登陆账号")
+//    @TableField("login_account")
+//	@ColumnType(length=20)
+//	@ColumnComment("登陆账号")
+//    private String loginAccount;
+//
+//    /**
+//     * 登陆授权码
+//     */
+//    @Excel(name="登陆密码")
+//    @TableField("login_password")
+//	@ColumnType(length=255)
+//	@ColumnComment("登陆授权码")
+//    private String loginPassword;
+//
+//    /**
+//     * 手机号
+//     */
+//    @Excel(name="手机号")
+//    @TableField("phone_code")
+//	@ColumnType(length=4)
+//	@ColumnComment("手机号")
+//    private String phoneCode;
+//
+//    /**
+//     * 加密
+//     */
+//    @Excel(name="加密")
+//    @TableField("salt")
+//	@ColumnType(length=64)
+//	@ColumnComment("加密")
+//    private String salt;
+//
+//    /**
+//     * 询问次数
+//     */
+//    @TableField
+//	@ColumnType(length=10)
+//	@ColumnComment("询问次数")
+//    private int askCount ; // 询问次数
+//
+//    /**
+//     * 总的次数
+//     */
+//    @TableField
+//	@ColumnType(length=10)
+//	@ColumnComment("总的次数")
+//    private int totalCount ; // 总的次数
+//
+//    /**
+//     * 支付次数
+//     */
+//    @TableField
+//	@ColumnType(length=2)
+//	@ColumnComment("支付次数")
+//    private int payCount ; // 支付次数
+//}
