bb9ee46a99c4d7a4d2943724e0663b24ea117841
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复并优化PromptCollectionMapper接口文档注释。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/PromptCollectionMapper.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/PromptCollectionMapper.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.brain.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
-
-/**
- * 资产收藏Mapper接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface PromptCollectionMapper extends IBaseMapper<PromptCollectionEntity>{
-}
+//package com.alinesno.infra.smart.brain.mapper;
+//
+//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+//import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
+//
+///**
+// * 资产收藏Mapper接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface PromptCollectionMapper extends IBaseMapper<PromptCollectionEntity>{
+//}
