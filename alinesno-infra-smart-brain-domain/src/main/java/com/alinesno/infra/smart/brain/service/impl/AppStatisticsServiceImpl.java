7a604e908cd7fbdd9f4858c081e8402d2acd2323
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复了文件格式，统一了导入包和注释风格。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/AppStatisticsServiceImpl.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/AppStatisticsServiceImpl.java
@@ -1,22 +1,22 @@
-package com.alinesno.infra.smart.brain.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.brain.entity.AppStatisticsEntity;
-import com.alinesno.infra.smart.brain.mapper.AppStatisticsMapper;
-import com.alinesno.infra.smart.brain.service.IAppStatisticsService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-
-/**
- * 数据资产统计Service业务层处理
- *
-
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Slf4j
-@Service
-public class AppStatisticsServiceImpl extends IBaseServiceImpl<AppStatisticsEntity, AppStatisticsMapper> implements IAppStatisticsService {
-
-}
+//package com.alinesno.infra.smart.brain.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.brain.entity.AppStatisticsEntity;
+//import com.alinesno.infra.smart.brain.mapper.AppStatisticsMapper;
+//import com.alinesno.infra.smart.brain.service.IAppStatisticsService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//
+///**
+// * 数据资产统计Service业务层处理
+// *
+//
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Slf4j
+//@Service
+//public class AppStatisticsServiceImpl extends IBaseServiceImpl<AppStatisticsEntity, AppStatisticsMapper> implements IAppStatisticsService {
+//
+//}
