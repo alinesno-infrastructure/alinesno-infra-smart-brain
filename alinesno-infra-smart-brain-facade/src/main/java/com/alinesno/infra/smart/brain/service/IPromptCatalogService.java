0f18f5beee97ef31c06a73ebd2792986a488f6d8
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:53 +0800

    修复了IPromptCatalogService接口文件的格式问题。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptCatalogService.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IPromptCatalogService.java
@@ -1,35 +1,35 @@
-package com.alinesno.infra.smart.brain.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.brain.api.TreeSelectDto;
-import com.alinesno.infra.smart.brain.entity.PromptCatalogEntity;
-
-import java.util.List;
-
-/**
- * Prompt指令类型Service接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface IPromptCatalogService extends IBaseService<PromptCatalogEntity> {
-
-    /**
-     * 查询出指令类型列表
-     * @param promptCatalog
-     * @return
-     */
-    List<PromptCatalogEntity> selectCatalogList(PromptCatalogEntity promptCatalog);
-
-    /**
-     * 保存用户类型
-     * @param entity
-     */
-    void insertCatalog(PromptCatalogEntity entity);
-
-    /**
-     * 查询类型列表树
-     * @return
-     */
-    List<TreeSelectDto> selectCatalogTreeList();
-}
+//package com.alinesno.infra.smart.brain.service;
+//
+//import com.alinesno.infra.common.facade.services.IBaseService;
+//import com.alinesno.infra.smart.brain.api.TreeSelectDto;
+//import com.alinesno.infra.smart.brain.entity.PromptCatalogEntity;
+//
+//import java.util.List;
+//
+///**
+// * Prompt指令类型Service接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface IPromptCatalogService extends IBaseService<PromptCatalogEntity> {
+//
+//    /**
+//     * 查询出指令类型列表
+//     * @param promptCatalog
+//     * @return
+//     */
+//    List<PromptCatalogEntity> selectCatalogList(PromptCatalogEntity promptCatalog);
+//
+//    /**
+//     * 保存用户类型
+//     * @param entity
+//     */
+//    void insertCatalog(PromptCatalogEntity entity);
+//
+//    /**
+//     * 查询类型列表树
+//     * @return
+//     */
+//    List<TreeSelectDto> selectCatalogTreeList();
+//}
