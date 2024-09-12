5438643c9799002ca971060b7138aeef7869b9b3
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修复并优化IndustryRoleServiceImpl类的业务逻辑实现。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/IndustryRoleServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/IndustryRoleServiceImpl.java
@@ -1,20 +1,24 @@
 package com.alinesno.infra.smart.assistant.service.impl;
 
 import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.common.core.context.SpringContext;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
+import com.alinesno.infra.smart.assistant.chain.IBaseExpertService;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.entity.WorkflowExecutionEntity;
 import com.alinesno.infra.smart.assistant.mapper.IndustryRoleMapper;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
-import com.alinesno.infra.smart.brain.api.reponse.TaskContentDto;
+import com.alinesno.infra.smart.assistant.service.IWorkflowExecutionService;
+import com.alinesno.infra.smart.brain.api.dto.PromptMessageDto;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 应用构建Service业务层处理
@@ -26,78 +30,16 @@ import java.util.Map;
 @Service
 public class IndustryRoleServiceImpl extends IBaseServiceImpl<IndustryRoleEntity, IndustryRoleMapper> implements IIndustryRoleService {
 
-//    @Autowired
-//    private IRoleChainService roleChainService ;
 
-    @Value("${spring.application.name}")
-    private String applicationName;
+    @Autowired
+    private IWorkflowExecutionService workflowExecutionService ;
 
-//    @Autowired
-//    private PublishRedisService publishService ;
-
-//    @Resource(name="chainRunner")
-//    private IBaseExpertService baseExpert ;
-
-//    @Autowired
-//    private INoticeService noticeService ;
-
-    @Override
-    public void saveRoleChainInfo(String roleId) {
-
-//        chain.setEnable("1");
-//        chain.setChainApplicationName(applicationName);
-//        chain.setCreateTime(new Date());
-//
-//        roleChainService.save(chain) ;
-//
-//        IndustryRoleEntity role = this.getById(roleId) ;
-//        role.setChainId(chain.getId());
-//        this.update(role) ;
-
-        // 发送消息用于规则的热更新
-//        publishService.sendMsg(MessageConstants.RELOAD_RULE);
-    }
-
-    @Override
-    public void runRoleChainByRoleId(Map<String , Object> params , long roleId) {
-
-        Assert.notNull(params , "请求参数为空.");
-
-//        IndustryRoleEntity role = getById(roleId) ;
-//        RoleChainEntity roleChain = roleChainService.getById(role.getChainId()) ;
-//
-//        String chainName = roleChain.getChainName() ;
-//        Long chainId = roleChain.getId() ;
-
-//        baseExpert.processExpert(params , chainName , chainId);
-    }
-
-    @Override
-    public void runChainAgent(TaskContentDto dto , long roleId) {
-
-        // TODO 待优化获取多个脚本的问题
-        String msg = dto.getCodeContent().get(0).getContent();
-
-//        NoticeEntity noticeEntity = noticeService.getByBusinessId(dto.getBusinessId()) ;
-//        NoticeDto noticeDto = new NoticeDto() ;
-//
-//        if(noticeEntity != null){
-//            BeanUtils.copyProperties(noticeEntity , dto);
-//            noticeDto.setTaskName(dto.getGenContent());
-//        }
-
-        // 发送培训相关的要求到任务中
-        Map<String , Object> params = new HashMap<>() ;
-        params.put("label1" , msg) ;
-
-        runRoleChainByRoleId(params , roleId);
-    }
+    private static final Gson gson = new Gson() ;
 
     @Override
     public List<IndustryRoleEntity> getNewestRole() {
 
         LambdaQueryWrapper<IndustryRoleEntity> queryWrapper = new LambdaQueryWrapper<>();
-
         queryWrapper.orderByDesc(IndustryRoleEntity::getAddTime);
         queryWrapper.last("limit 8");
 
@@ -126,6 +68,28 @@ public class IndustryRoleServiceImpl extends IBaseServiceImpl<IndustryRoleEntity
         role.setRoleAvatar("1830185154541305857");
 
         return role;
+    }
+
+    @Override
+    public void updatePromptContent(List<PromptMessageDto> messageDto, String roleId) {
+        IndustryRoleEntity e = getById(roleId) ;
+        e.setPromptContent(gson.toJson(messageDto));
+        this.update(e) ;
+    }
+
+    @Override
+    public WorkflowExecutionDto runRoleAgent(MessageTaskInfo taskInfo) {
+
+        long roleId = taskInfo.getRoleId() ;
+        IndustryRoleEntity role = getById(roleId) ;
+
+        // 获取到节点的执行内容信息
+        String businessIds = taskInfo.getPreBusinessId() ;
+        WorkflowExecutionEntity workflowExecutionEntity = workflowExecutionService.getById(businessIds) ;
+
+        IBaseExpertService baseExpertService = (IBaseExpertService) SpringContext.getBean(role.getChainId());
+
+        return baseExpertService.runRoleAgent(role , workflowExecutionEntity , taskInfo);
     }
 
 }
