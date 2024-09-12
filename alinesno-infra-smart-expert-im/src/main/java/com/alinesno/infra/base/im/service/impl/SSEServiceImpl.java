1f9fa0b33879744aebd8069c44a080f45716424a
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    修复并更新SSEServiceImpl类以兼容智能IM模块。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
@@ -1,10 +1,10 @@
 package com.alinesno.infra.base.im.service.impl;
 
 import cn.hutool.core.lang.Assert;
-import com.alinesno.infra.base.im.dto.ChatMessageDto;
-import com.alinesno.infra.base.im.dto.MessageTaskInfo;
-import com.alinesno.infra.base.im.service.ISSEService;
+import com.alinesno.infra.smart.im.dto.ChatMessageDto;
 import com.alinesno.infra.base.im.utils.AgentUtils;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.service.ISSEService;
 import jakarta.validation.constraints.NotBlank;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
