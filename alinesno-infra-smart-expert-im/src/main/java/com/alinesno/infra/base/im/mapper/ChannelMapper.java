aad16e6e92cb3e4497175a3ed554b781f608d677
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复: 更新 ChannelMapper 接口引用至正确实体类。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelMapper.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/ChannelMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.base.im.mapper;
 
-import com.alinesno.infra.base.im.entity.ChannelEntity;
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.ChannelEntity;
 
 public interface ChannelMapper extends IBaseMapper<ChannelEntity> {
     
