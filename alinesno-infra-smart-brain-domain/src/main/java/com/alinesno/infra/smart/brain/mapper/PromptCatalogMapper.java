04a1b05ca5b1323a10aa1d25ddb5c335c34b15f6
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:53 +0800

    修复并格式化PromptCatalogMapper接口文件。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/PromptCatalogMapper.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/mapper/PromptCatalogMapper.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.brain.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.brain.entity.PromptCatalogEntity;
-
-/**
- * Prompt指令类型Mapper接口
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface PromptCatalogMapper extends IBaseMapper<PromptCatalogEntity>{
-}
+//package com.alinesno.infra.smart.brain.mapper;
+//
+//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+//import com.alinesno.infra.smart.brain.entity.PromptCatalogEntity;
+//
+///**
+// * Prompt指令类型Mapper接口
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface PromptCatalogMapper extends IBaseMapper<PromptCatalogEntity>{
+//}
