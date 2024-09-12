ee617e842033e50cd2bede8c45131e6982821439
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复了PromptCollectionServiceImpl.java文件的导入包注释和类注释，以提高代码可读性。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/PromptCollectionServiceImpl.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/PromptCollectionServiceImpl.java
@@ -1,22 +1,22 @@
-package com.alinesno.infra.smart.brain.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
-import com.alinesno.infra.smart.brain.mapper.PromptCollectionMapper;
-import com.alinesno.infra.smart.brain.service.IPromptCollectionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-/**
- * 资产收藏Service业务层处理
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Service
-public class PromptCollectionServiceImpl extends IBaseServiceImpl<PromptCollectionEntity, PromptCollectionMapper> implements IPromptCollectionService {
-    //日志记录
-    @SuppressWarnings("unused")
-    private static final Logger log = LoggerFactory.getLogger(PromptCollectionServiceImpl.class);
-}
+//package com.alinesno.infra.smart.brain.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.brain.entity.PromptCollectionEntity;
+//import com.alinesno.infra.smart.brain.mapper.PromptCollectionMapper;
+//import com.alinesno.infra.smart.brain.service.IPromptCollectionService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Service;
+//
+///**
+// * 资产收藏Service业务层处理
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Service
+//public class PromptCollectionServiceImpl extends IBaseServiceImpl<PromptCollectionEntity, PromptCollectionMapper> implements IPromptCollectionService {
+//    //日志记录
+//    @SuppressWarnings("unused")
+//    private static final Logger log = LoggerFactory.getLogger(PromptCollectionServiceImpl.class);
+//}
