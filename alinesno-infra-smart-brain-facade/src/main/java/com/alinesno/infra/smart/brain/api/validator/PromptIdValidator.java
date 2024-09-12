b55efc38cb26fb9e8e008c77e6f6d36e5cd082ea
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复并完善PromptIdValidator验证逻辑

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/api/validator/PromptIdValidator.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/api/validator/PromptIdValidator.java
@@ -1,21 +1,21 @@
-package com.alinesno.infra.smart.brain.api.validator;
-
-import com.alinesno.infra.smart.brain.api.BrainTaskDto;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-
-/**
- * 自定义验证器实现类
- * 用于验证PromptId的有效性
- */
-public class PromptIdValidator implements ConstraintValidator<ValidPromptId, BrainTaskDto> {
-
-    @Override
-    public void initialize(ValidPromptId constraintAnnotation) {
-    }
-
-    @Override
-    public boolean isValid(BrainTaskDto dto, ConstraintValidatorContext context) {
-        return dto.getPromptId() != null ;
-    }
-}
+//package com.alinesno.infra.smart.brain.api.validator;
+//
+//import com.alinesno.infra.smart.brain.api.BrainTaskDto;
+//import jakarta.validation.ConstraintValidator;
+//import jakarta.validation.ConstraintValidatorContext;
+//
+///**
+// * 自定义验证器实现类
+// * 用于验证PromptId的有效性
+// */
+//public class PromptIdValidator implements ConstraintValidator<ValidPromptId, BrainTaskDto> {
+//
+//    @Override
+//    public void initialize(ValidPromptId constraintAnnotation) {
+//    }
+//
+//    @Override
+//    public boolean isValid(BrainTaskDto dto, ConstraintValidatorContext context) {
+//        return dto.getPromptId() != null ;
+//    }
+//}
