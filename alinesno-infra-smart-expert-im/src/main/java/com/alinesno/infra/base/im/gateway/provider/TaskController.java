ba54754a3f8ee960320b4e9f6c6805313abc11fa
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:51 +0800

    feat: 更新IM任务控制器接口，将基础IM消息任务信息更新为智能IM消息任务信息。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/TaskController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/TaskController.java
@@ -1,8 +1,8 @@
 package com.alinesno.infra.base.im.gateway.provider;
 
-import com.alinesno.infra.base.im.dto.MessageTaskInfo;
-import com.alinesno.infra.base.im.service.ITaskService;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.service.ITaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
