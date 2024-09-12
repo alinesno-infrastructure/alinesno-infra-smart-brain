4ba717a46f5028d2ebd0806845ef79d7db256f7f
周健(James) <jamesAip@gmail.com>
2024-09-13 00:18:52 +0800

    修复并优化PromptModelServiceImpl类的文档注释及导入语句。

--- alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/PromptModelServiceImpl.java
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/service/impl/PromptModelServiceImpl.java
@@ -1,24 +1,24 @@
-package com.alinesno.infra.smart.brain.service.impl;
-
-import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.brain.entity.PromptModelEntity;
-import com.alinesno.infra.smart.brain.mapper.PromptModelMapper;
-import com.alinesno.infra.smart.brain.service.IPromptModelService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-
-/**
- *
- * 模型管理Service业务层处理
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-@Service
-public class PromptModelServiceImpl extends IBaseServiceImpl<PromptModelEntity, PromptModelMapper> implements IPromptModelService {
-    //日志记录
-    @SuppressWarnings("unused")
-    private static final Logger log = LoggerFactory.getLogger(PromptModelServiceImpl.class);
-}
+//package com.alinesno.infra.smart.brain.service.impl;
+//
+//import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+//import com.alinesno.infra.smart.brain.entity.PromptModelEntity;
+//import com.alinesno.infra.smart.brain.mapper.PromptModelMapper;
+//import com.alinesno.infra.smart.brain.service.IPromptModelService;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Service;
+//
+//
+///**
+// *
+// * 模型管理Service业务层处理
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//@Service
+//public class PromptModelServiceImpl extends IBaseServiceImpl<PromptModelEntity, PromptModelMapper> implements IPromptModelService {
+//    //日志记录
+//    @SuppressWarnings("unused")
+//    private static final Logger log = LoggerFactory.getLogger(PromptModelServiceImpl.class);
+//}
