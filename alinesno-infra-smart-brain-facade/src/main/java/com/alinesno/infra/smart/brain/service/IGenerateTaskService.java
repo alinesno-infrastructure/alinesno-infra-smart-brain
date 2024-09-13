1420e2318c9490c84b41f199ff49efce3f8600b1
李伟(Michael) <michaelAip@gmail.com>
2024-09-13 21:57:10 +0800

    feat:优化 GenerateTaskService 接口，修改并新增方法实现。

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IGenerateTaskService.java
+++ alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IGenerateTaskService.java
@@ -14,13 +14,7 @@ public interface IGenerateTaskService extends IBaseService<GenerateTaskEntity> {
      * @param dto
      * @return
      */
-    String commitTask(BrainTaskDto dto);
-
-    /**
-     * 提交任务到CMS服务中
-     * @param dto
-     */
-    void commitTaskToCms(BrainTaskDto dto);
+    Long commitTask(BrainTaskDto dto);
 
     /**
      * 查询所有未完成的任务
@@ -28,36 +22,11 @@ public interface IGenerateTaskService extends IBaseService<GenerateTaskEntity> {
      */
     List<GenerateTaskEntity> getAllUnfinishedTasks(int retryCount);
 
-//    /**
-//     * 获取到LLM生成的内容
-//     * @param businessId
-//     * @return
-//     */
-//    TaskContentDto getContentByBusinessId(String businessId);
-
-    /**
-     * 重置重试次数
-     * @param taskId
-     */
-    void resetRetry(Long taskId);
-
-//    /**
-//     * 更新生成内容
-//     * @param dto
-//     */
-//    void modifyContent(TaskContentDto dto);
-
     /**
      * 查询出所有进行超时而且异常的任务
      * @param jobOutTime
      * @return
      */
     List<GenerateTaskEntity> getAllUnstableTasks(int jobOutTime);
-
-    /**
-     * 重新运行任务服务
-     * @param dto
-     */
-    void resetRetryTask(GenerateTaskEntity dto);
 
 }
