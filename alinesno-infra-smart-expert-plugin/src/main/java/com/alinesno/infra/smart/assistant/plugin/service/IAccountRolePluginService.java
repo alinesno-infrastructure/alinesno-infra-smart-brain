92e311175f8797b087d77de4948aabe0da65ea98
李伟(Michael) <michaelAip@gmail.com>
2024-09-13 21:57:09 +0800

    更新:alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/IAccountRolePluginService.java

--- alinesno-infra-smart-expert-plugin/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/IAccountRolePluginService.java
+++
@@ -1,8 +0,0 @@
-//package com.alinesno.infra.smart.assistant.plugin.service;
-//
-//import com.alinesno.infra.common.facade.services.IBaseService;
-//import com.alinesno.infra.smart.assistant.entity.AccountRolePluginEntity;
-//
-//public interface IAccountRolePluginService extends IBaseService<AccountRolePluginEntity> {
-//
-//}
