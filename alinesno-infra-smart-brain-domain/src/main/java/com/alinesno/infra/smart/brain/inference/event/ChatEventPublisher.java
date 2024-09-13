4ec6fe0f14efde8ae0acca4b0203ff0cf1ff0ec7
李伟(Michael) <michaelAip@gmail.com>
2024-09-13 21:57:09 +0800

    修复并优化了ChatEventPublisher类的事件发布逻辑。

---
+++ alinesno-infra-smart-brain-domain/src/main/java/com/alinesno/infra/smart/brain/inference/event/ChatEventPublisher.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.smart.brain.inference.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatEventPublisher {
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    public void publishEvent(TaskEvent entity) {
+        eventPublisher.publishEvent(entity);
+    }
+}
