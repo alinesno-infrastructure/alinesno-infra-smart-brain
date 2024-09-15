1ec5e3d1f37c134c2534195b9dd7136b81e5cf12
陈伟(Victor) <victorAip@gmail.com>
2024-09-15 22:17:00 +0800

    新增 VectorSearchDto 类，用于封装向量搜索请求参数。

---
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/dto/VectorSearchDto.java
@@ -0,0 +1,30 @@
+package com.alinesno.infra.smart.assistant.adapter.dto;
+
+import lombok.Data;
+
+/**
+ * 用于向量搜索的DTO类
+ * 这个类封装了向量搜索请求的必要参数，以便在智能助手服务中使用
+ */
+@Data
+public class VectorSearchDto {
+
+    /**
+     * 数据集的唯一标识符
+     * 用于指定要进行向量搜索的数据集
+     */
+    private String datasetId;
+
+    /**
+     * 用户输入的搜索文本
+     * 用于在数据集中查找与该文本相关的项
+     */
+    private String searchText;
+
+    /**
+     * 搜索结果中返回的顶级匹配项的数量
+     * 用于指定返回结果的数量，表示返回多少个最相关的项
+     */
+    private int topK;
+
+}
