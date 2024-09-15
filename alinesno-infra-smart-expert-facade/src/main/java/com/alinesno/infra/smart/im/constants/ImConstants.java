dbf6878fb7284f1856843cec56a42e7e543591c8
陈伟(Victor) <victorAip@gmail.com>
2024-09-16 07:58:40 +0800

    feat:添加智能IM应用常量接口及其描述

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/constants/ImConstants.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/constants/ImConstants.java
@@ -1,5 +1,16 @@
 package com.alinesno.infra.smart.im.constants;
 
+/**
+ * 该接口定义了智能IM应用中的常量
+ * 常量用于标识默认头像和不同类型的功能
+ */
 public interface ImConstants {
+    // 默认头像的标识符
     String DEFAULT_AVATAR = "1830185154541305857" ;
+
+    // 功能类型的标识符
+    String TYPE_FUNCTION = "function";
+
+    // 修改类型的标识符
+    String TYPE_MODIFY = "modify";
 }
