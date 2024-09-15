7f17c8c1c587b650b4d0db4e98809953ba836d78
陈伟(Victor) <victorAip@gmail.com>
2024-09-15 22:17:00 +0800

    修改IndustryRoleEntity类，添加知识库ID字段。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/IndustryRoleEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/IndustryRoleEntity.java
@@ -4,6 +4,7 @@ import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
@@ -64,4 +65,7 @@ public class IndustryRoleEntity extends InfraBaseEntity {
     @ColumnType(value = MySqlTypeConstant.BIGINT, length = 11)
     @ColumnComment("会话次数")
     private Long chatCount = 0L ;
+
+    @Column(type = MySqlTypeConstant.VARCHAR, length = 32, isNull = true , comment = "知识库ID")
+    private String knowledgeId ;
 }
