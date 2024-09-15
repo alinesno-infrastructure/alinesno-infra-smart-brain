18aeee0c3607036f89a20a4cf34187e3602c4aed
陈伟(Victor) <victorAip@gmail.com>
2024-09-15 22:17:00 +0800

    新增 BaseSearchConsumer 接口实现基础的搜索管理服务。

---
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/BaseSearchConsumer.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.smart.assistant.adapter;
+
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.facade.response.R;
+import com.alinesno.infra.smart.assistant.adapter.dto.VectorSearchDto;
+import com.dtflys.forest.annotation.*;
+import com.dtflys.forest.callback.OnProgress;
+
+/**
+ * 基础的搜索管理服务
+ */
+@BaseRequest(baseURL = "#{alinesno.infra.gateway.host}/base-search" , connectTimeout = 30*1000)
+public interface BaseSearchConsumer {
+
+   /**
+    * 上传文件到知识库
+    * @param filePath
+    * @param datasetId
+    * @param onProgress
+    * @return
+    */
+   @Post(url = "/api/base/search/vectorData/upload" , contentType = "multipart/form-data")
+   AjaxResult datasetUpload(@DataFile("file") String filePath , @Body("datasetId") String datasetId  , OnProgress onProgress) ;
+
+   /**
+    * 创建知识库数据集
+    */
+   @Post(url = "/api/base/search/vectorData/createDataset")
+   R<String> datasetCreate(@Query("datasetDesc") String datasetDesc , @Query("datasetName") String datasetName) ;
+
+   /**
+    * 查询数据集
+    */
+   @Post(url = "/api/base/search/vectorSearch/search")
+   R<String> datasetSearch(@JSONBody VectorSearchDto topK) ;
+
+}
