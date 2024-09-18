9270793aa17eb12e55775d2704bb74ccab962e01
李伟(Michael) <michaelAip@gmail.com>
2024-09-19 07:06:19 +0800

    创建 parseDataset.vue 文件，包含配置知识库数据集的组件结构与导航。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/knowledge/parseDataset.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="app-container" >
+        <el-page-header @back="goBack">
+          <template #content>
+              <span class="text-large font-600 mr-3" style="font-size: 16px;"> 配置知识库数据集</span>
+          </template>
+      </el-page-header>
+
+      <div style="margin-top:20px">
+          <el-tabs :tab-position="tabPosition" style="height: calc(100vh - 150px)" class="demo-tabs">
+              <el-tab-pane style="height: 100%;" label="数据源管理">
+                  <DatasetList />
+              </el-tab-pane>
+              <el-tab-pane label="向量性测试">
+                  <DatasetSearch />
+              </el-tab-pane>
+          </el-tabs>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import DatasetList from './datasetList';
+import DatasetSearch from './datasetSearch';
+
+const tabPosition = ref('left')
+const router = useRouter();
+
+const goBack = () => {
+  console.log('go back')
+  let path = '/expert/smart/assistant/role/index';
+  router.push(path)
+}
+</script>
+
+<style lang="scss">
+
+.demo-tabs{
+  .el-tabs__nav-wrap::after{
+      height:0px !important;
+  }
+}
+
+</style>
