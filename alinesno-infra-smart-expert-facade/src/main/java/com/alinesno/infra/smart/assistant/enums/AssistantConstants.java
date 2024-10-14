5977e540d5e14a7874c32601715df9f3db9d6c18
周健(James) <jamesAip@gmail.com>
2024-10-14 21:09:14 +0800

    修改 AssistantConstants 类，新增常量 Prefix_Assistant_Flow。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
@@ -4,6 +4,7 @@ public interface AssistantConstants {
 
     String PREFIX_ASSISTANT = "Agent_" ;
     String PREFIX_ASSISTANT_DEFAULT = "Agent_DefaultExpert" ;
+    String PREFIX_ASSISTANT_FLOW = "Agent_ScriptFlow" ;
 
     String AIP_HELP_AGENT =  "AIP_DEFAULT_HELP_AGENT";
 }
