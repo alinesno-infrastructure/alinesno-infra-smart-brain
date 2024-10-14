36bd2ca9d7d6294a0d42499a279073a64c509e6e
周健(James) <jamesAip@gmail.com>
2024-10-14 21:09:13 +0800

    新增TestDao类，模拟简单DAO实现。

---
+++ alinesno-infra-smart-expert-gateway/src/main/java/com/alinesno/infra/smart/assistant/gateway/dynamic/TestDao.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.gateway.dynamic;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 模拟一个简单的dao实现
+ * @author rongdi
+ * @date 2021-01-06
+ */
+@Component
+public class TestDao {
+
+    public String query(String msg) {
+        return "msg:"+msg;
+    }
+
+}
