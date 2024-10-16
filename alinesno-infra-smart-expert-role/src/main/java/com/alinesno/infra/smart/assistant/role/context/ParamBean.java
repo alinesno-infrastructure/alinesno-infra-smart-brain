c095ce40c25874686a477e5c7e9e9feca7ef5370
周健(James) <jamesAip@gmail.com>
2024-10-16 08:27:34 +0800

    feat: 添加 ParamBean 类，包含全局参数属性。

---
+++ alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/context/ParamBean.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.role.context;
+
+import lombok.Data;
+
+/**
+ * 全局参数
+ */
+@Data
+public class ParamBean {
+
+    private String value ;
+    private long addTime ;
+
+}
