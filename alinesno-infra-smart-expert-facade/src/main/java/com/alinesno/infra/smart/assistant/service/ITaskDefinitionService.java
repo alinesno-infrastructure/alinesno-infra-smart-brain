ea033902242a0c24da4873ac011c45b34d9d82f8
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat:定义 ITaskDefinitionService 接口，包含基本服务方法。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/ITaskDefinitionService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.TaskDefinitionEntity;
+
+public interface ITaskDefinitionService extends IBaseService<TaskDefinitionEntity> {
+}
