e25612fccc47399ddeac56eb1f41aa86a37d5bd4
李伟(Michael) <michaelAip@gmail.com>
2024-10-17 00:14:28 +0800

    feat: 添加 FlowMap 组件模板结构

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/common/FlowMap.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="flow-design-map" class="flow-design-map">
+    <!-- <img :src="$store.state.flow.mapImg" /> -->
+    <div id="flow-design-map-mask" class="map-mask" :style="mapMask" @mousedown="handleMouseDown" @mouseup="handleMouseUp" @mouseleave="handleMouseUp"></div>
+  </div>
+</template>
+
+<script setup>
+
+// import { flowMixin } from './flowMixin'
+
+</script>
