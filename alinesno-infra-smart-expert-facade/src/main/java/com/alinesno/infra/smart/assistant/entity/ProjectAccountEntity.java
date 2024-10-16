bdb8ede17bd7dd9828acf28c93c7dd06237a050c
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat: 添加 `ProjectAccountEntity` 类，包含账户ID和应用排序属性。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/ProjectAccountEntity.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.smart.assistant.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 账号与应用关联
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("project_account")
+public class ProjectAccountEntity extends InfraBaseEntity {
+
+    @TableField("account_id")
+    @ColumnType(length = 32)
+    @ColumnComment("账户ID")
+    private Long accountId;
+
+    @TableField("app_order")
+    @ColumnType
+    @ColumnComment("应用排序")
+    private Long appOrder;
+
+}
