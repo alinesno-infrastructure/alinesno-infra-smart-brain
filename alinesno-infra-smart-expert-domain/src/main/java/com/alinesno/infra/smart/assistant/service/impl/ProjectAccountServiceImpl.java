ada92f48cc4cdc510bd325d655ddb5576e6b0fa5
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:28 +0800

    feat:实现 ProjectAccountServiceImpl 类，提供项目账户服务的基础实现。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/ProjectAccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.smart.assistant.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.ProjectAccountEntity;
+import com.alinesno.infra.smart.assistant.mapper.ProjectAccountMapper;
+import com.alinesno.infra.smart.assistant.service.IProjectAccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class ProjectAccountServiceImpl extends IBaseServiceImpl<ProjectAccountEntity, ProjectAccountMapper> implements IProjectAccountService {
+
+}
