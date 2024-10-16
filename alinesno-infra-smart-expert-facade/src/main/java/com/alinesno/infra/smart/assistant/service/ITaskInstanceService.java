6034641ea00e966d995fd26d60b6b8916b9d0249
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:28 +0800

    feat:新增 ITaskInstanceService 接口，定义任务实例服务的基础操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/ITaskInstanceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.TaskInstanceEntity;
+
+public interface ITaskInstanceService extends IBaseService<TaskInstanceEntity> {
+}
