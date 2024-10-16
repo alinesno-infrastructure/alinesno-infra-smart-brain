01f4a48e80c6b588168503f85c50225cf4910b8c
梁明树(Nina) <ninaAip@gmail.com>
2024-10-16 08:27:34 +0800

    改写 AssistantConstants 枚举类，调整默认脚本处理及新增脚本引擎相关常量。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
@@ -3,8 +3,15 @@ package com.alinesno.infra.smart.assistant.enums;
 public interface AssistantConstants {
 
     String PREFIX_ASSISTANT = "Agent_" ;
+
+    // 默认脚本处理
     String PREFIX_ASSISTANT_DEFAULT = "Agent_DefaultExpert" ;
-    String PREFIX_ASSISTANT_FLOW = "Agent_ScriptFlow" ;
+
+    // groovy脚本流程引擎
+    String PREFIX_ASSISTANT_SCRIPT = "Agent_ScriptExpert" ;
+
+    // 流程脚本引擎
+    String PREFIX_ASSISTANT_FLOW = "Agent_FlowExpert" ;
 
     String AIP_HELP_AGENT =  "AIP_DEFAULT_HELP_AGENT";
 }
