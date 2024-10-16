b1d2b8feb8c0e46f4fda35ee0edb669362a94b98
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat:新增 `TaskInstanceMapper` 接口，实现基本映射功能。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/TaskInstanceMapper.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.entity.TaskInstanceEntity;
+
+/**
+ * ProcessDefinitionMapper接口
+ */
+public interface TaskInstanceMapper extends IBaseMapper<TaskInstanceEntity> {
+
+}
