e7486550eb45c3a83e79fefc88949c0828fa4744
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat:新增智能体事件实体类 `AgentEventEntity`，包含基本属性。

---
+++ alinesno-infra-smart-expert-inferen/src/main/java/com/alinesno/infra/smart/inference/agent/entity/AgentEventEntity.java
@@ -0,0 +1,35 @@
+package com.alinesno.infra.smart.inference.agent.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * 智能体事件类
+ */
+@EqualsAndHashCode(callSuper = true)
+@TableName("life_agent_event")
+@Data
+@NoArgsConstructor
+public class AgentEventEntity extends InfraBaseEntity {
+
+    @TableField
+    private long agentId ; // 智能体ID
+
+    @TableField
+    private String code; // 事件代码
+
+    @TableField
+    private String subject;  // 主题
+
+    @TableField
+    private String predicate;  // 谓词
+
+    @TableField
+    private String object; // 对象
+
+}
