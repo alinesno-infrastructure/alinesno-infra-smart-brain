3724aa789e57b07c10eca35a3b2e1d6437bea3ad
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat: 实现 TaskDefinitionServiceImpl 类，提供任务定义服务基础实现。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/TaskDefinitionServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.smart.assistant.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.TaskDefinitionEntity;
+import com.alinesno.infra.smart.assistant.mapper.TaskDefinitionMapper;
+import com.alinesno.infra.smart.assistant.service.ITaskDefinitionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class TaskDefinitionServiceImpl extends IBaseServiceImpl<TaskDefinitionEntity, TaskDefinitionMapper> implements ITaskDefinitionService {
+}
