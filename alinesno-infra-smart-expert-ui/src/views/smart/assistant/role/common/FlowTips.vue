175e813bab807ee2905de94fde22ad05bfb0b460
李伟(Michael) <michaelAip@gmail.com>
2024-10-17 00:14:28 +0800

    feat: 添加 FlowTips 组件，展示流程状态提示信息。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/common/FlowTips.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flow-tips">
+    <div class="flow-tips-item">
+      <span class="flow-tips-item-icon flow-tips-complete">●</span>
+      <span>已完成</span>
+    </div>
+    <div class="flow-tips-item">
+      <span class="flow-tips-item-icon flow-tips-current">●</span>
+      <span>进行中</span>
+    </div>
+    <div class="flow-tips-item">
+      <span class="flow-tips-item-icon flow-tips-not">●</span>
+      <span>未进行</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { flowMixin } from './flowMixin';
+</script>
