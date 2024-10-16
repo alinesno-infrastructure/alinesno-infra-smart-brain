247c0b58e86c908a7fa5291d63ec2dfe4cbe8c8b
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat:新增 IProcessInstanceService 接口，包含基本属性和方法。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IProcessInstanceService.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ProcessInstanceEntity;
+
+/**
+ */
+public interface IProcessInstanceService extends IBaseService<ProcessInstanceEntity> {
+
+
+}
