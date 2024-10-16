ab100606bfaaf55f684f8b5d9be2653210eca1cc
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:29 +0800

    feat:新增 ProcessDefinitionDto 类，用于封装流程定义的相关信息。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/task/ProcessDefinitionDto.java
@@ -0,0 +1,36 @@
+package com.alinesno.infra.smart.assistant.api.task;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 流程定义信息
+ * 该类用于封装流程定义的相关信息，包括流程的上下文环境和任务列表
+ */
+@ToString
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProcessDefinitionDto implements Serializable {
+
+    /**
+     * 项目ID
+     */
+    private long projectId ;
+
+    /**
+     * 流程上下文信息
+     * 包含了流程执行时的环境和配置信息
+     */
+    private ProcessContextDto context ;
+
+    /**
+     * 流程任务列表
+     * 定义了流程中包含的所有任务信息
+     */
+    private List<ProcessTaskDto> taskFlow ;
+
+}
