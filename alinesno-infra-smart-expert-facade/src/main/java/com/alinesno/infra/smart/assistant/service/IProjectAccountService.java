10ea2cd5f56c6adafc09c36fe2e7f370b295534e
王磊(Levi) <leviAip@gmail.com>
2024-10-17 00:14:28 +0800

    feat:新增 IProjectAccountService 接口，包含基本属性和方法。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IProjectAccountService.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ProjectAccountEntity;
+
+/**
+ * CredentialService接口
+ */
+public interface IProjectAccountService extends IBaseService<ProjectAccountEntity> {
+}
