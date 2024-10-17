b492631aa6ddfe39b83f22e2e01a7b7d3b6909b9
陈伟(Victor) <victorAip@gmail.com>
2024-10-17 17:18:04 +0800

    feat:在 `ITaskDefinitionService` 接口中新增流程定义提交、运行验证任务和验证流程方法。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/ITaskDefinitionService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/ITaskDefinitionService.java
@@ -1,7 +1,37 @@
 package com.alinesno.infra.smart.assistant.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.api.task.ProcessDefinitionDto;
+import com.alinesno.infra.smart.assistant.api.task.ProcessTaskValidateDto;
 import com.alinesno.infra.smart.assistant.entity.TaskDefinitionEntity;
 
 public interface ITaskDefinitionService extends IBaseService<TaskDefinitionEntity> {
+
+    /**
+     * 保存流程定义
+     *
+     * @param dto
+     * @return
+     */
+    long commitProcessDefinition(ProcessDefinitionDto dto);
+
+    /**
+     * 运行验证任务
+     * @param dto
+     */
+    void runProcessTask(ProcessTaskValidateDto dto);
+
+//    /**
+//     * 更新流程定义
+//     * @param dto
+//     */
+//    void updateProcessDefinition(ProcessDefinitionDto dto);
+
+    /**
+     * 验证流程
+     * @param dto
+     * @return
+     */
+    long validateProcessDefinition(ProcessDefinitionDto dto);
+
 }
