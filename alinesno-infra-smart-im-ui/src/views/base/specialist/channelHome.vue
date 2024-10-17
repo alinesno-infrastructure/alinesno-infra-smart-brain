a4263d68235a9594dafa473e400a961788a51c55
陈伟(Victor) <victorAip@gmail.com>
2024-10-17 12:31:15 +0800

    feat:新增频道市场搜索及Banner展示功能

--- alinesno-infra-smart-im-ui/src/views/base/specialist/channelHome.vue
+++ alinesno-infra-smart-im-ui/src/views/base/specialist/channelHome.vue
@@ -2,23 +2,154 @@
 
   <div class="app-container tpl-app">
 
+    <div class="search-container-panel">
+      <el-row>
+        <el-col :span="24">
+          <div class="feature-header-xqbyQk" style="    
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr minmax(auto, 480px) 1fr;
+    gap: 12px;
+    width: 100%;">
+            <div style="gap: 12px;">
+              <h1 style="font-size: 20px; font-weight: 500; font-style: normal; line-height: 32px; color: rgba(var(--coze-fg-4), var(--coze-fg-4-alpha)); margin: 0px 0px 0px 10px; float: left;">
+                频道市场</h1>
+            </div>
+            <div class="search-container-weDuEn">
+              <el-input
+      v-model="input1"
+      style="width: 400px"
+      size="large"
+      placeholder="搜索频道"
+      :suffix-icon="Search"
+    />
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <div class="banner-container-panel">
+      <el-row>
+        <el-col :span="15">
+
+          <div
+            style="gap: 0px;margin-left:10px;cursor: pointer;align-items: flex-start;width: 100%;height: 270px;padding: 16px 0;display: inline-flex;flex-direction: column;">
+            <div>
+              <div style="height: 28px;"><img
+                  src="https://lf-coze-web-cdn.coze.cn/obj/coze-web-cn/mf/marketplace/static/image/brand-daily-rec.48709716.png"
+                  data-src="https://lf-coze-web-cdn.coze.cn/obj/coze-web-cn/mf/marketplace/static/image/brand-daily-rec.48709716.png"
+                  height="28"></div>
+              <h1
+                style="-webkit-line-clamp: 2;max-height: 112px;margin-bottom: 8px;font-size: 40px;font-weight: 700;line-height: 48px;color: #1D1C23;">
+                <span>🪡拆解产品卖点，提炼核心特色，神器在此，转化无忧</span>
+              </h1>
+              <p style="
+    height: 24px;
+    margin-bottom: 28px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    color: rgba(29, 28, 35, 0.6);
+"><span>快速让产品的核心价值得到用户认可，将卖点转化顾客的买点，快来体验吧！</span></p>
+            </div>
+            <el-button type="primary" bg text size="large">
+              <span class="semi-button-content" x-semi-prop="children">立即聊聊</span>
+            </el-button>
+          </div>
+
+
+        </el-col>
+        <el-col :span="9">
+
+          <div class="right-container-_0GQ3U"
+            style="background-image: url(&quot;https://p9-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/765a47720e944173bbbd678aefbcae0a~tplv-13w3uml6bg-crop-center-v1:160:90.image?rk3s=2e2596fd&amp;x-expires=1731720832&amp;x-signature=06aGzxG4xmlYmTcQjLXvchlLric%3D&quot;); background-repeat: no-repeat; background-size: cover;">
+            <img
+              src="https://p3-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/765a47720e944173bbbd678aefbcae0a~tplv-13w3uml6bg-crop-center-v1:960:540.image?rk3s=2e2596fd&amp;x-expires=1731720832&amp;x-signature=3mc%2BXg3y33CglmFfKM1ZekRzLA8%3D"
+              class="bot-banner-bg-_IItqZ" style="
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 4px 24px 0 rgba(0, 0, 0, 0.12);
+">
+            <div class=""
+              style="gap: 16px;position: absolute;bottom: 0;left: 0;width: 100%;padding: 5px;background: linear-gradient(0deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%);align-items: center;flex-direction: row;display: inline-flex;position: absolute;bottom: 0;left: 0;width: 100%;/* padding: 32px 24px 24px; */background: linear-gradient(0deg, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%);">
+              <span class="" style="
+    width: 64px;
+    height: 64px;
+    background: #f0f0f5;
+    border-radius: 8px;
+"><img src="https://p6-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/6f8d3536f35a4e9ab28ffccfbb8b12c9~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&amp;x-expires=1731720832&amp;x-signature=YJXYms7Xo1cTrUMOL6TVPo3GSiA%3D"
+                  class="" style="
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    width: 100%;
+"></span>
+              <div class="" x-semi-prop="children"
+                style="gap: 4px;overflow: hidden;flex: 1;align-items: flex-start;width: 100%;display: inline-flex;flex-direction: column;">
+                <p class="" style="
+    width: 100%;
+    font-size: 10px;
+    font-weight: 400;
+    font-style: normal;
+    line-height: 12px;
+    color: #FFF;
+    display: block;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 0px;
+"><span>商业服务</span></p>
+                <h1 class="" style="
+    width: 100%;
+    font-size: 18px;
+    font-weight: 600;
+    font-style: normal;
+    line-height: 24px;
+    color: #FFF;
+    margin: 0px;
+"><span>产品卖点提炼神器</span></h1>
+                <div class=""
+                  style="gap: 4px;flex-shrink: 0;max-width: 100%;height: 18px;margin: 0px;color: rgba(255, 255, 255, 0.39);display: inline-flex;flex-direction: row;">
+                  <div class="semi-image avatar-oDHtb3"
+                    style="width: 14px;height: 14px;overflow: hidden;border-radius: 12px;"><img
+                      src="https://p3-passport.byteacctimg.com/img/user-avatar/2f5f197d9767f138e735359e3822661b~300x300.image"
+                      data-src="https://p3-passport.byteacctimg.com/img/user-avatar/2f5f197d9767f138e735359e3822661b~300x300.image"
+                      class="semi-image-img" width="14" height="14"></div>
+                  <div class="" x-semi-prop="children"
+                    style="gap: 2px;flex: 1;font-size: 12px;font-weight: 400;line-height: 18px;"><span class=""
+                      style="max-width: 150px;"><span>罗文</span></span></div><span class="" style="
+    flex: 1;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 18px;
+"><span>@LUOWEN</span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </el-col>
+      </el-row>
+    </div>
+
     <div class="channel-container-panel">
       <el-row>
-        <el-col :span="6" v-for="(item , index) in demeChannel" :key="index" style="padding:8px;">
-          <div class="semi-card-container">
+
+        <el-col :span="6" v-for="(item, index) in publicChatChannel" :key="index" style="padding:8px;">
+          <div class="semi-card-container" @click="handleChannelChat(item)">
             <div class="semi-space cart-head-continer" style="gap: 16px;">
               <div class="cart-head-content">
                 <div class="cart-head-content">
                   <span class="semi-avatar semi-avatar-square">
-                    <img
-                      :src="item.icon"
-                      class="">
+                    <img :src="imagePath(item)"  class="">
                   </span>
                 </div>
               </div>
               <div class="semi-space info-container" style="gap: 6px;">
                 <span class="semi-typography card-title">
-                  <span>BO播客生成计划</span>
+                  <span>
+                    {{ item.channelName }}
+                  </span>
                 </span>
                 <div class="semi-space container-center" style="gap: 6px;">
                   <div class="semi-image avatar-oDHtb3" style="width: 14px; height: 14px;">
@@ -28,14 +159,11 @@
                   </div>
                   <div class="semi-space semi-space-align-center semi-space-horizontal" style="gap: 2px;">
                     <span class="semi-typography text" style="max-width: 150px;"><span>韦恩W</span></span>
-                  </div><span class="semi-typography text-nickname">
-                    <span>@wayne2012</span>
-                  </span>
+                  </div>
                 </div>
                 <p class="semi-typography card-desc" style="-webkit-line-clamp: 3;">
                   <span>
-                    BO播客生成计划是一款基于语音识别和自然语言处理技术的对话机器人，用户可以通过它快速创建个性化的播客节目，提高节目制作效率和质量。同时，它还提供了丰富的音频特效和语音转文字功能，让用户的播客节目更具吸引力和可读性。
-                    现在微信公众号文章链接、抖音分享、小红书分享（VLOG、PLOG）都支持
+                    {{ truncateString(item.channelDesc,65) }}
                   </span>
                 </p>
                 <div class="semi-space card-tag-list" style="gap: 4px;"></div>
@@ -43,7 +171,8 @@
             </div>
             <div class="semi-divider semi-divider-horizontal"></div>
             <div class="semi-space" style="height: 20px; width: 100%; gap: 8px;">
-              <div class="semi-space semi-space-align-center semi-space-horizontal" x-semi-prop="children" style="display: inline-flex;">
+              <div class="semi-space semi-space-align-center semi-space-horizontal" x-semi-prop="children"
+                style="display: inline-flex;">
                 <div class="semi-space card-statics" style="gap: 8px;">
                   <span class="semi-typography text-h6"><i class="fa-solid fa-user-ninja"></i> 1.2K</span>
                   <span class="semi-typography text-h6"><i class="fa-solid fa-link"></i> 2.1K</span>
@@ -168,71 +297,6 @@ const demeChannel = ref([
     "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/8d2b7705ea1f458e8290f618e02f580d~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=IZkhM%2FvHrr2gpkF%2BPQO5ea9dYuY%3D"
   },
   {
-    "channelName": "专业英语翻译",
-    "channelDesc": "能翻译所有英语字，词，文章，论文等。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/e5e9baa528b945e6b2e8a1c3dda5775b~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=DtNyNMVpLHpXMTDr868xaq%2BRfKs%3D"
-  },
-  {
-    "channelName": "奇幻小冒险",
-    "channelDesc": "在一个遥远的国度里，隐藏着一个被大自然宠爱的村庄。这里的每一天都充满了新的冒险和温馨的故事。而你的旅程，就从这里开始……",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/2f36541186224f47863996fd44f81f9a~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=ezd4tmzPBVdE%2FKRGZnRHiGYUmuc%3D"
-  },
-  {
-    "channelName": "文生视频✨提示专家",
-    "channelDesc": "全面支持可灵、清影、海螺、通义、Vidu、筑梦、Sora、即梦等文生视频大模型，一帧一画皆是惊喜！👀 让灵感瞬间成影！",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/f65a8eee94384a41a6cd03f0ca06149f~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=U4jDFQbyYIgN%2Fy8tFSzCq29wm0U%3D"
-  },
-  {
-    "channelName": "合成新元素",
-    "channelDesc": "通过对话的方式玩一个类似「涂鸦上帝」的元素合成游戏。初始元素是 💧 水、🔥 火、🌬️ 风、🌍 土，你可以通过不断的自由组合，来随机生成新的物质。试试看谁能把「🥽 Vision Pro」生成出来？",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/8a954c89f3104df3b404087f050e5108~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=TxPNdBsD%2Bc0Eb0yenxSmC5hgT2I%3D"
-  },
-  {
-    "channelName": "认真看一百本书",
-    "channelDesc": "一款专门为阅读爱好者设计的AI读书工具。只需输入书名，AI便能迅速整理出书籍的核心内容与相关背景资料，并以简洁明了的卡片形式展示，使阅读和学习变得更加高效便捷。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/59ba56af5a8d4bbcb51f76247a40c754~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=YvhkYVJ93ndlW%2FJsZmOc1SuYHAE%3D"
-  },
-  {
-    "channelName": "漫画生成：橘猫漫画家",
-    "channelDesc": "一个能够一键帮你制作猫咪漫画的bot，欢迎来到“橘猫漫画家”，您的口袋里的漫画工作室！每一个关于橘猫的想法都能变成生动的漫画。记住，也许我们每个人都是一只橘猫。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/8d2b7705ea1f458e8290f618e02f580d~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=IZkhM%2FvHrr2gpkF%2BPQO5ea9dYuY%3D"
-  },
-  {
-    "channelName": "专业英语翻译",
-    "channelDesc": "能翻译所有英语字，词，文章，论文等。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/e5e9baa528b945e6b2e8a1c3dda5775b~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=DtNyNMVpLHpXMTDr868xaq%2BRfKs%3D"
-  },
-  {
-    "channelName": "奇幻小冒险",
-    "channelDesc": "在一个遥远的国度里，隐藏着一个被大自然宠爱的村庄。这里的每一天都充满了新的冒险和温馨的故事。而你的旅程，就从这里开始……",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/2f36541186224f47863996fd44f81f9a~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=ezd4tmzPBVdE%2FKRGZnRHiGYUmuc%3D"
-  },
-  {
-    "channelName": "文生视频✨提示专家",
-    "channelDesc": "全面支持可灵、清影、海螺、通义、Vidu、筑梦、Sora、即梦等文生视频大模型，一帧一画皆是惊喜！👀 让灵感瞬间成影！",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/f65a8eee94384a41a6cd03f0ca06149f~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=U4jDFQbyYIgN%2Fy8tFSzCq29wm0U%3D"
-  },
-  {
-    "channelName": "合成新元素",
-    "channelDesc": "通过对话的方式玩一个类似「涂鸦上帝」的元素合成游戏。初始元素是 💧 水、🔥 火、🌬️ 风、🌍 土，你可以通过不断的自由组合，来随机生成新的物质。试试看谁能把「🥽 Vision Pro」生成出来？",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/8a954c89f3104df3b404087f050e5108~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=TxPNdBsD%2Bc0Eb0yenxSmC5hgT2I%3D"
-  },
-  {
-    "channelName": "认真看一百本书",
-    "channelDesc": "一款专门为阅读爱好者设计的AI读书工具。只需输入书名，AI便能迅速整理出书籍的核心内容与相关背景资料，并以简洁明了的卡片形式展示，使阅读和学习变得更加高效便捷。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/59ba56af5a8d4bbcb51f76247a40c754~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=YvhkYVJ93ndlW%2FJsZmOc1SuYHAE%3D"
-  },
-  {
-    "channelName": "漫画生成：橘猫漫画家",
-    "channelDesc": "一个能够一键帮你制作猫咪漫画的bot，欢迎来到“橘猫漫画家”，您的口袋里的漫画工作室！每一个关于橘猫的想法都能变成生动的漫画。记住，也许我们每个人都是一只橘猫。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/8d2b7705ea1f458e8290f618e02f580d~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=IZkhM%2FvHrr2gpkF%2BPQO5ea9dYuY%3D"
-  },
-  {
-    "channelName": "专业英语翻译",
-    "channelDesc": "能翻译所有英语字，词，文章，论文等。",
-    "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/e5e9baa528b945e6b2e8a1c3dda5775b~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=DtNyNMVpLHpXMTDr868xaq%2BRfKs%3D"
-  },
-  {
     "channelName": "万能英语助手",
     "channelDesc": "擅长：单词记忆和学习、口语对话训练、英语知识讲解、翻译。",
     "icon": "https://p26-flow-product-sign.byteimg.com/tos-cn-i-13w3uml6bg/4792a3611e0c47d2b62dcf6eeafe16e5~tplv-13w3uml6bg-resize:128:128.image?rk3s=2e2596fd&x-expires=1731712214&x-signature=0FwgEHo%2BZ3wG8mQta6SvigGeJpg%3D"
@@ -271,6 +335,9 @@ function handleAllMyChannel() {
     publicChatChannel.value = items; // .filter(item => item.channelType === '9');
     // recChatChannel.value = items.filter(item => item.channelType === '3');
 
+    // 添加DemoChannel频道
+    publicChatChannel.value = publicChatChannel.value.concat(demeChannel.value);
+
     loading.value = false;
   })
 }
@@ -285,137 +352,3 @@ function handleChannelChat(item) {
 handleAllMyChannel();
 
 </script>
-
-
-<style scoped lang="scss">
-.cart-head-continer {
-  gap: 16px;
-  align-items: flex-start;
-  flex-direction: row;
-  display: inline-flex;
-}
-
-.semi-space.card-statics {
-    font-size: 13px;
-    color: #a5a5a5;
-    display: inline-flex;
-    line-height: 30px;
-    padding-top: 3px;
-}
-
-.semi-card-container {
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  width: 100%;
-  margin-bottom: 16px;
-  padding: 20px 20px 16px;
-  background: #FFF;
-  border: 1px solid rgba(6, 7, 9, 0.1);
-  border-radius: 8px;
-
-  .semi-divider {
-    margin-top: 20px;
-    margin-bottom: 16px;
-    border-bottom: 0.5px solid #f0f0f5;
-    display: flex;
-    width: 100%;
-    box-sizing: border-box;
-    color: var(--semi-color-text-0);
-    margin: 1px 0 1px 0;
-  }
-
-  .card-desc {
-    width: 100%;
-    height: 60px;
-    margin-top: 0px;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 20px;
-    color: rgba(56, 55, 67, 0.8);
-    -webkit-box-orient: vertical;
-    display: -webkit-box;
-    overflow: hidden;
-  }
-
-  .info-container {
-    overflow: hidden;
-    width: 100%;
-    height: 136px;
-    align-items: flex-start;
-    flex-direction: column;
-    display: inline-flex;
-
-    .container-center {
-      flex-shrink: 0;
-      max-width: 100%;
-      height: 18px;
-      display: inline-flex;
-
-      .avatar-oDHtb3 {
-        overflow: hidden;
-        border-radius: 0;
-        border-radius: 12px;
-        display: inline-flex;
-      }
-
-      .semi-space-align-center {
-        align-items: flex-start;
-        flex-direction: column;
-        display: inline-flex;
-
-        .text {
-          flex: 1;
-          font-size: 13px;
-          font-weight: 400;
-          line-height: 18px;
-          color: #a5a5a5;
-        }
-
-      }
-
-      .text-nickname {
-        flex: 1;
-        font-size: 13px;
-        font-weight: 400;
-        line-height: 18px;
-        color: #a5a5a5;
-      }
-
-    }
-
-  }
-
-  .card-title {
-    height: 24px;
-    font-size: 18px;
-    font-weight: 600;
-    line-height: 24px;
-    color: #1C1D23;
-  }
-
-  .semi-avatar-square {
-    border-radius: 8px;
-    width: 64px;
-    height: 64px;
-    border: 1px solid #f0f0f5;
-    background: #f0f0f5;
-    align-items: center;
-    display: inline-flex;
-    justify-content: center;
-    overflow: hidden;
-    position: relative;
-    text-align: center;
-    vertical-align: middle;
-    white-space: nowrap;
-
-    img {
-      display: block;
-      height: 100%;
-      object-fit: cover;
-      width: 100%;
-    }
-
-  }
-}
-</style>
