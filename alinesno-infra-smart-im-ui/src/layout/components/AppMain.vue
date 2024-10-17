425eb57f73aeb00f5735610607bb5217bff60ab1
陈伟(Victor) <victorAip@gmail.com>
2024-10-17 12:31:15 +0800

    fix:调整 AppMain.vue 组件高度计算，修正高度值从90px改为50px。

--- alinesno-infra-smart-im-ui/src/layout/components/AppMain.vue
+++ alinesno-infra-smart-im-ui/src/layout/components/AppMain.vue
@@ -21,8 +21,8 @@ const tagsViewStore = useTagsViewStore()
 <style lang="scss" scoped>
 .app-main {
   /* 50= navbar  50  */
-  min-height: calc(100vh - 90px);
-  height: calc(100vh - 90px);
+  min-height: calc(100vh - 50px);
+  height: calc(100vh - 50px);
   width: 100%;
   position: relative;
   // overflow: hidden;
