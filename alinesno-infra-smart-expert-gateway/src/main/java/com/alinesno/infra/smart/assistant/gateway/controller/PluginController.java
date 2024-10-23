83f74b8cd2fc80290e8d6907e3f7f1b9dabf03eb
陈伟(Victor) <victorAip@gmail.com>
2024-10-23 19:22:05 +0800

    feat:在 PluginController 中添加数据权限注解 @DataPermissionScope

--- alinesno-infra-smart-expert-gateway/src/main/java/com/alinesno/infra/smart/assistant/gateway/controller/PluginController.java
+++ alinesno-infra-smart-expert-gateway/src/main/java/com/alinesno/infra/smart/assistant/gateway/controller/PluginController.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.assistant.gateway.controller;
 
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
@@ -43,6 +44,7 @@ public class PluginController extends BaseController<PluginEntity, IPluginServic
      * @param page DatatablesPageBean对象
      * @return 包含DataTables数据的TableDataInfo对象
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
