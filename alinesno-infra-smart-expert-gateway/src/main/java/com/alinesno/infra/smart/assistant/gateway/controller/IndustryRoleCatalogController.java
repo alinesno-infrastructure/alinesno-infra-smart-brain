0a337786cd984fcbe095ade9e162fb6089aa2870
陈伟(Victor) <victorAip@gmail.com>
2024-10-23 19:22:05 +0800

    feat:在 `IndustryRoleCatalogController` 中添加数据权限注解以增强数据访问控制。

--- alinesno-infra-smart-expert-gateway/src/main/java/com/alinesno/infra/smart/assistant/gateway/controller/IndustryRoleCatalogController.java
+++ alinesno-infra-smart-expert-gateway/src/main/java/com/alinesno/infra/smart/assistant/gateway/controller/IndustryRoleCatalogController.java
@@ -2,6 +2,10 @@ package com.alinesno.infra.smart.assistant.gateway.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
@@ -43,6 +47,7 @@ public class IndustryRoleCatalogController extends BaseController<IndustryRoleCa
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
@@ -50,9 +55,10 @@ public class IndustryRoleCatalogController extends BaseController<IndustryRoleCa
         return this.toPage(model, this.getFeign(), page);
     }
 
+    @DataPermissionQuery
     @GetMapping("/list")
-    public AjaxResult list(IndustryRoleCatalogEntity promptCatalog) {
-        List<IndustryRoleCatalogEntity> promptCatalogEntities = service.selectCatalogList(promptCatalog);
+    public AjaxResult list(IndustryRoleCatalogEntity promptCatalog , PermissionQuery query) {
+        List<IndustryRoleCatalogEntity> promptCatalogEntities = service.selectCatalogList(promptCatalog , query);
 
         return AjaxResult.success("操作成功." , promptCatalogEntities) ;
     }
@@ -61,6 +67,7 @@ public class IndustryRoleCatalogController extends BaseController<IndustryRoleCa
      * 保存角色类型
      * @return
      */
+    @DataPermissionSave
     @PostMapping("/insertCatalog")
     public AjaxResult insertCatalog(@RequestBody IndustryRoleCatalogEntity entity){
 
@@ -74,10 +81,10 @@ public class IndustryRoleCatalogController extends BaseController<IndustryRoleCa
      * @param deptId
      * @return
      */
+    @DataPermissionQuery
     @GetMapping("/excludeChild/{id}")
-    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId)
-    {
-        List<IndustryRoleCatalogEntity> depts = service.selectCatalogList(new IndustryRoleCatalogEntity());
+    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId , PermissionQuery query) {
+        List<IndustryRoleCatalogEntity> depts = service.selectCatalogList(new IndustryRoleCatalogEntity(), query);
         depts.removeIf(d -> d.getId().longValue() == deptId || ArrayUtils.contains(StringUtils.split(d.getAncestors(), ","), deptId + ""));
         return AjaxResult.success("操作成功." , depts);
     }
