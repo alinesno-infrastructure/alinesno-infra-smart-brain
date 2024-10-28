3a6e1991240c854e325a7abecb35edb8ecb37f7f
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:13:07 +0800

    refactor: 优化 RoleChatController 中的角色对话方法参数及服务注入

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/RoleChatController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/RoleChatController.java
@@ -1,35 +1,29 @@
 package com.alinesno.infra.base.im.gateway.provider;
 
-import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.IdUtil;
-import com.alibaba.dashscope.aigc.generation.GenerationResult;
 import com.alibaba.dashscope.common.Message;
-import com.alibaba.dashscope.common.ResultCallback;
 import com.alibaba.dashscope.common.Role;
-import com.alibaba.dashscope.utils.JsonUtils;
 import com.alinesno.infra.base.im.service.IRoleChatService;
 import com.alinesno.infra.base.im.utils.AgentUtils;
-import com.alinesno.infra.common.core.context.SpringContext;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
-import com.alinesno.infra.smart.assistant.api.prompt.PromptMessage;
-import com.alinesno.infra.smart.assistant.chain.IBaseExpertService;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.enums.AssistantConstants;
 import com.alinesno.infra.smart.assistant.role.llm.QianWenLLM;
 import com.alinesno.infra.smart.assistant.role.llm.adapter.MessageManager;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
-import com.alinesno.infra.smart.brain.api.dto.PromptMessageDto;
 import com.alinesno.infra.smart.im.dto.ChatMessageDto;
+import com.alinesno.infra.smart.im.dto.ChatSendMessageDto;
+import com.alinesno.infra.smart.im.service.IMessageService;
 import com.alinesno.infra.smart.im.service.ISSEService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Semaphore;
 
 /**
  * 使用于多个网站针对于专家单独咨询的接口
@@ -41,6 +35,9 @@ public class RoleChatController extends SuperController {
 
     @Autowired
     private IRoleChatService roleChatService;
+
+    @Autowired
+    private IMessageService messageService ;
 
     @Autowired
     private IIndustryRoleService industryRoleService;
@@ -99,110 +96,40 @@ public class RoleChatController extends SuperController {
 
     /**
      * 角色对话
-     * @param message
+     * @param chatMessage
+     * @param roleId
      * @return
      */
     @SneakyThrows
     @PostMapping("/chatRole")
-    public AjaxResult chatRole(@RequestBody PromptMessageDto message , long roleId){
+    public AjaxResult chatRole(@RequestBody ChatSendMessageDto chatMessage , long roleId){
 
         IndustryRoleEntity role = industryRoleService.getById(roleId) ;
-        SseEmitter emitter = sseService.getConn(String.valueOf(role.getId())) ;
 
-        ThreadUtil.execute(() -> {
-            try {
-                runChatStream(role , message , emitter);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        long currentAccountId = CurrentAccountJwt.getUserId();
+
+        List<IndustryRoleEntity> roleList = new ArrayList<>() ;
+        roleList.add(role);
+
+        List<ChatMessageDto> personDto = new ArrayList<>() ;
+        roleList.forEach(r -> {
+            ChatMessageDto msg =  AgentUtils.getChatMessageDto(r, IdUtil.getSnowflakeNextId()) ;
+            msg.setAccountId(currentAccountId);
+            personDto.add(msg) ;
         });
 
-        long bId = IdUtil.getSnowflakeNextId() ;
-        ChatMessageDto msgDto = AgentUtils.getChatMessageDto(role, bId) ;
+        chatMessage.setUsers(Collections.singletonList(roleId));
+        chatMessage.setAccountId(currentAccountId);
+        messageService.sendUserMessage(chatMessage , roleList , personDto);
 
-        msgDto.setChatText(message.getContent());
-        msgDto.setName("软件工程师罗小东");
+       ChatMessageDto msgDto = AgentUtils.getChatMessageDto(role, IdUtil.getSnowflakeNextId()) ;
+
+        msgDto.setChatText(chatMessage.getMessage());
+        msgDto.setName(CurrentAccountJwt.get().getName());
         msgDto.setRoleType("person");
         msgDto.setIcon("1808350003370057729");
 
-        return AjaxResult.success(msgDto);
-    }
-
-    private void runChatStream(IndustryRoleEntity role, PromptMessageDto message, SseEmitter emitter) throws InterruptedException {
-
-        IBaseExpertService expertService = getBaseExpertService(role);
-        List<PromptMessage> parseMessageList = expertService.promptMessages(message.getContent() , role) ;
-
-        MessageManager msgManager = new MessageManager(10);
-
-        for(PromptMessage m : parseMessageList) {
-            com.alibaba.dashscope.common.Message msg = com.alibaba.dashscope.common.Message.builder()
-                    .role(m.getRole())
-                    .content(m.getContent())
-                    .build();
-            msgManager.add(msg);
-        }
-
-        Semaphore semaphore = new Semaphore(0);
-        StringBuilder fullContent = new StringBuilder();
-        long bId = IdUtil.getSnowflakeNextId() ;
-
-        qianWenLLM.getGeneration(msgManager , new ResultCallback<>() {
-            @SneakyThrows
-            @Override
-            public void onEvent(GenerationResult message) {
-
-                String msg = message.getOutput().getChoices().get(0).getMessage().getContent();
-                String finishReason = message.getOutput().getChoices().get(0).getFinishReason() ;
-
-                log.info("Received message: {}", JsonUtils.toJson(message));
-
-                ChatMessageDto msgDto = AgentUtils.getChatMessageDto(role, bId) ;
-                msgDto.setLoading(false);
-                msgDto.setChatText(msg);
-                emitter.send(msgDto) ;
-
-                if (finishReason != null && finishReason.equals("stop")) {
-                    emitter.send("[DONE]");
-                    semaphore.release();
-                }
-            }
-
-            @Override
-            public void onError(Exception err) {
-                log.error("Exception occurred: {}", err.getMessage());
-                semaphore.release();
-            }
-
-            @Override
-            public void onComplete() {
-                log.info("Completed");
-                semaphore.release();
-                log.info("Full content: \n{}", fullContent);
-            }
-        }) ;
-
-        semaphore.acquire();
-    }
-
-    /**
-     * 根据行业角色获取专家服务
-     *
-     * @param role 行业角色实体对象，包含了链ID等信息，用于定位特定的专家服务实例
-     * @return 返回对应的专家服务接口实现对象如果无法获取到合适的实现对象，将返回默认角色的专家服务
-     */
-    private IBaseExpertService getBaseExpertService(IndustryRoleEntity role) {
-        IBaseExpertService expertService = null;
-
-        // 从Spring上下文中根据链ID获取Bean
-        try{
-            expertService = (IBaseExpertService) SpringContext.getBean(role.getChainId());
-        }catch (Exception e){
-            log.error("无法获取到指定的专家服务实例，使用默认角色", e);
-            expertService = (IBaseExpertService) SpringContext.getBean(AssistantConstants.PREFIX_ASSISTANT_DEFAULT); // 使用默认角色
-        }
-
-        return expertService;
+        return AjaxResult.success(msgDto) ;
     }
 
 }
