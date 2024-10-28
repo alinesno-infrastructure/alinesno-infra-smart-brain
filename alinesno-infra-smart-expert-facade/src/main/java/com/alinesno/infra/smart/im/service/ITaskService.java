4313891cf0d4e57e80f905c32cf55e36422322a1
陈伟(Victor) <victorAip@gmail.com>
2024-10-29 00:13:07 +0800

    feat:在 ITaskService 接口中新增处理工作流消息的方法。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/ITaskService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/ITaskService.java
@@ -1,8 +1,10 @@
 package com.alinesno.infra.smart.im.service;
 
+import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -16,6 +18,22 @@ public interface ITaskService {
     void addTask(@NotNull MessageTaskInfo taskInfo) ;
 
     /**
+     * 更新消息信息
+     * @param taskInfo
+     * @param genContent
+     * @throws IOException
+     */
+    void handleWorkflowMessage(MessageTaskInfo taskInfo, WorkflowExecutionDto genContent) ;
+
+    /**
+     * 更新消息信息
+     * @param taskInfo
+     * @param genContent
+     * @throws IOException
+     */
+    void handleWorkflowMessageWithoutMessage(MessageTaskInfo taskInfo, WorkflowExecutionDto genContent);
+
+    /**
      * 获取到完成的任务列表
      * @return
      */
@@ -27,5 +45,4 @@ public interface ITaskService {
      * @return
      */
     List<MessageTaskInfo> getTaskMessage(String channelId) ;
-
 }
