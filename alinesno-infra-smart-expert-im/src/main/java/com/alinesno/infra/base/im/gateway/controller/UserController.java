82820b9e17f0edfbae2849cd93586708c39fedcc
李娜(Allen) <allenAip@gmail.com>
2024-11-01 00:11:55 +0800

    feat:在 UserController 中添加 DataPermissionScope 注解

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/controller/UserController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.base.im.gateway.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
@@ -9,7 +10,6 @@ import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
 import com.alinesno.infra.smart.im.service.IChannelRoleService;
 import com.alinesno.infra.smart.im.service.IChannelService;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.builder.ToStringBuilder;
@@ -51,20 +51,23 @@ public class UserController extends BaseController<IndustryRoleEntity, IIndustry
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
         // return roleService.getAgentList(page.getPageNum() , page.getPageSize() , "");
 
-        Page<IndustryRoleEntity> svcPage = Page.of(page.getPageNum() , page.getPageSize()) ;
-        svcPage = roleService.page(svcPage) ;
+        return this.toPage(model, this.getFeign(), page);
 
-        TableDataInfo tableDataInfo = new TableDataInfo();
-        tableDataInfo.setRows(svcPage.getRecords());
-        tableDataInfo.setTotal(svcPage.getTotal());
-
-        return tableDataInfo;
+//        Page<IndustryRoleEntity> svcPage = Page.of(page.getPageNum() , page.getPageSize()) ;
+//        svcPage = roleService.page(svcPage) ;
+//
+//        TableDataInfo tableDataInfo = new TableDataInfo();
+//        tableDataInfo.setRows(svcPage.getRecords());
+//        tableDataInfo.setTotal(svcPage.getTotal());
+//
+//        return tableDataInfo;
     }
 
     /**
