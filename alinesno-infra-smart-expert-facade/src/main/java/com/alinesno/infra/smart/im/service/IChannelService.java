67c060ebf947b055159e0cf0829fa63ac248fbe1
陈伟(Victor) <victorAip@gmail.com>
2024-11-04 00:11:39 +0800

    fix: 修复 IChannelService 接口中多余的空行

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IChannelService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IChannelService.java
@@ -71,4 +71,5 @@ public interface IChannelService extends IBaseService<ChannelEntity> {
      * @param recommendChannels
      */
     void batchCreateChannel(List<ChannelEntity> recommendChannels);
+
 }
