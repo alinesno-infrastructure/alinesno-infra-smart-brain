ddfc7e28d4225dfcf5ff26fc5dbf5d72b78cbf41
陈伟(Victor) <victorAip@gmail.com>
2024-11-04 00:11:38 +0800

    feat:在 rightPanel.vue 中添加动态角色信息展示及设置方法

--- alinesno-infra-smart-im-ui/src/views/base/agent/rightPanel.vue
+++ alinesno-infra-smart-im-ui/src/views/base/agent/rightPanel.vue
@@ -9,8 +9,9 @@
 
     <!-- 中间内容区域 -->
     <div class="content-area">
-      <div class="intro">4 道 AI 测试题，Get 你在未来世界的隐藏身份！ 完成 4 道题目, 即可解锁奖品，iPhone 16 Pro Max、Apple Watch S9、AirPods
-        4、扣子周边等500份奖品你来拿！</div>
+      <div class="intro">
+        {{ roleInfo.responsibilities }}
+      </div>
       <p><strong>任务周期</strong></p>
       <p class="skill-tip">技能：生成文档、更新审批</p>
       <p class="skill-tip">生成时长:1分32秒32</p>
@@ -27,6 +28,20 @@
 <script setup>
 // 导入Vue3的ref
 import { ref } from 'vue';
+
+const roleInfo = ref({
+  responsibilities: '角色描述'
+});
+
+const setRoleInfo = (value) => {
+  console.log(value)
+  roleInfo.value = value ;
+}
+
+/** 暴露方法 */
+defineExpose({
+  setRoleInfo
+})
 </script>
 
 <style lang="scss" scoped>
