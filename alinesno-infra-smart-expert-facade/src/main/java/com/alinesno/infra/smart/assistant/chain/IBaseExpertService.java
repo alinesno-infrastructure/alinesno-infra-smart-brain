f101c699c7eeb3e7d75d4c11b2560fbd6527a06a
王磊(Levi) <leviAip@gmail.com>
2024-11-06 00:13:32 +0800

    refactor: 修改 IBaseExpertService 接口参数，将 WorkflowExecutionEntity 替换为 MessageEntity

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/chain/IBaseExpertService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/chain/IBaseExpertService.java
@@ -3,8 +3,8 @@ package com.alinesno.infra.smart.assistant.chain;
 import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.assistant.api.prompt.PromptMessage;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.entity.WorkflowExecutionEntity;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
 
 import java.util.List;
 
@@ -15,11 +15,11 @@ public interface IBaseExpertService {
      * 专家运行
      *
      * @param role                    角色信息
-     * @param workflowExecutionEntity 工作流执行实体
+     * @param message 工作流执行实体
      * @param taskInfo                消息任务信息
      * @return
      */
-    WorkflowExecutionDto runRoleAgent(IndustryRoleEntity role, WorkflowExecutionEntity workflowExecutionEntity, MessageTaskInfo taskInfo);
+    WorkflowExecutionDto runRoleAgent(IndustryRoleEntity role, MessageEntity message, MessageTaskInfo taskInfo);
 
     /**
      * 获取到PromptMessage信息列表
