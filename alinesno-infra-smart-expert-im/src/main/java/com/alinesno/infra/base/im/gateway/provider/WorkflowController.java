80c998f9cfd4defe58903dca64593bd8caeeecd0
王磊(Levi) <leviAip@gmail.com>
2024-11-06 00:13:32 +0800

    改进 `WorkflowController` 类，增加消息内容获取及更新功能，并引入相关服务
    这句commit信息概括了修改的核心内容，即在 `WorkflowController` 类中新增了根据消息ID获取消息内容以及更新消息内容的功能，并且引入了相关的服务类。

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/WorkflowController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/WorkflowController.java
@@ -1,10 +1,18 @@
 package com.alinesno.infra.base.im.gateway.provider;
 
-import com.alinesno.infra.base.im.service.IWorkflowService;
+import com.alinesno.infra.base.im.utils.AgentUtils;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
-import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.dto.ChatMessageDto;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.alinesno.infra.smart.im.dto.MessageUpdateDto;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
+import com.alinesno.infra.smart.im.enums.TaskStatusEnums;
+import com.alinesno.infra.smart.im.service.IMessageService;
+import com.alinesno.infra.smart.im.service.ISSEService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -25,31 +33,107 @@ import org.springframework.web.bind.annotation.*;
 public class WorkflowController extends SuperController {
 
     @Autowired
-    private IWorkflowService workflowService ;
+    private IMessageService messageService;
+
+    @Autowired
+    private IIndustryRoleService roleService ;
+
+    @Autowired
+    private ISSEService sseService;
 
     /**
      * 根据业务id获取到消息内容
      */
     @GetMapping("/detail")
-    public AjaxResult getWorkflowExecution(@RequestParam String businessId) {
+    public AjaxResult getWorkflowExecution(@RequestParam String messageId) {
 
-        WorkflowExecutionDto dto = workflowService.getWorkflowExecution(businessId) ;
+//        WorkflowExecutionDto dto = workflowService.getWorkflowExecution(businessId) ;
 
+        MessageEntity dto = messageService.getById(messageId) ;
         return AjaxResult.success(dto) ;
     }
 
     /**
      * 更新消息内容
      */
+    @SneakyThrows
     @PostMapping("/updateContent")
     public AjaxResult updateContent(@RequestBody MessageUpdateDto dto) {
 
         log.debug("updateContent:{}", dto);
 
-        // 更新消息内容
-        workflowService.updateContent(dto)  ;
+        MessageEntity message = messageService.getById(dto.getBusinessId());
+        IndustryRoleEntity role = roleService.getById(message.getRoleId());
+
+        // 添加到消息记录表中
+        MessageTaskInfo taskInfo = new MessageTaskInfo();
+
+        taskInfo.setRoleId(message.getRoleId());
+        taskInfo.setChannelId(message.getChannelId());
+        // taskInfo.setUsageTime(message.get);
+        taskInfo.setRoleDto(role);
+
+        log.info("任务处理完成: {}", taskInfo);
+
+        ChatMessageDto queMessage = AgentUtils.genTaskStatusMessageDto(taskInfo , null) ;
+        queMessage.setChatText(dto.getContent());
+
+        queMessage.setRoleId(taskInfo.getRoleId());
+        queMessage.setAccountId(taskInfo.getAccountId());
+        queMessage.setLoading(false);
+        queMessage.setStatus(TaskStatusEnums.COMPLETED.getValue());
+
+        // 保存到消息记录表中
+        Long newMessageId = messageService.saveChatMessage(queMessage , taskInfo.getChannelId());
+
+        queMessage.setBusinessId(newMessageId);
+        sseService.send(String.valueOf(taskInfo.getChannelId()), queMessage);
 
         return ok() ;
+
+//        WorkflowExecutionEntity workflowExecution = workflowExecutionService.getById(dto.getBusinessId());
+//        workflowExecution.setId(null); // 变成新的消息进行保存
+//
+//        if(Boolean.parseBoolean(dto.getCode())){
+//            workflowExecution.setGenContent(formatCodeBlack(dto.getContent()));
+//        }else{
+//            workflowExecution.setGenContent(dto.getContent().toString());
+//        }
+//
+//        workflowExecutionService.save(workflowExecution) ;
+//
+//        IndustryRoleEntity role = roleService.getById(workflowExecution.getRoleId());
+//
+//        // 添加到消息记录表中
+//        MessageTaskInfo taskInfo = new MessageTaskInfo();
+//
+//        taskInfo.setRoleId(workflowExecution.getRoleId());
+//        taskInfo.setChannelId(workflowExecution.getChannelId());
+//        taskInfo.setUsageTime(workflowExecution.getUsageTimeSeconds());
+//        taskInfo.setRoleDto(role);
+//
+//        log.info("任务处理完成: {}", taskInfo);
+//
+//        ChatMessageDto queMessage = AgentUtils.genTaskStatusMessageDto(taskInfo , workflowExecution.getUsageTimeSeconds()) ;
+//
+//        if(Boolean.parseBoolean(dto.getCode())){
+//            queMessage.setChatText(formatCodeBlack(dto.getContent()));
+//        }else{
+//            queMessage.setChatText(dto.getContent());
+//        }
+//
+//        queMessage.setBusinessId(workflowExecution.getId());
+//        queMessage.setRoleId(taskInfo.getRoleId());
+//        queMessage.setAccountId(taskInfo.getAccountId());
+//        queMessage.setLoading(false);
+//        queMessage.setStatus(TaskStatusEnums.COMPLETED.getValue());
+//
+//        sseService.send(String.valueOf(taskInfo.getChannelId()), queMessage);
+//
+//        // 保存到消息记录表中
+//        messageService.saveChatMessage(queMessage , taskInfo.getChannelId());
+
+//        return ok() ;
     }
 
 }
