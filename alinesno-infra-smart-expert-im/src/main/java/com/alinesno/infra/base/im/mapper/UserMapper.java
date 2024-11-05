989464dfebaab241ab7322b20b589ac52a2237b2
王磊(Levi) <leviAip@gmail.com>
2024-11-06 00:13:31 +0800

    更新:alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/UserMapper.java

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-//package com.alinesno.infra.base.im.mapper;
-//
-//import com.alinesno.infra.base.im.entity.UserEntity;
-//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-//
-//public interface UserMapper extends IBaseMapper<UserEntity> {
-//
-//}
