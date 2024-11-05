04085e25911807917cf3a7997a65b3f0eca04b20
王磊(Levi) <leviAip@gmail.com>
2024-11-06 00:13:31 +0800

    更新:alinesno-infra-smart-expert-queue/src/main/java/com/alinesno/infra/smart/assistant/queue/QueueConfiguration.java

--- alinesno-infra-smart-expert-queue/src/main/java/com/alinesno/infra/smart/assistant/queue/QueueConfiguration.java
+++
@@ -1,9 +0,0 @@
-//package com.alinesno.infra.smart.assistant.queue;
-//
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.context.annotation.Configuration;
-//
-//@MapperScan("com.alinesno.infra.smart.assistant.queue.mapper")
-//@Configuration
-//public class QueueConfiguration {
-//}
