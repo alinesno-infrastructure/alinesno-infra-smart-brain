be9d6ad9373a0ee429b74dc206f344cb3c374328
王磊(Levi) <leviAip@gmail.com>
2024-11-06 00:13:31 +0800

    更新:alinesno-infra-smart-expert-queue/src/main/java/com/alinesno/infra/smart/assistant/queue/mapper/MessageQueueConfigMapper.java

--- alinesno-infra-smart-expert-queue/src/main/java/com/alinesno/infra/smart/assistant/queue/mapper/MessageQueueConfigMapper.java
+++
@@ -1,9 +0,0 @@
-//package com.alinesno.infra.smart.assistant.queue.mapper;
-//
-//
-//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-//import com.alinesno.infra.smart.assistant.entity.MessageQueueConfigEntity;
-//
-//public interface MessageQueueConfigMapper extends IBaseMapper<MessageQueueConfigEntity> {
-//
-//}
