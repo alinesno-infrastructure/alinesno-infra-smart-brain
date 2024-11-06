8c25f2e0d8e8d8589b541b1e5dae938dbea1a591
周健(James) <jamesAip@gmail.com>
2024-11-07 00:13:13 +0800

    feat:新增 IScreenService 接口，实现基本的服务功能。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/IScreenService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.screen.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.screen.entity.ScreenEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IScreenService extends IBaseService<ScreenEntity> {
+
+}
