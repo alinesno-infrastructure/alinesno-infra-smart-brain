2657861bc12657449a8c93894cde747461c522b9
周健(James) <jamesAip@gmail.com>
2024-11-07 00:13:13 +0800

    feat: 添加 `AjaxResultEventStreamConverter` 类以支持事件流转换。

---
+++ alinesno-infra-smart-expert-boot/src/main/java/com/alinesno/infra/smart/assistant/config/AjaxResultEventStreamConverter.java
@@ -0,0 +1,36 @@
+package com.alinesno.infra.smart.assistant.config;
+
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+
+public class AjaxResultEventStreamConverter extends AbstractHttpMessageConverter<AjaxResult> {
+
+    public AjaxResultEventStreamConverter() {
+        super(MediaType.TEXT_EVENT_STREAM);
+    }
+
+    @Override
+    protected boolean supports(Class<?> clazz) {
+        return AjaxResult.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    protected AjaxResult readInternal(Class<? extends AjaxResult> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+
+    @Override
+    protected void writeInternal(AjaxResult ajaxResult, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+//        OutputStream out = outputMessage.getBody();
+//        String content = "data: " + ajaxResult.toString() + "\n\n";
+//        out.write(content.getBytes(StandardCharsets.UTF_8));
+//        out.flush();
+    }
+}
