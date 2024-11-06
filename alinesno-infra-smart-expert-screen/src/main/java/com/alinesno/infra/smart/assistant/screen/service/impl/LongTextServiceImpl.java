898f2bb40a7c0e4323df40972f0bd35972b46214
周健(James) <jamesAip@gmail.com>
2024-11-07 00:13:14 +0800

    feat:实现 LongTextServiceImpl 类，提供长文本服务的默认实现。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/impl/LongTextServiceImpl.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.screen.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.screen.entity.LongTextEntity;
+import com.alinesno.infra.smart.assistant.screen.mapper.LongTextMapper;
+import com.alinesno.infra.smart.assistant.screen.service.ILongTextService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class LongTextServiceImpl extends IBaseServiceImpl<LongTextEntity, LongTextMapper> implements ILongTextService {
+
+
+}
