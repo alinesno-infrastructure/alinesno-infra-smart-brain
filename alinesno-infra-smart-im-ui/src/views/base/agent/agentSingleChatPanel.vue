f509240bd5c3668cf1f0931a777463c1d451a1a6
张婷(Tina) <tinaAip@gmail.com>
2024-11-07 00:13:14 +0800

    优化 agentSingleChatPanel.vue 文件代码结构和可读性

--- alinesno-infra-smart-im-ui/src/views/base/agent/agentSingleChatPanel.vue
+++ alinesno-infra-smart-im-ui/src/views/base/agent/agentSingleChatPanel.vue
@@ -1,11 +1,7 @@
 <!-- 意见审批  -->
 <template>
-  <div class="acp-dashboard aip-chat-dashboard-panel" 
-        v-loading="loading" 
-        element-loading-text="Loading..."
-        :element-loading-spinner="svg"
-        element-loading-svg-view-box="-10, -10, 50, 50"
-        style="padding:0px !important;">
+  <div class="acp-dashboard aip-chat-dashboard-panel" v-loading="loading" element-loading-text="Loading..."
+    :element-loading-spinner="svg" element-loading-svg-view-box="-10, -10, 50, 50" style="padding:0px !important;">
     <div class="smart-container inner-smart-container">
       <el-row>
         <el-col :span="19" style="padding: 0 5%;">
@@ -13,99 +9,96 @@
             <div class="robot-chat-header">
               <div class="chat-header-title">
                 <!-- <img class="aip-chat-box-title-img" :src="imagePathByPath(roleInfo.roleAvatar)" /> -->
-                {{ roleInfo.roleName}} 
+                {{ roleInfo.roleName }}
               </div>
               <div class="chat-header-desc">
-                ({{ truncateString(roleInfo.responsibilities,60) }}) 
+                ({{ truncateString(roleInfo.responsibilities, 60) }})
               </div>
               <div class="chat-header-desc" style="float: right;margin-top: -10px;">
-                  <el-button type="primary" text bg size="large" @click="taskFlowDialogVisible = true" >
-                    <i class="fa-solid fa-truck-fast icon-btn"></i>
-                  </el-button>
+                <el-button type="primary" text bg size="large" @click="taskFlowDialogVisible = true">
+                  <i class="fa-solid fa-truck-fast icon-btn"></i>
+                </el-button>
               </div>
             </div>
 
             <div class="robot-chat-body inner-robot-chat-body" style="height: calc(100vh - 140px)">
-                <!-- 聊天窗口_start -->
-                <el-scrollbar class="scroll-panel" ref="scrollbarRef" loading always wrap-style="padding:10px">
+              <!-- 聊天窗口_start -->
+              <el-scrollbar class="scroll-panel" ref="scrollbarRef" loading always wrap-style="padding:10px">
 
-                    <div ref="innerRef">
+                <div ref="innerRef">
 
-                      <div class="robot-chat-ai-say-box" v-for="(item, index) in messageList" 
-                          @mouseover="showTools(item)"
-                          @mouseleave="hideTools(item)"
-                          :key="index">
+                  <div class="robot-chat-ai-say-box" v-for="(item, index) in messageList" @mouseover="showTools(item)"
+                    @mouseleave="hideTools(item)" :key="index">
 
-                          <div class="chat-ai-header" :class="item.roleType == 'person' ? 'say-right-window' : ''">
-                            <div class="header-images">
-                                <img :src="imagePath(item)" />
-                            </div>
-                            </div>
-
-                            <div class="chat-ai-say-body" :class="item.roleType == 'person' ? 'say-right-window' : ''" style="max-width:calc(100% - 135px)">
-                            <div class="say-message-info" v-if="item.roleType == 'person'"> 
-                                <span style="margin-left:10px" :class="item.showTools?'show-tools':'hide-tools'"> {{ item.dateTime }}</span> 
-                                {{ item.name }}
-                            </div>
-                            <div class="say-message-info" v-else> 
-                                {{ item.name }}
-
-                                <el-button v-if="item.loading" size="default" type="primary" loading text>任务处理中</el-button>
-
-                                <span style="margin-left:10px" :class="item.showTools?'show-tools':'hide-tools'"> {{ item.dateTime }} </span>
-                            </div>
-
-                            <div class="say-message-body markdown-body" v-html="readerHtml(item.chatText)"></div>
-
-                            <div class="chat-ai-say-tools" style="margin-top: 3px;;text-align: right;float:right" :class="item.showTools?'show-tools':'hide-tools'">
-                                <el-button type="danger" link icon="Promotion" size="small" @click="handleBusinessIdToMessageBox(item)">选择</el-button>
-                                <el-button type="primary" link icon="EditPen" size="small" @click="handleEditGenContent(item)">复制</el-button>
-                            </div> 
-                          </div>
+                    <div class="chat-ai-header" :class="item.roleType == 'person' ? 'say-right-window' : ''">
+                      <div class="header-images">
+                        <img :src="imagePath(item)" />
                       </div>
-
                     </div>
 
-                </el-scrollbar>
-                <!-- 聊天窗口_end -->
+                    <div class="chat-ai-say-body" :class="item.roleType == 'person' ? 'say-right-window' : ''"
+                      style="max-width:calc(100% - 135px)">
+                      <div class="say-message-info" v-if="item.roleType == 'person'">
+                        <span style="margin-left:10px" :class="item.showTools ? 'show-tools' : 'hide-tools'"> {{
+      item.dateTime }}</span>
+                        {{ item.name }}
+                      </div>
+                      <div class="say-message-info" v-else>
+                        {{ item.name }}
+
+                        <el-button v-if="item.loading" size="default" type="primary" loading text>任务处理中</el-button>
+
+                        <span style="margin-left:10px" :class="item.showTools ? 'show-tools' : 'hide-tools'"> {{
+      item.dateTime }} </span>
+                      </div>
+
+                      <div class="say-message-body markdown-body" v-html="readerHtml(item.chatText)"></div>
+
+                      <div class="chat-ai-say-tools" style="margin-top: 3px;;text-align: right;float:right"
+                        :class="item.showTools ? 'show-tools' : 'hide-tools'">
+                        <el-button type="danger" link icon="Promotion" size="small"
+                          @click="handleBusinessIdToMessageBox(item)">选择</el-button>
+                        <el-button type="primary" link icon="EditPen" size="small"
+                          @click="handleEditGenContent(item)">复制</el-button>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+              </el-scrollbar>
+              <!-- 聊天窗口_end -->
             </div>
 
             <div class="robot-chat-footer chat-container" style="float:left;width:100%">
 
               <el-row :gutter="20">
                 <el-col :span="16">
-                    <div class="message-input">
+                  <div class="message-input">
 
-                        <el-input
-                            class="input-chat-box"
-                            @keydown.ctrl.enter.prevent="keyDown"
-                            v-model="message"
-                            :options="mentionOptions"
-                            :prefix="['@']"
-                            placeholder="请输入你的问题."
-                            @select="handleSelect"
-                          >
-                          <template #label="{ item }">
-                            {{ item.label }}
-                          </template>
-                        </el-input> 
+                    <el-input class="input-chat-box" @keydown.ctrl.enter.prevent="keyDown" v-model="message"
+                      :options="mentionOptions" :prefix="['@']" placeholder="请输入你的问题." @select="handleSelect">
+                      <template #label="{ item }">
+                        {{ item.label }}
+                      </template>
+                    </el-input>
 
-                    </div>
+                  </div>
                 </el-col>
 
                 <el-col :span="8" style="text-align: right;">
 
-                    <el-tooltip class="box-item" effect="dark" content="确认发送指令给Agent，快捷键：Enter+Ctrl" placement="top" >
-                      <el-button type="danger" text bg size="large" @click="sendMessage('send')">
-                        <i class="fa-solid fa-paper-plane icon-btn"></i>
-                      </el-button>
-                    </el-tooltip>
+                  <el-tooltip class="box-item" effect="dark" content="确认发送指令给Agent，快捷键：Enter+Ctrl" placement="top">
+                    <el-button type="danger" text bg size="large" @click="sendMessage('send')">
+                      <i class="fa-solid fa-paper-plane icon-btn"></i>
+                    </el-button>
+                  </el-tooltip>
 
-                    <el-tooltip class="box-item" effect="dark" content="执行任务" placement="top" >
-                      <el-button type="warning" text bg size="large" @click="sendMessage('function')" >
-                        <i class="fa-solid fa-feather icon-btn"></i>
-                      </el-button>
-                    </el-tooltip>
+                  <el-tooltip class="box-item" effect="dark" content="执行任务" placement="top">
+                    <el-button type="warning" text bg size="large" @click="sendMessage('function')">
+                      <i class="fa-solid fa-feather icon-btn"></i>
+                    </el-button>
+                  </el-tooltip>
                 </el-col>
               </el-row>
 
@@ -114,9 +107,9 @@
         </el-col>
 
         <el-col :span="5">
-            <AgentSingleRightPanel ref="agentSingleRightPanelRef" />
+          <AgentSingleRightPanel ref="agentSingleRightPanelRef" />
         </el-col>
-       
+
       </el-row>
     </div>
 
@@ -125,15 +118,16 @@
 
 <script setup>
 
+import { ElLoading } from 'element-plus'
 import MarkdownIt from 'markdown-it';
 import mdKatex from '@traptitech/markdown-it-katex';
 import hljs from 'highlight.js';
 
 import AgentSingleRightPanel from './rightPanel.vue'
 
-import { getInfo, chatRole }from '@/api/base/im/roleChat'
+import { getInfo, chatRole } from '@/api/base/im/roleChat'
 import { getParam } from '@/utils/ruoyi'
-import { openSseConnect , handleCloseAllSse } from "@/api/base/im/chatsse";
+import { openSseConnect, handleCloseSse } from "@/api/base/im/chatsse";
 import { nextTick, onMounted } from "vue";
 
 const { proxy } = getCurrentInstance();
@@ -150,6 +144,8 @@ const businessId = ref(null);
 const innerRef = ref(null); // 滚动条的处理_starter
 const scrollbarRef = ref(null);
 const messageList = ref([]);
+
+const streamLoading = ref(null)
 
 const mdi = new MarkdownIt({
   html: true,
@@ -172,7 +168,7 @@ function initChatBoxScroll() {
     const element = innerRef.value;  // 获取滚动元素
     const scrollHeight = element.scrollHeight;
 
-    scrollbarRef.value.setScrollTop(scrollHeight) ; 
+    scrollbarRef.value.setScrollTop(scrollHeight);
   })
 
 }
@@ -182,11 +178,11 @@ function highlightBlock(str, lang) {
 }
 
 function keyDown(e) {
-  if(!message.value){
-    return ;
+  if (!message.value) {
+    return;
   }
   sendMessage('send');
-  message.value = '' ;
+  message.value = '';
 }
 
 /** 读取html文本 */
@@ -198,7 +194,7 @@ function readerHtml(chatText) {
 const pushResponseMessageList = (newMessage) => {
   console.log(`--->>> newMessage = ${JSON.stringify(newMessage)}`);
 
-  if(newMessage.llmStream === true){ // 是否为流式输出
+  if (newMessage.llmStream === true) { // 是否为流式输出
 
     // 查找是否有相同businessId的消息
     const existingIndex = messageList.value.findIndex(item => item.businessId === newMessage.businessId);
@@ -210,8 +206,8 @@ const pushResponseMessageList = (newMessage) => {
       // 否则，添加新消息
       messageList.value.push(newMessage);
     }
-  }else{
-      messageList.value.push(newMessage);
+  } else {
+    messageList.value.push(newMessage);
   }
 
   // 调用初始化滚动条的函数
@@ -219,34 +215,51 @@ const pushResponseMessageList = (newMessage) => {
 };
 
 /** 连接sse */
-function handleSseConnect(channelId){
+function handleSseConnect(channelId) {
   nextTick(() => {
-    if(channelId){
+    if (channelId) {
 
-        let sseSource = openSseConnect(channelId) ;
-        // 接收到数据
-        sseSource.onmessage = function (event) {
-            if (!event.data.includes('[DONE]')) {
-              const data = JSON.parse(event.data);
-              pushResponseMessageList(data);
-            }else{
-              console.log('消息接收结束.')  
-            }
+      let sseSource = openSseConnect(channelId);
+      // 接收到数据
+      sseSource.onmessage = function (event) {
+
+        if (!event.data.includes('[DONE]')) {
+          let resData = event.data;
+
+          // console.log('sseSource.onmessage = ' + resData);
+
+          if (resData != 'ping') {  // 非心跳消息
+            const data = JSON.parse(resData);
+            // console.log('--->>> channelId = ' + channelId + ' , data = ' + JSON.stringify(data));
+            // handlePushResponseMessageList(data);
+            pushResponseMessageList(data);
+          }
+        } else {
+          console.log('消息接收结束.')
+          if (streamLoading.value) {
+            streamLoading.value.close();
+          }
         }
 
-      // })
+        // if (!event.data.includes('[DONE]')) {
+        //   const data = JSON.parse(event.data);
+        //   pushResponseMessageList(data);
+        // } else {
+        //   console.log('消息接收结束.')
+        // }
+      }
     }
   })
 }
 
-function handleBusinessIdToMessageBox(item){
-  const businessIdMessage = ' #' + item.businessId + ' ' ;
+function handleBusinessIdToMessageBox(item) {
+  const businessIdMessage = ' #' + item.businessId + ' ';
   businessId.value = item.businessId;
-  message.value += businessIdMessage ;
+  message.value += businessIdMessage;
 }
 
 /** 获取角色信息 */
-function handleGetInfo(roleId){
+function handleGetInfo(roleId) {
   getInfo(roleId).then(res => {
     let role = res.role;
     let msg = res.message;
@@ -266,19 +279,25 @@ function handleGetInfo(roleId){
 /** 发送消息 */
 const sendMessage = (type) => {
 
-  if(!message.value){
+  if (!message.value) {
     proxy.$modal.msgError("请输入消息内容.");
-    return ;
+    return;
   }
+
+  streamLoading.value = ElLoading.service({
+    lock: true,
+    text: '任务执行中，请勿操作其它界面 ...',
+    background: 'rgba(255, 255, 255, 0.1)',
+  })
 
   let formData = {
     channelId: channelId.value,
     message: message.value,
-    businessIds: [ businessId.value ],
+    businessIds: [businessId.value],
     type: type
   }
-  
-  chatRole(formData , roleId.value).then(res => {
+
+  chatRole(formData, roleId.value).then(res => {
     proxy.$modal.msgSuccess("发送成功");
     pushResponseMessageList(res.data);
   })
@@ -288,9 +307,9 @@ const sendMessage = (type) => {
 
 /** 显示工具条 */
 function showTools(item) {
-    messageList.value.forEach((i) => {
-      i.showTools = i === item; // 只有当前项的 showTools 被设置为 true
-    });
+  messageList.value.forEach((i) => {
+    i.showTools = i === item; // 只有当前项的 showTools 被设置为 true
+  });
 }
 
 /** 隐藏工具条 */
@@ -308,20 +327,29 @@ onMounted(() => {
   handleGetInfo(roleId.value);
 })
 
+
+// 销毁信息
+onBeforeUnmount(() => {
+  handleCloseSse(channelId.value).then(res => {
+    console.log('关闭sse连接成功:' + channelId)
+  })
+});
+
 </script>
 
 <style lang="scss" scoped>
 .inner-smart-container {
-    max-width: 100%! important;
+  max-width: 100% ! important;
 
-    .robot-chat-windows{
-      border:0px !important;
-    }
+  .robot-chat-windows {
+    border: 0px !important;
+  }
 
-    .inner-robot-chat-body {
-        height: calc(100vh - 100px);
-    }
+  .inner-robot-chat-body {
+    height: calc(100vh - 100px);
+  }
 }
+
 .scroll-panel {
   padding-bottom: 10px;
   float: left;
@@ -330,11 +358,11 @@ onMounted(() => {
   overflow: hidden;
 }
 
-.show-tools{
+.show-tools {
   visibility: visible;
 }
 
-.hide-tools{
+.hide-tools {
   visibility: hidden;
 }
 
@@ -396,5 +424,4 @@ onMounted(() => {
   }
 
 }
-
 </style>
