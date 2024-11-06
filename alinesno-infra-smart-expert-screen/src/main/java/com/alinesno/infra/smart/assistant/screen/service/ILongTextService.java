a4f1bd707f6b484405441d381e8613730fb05179
周健(James) <jamesAip@gmail.com>
2024-11-07 00:13:14 +0800

    feat:新增 `ILongTextService` 接口，包含长文本实体的基本操作。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/ILongTextService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.screen.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.screen.entity.LongTextEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ILongTextService extends IBaseService<LongTextEntity> {
+
+}
