0cee498bf161139ba04440427a5df2e6fb46fdef
张婷(Tina) <tinaAip@gmail.com>
2024-11-07 00:13:14 +0800

    修复聊天界面显示问题并优化按钮样式

--- alinesno-infra-smart-im-ui/src/views/base/specialist/smartService.vue
+++ alinesno-infra-smart-im-ui/src/views/base/specialist/smartService.vue
@@ -8,30 +8,30 @@
             <div class="robot-chat-header">
               <div class="chat-header-title">
                 <img class="aip-chat-box-title-img" :src="imagePath(channelInfo)" />
-                {{ channelInfo.channelName }} 
+                {{ channelInfo.channelName }}
               </div>
               <div class="chat-header-desc">
-                ({{ truncateString(channelInfo.channelDesc,30) }}) 
+                ({{ truncateString(channelInfo.channelDesc, 30) }})
               </div>
               <div class="chat-header-desc" style="float: right;margin-top: -10px;display: flex;">
 
-                  <div v-if="channelInfo.knowledgeType" >
-                    <img v-if="channelInfo.knowledgeType.includes('docx')" src="http://data.linesno.com/dataset_icons/docx.webp" style="width: 30px;margin: 5px;" />
-                    <img v-if="channelInfo.knowledgeType.includes('pdf')" src="http://data.linesno.com/dataset_icons/pdf.webp"  style="width: 30px;margin: 5px;" />
-                  </div>
+                <div v-if="channelInfo.knowledgeType">
+                  <img v-if="channelInfo.knowledgeType.includes('docx')"
+                    src="http://data.linesno.com/dataset_icons/docx.webp" style="width: 30px;margin: 5px;" />
+                  <img v-if="channelInfo.knowledgeType.includes('pdf')"
+                    src="http://data.linesno.com/dataset_icons/pdf.webp" style="width: 30px;margin: 5px;" />
+                </div>
 
-                  <el-button type="primary" text bg size="large" @click="taskFlowDialogVisible = true" >
-                    <i class="fa-solid fa-truck-fast icon-btn"></i>
-                  </el-button>
+                <el-button type="primary" text bg size="large" @click="taskFlowDialogVisible = true">
+                  <i class="fa-solid fa-truck-fast icon-btn"></i>
+                </el-button>
               </div>
             </div>
 
             <div class="robot-chat-body" v-loading="loading">
               <!-- 聊天窗口_start -->
-              <ChatList ref="chatListRef" 
-                @sendMessageToChatBox="sendMessageToChatBox" 
-                @handleSelectPreBusinessId="handleSelectPreBusinessId" 
-                @handleEditorContent="handleEditorContent" />
+              <ChatList ref="chatListRef" @sendMessageToChatBox="sendMessageToChatBox"
+                @handleSelectPreBusinessId="handleSelectPreBusinessId" @handleEditorContent="handleEditorContent" />
               <!-- 聊天窗口_end -->
             </div>
 
@@ -39,43 +39,37 @@
 
               <el-row :gutter="20">
                 <el-col :span="17">
-                    <div class="message-input">
+                  <div class="message-input">
 
-                        <el-mention
-                            class="input-chat-box"
-                            v-model="message"
-                            :options="mentionOptions"
-                            :prefix="['@']"
-                            placeholder="输入@可指定角色, #可指定输出结果"
-                            @select="handleSelect"
-                          >
-                          <template #label="{ item }">
-                            {{ item.label }}
-                          </template>
-                        </el-mention> 
+                    <el-mention class="input-chat-box" v-model="message" :options="mentionOptions" :prefix="['@']"
+                      placeholder="输入@可指定角色, #可指定输出结果" @select="handleSelect">
+                      <template #label="{ item }">
+                        {{ item.label }}
+                      </template>
+                    </el-mention>
 
-                    </div>
+                  </div>
                 </el-col>
 
                 <el-col :span="7" style="text-align: right;">
 
-                    <el-tooltip class="box-item" effect="dark" content="确认发送指令给Agent，快捷键：Enter+Ctrl" placement="top" >
-                      <el-button type="danger" text bg size="large" @click="sendMessage('send')">
-                        <i class="fa-solid fa-paper-plane icon-btn"></i>
-                      </el-button>
-                    </el-tooltip>
+                  <el-tooltip class="box-item" effect="dark" content="确认发送指令给Agent，快捷键：Enter+Ctrl" placement="top">
+                    <el-button type="danger" text bg size="large" @click="sendMessage('send')">
+                      <i class="fa-solid fa-paper-plane icon-btn"></i>
+                    </el-button>
+                  </el-tooltip>
 
-                    <el-tooltip class="box-item" effect="dark" content="执行任务" placement="top" >
-                      <el-button type="warning" text bg size="large" @click="sendMessage('function')" >
-                        <i class="fa-solid fa-feather icon-btn"></i>
-                      </el-button>
-                    </el-tooltip>
+                  <el-tooltip class="box-item" effect="dark" content="执行任务" placement="top">
+                    <el-button type="warning" text bg size="large" @click="sendMessage('function')">
+                      <i class="fa-solid fa-feather icon-btn"></i>
+                    </el-button>
+                  </el-tooltip>
 
-                    <el-tooltip class="box-item" effect="dark" content="上传文档文件" placement="top" >
-                      <el-button type="primary" text bg size="large" @click="handleUploadFile" >
-                        <i class="fa-solid fa-file-word icon-btn"></i>
-                      </el-button>
-                    </el-tooltip>
+                  <el-tooltip class="box-item" effect="dark" content="上传文档文件" placement="top">
+                    <el-button type="primary" text bg size="large" @click="handleUploadFile">
+                      <i class="fa-solid fa-file-word icon-btn"></i>
+                    </el-button>
+                  </el-tooltip>
 
                 </el-col>
               </el-row>
@@ -84,16 +78,17 @@
           </div>
         </el-col>
 
-        <el-col :span="5"  style="background-color:#fafafa">
+        <el-col :span="5" style="background-color:#fafafa">
           <SmartServiceAgent ref="smartServiceAgentRef" @mentionUser="mentionUser" />
         </el-col>
-       
+
       </el-row>
     </div>
 
     <!-- 任务运行状态 -->
-    <el-dialog v-model="taskFlowDialogVisible" title="频道任务运行状态" width="60%" :before-close="handleClose" destroy-on-close append-to-body>
-      <br/>
+    <el-dialog v-model="taskFlowDialogVisible" title="频道任务运行状态" width="60%" :before-close="handleClose" destroy-on-close
+      append-to-body>
+      <br />
       <AgentTaskFlow />
       <template #footer>
         <span class="dialog-footer">
@@ -126,25 +121,25 @@ import AgentTaskFlow from './agentTaskFlow'
 import ChatMessageEditor from './chatMessageEditor'
 
 // --->>> 引入方法 -->>
-import { chatAssistantContent , updateAssistantContent , chatMessage, sendUserMessage}from '@/api/base/im/robot'
+import { chatAssistantContent, updateAssistantContent, chatMessage, sendUserMessage } from '@/api/base/im/robot'
 import { getChannel } from "@/api/base/im/channel";
 import { getParam } from '@/utils/ruoyi'
 import { formatMessage } from '@/utils/chat'
-import { openSseConnect , handleCloseAllSse } from "@/api/base/im/chatsse";
+import { openSseConnect, handleCloseSse } from "@/api/base/im/chatsse";
 
 const streamLoading = ref(null)
 
 // --->>> 定义变量 -->>
 const chatListRef = ref();
 const router = useRouter();
-const {proxy} = getCurrentInstance();
+const { proxy } = getCurrentInstance();
 
 const loading = ref(false)
-const businessId  = ref("") ;
-const editorLoading = ref(true) ;
+const businessId = ref("");
+const editorLoading = ref(true);
 const editDialogVisible = ref(false)
 const taskFlowDialogVisible = ref(false)
-const uploadChildComp = ref(null) 
+const uploadChildComp = ref(null)
 const selectedUsers = ref([]);
 const selectedBusinessId = ref([]);
 const channelUsers = ref([]);
@@ -156,16 +151,16 @@ const channelInfo = ref({})
 const mentionOptions = ref([]);
 
 const mentionUser = (itemArr) => {
-  if(itemArr.length == 0){
+  if (itemArr.length == 0) {
     channelUsers.value = [];
     mentionOptions.value = [{}];
-    return ;
+    return;
   }
-  channelUsers.value = itemArr ;
+  channelUsers.value = itemArr;
   mentionOptions.value = itemArr.map(item => ({
-      value: item.roleName,
-      label: item.roleName,
-      id: item.id
+    value: item.roleName,
+    label: item.roleName,
+    id: item.id
   }));
 };
 
@@ -176,41 +171,45 @@ const handleSelect = (value, prefix) => {
 };
 
 const handleClose = () => {
-  editDialogVisible.value = false ;
-  taskFlowDialogVisible.value = false ;
+  editDialogVisible.value = false;
+  taskFlowDialogVisible.value = false;
 }
 
 /** 发送消息 */
 const sendMessage = (type) => {
 
-  if(!message.value){
+  if (!message.value) {
     proxy.$modal.msgError("请输入消息内容.");
-    return ;
+    return;
   }
 
-  streamLoading.value = ElLoading.service({lock: true,text: '任务执行中，请勿操作其它界面 ...',background: 'rgba(255, 255, 255, 0.1)',})
+  streamLoading.value = ElLoading.service({
+    lock: true,
+    text: '任务执行中，请勿操作其它界面 ...',
+    background: 'rgba(255, 255, 255, 0.1)',
+  })
 
   const formattedMessage = formatMessage(message.value, channelUsers.value);
 
   chatListRef.value.pushMessageList(formattedMessage);
-  handleSendUserMessage(message.value, type) ;
+  handleSendUserMessage(message.value, type);
 
   message.value = '';
   selectedUsers.value = [];
-  selectedBusinessId.value = [] ;
+  selectedBusinessId.value = [];
 };
 
 /** 同步消息到后端 */
-function handleSendUserMessage(message , type){
+function handleSendUserMessage(message, type) {
 
   let channelId = getParam("channel");
 
   // 只返回id列表
   let users = selectedUsers.value.map(item => {
-    return item.id ;
-  }) 
+    return item.id;
+  })
 
-  sendUserMessage(message , users, selectedBusinessId.value , channelId , type).then(response => {
+  sendUserMessage(message, users, selectedBusinessId.value, channelId, type).then(response => {
     console.log("发送消息", response.data);
     response.data.forEach(item => {
       chatListRef.value.pushResponseMessageList(item);
@@ -219,7 +218,7 @@ function handleSendUserMessage(message , type){
 }
 
 /** 发送消息组服务组件 */
-function handlePushResponseMessageList(item){
+function handlePushResponseMessageList(item) {
 
   chatListRef.value.pushResponseMessageList(item);
 
@@ -228,27 +227,27 @@ function handlePushResponseMessageList(item){
 }
 
 /** 上传文档文件 */
-function handleUploadFile(){
+function handleUploadFile() {
   uploadChildComp.value.handleOpenUpload(true);
 }
 
 // 发送消息到发送窗口
 const sendMessageToChatBox = (msg) => {
-  message.value += msg ;
+  message.value += msg;
 }
 
 /** 选择业务id */
-const handleSelectPreBusinessId= (bId) => {
-    selectedBusinessId.value.push(bId);
+const handleSelectPreBusinessId = (bId) => {
+  selectedBusinessId.value.push(bId);
 }
 
 /** 获取到会话信息 */
 function handleChatMessage(channelId) {
-  if(channelId){
-    loading.value = true ;
+  if (channelId) {
+    loading.value = true;
     chatMessage(channelId).then(response => {
-      const data = response.data ;
-      chatListRef.value.currentResponseMessageList(data); 
+      const data = response.data;
+      chatListRef.value.currentResponseMessageList(data);
 
       // 处理sse消息内容
       handleSseConnect(channelId);
@@ -259,94 +258,102 @@ function handleChatMessage(channelId) {
 }
 
 /** 编辑生成内容 */
-function handleEditorContent(bId){
-  editDialogVisible.value = true ; 
-  businessId.value = bId ;
+function handleEditorContent(bId) {
+  editDialogVisible.value = true;
+  businessId.value = bId;
 
   chatAssistantContent(bId).then(response => {
-    editorLoading.value = false ;
+    editorLoading.value = false;
   })
 }
 
 
 /** 查询当前频道 */
-function handleGetChannel(channelId){
-    if(channelId){
-      getChannel(channelId).then(response => {
-        channelInfo.value = response.data ;
+function handleGetChannel(channelId) {
+  if (channelId) {
+    getChannel(channelId).then(response => {
+      channelInfo.value = response.data;
 
-        nextTick(() => {
-          smartServiceAgentRef.value.setChannelInfo(channelInfo.value)
-        })
-
+      nextTick(() => {
+        smartServiceAgentRef.value.setChannelInfo(channelInfo.value)
       })
-    }
+
+    })
+  }
 }
 
 /** 连接sse */
-function handleSseConnect(channelId){
+function handleSseConnect(channelId) {
   nextTick(() => {
-    if(channelId){
-      handleCloseAllSse().then(res => {
+    if (channelId) {
+      let sseSource = openSseConnect(channelId);
+      // 接收到数据
+      sseSource.onmessage = function (event) {
+        if (!event.data.includes('[DONE]')) {
+          let resData = event.data;
 
-        let sseSource = openSseConnect(channelId) ;
-        // 接收到数据
-        sseSource.onmessage = function (event) {
-            if (!event.data.includes('[DONE]')) {
-              const data = JSON.parse(event.data);
+          // console.log('sseSource.onmessage = ' + resData);
 
-              console.log('--->>> channelId = ' + channelId + ' , data = ' + JSON.stringify(data));
-
-              handlePushResponseMessageList(data);
-            }else{
-              console.log('消息接收结束.')  
-              streamLoading.value.close() ;
-            }
+          if (resData != 'ping') {  // 非心跳消息
+            const data = JSON.parse(resData);
+            console.log('--->>> channelId = ' + channelId + ' , data = ' + JSON.stringify(data));
+            handlePushResponseMessageList(data);
+          }
+        } else {
+          console.log('消息接收结束.')
+          if (streamLoading.value) {
+            streamLoading.value.close();
+          }
         }
-
-      })
+      }
     }
   })
 }
 
 /** 快捷键监听事件 */
 function keyDown(e) {
-  if(!message.value){
+  if (!message.value) {
     console.log("空消息,不处理.")
-    return ;
+    return;
   }
 
   if (e.ctrlKey && e.keyCode == 13) { // 生成内容 
     sendMessage('send')
-    message.value = '' ;
-  }else if (e.altKey && e.keyCode == 13) {  // 进行文案修改
-      sendMessage('modify')
-      message.value = '' ;
-  }else if(e.shiftKey && e.keyCode == 13){  // 调用方法
-      sendMessage('function')
-      message.value = '' ;
+    message.value = '';
+  } else if (e.altKey && e.keyCode == 13) {  // 进行文案修改
+    sendMessage('modify')
+    message.value = '';
+  } else if (e.shiftKey && e.keyCode == 13) {  // 调用方法
+    sendMessage('function')
+    message.value = '';
   }
 
 }
 
 onMounted(() => {
-	//绑定监听事件
-	window.addEventListener('keydown', keyDown)
+  //绑定监听事件
+  window.addEventListener('keydown', keyDown)
 });
 
-onUnmounted(() => {
-	//销毁事件
-	window.removeEventListener('keydown', keyDown, false)
+onBeforeUnmount(() => {
+  //销毁事件
+  window.removeEventListener('keydown', keyDown, false)
+
+  // 关闭sse连接
+  let channelId = channelInfo.value.id;
+  handleCloseSse(channelId).then(res => {
+    console.log('关闭sse连接成功:' + channelId)
+  })
 });
 
 /** 监听路由变化 */
-watch(() =>  router.currentRoute.value.path,(toPath) => {
-    const channelId = getParam("channel");
+watch(() => router.currentRoute.value.path, (toPath) => {
+  const channelId = getParam("channel");
 
-    handleGetChannel(channelId);
-    handleChatMessage(channelId) ;
+  handleGetChannel(channelId);
+  handleChatMessage(channelId);
 
-    },{immediate: true,deep: true}
+}, { immediate: true, deep: true }
 )
-  
+
 </script>
