7b2affca36c4b7ecd276eb1238568fc44a9fb471
李娜(Allen) <allenAip@gmail.com>
2024-11-08 00:11:52 +0800

    feat:在 `ScreenServiceImpl` 中新增 `saveScreen` 方法，用于保存屏幕信息并创建知识库。

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/impl/ScreenServiceImpl.java
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/impl/ScreenServiceImpl.java
@@ -1,11 +1,14 @@
 package com.alinesno.infra.smart.assistant.screen.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.adapter.CloudStorageConsumer;
+import com.alinesno.infra.common.facade.response.R;
+import com.alinesno.infra.smart.assistant.adapter.BaseSearchConsumer;
+import com.alinesno.infra.smart.assistant.screen.dto.ScreenDto;
 import com.alinesno.infra.smart.assistant.screen.entity.ScreenEntity;
 import com.alinesno.infra.smart.assistant.screen.mapper.ScreenMapper;
 import com.alinesno.infra.smart.assistant.screen.service.IScreenService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,22 @@ import org.springframework.stereotype.Service;
 public class ScreenServiceImpl extends IBaseServiceImpl<ScreenEntity, ScreenMapper> implements IScreenService {
 
     @Autowired
-    private CloudStorageConsumer storageConsumer ;
+    private BaseSearchConsumer baseSearchConsumer; ;
 
+    @Override
+    public ScreenEntity saveScreen(ScreenDto screenDto) {
+
+        ScreenEntity screenEntity = new ScreenEntity();
+        BeanUtils.copyProperties(screenDto, screenEntity);
+
+        // 创建频道知识库
+        R<String> createDataset = baseSearchConsumer.datasetCreate(
+                screenEntity.getScreenDesc() ,
+                screenEntity.getScreenName()) ;
+
+        screenEntity.setKnowledgeId(createDataset.getData());
+        save(screenEntity);
+
+        return screenEntity ;
+    }
 }
