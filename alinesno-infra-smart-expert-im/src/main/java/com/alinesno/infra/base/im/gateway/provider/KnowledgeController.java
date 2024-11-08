dd6629b65b62d743487e4407809d223cfbfbf757
李娜(Allen) <allenAip@gmail.com>
2024-11-09 00:11:35 +0800

    修复: KnowledgeController中的文件导入逻辑并优化日志记录

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/KnowledgeController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/KnowledgeController.java
@@ -101,56 +101,5 @@ public class KnowledgeController {
 
 
 
-    /**
-     * 导入数据
-     *
-     * @param file 导入文件
-     */
-    @PostMapping(value = "/screenData", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public AjaxResult screenData(@RequestPart("file") MultipartFile file, long screenId) throws Exception {
 
-        // localPath文件夹路径是否存在，不存在则创建
-        FileUtils.forceMkdir(new File(localPath)) ;
-
-        File tmpFile = new File(localPath , Objects.requireNonNull(file.getOriginalFilename())) ;
-        file.transferTo(tmpFile);
-
-        String fileType = FileTypeUtil.getType(tmpFile) ;
-
-        // 获取原始文件名
-        String fileName = file.getOriginalFilename();
-        ChatMessageDto personDto = AgentUtils.getUploadChatMessageDto(fileName , fileType) ;
-
-//        // 上传到知识库角色
-//        ChannelEntity channelEntity = channelService.getById(channelId) ;
-        String datasetId = null ; //  channelEntity.getKnowledgeId() ;
-
-        R<String> result = searchController.datasetUpload(tmpFile.getAbsolutePath() , datasetId , progress -> {
-            log.debug("total bytes: " + progress.getTotalBytes());   // 文件大小
-            log.debug("current bytes: " + progress.getCurrentBytes());   // 已上传字节数
-            log.debug("progress: " + Math.round(progress.getRate() * 100) + "%");  // 已上传百分比
-
-            if (progress.isDone()) {   // 是否上传完成
-                log.debug("--------   Upload Completed!   --------");
-
-                // 拼接到知识库类型，先判断之前是否有同一类型的，如果包含则不往上拼接
-//                String knowledgeType = channelEntity.getKnowledgeType() ;
-//                if(knowledgeType == null || !knowledgeType.contains(fileType)){
-//                    knowledgeType = (knowledgeType==null?"":knowledgeType+ "|") + fileType ;
-//                    channelEntity.setKnowledgeType(knowledgeType);
-//                }
-//                channelService.updateById(channelEntity) ;
-//
-//                // 保存消息实体
-//                messageService.saveChatMessage(personDto , channelId) ;
-
-            }
-
-        }) ;
-        log.debug("upload file result = {}" , result);
-
-        FileUtils.forceDelete(tmpFile);
-
-        return AjaxResult.success(personDto) ;
-    }
 }
