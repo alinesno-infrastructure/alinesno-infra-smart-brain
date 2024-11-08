c1818e33c1005d4bedfbce09b5fdc3f25eeb6c24
张婷(Tina) <tinaAip@gmail.com>
2024-11-09 00:11:35 +0800

    feat: 添加场景列表查询及文件上传OSS功能，并更新章节相关接口实现。

--- alinesno-infra-smart-im-ui/src/api/base/im/screen.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/screen.js
@@ -9,11 +9,50 @@ var managerUrl = {
   createScreen: prefix + "saveOrUpdate" , 
   updateChapterEditor: prefix + "updateChapterEditor" , 
   getScreen: prefix +"getScreen",
+  screenList: prefix +"screenList",
+  uploadOss: prefix +"uploadOss",
 
   saveChapter: chapterPrefix +"saveChapters",
   chatRole: chapterPrefix +"chatRole",
+  chatRoleSync: chapterPrefix +"chatRoleSync",
   updateChapterContentEditor: chapterPrefix +"updateChapterContentEditor",
   getChapterByRole: chapterPrefix +"getChapterByRole",
+  getChapterContent: chapterPrefix +"getChapterContent",
+  updateChapterContent: chapterPrefix +"updateChapterContent",
+}
+
+// 上传文件到OSS
+export function uploadOss(screenId) {
+  return request({
+    url: managerUrl.uploadOss + '?screenId=' + parseStrEmpty(screenId),
+    method: 'get',
+  })
+}
+
+// 场景列表
+export function screenList(query) {
+  return request({
+    url: managerUrl.screenList,
+    method: 'get',
+    params: query
+  })
+}
+
+// 更新章节内容
+export function updateChapterContent(data) {
+  return request({
+    url: managerUrl.updateChapterContent, 
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询章节内容
+export function getChapterContent(id) {
+  return request({
+    url: chapterPrefix + 'getChapterContent?chapterId=' + parseStrEmpty(id),
+    method: 'get'
+  })
 }
 
 // 查询章节详细
@@ -28,6 +67,14 @@ export function getChapterByRole(roleId , screenId) {
 export function updateChapterContentEditor(data) {
   return request({
     url: managerUrl.updateChapterContentEditor,
+    method: 'post',
+    data: data
+  })
+}
+
+export function chatRoleSync(data) {
+  return request({
+    url: managerUrl.chatRoleSync , 
     method: 'post',
     data: data
   })
