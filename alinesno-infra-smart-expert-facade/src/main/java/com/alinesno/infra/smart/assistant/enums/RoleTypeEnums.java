34910eb0956eea81b0766edf1b68e8cc93855e85
李伟(Michael) <michaelAip@gmail.com>
2024-11-12 00:12:18 +0800

    feat: 添加角色类型枚举类及相应方法实现

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/RoleTypeEnums.java
@@ -0,0 +1,32 @@
+package com.alinesno.infra.smart.assistant.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 角色类型枚举
+ */
+@AllArgsConstructor
+@Getter
+public enum RoleTypeEnums {
+
+    SINGLE_ROLE("single_role", "单角色", "自己单独完成一个聊天，可流式输出或者同步输出"),
+    COLLABORATIVE_ROLE("collaborative_role", "协作角色", "与其它角色协作才可以完成一个工作，可流式输出或者同步输出"),
+    SCENARIO_ROLE("scenario_role", "场景角色", "与其它角色协作才可以完成一个工作，只能同步输出");
+
+    private final String key;
+    private final String name;
+    private final String description;
+
+    /**
+     * 通过key获取到角色信息
+     */
+    public static RoleTypeEnums getRoleTypeByKey(String key) {
+        for (RoleTypeEnums roleType : RoleTypeEnums.values()) {
+            if (roleType.getKey().equals(key)) {
+                return roleType;
+            }
+        }
+        return null;
+    }
+}
