625711ebd54eeaaabd61edeb9ccf704fd35fc829
李伟(Michael) <michaelAip@gmail.com>
2024-11-12 00:12:18 +0800

    feat:新增 RoleExecuteServiceImpl 类实现角色执行服务逻辑。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/impl/RoleExecuteServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.screen.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.screen.entity.RoleExecuteEntity;
+import com.alinesno.infra.smart.assistant.screen.mapper.RoleExecuteMapper;
+import com.alinesno.infra.smart.assistant.screen.service.IRoleExecuteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RoleExecuteServiceImpl extends IBaseServiceImpl<RoleExecuteEntity, RoleExecuteMapper> implements IRoleExecuteService {
+
+}
