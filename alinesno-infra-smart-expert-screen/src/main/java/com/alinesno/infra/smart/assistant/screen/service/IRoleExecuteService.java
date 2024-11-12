42102e92439a32e946529295df21859e38cec375
王磊(Levi) <leviAip@gmail.com>
2024-11-13 00:13:08 +0800

    feat:在 `IRoleExecuteService` 接口中添加任务管理相关方法。

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/IRoleExecuteService.java
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/IRoleExecuteService.java
@@ -1,7 +1,10 @@
 package com.alinesno.infra.smart.assistant.screen.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.screen.dto.RoleTaskDto;
 import com.alinesno.infra.smart.assistant.screen.entity.RoleExecuteEntity;
+
+import java.util.List;
 
 /**
  *
@@ -11,4 +14,31 @@ import com.alinesno.infra.smart.assistant.screen.entity.RoleExecuteEntity;
  */
 public interface IRoleExecuteService extends IBaseService<RoleExecuteEntity> {
 
+    /**
+     * 更新任务状态
+     *
+     * @param screenId
+     * @param tasks
+     */
+    void saveNewTasks(long screenId, List<RoleTaskDto> tasks);
+
+    /**
+     * 是否当前频道的任务全部完成
+     * @param screenId
+     * @return
+     */
+    boolean isAllTaskCompleted(Long screenId);
+
+    /**
+     * 标识任务已经完成
+     * @param id
+     */
+    void finishTask(Long id);
+
+    /**
+     * 获取前一步任务的内容
+     * @param preRoleId
+     * @return
+     */
+    String getPreContent(String preRoleId , long screenId);
 }
