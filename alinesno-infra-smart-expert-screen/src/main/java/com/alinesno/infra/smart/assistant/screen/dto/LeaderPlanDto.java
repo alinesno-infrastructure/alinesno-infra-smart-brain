d6b2e7da3114488464fc73d9f1ae0781e7731911
王磊(Levi) <leviAip@gmail.com>
2024-11-13 00:13:08 +0800

    feat:新增 LeaderPlanDto 类，用于场景管理和执行目标定义。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/dto/LeaderPlanDto.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.smart.assistant.screen.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LeaderPlanDto {
+
+    @Min(value = 1, message = "screenId不能为空")
+    private Long screenId ; // 场景ID
+
+    @NotBlank(message = "执行目标不能为空")
+    private String message ; // 执行目标
+
+}
