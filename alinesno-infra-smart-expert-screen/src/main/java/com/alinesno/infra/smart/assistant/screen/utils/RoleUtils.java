08780ce962f1cdda75570c6b8f0d5b613dc936b8
王磊(Levi) <leviAip@gmail.com>
2024-11-13 00:13:08 +0800

    feat: 添加 RoleUtils 工具类，包含获取编辑人员和角色实体的方法。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/utils/RoleUtils.java
@@ -0,0 +1,60 @@
+package com.alinesno.infra.smart.assistant.screen.utils;
+
+import com.alinesno.infra.smart.assistant.api.IndustryRoleDto;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RoleUtils {
+
+    // 查询编辑人员的公共方法
+     public static List<IndustryRoleDto> getEditors(IIndustryRoleService roleService , String editorString) {
+        if (editorString == null) {
+            return Collections.emptyList();
+        }
+        try {
+            String[] split = editorString.split(",");
+            List<Long> ids = Arrays.stream(split).map(s -> {
+                try {
+                    return Long.parseLong(s);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException("无效的编辑人员ID: " + s, e);
+                }
+            }).toList();
+            List<IndustryRoleEntity> roles = roleService.listByIds(ids);
+
+            return roles.stream().map(role -> {
+                IndustryRoleDto dto1 = new IndustryRoleDto();
+                BeanUtils.copyProperties(role, dto1);
+                return dto1;
+            }).toList();
+        } catch (RuntimeException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    public static List<IndustryRoleEntity> getRoleEntity(IIndustryRoleService roleService , String editorString) {
+        if (editorString == null) {
+            return Collections.emptyList();
+        }
+        try {
+            String[] split = editorString.split(",");
+            List<Long> ids = Arrays.stream(split).map(s -> {
+                try {
+                    return Long.parseLong(s);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException("无效的编辑人员ID: " + s, e);
+                }
+            }).toList();
+
+            return roleService.listByIds(ids);
+        } catch (RuntimeException e) {
+            return Collections.emptyList();
+        }
+    }
+
+}
