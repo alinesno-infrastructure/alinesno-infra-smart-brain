c1d4a8a019a6f4c7a44dab68fd12430a7494dd1b
王磊(Levi) <leviAip@gmail.com>
2024-11-13 00:13:08 +0800

    feat: 添加 ChapterEntity 类的基本属性及其注解配置

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/entity/ChapterEntity.java
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/entity/ChapterEntity.java
@@ -19,34 +19,44 @@ import java.util.List;
 public class ChapterEntity extends InfraBaseEntity {
 
     // 章节的名称、层级、排序等、编写要求、附加要求、父节点ID、是否为叶子节点
+    @TableField
     @Column(name = "chapter_name", type = MySqlTypeConstant.VARCHAR, length = 128, comment = "章节的名称")
     private String chapterName;
 
+    @TableField
     @Column(name = "chapter_level", type = MySqlTypeConstant.INT, comment = "章节的层级")
     private Integer chapterLevel;
 
+    @TableField
     @Column(name = "chapter_sort", type = MySqlTypeConstant.INT, comment = "章节的排序")
     private Integer chapterSort;
 
+    @TableField
     @Column(name = "chapter_require", type = MySqlTypeConstant.VARCHAR, length = 256, comment = "编写要求")
     private String chapterRequire;
 
+    @TableField
     @Column(name = "chapter_additional_require", type = MySqlTypeConstant.VARCHAR, length = 256, comment = "附加要求")
     private String chapterAdditionalRequire;
 
+    @TableField
     @Column(name = "parent_chapter_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "父章节ID")
     private Long parentChapterId;
 
+    @TableField
     @Column(name = "is_leaf", type = MySqlTypeConstant.TINYINT, length = 1, comment = "是否为叶子节点")
     private Boolean isLeaf;
 
+    @TableField
     @Column(name = "screen_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "关联的场景ID")
     private Long screenId ; // 关联的场景ID
 
     // 编写人员
+    @TableField
     @Column(name = "chapter_editor", type = MySqlTypeConstant.BIGINT, length = 32, comment = "章节编辑人员")
     private Long chapterEditor;
 
+    @TableField
     @Column(name = "content", type = MySqlTypeConstant.LONGTEXT, comment = "本章节文本内容")
     private String content ;
 
