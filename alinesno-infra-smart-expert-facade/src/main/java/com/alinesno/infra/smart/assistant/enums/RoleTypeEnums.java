a00df18105e2fe8b906b88e0b6366bca585c02cb
王磊(Levi) <leviAip@gmail.com>
2024-11-13 00:13:09 +0800

    feat:添加 `COMBINED_ROLE` 角色类型，扩展角色枚举功能。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/RoleTypeEnums.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/RoleTypeEnums.java
@@ -12,7 +12,8 @@ public enum RoleTypeEnums {
 
     SINGLE_ROLE("single_role", "单角色", "自己单独完成一个聊天，可流式输出或者同步输出"),
     COLLABORATIVE_ROLE("collaborative_role", "协作角色", "与其它角色协作才可以完成一个工作，可流式输出或者同步输出"),
-    SCENARIO_ROLE("scenario_role", "场景角色", "与其它角色协作才可以完成一个工作，只能同步输出");
+    SCENARIO_ROLE("scenario_role", "场景角色", "与其它角色协作才可以完成一个工作，只能同步输出"),
+    COMBINED_ROLE("combined_role", "组合角色", "结合了协作与场景角色的特点，既可以在协作中发挥作用，也可以适应特定场景，支持流式输出和同步输出");;
 
     private final String key;
     private final String name;
