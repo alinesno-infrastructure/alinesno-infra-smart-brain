e9990b90baedac13acc4919b262e400352b71882
李娜(Allen) <allenAip@gmail.com>
2024-11-14 00:11:20 +0800

    feat: 添加 `ToolExecuteListener` 类，用于监听工具执行事件并处理任务状态。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/agent/listener/ToolExecuteListener.java
@@ -0,0 +1,46 @@
+package com.alinesno.infra.smart.assistant.screen.agent.listener;
+
+import com.alinesno.infra.smart.assistant.screen.agent.event.ToolExecuteEvent;
+import com.alinesno.infra.smart.assistant.screen.dto.RoleTaskDto;
+import com.alinesno.infra.smart.assistant.screen.service.IRoleExecuteService;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.service.ISSEService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 监听工具执行过程的组件
+ * 该类的主要作用是作为工具执行过程中的事件监听器，可以对工具执行前后的状态进行处理或记录
+ */
+@Slf4j
+@Component
+public class ToolExecuteListener {
+
+    private final ApplicationEventPublisher publisher;
+
+    @Autowired
+    private IRoleExecuteService roleExecuteService ;
+
+    @Autowired
+    private IIndustryRoleService roleService;
+
+    @Autowired
+    private ISSEService sseService ;
+
+    public ToolExecuteListener(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @SneakyThrows
+    @EventListener
+    public void execute(ToolExecuteEvent event) {
+
+        RoleTaskDto task = event.getTask() ;
+        log.debug("Worker received task: " + task);
+
+    }
+}
