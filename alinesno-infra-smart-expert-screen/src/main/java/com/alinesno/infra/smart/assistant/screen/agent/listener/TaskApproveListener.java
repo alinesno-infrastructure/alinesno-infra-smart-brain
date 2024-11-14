857731ec5bb4f9b84c38339b6fe90db7e1931e7e
张婷(Tina) <tinaAip@gmail.com>
2024-11-15 00:11:42 +0800

    feat:添加任务审批监听器并实现消息发送功能

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/agent/listener/TaskApproveListener.java
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/agent/listener/TaskApproveListener.java
@@ -1,16 +1,22 @@
 package com.alinesno.infra.smart.assistant.screen.agent.listener;
 
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.screen.agent.event.TaskApproveEvent;
 import com.alinesno.infra.smart.assistant.screen.dto.RoleTaskDto;
 import com.alinesno.infra.smart.assistant.screen.service.IRoleExecuteService;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.dto.ChatMessageDto;
 import com.alinesno.infra.smart.im.service.ISSEService;
+import com.alinesno.infra.smart.utils.AgentUtils;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * 监听工具执行过程的组件
@@ -40,7 +46,22 @@ public class TaskApproveListener {
     public void execute(TaskApproveEvent event) {
 
         RoleTaskDto task = event.getTask() ;
-        log.debug("Worker received task: " + task);
+        log.debug("Task approve: name = {} , params = {} "  , task.getRoleName(), task.getTaskDesc());
 
+        sendSSE(task , "我是审核人员:任务已经处理完成。");
+    }
+
+    private void sendSSE(RoleTaskDto task, String msg) throws IOException {
+
+        IndustryRoleEntity role = task.getLeaderRole() ;
+
+        role.setId(Long.parseLong("1851439911174524930"));  // 模拟数据
+        role.setRoleAvatar("1851439744899731458");
+        role.setRoleName("审核人员");
+
+        ChatMessageDto message = AgentUtils.getChatMessageDto(role , IdUtil.getSnowflakeNextId());
+        message.setChatText(msg);
+        message.setLoading(false);
+        sseService.send(String.valueOf(task.getScreenId()) , message);
     }
 }
