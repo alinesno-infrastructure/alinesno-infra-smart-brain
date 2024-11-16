50699e3f77142a85ff2162fb88a0f455ef525d31
张婷(Tina) <tinaAip@gmail.com>
2024-11-17 00:11:31 +0800

    feat:新增 ToolInfo 注解，用于描述工具相关信息。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/annotation/ToolInfo.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ToolInfo {
+    String name() default "";
+    String description();
+}
