4b43f54b329e7c048fc942e172f8501b5e948f93
张婷(Tina) <tinaAip@gmail.com>
2024-11-17 00:11:32 +0800

    feat:新增 PluginEntity 类属性，包括插件图标、类型、脚本及指标信息

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/PluginEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/PluginEntity.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,9 +16,16 @@ import javax.persistence.Entity;
 @Data
 @Entity
 @TableName("plugins")
+@TableComment("插件信息")
 public class PluginEntity extends InfraBaseEntity {
 
-    @ColumnComment("渠道名称")
+    // 插件图标
+    @ColumnComment("插件图标")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @TableField(value = "icon")
+    private String icon;
+
+    @ColumnComment("插件名称")
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
     @TableField(value = "name")
     private String name;
@@ -27,19 +35,45 @@ public class PluginEntity extends InfraBaseEntity {
     @TableField(value = "author")
     private String author;
 
-    @ColumnComment("jar名称")
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 256)
-    @TableField(value = "jar_name")
-    private String jarName;
-
-    @ColumnComment("jar本地路径")
-    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 256)
-    @TableField(value = "local_path")
-    private String localPath;
-
     @ColumnComment("描述信息")
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
     @TableField(value = "description")
     private String description;
+
+    @ColumnComment("插件类型")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
+    @TableField(value = "plugin_type")
+    private String pluginType;
+
+    // 插件脚本(groovy)
+    @ColumnComment("插件脚本")
+    @ColumnType(value = MySqlTypeConstant.LONGTEXT)
+    @TableField(value = "groovy_script")
+    private String groovyScript;
+
+    // ------>>>>>>>>>>>> 指标信息 ---->>>>>>>>>.
+    // 是否上架
+    @ColumnComment("是否上架")
+    @ColumnType(value = MySqlTypeConstant.TINYINT, length = 1)
+    @TableField(value = "is_online")
+    private boolean isOnline;
+
+    // 使用数量/成功次数/异常次数
+    @ColumnComment("使用数量")
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 11)
+    @TableField(value = "use_count")
+    private Long useCount;
+
+    @ColumnComment("异常数量")
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 11)
+    @TableField(value = "error_count")
+    private Long errorCount;
+
+    @ColumnComment("成功数量")
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 11)
+    @TableField(value = "success_count")
+    private Long successCount;
+
+
 
 }
