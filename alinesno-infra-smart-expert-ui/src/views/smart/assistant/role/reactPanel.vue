9b9307777456ad9ee9611b889521bd017538e50f
张婷(Tina) <tinaAip@gmail.com>
2024-11-17 00:11:31 +0800

    feat: 添加角色推理配置页面组件 `reactPanel.vue`，包含页面头部信息展示。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/reactPanel.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+     <el-page-header @back="goBack" :content="'['+ currentRole.roleName +']角色推理配置'"></el-page-header>
+  </div>
+</template>
+
+<script setup>
+
+const currentRole = ref({
+    roleName:''
+});
+const currentRoleId = ref(null)
+
+/** 获取角色信息 */
+function getRoleInfo() {
+    currentRoleId.value = router.currentRoute.value.query.roleId;
+}
+
+nextTick(() => {
+    getRoleInfo();
+})
+
+</script>
+
+<style scoped lang="scss">
+</style>
