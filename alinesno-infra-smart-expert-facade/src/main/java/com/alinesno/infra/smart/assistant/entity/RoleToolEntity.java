2b9226891f85998cda7929c1e1515f1bf4d0251c
李伟(Michael) <michaelAip@gmail.com>
2024-11-19 00:13:23 +0800

    feat:新增 `RoleToolEntity` 类，用于存储角色与工具的信息。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/RoleToolEntity.java
@@ -0,0 +1,32 @@
+package com.alinesno.infra.smart.assistant.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@TableName("role_tool")
+@TableComment("角色工具信息")
+public class RoleToolEntity extends InfraBaseEntity {
+
+    // 角色信息、插件信息
+    @ColumnComment("角色ID")
+    @Column(length = 50)
+    @TableField(value = "role_id")
+    private Long roleId;
+
+    @ColumnComment("插件ID")
+    @Column(length = 50)
+    @TableField(value = "plugin_id")
+    private Long toolId;
+
+}
