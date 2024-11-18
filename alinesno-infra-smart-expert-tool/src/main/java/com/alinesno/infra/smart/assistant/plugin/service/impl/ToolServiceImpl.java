529c66ebfc9f9ad37a56325eb8536fcfafc64657
李伟(Michael) <michaelAip@gmail.com>
2024-11-19 00:13:23 +0800

    feat:新增 ToolServiceImpl 类，实现 IToolService 接口。

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolServiceImpl.java
@@ -0,0 +1,21 @@
+package com.alinesno.infra.smart.assistant.plugin.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.ToolEntity;
+import com.alinesno.infra.smart.assistant.plugin.mapper.ToolMapper;
+import com.alinesno.infra.smart.assistant.service.IToolService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 应用构建Service业务层处理
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Slf4j
+@Service
+public class ToolServiceImpl extends IBaseServiceImpl<ToolEntity, ToolMapper> implements IToolService {
+
+
+}
