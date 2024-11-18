05ae1283052d004376d0a23e2aaf39e88b82894c
李伟(Michael) <michaelAip@gmail.com>
2024-11-19 00:13:23 +0800

    feat: 添加 IToolService 接口，定义应用构建服务的基础操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IToolService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ToolEntity;
+
+/**
+ * 应用构建Service接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IToolService extends IBaseService<ToolEntity> {
+
+}
