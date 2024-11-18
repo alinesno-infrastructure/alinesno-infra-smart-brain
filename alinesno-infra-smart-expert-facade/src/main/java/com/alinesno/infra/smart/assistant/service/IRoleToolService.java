6be506425cb2206acd19c17d837bc063b546a343
李伟(Michael) <michaelAip@gmail.com>
2024-11-19 00:13:23 +0800

    feat:定义 IRoleToolService 接口，包含查询和更新用户插件的方法。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IRoleToolService.java
@@ -0,0 +1,26 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.RoleToolEntity;
+
+import java.util.List;
+
+/**
+ * 应用构建Service接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IRoleToolService extends IBaseService<RoleToolEntity> {
+
+    /**
+     * 查询用户插件
+     */
+    List<RoleToolEntity> findTools(Long roleId);
+
+    /**
+     * 更新用户插件
+     */
+    boolean updateRoleTools(Long roleId, List<RoleToolEntity> tools);
+
+}
