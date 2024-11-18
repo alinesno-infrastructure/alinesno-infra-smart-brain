47a4b412741b3f37756d88ead8c9e5667ad60f3f
李伟(Michael) <michaelAip@gmail.com>
2024-11-19 00:13:23 +0800

    feat:新增 RoleToolMapper 接口，实现基本映射功能。

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/mapper/RoleToolMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.smart.assistant.plugin.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.entity.RoleToolEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 应用构建Mapper接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Mapper
+public interface RoleToolMapper extends IBaseMapper<RoleToolEntity> {
+
+}
