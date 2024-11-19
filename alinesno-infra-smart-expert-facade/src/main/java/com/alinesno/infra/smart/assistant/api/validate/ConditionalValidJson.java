5d32bed62308ce6f20523a665773458d984af94e
李伟(Michael) <michaelAip@gmail.com>
2024-11-20 00:13:05 +0800

    feat:新增条件验证注解 ConditionalValidJson，用于校验JSON对象的有效性。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/validate/ConditionalValidJson.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.api.validate;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = ConditionalValidJsonValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConditionalValidJson {
+    String message() default "无效的JSON对象";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
