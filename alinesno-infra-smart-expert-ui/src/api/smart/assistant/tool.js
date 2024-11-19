091b77226ab97e1e209e17bda1c8b0af1905bd5c
李伟(Michael) <michaelAip@gmail.com>
2024-11-20 00:13:05 +0800

    feat: 添加智能助手工具相关的API接口配置及功能函数。

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/tool.js
@@ -0,0 +1,125 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/tool/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'saveTool' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  downloadFile: prefix + "downloadFile",
+  validateToolScript: prefix + "validateToolScript",
+  updateToolScript: prefix + "updateToolScript",
+}
+
+// 验证脚本任务
+export function validateToolScript(data) {
+  return request({
+    url: managerUrl.validateToolScript ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 提交脚本任务
+export function updateToolScript(data) {
+  return request({
+    url: managerUrl.updateToolScript , 
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询应用列表
+export function listTool(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询应用详细
+export function getTool(id) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 新增应用
+export function addTool(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改应用
+export function updateTool(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除应用
+export function delTool(id) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
+
+// 应用密码重置
+export function resetToolPwd(id, password) {
+  const data = {
+    id,
+    password
+  }
+  return request({
+    url: '/api/infra/base/starter/application/resetPwd',
+    method: 'put',
+    data: data
+  })
+}
+
+// 应用状态修改
+export function changeToolStatus(id, status) {
+  const data = {
+    id,
+    status
+  }
+  return request({
+    url: '/api/infra/base/starter/application/changeStatus',
+    method: 'put',
+    data: data
+  })
+}
+
+// 查询部门下拉树结构
+export function deptTreeSelect() {
+  return request({
+    url: '/api/infra/base/starter/application/deptTree',
+    method: 'get'
+  })
+}
