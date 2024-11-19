0b82b6c3d6f5282f1128e10b8e8b92ce4104f7af
李伟(Michael) <michaelAip@gmail.com>
2024-11-20 00:13:05 +0800

    feat:在 `IToolService` 接口中添加验证工具脚本和按角色获取工具的方法。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IToolService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IToolService.java
@@ -1,7 +1,11 @@
 package com.alinesno.infra.smart.assistant.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.api.ToolDto;
+import com.alinesno.infra.smart.assistant.api.ToolRequestDto;
 import com.alinesno.infra.smart.assistant.entity.ToolEntity;
+
+import java.util.List;
 
 /**
  * 应用构建Service接口
@@ -11,4 +15,17 @@ import com.alinesno.infra.smart.assistant.entity.ToolEntity;
  */
 public interface IToolService extends IBaseService<ToolEntity> {
 
+    /**
+     * 验证工具脚本
+     * @param dto
+     * @return
+     */
+    String validateToolScript(ToolRequestDto dto);
+
+    /**
+     * 根据角色获取工具
+     * @param roleId
+     * @return
+     */
+    List<ToolDto> getByRole(long roleId);
 }
