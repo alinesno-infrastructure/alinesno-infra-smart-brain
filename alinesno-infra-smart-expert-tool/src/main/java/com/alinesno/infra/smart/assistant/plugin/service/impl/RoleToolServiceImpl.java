8a19e942b12db2ad46629ce14f8adeb4f2c9655e
李伟(Michael) <michaelAip@gmail.com>
2024-11-20 00:13:05 +0800

    改进 `RoleToolServiceImpl` 实现角色工具的查询与更新功能

--- alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/RoleToolServiceImpl.java
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/RoleToolServiceImpl.java
@@ -4,6 +4,8 @@ import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.entity.RoleToolEntity;
 import com.alinesno.infra.smart.assistant.plugin.mapper.RoleToolMapper;
 import com.alinesno.infra.smart.assistant.service.IRoleToolService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +23,21 @@ public class RoleToolServiceImpl extends IBaseServiceImpl<RoleToolEntity, RoleTo
 
     @Override
     public List<RoleToolEntity> findTools(Long roleId) {
-        return null;
+
+        LambdaQueryWrapper<RoleToolEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(RoleToolEntity::getRoleId, roleId);
+
+        return this.list(queryWrapper);
     }
 
     @Override
     public boolean updateRoleTools(Long roleId, List<RoleToolEntity> tools) {
-        return false;
+
+        // 删除所有的用户工具
+        this.remove(new LambdaUpdateWrapper<RoleToolEntity>().eq(RoleToolEntity::getRoleId, roleId));
+
+        // 重新添加用户工具
+        return this.saveBatch(tools);
     }
 
 }
