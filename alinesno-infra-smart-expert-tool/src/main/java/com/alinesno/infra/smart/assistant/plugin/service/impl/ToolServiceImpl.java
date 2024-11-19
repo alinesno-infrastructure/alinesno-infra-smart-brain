b28d3a3d90694e4c4d4fe08e2ebf106a796ce7f5
李伟(Michael) <michaelAip@gmail.com>
2024-11-20 00:13:05 +0800

    feat:新增工具服务实现类中的脚本验证及角色关联方法

--- alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolServiceImpl.java
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolServiceImpl.java
@@ -1,11 +1,24 @@
 package com.alinesno.infra.smart.assistant.plugin.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.api.ToolDto;
+import com.alinesno.infra.smart.assistant.api.ToolRequestDto;
+import com.alinesno.infra.smart.assistant.entity.RoleToolEntity;
 import com.alinesno.infra.smart.assistant.entity.ToolEntity;
 import com.alinesno.infra.smart.assistant.plugin.mapper.ToolMapper;
+import com.alinesno.infra.smart.assistant.plugin.tool.ToolExecutor;
+import com.alinesno.infra.smart.assistant.service.IRoleToolService;
 import com.alinesno.infra.smart.assistant.service.IToolService;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 应用构建Service业务层处理
@@ -17,5 +30,54 @@ import org.springframework.stereotype.Service;
 @Service
 public class ToolServiceImpl extends IBaseServiceImpl<ToolEntity, ToolMapper> implements IToolService {
 
+    @Autowired
+    private IRoleToolService roleToolService ;
 
+    @Override
+    public String validateToolScript(ToolRequestDto dto) {
+
+        String groovyScript = dto.getScript() ;
+        ToolEntity toolEntity = getById(dto.getToolId()) ;
+        toolEntity.setUseCount(toolEntity.getUseCount() == null ? 1 : toolEntity.getUseCount()+1) ;
+
+        try{
+            String paramStr =  dto.getParams() ;
+
+            Gson gson = new Gson();
+            Type type = new TypeToken<Map<String, Object>>(){}.getType();
+            Map<String, Object> paramMap = gson.fromJson(paramStr, type);
+
+            Object output = ToolExecutor.executeGroovyScript(groovyScript, paramMap);
+            log.debug("工具执行结果：{}", output);
+
+            // 更新工具使用次数
+            toolEntity.setSuccessCount(toolEntity.getSuccessCount() == null ? 1 : toolEntity.getSuccessCount()+1);
+            update(toolEntity);
+
+            return String.valueOf(output) ;
+        }catch (Exception e){
+
+            toolEntity.setErrorCount(toolEntity.getErrorCount() == null ? 1 : toolEntity.getErrorCount()+1);
+            update(toolEntity);
+
+            log.error("工具执行异常：{}", e.getMessage(), e);
+            return "角色脚本执行失败:" + e.getMessage() ;
+        }
+
+
+    }
+
+    @Override
+    public List<ToolDto> getByRole(long roleId) {
+
+        List<RoleToolEntity> roleTools = roleToolService.findTools(roleId) ;
+        List<ToolEntity> tools = listByIds(roleTools.stream().map(RoleToolEntity::getToolId).toList()) ;
+
+        return tools.stream().map(tool -> {
+            ToolDto dto = new ToolDto();
+            BeanUtils.copyProperties(tool, dto);
+            return dto;
+        }).toList();
+
+    }
 }
