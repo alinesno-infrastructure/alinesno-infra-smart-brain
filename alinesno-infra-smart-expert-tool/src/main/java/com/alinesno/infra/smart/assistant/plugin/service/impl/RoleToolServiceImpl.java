7c5b0474b5570196c1b1c231c574851f71cc720e
李伟(Michael) <michaelAip@gmail.com>
2024-11-21 00:13:31 +0800

    feat:在 `RoleToolServiceImpl` 中添加获取和更新工具的方法，并引入 `ToolMapper`

--- alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/RoleToolServiceImpl.java
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/RoleToolServiceImpl.java
@@ -2,11 +2,14 @@ package com.alinesno.infra.smart.assistant.plugin.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.entity.RoleToolEntity;
+import com.alinesno.infra.smart.assistant.entity.ToolEntity;
 import com.alinesno.infra.smart.assistant.plugin.mapper.RoleToolMapper;
+import com.alinesno.infra.smart.assistant.plugin.mapper.ToolMapper;
 import com.alinesno.infra.smart.assistant.service.IRoleToolService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,23 +24,32 @@ import java.util.List;
 @Service
 public class RoleToolServiceImpl extends IBaseServiceImpl<RoleToolEntity, RoleToolMapper> implements IRoleToolService {
 
+    @Autowired
+    private ToolMapper toolMapper ;
+
     @Override
-    public List<RoleToolEntity> findTools(Long roleId) {
+    public List<ToolEntity> findTools(Long roleId) {
 
         LambdaQueryWrapper<RoleToolEntity> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(RoleToolEntity::getRoleId, roleId);
 
-        return this.list(queryWrapper);
+        List<RoleToolEntity> roleTools = this.list(queryWrapper);
+        if(roleTools == null || roleTools.isEmpty()){
+            return null ;
+        }
+
+        return toolMapper.selectBatchIds(roleTools.stream().map(RoleToolEntity::getToolId).toList()) ;
     }
 
     @Override
-    public boolean updateRoleTools(Long roleId, List<RoleToolEntity> tools) {
+    public boolean updateRoleTools(Long roleId, List<Long> tools) {
 
         // 删除所有的用户工具
         this.remove(new LambdaUpdateWrapper<RoleToolEntity>().eq(RoleToolEntity::getRoleId, roleId));
 
         // 重新添加用户工具
-        return this.saveBatch(tools);
+        List<RoleToolEntity> roleTools = tools.stream().map(toolId -> new RoleToolEntity(roleId, toolId)).toList();
+        return this.saveBatch(roleTools);
     }
 
 }
