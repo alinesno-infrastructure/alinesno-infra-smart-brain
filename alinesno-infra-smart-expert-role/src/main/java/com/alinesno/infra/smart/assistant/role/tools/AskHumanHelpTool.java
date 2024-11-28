00ff47d64045767217911337996bb44c849458ba
李娜(Allen) <allenAip@gmail.com>
2024-11-29 00:11:16 +0800

    feat:新增 AskHumanHelpTool 类，用于请求人类帮助处理问题。

---
+++ alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/tools/AskHumanHelpTool.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.smart.assistant.role.tools;
+
+import com.alinesno.infra.smart.assistant.annotation.ParamInfo;
+import com.alinesno.infra.smart.assistant.annotation.ToolInfo;
+import com.alinesno.infra.smart.assistant.plugin.tool.Tool;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 最终结果实现
+ */
+@Slf4j
+@ToolInfo(description = "如果需要人类帮助，或者对事情不清楚的存在疑问的，可以使用这个工具，请使用它。")
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@Data
+public class AskHumanHelpTool extends Tool {
+
+    @ParamInfo(description = "需要问人类的问题")
+    private String question ;
+
+    public AskHumanHelpTool(String question) {
+        this.question = question;
+    }
+
+    @Override
+    public String execute() {
+        log.debug("askHumanHelpTool : {}", question);
+        return "";
+    }
+}
