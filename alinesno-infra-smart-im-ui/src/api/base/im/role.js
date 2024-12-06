46ee2adf422a1b0492a2969b64fdaf85facfea49
李伟(Michael) <michaelAip@gmail.com>
2024-12-07 00:13:04 +0800

    feat: 添加角色管理相关API接口代码

---
+++ alinesno-infra-smart-im-ui/src/api/base/im/role.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/role/' ;
+var managerUrl = {
+  marketDatatables : prefix +"marketDatatables" ,
+  employRole: prefix +"employRole" ,
+}
+
+// 录用角色
+export function employRole(roleId) {
+  return request({
+    url: managerUrl.employRole + '?roleId=' + roleId ,
+    method: 'get',
+  })
+}
+
+// 查询应用列表
+export function listMarketRole(query) {
+  return request({
+    url: managerUrl.marketDatatables,
+    method: 'post',
+    params: query
+  })
+}
