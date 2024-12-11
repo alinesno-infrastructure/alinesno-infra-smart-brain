852805139cad64a8418faa8abf9b835532cf2e5e
周健(James) <jamesAip@gmail.com>
2024-12-12 00:12:08 +0800

    feat:添加 `MessageEntity` 引入并初始化 `IMessageService` 服务。

--- alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/event/StreamMessagePublisher.java
+++ alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/event/StreamMessagePublisher.java
@@ -2,10 +2,14 @@ package com.alinesno.infra.smart.assistant.role.event;
 
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
 import com.alinesno.infra.smart.im.event.StreamMessageEvent;
+import com.alinesno.infra.smart.im.service.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class StreamMessagePublisher {
@@ -15,7 +19,6 @@ public class StreamMessagePublisher {
 
     public void doStuffAndPublishAnEvent(final String message , IndustryRoleEntity role , MessageTaskInfo taskInfo, long bId) {
         System.out.println("LLM Stream Message Publishing custom event. ");
-
         StreamMessageEvent customEvent = new StreamMessageEvent(this, message , role , taskInfo , bId);
         applicationEventPublisher.publishEvent(customEvent);
     }
