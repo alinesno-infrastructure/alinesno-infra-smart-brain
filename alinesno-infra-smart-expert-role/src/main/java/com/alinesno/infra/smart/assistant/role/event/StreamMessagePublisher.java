a31b3f93850c9409c8569c4b58c53881cfca8aa7
黄涛(Leo) <leoAip@gmail.com>
2024-12-19 00:12:36 +0800

    refactor(StreamMessagePublisher): 移除未使用的导入和变量

--- alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/event/StreamMessagePublisher.java
+++ alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/event/StreamMessagePublisher.java
@@ -2,14 +2,10 @@ package com.alinesno.infra.smart.assistant.role.event;
 
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
-import com.alinesno.infra.smart.im.entity.MessageEntity;
 import com.alinesno.infra.smart.im.event.StreamMessageEvent;
-import com.alinesno.infra.smart.im.service.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 @Component
 public class StreamMessagePublisher {
