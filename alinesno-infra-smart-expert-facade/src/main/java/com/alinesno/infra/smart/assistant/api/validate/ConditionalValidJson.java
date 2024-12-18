a33d17340c4f906aeeeca40385f18c74b2431484
李娜(Allen) <allenAip@gmail.com>
2024-12-19 00:12:36 +0800

    fix:修复 ConditionalValidJson 类的导入问题，添加缺失的空行以提高代码可读性。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/validate/ConditionalValidJson.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/validate/ConditionalValidJson.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.smart.assistant.api.validate;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
