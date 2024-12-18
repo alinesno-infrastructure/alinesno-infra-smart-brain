380e8bdb2335b85a1283e0f67f2f7724ec19e239
李娜(Allen) <allenAip@gmail.com>
2024-12-19 00:12:36 +0800

    fix:修复 ValidJsonValidator 类中导入语句顺序问题

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/validate/ValidJsonValidator.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/validate/ValidJsonValidator.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.api.validate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
