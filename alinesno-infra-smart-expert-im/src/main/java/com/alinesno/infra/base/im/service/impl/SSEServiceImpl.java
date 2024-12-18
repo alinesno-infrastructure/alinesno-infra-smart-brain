54f830d04b7b3bb1f46da035e175404adcfb4eec
李娜(Allen) <allenAip@gmail.com>
2024-12-19 00:12:36 +0800

    fix:修复SSEServiceImpl导入包顺序问题

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/service/impl/SSEServiceImpl.java
@@ -1,10 +1,10 @@
 package com.alinesno.infra.base.im.service.impl;
 
 import cn.hutool.core.lang.Assert;
-import com.alinesno.infra.smart.utils.AgentUtils;
 import com.alinesno.infra.smart.im.dto.ChatMessageDto;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.alinesno.infra.smart.im.service.ISSEService;
+import com.alinesno.infra.smart.utils.AgentUtils;
 import jakarta.validation.constraints.NotBlank;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
