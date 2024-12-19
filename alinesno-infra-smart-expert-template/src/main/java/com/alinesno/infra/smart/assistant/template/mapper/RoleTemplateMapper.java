cf5fc0da5bd44ce51f71fcf2c7bdcedf69a2cd52
陈伟(Victor) <victorAip@gmail.com>
2024-12-20 00:12:20 +0800

    feat:新增 RoleTemplateMapper 接口，实现基础映射功能。

---
+++ alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/mapper/RoleTemplateMapper.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.template.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.template.entity.RoleTemplateEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 项目模块 Mapper 接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Mapper
+public interface RoleTemplateMapper extends IBaseMapper<RoleTemplateEntity> {
+}
