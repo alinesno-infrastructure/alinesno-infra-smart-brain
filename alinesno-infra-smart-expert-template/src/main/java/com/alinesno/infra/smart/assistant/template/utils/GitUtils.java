08b217d5345942d0d5c588ccf2837c5b3e379047
陈伟(Victor) <victorAip@gmail.com>
2024-12-20 00:12:20 +0800

    feat:添加 GitUtils 类，包含 Git 仓库克隆功能

---
+++ alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/utils/GitUtils.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.smart.assistant.template.utils;
+
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
+/**
+ * Gitlab工具类
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public class GitUtils {
+
+	/**
+	 * clone 工程
+	 * @param uri
+	 * @param path
+	 */
+	public static void clone(String uri , String path) {
+        try {
+            Git.cloneRepository()
+                .setURI(uri)
+                .setDirectory(new File(path))
+                .call();
+        } catch (InvalidRemoteException e) {
+            System.err.println("Invalid remote repository: " + e.getMessage());
+        } catch (TransportException e) {
+            System.err.println("Error occurred during transport: " + e.getMessage());
+        } catch (GitAPIException e) {
+            System.err.println("Git API error: " + e.getMessage());
+        }
+    }
+	
+}
