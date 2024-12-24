929a57108796e1a58031ce3e837fcc86c15a69e0
陈伟(Victor) <victorAip@gmail.com>
2024-12-25 00:10:12 +0800

    feat:新增 RolePushOrgMapper 接口，用于应用构建相关操作。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/RolePushOrgMapper.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.entity.ApiKeyEntity;
+import com.alinesno.infra.smart.assistant.entity.RolePushOrgEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 应用构建Mapper接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Mapper
+public interface RolePushOrgMapper extends IBaseMapper<RolePushOrgEntity> {
+
+}
