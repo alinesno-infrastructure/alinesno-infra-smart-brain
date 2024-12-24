866add6d6c5720c35aa3492c83b41bdee8423441
陈伟(Victor) <victorAip@gmail.com>
2024-12-25 00:10:12 +0800

    feat:新增 IRolePushOrgService 接口，定义应用构建服务的基本操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IRolePushOrgService.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ApiKeyEntity;
+import com.alinesno.infra.smart.assistant.entity.RolePushOrgEntity;
+
+/**
+ * 应用构建Service接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IRolePushOrgService extends IBaseService<RolePushOrgEntity> {
+
+
+}
