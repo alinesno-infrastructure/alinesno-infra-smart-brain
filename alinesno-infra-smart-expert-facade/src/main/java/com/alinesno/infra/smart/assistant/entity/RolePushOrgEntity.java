2867a63fb50a760ed5b9f5eb1dc426fb3c685cec
周健(James) <jamesAip@gmail.com>
2024-12-26 00:11:42 +0800

    feat:在 RolePushOrgEntity 类中新增版本号、是否过期及录用时间字段

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/RolePushOrgEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/RolePushOrgEntity.java
@@ -5,9 +5,12 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.Date;
 
 /**
  * 组织角色(用于推送角色信息，一个角色可以推送给多个组织)
@@ -15,6 +18,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @TableName("industry_role_push_org") // MyBatis-Plus 表名注解
+@TableComment("推送组织角色")
 public class RolePushOrgEntity extends InfraBaseEntity {
 
     @TableField("role_id")
@@ -38,5 +42,23 @@ public class RolePushOrgEntity extends InfraBaseEntity {
     @Column(type = MySqlTypeConstant.SMALLINT, length = 1)
     @ColumnComment("是否覆盖")
     private boolean isCover;
+
+    // 版本号
+    @TableField("version")
+    @Column(type = MySqlTypeConstant.INT, length = 11)
+    @ColumnComment("版本号")
+    private int version;
+
+    // 是否过期(1过期|0未过期)
+    @TableField("has_expired")
+    @Column(type = MySqlTypeConstant.SMALLINT, length = 1)
+    @ColumnComment("是否过期")
+    private int hasExpired;
+
+    // 录用时间
+    @TableField("accept_time")
+    @Column(type = MySqlTypeConstant.DATETIME)
+    @ColumnComment("录用时间")
+    private Date acceptTime;
 
 }
