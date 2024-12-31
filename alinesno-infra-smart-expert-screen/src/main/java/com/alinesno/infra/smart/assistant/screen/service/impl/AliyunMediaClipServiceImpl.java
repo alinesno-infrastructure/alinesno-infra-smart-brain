8a5d87e7a3e708f47f95cb351140bd7df98a439e
李伟(Michael) <michaelAip@gmail.com>
2025-01-01 00:10:46 +0800

    feat:新增阿里云视频剪辑服务实现类

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/service/impl/AliyunMediaClipServiceImpl.java
@@ -0,0 +1,163 @@
+package com.alinesno.infra.smart.assistant.screen.service.impl;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.smart.assistant.screen.dto.media.MediaBean;
+import com.alinesno.infra.smart.assistant.screen.dto.media.VideoBean;
+import com.alinesno.infra.smart.assistant.screen.properties.AliyunProperties;
+import com.alinesno.infra.smart.assistant.screen.service.IAliyunMediaClipService;
+import com.aliyun.ice20201109.Client;
+import com.aliyun.ice20201109.models.GetBatchMediaProducingJobRequest;
+import com.aliyun.ice20201109.models.GetBatchMediaProducingJobResponse;
+import com.aliyun.ice20201109.models.SubmitBatchMediaProducingJobRequest;
+import com.aliyun.ice20201109.models.SubmitBatchMediaProducingJobResponse;
+import com.aliyun.teaopenapi.models.Config;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * 阿里云视频剪辑服务实现
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Slf4j
+@Service
+public class AliyunMediaClipServiceImpl implements IAliyunMediaClipService {
+
+    private Client iceClient;
+
+    private final AliyunProperties aliyunProperties;
+
+    @SneakyThrows
+    public AliyunMediaClipServiceImpl(AliyunProperties aliyunProperties) {
+        this.aliyunProperties = aliyunProperties;
+        initClient(aliyunProperties);
+    }
+
+    @Override
+    public void generateGlobalKouBaoVideo(VideoBean bean) {
+        log.debug("开始生成视频:{}" , bean);
+    }
+
+    @SneakyThrows
+    @Override
+    public JSONArray generateGroupKouBaoVideo(VideoBean bean) {
+        log.debug("开始生成视频:{}" , bean);
+        return runAliyunMediaClip(bean);
+    }
+
+    public JSONArray runAliyunMediaClip(VideoBean videoBean) throws Exception {
+        JSONArray mediaGroupArray = new JSONArray();
+
+        List<MediaBean> mediaArray = videoBean.getMediaArray() ;
+        int index = 1 ;
+        for (MediaBean mediaBean : mediaArray) {
+            JSONObject mediaGroup = new JSONObject();
+
+            mediaGroup.put("GroupName", "middle_" + index);
+            mediaGroup.put("MediaArray", mediaBean.getMediaArray()) ; // 媒体地址
+            mediaGroup.put("SpeechTextArray" , mediaBean.getMediaSpeechTextArray()); // 口播文案
+            mediaGroup.put("Duration" , mediaBean.getDuration()) ;
+
+            mediaGroupArray.add(mediaGroup);
+            index ++ ;
+        }
+
+        List<String> titleArray = videoBean.getTitleArray() ; // 视频标题
+
+        JSONObject inputConfig = new JSONObject();
+        inputConfig.put("MediaGroupArray", mediaGroupArray);
+        inputConfig.put("TitleArray", titleArray);
+
+        if(videoBean.isGlobalVoiceover()){
+            inputConfig.put("SpeechTextArray", videoBean.getSpeechTextArray());  // 口播文案
+        }
+
+        if(CollectionUtils.isEmpty(videoBean.getBackgroundImageArray())){  // 背景图片
+            inputConfig.put("BackgroundImageArray", videoBean.getBackgroundImageArray());
+        }
+
+        if(CollectionUtils.isEmpty(videoBean.getBackgroundMusicArray())){  // 背景音乐
+            inputConfig.put("BackgroundMusicArray", videoBean.getBackgroundMusicArray());
+        }
+
+        int produceCount = videoBean.getProduceCount() ; // 生成的成片数
+
+        // 成片宽高，生成横屏文件
+        int outputWidth =  videoBean.getOutputWidth() ;
+        int outputHeight =  videoBean.getOutputHeight() ;
+
+        // 成片oss地址，需包含{index} 占位符
+        String mediaUrl = "http://" + aliyunProperties.getBucketName() + ".oss-" + aliyunProperties.getRegionId() + ".aliyuncs.com/script/output_{index}_"+System.currentTimeMillis()+"_w.mp4";
+
+        JSONObject outputConfig = new JSONObject();
+        outputConfig.put("MediaURL", mediaUrl);
+        outputConfig.put("Count", produceCount);
+        outputConfig.put("Width", outputWidth);
+        outputConfig.put("Height", outputHeight);
+
+        log.debug("请求视频生成数据:{}" , JSONUtil.toJsonPrettyStr(inputConfig.toJSONString()));
+
+        // 提交一键成片任务
+        SubmitBatchMediaProducingJobRequest request = new SubmitBatchMediaProducingJobRequest();
+        request.setInputConfig(inputConfig.toJSONString());
+        request.setOutputConfig(outputConfig.toJSONString());
+
+        SubmitBatchMediaProducingJobResponse response = iceClient.submitBatchMediaProducingJob(request);
+        String jobId = response.getBody().getJobId();
+        log.debug("Start script batch job, batchJobId: " + jobId);
+
+        JSONArray outputMedia = null ;
+
+        // 轮询任务状态直到全部结束
+        log.debug("Waiting job finished...");
+        int maxTry = 3000;
+        int i = 0;
+        while (i < maxTry) {
+            Thread.sleep(3000);
+            i++;
+            GetBatchMediaProducingJobRequest getRequest = new GetBatchMediaProducingJobRequest();
+            getRequest.setJobId(jobId);
+            GetBatchMediaProducingJobResponse getResponse = iceClient.getBatchMediaProducingJob(getRequest);
+            String status = getResponse.getBody().getEditingBatchJob().getStatus();
+            log.debug("BatchJobId: " + jobId + ", status:" + status);
+
+            if ("Failed".equals(status)) {
+                log.debug("Batch job failed. JobInfo: " + JSONObject.toJSONString(getResponse.getBody().getEditingBatchJob()));
+                throw new Exception("Produce failed. BatchJobId: " + jobId);
+            }
+
+            if ("Finished".equals(status)) {
+                log.debug("Batch job finished. JobInfo: " + JSONObject.toJSONString(getResponse.getBody().getEditingBatchJob().getSubJobList()));
+                outputMedia = JSONArray.parseArray(JSONObject.toJSONString(getResponse.getBody().getEditingBatchJob().getSubJobList())) ;
+                break;
+            }
+        }
+
+        return outputMedia ;
+    }
+
+    /**
+     * 初始化iceClient
+      * @throws Exception
+     */
+    public void initClient(AliyunProperties aliyunProperties) throws Exception {
+        com.aliyun.credentials.Client credentialClient = new com.aliyun.credentials.Client();
+
+        Config config = new Config();
+        config.setCredential(credentialClient);
+
+        config.accessKeyId = aliyunProperties.getAccessKeyId() ;
+        config.accessKeySecret = aliyunProperties.getAccessKeySecret() ;
+
+        config.endpoint = "ice." + aliyunProperties.getRegionId() + ".aliyuncs.com";
+        config.regionId =  aliyunProperties.getRegionId() ;
+
+        iceClient = new Client(config);
+    }
+}
