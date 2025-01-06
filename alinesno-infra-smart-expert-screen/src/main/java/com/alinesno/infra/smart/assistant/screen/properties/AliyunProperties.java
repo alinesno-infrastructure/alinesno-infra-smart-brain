13e95238cf32397ea3867e0c36178452a0f10485
陈伟(Victor) <victorAip@gmail.com>
2025-01-06 19:17:58 +0800

    feat:在 AliyunProperties 类中初始化 accessKeyId 和 accessKeySecret 字段

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/properties/AliyunProperties.java
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/properties/AliyunProperties.java
@@ -15,7 +15,7 @@ public class AliyunProperties {
     private String regionId = "cn-shanghai" ;
     private String endpoint = "http://oss-cn-shanghai.aliyuncs.com";  // Endpoint以杭州为例，其它Region请按实际情况填写。
     private String bucketName = "aip-media";  // 阿里云OSS的Bucket名称
-    private String accessKeyId;  // 阿里云OSS的AccessKeyId
-    private String accessKeySecret;  // 阿里云OSS的AccessKeySecret
+    private String accessKeyId = "";  // 阿里云OSS的AccessKeyId
+    private String accessKeySecret = "";  // 阿里云OSS的AccessKeySecret
 
 }
