2e33a443a752c01ec74c58d991db77dd21338461
陈伟(Victor) <victorAip@gmail.com>
2025-01-09 00:13:04 +0800

    feat:新增流程片段处理功能及更新内容接口

--- alinesno-infra-smart-im-ui/src/api/base/im/workflow.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/workflow.js
@@ -6,7 +6,8 @@ import { parseStrEmpty } from "@/utils/ruoyi";
 var prefix = '/v1/api/infra/base/im/workflow/' ;
 var managerUrl = {
   detailUrl: prefix +"detail",
-  updateContent: prefix + "updateContent"
+  updateContent: prefix + "updateContent",
+  processParagraph: prefix +"processParagraph",
 }
 
 // 查询应用详细
@@ -21,6 +22,21 @@ export function getMessage(messageId) {
 export function updateContent(data) {
   return request({
     url: managerUrl.updateContent,
+    method: 'post',
+    headers: {
+      isEncrypt: true 
+    },
+    data: data
+  })
+}
+
+// 内容片断修改
+export function processParagraph(data) {
+  return request({
+    url: managerUrl.processParagraph, 
+    headers: {
+      isEncrypt: true 
+    },
     method: 'post',
     data: data
   })
