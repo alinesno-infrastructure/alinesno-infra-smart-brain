4a4fc67f79320337804cdc7b3a49418dc1d00cc1
陈伟(Victor) <victorAip@gmail.com>
2025-01-09 00:13:04 +0800

    feat:在 WorkflowController 中新增处理文章段落的方法 processParagraph

--- alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/WorkflowController.java
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/WorkflowController.java
@@ -2,21 +2,30 @@ package com.alinesno.infra.base.im.gateway.provider;
 
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
+import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.screen.dto.ParagraphProcessRequestDTO;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
 import com.alinesno.infra.smart.im.dto.ChatMessageDto;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.alinesno.infra.smart.im.dto.MessageUpdateDto;
+import com.alinesno.infra.smart.im.dto.WorkflowProcessRequestDTO;
 import com.alinesno.infra.smart.im.entity.MessageEntity;
 import com.alinesno.infra.smart.im.enums.TaskStatusEnums;
 import com.alinesno.infra.smart.im.service.IMessageService;
 import com.alinesno.infra.smart.im.service.ISSEService;
 import com.alinesno.infra.smart.utils.AgentUtils;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * 应用构建Controller
@@ -135,5 +144,33 @@ public class WorkflowController extends SuperController {
 
 //        return ok() ;
     }
+
+    /**
+     * 对文章某段的处理，包括扩写、重写、润色。
+     */
+    @PostMapping("/processParagraph")
+    public AjaxResult processParagraph(@RequestBody @Validated WorkflowProcessRequestDTO dto) {
+        log.debug("WorkflowProcessRequestDTO = {}" , dto) ;
+
+        MessageTaskInfo taskInfo = new MessageTaskInfo() ;
+
+        taskInfo.setRoleId(dto.getRoleId());
+        taskInfo.setChannelId(dto.getChannelId());
+        taskInfo.setText(dto.getAction() +":"+dto.getRequirement()+ ":"+ dto.getModifyContent());
+        taskInfo.setModify(true);
+        taskInfo.setModifyPreBusinessId(false); ;
+
+        Map<String , Object> paramMap = new java.util.HashMap<>(Map.of("modifyContent", dto.getModifyContent()));
+        paramMap.put("action" , dto.getAction()) ;
+        paramMap.put("requirement" , dto.getRequirement()) ;
+
+        taskInfo.setParams(paramMap);
+
+        WorkflowExecutionDto genContent  = roleService.runRoleAgent(taskInfo) ;
+        log.debug("chatRole = {}" , genContent);
+
+        return AjaxResult.success("操作成功" , genContent.getGenContent()) ;
+    }
+
 
 }
