8e6f00e0d95b0a661c841cb7166455f9afccd0f5
陈伟(Victor) <victorAip@gmail.com>
2025-01-18 00:11:34 +0800

    refactor(TemplateEntity): 移除 TemplateEntity 类中的冗余注释

--- alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/entity/TemplateEntity.java
+++ alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/entity/TemplateEntity.java
@@ -8,12 +8,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- * <p>
- * 存储文件实体类
- * </p>
- * <p>
- * 该实体类用于存储文件的相关信息，包括DFS组名、下载次数、下载密码、过期日期、文件扩展名、文件标识、文件名称、云存储文件名称、文件长度、文件URL、文件源、是否公开、保存类型、阿里云链接、本地磁盘链接、本地磁盘地址、FastDFS链接、PaxosSURLRe链接、七牛链接、文件bucket、文件MD5、接口密钥、URLBFS和URLMongoDB等。
- * </p>
  *
  * @author luoxiaodong
  * @version 1.0.0
