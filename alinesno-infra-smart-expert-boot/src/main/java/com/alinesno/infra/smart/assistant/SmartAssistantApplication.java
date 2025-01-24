a052bb832a6878f39e17811fe9be0d944381eba0
张婷(Tina) <tinaAip@gmail.com>
2025-01-25 00:11:36 +0800

    feat:在 SmartAssistantApplication 类中引入示例工具集成

--- alinesno-infra-smart-expert-boot/src/main/java/com/alinesno/infra/smart/assistant/SmartAssistantApplication.java
+++ alinesno-infra-smart-expert-boot/src/main/java/com/alinesno/infra/smart/assistant/SmartAssistantApplication.java
@@ -3,7 +3,6 @@ package com.alinesno.infra.smart.assistant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 集成一个Java开发示例工具
