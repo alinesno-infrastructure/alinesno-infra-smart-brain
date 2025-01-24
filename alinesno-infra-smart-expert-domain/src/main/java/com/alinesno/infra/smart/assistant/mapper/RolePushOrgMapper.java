be84ebe3de3825f9a8a3f4e1592eca4b01de74d3
张婷(Tina) <tinaAip@gmail.com>
2025-01-25 00:11:35 +0800

    refactor: 移除 RolePushOrgMapper 中未使用的 ApiKeyEntity 引用

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/RolePushOrgMapper.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/RolePushOrgMapper.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.smart.assistant.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.entity.ApiKeyEntity;
 import com.alinesno.infra.smart.assistant.entity.RolePushOrgEntity;
 import org.apache.ibatis.annotations.Mapper;
 
