f2f185c6514af439be463fad598c1d321a7b065f
张婷(Tina) <tinaAip@gmail.com>
2025-02-13 00:13:22 +0800

    feat:新增 StartNode 类，实现工作流启动节点功能。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/start-node/index.js
@@ -0,0 +1,17 @@
+// 引入所需模块
+import StartNodeVue from './index.vue'
+import { AppNode, AppNodeModel } from '@/views/smart/assistant/workflow/common/appNode'
+
+// 定义 StartNode 类，继承自 AppNode
+class StartNode extends AppNode {
+    constructor(props) {
+        super(props, StartNodeVue);
+    }
+}
+
+// 导出默认对象
+export default {
+    type: 'start-node',
+    model: AppNodeModel,
+    view: StartNode
+};
