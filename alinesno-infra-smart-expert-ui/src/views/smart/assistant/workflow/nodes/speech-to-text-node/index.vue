0ff2bc233bca3ef665a0bb36c371cfdd2dfc384c
张婷(Tina) <tinaAip@gmail.com>
2025-02-13 00:13:22 +0800

    feat:新增语音转文字节点组件

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/speech-to-text-node/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="workflow-container">
+    语音转文字 
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .workflow-container {
+    height: 300px;
+    width: 300px;
+    background-color: red;
+  }
+</style>
