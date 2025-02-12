3079d26d39ecdbd41da2ca7184a66dd017b82356
张婷(Tina) <tinaAip@gmail.com>
2025-02-13 00:13:23 +0800

    feat:在 AbstractWorkflowNode 中添加节点属性和方法

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/AbstractWorkflowNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/AbstractWorkflowNode.java
@@ -3,6 +3,9 @@ package com.alinesno.infra.smart.assistant.workflow;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 这是一个抽象父类，继承自 WorkflowNode 接口。
  * 它作为所有具体工作流节点类的基类，包含了所有工作流节点可能通用的属性和方法。
@@ -11,5 +14,26 @@ import lombok.Data;
  */
 @Data
 public abstract class AbstractWorkflowNode implements WorkflowNode {
-    // 父类可以包含所有节点通用的属性和方法
+
+    // 节点唯一标识符，用于唯一标识一个节点
+    private String id;
+
+    // 节点名称，用于唯一标识一个节点
+    private String name;
+
+    // 节点的x坐标，用于定义节点在界面中的水平位置
+    private int x;
+
+    // 节点的y坐标，用于定义节点在界面中的垂直位置
+    private int y;
+
+    // 节点的高度，用于定义节点在界面中占据的垂直空间大小
+    private int height;
+
+    // 节点类型，用于区分不同功能或属性的节点
+    private String type ;
+
+    // 节点属性集合，用于存储节点的额外属性信息，如颜色、大小等
+    private Map<String, Object> properties = new HashMap<>();
+
 }
