4c78d61975b3cac27e15bc2b0f14638d37f91b36
张婷(Tina) <tinaAip@gmail.com>
2025-02-14 00:14:16 +0800

    feat:新增 LlmModelServiceImpl 类，实现应用构建 Service 业务层处理

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/LlmModelServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.smart.assistant.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.LlmModelEntity;
+import com.alinesno.infra.smart.assistant.mapper.LlmModelMapper;
+import com.alinesno.infra.smart.assistant.service.ILlmModelService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 应用构建Service业务层处理
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Slf4j
+@Service
+public class LlmModelServiceImpl extends IBaseServiceImpl<LlmModelEntity, LlmModelMapper> implements ILlmModelService {
+
+}
