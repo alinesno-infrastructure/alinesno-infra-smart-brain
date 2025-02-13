b82b0d1a2fc1366b2586c076a49a6ba92e412882
张婷(Tina) <tinaAip@gmail.com>
2025-02-14 00:14:16 +0800

    feat:新增 ILlmModelService 接口，定义应用构建服务的基本操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/ILlmModelService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.LlmModelEntity;
+
+/**
+ * 应用构建Service接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface ILlmModelService extends IBaseService<LlmModelEntity> {
+
+}
