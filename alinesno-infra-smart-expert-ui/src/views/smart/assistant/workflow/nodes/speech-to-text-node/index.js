5eea3eddaea910926beb2d1832282ffb2eed3e8c
张婷(Tina) <tinaAip@gmail.com>
2025-02-14 00:14:16 +0800

    feat:新增 SpeechToTextView 类以自定义语音转文字节点的视图

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/speech-to-text-node/index.js
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/speech-to-text-node/index.js
@@ -1,13 +1,16 @@
 import SpeechToTextVue from './index.vue'
 import { AppNode, AppNodeModel } from '@/views/smart/assistant/workflow/common/appNode'
 
-class SpeechToTextNode extends AppNode {
+// 自定义节点的 view
+class SpeechToTextView extends AppNode {
     constructor(props) {
         super(props, SpeechToTextVue);
     }
 }
+
+
 export default {
   type: 'speech_to_text',
   model: AppNodeModel,
-  view: SpeechToTextNode
+  view: SpeechToTextView
 }
