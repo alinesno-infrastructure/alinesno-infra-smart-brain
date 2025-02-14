b42e0b6df0f1f435fc0768e30fcc5a00daf690d3
李伟(Michael) <michaelAip@gmail.com>
2025-02-15 00:12:00 +0800

    feat:新增 LlmModelEntity 类属性，包括图标、模型名称、模型类型等字段

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/LlmModelEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/LlmModelEntity.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.smart.assistant.entity;
 
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
@@ -21,6 +22,7 @@ public class LlmModelEntity extends InfraBaseEntity {
 
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("图标")
+    @TableField("icon")
     private String icon;
 
     /**
@@ -28,13 +30,23 @@ public class LlmModelEntity extends InfraBaseEntity {
      */
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("大模型名称")
+    @TableField("model_name")
     private String modelName;
+
+    /**
+     * 模型类型
+     */
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
+    @ColumnComment("模型类型")
+    @TableField("model_type")
+    private String modelType;
 
     /**
      * 大模型的描述信息，该字段在数据库表中可以为空。
      */
     @ColumnType(value = MySqlTypeConstant.TEXT)
     @ColumnComment("大模型描述")
+    @TableField("description")
     private String description;
 
     /**
@@ -42,13 +54,15 @@ public class LlmModelEntity extends InfraBaseEntity {
      */
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("所属提供商名称")
-    private String providerId;
+    @TableField("provider_code")
+    private String providerCode;
 
     /**
      * 接入大模型所需的 API 密钥，该字段在数据库表中不能为空。
      */
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("API 密钥")
+    @TableField("api_key")
     private String apiKey;
 
     /**
@@ -56,6 +70,7 @@ public class LlmModelEntity extends InfraBaseEntity {
      */
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("API 地址")
+    @TableField("api_url")
     private String apiUrl;
 
     /**
@@ -63,6 +78,15 @@ public class LlmModelEntity extends InfraBaseEntity {
      */
     @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
     @ColumnComment("模型名称")
+    @TableField("model")
     private String model;
+
+    /**
+     * 模型权限
+     */
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
+    @ColumnComment("模型权限")
+    @TableField("model_permission")
+    private String modelPermission;
 
 }
