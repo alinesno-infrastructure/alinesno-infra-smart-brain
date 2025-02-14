d19d6c3b10cda0903dc09fa60f58c1dd916066b6
李伟(Michael) <michaelAip@gmail.com>
2025-02-15 00:12:00 +0800

    feat:在 NodeComponents.vue 中为 workflowNodes 添加 properties 属性，并更新图标和颜色配置

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/components/NodeComponents.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/components/NodeComponents.vue
@@ -10,7 +10,7 @@
             <div class="node-components-content" v-for="item in workflowNodes" :key="item.name"
                 @click="clickNode(item)">
                 <div class="node-components-icon">
-                    <i :class="item.icon" />
+                    <i :class="item.properties.icon" />
                 </div>
                 <div class="node-info">
                     <span>{{ item.label }}</span>
@@ -42,13 +42,13 @@ const searchNodeComponent = ref('')
 const workflowNodes = [
     {
         id: '1001',
-        icon: 'fa-solid fa-comments',
         name: "AI_CHAT",
         label: "AI 对话",
         description: "与 AI 大模型进行对话",
         type: "ai_chat",
-        color: "#409EFF",
         properties: {
+            icon: 'fa-solid fa-comments',
+            color: "#2962FF",
             stepName: 'AI 对话',
             showNode: true,
             x: 440,
@@ -59,13 +59,13 @@ const workflowNodes = [
     },
     {
         id: '1002',
-        icon: 'fa-solid fa-image',
         name: "IMAGE_UNDERSTAND",
         label: "图片理解",
         description: "识别出图片中的对象、场景等信息回答用户问题",
         type: "image_understand",
-        color: "#67C23A",
         properties: {
+            icon: 'fa-solid fa-image',
+            color: "#388E3C",
             stepName: '图片理解',
             showNode: true,
             x: 440,
@@ -76,13 +76,13 @@ const workflowNodes = [
     },
     {
         id: '1003',
-        icon: 'fa-solid fa-palette',
         name: "IMAGE_GENERATE",
         label: "图片生成",
         description: "根据提供的文本内容生成图片",
         type: "image_generate",
-        color: "#E6A23C",
         properties: {
+            icon: 'fa-solid fa-palette',
+            color: "#F57C00",
             stepName: '图片理解',
             showNode: true,
             x: 440,
@@ -93,13 +93,13 @@ const workflowNodes = [
     },
     {
         id: '1004',
-        icon: 'fa-solid fa-book',
         name: "KNOWLEDGE_SEARCH",
         label: "知识库检索",
         description: "关联知识库，查找与问题相关的分段",
         type: "knowledge_search",
-        color: "#F56C6C",
         properties: {
+            icon: 'fa-solid fa-book',
+            color: "#D32F2F",
             stepName: '图片理解',
             showNode: true,
             x: 440,
@@ -110,13 +110,13 @@ const workflowNodes = [
     },
     {
         id: '1005',
-        icon: 'fa-solid fa-magnifying-glass-chart',
         name: "RERANKER",
         label: "多路召回",
         description: "使用重排模型对多个知识库的检索结果进行二次召回",
         type: "reranker",
-        color: "#909399",
         properties: {
+            icon: 'fa-solid fa-magnifying-glass-chart',
+            color: "#616161",
             stepName: '多路召回',
             showNode: true,
             x: 440,
@@ -127,30 +127,47 @@ const workflowNodes = [
     },
     {
         id: '1006',
-        icon: 'fa-solid fa-question',
         name: "CONDITION",
         label: "判断器",
         description: "根据不同条件执行不同的节点",
         type: "condition",
-        color: "#5C6BC0",
         properties: {
+            icon: 'fa-solid fa-question',
+            color: "#303F9F",
             stepName: '判断器',
             showNode: true,
             x: 440,
             y: 340,
-            height: 320,
-            width: 280
+            height: 330,
+            width: 540,
+            branch_condition_list: [
+                {
+                    "index": 0,
+                    "height": 120,
+                    "id": "1009"
+                },
+                {
+                    "index": 1,
+                    "height": 120,
+                    "id": "4908"
+                },
+                {
+                    "index": 2,
+                    "height": 44 ,
+                    "id": "161"
+                }
+            ]
         }
     },
     {
         id: '1007',
-        icon: 'fa-solid fa-reply',
         name: "REPLAY",
         label: "指定回复",
         description: "指定回复内容，引用变量会转换为字符串进行输出",
         type: "reply",
-        color: "#FFB74D",
         properties: {
+            icon: 'fa-solid fa-reply',
+            color: "#FF9800",
             stepName: '指定回复',
             showNode: true,
             x: 440,
@@ -159,25 +176,15 @@ const workflowNodes = [
             width: 280
         }
     },
-    // {
-    //     id: '1008',
-    //     icon: 'fa-solid fa-file-signature',
-    //     name: "FORM_COLLECTION",
-    //     label: "表单收集",
-    //     description: "在问答过程中用于收集用户信息，可以根据收集到表单数据执行后续流程",
-    //     type: "form_collection",
-    //     color: "#81C784",
-    //     properties: {}
-    // },
     {
         id: '1009',
-        icon: 'fa-solid fa-pencil',
         name: "QUESTION",
         label: "问题优化",
         description: "根据历史聊天记录优化完善当前问题，更利于匹配知识库分段",
         type: "question",
-        color: "#FF8A65",
         properties: {
+            icon: 'fa-solid fa-pencil',
+            color: "#F4511E",
             stepName: '问题优化',
             showNode: true,
             x: 440,
@@ -188,13 +195,13 @@ const workflowNodes = [
     },
     {
         id: '1010',
-        icon: 'fa-solid fa-file-lines',
         name: "DOCUMENT_EXTRACT",
         label: "文档内容提取",
         description: "提取文档中的内容",
         type: "document_extract",
-        color: "#BA68C8",
         properties: {
+            icon: 'fa-solid fa-file-lines',
+            color: "#8E24AA",
             stepName: '文档内容提取',
             showNode: true,
             x: 440,
@@ -205,13 +212,13 @@ const workflowNodes = [
     },
     {
         id: '1011',
-        icon: 'fa-solid fa-masks-theater',
         name: "SPEECH_TO_TEXT",
         label: "语音转文本",
         description: "将音频通过语音识别模型转换为文本",
         type: "speech_to_text",
-        color: "#4DD0E1",
         properties: {
+            icon: 'fa-solid fa-masks-theater',
+            color: "#00ACC1",
             stepName: '语音转文本',
             showNode: true,
             x: 440,
@@ -222,13 +229,13 @@ const workflowNodes = [
     },
     {
         id: '1012',
-        icon: 'fa-solid fa-volume-high',
         name: "TEXT_TO_SPEECH",
         label: "文本转语音",
         description: "将文本通过语音合成模型转换为音频",
         type: "text_to_speech",
-        color: "#FFD54F",
         properties: {
+            icon: 'fa-solid fa-volume-high',
+            color: "#FFB300",
             stepName: '文本转语音',
             showNode: true,
             x: 440,
@@ -239,21 +246,21 @@ const workflowNodes = [
     },
     {
         id: '1013',
-        icon: 'fas fa-file-signature',
         name: "function",
         label: "脚本功能",
         description: "使用Groovy脚本进行编辑开发",
         type: "function",
-        color: "#909399",
         properties: {
+            icon: 'fas fa-file-signature',
+            color: "#424242",
             stepName: '脚本功能',
             showNode: true,
             x: 440,
             y: 340,
-            height: 570,
+            height: 310,
             width: 280
         }
-    },
+    }
 ];
 
 const clickNode = (item) => {
@@ -289,13 +296,13 @@ const clickNode = (item) => {
     .node-components-icon {
         width: 35px;
         height: 35px;
-        background: #3b5998 ;
+        background: #3b5998;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         color: rgb(255, 255, 255);
-        border-radius: 50%;
+        border-radius: 5px;
     }
 
     .node-components-content {
