41f1d72dca15343a9f54de20099f12be5ebbb994
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:52 +0800

    feat:新增 DatasetKnowledgeMapper 接口文件

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/mapper/DatasetKnowledgeMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.search.mapper;
+
+import com.alinesno.infra.base.search.entity.DatasetKnowledgeEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 应用管理Mapper接口
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface DatasetKnowledgeMapper extends IBaseMapper<DatasetKnowledgeEntity> {
+}
