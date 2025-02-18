68add819d05bf56c50c062c6cf672f6c003018ca
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:50 +0800

    feat:新增 ISearchLogService 接口文件

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/ISearchLogService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.search.service;
+
+import com.alinesno.infra.base.search.entity.SearchLogEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+/**
+ * 搜索日志Service接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface ISearchLogService extends IBaseService<SearchLogEntity> {
+}
