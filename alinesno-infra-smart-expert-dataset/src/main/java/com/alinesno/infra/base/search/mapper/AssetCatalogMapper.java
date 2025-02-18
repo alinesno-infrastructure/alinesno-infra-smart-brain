d741c41c761bd6d875a2d4c60a3acdceb81fd157
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:53 +0800

    feat:新增 AssetCatalogMapper 接口文件

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/mapper/AssetCatalogMapper.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.base.search.mapper;
+
+
+import com.alinesno.infra.base.search.entity.AssetCatalogEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * 资产编目Mapper接口
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+public interface AssetCatalogMapper extends IBaseMapper<AssetCatalogEntity> {
+}
