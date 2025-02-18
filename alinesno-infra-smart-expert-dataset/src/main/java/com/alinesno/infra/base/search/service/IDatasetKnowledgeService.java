94d3b7350b1a20d1856119450e7323d7906d45bb
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:51 +0800

    feat:新增 IDatasetKnowledgeService 接口，提供数据集知识管理功能

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/IDatasetKnowledgeService.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.base.search.service;
+
+import com.alinesno.infra.base.search.api.DataProcessingDto;
+import com.alinesno.infra.base.search.entity.DatasetKnowledgeEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * 数据集知识管理Service接口
+ * 该接口提供了对数据集知识的基本操作，包括文档解析等功能
+ * 主要用于处理数据集中的文档，提取并保存有用信息
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IDatasetKnowledgeService extends IBaseService<DatasetKnowledgeEntity> {
+
+    /**
+     * 文档解析功能
+     * 该方法用于接收一个数据集ID和一组句子，将这些句子作为文档内容进行解析
+     * 并根据文件名和文件类型对解析结果进行相应处理
+     *
+     * @param datasetId 数据集的唯一标识
+     * @param sentenceList 待解析的句子列表，代表文档的内容
+     * @param fileName 文件名，用于在解析过程中标识文档
+     * @param fileType 文件类型，影响解析方式或存储格式
+     */
+    void parserDocument(Long datasetId, List<String> sentenceList, String fileName, String fileType);
+
+    /**
+     * 批量保存到数据集
+     * @param datasetId
+     * @param sentenceList
+     */
+    void saveBatchToDataset(Long datasetId, List<String> sentenceList , String fileName);
+
+    /**
+     * 保存数据集临时文件
+     *
+     * @param datasetId
+     * @param fileName
+     * @param targetFile
+     * @param fileType
+     * @param fileSuffix
+     * @return
+     */
+    void saveDatasetTmpFile(Long datasetId, String fileName, File targetFile, String fileType, String fileSuffix);
+
+    /**
+     * 查询临时文件
+     * @param datasetId
+     * @return
+     */
+    List<DatasetKnowledgeEntity> queryTmpFileByDatasetId(Long datasetId);
+
+    /**
+     * 处理解析并上传到向量库
+     * @param dto
+     * @return
+     */
+    void dataUploadToVectorDataset(DataProcessingDto dto);
+}
