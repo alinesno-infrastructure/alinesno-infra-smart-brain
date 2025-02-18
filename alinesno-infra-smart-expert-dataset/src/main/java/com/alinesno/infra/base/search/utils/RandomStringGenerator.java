a1e5828cb77322fbe0350f1ab53e12a7827de057
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:52 +0800

    feat:新增 RandomStringGenerator 工具类，用于生成随机字符串

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/utils/RandomStringGenerator.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.base.search.utils;
+
+import java.security.SecureRandom;
+
+/**
+ * 生成随机字符串的工具类。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public class RandomStringGenerator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int LENGTH = 32;
+
+    /**
+     * 生成指定长度的随机字符串。
+     *
+     * @return 生成的随机字符串。
+     */
+    public static String generateRandomString() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(LENGTH);
+
+        for (int i = 0; i < LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
+    }
+}
