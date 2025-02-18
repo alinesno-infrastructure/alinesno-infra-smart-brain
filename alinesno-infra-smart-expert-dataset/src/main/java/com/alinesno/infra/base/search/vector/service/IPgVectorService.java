672fbae7d6b0fbd7f01105f463fc62d1eb0c2a6f
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:51 +0800

    feat:新增 IPgVectorService 接口，包含向量查询、索引创建等功能

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/vector/service/IPgVectorService.java
@@ -0,0 +1,73 @@
+package com.alinesno.infra.base.search.vector.service;
+
+import com.alinesno.infra.smart.assistant.adapter.dto.DocumentVectorBean;
+import com.alinesno.infra.smart.assistant.adapter.dto.VectorSearchDto;
+
+import java.util.List;
+
+public interface IPgVectorService {
+
+    /**
+     * 通过文本查询索引内容
+     * @param indexName
+     * @param fileName
+     * @param queryText
+     * @param size
+     * @return
+     */
+    List<DocumentVectorBean> queryDocument(String indexName, String fileName , String queryText , int size) ;
+
+    /**
+     * 通过文本查询向量库内容
+     * @param indexName
+     * @param queryText
+     * @param size
+     * @return
+     */
+    List<DocumentVectorBean> queryVectorDocument(String indexName, String queryText , int size) ;
+
+    /**
+     * 通过文本查询向量库内容
+     * @param dto
+     * @return
+     */
+    List<DocumentVectorBean> queryVectorDocument(VectorSearchDto dto) ;
+
+    /**
+     * 创建向量索引库
+     * @param indexName
+     */
+    void createVectorIndex(String indexName);
+
+    /**
+     * 插入向量数据
+     * @param documentVectorBean
+     */
+    void insertVector(DocumentVectorBean documentVectorBean);
+
+    /**
+     * 更新向量数据
+     * @param documentVectorBean
+     */
+    void updateVector(DocumentVectorBean documentVectorBean) ;
+
+    /**
+     * 删除向量索引库
+     * @param indexName
+     */
+    void deleteVectorIndex(String indexName, long documentId);
+
+    /**
+     * 通过全文查询向量库内容
+     * @param dto
+     * @return
+     */
+    List<DocumentVectorBean> queryFullTextDocument(VectorSearchDto dto);
+
+    /**
+     * 通过混合查询向量库内容
+     * @param dto
+     * @return
+     */
+    List<DocumentVectorBean> queryHybridDocument(VectorSearchDto dto);
+}
