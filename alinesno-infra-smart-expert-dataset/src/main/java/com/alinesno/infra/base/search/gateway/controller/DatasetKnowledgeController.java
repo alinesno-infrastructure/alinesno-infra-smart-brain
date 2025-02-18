0717a95b22499e4ac6410c1c53ac30a463ae20c5
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:49 +0800

    feat:新增 DatasetKnowledgeController 控制器类

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/gateway/controller/DatasetKnowledgeController.java
@@ -0,0 +1,187 @@
+package com.alinesno.infra.base.search.gateway.controller;
+
+import cn.hutool.core.io.FileTypeUtil;
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.base.search.api.DataProcessingDto;
+import com.alinesno.infra.base.search.entity.DatasetKnowledgeEntity;
+import com.alinesno.infra.base.search.enums.FileTypeEnums;
+import com.alinesno.infra.base.search.service.IDatasetKnowledgeService;
+import com.alinesno.infra.base.search.service.IDocumentParserService;
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.facade.pageable.ConditionDto;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.google.gson.Gson;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/base/search/datasetKnowledge")
+public class DatasetKnowledgeController extends BaseController<DatasetKnowledgeEntity, IDatasetKnowledgeService> {
+
+    @Autowired
+    private IDatasetKnowledgeService service;
+
+    @Autowired
+    private IDocumentParserService documentParserService ;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
+
+    /**
+     * 获取BusinessLogEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+
+        String datasetId = request.getParameter("datasetId") ;
+
+        List<ConditionDto> conditionList = new ArrayList<>() ;
+
+        ConditionDto orderConditionDto = new ConditionDto() ;
+        orderConditionDto.setType("orderBy");
+        orderConditionDto.setColumn("updateTime");
+        orderConditionDto.setValue("false");
+
+        conditionList.add(orderConditionDto) ;
+
+        page.setConditionList(conditionList);
+
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    /**
+     * 查询上传未导入的文件
+     */
+    @GetMapping("/queryTmpFileByDatasetId")
+    public AjaxResult queryTmpFileByDatasetId(@RequestParam Long datasetId) {
+        List<DatasetKnowledgeEntity> list = service.queryTmpFileByDatasetId(datasetId) ;
+        return AjaxResult.success(list) ;
+    }
+
+    /**
+     * 上传解析文件
+     */
+    @PostMapping("/uploadTmpFileByDatasetId")
+    public AjaxResult uploadTmpFileByDatasetId(@Valid @RequestBody DataProcessingDto dto) throws Exception {
+        log.debug("dto = {}" , dto);
+
+        service.dataUploadToVectorDataset(dto) ;
+
+        return AjaxResult.success("解析成功") ;
+    }
+
+    /**
+     * 上传文件
+     */
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public AjaxResult upload(@RequestPart("file") MultipartFile file, Long datasetId) throws Exception {
+        String fileName = file.getOriginalFilename();
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        String fileType = FileTypeUtil.getType(targetFile);
+
+        service.saveDatasetTmpFile(datasetId, fileName, targetFile , fileType, fileSuffix) ;
+
+        // 处理完成之后删除文件
+        // FileUtils.forceDeleteOnExit(targetFile);
+
+        return AjaxResult.success("上传成功") ;
+    }
+
+    /**
+     * 导入数据
+     *
+     * @param file 导入文件
+     */
+    @PostMapping(value = "/importData", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, Long datasetId) throws Exception {
+
+        // 获取原始文件名
+        String fileName = file.getOriginalFilename();
+        List<String> sentenceList = new ArrayList<>();
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        String fileType = FileTypeUtil.getType(targetFile);
+
+        FileTypeEnums constants = FileTypeEnums.getByValue(fileSuffix.toLowerCase()) ;
+        assert constants != null;
+
+        sentenceList = switch (constants) {
+            case PDF ->  documentParserService.parsePDF(targetFile);
+            case MD -> documentParserService.parseMD(targetFile);
+            case EXCEL, EXCEL_OLD -> documentParserService.parseExcel(targetFile);
+            case DOCX -> documentParserService.getContentDocx(targetFile);
+            case DOC -> documentParserService.getContentDoc(targetFile);
+            default -> sentenceList;
+        };
+
+        log.debug("sentenceList = {}" , new Gson().toJson(sentenceList));
+
+        // 处理完成之后删除文件
+        FileUtils.forceDeleteOnExit(targetFile);
+
+        if(sentenceList.isEmpty()){
+            return AjaxResult.error(fileName + " 文档解析失败.") ;
+        }
+
+        service.parserDocument(datasetId, sentenceList, fileName, fileType);
+
+        return AjaxResult.success(fileName) ;
+    }
+
+
+
+    @Override
+    public IDatasetKnowledgeService getFeign() {
+        return this.service;
+    }
+}
+
