494ece72a203d2afe7ce3c4d9f1f8f02eec3d983
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:52 +0800

    feat:新增 PPtPageBean 类，包含页码和内容属性。

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/utils/bean/PPtPageBean.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.base.search.utils.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PPtPageBean {
+
+    private int pageNum ;
+    private String content ;
+
+}
