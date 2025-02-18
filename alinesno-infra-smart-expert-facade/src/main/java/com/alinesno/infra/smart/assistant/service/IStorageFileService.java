0c24584f3a435643568e49073f565c4ba402e42f
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:49 +0800

    feat:新增 IStorageFileService 接口，定义存储文件服务的基础操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IStorageFileService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.StorageFileEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IStorageFileService extends IBaseService<StorageFileEntity> {
+}
