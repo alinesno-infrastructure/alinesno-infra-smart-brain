a46a0cbf16fb6960ab426af3838b8c439791a9b2
张婷(Tina) <tinaAip@gmail.com>
2025-02-19 00:24:53 +0800

    feat:在智能助手插件视图中添加插件分类功能

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/plugin/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/plugin/index.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="app-container">
     <el-row :gutter="20">
-      <!--应用数据-->
-      <el-col :span="24" :xs="24">
-        <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+      <!-- 插件分类  -->
+      <el-col :span="3" :xs="24">
+        <div class="plugin-catalog">
+          <div class="catalog-title">
+            <i class="fa-solid fa-computer"></i> 插件分类
+          </div>
+          <div class="catalog-content">
+            <div 
+              v-for="(category, index) in pluginCategories" 
+              :key="index" 
+              @click="handleCategoryClick(category.label)"
+              class="catalog-item"
+            >
+              <i :class="category.code"></i> {{ category.label }}
+            </div>
+          </div>
+        </div>
+      </el-col>
 
+      <!--应用数据-->
+      <el-col :span="21" :xs="24">
+        <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
           <el-form-item label="工具名称" prop="name">
             <el-input v-model="queryParams['condition[name|like]']" placeholder="请输入工具名称" clearable style="width: 240px"
               @keyup.enter="handleQuery" />
           </el-form-item>
-          <el-form-item label="工具类型" prop="toolType" label-width="100px">
-            <el-input v-model="queryParams['condition[toolType|like]']" placeholder="请输入工具类型" clearable
+          <el-form-item label="插件类型" prop="pluginType" label-width="100px">
+            <el-input v-model="queryParams['condition[pluginType|like]']" placeholder="请输入插件类型" clearable
               style="width: 240px" @keyup.enter="handleQuery" />
           </el-form-item>
-
           <el-form-item>
             <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
             <el-button icon="Refresh" @click="resetQuery">重置</el-button>
@@ -22,26 +39,22 @@
 
         <el-row :gutter="10" class="mb8">
           <el-col :span="1.5">
-            <el-button type="primary" plain icon="Plus" @click="handleAdd">新增
-            </el-button>
+            <el-button type="primary" plain icon="Plus" @click="handleAdd">新增</el-button>
           </el-col>
           <el-col :span="1.5">
             <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
-              v-hasPermi="['system:Tool:edit']">修改
-            </el-button>
+              v-hasPermi="['system:Tool:edit']">修改</el-button>
           </el-col>
           <el-col :span="1.5">
             <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
-              v-hasPermi="['system:Tool:remove']">删除
-            </el-button>
+              v-hasPermi="['system:Tool:remove']">删除</el-button>
           </el-col>
-
           <right-toolbar v-model:showSearch="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
         </el-row>
 
         <el-table v-loading="loading" :data="ToolList" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="50" align="center" />
-          <el-table-column label="图标" align="center" width="60px" prop="icon" v-if="columns[0].visible">
+          <el-table-column label="图标" align="center" width="50px" prop="icon" v-if="columns[0].visible">
             <template #default="scope">
               <div class="role-icon">
                 <img :src="imagePath(scope.row.icon)" style="border-radius: 5px;" />
@@ -51,11 +64,8 @@
           <el-table-column label="工具名称" align="left" key="name" width="250" prop="name" v-if="columns[1].visible"
             :show-overflow-tooltip="true">
             <template #default="scope">
-              <div style="font-size: 15px;font-weight: 500;color: #3b5998;">
+              <div>
                 {{ scope.row.name }}
-              </div>
-              <div style="font-size: 13px;color: #a5a5a5;">
-                次数: {{ scope.row.useCount }}
               </div>
             </template>
           </el-table-column>
@@ -67,32 +77,27 @@
               </div>
             </template>
           </el-table-column>
-
-          <el-table-column label="工具类型" align="center" key="toolType" width="150" prop="toolType" v-if="columns[2].visible" :show-overflow-tooltip="true">
-              <template #default="scope">
-                {{ getToolTypeName(scope.row.toolType) }}
-              </template> 
+          <el-table-column label="插件类型" align="center" key="pluginType" width="150" prop="pluginType"
+            v-if="columns[2].visible" :show-overflow-tooltip="true">
+            <template #default="scope">
+              {{ getPluginTypeName(scope.row.pluginType) }}
+            </template>
           </el-table-column>
-
-          <el-table-column label="脚本" align="center" width="110"  key="target" prop="target" v-if="columns[6].visible" :show-overflow-tooltip="true">
+          <el-table-column label="脚本" align="center" width="110" key="target" prop="target" v-if="columns[6].visible"
+            :show-overflow-tooltip="true">
             <template #default="scope">
               <el-button type="primary" text @click="configToolScript(scope.row)">
-                <i class="fa-solid fa-code"></i>脚本
+                <i class="fa-solid fa-code"></i>编辑
               </el-button>
             </template>
           </el-table-column>
-
-          <el-table-column label="状态" align="center" width="100" key="hasStatus" prop="hasStatus" v-if="columns[1].visible" :show-overflow-tooltip="true" >
-              <template #default="scope">
-                <el-switch
-                    v-model="scope.row.hasStatus"
-                    :active-value="0"
-                    :inactive-value="1"
-                    @change="handleChangStatusField('hasStatus' , scope.row.hasStatus, scope.row.id)"
-                />
-              </template>
+          <el-table-column label="状态" align="center" width="100" key="hasStatus" prop="hasStatus"
+            v-if="columns[1].visible" :show-overflow-tooltip="true">
+            <template #default="scope">
+              <el-switch size="small" v-model="scope.row.hasStatus" :active-value="0" :inactive-value="1"
+                @change="handleChangStatusField('hasStatus', scope.row.hasStatus, scope.row.id)" />
+            </template>
           </el-table-column>
-
           <el-table-column label="更新时间" align="center" prop="addTime" v-if="columns[7].visible" width="160">
             <template #default="scope">
               <span>{{ parseTime(scope.row.addTime) }}</span>
@@ -109,7 +114,6 @@
                 <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
                   v-hasPermi="['system:Tool:remove']"></el-button>
               </el-tooltip>
-
             </template>
           </el-table-column>
         </el-table>
@@ -124,15 +128,9 @@
         <el-row>
           <el-col :span="24" class="editor-after-div">
             <el-form-item label="图标" prop="icon">
-              <el-upload 
-                :file-list="imageUrl" 
-                :action="upload.url + '?type=img&updateSupport=' + upload.updateSupport"
-                list-type="picture-card" 
-                :auto-upload="true" 
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload" 
-                :headers="upload.headers" 
-                :disabled="upload.isUploading"
+              <el-upload :file-list="imageUrl" :action="upload.url + '?type=img&updateSupport=' + upload.updateSupport"
+                list-type="picture-card" :auto-upload="true" :on-success="handleAvatarSuccess"
+                :before-upload="beforeAvatarUpload" :headers="upload.headers" :disabled="upload.isUploading"
                 :on-progress="handleFileUploadProgress">
                 <el-icon class="avatar-uploader-icon">
                   <Plus />
@@ -143,9 +141,9 @@
         </el-row>
         <el-row>
           <el-col :span="24">
-            <el-form-item label="工具类型" prop="toolType">
-              <el-radio-group v-model="form.toolType">
-                <el-radio v-for="item in toolTypes" :key="item.key" :label="item.key">{{ item.name }}</el-radio>
+            <el-form-item label="插件类型" prop="pluginType">
+              <el-radio-group v-model="form.pluginType">
+                <el-radio v-for="item in pluginCategories" :key="item.label" :label="item.label">{{ item.label }}</el-radio>
               </el-radio-group>
             </el-form-item>
           </el-col>
@@ -157,7 +155,6 @@
             </el-form-item>
           </el-col>
         </el-row>
-
         <el-row>
           <el-col :span="24">
             <el-form-item label="描述" prop="description">
@@ -165,42 +162,11 @@
             </el-form-item>
           </el-col>
         </el-row>
-
       </el-form>
       <template #footer>
         <div class="dialog-footer">
           <el-button type="primary" @click="submitForm">确 定</el-button>
           <el-button @click="cancel">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
-
-    <!-- 应用导入对话框 -->
-    <el-dialog :title="upload.title" v-model="upload.open" width="400px" append-to-body>
-      <el-upload ref="uploadRef" :limit="1" accept=".xlsx, .xls" :headers="upload.headers"
-        :action="upload.url + '?updateSupport=' + upload.updateSupport" :disabled="upload.isUploading"
-        :on-progress="handleFileUploadProgress" :on-success="handleFileSuccess" :auto-upload="false" drag>
-        <el-icon class="el-icon--upload">
-          <upload-filled />
-        </el-icon>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <template #tip>
-          <div class="el-upload__tip text-center">
-            <div class="el-upload__tip">
-              <el-checkbox v-model="upload.updateSupport" />
-              是否更新已经存在的应用数据
-            </div>
-            <span>仅允许导入xls、xlsx格式文件。</span>
-            <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;"
-              @click="importTemplate">下载模板
-            </el-link>
-          </div>
-        </template>
-      </el-upload>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitFileForm">确 定</el-button>
-          <el-button @click="upload.open = false">取 消</el-button>
         </div>
       </template>
     </el-dialog>
@@ -217,11 +183,11 @@ import {
   updateTool,
   addTool,
 } from "@/api/smart/assistant/tool";
-import { reactive } from "vue";
+import { reactive, ref, computed } from "vue";
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 const { proxy } = getCurrentInstance();
-// const { sys_normal_disable, sys_Tool_sex } = proxy.useDict("sys_normal_disable", "sys_Tool_sex");
 
 const ToolList = ref([]);
 const open = ref(false);
@@ -234,12 +200,19 @@ const total = ref(0);
 const title = ref("");
 const dateRange = ref([]);
 
-const imageUrl = ref([])
+const imageUrl = ref([]);
 
-const toolTypes = ref([
-  { key: 'utility-tools', name: '实用工具' },
-  { key: 'web-search', name: '网页搜索' },
-  { key: 'project-management', name: '项目管理' },
+// 列显隐信息
+const columns = ref([
+  { key: 0, label: `图标`, visible: true },
+  { key: 1, label: `工具名称`, visible: true },
+  { key: 2, label: `插件类型`, visible: true },
+  { key: 3, label: `使用场景`, visible: true },
+  { key: 4, label: `状态`, visible: true },
+  { key: 5, label: `工具描述`, visible: true },
+  { key: 6, label: `应用目标`, visible: true },
+  { key: 7, label: `创建时间`, visible: true },
+  { key: 8, label: `编辑`, visible: true },
 ]);
 
 /*** 应用导入参数 */
@@ -259,28 +232,15 @@ const upload = reactive({
   // 显示地址
   display: import.meta.env.VITE_APP_BASE_API + "/v1/api/infra/base/im/chat/displayImage/"
 });
-// 列显隐信息
-const columns = ref([
-  { key: 0, label: `图标`, visible: true },
-  { key: 1, label: `工具名称`, visible: true },
-  { key: 2, label: `工具类型`, visible: true },
-  { key: 3, label: `使用场景`, visible: true },
-  { key: 4, label: `状态`, visible: true },
-  { key: 5, label: `工具描述`, visible: true },
-  { key: 6, label: `应用目标`, visible: true },
-  { key: 7, label: `创建时间`, visible: true },
-  { key: 8, label: `编辑`, visible: true },
-
-]);
 
 const data = reactive({
   form: {},
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    ToolName: undefined,
+    toolName: undefined,
     name: undefined,
-    toolType: undefined,
+    pluginType: undefined,
     status: undefined,
     deptId: undefined
   },
@@ -292,7 +252,7 @@ const data = reactive({
       message: "工具名称长度必须介于 2 和 20 之间",
       trigger: "blur"
     }],
-    toolType: [{ required: true, message: "工具类型不能为空", trigger: "blur" }],
+    pluginType: [{ required: true, message: "插件类型不能为空", trigger: "blur" }],
     screen: [{ required: true, message: "使用场景不能为空", trigger: "blur" }],
     hasStatus: [{ required: true, message: "状态不能为空", trigger: "blur" }],
     description: [{ required: true, message: "工具描述不能为空", trigger: "blur" }],
@@ -302,6 +262,23 @@ const data = reactive({
 
 const { queryParams, form, rules } = toRefs(data);
 
+// 定义插件分类数组，每个元素包含 code 和 label
+const pluginCategories = ref([
+  { code: 'fa-solid fa-newspaper', label: '新闻阅读' },
+  { code: 'fa-solid fa-image', label: '图像' },
+  { code: 'fa-solid fa-screwdriver-wrench', label: '实用工具' },
+  { code: 'fa-solid fa-house-chimney', label: '便利生活' },
+  { code: 'fa-solid fa-magnifying-glass', label: '网页搜索' },
+  { code: 'fa-solid fa-graduation-cap', label: '科学与教育' },
+  { code: 'fa-solid fa-users', label: '社交' },
+  { code: 'fa-solid fa-gamepad', label: '游戏与娱乐' },
+  { code: 'fa-solid fa-money-bill-trend-up', label: '金融与商业' }
+]);
+
+// 处理分类项点击事件
+const handleCategoryClick = (category) => {
+  console.log(`点击了分类：${category}`);
+};
 
 /** 查询应用列表 */
 function getList() {
@@ -315,7 +292,6 @@ function getList() {
 
 /** 图片上传成功 */
 const handleAvatarSuccess = (response, uploadFile) => {
-  // imageUrl.value = URL.createObjectURL(uploadFile.raw);
   imageUrl.value = response.data ? response.data.split(',').map(url => { return { url: upload.display + url } }) : [];
   form.value.icon = response.data;
   console.log('form.icon = ' + form.value.icon);
@@ -331,21 +307,20 @@ const beforeAvatarUpload = (rawFile) => {
 };
 
 /** 配置执行脚本 */
-function configToolScript(row){
-  router.push({path:"/expert/smart/assistant/tool/script" , query: {toolId: row.id}});
+function configToolScript(row) {
+  router.push({ path: "/expert/smart/assistant/tool/script", query: { toolId: row.id } });
 }
 
-/** 获取工具类型名称 */
-const getToolTypeName = computed(() => {
+/** 获取插件类型名称 */
+const getPluginTypeName = computed(() => {
   return (key) => {
-    const toolType = toolTypes.value.find(type => type.key === key);
-    return toolType ? toolType.name : '未知类型';
+    const pluginType = pluginCategories.value.find(type => type.label === key);
+    return pluginType ? pluginType.label : '未知类型';
   };
 });
 
 /** 搜索按钮操作 */
 function handleQuery() {
-  console.log(queryParams);
   queryParams.value.pageNum = 1;
   getList();
 };
@@ -355,7 +330,7 @@ function resetQuery() {
   dateRange.value = [];
   proxy.resetForm("queryRef");
   queryParams.value.name = undefined;
-  queryParams.value.toolType = undefined;
+  queryParams.value.pluginType = undefined;
   proxy.$refs.deptTreeRef.setCurrentKey(null);
   handleQuery();
 };
@@ -363,7 +338,6 @@ function resetQuery() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const applicationIds = row.id || ids.value;
-
   proxy.$modal.confirm('是否确认删除应用编号为"' + applicationIds + '"的数据项？').then(function () {
     return delTool(applicationIds);
   }).then(() => {
@@ -374,17 +348,15 @@ function handleDelete(row) {
 };
 
 /** 修改状态 */
-const handleChangStatusField = async(field , value , id) => {
-   // 判断tags值 这样就不会进页面时调用了
-     const res = await changStatusField({
-        field: field,
-        value: value?1:0,
-        id: id
-     }).catch(() => { })
-     if (res && res.code == 200) {
-        // 刷新表格
-        getList()
-     }
+const handleChangStatusField = async (field, value, id) => {
+  const res = await changStatusField({
+    field: field,
+    value: value ? 1 : 0,
+    id: id
+  }).catch(() => { })
+  if (res && res.code == 200) {
+    getList();
+  }
 }
 
 /** 选择条数  */
@@ -399,7 +371,7 @@ function reset() {
   form.value = {
     applicationId: undefined,
     name: undefined,
-    toolType: undefined,
+    pluginType: undefined,
     screen: undefined,
     hasStatus: undefined,
     description: undefined,
@@ -430,7 +402,6 @@ function handleUpdate(row) {
     form.value.applicationId = applicationId;
     open.value = true;
     title.value = "修改应用";
-
   });
 };
 
@@ -456,14 +427,45 @@ function submitForm() {
 };
 
 getList();
+
 </script>
 
 <style lang="scss" scoped>
 .role-icon {
   img {
-    width: 45px;
-    height: 45px;
+    width: 35px;
+    height: 35px;
     border-radius: 50%;
+  }
+}
+
+.plugin-catalog {
+  background: #fafafa;
+  height: calc(99vh - 100px);
+  border-radius: 2px;
+  padding: 9px;
+
+  .catalog-title {
+    padding: 10px;
+    font-size: 14px;
+    font-weight: bold;
+    color: #444;
+    margin-bottom: 5px;
+  }
+
+  .catalog-content {
+    .catalog-item {
+      padding: 12px 10px;
+      color: #443;
+      cursor: pointer;
+      margin-bottom: 4px;
+      border-radius: 4px;
+      font-size: 13px;
+
+      &:hover {
+        background: #e9eaf3;
+      }
+    }
   }
 }
 </style>
