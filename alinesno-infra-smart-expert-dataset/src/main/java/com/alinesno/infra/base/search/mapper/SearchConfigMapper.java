b97cadbbbf2b7bbaba7cdf24fa265f240a2d7a3e
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:49 +0800

    feat:新增 SearchConfigMapper 接口，实现持久层操作

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/mapper/SearchConfigMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.base.search.mapper;
+
+import com.alinesno.infra.base.search.entity.SearchConfigEntity;
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author LuoXiaoDong
+ * @version 1.0.0
+ */
+public interface SearchConfigMapper extends IBaseMapper<SearchConfigEntity> {
+
+}
