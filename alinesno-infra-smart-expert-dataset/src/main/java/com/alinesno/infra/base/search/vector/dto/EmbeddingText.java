14242032cf2a4d98fafb7db2bb6c57b40dea40ba
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:51 +0800

    feat:新增 EmbeddingText 类，包含文本字符串属性

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/vector/dto/EmbeddingText.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.base.search.vector.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmbeddingText{
+    String text ;
+}
