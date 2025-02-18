f15ee071d54dd7ddbd4b4915dfa15c0ee8b266a7
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:49 +0800

    feat:新增 datasetKnowledge API 接口文件

---
+++ alinesno-infra-smart-expert-ui/src/api/base/search/datasetKnowledge.js
@@ -0,0 +1,96 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 数据库接口操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/base/search/datasetKnowledge/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    downloadfile: prefix + "downloadfile",
+    queryTmpFileByDatasetId: prefix + "queryTmpFileByDatasetId",
+    uploadTmpFileByDatasetId: prefix + "uploadTmpFileByDatasetId",
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 上传解析文件
+export function uploadTmpFileByDatasetId(data) {
+  return request({
+    url: managerUrl.uploadTmpFileByDatasetId ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 获取数据集未导入文件
+export function queryTmpFileByDatasetId(id) {
+  return request({
+    url: managerUrl.queryTmpFileByDatasetId + '?datasetId=' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 查询数据库列表
+export function listDatasetKnowledge(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询数据库详细
+export function getDatasetKnowledge(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增数据库
+export function addDatasetKnowledge(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改数据库
+export function updateDatasetKnowledge(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除数据库
+export function delDatasetKnowledge(id) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
