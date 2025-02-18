4527cf8aaab7a70eb23f07b48ab0f0635d5e3260
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:51 +0800

    feat:新增 MilvusVectorDatasetServiceImpl 类，实现向量数据集服务功能

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/vector/MilvusVectorDatasetServiceImpl.java
@@ -0,0 +1,103 @@
+package com.alinesno.infra.base.search.service.vector;
+
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.base.search.api.SearchUpdateConfigDto;
+import com.alinesno.infra.base.search.entity.VectorDatasetEntity;
+import com.alinesno.infra.base.search.mapper.VectorDatasetMapper;
+import com.alinesno.infra.base.search.service.IVectorDatasetService;
+import com.alinesno.infra.base.search.vector.dto.EmbeddingBean;
+import com.alinesno.infra.base.search.vector.service.IMilvusDataService;
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.smart.assistant.adapter.dto.DocumentVectorBean;
+import com.alinesno.infra.smart.assistant.adapter.dto.VectorSearchDto;
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 应用构建Service业务层处理
+ * 
+ * @version 1.0.0
+ * @since 2023-09-30
+ */
+@Slf4j
+@Service
+public class MilvusVectorDatasetServiceImpl extends IBaseServiceImpl<VectorDatasetEntity, VectorDatasetMapper> implements IVectorDatasetService {
+
+    @Autowired
+    private IMilvusDataService milvusDataService;
+
+    private static final Gson gson = new Gson() ;
+
+//    @Autowired
+//    private EmbeddingConsumer embeddingConsumer ;
+
+    @Async
+    @Override
+    public void insertDatasetKnowledge(Long datasetId, List<String> sentenceList, String fileName, String fileType) {
+
+        VectorDatasetEntity vectorDatasetEntity = getById(datasetId) ;
+
+        int datasetSize = sentenceList.size() + vectorDatasetEntity.getDatasetSize() ;
+        log.debug("datasetSize = {}" , datasetSize);
+
+        vectorDatasetEntity.setDatasetSize(datasetSize);
+        update(vectorDatasetEntity) ;
+
+        String collectionName = vectorDatasetEntity.getCollectionName();
+
+        for(String content : sentenceList){
+
+            String vectorData = null ; //  embeddingConsumer.embeddings(gson.toJson(List.of(new EmbeddingText(content)))) ;
+            log.debug("vectorData = {}" , vectorData);
+
+            EmbeddingBean embeddingBean = new EmbeddingBean() ;
+
+            embeddingBean.setId(IdUtil.getSnowflakeNextId());
+            embeddingBean.setDatasetId(datasetId);
+            embeddingBean.setDocumentContent(content);
+            embeddingBean.setDocumentVector(vectorData);
+
+            milvusDataService.insertData(collectionName , "novel" , embeddingBean);
+        }
+
+        milvusDataService.buildIndexByCollection(collectionName) ;
+
+    }
+
+    @Override
+    public List<DocumentVectorBean> search(VectorSearchDto dto) {
+        return null;
+    }
+
+//    @Override
+//    public void buildCreateCollectionParam(String collectionName, String description, int shardsNum) {
+//
+//    }
+
+    @Override
+    public String getVectorEngine() {
+        return "milvus" ;
+    }
+
+    @Override
+    public List<DocumentVectorBean> rerankSearch(VectorSearchDto dto) {
+        return null;
+    }
+
+    @Override
+    public void updateDatasetConfig(SearchUpdateConfigDto configDto) {
+
+    }
+
+    @Override
+    public List<VectorDatasetEntity> latestDatasets(int i, PermissionQuery query) {
+        return null;
+    }
+
+}
