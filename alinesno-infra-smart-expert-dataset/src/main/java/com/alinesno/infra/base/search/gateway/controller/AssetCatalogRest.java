504c02442f9e204249f3374a3183039bd861d519
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:50 +0800

    feat:新增 AssetCatalogRest 控制器类，包含数据表格和查询功能

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/gateway/controller/AssetCatalogRest.java
@@ -0,0 +1,88 @@
+package com.alinesno.infra.base.search.gateway.controller;
+
+import com.alinesno.infra.base.search.entity.AssetCatalogEntity;
+import com.alinesno.infra.base.search.service.IAssetCatalogService;
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/base/search/catalog")
+public class AssetCatalogRest extends BaseController<AssetCatalogEntity, IAssetCatalogService> {
+
+    @Autowired
+    private IAssetCatalogService assetCatalogService;
+
+    @DataPermissionScope
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign() , page) ;
+    }
+
+    @DataPermissionQuery
+    @GetMapping("/list")
+    public AjaxResult list(AssetCatalogEntity promptCatalog , PermissionQuery query) {
+        List<AssetCatalogEntity> promptCatalogEntities = assetCatalogService.selectCatalogList(promptCatalog , query);
+
+        return AjaxResult.success("操作成功." , promptCatalogEntities) ;
+    }
+
+    /**
+     * 保存角色类型
+     * @return
+     */
+    @DataPermissionSave
+    @PostMapping("/insertCatalog")
+    public AjaxResult insertCatalog(@RequestBody AssetCatalogEntity entity){
+
+        assetCatalogService.insertCatalog(entity) ;
+
+        return AjaxResult.success("操作成功.") ;
+    }
+
+    /**
+     * 获取到子类
+     * @param deptId
+     * @return
+     */
+    @DataPermissionQuery
+    @GetMapping("/excludeChild/{id}")
+    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId , PermissionQuery query)
+    {
+        List<AssetCatalogEntity> depts = assetCatalogService.selectCatalogList(new AssetCatalogEntity(), query);
+        depts.removeIf(d -> d.getId().longValue() == deptId || ArrayUtils.contains(StringUtils.split(d.getAncestors(), ","), deptId + ""));
+        return AjaxResult.success("操作成功." , depts);
+    }
+
+    @Override
+    public IAssetCatalogService getFeign() {
+        return this.assetCatalogService;
+    }
+}
