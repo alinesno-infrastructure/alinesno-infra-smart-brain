3cdc7f28f259e7daa628a83d5deeaefe02b8be8a
李娜(Allen) <allenAip@gmail.com>
2025-02-19 00:24:52 +0800

    feat:新增 CloudStorageConsumer 接口，支持文件上传与下载功能。

---
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/service/CloudStorageConsumer.java
@@ -0,0 +1,43 @@
+package com.alinesno.infra.smart.assistant.adapter.service;
+
+import com.alinesno.infra.common.facade.response.R;
+import com.dtflys.forest.callback.OnProgress;
+
+import java.io.File;
+
+/**
+ * 云文件存储上传
+ */
+public interface CloudStorageConsumer {
+
+    /**
+     * 上传文件到指定的存储平台并返回访问的url
+     */
+    R<String> uploadCallbackUrl(File file) ;
+
+    /**
+     * 上传文件到指定的存储平台并返回访问的url
+     * @param file
+     * @param platform
+     * @return
+     */
+    R<String> uploadCallbackUrl(File file, String platform);
+
+    /**
+     * 上传文件到存储
+     *
+     * @param file 待上传的文件，使用multipart/form-data方式提交
+     * @return 返回上传结果的AjaxResult对象
+     */
+    R<String> upload(File file) ;
+
+    /**
+     * 从存储下载文件
+     *
+     * @param storageId 存储文件的唯一标识符，用于定位特定文件
+     * @param onProgress 下载进度回调接口，用于实时获取下载进度
+     * @return 返回文件的字节数组表示形式
+     */
+    byte[] download(String storageId, OnProgress onProgress) ;
+
+}
