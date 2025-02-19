104d44a62dc720941028dce5729a1fe7494c7ff2
李伟(Michael) <michaelAip@gmail.com>
2025-02-20 00:13:48 +0800

    feat:新增 roleChat.js 文件，包含获取角色信息和角色聊天功能接口

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/roleChat.js
@@ -0,0 +1,30 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/v1/api/infra/base/im/roleChat/' ;
+var managerUrl = {
+  getInfo: prefix +"getInfo",
+  chatRole: prefix + "chatRole"
+}
+
+// 查询应用详细
+export function getInfo(roleId) {
+  return request({
+    url: managerUrl.getInfo + '?roleId=' + parseStrEmpty(roleId),
+    method: 'get'
+  })
+}
+
+// 与Role聊天 
+export function chatRole(data , roleId) {
+  return request({
+    url: managerUrl.chatRole + '?roleId=' + parseStrEmpty(roleId),
+    headers: {
+      isEncrypt: true
+    },
+    method: 'post',
+    data: data
+  })
+}
