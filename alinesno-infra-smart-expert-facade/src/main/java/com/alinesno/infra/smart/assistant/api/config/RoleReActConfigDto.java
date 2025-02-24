cf9a0667846048b60f504e336c6aabb8fc564588
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:12 +0800

    refactor: 重构 RoleReActConfigDto 类，移除不必要的内部类并添加语料搜索配置

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/RoleReActConfigDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/RoleReActConfigDto.java
@@ -56,68 +56,7 @@ public class RoleReActConfigDto {
     // 猜测用户问题相关数据配置
     private GuessWhatYouAskData guessWhatYouAskData;
 
-    /**
-     * 模型配置内部类
-     */
-    @Data
-    public static class ModelConfig {
-        // 人工智能模型名称
-        private String aiModel;
-        // 记忆轮数，可能用于记录对话轮次相关信息
-        private int memoryRounds;
-        // 回复内容长度限制
-        private int replyLimit;
-        // 温度参数，影响生成文本的随机性，值越高越随机
-        private double temperature;
-        // 核采样参数，用于控制生成文本的多样性
-        private double topP;
-        // 回复格式，例如json格式
-        private String replayFormat;
-        // 停止序列，当生成文本遇到该序列时停止
-        private String stopSequences;
-        // 是否启用回复格式功能
-        private boolean replyFormatEnabled ;
-        // 是否启用停止序列功能
-        private boolean stopSequencesEnabled ;
-    }
+    // 语料搜索配置
+    private DatasetSearchConfig datasetSearchConfig ;
 
-    /**
-     * 语音输入数据内部类
-     */
-    @Data
-    public static class VoiceInputData {
-        // 是否启用语音输入功能
-        private boolean isEnable;
-        // 语音模型名称
-        private String voiceModel;
-        // 语音语速
-        private int speechRate;
-    }
-
-    /**
-     * 语音播放
-     */
-    @Data
-    public static class VoicePlayData {
-        // 语音模型名称，可能用于语音播放
-        private String voiceModel;
-        // 语音速度，用于语音播放的速度
-        private int speechRate;
-    }
-
-
-    /**
-     * 猜测用户问题数据内部类
-     */
-    @Data
-    public static class GuessWhatYouAskData {
-        // 是否启用猜测用户问题功能
-        private boolean isEnable;
-        // 语音模型名称，可能用于语音提示猜测问题
-        private String voiceModel;
-        // 提示内容，用于引导猜测用户问题的规则
-        private String prompt;
-        // 语音速度，用于猜测问题语音提示的速度
-        private int voiceSpeed;
-    }
 }
