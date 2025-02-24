6b03bcbbe228c88f9ce042694f26f33622bb88c7
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:12 +0800

    refactor: 注释掉 RoleToolEntity 类的代码

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/RoleToolEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/entity/RoleToolEntity.java
@@ -1,38 +1,38 @@
-package com.alinesno.infra.smart.assistant.entity;
-
-import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
-import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-@Entity
-@NoArgsConstructor
-@TableName("role_tool")
-@TableComment("角色工具信息")
-public class RoleToolEntity extends InfraBaseEntity {
-
-    // 角色信息、插件信息
-    @ColumnComment("角色ID")
-    @Column(length = 50)
-    @TableField(value = "role_id")
-    private Long roleId;
-
-    @ColumnComment("插件ID")
-    @Column(length = 50)
-    @TableField(value = "plugin_id")
-    private Long toolId;
-
-    public RoleToolEntity(Long roleId, Long toolId) {
-        this.roleId = roleId;
-        this.toolId = toolId;
-    }
-}
+//package com.alinesno.infra.smart.assistant.entity;
+//
+//import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+//import com.baomidou.mybatisplus.annotation.TableField;
+//import com.baomidou.mybatisplus.annotation.TableName;
+//import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+//import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+//import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.Entity;
+//
+//@EqualsAndHashCode(callSuper = true)
+//@Data
+//@Entity
+//@NoArgsConstructor
+//@TableName("role_tool")
+//@TableComment("角色工具信息")
+//public class RoleToolEntity extends InfraBaseEntity {
+//
+//    // 角色信息、插件信息
+//    @ColumnComment("角色ID")
+//    @Column(length = 50)
+//    @TableField(value = "role_id")
+//    private Long roleId;
+//
+//    @ColumnComment("插件ID")
+//    @Column(length = 50)
+//    @TableField(value = "plugin_id")
+//    private Long toolId;
+//
+//    public RoleToolEntity(Long roleId, Long toolId) {
+//        this.roleId = roleId;
+//        this.toolId = toolId;
+//    }
+//}
