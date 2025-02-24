ed63392889e4250842305920ffafab99b28441a7
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:11 +0800

    feat:新增 IToolTypeService 接口，定义应用构建服务的基本操作。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/service/IToolTypeService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.ToolTypeEntity;
+
+/**
+ * 应用构建Service接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface IToolTypeService extends IBaseService<ToolTypeEntity> {
+
+
+}
