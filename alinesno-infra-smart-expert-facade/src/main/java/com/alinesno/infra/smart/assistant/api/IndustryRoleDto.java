895fb378001a1ff04e50005c08e9c5bfa811369a
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:12 +0800

    feat:新增 IndustryRoleDto 类，包含角色详细信息及属性

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/IndustryRoleDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/IndustryRoleDto.java
@@ -1,39 +1,93 @@
 package com.alinesno.infra.smart.assistant.api;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.alinesno.infra.smart.assistant.api.config.*;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
+import java.util.List;
+
+/**
+ * 角色数据传输对象
+ */
 @Data
 public class IndustryRoleDto {
 
-    private Long id ;
-
-    private String roleName; // 角色名称
-
-    private String roleAvatar ; // 角色头像
-
-    private Long industryCatalog ; // 所属行业
-
+    private Long id;
+    private Long industryCatalog; // 所属行业
     private String responsibilities; // 角色职责描述
-
     private String skills; // 所需技能描述
-
     private String roleLevel; // 角色等级
-
-    private String chatCount; // 会话次数
-
     private String roleType; //  角色类型，用于区分不同的角色类别
+    private ModelConfig modelConfig; // 模型配置相关信息
+    private List<String> knowledgeBaseIds; // 知识库ID列表，用于关联相关知识数据
+    private List<String> selectionToolsData; // 选择工具数据列表，可能用于特定操作或查询的工具标识
+    private boolean longTermMemoryEnabled; // 是否启用长期记忆功能
+    private boolean voiceInputStatus; // 是否启用语音输入状态
+    private boolean guessWhatYouAskStatus; // 是否用户问题建议
+    private boolean voicePlayStatus; // 语音播放
+    private VoiceInputData voiceInputData; // 语音输入相关数据配置
+    private VoicePlayData voicePlayData; // 语音播放
+    private GuessWhatYouAskData guessWhatYouAskData; // 猜测用户问题相关数据配置
+    private DatasetSearchConfig datasetSearchConfig ;   // 数据集搜索配置
+    private String roleAvatar; // 角色头像
+    private String roleName; // 角色名称
+    private String backstory; // 角色背景
+    private String chainId; // 其他角色相关字段
+    private String greeting; // 开场白
+    private boolean askHumanHelp = false; // 是否需要人类帮助
+    private String scriptType; // 执行类型(脚本script|流程flow|默认default)
+    private String otherAttributes; // 其他角色相关字段
+    private String promptContent; // 指令内容
+    private Long chatCount = 0L; // 会话次数
+    private boolean hasRecommended; // 推荐角色(用于Hero推荐)
+    private int hasSale = 0; // 是否销售(1销售|0不可销售|9不可转售)
+    private Long saleFromRoleId = 0L; // 销售来源角色ID
+    private String knowledgeId; // 知识库ID
+    private Long modelId; // 模型
+    private String executeScript; // 执行脚本
+    private String auditScript; // 审核脚本
+    private String functionCallbackScript; // 功能回调脚本
 
     // 假设存在一个静态方法 fromEntity，用于从 IndustryRoleEntity 转换为 IndustryRoleDto
     public static IndustryRoleDto fromEntity(IndustryRoleEntity entity) {
-        // 实现将 IndustryRoleEntity 转换为 IndustryRoleDto 的逻辑
-        // 例如，如果存在相应的构造函数或转换方法，则在此处使用它们
-
-        // 返回 IndustryRoleDto 对象
         IndustryRoleDto dto = new IndustryRoleDto();
-        BeanUtils.copyProperties(entity , dto);
+        BeanUtils.copyProperties(entity, dto);
 
-        return dto ;
+        // 处理特殊类型字段的转换
+        if (entity.getModelConfig() != null) {
+            dto.setModelConfig(JSONObject.parseObject(entity.getModelConfig(), ModelConfig.class));
+        }
+
+        if (entity.getKnowledgeBaseIds() != null) {
+            List<String> knowledgeBaseIds = JSONObject.parseObject(entity.getKnowledgeBaseIds(), new TypeReference<>() {});
+            dto.setKnowledgeBaseIds(knowledgeBaseIds);
+        }
+
+        if (entity.getSelectionToolsData() != null) {
+            List<String> knowledgeBaseIds = JSONObject.parseObject(entity.getSelectionToolsData(), new TypeReference<>() {});
+            dto.setSelectionToolsData(knowledgeBaseIds);
+        }
+
+        // 数据集配置
+        if (entity.getDatasetSearchConfig() != null) {
+            dto.setDatasetSearchConfig(JSONObject.parseObject(entity.getDatasetSearchConfig(), DatasetSearchConfig.class));
+        }
+
+        if (entity.getVoiceInputData() != null) {
+            dto.setVoiceInputData(JSONObject.parseObject(entity.getVoiceInputData(), VoiceInputData.class));
+        }
+
+        if (entity.getGuessWhatYouAskData() != null) {
+            dto.setGuessWhatYouAskData(JSONObject.parseObject(entity.getGuessWhatYouAskData(), GuessWhatYouAskData.class));
+        }
+
+        if (entity.getVoicePlayData() != null) {
+            dto.setVoicePlayData(JSONObject.parseObject(entity.getVoicePlayData(), VoicePlayData.class));
+        }
+
+        return dto;
     }
 }
