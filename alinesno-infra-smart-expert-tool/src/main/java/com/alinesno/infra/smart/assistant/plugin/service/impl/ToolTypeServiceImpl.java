cf94615259bce85fdb67f55bbea21aab928d5492
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:11 +0800

    feat:新增 ToolTypeServiceImpl 类，实现应用构建 Service 业务层处理

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolTypeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.smart.assistant.plugin.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.ToolTypeEntity;
+import com.alinesno.infra.smart.assistant.plugin.mapper.ToolTypeMapper;
+import com.alinesno.infra.smart.assistant.service.IToolTypeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 应用构建Service业务层处理
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Slf4j
+@Service
+public class ToolTypeServiceImpl extends IBaseServiceImpl<ToolTypeEntity, ToolTypeMapper> implements IToolTypeService {
+
+}
