90fe6b8e2e3f637fede809f646822b383a89eb9d
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:11 +0800

    feat:新增 VoicePlayData 类，包含语音模型名称及语音速度配置

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/VoicePlayData.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.api.config;
+
+import lombok.Data;
+
+/**
+     * 语音播放
+     */
+    @Data
+    public class VoicePlayData {
+        // 语音模型名称，可能用于语音播放
+        private String voiceModel;
+        // 语音速度，用于语音播放的速度
+        private int speechRate;
+    }
