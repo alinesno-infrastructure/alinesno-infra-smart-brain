94f774682501fb6bf23797bedaecd567b59fc1bb
王磊(Levi) <leviAip@gmail.com>
2025-02-25 00:15:11 +0800

    feat:添加 ToolTypeController 控制器类，处理工具类型相关请求

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/controller/ToolTypeController.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.smart.assistant.plugin.controller;
+
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.assistant.entity.ToolTypeEntity;
+import com.alinesno.infra.smart.assistant.service.IToolTypeService;
+import io.swagger.annotations.Api;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 应用构建Controller
+ * 处理与ApplicationEntity相关的请求
+ * 继承自BaseController类并实现IApplicationService接口
+ * 
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Slf4j
+@Api(tags = "Channel")
+@RestController
+@Scope("prototype")
+@RequestMapping("/api/infra/smart/assistant/toolType")
+public class ToolTypeController extends BaseController<ToolTypeEntity, IToolTypeService> {
+
+    @Autowired
+    private IToolTypeService service;
+
+    /**
+     * 获取ApplicationEntity的DataTables数据
+     * 
+     * @param request HttpServletRequest对象
+     * @param model Model对象
+     * @param page DatatablesPageBean对象
+     * @return 包含DataTables数据的TableDataInfo对象
+     */
+    @DataPermissionScope
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    @DataPermissionSave
+    @Override
+    public AjaxResult save(Model model, @RequestBody ToolTypeEntity entity) throws Exception {
+        return super.save(model, entity);
+    }
+
+    @Override
+    public IToolTypeService getFeign() {
+        return this.service;
+    }
+}
