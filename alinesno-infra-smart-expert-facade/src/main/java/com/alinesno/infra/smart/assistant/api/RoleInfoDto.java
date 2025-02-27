b05ee1e654e3e7b07dac27aac80b9aa19eb72280
张婷(Tina) <tinaAip@gmail.com>
2025-02-28 00:13:52 +0800

    feat:新增 RoleInfoDto 类，包含角色基本信息及校验规则

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/RoleInfoDto.java
@@ -0,0 +1,34 @@
+package com.alinesno.infra.smart.assistant.api;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 角色基础信息
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class RoleInfoDto extends BaseDto {
+
+    @NotBlank(message = "角色头像不能为空")
+    private String roleAvatar; // 角色头像
+
+    @NotBlank(message = "角色类型不能为空")
+    private String roleType;
+
+    @Min(value = 1, message = "行业分类不能为空")
+    private Long industryCatalog;
+
+    @NotBlank(message = "角色名称不能为空")
+    private String roleName; // 角色名称
+
+    @NotBlank(message = "角色背景不能为空")
+    private String responsibilities; // 角色背景
+
+    @NotBlank(message = "执行类型不能为空")
+    private String scriptType ;
+
+}
