3e8614117d2d8b34a6935ba5609533e843fbcdb6
陈伟(Victor) <victorAip@gmail.com>
2025-03-01 00:13:32 +0800

    feat:在 FlowCascader.vue 中添加数据绑定及计算属性处理

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/common/FlowCascader.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/common/FlowCascader.vue
@@ -1,13 +1,13 @@
 <template>
-    <div style="width:100%" @mouseover="handleMouseOver">
+    <div style="width:100%">
         <el-cascader
             style="width:100%"
-            v-model="value"
+            v-model="data"
             :options="options"
             :props="{ expandTrigger: 'hover'}"
-            @change="handleChange"
             placeholder="请选择内容"
             />
+            <!-- @change="handleChange" -->
     </div>
 </template>
 
@@ -23,6 +23,20 @@ const props = defineProps({
   }
 });
 
+const currentData = ref(null)
+
+const emit = defineEmits(['update:modelValue'])
+const data = computed({
+  set: (value) => {
+    console.log('data = ' + JSON.stringify(value))
+    currentData.value = value
+    emit('update:modelValue', value)
+  },
+  get: () => {
+    return props.modelValue
+  }
+})
+
 const upNodeListObj = ref(null); // 上游节点列表
 const options = ref([])  // 节点列表
 
@@ -36,7 +50,7 @@ const getUpNodeList = () =>{
       const upNodeList = props.nodeModel.graphModel.getNodeIncomingNode(nodeId);
 
       for (const upNode of upNodeList) {
-        console.log('upNode.properties = ' + JSON.stringify(upNode.properties))
+
         if (!visited.has(upNode.id)) {
             visited.add(upNode.id);
             result.push(upNode);
@@ -65,7 +79,10 @@ const convertToOptions = (nodeList) => {
         };
         options.unshift(option);
 
-        if (item.properties?.globalFields && item.type === 'start') {
+        // console.log('type = '+item.type+' item.properties = ' + JSON.stringify(item.properties))
+        console.log(item.properties.config.globalFields);
+
+        if (item.properties?.config?.globalFields && item.type === 'start') {
             firstElement = {
                 value: 'global',
                 label: '全局变量', // 这里需要根据 t 函数替换，这里先写死
@@ -82,153 +99,29 @@ const convertToOptions = (nodeList) => {
     return options;
 };
 
-const handleMouseOver = () =>{
-    upNodeListObj.value = getUpNodeList();
-    console.log('function mouseOver = ' + props.nodeModel.id + ' , upNodeList = ' + upNodeListObj.value)
-    options.value = convertToOptions(upNodeListObj.value);
-    console.log('options = ' + JSON.stringify(options.value))
+const nodeIncomingNodeRefresh = () =>{
+  upNodeListObj.value = getUpNodeList();
+  options.value = convertToOptions(upNodeListObj.value);
 }
 
+// const handleMouseOver = () => {
+//   console.log('handleMouseOver , currentData = ' + currentData.value);
+//   if(!currentData.value){
+//     nodeIncomingNodeRefresh();
+//   }
+// };
 
-//   {
-//     value: 'guide',
-//     label: 'Guide',
-//     children: [
-//       {
-//         value: 'disciplines',
-//         label: 'Disciplines',
-//         children: [
-//           {
-//             value: 'consistency',
-//             label: 'Consistency',
-//           },
-//           {
-//             value: 'controllability',
-//             label: 'Controllability',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'navigation',
-//         label: 'Navigation',
-//         children: [
-//           {
-//             value: 'side nav',
-//             label: 'Side Navigation',
-//           },
-//           {
-//             value: 'top nav',
-//             label: 'Top Navigation',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     value: 'component',
-//     label: 'Component',
-//     children: [
-//       {
-//         value: 'basic',
-//         label: 'Basic',
-//         children: [
-//           {
-//             value: 'layout',
-//             label: 'Layout',
-//           },
-//           {
-//             value: 'button',
-//             label: 'Button',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'form',
-//         label: 'Form',
-//         children: [
-//           {
-//             value: 'radio',
-//             label: 'Radio',
-//           },
-//           {
-//             value: 'form',
-//             label: 'Form',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'data',
-//         label: 'Data',
-//         children: [
-//           {
-//             value: 'table',
-//             label: 'Table',
-//           },
-//           {
-//             value: 'badge',
-//             label: 'Badge',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'notice',
-//         label: 'Notice',
-//         children: [
-//           {
-//             value: 'alert',
-//             label: 'Alert',
-//           },
-//           {
-//             value: 'notification',
-//             label: 'Notification',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'navigation',
-//         label: 'Navigation',
-//         children: [
-//           {
-//             value: 'menu',
-//             label: 'Menu',
-//           },
-//           {
-//             value: 'steps',
-//             label: 'Steps',
-//           },
-//         ],
-//       },
-//       {
-//         value: 'others',
-//         label: 'Others',
-//         children: [
-//           {
-//             value: 'dialog',
-//             label: 'Dialog',
-//           },
-//           {
-//             value: 'collapse',
-//             label: 'Collapse',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-//   {
-//     value: 'resource',
-//     label: 'Resource',
-//     children: [
-//       {
-//         value: 'axure',
-//         label: 'Axure Components',
-//       },
-//       {
-//         value: 'docs',
-//         label: 'Design Documentation',
-//       },
-//     ],
-//   },
-// ]
+onMounted(() => {
+
+  props.nodeModel.graphModel.eventCenter.on('element:click', () => {
+    if(!currentData.value){
+      nodeIncomingNodeRefresh();
+    }
+  })
+
+})
+
+defineExpose({ nodeIncomingNodeRefresh });
 
 </script>
 
