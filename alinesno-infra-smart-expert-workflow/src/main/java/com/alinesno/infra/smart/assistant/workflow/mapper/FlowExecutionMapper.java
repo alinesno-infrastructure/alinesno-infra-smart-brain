392b6f60e63cda7a36174ee882a377df203d0783
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:56 +0800

    feat:新增 FlowExecutionMapper 接口，用于工作流执行表的数据访问操作。

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/mapper/FlowExecutionMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.workflow.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.workflow.entity.FlowExecutionEntity;
+
+/**
+ * 工作流执行表 Mapper 接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface FlowExecutionMapper extends IBaseMapper<FlowExecutionEntity> {
+}
