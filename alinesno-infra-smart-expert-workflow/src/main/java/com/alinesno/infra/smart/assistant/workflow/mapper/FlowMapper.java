cc56e0515910fc7f7b555796698f43bc1c9bc8e8
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:57 +0800

    feat:新增 FlowMapper 接口，定义工作流表的 Mapper 接口

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/mapper/FlowMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.workflow.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.workflow.entity.FlowEntity;
+
+/**
+ * 工作流表 Mapper 接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface FlowMapper extends IBaseMapper<FlowEntity> {
+}
