e7e1773f82a0ab3be08d8ca25eadc9520d335934
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:56 +0800

    feat:新增 FlowNode 接口，定义工作流节点的通用规范

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/FlowNode.java
@@ -0,0 +1,12 @@
+// WorkflowNode.java
+package com.alinesno.infra.smart.assistant.workflow.nodes;
+
+/**
+ * 这是一个统一的接口类，定义了工作流节点的通用规范。
+ * 所有具体的工作流节点类都应该实现这个接口，以确保它们具备工作流节点的基本特性。
+ * 目前该接口可以作为所有工作流节点类的标识，后续可以在此接口中定义所有节点通用的方法。
+ */
+public interface FlowNode {
+    // 可以在这里定义所有节点通用的方法
+
+}
