44339457d025dd7e542a92a56ce1f06b425128e0
陈伟(Victor) <victorAip@gmail.com>
2025-03-02 00:12:56 +0800

    feat:新增 flow.js 文件，包含流程操作相关接口

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/flow.js
@@ -0,0 +1,60 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 流程操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/flow/' ;
+var managerUrl = {
+    processAndSave: prefix + "processAndSave",  // 流程保存和解析
+    publishFlow: prefix + "publish",  // 发布工作流
+    getLatestPublishedFlow: prefix + "latestPublished",  // 获取最新版本已发布流程
+    getUnpublishedFlow: prefix + "unpublished",  // 获取未发布流程
+    getLatestFlow: prefix + "latest"  // 获取最新流程
+}
+
+// 流程保存和解析
+export function processAndSave(data, roleId) {
+    return request({
+        url: managerUrl.processAndSave + "?roleId=" + parseStrEmpty(roleId),
+        method: 'post',
+        data: data
+    })
+}
+
+// 发布工作流
+export function publishFlow(roleId) {
+    return request({
+        url: managerUrl.publishFlow + "?roleId=" + parseStrEmpty(roleId),
+        method: 'post'
+    })
+}
+
+// 获取最新流程
+export function getLatestFlow(roleId) {
+    return request({
+        url: managerUrl.getLatestFlow+ "?roleId=" + parseStrEmpty(roleId),
+        method: 'get'
+    })
+}
+
+// 获取最新版本的已发布流程
+export function getLatestPublishedFlow(roleId) {
+    return request({
+        url: managerUrl.getLatestPublishedFlow + "?roleId=" + parseStrEmpty(roleId),
+        method: 'get'
+    })
+}
+
+// 获取指定角色的未发布流程
+export function getUnpublishedFlow(roleId) {
+    return request({
+        url: managerUrl.getUnpublishedFlow + "?roleId=" + parseStrEmpty(roleId),
+        method: 'get'
+    })
+}
