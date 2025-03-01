bdaa13fd914d4b14ec5bdc3f924738ddac594688
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:57 +0800

    feat:新增 ImageUnderstandNode 类，实现图片内容理解功能。

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/ImageUnderstandNode.java
@@ -0,0 +1,23 @@
+// ImageUnderstandNode.java
+package com.alinesno.infra.smart.assistant.workflow.nodes.step;
+
+import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 该类表示图片理解节点，继承自 AbstractFlowNode 类。
+ * 主要功能是识别出图片中的对象、场景等信息，并根据这些信息回答用户问题。
+ * 在工作流中，当需要对图片进行内容理解时会使用该节点。
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ImageUnderstandNode extends AbstractFlowNode {
+
+    /**
+     * 构造函数，初始化节点类型为 "image_understand"。
+     */
+    public ImageUnderstandNode() {
+        setType("image_understand");
+    }
+}
