fa1569d741573f1f8e696312777cfcb4c2016648
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:57 +0800

    feat:新增 FlowNodeExecutionMapper 接口

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/mapper/FlowNodeExecutionMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.workflow.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.workflow.entity.FlowNodeExecutionEntity;
+
+/**
+ * 工作流节点执行表 Mapper 接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+public interface FlowNodeExecutionMapper extends IBaseMapper<FlowNodeExecutionEntity> {
+}
