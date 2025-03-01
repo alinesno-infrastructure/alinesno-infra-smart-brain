df8a984a36454d1453a6bdc6e3ca3056e5a1de16
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:57 +0800

    feat:新增 FlowNodeExecutionEntity 类，用于记录工作流节点执行情况

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/entity/FlowNodeExecutionEntity.java
@@ -0,0 +1,67 @@
+package com.alinesno.infra.smart.assistant.workflow.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 工作流节点执行表实体类，记录每个节点执行的情况
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("flow_node_executions")
+@TableComment(value = "工作流节点执行表")
+public class FlowNodeExecutionEntity extends InfraBaseEntity {
+
+    /**
+     * 所属工作流执行实例的 ID
+     */
+    @TableField(value = "flow_execution_id")
+    @ColumnType(value = MySqlTypeConstant.BIGINT)
+    @ColumnComment("所属工作流执行实例的 ID")
+    private Long flowExecutionId;
+
+    /**
+     * 节点 ID
+     */
+    @TableField(value = "node_id")
+    @ColumnType(value = MySqlTypeConstant.BIGINT)
+    @ColumnComment("节点 ID")
+    private Long nodeId;
+
+    /**
+     * 执行状态
+     */
+    @TableField(value = "execution_status")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
+    @ColumnComment("执行状态")
+    private String executionStatus;
+
+    /**
+     * 开始时间
+     */
+    @TableField(value = "start_time")
+    @ColumnType(value = MySqlTypeConstant.DATETIME)
+    @ColumnComment("开始时间")
+    private Date startTime;
+
+    /**
+     * 结束时间
+     */
+    @TableField(value = "end_time")
+    @ColumnType(value = MySqlTypeConstant.DATETIME)
+    @ColumnComment("结束时间")
+    private Date endTime;
+}
