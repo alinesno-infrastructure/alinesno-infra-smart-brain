84f1a2455e0d372ae310174e2e6963eab2279441
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:56 +0800

    feat:新增 RerankerNode 类，实现多路召回功能。

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/RerankerNode.java
@@ -0,0 +1,23 @@
+// RerankerNode.java
+package com.alinesno.infra.smart.assistant.workflow.nodes.step;
+
+import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 该类表示多路召回节点，继承自 AbstractFlowNode 类。
+ * 其作用是使用重排模型对多个知识库的检索结果进行二次召回，
+ * 以提高检索结果的准确性和相关性。在工作流中，当需要对初步检索结果进行优化时，会使用该节点。
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class RerankerNode extends AbstractFlowNode {
+
+    /**
+     * 构造函数，初始化节点类型为 "reranker"。
+     */
+    public RerankerNode() {
+        setType("reranker");
+    }
+}
