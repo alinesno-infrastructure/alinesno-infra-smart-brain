322cceea624fdd43885e24fe757a1cc7624ad2aa
周健(James) <jamesAip@gmail.com>
2025-03-02 00:12:56 +0800

    feat:新增 DocumentExtractNode 类，用于文档内容提取

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/DocumentExtractNode.java
@@ -0,0 +1,23 @@
+// DocumentExtractNode.java
+package com.alinesno.infra.smart.assistant.workflow.nodes.step;
+
+import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 该类表示文档内容提取节点，继承自 AbstractFlowNode 类。
+ * 用于从文档中提取所需的内容，如文本、数据等。
+ * 在工作流中，当需要对文档进行内容解析和提取时，会使用该节点。
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class DocumentExtractNode extends AbstractFlowNode {
+
+    /**
+     * 构造函数，初始化节点类型为 "document_extract"。
+     */
+    public DocumentExtractNode() {
+        setType("document_extract");
+    }
+}
