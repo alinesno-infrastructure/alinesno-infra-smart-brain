06aa9abf29b71d5ce8d0fe99739b5baf08bf5f02
张婷(Tina) <tinaAip@gmail.com>
2025-03-01 08:17:19 +0800

    feat:更新 question-node 组件，优化表单字段绑定及交互

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/question-node/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/question-node/index.vue
@@ -16,28 +16,33 @@
       <div class="settings-title">节点设置</div>
       <!-- 节点设置表单区域 -->
       <div class="settings-form">
-        <el-form :model="form" label-width="auto" label-position="top">
+        <el-form :model="formData" label-width="auto" label-position="top">
           <el-form-item label="AI模型">
-            <!-- <el-select v-model="value" placeholder="请选择AI模型" style="width: 240px">
-              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
-            </el-select> -->
-            <LLMSelector :nodeModel="props.nodeModel" />
+            <LLMSelector :nodeModel="props.nodeModel" v-model="formData.llmModelId" />
           </el-form-item>
-          <el-form-item label="系统角色">
-            <el-input type="textarea" :rows="3" resize="none" placeholder="角色设置" />
-          </el-form-item>
-          <el-form-item label="提示词">
-            <el-input type="textarea" :rows="4" resize="none" placeholder="角色提示词" />
+          <el-form-item label="角色提示词">
+            <!-- <el-input type="textarea" :rows="4" resize="none" placeholder="角色提示词" /> -->
+
+            <div class="function-CodemirrorEditor mb-8" style="height: 120px;width:100%">
+              <ScriptEditorPanel ref="auditEditorRef" :lang="'markdown'" />
+              <div class="function-CodemirrorEditor__footer">
+                <el-button text @click="openCodemirrorDialog" style="background-color: transparent !important;" class="magnify">
+                  <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                </el-button>
+              </div>
+            </div>
+
           </el-form-item>
           <el-form-item label="历史聊天记录">
-            <el-input-number v-model="num" :step="2" />
+            <el-input-number v-model="form.historyNumber" :step="1" />
           </el-form-item>
           <el-form-item label="返回内容">
-            <el-switch v-model="value1" size="small" />
+            <el-switch v-model="form.isPrint" size="small" />
           </el-form-item>
         </el-form>
       </div>
     </div>
+
     <!-- 输出参数部分 -->
     <div class="node-output">
       <!-- 输出参数标题 -->
@@ -47,11 +52,35 @@
         回答内容 {answer}
       </div>
     </div>
+
+    <el-dialog
+      v-model="dialogVisible"
+      :title="'函数内容（Groovy）'"
+      append-to-body
+      width="800"
+    >
+      <template #header>
+        <div class="dialog-footer mt-24" style="display: flex;align-items: center; justify-content: space-between; ">
+          <div>
+            提示词
+          </div>
+          <div>
+            <el-button type="primary" size="large" text bg @click="confirmAndSave"> 确定保存 </el-button>
+          </div>
+        </div>
+      </template>
+      <ScriptEditorFullPanel ref="auditFullEditorRef" :lang="'markdown'" />
+    </el-dialog>
+
   </div>
 </template>
 
 <script setup>
+import { set } from 'lodash'
 import { ref, reactive } from 'vue'
+
+import ScriptEditorPanel from '@/views/smart/assistant/workflow/components/ScriptEditor';
+import ScriptEditorFullPanel from '@/views/smart/assistant/workflow/components/NodeScriptEditor';
 
 import LLMSelector from '@/views/smart/assistant/workflow/components/LLMSelector'
 
@@ -70,46 +99,96 @@ const props = defineProps({
 });
 
 // 绑定选择框的值
-const value = ref('')
+const auditEditorRef = ref(null)
+const auditFullEditorRef = ref(null)
+
+const chatDataCode = ref('')
 const value1 = ref('')
 const num = ref('1')
 
+const dialogVisible = ref(false)
+
 // 表单数据对象
 const form = reactive({
-  name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
-  desc: '',
+  llmModelId: '' , 
+  prompt: '' , 
+  historyNumber: 2,
+  isPrint: true 
 })
 
+const formData = computed({
+  get: () => {
+    if (props.nodeModel.properties.node_data) {
+      return props.nodeModel.properties.node_data
+    } else {
+      set(props.nodeModel.properties, 'node_data', form)
+    }
+    return props.nodeModel.properties.node_data
+  },
+  set: (value) => {
+    set(props.nodeModel.properties, 'node_data', value)
+  }
+})
+
+// 确认保存
+function confirmAndSave() {
+  console.log('confirmAndSave')
+
+  chatDataCode.value = auditFullEditorRef.value.getRawScript()
+  auditEditorRef.value.setRawScript(chatDataCode.value)
+
+  console.log('auditFullEditorRef.value.getRawScript() = ' + auditFullEditorRef.value.getRawScript())
+  dialogVisible.value = false
+
+  // 配置
+  form.prompt = chatDataCode.value
+}
+
+function openCodemirrorDialog() {
+
+  console.log('auditEditorRef.value.getRawScript() = ' + auditEditorRef.value.getRawScript());
+
+  chatDataCode.value = auditEditorRef.value.getRawScript();
+  console.log('chatDatacode = ' + chatDataCode.value)
+  dialogVisible.value = true
+
+  nextTick(() => {
+    auditFullEditorRef.value.setRawScript(chatDataCode.value)
+  })
+}
+
 // 选择框的选项列表
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
-  },
-  {
-    value: 'Option2',
-    label: 'Option2',
-  },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-]
+// const options = [
+//   {
+//     value: 'Option1',
+//     label: 'Option1',
+//   },
+//   {
+//     value: 'Option2',
+//     label: 'Option2',
+//   },
+//   {
+//     value: 'Option3',
+//     label: 'Option3',
+//   },
+//   {
+//     value: 'Option4',
+//     label: 'Option4',
+//   },
+//   {
+//     value: 'Option5',
+//     label: 'Option5',
+//   },
+// ]
+
 </script>
 
 <style lang="scss" scoped>
+
+.function-CodemirrorEditor__footer {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+}
+
 </style>
