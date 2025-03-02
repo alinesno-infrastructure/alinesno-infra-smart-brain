f7e6b4d35783ae330d9b00d38d0363833ccb14bd
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:57 +0800

    feat:新增 KnowledgeSearchNode 类，支持从知识库检索信息，并添加 Spring 相关注解。

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/KnowledgeSearchNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/KnowledgeSearchNode.java
@@ -1,9 +1,12 @@
 // KnowledgeSearchNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * 该类表示知识库检索节点，继承自 AbstractFlowNode 类。
@@ -11,6 +14,8 @@ import lombok.EqualsAndHashCode;
  * 在工作流中，当需要从知识库中获取相关信息时，会使用该节点。
  */
 @Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "knowledge_search")
 @EqualsAndHashCode(callSuper = true)
 public class KnowledgeSearchNode extends AbstractFlowNode {
 
