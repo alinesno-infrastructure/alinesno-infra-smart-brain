378bd1a34c66daac426e6122bd509f09d5d2e3c7
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:57 +0800

    feat:AiChatNode 类增加 Spring 注解及常量绑定

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/AiChatNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/AiChatNode.java
@@ -1,9 +1,12 @@
 // AiChatNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * 该类表示 AI 对话节点，继承自 AbstractFlowNode 类。
@@ -11,6 +14,8 @@ import lombok.EqualsAndHashCode;
  * 当工作流执行到该节点时，会触发与 AI 大模型的交互过程。
  */
 @Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "ai_chat")
 @EqualsAndHashCode(callSuper = true)
 public class AiChatNode extends AbstractFlowNode {
 
