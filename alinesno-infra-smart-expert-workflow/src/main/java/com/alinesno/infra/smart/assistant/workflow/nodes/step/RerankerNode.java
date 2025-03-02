7f1640dc923cb1ad4960eb2e8fa56bdb69f4dbc6
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:57 +0800

    feat:添加 RerankerNode 类，并实现Spring注解配置

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/RerankerNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/RerankerNode.java
@@ -1,9 +1,12 @@
 // RerankerNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * 该类表示多路召回节点，继承自 AbstractFlowNode 类。
@@ -11,6 +14,8 @@ import lombok.EqualsAndHashCode;
  * 以提高检索结果的准确性和相关性。在工作流中，当需要对初步检索结果进行优化时，会使用该节点。
  */
 @Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "reranker")
 @EqualsAndHashCode(callSuper = true)
 public class RerankerNode extends AbstractFlowNode {
 
