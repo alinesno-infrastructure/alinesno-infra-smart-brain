96bf37363c990f4b2e4ced0d992f6ef8ce92d882
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:57 +0800

    feat:新增 DocumentExtractNode 类，并添加 Spring 相关注解

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/DocumentExtractNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/DocumentExtractNode.java
@@ -1,9 +1,12 @@
 // DocumentExtractNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * 该类表示文档内容提取节点，继承自 AbstractFlowNode 类。
@@ -11,6 +14,8 @@ import lombok.EqualsAndHashCode;
  * 在工作流中，当需要对文档进行内容解析和提取时，会使用该节点。
  */
 @Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "document_extract")
 @EqualsAndHashCode(callSuper = true)
 public class DocumentExtractNode extends AbstractFlowNode {
 
