fac6a4edd0ec261dc04e58006624c5ab9564aac6
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:57 +0800

    feat:新增HttpApiNode类，实现HTTP API节点功能。

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/HttpApiNode.java
@@ -0,0 +1,26 @@
+// ReplyNode.java
+package com.alinesno.infra.smart.assistant.workflow.nodes.step;
+
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
+import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+/**
+ * HttpApiNode类，继承自AbstractFlowNode，表示一个HTTP API节点。
+ */
+@Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "http_api")
+@EqualsAndHashCode(callSuper = true)
+public class HttpApiNode extends AbstractFlowNode {
+
+    /**
+     * 构造函数，初始化节点类型为 "reply"。
+     */
+    public HttpApiNode() {
+        setType("http_api");
+    }
+}
