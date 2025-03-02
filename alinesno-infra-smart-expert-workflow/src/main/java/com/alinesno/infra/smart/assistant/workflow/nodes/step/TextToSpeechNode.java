9c0c344d8e9556eb74699aff5ae4efaa207377c3
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:58 +0800

    feat:在 TextToSpeechNode 类中添加 Spring 相关注解和服务注册

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/TextToSpeechNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/TextToSpeechNode.java
@@ -1,9 +1,12 @@
 // TextToSpeechNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 /**
  * 该类表示文本转语音节点，继承自 AbstractFlowNode 类。
@@ -11,6 +14,8 @@ import lombok.EqualsAndHashCode;
  * 在工作流中，当需要将文本信息以语音形式输出时，会使用该节点。
  */
 @Data
+@Scope("prototype")
+@Service(FlowConst.FLOW_STEP_NODE + "text_to_speech")
 @EqualsAndHashCode(callSuper = true)
 public class TextToSpeechNode extends AbstractFlowNode {
 
