54bf67a22289163dca12ff4aecdcd79fe65d2f39
李伟(Michael) <michaelAip@gmail.com>
2025-03-03 00:11:58 +0800

    feat:在 AbstractFlowNode 中添加 executeNode 方法及日志记录功能

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/AbstractFlowNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/AbstractFlowNode.java
@@ -1,7 +1,11 @@
 // AbstractWorkflowNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes;
 
+import com.alinesno.infra.smart.assistant.workflow.dto.FlowNodeDto;
+import com.alinesno.infra.smart.assistant.workflow.entity.FlowExecutionEntity;
+import com.alinesno.infra.smart.assistant.workflow.entity.FlowNodeExecutionEntity;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +16,7 @@ import java.util.Map;
  * 由于它是抽象类，不能直接实例化，具体的工作流节点类需要继承该类并实现必要的逻辑。
  * 使用 Lombok 的 @Data 注解自动生成 getter、setter、toString、equals 和 hashCode 方法。
  */
+@Slf4j
 @Data
 public abstract class AbstractFlowNode implements FlowNode {
 
@@ -36,4 +41,19 @@ public abstract class AbstractFlowNode implements FlowNode {
     // 节点属性集合，用于存储节点的额外属性信息，如颜色、大小等
     private Map<String, Object> properties = new HashMap<>();
 
+    /**
+     * 执行节点任务
+     *
+     * @param node
+     * @param flowExecution
+     * @param flowNodeExecution
+     * @param output
+     */
+    public void executeNode(FlowNodeDto node, FlowExecutionEntity flowExecution , FlowNodeExecutionEntity flowNodeExecution, Map<String, Object> output) {
+       log.debug("执行节点任务:{} , node:{}" , this.getType() , node.getProperties());
+       output.put(node.getStepName() , node.getType()) ;
+
+       // 输出output
+        log.debug("--->>> node.getStepName = {} , 输出output:{}" , node.getStepName() , output);
+    }
 }
