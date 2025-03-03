758c12df7d79a1d902f58b3e237e34d0944e5d90
周健(James) <jamesAip@gmail.com>
2025-03-04 00:16:09 +0800

    feat:在 FunctionNode 类中添加日志记录功能

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/FunctionNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/FunctionNode.java
@@ -3,8 +3,8 @@ package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
  * 允许使用 Groovy 脚本进行编辑开发，以实现一些自定义的功能和逻辑。
  * 在工作流中，当需要执行特定的脚本逻辑时，会使用该节点。
  */
-@Data
+@Slf4j
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "function")
 @EqualsAndHashCode(callSuper = true)
@@ -24,5 +24,10 @@ public class FunctionNode extends AbstractFlowNode {
      */
     public FunctionNode() {
         setType("function");
+    }
+
+    @Override
+    protected void handleNode() {
+        log.debug( "node type = {} output = {}" , node.getType() , output) ;
     }
 }
