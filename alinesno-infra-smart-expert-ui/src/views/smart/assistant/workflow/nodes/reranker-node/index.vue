983a1bfdae4994c36bbfad937b3eb752fed5eb93
张婷(Tina) <tinaAip@gmail.com>
2025-03-04 00:16:09 +0800

    feat:将 reranker-node 组件重构为使用 FlowContainer 组件

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/reranker-node/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/reranker-node/index.vue
@@ -1,15 +1,7 @@
 <template>
   <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
-  <div class="workflow-node-container">
-    <!-- 节点标题部分，包含图标和名称 -->
-    <div class="node-title">
-      <div class="node-icon">
-        <i :class="props.properties.icon"></i>
-      </div>
-      <div class="node-name">
-        {{ props.properties.stepName }}
-      </div>
-    </div>
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
+
     <!-- 节点设置部分 -->
     <div class="node-settings">
       <!-- 节点设置标题 -->
@@ -63,18 +55,6 @@
         </div>
       </div>
     </div>
-    <!-- 输出参数部分 -->
-    <div class="node-output">
-      <!-- 输出参数标题 -->
-      <div class="output-title">输出参数</div>
-      <!-- 输出参数内容 -->
-      <div class="output-content">
-        重排结果列表 {result_list}
-      </div>
-      <div class="output-content">
-        重排结果{result}
-      </div>
-    </div>
 
     <el-dialog title="重排配置" v-model="datasetParamsConfigDialogVisible" width="600px" append-to-body>
         <div style="margin-bottom:30px">
@@ -82,13 +62,14 @@
         </div>
     </el-dialog>
 
-  </div>
+  </FlowContainer>
 </template>
 
 <script setup>
 import { set } from 'lodash'
 import { ref, reactive } from 'vue'
 
+import FlowContainer from '@/views/smart/assistant/workflow/common/FlowContainer'
 import LLMSelector from '@/views/smart/assistant/workflow/components/LLMSelector'
 import FlowCascader from '@/views/smart/assistant/workflow/common/FlowCascader'
 
@@ -112,11 +93,6 @@ const datasetConfigDialogVisible = ref(false)
 const datasetParamsConfigDialogVisible = ref(false)
 
 const datasetParamsChoicePanelRef= ref(null)
-// const datasetChoicePanelRef = ref(null)
-
-// 绑定选择框的值
-// const value = ref('')
-// const value1 = ref('')
 
 // 表单数据对象
 const form = reactive({
@@ -144,30 +120,6 @@ const formData = computed({
   }
 })
 
-// 选择框的选项列表
-// const options = [
-//   {
-//     value: 'Option1',
-//     label: 'Option1',
-//   },
-//   {
-//     value: 'Option2',
-//     label: 'Option2',
-//   },
-//   {
-//     value: 'Option3',
-//     label: 'Option3',
-//   },
-//   {
-//     value: 'Option4',
-//     label: 'Option4',
-//   },
-//   {
-//     value: 'Option5',
-//     label: 'Option5',
-//   },
-// ]
-
 // 关闭数据集配置窗口
 const handleSelectDatasetParamsConfigClose = (formData) => {
     if (datasetParamsConfigDialogVisible.value) {
@@ -178,14 +130,13 @@ const handleSelectDatasetParamsConfigClose = (formData) => {
     }
 }
 
-// 关闭数据集配置窗口
-function handleSelectDatasetConfigClose(selectItem) {
-    if (datasetConfigDialogVisible.value) {
-        datasetConfigDialogVisible.value = false;
-        selectionDatasetData.value = selectItem ; // datasetChoicePanelRef.value.getSelectItemList();
-        // agentModelConfigForm.value.knowledgeBaseIds = selectionDatasetData.value;
-    }
-}
+// // 关闭数据集配置窗口
+// function handleSelectDatasetConfigClose(selectItem) {
+//     if (datasetConfigDialogVisible.value) {
+//         datasetConfigDialogVisible.value = false;
+//         selectionDatasetData.value = selectItem ; // datasetChoicePanelRef.value.getSelectItemList();
+//     }
+// }
 
 // 搜索配置
 const handleSearchSettings = () => {
