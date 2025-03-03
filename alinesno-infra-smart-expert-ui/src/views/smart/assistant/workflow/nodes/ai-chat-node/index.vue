b9faf4e1719dcf191720aff9c1f6d381f3be9565
张婷(Tina) <tinaAip@gmail.com>
2025-03-04 00:16:09 +0800

    refactor: 重构AI聊天节点组件，优化结构并移除冗余代码

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/ai-chat-node/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/ai-chat-node/index.vue
@@ -1,15 +1,7 @@
 <template>
   <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
-  <div class="workflow-node-container">
-    <!-- 节点标题部分，包含图标和名称 -->
-    <div class="node-title">
-      <div class="node-icon">
-        <i :class="props.properties.icon"></i>
-      </div>
-      <div class="node-name">
-        {{ props.properties.stepName }}
-      </div>
-    </div>
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
+
     <!-- 节点设置部分 -->
     <div class="node-settings">
       <!-- 节点设置标题 -->
@@ -39,18 +31,6 @@
         </el-form>
       </div>
     </div>
-    <!-- 输出参数部分 -->
-    <div class="node-output">
-      <!-- 输出参数标题 -->
-      <div class="output-title">输出参数</div>
-      <!-- 输出参数内容 -->
-      <div class="output-content">
-        回答内容 {answer}
-      </div>
-      <div class="output-content">
-        思考过程 {reasoning_content}
-      </div>
-    </div>
 
     <el-dialog
       v-model="dialogVisible"
@@ -70,13 +50,14 @@
       <ScriptEditorFullPanel ref="auditFullEditorRef" :lang="'markdown'" />
     </el-dialog>
 
-  </div>
+  </FlowContainer>
 </template>
 
 <script setup>
 import { set } from 'lodash'
 import { ref, reactive } from 'vue'
 
+import FlowContainer from '@/views/smart/assistant/workflow/common/FlowContainer'
 import ScriptEditorPanel from '@/views/smart/assistant/workflow/components/ScriptEditor';
 import ScriptEditorFullPanel from '@/views/smart/assistant/workflow/components/NodeScriptEditor';
 
