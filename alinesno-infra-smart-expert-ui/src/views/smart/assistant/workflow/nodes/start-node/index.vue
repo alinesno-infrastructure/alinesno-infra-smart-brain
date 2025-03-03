b567bcb6c125db672a5bb1dce9e0d9949b8fa60a
张婷(Tina) <tinaAip@gmail.com>
2025-03-04 00:16:09 +0800

    feat:重构启动节点组件，采用FlowContainer封装并优化全局变量和输出参数的显示逻辑

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/start-node/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/start-node/index.vue
@@ -1,44 +1,33 @@
 <template>
   <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
-  <div class="workflow-node-container">
-    <!-- 节点标题部分，包含图标和名称 -->
-    <div class="node-title">
-      <div class="node-icon">
-        <i :class="props.properties.icon"></i>
-      </div>
-      <div class="node-name">
-        {{ props.properties.stepName }}
-      </div>
-    </div>
-    <!-- 节点设置部分 -->
-    <div class="node-output">
-      <div class="output-title">全局变量</div>
-      <div class="output-content copy-coutput-var" v-for="item in props.properties.config.globalFields" :key="item.value">
-        {{ item.label }} {{ '{' + item.value + '}' }}
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
 
-        <el-button link @click="copyClick(`{{global.${item.value}}}`)" style="padding: 0">
-          <i class="fa-solid fa-clone copy-btn"></i>
-        </el-button>
+      <!-- 节点设置部分 -->
+      <div class="node-output">
+        <div class="output-title">全局变量</div>
+        <template v-for="(item , index) in props.properties.config.globalFields" :key="item.value">
+          <div class="output-content copy-coutput-var" 
+             @mouseenter="showCopyBtn = index"
+             @mouseleave="showCopyBtn = null"
+          >
+            {{ item.label }} {{ '{' + item.value + '}' }}
 
-      </div>
-    </div>
-    <!-- 输出参数部分 -->
-    <div class="node-output">
-      <div class="output-title">输出参数</div>
-      <div class="output-content copy-coutput-var" v-for="item in props.properties.config.fields" :key="item.value">
-        {{ item.label }} {{ '{' + item.value + '}' }}
+            <el-button v-if="showCopyBtn === index"  link @click="copyClick(`{{global.${item.value}}}`)" style="padding: 0">
+              <i class="fa-solid fa-clone copy-btn"></i>
+            </el-button>
 
-        <el-button link @click="copyClick(`{{global.${item.value}}}`)" style="padding: 0">
-          <i class="fa-solid fa-clone copy-btn"></i>
-        </el-button>
+          </div>
+        </template>
       </div>
-    </div>
-  </div>
+
+  </FlowContainer>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import FlowContainer from '@/views/smart/assistant/workflow/common/FlowContainer'
+import { copyClick } from '@/utils/clipboard'
 
+const showCopyBtn = ref(-1);
 const props = defineProps({
   properties: {
     type: Object,
@@ -53,47 +42,6 @@ const props = defineProps({
   }
 });
 
-// 绑定选择框的值
-const value = ref('')
-const value1 = ref('')
-const num = ref('1')
-
-// 表单数据对象
-const form = reactive({
-  name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  resource: '',
-  desc: '',
-})
-
-// 选择框的选项列表
-const options = [
-  {
-    value: 'Option1',
-    label: 'Option1',
-  },
-  {
-    value: 'Option2',
-    label: 'Option2',
-  },
-  {
-    value: 'Option3',
-    label: 'Option3',
-  },
-  {
-    value: 'Option4',
-    label: 'Option4',
-  },
-  {
-    value: 'Option5',
-    label: 'Option5',
-  },
-]
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
