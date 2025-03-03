6543f046b0fd9a04981ebb8b09fa8eeb4489e446
张婷(Tina) <tinaAip@gmail.com>
2025-03-04 00:16:09 +0800

    feat:在 function-node 组件中引入 FlowContainer 并重构布局

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/function-node/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/function-node/index.vue
@@ -1,15 +1,7 @@
 <template>
   <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
-  <div class="workflow-node-container">
-    <!-- 节点标题部分，包含图标和名称 -->
-    <div class="node-title">
-      <div class="node-icon">
-        <i :class="props.properties.icon"></i>
-      </div>
-      <div class="node-name">
-        {{ props.properties.stepName }}
-      </div>
-    </div>
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
+
     <!-- 节点设置部分 -->
     <div class="node-settings">
       <!-- 节点设置标题 -->
@@ -80,17 +72,6 @@
           </el-form-item>
         </el-form>
       </div>
-
-    </div>
-
-    <!-- 输出参数部分 -->
-    <div class="node-output">
-      <!-- 输出参数标题 -->
-      <div class="output-title">输出参数</div>
-      <!-- 输出参数内容 -->
-      <div class="output-content">
-        结果 {result} <br /> {{ props.upNodeList }}
-      </div>
     </div>
 
     <el-dialog v-model="dialogVisible" :title="'函数内容（Groovy）'" append-to-body fullscreen>
@@ -132,12 +113,15 @@
         </span>
       </template>
     </el-dialog>
-  </div>
+
+  </FlowContainer>
 </template>
 
 <script setup>
 import { set } from 'lodash'
 import { ref, reactive, onMounted, nextTick, computed } from 'vue'
+
+import FlowContainer from '@/views/smart/assistant/workflow/common/FlowContainer'
 import ScriptEditorPanel from '@/views/smart/assistant/workflow/components/ScriptEditor'
 import ScriptEditorFullPanel from '@/views/smart/assistant/workflow/components/ScriptEditorFull'
 import FlowCascader from '@/views/smart/assistant/workflow/common/FlowCascader'
@@ -238,32 +222,9 @@ function submitDialog() {
 
 <style lang="scss" scoped>
 .workflow-node-container {
-  padding: 16px;
-  border: 1px solid #e4e7ed;
-  border-radius: 4px;
-  background-color: #fff;
-
-  .node-title {
-    display: flex;
-    align-items: center;
-    margin-bottom: 16px;
-
-    .node-icon {
-      margin-right: 8px;
-    }
-
-    .node-name {
-      font-size: 16px;
-      font-weight: 600;
-    }
-  }
 
   .node-settings {
-    margin-bottom: 16px;
-
-    .settings-title {
-      margin-bottom: 8px;
-    }
+    margin-bottom: 10px;
 
     .empty-params{
       font-size: 13px;
@@ -281,31 +242,6 @@ function submitDialog() {
       margin-bottom: 3px;
     }
 
-    .settings-form {
-      .form-params-item {
-        margin-bottom: 0px;
-        border-radius: 4px;
-        padding: 8px 0px;
-        transition: border-color 0.3s;
-
-        &:hover {
-          border-color: #409eff;
-        }
-      }
-    }
-  }
-
-  .node-output {
-    .output-title {
-      font-size: 14px;
-      font-weight: 600;
-      margin-bottom: 8px;
-    }
-
-    .output-content {
-      font-size: 13px;
-      color: #606266;
-    }
   }
 
   .function-CodemirrorEditor {
