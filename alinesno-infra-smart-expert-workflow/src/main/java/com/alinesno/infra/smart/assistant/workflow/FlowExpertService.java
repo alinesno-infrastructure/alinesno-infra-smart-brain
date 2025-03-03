f084d3f5d8192aeb9e1a11fed4b2bed3367571dc
周健(James) <jamesAip@gmail.com>
2025-03-04 00:16:09 +0800

    feat:新增 FlowExpertService 类，实现流程节点编排功能。

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/FlowExpertService.java
@@ -0,0 +1,57 @@
+package com.alinesno.infra.smart.assistant.workflow;
+
+import com.alinesno.infra.smart.assistant.api.CodeContent;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.enums.AssistantConstants;
+import com.alinesno.infra.smart.assistant.role.ExpertService;
+import com.alinesno.infra.smart.assistant.workflow.service.IFlowService;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 流程节点编排引擎，执行多个流程节点并返回结果任务
+ */
+@Scope("prototype")
+@Slf4j
+@Service(AssistantConstants.PREFIX_ASSISTANT_FLOW)
+public class FlowExpertService extends ExpertService {
+
+    @Autowired
+    private IFlowService flowService ;
+
+    @Override
+    protected String handleRole(IndustryRoleEntity role,
+                                MessageEntity workflowExecution,
+                                MessageTaskInfo taskInfo) {
+
+        String result = flowService.runRoleFlow(taskInfo , role , workflowExecution , this);
+        log.debug("handleRole result : {}" , result);
+
+        return result ;
+    }
+
+    @Override
+    protected String handleModifyCall(IndustryRoleEntity role,
+                                      MessageEntity workflowExecution,
+                                      List<CodeContent> codeContentList,
+                                      MessageTaskInfo taskInfo) {
+
+        return null;
+    }
+
+    @Override
+    protected String handleFunctionCall(IndustryRoleEntity role,
+                                        MessageEntity workflowExecution,
+                                        List<CodeContent> codeContentList,
+                                        MessageTaskInfo taskInfo) {
+
+        return null;
+    }
+
+}
