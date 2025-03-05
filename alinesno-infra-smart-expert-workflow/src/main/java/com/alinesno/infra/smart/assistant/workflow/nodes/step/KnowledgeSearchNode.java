90ea743a419466d7499a89d8e83f90725881bebe
陈伟(Victor) <victorAip@gmail.com>
2025-03-06 00:17:21 +0800

    feat:添加 KnowledgeSearchNode 类的知识库检索功能

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/KnowledgeSearchNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/KnowledgeSearchNode.java
@@ -1,18 +1,25 @@
 // KnowledgeSearchNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.smart.assistant.role.context.AgentConstants;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * 该类表示知识库检索节点，继承自 AbstractFlowNode 类。
  * 主要用于关联知识库，查找与用户提出的问题相关的分段内容。
  * 在工作流中，当需要从知识库中获取相关信息时，会使用该节点。
  */
+@Slf4j
 @Data
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "knowledge_search")
@@ -26,8 +33,20 @@ public class KnowledgeSearchNode extends AbstractFlowNode {
         setType("knowledge_search");
     }
 
+    @SneakyThrows
     @Override
     protected void handleNode() {
 
+        String nodeStepId = IdUtil.getSnowflakeNextIdStr() ;
+
+        eventStepMessage("节点检索知识库" , AgentConstants.STEP_START , nodeStepId) ;
+        eventStepMessage("节点检索知识库" , AgentConstants.STEP_FINISH, nodeStepId) ;
+
+        String prompt = "写一篇爱情故事." ;
+
+        CompletableFuture<String> future = getStringCompletableFuture(prompt);
+
+        String output = future.get();
+        log.debug("output = {}" , output);
     }
 }
