3204c6b2553d9a3a3be6490741a0f264ceeb799e
陈伟(Victor) <victorAip@gmail.com>
2025-03-06 00:17:22 +0800

    feat:在 ChatMessageDto 中添加流程节点相关属性和列表

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatMessageDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatMessageDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +23,9 @@ public class ChatMessageDto {
     private String dateTime ;
     private Object chatText;
     private String reasoningText ;  // 推理内容
+
+    private FlowStepStatusDto flowStep ; // 流程节点运行状态
+    private List<FlowStepStatusDto> flowStepArr = new ArrayList<>(); // 流程信息
     private String readerType ; // 读取方式和类型
 
     private boolean isLoading = false ; // 是否还在加载中
@@ -29,13 +33,10 @@ public class ChatMessageDto {
 
     @JsonSerialize(using = ToStringSerializer.class)
     private long businessId ; // 生成的唯一业务ID标识
-
     private String className ; // 返回的样式标识
 
     // fix:用于消息类型的区分(同一businessId的情况下)
     private boolean llmStream = false ; // 是否为LLM输出流
-
-    // 消息引用
-    private List<MessageReferenceDto> contentReferenceArticle ;
+    private List<MessageReferenceDto> contentReferenceArticle ; // 消息引用
 
 }
