00d0c8e69d8feb0703f421907c998bb8497902cb
李伟(Michael) <michaelAip@gmail.com>
2025-03-07 00:12:08 +0800

    feat:添加 TestFunctionNode 测试类

---
+++ alinesno-infra-smart-expert-workflow/src/test/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/TestFunctionNode.java
@@ -0,0 +1,30 @@
+package com.alinesno.infra.smart.assistant.workflow.nodes.step;
+
+public class TestFunctionNode {
+//    public static void main(String[] args) {
+//        // 创建一个用于捕获输出的输出流
+//        OutputStream outputStream = new ByteArrayOutputStream();
+//
+//        // 创建 GraalVM 上下文
+//        try (Context context = Context.newBuilder("java")
+//                .out(outputStream)
+//                .err(outputStream)
+//                .build()) {
+//
+//            // 定义要执行的 Groovy 代码
+//            String groovyCode = """
+//
+//                    """;
+//
+//            // 执行 Groovy 代码
+//            Value result = context.eval("java", groovyCode);
+//
+//            // 输出执行结果
+//            System.out.println("Groovy 代码执行结果: " + result.asString());
+//            System.out.println("Groovy 代码的输出: " + outputStream.toString());
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
