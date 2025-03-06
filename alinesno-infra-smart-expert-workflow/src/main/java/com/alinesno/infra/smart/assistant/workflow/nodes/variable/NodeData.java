91132acb46c2781e5d0f327640f20a75ec318869
李伟(Michael) <michaelAip@gmail.com>
2025-03-07 00:12:08 +0800

    feat:添加 NodeData 类的 Lombok 注解并优化字段注释

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/variable/NodeData.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/variable/NodeData.java
@@ -1,8 +1,14 @@
 package com.alinesno.infra.smart.assistant.workflow.nodes.variable;
 
+import lombok.Data;
+
 /**
  * 节点配置内容
  */
+@Data
 public class NodeData {
-    private boolean isPrint ; // 是否打印输出结果
+
+    // 是否打印的标志
+    private boolean isPrint;
+
 }
