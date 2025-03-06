abf7179c938f9b679ba4cb62ad89989a4bc04604
李伟(Michael) <michaelAip@gmail.com>
2025-03-07 00:12:08 +0800

    feat:在 FunctionNode 中添加 Groovy 脚本执行功能及相关依赖注入

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/FunctionNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/FunctionNode.java
@@ -1,14 +1,28 @@
 // FunctionNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.smart.assistant.api.CodeContent;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.role.context.ContextManager;
+import com.alinesno.infra.smart.assistant.role.tools.ToolsUtil;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import com.alinesno.infra.smart.assistant.workflow.nodes.variable.step.FunctionNodeData;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
+import com.alinesno.infra.smart.im.service.IMessageService;
+import com.alinesno.infra.smart.utils.CodeBlockParser;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 import lombok.EqualsAndHashCode;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -22,6 +36,9 @@ import java.util.concurrent.CompletableFuture;
 @EqualsAndHashCode(callSuper = true)
 public class FunctionNode extends AbstractFlowNode {
 
+    @Autowired
+    protected IMessageService messageService;
+
     /**
      * 构造函数，初始化节点类型为 "function"。
      */
@@ -32,15 +49,98 @@ public class FunctionNode extends AbstractFlowNode {
     @SneakyThrows
     @Override
     protected void handleNode() {
-        log.debug( "node type = {} output = {}" , node.getType() , output) ;
+        log.debug("node type = {} output = {}" , node.getType() , output) ;
+        log.debug("FunctionNodeData = {}" , getNodeData());
 
-        String prompt = "你是谁." ;
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
 
-        CompletableFuture<String> future = getStringCompletableFuture(prompt);
 
-        String output = future.get();
-        log.debug("output = {}" , output);
+            // 上一个任务节点不为空，执行任务并记录
+            List<CodeContent> codeContentLis = null ;
+            if(workflowExecution != null){
+                String gentContent = workflowExecution.getContent();
+                codeContentLis = CodeBlockParser.parseCodeBlocks(gentContent);
+            }
 
+            String scriptText = getNodeData().getRawScript() ;
+
+            String nodeOutput = executeGroovyScript(role ,
+                    workflowExecution ,
+                    taskInfo ,
+                    codeContentLis ,
+                    scriptText) ;
+
+            log.debug("handleNode nodeOutput : {}", nodeOutput);
+            eventNodeMessage(nodeOutput);
+
+            // 生成任务结果
+            return nodeOutput;
+        });
+        future.get() ;
+
+        output.put(node.getStepName()+".result" ,taskInfo.getFlowStepContent());
+        log.debug("message = {}" , taskInfo.getFlowStepContent());
+        System.out.println(taskInfo.getFlowStepContent());
+    }
+
+    /**
+     * 获取节点数据，用于执行脚本逻辑。
+     * @return
+     */
+    private FunctionNodeData getNodeData(){
+        String nodeDataJson = String.valueOf(node.getProperties().get("node_data")) ;
+        return JSONObject.parseObject(nodeDataJson , FunctionNodeData.class) ;
+    }
+
+    /**
+     * 执行groovy脚本并返回执行结果
+     *
+     * @param role
+     * @param workflow
+     * @param taskInfo
+     * @param codeContentList
+     * @param scriptText
+     * @return
+     */
+    private String executeGroovyScript(IndustryRoleEntity role,
+                                       MessageEntity workflow ,
+                                       MessageTaskInfo taskInfo,
+                                       List<CodeContent> codeContentList,
+                                       String scriptText) {
+
+        // TODO 待处理Groovy脚本安全的问题
+
+        ToolsUtil tools = new ToolsUtil() ;
+
+        // 创建 Binding 对象，用于绑定变量到 Groovy 脚本
+        Binding binding = new Binding();
+        binding.setVariable("role", role); // 角色信息
+        binding.setVariable("taskInfo", taskInfo); // 任务信息
+        binding.setVariable("workflow", workflow); // 执行流程节点
+        binding.setVariable("qianWenLLM", flowExpertService.getQianWenLLM()); // 文本和图片生成
+        binding.setVariable("agentFlexLLM", flowExpertService.getAgentFlexLLM()); // 多模型适配生成
+        binding.setVariable("qianWenAuditLLM", flowExpertService.getQianWenAuditLLM());  // 语音生成
+        binding.setVariable("templateService", flowExpertService.getTemplateService()); // 模板引擎
+        binding.setVariable("expertService", flowExpertService);  // 操作服务
+        binding.setVariable("secretKey",flowExpertService.getSecretKey());  // 操作服务
+        binding.setVariable("channelInfo",flowExpertService.getChannelInfo(taskInfo.getChannelId()));  // 操作服务
+        binding.setVariable("screenInfo",flowExpertService.getScreenInfo(taskInfo.getScreenId()));  // 操作服务
+        binding.setVariable("tools", tools); // 工具类
+
+        binding.setVariable("codeContent", codeContentList == null || codeContentList.isEmpty() ? null : codeContentList.get(0));  // 生成 代码
+
+        binding.setVariable("contextMap", ContextManager.getInstance());
+        binding.setVariable("log", log); // 日志输出
+
+        // 创建 GroovyShell 实例
+        GroovyShell shell = new GroovyShell(this.getClass().getClassLoader(), binding);
+
+        // 执行 Groovy 脚本
+        try{
+            return String.valueOf(shell.evaluate(scriptText)) ;
+        }catch (Exception e){
+            return "角色脚本执行失败:" + e.getMessage() ;
+        }
     }
 
 }
