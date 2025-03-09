0d5584be86b7252aa812fc489c18a69041509b77
王磊(Levi) <leviAip@gmail.com>
2025-03-09 18:58:57 +0800

    feat:添加 ImageGenerateNode 类，支持从节点数据中解析并记录日志

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/ImageGenerateNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/ImageGenerateNode.java
@@ -1,10 +1,13 @@
 // ImageGenerateNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import com.alinesno.infra.smart.assistant.workflow.nodes.variable.step.ImageGenerateNodeData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
  * 其功能是根据提供的文本内容生成相应的图片。
  * 在工作流中，如果需要根据文本描述生成图片，就会用到这个节点。
  */
+@Slf4j
 @Data
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "image_generate")
@@ -28,6 +32,12 @@ public class ImageGenerateNode extends AbstractFlowNode {
 
     @Override
     protected void handleNode() {
+        ImageGenerateNodeData nodeData = getNodeData() ;
+        log.debug("node type = {} output = {}" , node.getType() , output);
+    }
 
+    private ImageGenerateNodeData getNodeData(){
+        String nodeDataJson = String.valueOf(node.getProperties().get("node_data")) ;
+        return JSONObject.parseObject(nodeDataJson , ImageGenerateNodeData.class) ;
     }
 }
