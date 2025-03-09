53d5b8d56c3ed56110b07822f04e8de401e82959
王磊(Levi) <leviAip@gmail.com>
2025-03-09 18:58:57 +0800

    feat:在TextToSpeechNode中添加日志记录及数据解析功能

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/TextToSpeechNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/TextToSpeechNode.java
@@ -1,10 +1,13 @@
 // TextToSpeechNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import com.alinesno.infra.smart.assistant.workflow.nodes.variable.step.TextToSpeechNodeData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Service;
  * 主要用于将文本通过语音合成模型转换为音频。
  * 在工作流中，当需要将文本信息以语音形式输出时，会使用该节点。
  */
+@Slf4j
 @Data
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "text_to_speech")
@@ -28,6 +32,13 @@ public class TextToSpeechNode extends AbstractFlowNode {
 
     @Override
     protected void handleNode() {
+        TextToSpeechNodeData nodeData = getNodeData() ;
+        log.debug("nodeData = {}" , nodeData) ;
+        log.debug("node type = {} output = {}" , node.getType() , output);
+    }
 
+    private TextToSpeechNodeData getNodeData(){
+        String nodeDataJson = String.valueOf(node.getProperties().get("node_data")) ;
+        return JSONObject.parseObject(nodeDataJson , TextToSpeechNodeData.class) ;
     }
 }
