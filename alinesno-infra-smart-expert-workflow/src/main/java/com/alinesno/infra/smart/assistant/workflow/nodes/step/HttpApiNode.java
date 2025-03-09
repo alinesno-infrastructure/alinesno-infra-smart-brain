03458e764e9d1e7e4d9c1179a5ecc524eff5927a
王磊(Levi) <leviAip@gmail.com>
2025-03-09 18:58:57 +0800

    feat:添加 HttpApiNode 类处理HTTP API请求功能

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/HttpApiNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/HttpApiNode.java
@@ -1,16 +1,21 @@
 // ReplyNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import com.alinesno.infra.smart.assistant.workflow.nodes.variable.step.HttpApiNodeData;
+import com.alinesno.infra.smart.assistant.workflow.utils.HttpApiRequestor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
  * HttpApiNode类，继承自AbstractFlowNode，表示一个HTTP API节点。
  */
+@Slf4j
 @Data
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "http_api")
@@ -26,6 +31,17 @@ public class HttpApiNode extends AbstractFlowNode {
 
     @Override
     protected void handleNode() {
+        HttpApiNodeData nodeData = getNodeData() ;
+        log.debug("nodeData = {}" , nodeData) ;
+        log.debug("node type = {} output = {}" , node.getType() , output);
 
+        String result = HttpApiRequestor.sendRequest(nodeData) ;
+        eventNodeMessage(result) ;
+
+    }
+
+    private HttpApiNodeData getNodeData(){
+        String nodeDataJson = String.valueOf(node.getProperties().get("node_data")) ;
+        return JSONObject.parseObject(nodeDataJson , HttpApiNodeData.class) ;
     }
 }
