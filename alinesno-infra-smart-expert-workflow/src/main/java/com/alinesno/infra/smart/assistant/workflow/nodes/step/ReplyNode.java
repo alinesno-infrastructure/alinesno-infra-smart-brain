fde65a5ffcc8d6639977309ab71474d43a2b0315
王磊(Levi) <leviAip@gmail.com>
2025-03-09 18:58:57 +0800

    feat:添加 ReplyNode 类的节点数据处理逻辑

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/ReplyNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/ReplyNode.java
@@ -1,11 +1,13 @@
 // ReplyNode.java
 package com.alinesno.infra.smart.assistant.workflow.nodes.step;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.smart.assistant.workflow.constants.FlowConst;
 import com.alinesno.infra.smart.assistant.workflow.nodes.AbstractFlowNode;
+import com.alinesno.infra.smart.assistant.workflow.nodes.variable.step.ReplayNodeData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
  * 用于指定回复内容，并且引用变量会转换为字符串进行输出。
  * 在工作流中，当需要给出固定的回复信息时，会使用该节点。
  */
+@Slf4j
 @Data
 @Scope("prototype")
 @Service(FlowConst.FLOW_STEP_NODE + "reply")
@@ -27,9 +30,25 @@ public class ReplyNode extends AbstractFlowNode {
         setType("reply");
     }
 
-    @SneakyThrows
+//    @SneakyThrows
+//    @Override
+//    protected void handleNode() {
+//        eventMessage("指定回复内容.");
+//    }
+
     @Override
     protected void handleNode() {
-        eventMessage("指定回复内容.");
+        ReplayNodeData nodeData = getNodeData() ;
+        log.debug("nodeData = {}" , nodeData) ;
+        log.debug("node type = {} output = {}" , node.getType() , output);
+
+        if(nodeData.getReplayType().equals("text")){  // 直接返回回复的内容
+            eventNodeMessage(nodeData.getReplayContent()) ;
+        }
+    }
+
+    private ReplayNodeData getNodeData(){
+        String nodeDataJson = String.valueOf(node.getProperties().get("node_data")) ;
+        return JSONObject.parseObject(nodeDataJson , ReplayNodeData.class) ;
     }
 }
