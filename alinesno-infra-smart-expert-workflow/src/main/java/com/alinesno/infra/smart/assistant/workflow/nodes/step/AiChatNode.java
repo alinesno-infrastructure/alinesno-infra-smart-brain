df2af50d9de4dc2492dc97f60019100f2300b56e
李娜(Allen) <allenAip@gmail.com>
2025-03-12 00:13:25 +0800

    fix:修复 AiChatNode 中输出结果的键名格式问题，并优化异步调用逻辑

--- alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/AiChatNode.java
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/AiChatNode.java
@@ -64,19 +64,14 @@ public class AiChatNode extends AbstractFlowNode {
                 config.setModel(llmModel.getModel()) ;
 
                 Llm llm = new QwenLlm(config);
-//                CompletableFuture<String> future = getAiChatCompletableFuture(llm , nodeData.getPrompt()) ;
-//                CompletableFuture<String> future = getAiChatCompletableFuture(llm , nodeData.getPrompt()) ;
-//                String chatResult = future.get() ;
 
-//                String chatResult = getAiChatResult(llm , nodeData.getPrompt()) ;
-
-                CompletableFuture<String> future = getAiChatResultAsync(llm, nodeData.getPrompt());
+                CompletableFuture<String> future = getAiChatResultAsync(llm, replacePlaceholders(nodeData.getPrompt()));
                 // 设置超时时间为 60 秒
                 String chatResult = future.get(120, TimeUnit.SECONDS);
                 System.out.println("Chat result: " + chatResult);
 
-                output.put("{{"+node.getStepName()+".answer}}", chatResult) ;
-                output.put("{{"+node.getStepName()+".reasoning_content}}", chatResult) ;
+                output.put(node.getStepName()+".answer", chatResult) ;
+                output.put(node.getStepName()+".reasoning_content", chatResult) ;
             }
 
         }
@@ -86,73 +81,6 @@ public class AiChatNode extends AbstractFlowNode {
         String nodeDataJson =  node.getProperties().get("node_data")+"" ;
         return StringUtils.isNotEmpty(nodeDataJson)? JSONObject.parseObject(nodeDataJson , AiChatNodeData.class):null;
     }
-
-
-//    protected String getAiChatResult(Llm llm, String prompt) {
-//        AtomicReference<String> outputStr = new AtomicReference<>("");
-//        final Object lock = new Object();
-//        boolean[] isFinished = {false};
-//
-//        try {
-//            llm.chatStream(prompt, (context, response) -> {
-//                AiMessage message = response.getMessage();
-//                System.out.println(">>>> " + message);
-//
-//                FlowStepStatusDto stepDto = new FlowStepStatusDto();
-//                stepDto.setMessage("任务进行中...");
-//                stepDto.setStepId(node.getId());
-//                stepDto.setStatus(AgentConstants.STEP_PROCESS);
-//                stepDto.setFlowChatText(message.getContent());
-//                stepDto.setPrint(node.isPrint());
-//
-//                synchronized (lock) {
-//                    taskInfo.setFlowStep(stepDto);
-//                }
-//
-//                try {
-//                    synchronized (lock) {
-//                        if (message.getStatus() == MessageStatus.END) {
-//                            outputStr.set(message.getFullContent());
-//                            stepDto.setStatus(AgentConstants.STEP_FINISH);
-//                            streamMessagePublisher.doStuffAndPublishAnEvent(null, role, taskInfo, taskInfo.getTraceBusId());
-//                            isFinished[0] = true;
-//                            lock.notify(); // 唤醒等待的线程
-//                        } else {
-//                            streamMessagePublisher.doStuffAndPublishAnEvent(null, role, taskInfo, taskInfo.getTraceBusId());
-//                        }
-//                    }
-//                } catch (Exception e) {
-//                    // 处理发布事件时的异常
-//                    log.error(e.getMessage());
-//                }
-//            });
-//        } catch (Exception e) {
-//            // 处理 chatStream 方法的异常
-//            log.error(e.getMessage());
-//            synchronized (lock) {
-//                isFinished[0] = true;
-//                lock.notify();
-//            }
-//        }
-//
-//        synchronized (lock) {
-//            try {
-//                long startTime = System.currentTimeMillis();
-//                long timeout = 60000; // 超时时间为 60 秒
-//                while (!isFinished[0] && System.currentTimeMillis() - startTime < timeout) {
-//                    lock.wait(timeout - (System.currentTimeMillis() - startTime));
-//                }
-//                if (!isFinished[0]) {
-//                    throw new RuntimeException("等待聊天结果超时");
-//                }
-//            } catch (InterruptedException e) {
-//                Thread.currentThread().interrupt();
-//                throw new RuntimeException("等待聊天结果时被中断", e);
-//            }
-//        }
-//
-//        return outputStr.get();
-//    }
 
     @NotNull
     protected CompletableFuture<String> getAiChatCompletableFuture(Llm llm , String prompt) {
