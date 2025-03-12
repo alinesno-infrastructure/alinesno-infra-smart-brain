01e622d6268c00bbd1a048fec1deb6d01ebb6b62
周健(James) <jamesAip@gmail.com>
2025-03-12 20:21:34 +0800

    feat:新增 ChannelPublishServiceImpl 类，实现频道发布服务逻辑。

---
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/impl/ChannelPublishServiceImpl.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.smart.assistant.publish.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.publish.entity.ChannelPublishEntity;
+import com.alinesno.infra.smart.assistant.publish.mapper.ChannelPublishMapper;
+import com.alinesno.infra.smart.assistant.publish.service.IChannelPublishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 频道发布服务实现类
+ */
+@Slf4j
+@Service
+public class ChannelPublishServiceImpl extends IBaseServiceImpl<ChannelPublishEntity, ChannelPublishMapper> implements IChannelPublishService {
+
+
+}
