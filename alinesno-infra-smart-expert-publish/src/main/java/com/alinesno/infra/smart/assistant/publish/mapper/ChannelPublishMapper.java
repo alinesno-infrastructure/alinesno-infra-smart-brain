35ed7b9c8790f9a78a374220e8f481d1ed16816e
周健(James) <jamesAip@gmail.com>
2025-03-12 20:21:34 +0800

    feat:新增 ChannelPublishMapper 接口，继承 IBaseMapper 定义通道发布相关数据操作

---
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/mapper/ChannelPublishMapper.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.publish.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.publish.entity.ChannelPublishEntity;
+
+/**
+ * 通道发布映射器接口
+ * 继承自IBaseMapper，用于定义通道发布相关的数据操作
+ * 主要作用是为ChannelPublishEntity实体类提供基本的增删查改功能
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ChannelPublishMapper extends IBaseMapper<ChannelPublishEntity> {
+}
