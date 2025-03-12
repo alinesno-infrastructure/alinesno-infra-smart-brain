288225be6cec218e2cf42d52ac864355dce2d665
周健(James) <jamesAip@gmail.com>
2025-03-12 20:21:34 +0800

    feat:新增 IChannelPublishService 接口，定义发布渠道表的 Service 操作。

---
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/IChannelPublishService.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.publish.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.publish.entity.ChannelPublishEntity;
+
+/**
+ * 发布渠道表Service接口
+ */
+public interface IChannelPublishService extends IBaseService<ChannelPublishEntity> {
+
+}
