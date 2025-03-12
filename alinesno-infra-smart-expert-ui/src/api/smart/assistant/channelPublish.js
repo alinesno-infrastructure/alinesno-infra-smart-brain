a869046a82ebe409aec2c096d93ac6d703d4c195
张婷(Tina) <tinaAip@gmail.com>
2025-03-12 20:21:34 +0800

    feat:新增 channelPublish 相关 API 接口，包含渠道管理、字段修改、应用查询等功能。

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/channelPublish.js
@@ -0,0 +1,79 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/channelPublish/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'saveChannelPublish' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  channels: prefix + "channels",
+}
+
+// 获取到所有渠道
+export function getChannels(){
+  return request({
+    url: managerUrl.channels ,
+    method: 'get'
+  })
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询应用列表
+export function listChannelPublish(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询应用详细
+export function getChannelPublish(id) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 新增应用
+export function addChannelPublish(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改应用
+export function updateChannelPublish(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除应用
+export function delChannelPublish(id) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
