6dcf0a07bbab542f2b4ec8c907b8f882793292b6
周健(James) <jamesAip@gmail.com>
2025-03-12 20:21:34 +0800

    feat:新增 ChannelPublishEntity 类，用于定义发布渠道的基本信息。

---
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/entity/ChannelPublishEntity.java
@@ -0,0 +1,63 @@
+package com.alinesno.infra.smart.assistant.publish.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 发布渠道实体类，用于定义发布渠道的基础信息
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("role_publish")
+@TableComment(value = "发布渠道表")
+public class ChannelPublishEntity extends InfraBaseEntity implements Serializable {
+
+    @TableField(value = "role_id")
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 32)
+    @ColumnComment("角色ID")
+    private Long roleId;
+
+    @TableField(value = "name")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 32)
+    @ColumnComment("渠道名称")
+    private String name;
+
+    @TableField(value = "description")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 32)
+    @ColumnComment("渠道描述")
+    private String description;
+
+    @TableField(value = "icon_class")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
+    @ColumnComment("渠道图标类名")
+    private String iconClass;
+
+    @TableField(value = "param_key")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 255)
+    @ColumnComment("渠道参数键")
+    private String paramKey;
+
+    @TableField(value = "has_configured")
+    @ColumnType(value = MySqlTypeConstant.TINYINT)
+    @ColumnComment("是否已配置")
+    private int hasConfigured;
+
+    @TableField(value = "param_map")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 512)
+    @ColumnComment("渠道参数键")
+    private String configMap ;
+
+}
