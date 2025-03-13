95df698f0afaa7bb0d267552e2c13e6a4a798c23
李娜(Allen) <allenAip@gmail.com>
2025-03-14 00:13:25 +0800

    feat: 实现 ChannelPublishServiceImpl 的 updateConfig、getConfigsByParamKeys 和 getByShareToken 方法

--- alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/impl/ChannelPublishServiceImpl.java
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/impl/ChannelPublishServiceImpl.java
@@ -1,11 +1,17 @@
 package com.alinesno.infra.smart.assistant.publish.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.smart.assistant.publish.entity.ChannelPublishEntity;
 import com.alinesno.infra.smart.assistant.publish.mapper.ChannelPublishMapper;
 import com.alinesno.infra.smart.assistant.publish.service.IChannelPublishService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 频道发布服务实现类
@@ -14,5 +20,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChannelPublishServiceImpl extends IBaseServiceImpl<ChannelPublishEntity, ChannelPublishMapper> implements IChannelPublishService {
 
+    @Override
+    public void updateConfig(ChannelPublishEntity entity) {
+        this.saveOrUpdate(entity);
+    }
+
+    @Override
+    public Map<String, ChannelPublishEntity> getConfigsByParamKeys(List<String> paramKeys, PermissionQuery query, long roleId) {
+
+        LambdaQueryWrapper<ChannelPublishEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.setEntityClass(ChannelPublishEntity.class);
+        query.toWrapper(wrapper);
+        wrapper.in(ChannelPublishEntity::getParamKey, paramKeys);
+        wrapper.eq(ChannelPublishEntity::getRoleId, roleId);
+
+        List<ChannelPublishEntity> entities = list(wrapper) ;
+        Map<String, ChannelPublishEntity> configMap = new HashMap<>();
+        for (ChannelPublishEntity entity : entities) {
+            configMap.put(entity.getParamKey(), entity);
+        }
+
+        return configMap;
+    }
+
+    @Override
+    public ChannelPublishEntity getByShareToken(String shareId) {
+
+        if(shareId != null){
+            LambdaQueryWrapper<ChannelPublishEntity> wrapper = new LambdaQueryWrapper<>();
+            wrapper.setEntityClass(ChannelPublishEntity.class);
+            wrapper.eq(ChannelPublishEntity::getApiKey, shareId);
+            return getOne(wrapper);
+        }
+
+        return null;
+    }
 
 }
