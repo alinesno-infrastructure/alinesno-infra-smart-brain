8e1b06f1844d98470c02cc4e781ff6e3e2ff04dd
李娜(Allen) <allenAip@gmail.com>
2025-03-14 00:13:25 +0800

    feat:新增 IChannelPublishService 接口方法，包括更新配置、按 paramKey 查询配置及通过分享Token获取渠道信息。

--- alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/IChannelPublishService.java
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/IChannelPublishService.java
@@ -1,11 +1,38 @@
 package com.alinesno.infra.smart.assistant.publish.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.publish.entity.ChannelPublishEntity;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 发布渠道表Service接口
  */
 public interface IChannelPublishService extends IBaseService<ChannelPublishEntity> {
+
+    /**
+     * 更新渠道配置
+     * @param entity
+     */
+    void updateConfig(ChannelPublishEntity entity);
+
+    /**
+     * 根据 paramKey 列表查询配置信息
+     *
+     * @param paramKeys paramKey 列表
+     * @param query
+     * @param roleId
+     * @return 配置信息映射，键为 paramKey，值为对应的配置信息
+     */
+    Map<String, ChannelPublishEntity> getConfigsByParamKeys(List<String> paramKeys, PermissionQuery query, long roleId);
+
+    /**
+     * 根据分享Token
+     * @param shareId
+     * @return
+     */
+    ChannelPublishEntity getByShareToken(String shareId);
 
 }
