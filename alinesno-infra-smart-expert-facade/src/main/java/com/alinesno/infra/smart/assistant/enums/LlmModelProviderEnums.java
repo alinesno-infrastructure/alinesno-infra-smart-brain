4394734f97a70d3634ee908b94d8805ca302bd28
李伟(Michael) <michaelAip@gmail.com>
2025-03-15 00:12:33 +0800

    refactor: 重构 LlmModelProviderEnums 枚举类，优化结构并简化字段与方法

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/LlmModelProviderEnums.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/LlmModelProviderEnums.java
@@ -12,43 +12,21 @@ import java.util.Map;
 @Getter
 public enum LlmModelProviderEnums {
 
-    DEEDSEEK(
-            "DeepSeek",
-            "深度求索",
-            "deepseek",
-            "https://dashscope.aliyuncs.com/compatible-mode/v1",
-            createModelList("deepseek-v3", "deepseek-r1")
-    ),
-    DOUBAO(
-            "豆包",
-            "字节跳动",
-            "doubao",
-            "https://ark.cn-beijing.volces.com/api/v3",
-            createModelList("ep-20250210101146-9ds66")
-    ),
-    CHATGPT(
-            "ChatGPT",
-            "OpenAI公司",
-            "chatgpt",
-            "https://api.openai.com",
-            createModelList("gpt-3.5-turbo", "gpt-4")
-    ),
-    TONGYI_QIYWEN(
-            "通义千问",
-            "阿里巴巴",
-            "qwen",
-            "https://dashscope.aliyuncs.com/compatible-mode/v1",
-            createModelList("qwen-turbo", "qwen-plus")
-    );
+    QWEN("千问(Qwen)", "qwen", "https://dashscope.aliyuncs.com/compatible-mode/v1"),
+    OPENAI("ChatGPT", "openai", "https://api.openai.com"),
+    DOUBAO("豆包", "doubao", "https://ark.cn-beijing.volces.com/api/v3"),
+    GITEE("GiteeAI", "gitee", "https://ai.gitee.com"),
+    OLLAMA("Ollama", "ollama", "http://localhost:11434"),
+    DEEPSEEK("DeepSeek", "deepseek", "https://dashscope.aliyuncs.com/compatible-mode/v1"),
+    QWQ("千问（QwQ）", "qwq", "https://dashscope.aliyuncs.com/compatible-mode/v1");
 
     private final String displayName;
-    private final String company;
     private final String code;
     private final String url;
-    private final List<Map<String, String>> modelList;
 
     /**
      * 创建模型列表
+     *
      * @param modelNames 模型名称数组
      * @return 模型列表
      */
@@ -64,6 +42,7 @@ public enum LlmModelProviderEnums {
 
     /**
      * 返回包含所有枚举常量信息的列表，每个元素是一个包含显示名称、公司、代码、url 和模型列表的 Map
+     *
      * @return 包含所有枚举常量信息的列表
      */
     public static List<Map<String, Object>> getAllModelProvidersInfo() {
@@ -71,10 +50,8 @@ public enum LlmModelProviderEnums {
         for (LlmModelProviderEnums provider : values()) {
             Map<String, Object> infoMap = new HashMap<>();
             infoMap.put("displayName", provider.getDisplayName());
-            infoMap.put("company", provider.getCompany());
             infoMap.put("code", provider.getCode());
             infoMap.put("url", provider.getUrl());
-            infoMap.put("modelList", provider.getModelList());
             result.add(infoMap);
         }
         return result;
