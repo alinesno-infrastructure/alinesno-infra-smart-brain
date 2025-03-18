8e8ce22be85f0fdd034b62ec2cd127014f40a151
陈伟(Victor) <victorAip@gmail.com>
2025-03-19 00:15:11 +0800

    fix:修复模型名称和提供商名称输入框绑定问题，优化空数据提示文案及按钮样式。

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/index.vue
@@ -7,11 +7,11 @@
 
           <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
             <el-form-item label="模型名称" prop="modelName">
-              <el-input v-model="queryParams['condition[modelName|like]']" placeholder="请输入大模型名称" clearable
+              <el-input v-model="modelName" placeholder="请输入大模型名称" clearable
                 style="width: 240px" @keyup.enter="handleQuery" />
             </el-form-item>
             <el-form-item label="模型类型" prop="modelType">
-              <el-input v-model="queryParams['condition[providerId|like]']" placeholder="请输入所属提供商名称" clearable
+              <el-input v-model="providerId" placeholder="请输入所属提供商名称" clearable
                 style="width: 240px" @keyup.enter="handleQuery" />
             </el-form-item>
             <el-form-item>
@@ -30,11 +30,10 @@
           </el-row>
         </div>
 
-
-        <div class="gen-template-box" v-if="LlmModelList.size == 0">
+        <div class="gen-template-box" v-if="LlmModelList.length == 0">
           <el-col :sm="24">
-            <el-empty description="还没有创建模型,可以根据提示链接创建自己的工程模型">
-              <el-link type="primary" icon="el-icon-link">如何创建工程模板?</el-link>
+            <el-empty description="还没有创建模型,可以根据提示链接创建链接自己的模型">
+              <el-button type="primary" bg icon="Link" @click="handleAdd">新增加模型</el-button>
             </el-empty>
           </el-col>
         </div>
@@ -74,6 +73,9 @@
                   <el-button text bg type="primary">
                     {{ item.modelPermission == 'org' ? '组织' : '私有' }}
                   </el-button>
+                  <!-- <el-button text bg type="danger">
+                    {{ item.providerCode }}
+                  </el-button> -->
                 </div>
 
                 <div class="vc-text" title="">
@@ -125,8 +127,8 @@
         <el-row>
           <el-col :span="24">
             <el-form-item label="模型厂商" prop="providerCode">
-              <el-radio-group v-model="form.providerCode" @change="selectLLMProvider">
-                <el-radio v-for="item in providerOptions" :value="item.code" :label="item.code" :key="item.code">
+              <el-radio-group v-model="form.providerCode">
+                <el-radio v-for="item in providerOptions" size="large" :value="item.code" :label="item.code" :key="item.code" @change="handleSelectLLMProvider(item)">
                   <!-- 添加图标 -->
                   <img :src="'http://data.linesno.com/icons/llm/' + item.code + '.png'" alt="图标" style="width: 30px; height: 30px; border-radius: 50%;">
 
@@ -141,7 +143,7 @@
             <el-form-item label="模型类型" prop="modelType">
 
               <el-radio-group v-model="form.modelType">
-                <el-radio v-for="item in modelTypeOptions" :value="item.code" :label="item.displayName" :key="item.code">
+                <el-radio v-for="item in filteredOptions" :value="item.code" :label="item.code" :key="item.code" @change="handleSelectModelType(item)">
                   {{ item.displayName }}
                 </el-radio>
               </el-radio-group>
@@ -166,14 +168,19 @@
         <el-row>
           <el-col :span="24">
             <el-form-item label="模型地址" prop="apiUrl">
-              <el-input v-model="form.apiUrl" placeholder="请输入 API 地址" maxlength="255" />
+              <el-input v-model="form.apiUrl" placeholder="请输入 API 地址" maxlength="255" autocomplete="off" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="24">
             <el-form-item label="ApiKey" prop="apiKey">
-              <el-input type="password" v-model="form.apiKey" placeholder="请输入 API 密钥" maxlength="255" show-password="true" />
+              <el-input type="password" v-model="form.apiKey" placeholder="请输入 API 密钥" maxlength="255" show-password="true" autocomplete="off" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="24" v-if="form.providerCode === 'doubao' && form.modelType === 'image_generation'" prop="secretKey">
+            <el-form-item label="SecretKey" prop="secretKey">
+              <el-input type="password" v-model="form.secretKey" placeholder="请输入 secretKey 密钥" maxlength="255" show-password="true" autocomplete="off" />
             </el-form-item>
           </el-col>
         </el-row>
@@ -185,9 +192,9 @@
           </el-col>
         </el-row>
 
-        <el-divider content-position="left">测试结果</el-divider>
+        <div v-if="form.modelType === 'large_language_model'">
+          <el-divider content-position="left">测试结果</el-divider>
 
-        <div>
           <div class="reasoning-chat-content" v-if="testLlmModelReasoingReponse">
               {{ testLlmModelReasoingReponse }}
           </div> 
@@ -195,6 +202,22 @@
               {{ testLlmModelReponse }}
           </div> 
         </div>
+
+        <el-row v-if="form.modelType === 'image_generation' && generatedImageUrl">
+          <el-divider content-position="left"> 图片生成</el-divider>
+          <div>
+            <el-image
+              style="width:200px;border-radius: 5px;"
+              :src="generatedImageUrl"
+              fit="contain"
+              :preview-src-list="[generatedImageUrl]"
+              @error="handleImageError">
+              <template #loading>
+                <el-spinner></el-spinner>
+              </template>
+            </el-image>
+          </div>
+        </el-row>
 
       </el-form>
       <template #footer>
@@ -211,17 +234,6 @@
                 <i class="fa-solid fa-microphone-lines"></i> 试讲 
               </el-button>
            </div>
-
-          <!-- 语音合成模型测试 -->
-          <div v-if="form.modelType === 'speech_synthesis'" >
-            <img v-if="isSpeaking" :src="speakingIcon" style="width:35px" />
-            <el-button v-if="isSpeaking" type="danger" text bg size="large" @click="listenPlayVoiceOption()"> 
-              <i class="fa-regular fa-circle-stop"></i> &nbsp;&nbsp; 停止 
-            </el-button>
-            <el-button v-if="!isSpeaking" type="primary" text bg size="large" @click="listenPlayVoiceOption()"> 
-              <i class="fa-solid fa-headphones-simple"></i> &nbsp;&nbsp; 试听
-            </el-button>
-          </div>
 
           <!-- 大模型测试-->
           <el-button type="primary" 
@@ -244,8 +256,18 @@
           <!-- 图片生成测试-->
           <el-button type="primary" 
               v-if="form.modelType === 'image_generation'" 
-              @click="handleTestVectorModel" 
+              @click="handleTestGenerateImageModel" 
               text bg size="large" :loading="chatLoading">生成图片</el-button>
+
+          <!-- 语音合成 -->
+           <!-- 语音合成模型测试 -->
+          <div v-if="form.modelType === 'speech_synthesis'" >
+            <img v-if="isSpeaking" :src="speakingIcon" style="width:35px" />
+          </div>
+          <el-button type="primary" 
+              v-if="form.modelType === 'speech_synthesis'" 
+              @click="handleTestSpeechModel" 
+              text bg size="large" :loading="chatLoading">语音合成</el-button>
 
           <el-tooltip content="请先测试通过，确认模型可用." placement="top">
             <el-button type="primary" @click="submitForm" text bg size="large" :disabled="isValidatedStatus">保存</el-button>
@@ -253,6 +275,7 @@
 
           <el-button @click="cancel" text bg size="large">取 消</el-button>
         </div>
+        <br/>
       </template>
     </el-dialog>
 
@@ -260,8 +283,6 @@
 </template>
 
 <script setup name="LlmModel">
-
-// import { getToken } from "@/utils/auth";
 
 import {
   listLlmModel,
@@ -272,6 +293,8 @@ import {
   testLlmModel,
   getAllModelProvidersInfo,
   getAllModelTypesInfo,
+  getGenerateImage,
+  getSpeech
 } from "@/api/smart/assistant/llmModel";
 
 import { onMounted, reactive, ref } from "vue";
@@ -304,7 +327,12 @@ const streamLoading = ref(null)
 
 const providerOptions = ref(undefined)
 const modelTypeOptions = ref(undefined)
-const baseModelOptions = ref(undefined)
+const filteredOptions = ref([])
+// const baseModelOptions = ref(undefined)
+const generatedImageUrl = ref('');
+
+// 当前模型提供商
+const currentModelProvider = ref(undefined)
 
 const testLlmModelReasoingReponse = ref(undefined)
 const testLlmModelReponse = ref(undefined)
@@ -330,6 +358,7 @@ const data = reactive({
     providerCode: [{ required: true, message: "所属提供商名称不能为空", trigger: "blur" }],
     modelType: [{ required: true, message: "所属模型类型不能为空", trigger: "blur" }],
     apiKey: [{ required: true, message: "API 密钥不能为空", trigger: "blur" }],
+    secretKey: [{ required: true, message: "SecretKey 密钥不能为空", trigger: "blur" }],
     apiUrl: [{ required: true, message: "API 地址不能为空", trigger: "blur" }],
     model: [{ required: true, message: "模型名称不能为空", trigger: "blur" }],
   }
@@ -405,7 +434,10 @@ function cancel() {
 /** 新增按钮操作 */
 function handleAdd() {
   reset();
+
   isValidatedStatus.value = true 
+  generatedImageUrl.value = null;
+  
   open.value = true;
   title.value = "添加大模型";
 };
@@ -415,6 +447,7 @@ function handleUpdate(row) {
   reset();
 
   isValidatedStatus.value = true;
+  generatedImageUrl.value = null;
 
   const id = row.id || ids.value;
   getLlmModel(id).then(response => {
@@ -483,7 +516,6 @@ function handleTestVectorModel(){
         proxy.$modal.msgSuccess("验证通过");
       }).catch(error => {
         chatLoading.value = false ;
-        // proxy.$modal.msgError(error.message);
       });
     }
 
@@ -498,35 +530,11 @@ function handleAllModelProvidersInfo() {
 
   getAllModelTypesInfo().then(response => {
     modelTypeOptions.value = response.data;
+
+    filteredOptions.value = modelTypeOptions.value ; 
   });
-}
 
-/** 选中模型供应商 */
-function selectLLMProvider(value) {
-  console.log('value = ' + value);
-
-  let selectedItem = null;
-
-  for (let i = 0; i < providerOptions.value.length; i++) {
-    const item = providerOptions.value[i];
-    if (item.code === value) {
-      // 这里可以对选中的项进行处理，比如打印信息
-      console.log('找到匹配项:', item);
-      // 如果想提前结束查找，可添加 break 语句
-      selectedItem = item;
-      break;
-    }
-
-  }
-
-  if (selectedItem) {  // 查找到角色
-
-    form.value.apiUrl = selectedItem.url;
-    baseModelOptions.value = selectedItem.modelList;
-
-    console.log('form.apiUrl = ' + form.apiUrl + 'baseModelOptions = ' + baseModelOptions);
-  }
-
+  form.value.modelType = null;
 }
 
 // >>>>>>>>>>>>>>>>>> 模型测试
@@ -540,6 +548,99 @@ const handleClose = (done) => {
   chatLoading.value = false
   open.value = false 
 }
+
+// 模型管理
+const handleSelectLLMProvider = (item) => {
+  console.log('item = ' + JSON.stringify(item));
+
+  currentModelProvider.value = item ;
+
+  filteredOptions.value = item.supportedModels
+}
+
+// 模型类型选择
+const handleSelectModelType = (item) => {
+  // 先通过选择的厂家过滤出厂家的的信息
+  console.log('item = ' + JSON.stringify(item))
+  form.value.apiUrl = item.url ;
+
+  form.value.apiKey = null ;
+  form.value.secretKey = null ;
+  form.value.model = null ;
+}
+
+// 把表单验证包装成返回 Promise 的函数
+const validateForm = () => {
+    return new Promise((resolve, reject) => {
+        proxy.$refs["LlmModelRef"].validate((valid) => {
+            if (valid) {
+                resolve();
+            } else {
+                reject(new Error('表单验证未通过'));
+            }
+        });
+    });
+};
+
+// 验证语音播放
+const handleTestSpeechModel = async () => {
+
+  // 等待表单验证通过
+  await validateForm();
+
+  // 表单验证通过后，发出语音请求
+  chatLoading.value = true ;
+  form.value.testChannelId = channelId.value ; 
+
+  getSpeech(form.value).then(res => {
+    const audioBlob = new Blob([res], { type: 'audio/wav' }) // 这按照自己的数据类型来写type的内容
+    const audioUrl = URL.createObjectURL(audioBlob) // 生成url
+    const audio = new Audio(audioUrl);
+
+    audio.addEventListener('ended', () => {
+      console.log('音频播放完成');
+
+      chatLoading.value = false;
+      isValidatedStatus.value = false
+      isSpeaking.value = false 
+    });
+
+    isSpeaking.value = true 
+    audio.play();
+
+  }).catch(error => {
+    chatLoading.value = false;
+    console.error('操作失败:', error.message);
+  }); 
+};
+
+/** 测试生成图片 */
+const handleTestGenerateImageModel = async () => {
+
+  // 等待表单验证通过
+  await validateForm();
+
+  // 表单验证通过后，发出语音请求
+  chatLoading.value = true ;
+  form.value.testChannelId = channelId.value ; 
+
+  getGenerateImage(form.value).then(res => {
+    const url = URL.createObjectURL(res);
+    generatedImageUrl.value = url;
+
+    chatLoading.value = false; 
+    isValidatedStatus.value = false 
+
+  }).catch(error => {
+    chatLoading.value = false;
+    console.error('操作失败:', error.message);
+  }); 
+}
+
+const handleImageError = () => {
+  generatedImageUrl.value = '';
+  ElMessage.warning('图片加载失败，请重试');
+};
 
 /** 连接sse */
 function handleSseConnect(channelId) {
@@ -560,6 +661,7 @@ function handleSseConnect(channelId) {
             isValidatedStatus.value = false 
 
             if (data.hasError) {
+              isValidatedStatus.value = true 
               proxy.$modal.msgError("模型测试失败，请检查模型配置是否正确");
             }
 
@@ -644,6 +746,18 @@ onMounted(() => {
   padding: 10px;
   background: #f5f5f5;
   border-radius: 2px;
+}
+
+.image-container {
+  padding: 16px;
+  background: #f8f9fa;
+  border-radius: 8px;
+  margin-bottom: 16px;
+}
+
+.image-item {
+  width: 100%;
+  max-height: 400px;
 }
 
 </style>
