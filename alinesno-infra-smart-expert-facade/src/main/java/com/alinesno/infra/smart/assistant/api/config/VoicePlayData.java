e3d0ef010eadd5d573aa211d3c92d00fd6695b5e
王磊(Levi) <leviAip@gmail.com>
2025-03-20 00:12:30 +0800

    feat:新增 VoicePlayData 类中 voiceSpeechModel 属性，用于表示播放音色。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/VoicePlayData.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/VoicePlayData.java
@@ -9,6 +9,8 @@ import lombok.Data;
     public class VoicePlayData {
         // 语音模型名称，可能用于语音播放
         private String voiceModel;
+        // 播放音色
+        private String voiceSpeechModel ;
         // 语音速度，用于语音播放的速度
         private int speechRate;
     }
