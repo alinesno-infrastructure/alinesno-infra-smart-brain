4625ca757341aede579720f8ea0655d90a1a627c
周健(James) <jamesAip@gmail.com>
2025-03-20 19:57:06 +0800

    更新:alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IUserService.java

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-//package com.alinesno.infra.base.im.service;
-//
-//import com.alinesno.infra.base.im.entity.UserEntity;
-//import com.alinesno.infra.common.facade.services.IBaseService;
-//
-//public interface IUserService extends IBaseService<UserEntity> {
-//}
