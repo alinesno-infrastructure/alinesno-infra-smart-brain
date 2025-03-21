72801bf0a653aee1eb186dc27bb09c1121092886
林晓(Lily) <lilyAip@gmail.com>
2025-03-22 00:14:45 +0800

    feat:新增 playShareGenContent 方法，用于获取语音模型语音请求

--- alinesno-infra-smart-expert-ui/src/api/smart/assistant/publishChat.js
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/publishChat.js
@@ -6,7 +6,22 @@ var prefix = '/api/infra/smart/assistant/publishChat/' ;
 var managerUrl = {
   getShareInfo : prefix +"getShareInfo" ,
   chatShareRole: prefix + 'chatShareRole' ,
+  playShareGenContent: prefix + 'playShareGenContent' ,
   shareRecognize: prefix + 'shareRecognize' ,
+}
+
+// 获取语音模型语音请求
+export function playShareGenContent(item , shareId) {
+    return request({
+        url: managerUrl.playShareGenContent + '?shareId=' + parseStrEmpty(shareId) , 
+        method: 'post',
+        data: item ,
+        responseType: 'blob', // 显式声明返回二进制流
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'image/*' // 明确期望图片类型
+        }
+    })
 }
 
 // 语音识别
