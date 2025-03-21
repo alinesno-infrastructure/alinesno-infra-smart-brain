ed3c47bf49b1af9aceb513da90f27328a1747b8a
陈伟(Victor) <victorAip@gmail.com>
2025-03-22 00:14:45 +0800

    fix:修复语音输入按钮样式问题并优化录音逻辑

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/aiVoiceInput.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/aiVoiceInput.vue
@@ -1,12 +1,12 @@
 <template>
   <el-tooltip class="box-item" effect="dark" content="语音输入" placement="top">
-    <span style="margin-right:10px;" v-if="isRecording">
+    <span style="margin-right:10px;display: flex;" v-if="isRecording">
       <img :src="speakingIcon" style="width:35px" />
       <el-button type="primary" text bg size="large" @click="stopRecording">
         <i class="fa-solid fa-headset icon-btn"></i>
       </el-button>
     </span>
-    <el-button v-if="!isRecording" type="primary" size="large" text bg @click="listenPlayVoiceOption">
+    <el-button v-if="!isRecording" type="primary" size="large" text bg @click="listenPlayVoiceOption" :loading="chatLoading">
       <i class="fa-solid fa-microphone-lines icon-btn"></i>
     </el-button>
   </el-tooltip>
@@ -14,17 +14,25 @@
 
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
+import { ElMessage } from 'element-plus';
 import { recognizeForm } from '@/api/smart/assistant/roleChat';
 import speakingIcon from '@/assets/icons/speaking.gif';
 import Recorder from 'js-audio-recorder';
 
+const chatLoading = ref(false)
+
+// 接收父类传递的props
+const props = defineProps({
+  roleId: {
+    type: Number,
+    default: 0,
+  },
+  role: {
+    type: Object,
+  },
+});
+
 const isRecording = ref(false);
-const mediaRecorder = ref(null);
-const audioChunks = ref([]);
-const audioUrl = ref('');
-
-const streamLoading = ref(null);
-
 const recorder = new Recorder({
   sampleBits: 16, // 采样位数，支持 8 或 16，默认是16
   sampleRate: 16000, // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
@@ -85,6 +93,9 @@ const send = async () => {
   };
 
   var formData = new FormData();
+
+  formData.append('roleId', props.role.id);
+
   formData.append('act', "gpt.whisper");
   formData.append('prompt', "whisper");
   formData.append('duration', stat.value?.duration);
@@ -97,34 +108,42 @@ const send = async () => {
 
 // 发送音频文件到后端
 const sendAudioFileToBackend = async (audioFormData) => {
+  chatLoading.value = true;
   const response = await recognizeForm(audioFormData);
-  console.log('response = ' + response);
-  emits("sendAudioToBackend" , response);
+  chatLoading.value = false ;
+  console.log('response = ' + response.data);
+  if(response.data){
+    emits("sendAudioToBackend" , response.data);
+  }else{
+    chatLoading.value = false ;
+    ElMessage.error('录音识别失败，请重试或者确认麦克风正常.');
+  }
 };
 
-// 按下空格键开始录音
+// 按下 Ctrl + R 开始录音
 const onKeyDown = (event) => {
-  if (event.key === ' ' && !isRecording.value) {
-    listenPlayVoiceOption();
-  }
+    if (event.ctrlKey && event.key === 'b' && !isRecording.value) {
+        listenPlayVoiceOption();
+    }
 };
 
-// 抬起空格键停止录音
+// 抬起 Ctrl + R 停止录音
 const onKeyUp = (event) => {
-  if (event.key === ' ' && isRecording.value) {
-    stopRecording();
-  }
+    if (event.ctrlKey && event.key === 'b' && isRecording.value) {
+        stopRecording();
+    }
 };
 
 onMounted(() => {
-  window.addEventListener('keydown', onKeyDown);
-  window.addEventListener('keyup', onKeyUp);
+    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('keyup', onKeyUp);
 });
 
 onUnmounted(() => {
-  window.removeEventListener('keydown', onKeyDown);
-  window.removeEventListener('keyup', onKeyUp);
-});
+    window.removeEventListener('keydown', onKeyDown);
+    window.removeEventListener('keyup', onKeyUp);
+});  
+
 </script>
 
 <style scoped>
