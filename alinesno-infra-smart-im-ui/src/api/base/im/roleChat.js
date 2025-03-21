981eee0274322d840182188e12ff4032cee7fc2f
林晓(Lily) <lilyAip@gmail.com>
2025-03-22 00:14:44 +0800

    feat:新增语音相关接口及函数，包括语音识别与语音内容生成功能。

--- alinesno-infra-smart-im-ui/src/api/base/im/roleChat.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/roleChat.js
@@ -4,9 +4,52 @@ import { parseStrEmpty } from "@/utils/ruoyi";
 
 // 接口配置项
 var prefix = '/v1/api/infra/base/im/roleChat/' ;
+var speechPrefix = '/api/infra/smart/assistant/speechRecognition/' ;  // 语音识别
+
 var managerUrl = {
   getInfo: prefix +"getInfo",
-  chatRole: prefix + "chatRole"
+  chatRole: prefix + "chatRole",
+  recognizeForm: speechPrefix + "recognizeFormData" ,
+  recognize: speechPrefix + "recognize" ,
+  playGenContent: speechPrefix + "playGenContent" ,
+}
+
+// 获取语音模型语音请求
+export function playGenContent(item) {
+    return request({
+        url: managerUrl.playGenContent , 
+        method: 'post',
+        data: item ,
+        responseType: 'blob', // 显式声明返回二进制流
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'image/*' // 明确期望图片类型
+        }
+    })
+}
+
+// 语音识别
+export function recognizeForm(audioFormData) {
+  return request({
+    url: managerUrl.recognizeForm ,
+    method: 'post',
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    },
+    data: audioFormData  
+  })
+}
+
+// 语音识别
+export function recognize(data) {
+  return request({
+    url: managerUrl.recognize ,
+    method: 'post',
+    // headers: {
+    //     'Content-Type': 'multipart/form-data'
+    // },
+    data: data
+  })
 }
 
 // 查询应用详细
