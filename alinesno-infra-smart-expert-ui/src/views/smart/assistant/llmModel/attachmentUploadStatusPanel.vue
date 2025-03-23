7a4bb5c89cad60fb2383bc3aecff569f3f4f0409
李伟(Michael) <michaelAip@gmail.com>
2025-03-24 00:12:12 +0800

    feat:新增 attachmentUploadStatusPanel.vue 组件，支持文件上传数限制、图片上传数限制及多模态模型选择功能。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/attachmentUploadStatusPanel.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="row" style="padding: 20px">
+    <el-form :model="formData" :rules="rules" ref="formRef" label-width="120px" :label-position="'left'">
+
+      <el-form-item label="是否启用" prop="enable">
+        <el-radio-group size="large" v-model="formData.enable">
+          <el-radio :value="true">启用</el-radio>
+          <el-radio :value="false">不启用</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 文件上传数限制 -->
+      <el-form-item label="文件上传数" prop="fileUploadLimit">
+          <el-slider size="large" show-input v-model="formData.fileUploadLimit" :step="1" :max="20" />
+      </el-form-item>
+
+      <!-- 多模态模型选择项 -->
+      <el-form-item label="图片识别模型" prop="modelId">
+        <el-select v-model="formData.modelId" clearable placeholder="请选择多模态大模型" size="large" style="width:100%">
+          <el-option v-for="item in multiModelOptions" :key="item.id" :label="item.modelName" :value="item.id">
+            <template #default>
+              <div>
+                <img :src="'http://data.linesno.com/icons/llm/' + item.providerCode + '.png'" alt="图标" style="width: 25px; height: 25px; border-radius: 50%;">
+                {{ item.modelName }}
+              </div>
+            </template>
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <!-- 图片上传数限制 -->
+      <el-form-item v-if="multiModelOptions.length > 0 && formData.modelId" label="图片上传数" prop="imageUploadLimit">
+          <el-slider size="large" show-input v-model="formData.imageUploadLimit" :step="1" :max="10" />
+      </el-form-item>
+
+      <!-- 提交按钮 -->
+      <el-form-item>
+        <div style="width: 100%;text-align: right;margin-top:10px">
+          <el-button type="primary" @click="handleSubmit" size="large" text bg>确认</el-button>
+        </div>
+      </el-form-item>
+
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+
+import { nextTick, onUnmounted , ref } from 'vue';
+import { ElMessage } from 'element-plus'
+
+const emit = defineEmits(['handleAttachmentUploadStatusPanelClose'])
+
+// 初始化表单数据
+const formData = ref({
+  enable: true ,
+  modelId: '',
+  fileUploadLimit: 10,
+  imageUploadLimit: 0 // 默认图片上传数为 0
+});
+
+// 定义可用的语音模型数组
+const multiModelOptions = ref([]);
+
+// 表单验证规则
+const rules = ref({
+  enable: [
+    { required: true, message: '请确认是否选择模型', trigger: 'blur' }
+  ],
+  fileUploadLimit: [
+    { required: true, message: '请输入文件上传数量限制', trigger: 'change' }
+  ],
+  imageUploadLimit: [
+    { required: multiModelOptions.value.length > 0 && formData.value.modelId, message: '请输入图片上传数量限制', trigger: 'change' }
+  ]
+});
+
+// 获取表单实例
+const formRef = ref(null);
+
+const setMultiModalOptions = (models) => {
+  multiModelOptions.value = models;
+}
+
+const setConfigParams = (params) => {
+  formData.value = params ;
+}
+
+/** 获取到表单数据 */
+const getFormData = () => {
+  return formData.value
+}
+
+const handleSubmit = () => {
+    formRef.value.validate((valid) => {
+        if (valid) {
+            console.log('handleAttachmentUploadStatusPanelClose params = ' + JSON.stringify(formData.value));
+            emit("handleAttachmentUploadStatusPanelClose" , formData.value);
+            ElMessage.success('提交成功');
+        } else {
+            ElMessage.error('表单验证失败，请检查输入');
+        }
+    });
+};
+
+defineExpose({
+  setMultiModalOptions , 
+  getFormData,
+  setConfigParams
+})
+
+onUnmounted(() => {
+  // 这里 recorder 未定义，可能需要补充相关代码
+  // recorder.stop();
+  // recorder.destroy();
+})
+
+nextTick(() => {
+  // 初始化表单数据
+  formData.value.enable = true ;
+});
+
+</script>
+
+<style scoped lang="scss"></style>    
