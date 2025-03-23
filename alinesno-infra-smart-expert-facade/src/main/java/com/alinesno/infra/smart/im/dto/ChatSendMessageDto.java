20cc0c7f342a8ecc696b45100e6b1ccab508e356
周健(James) <jamesAip@gmail.com>
2025-03-24 00:12:13 +0800

    feat: 在 ChatSendMessageDto 类中新增 fileIds 属性以支持文件ID列表传输。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
@@ -11,6 +11,7 @@ public class ChatSendMessageDto implements Serializable {
     private String message;
     private List<Long> users; // Agent用户ID列表
     private List<Long> businessIds ;  // 业务ID列表
+    private List<Long> fileIds ; // 文件ID列表
     private Long channelId;
 
     private Long accountId ; // 操作账号ID
