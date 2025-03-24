f72a6913d1a9b39b0f83c45a14e80753f87f4153
周健(James) <jamesAip@gmail.com>
2025-03-24 19:36:00 +0800

    feat: 实现 PdfReaderServiceImpl 的 readAttachment 方法，支持读取 PDF 附件内容。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/PdfReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/PdfReaderServiceImpl.java
@@ -1,22 +1,39 @@
 package com.alinesno.infra.base.search.service.reader;
 
+import com.agentsflex.core.document.Document;
+import com.agentsflex.document.parser.PdfBoxDocumentParser;
 import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * PdfReaderServiceImpl 类是 PDF 附件读取服务的具体实现，
  * 它实现了 IAttachmentReaderService 接口，专门用于处理 PDF 附件的读取操作。
  * 通过该类可以根据附件的唯一标识来读取对应的 PDF 附件内容。
  */
-public class PdfReaderServiceImpl implements IAttachmentReaderService {
+@Service
+public class PdfReaderServiceImpl  extends BaseReaderServiceImpl {
 
     /**
      * 此方法用于根据给定的附件 ID 读取对应的 PDF 附件内容。
      *
-     * @param attachmentId 要读取的 PDF 附件的唯一标识符。
+     * @param attachmentDto 要读取的 PDF 附件的唯一标识符。
      * @return 返回读取到的 PDF 附件内容，若读取失败或无内容则返回 null。
      */
+    @SneakyThrows
     @Override
-    public String readAttachment(String attachmentId) {
-        return null;
+    public String readAttachment(FileAttachmentDto attachmentDto) {
+
+        File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+
+        FileInputStream stream = new FileInputStream(file);
+        PdfBoxDocumentParser parser = new PdfBoxDocumentParser();
+        Document document = parser.parse(stream);
+
+        return cleanText(document.getContent());
     }
 }
