e4b66620ea203c7ad278467555e8f2d5a1845d2d
周健(James) <jamesAip@gmail.com>
2025-03-24 19:36:00 +0800

    feat: 实现 ExcelReaderServiceImpl 类，扩展 BaseReaderServiceImpl 并调整读取方法参数为 FileAttachmentDto。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
@@ -1,22 +1,25 @@
 package com.alinesno.infra.base.search.service.reader;
 
 import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import org.springframework.stereotype.Service;
 
 /**
  * ExcelReaderServiceImpl 类是 Excel 附件读取服务的具体实现，
  * 它实现了 IAttachmentReaderService 接口，主要负责处理 Excel 附件的读取操作。
  * 该类提供了一个方法用于读取指定 Excel 附件的内容。
  */
-public class ExcelReaderServiceImpl implements IAttachmentReaderService {
+@Service
+public class ExcelReaderServiceImpl  extends BaseReaderServiceImpl {
 
     /**
      * 此方法用于读取指定 ID 的 Excel 附件的内容。
      *
-     * @param attachmentId 要读取的 Excel 附件的唯一标识符。
+     * @param attachmentDto 要读取的 Excel 附件的唯一标识符。
      * @return 返回读取到的 Excel 附件内容，如果读取失败或者没有内容则返回 null。
      */
     @Override
-    public String readAttachment(String attachmentId) {
+    public String readAttachment(FileAttachmentDto attachmentDto) {
         return null;
     }
 }
