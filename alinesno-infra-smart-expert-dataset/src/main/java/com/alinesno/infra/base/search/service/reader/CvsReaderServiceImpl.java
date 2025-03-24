ae120515e123bf076ef2a6764cf1e5514c8484cb
周健(James) <jamesAip@gmail.com>
2025-03-24 19:36:00 +0800

    feat: 实现 CvsReaderServiceImpl 类，继承 BaseReaderServiceImpl 并调整 readAttachment 方法参数为 FileAttachmentDto。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/CvsReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/CvsReaderServiceImpl.java
@@ -1,22 +1,25 @@
 package com.alinesno.infra.base.search.service.reader;
 
 import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import org.springframework.stereotype.Service;
 
 /**
  * CvsReaderServiceImpl 类是 CSV 附件读取服务的具体实现，
  * 它实现了 IAttachmentReaderService 接口，用于处理 CSV 附件的读取操作。
  * 该类提供了读取指定 CSV 附件内容的功能，具体实现逻辑由 readAttachment 方法完成。
  */
-public class CvsReaderServiceImpl implements IAttachmentReaderService {
+@Service
+public class CvsReaderServiceImpl extends BaseReaderServiceImpl {
 
     /**
      * 此方法用于读取指定 ID 的 CSV 附件内容。
      *
-     * @param attachmentId 要读取的 CSV 附件的唯一标识符。
+     * @param attachmentDto 要读取的 CSV 附件的唯一标识符。
      * @return 返回读取到的 CSV 附件内容，若读取失败或无内容则返回 null。
      */
     @Override
-    public String readAttachment(String attachmentId) {
+    public String readAttachment(FileAttachmentDto attachmentDto) {
         return null;
     }
 }
