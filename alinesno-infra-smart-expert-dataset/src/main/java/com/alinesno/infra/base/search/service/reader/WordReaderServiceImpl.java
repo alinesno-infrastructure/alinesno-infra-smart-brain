91d6df9d39aa1047b3c4595068bb7cff20ab5ab7
周健(James) <jamesAip@gmail.com>
2025-03-24 19:36:00 +0800

    feat: 实现 WordReaderServiceImpl 的 readAttachment 方法，支持读取 Word 附件并解析章节内容。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/WordReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/WordReaderServiceImpl.java
@@ -1,21 +1,40 @@
 package com.alinesno.infra.base.search.service.reader;
 
-import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import com.spire.doc.collections.SectionCollection;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+import com.spire.doc.Document;
+import com.spire.doc.collections.SectionCollection;
+
+import java.io.File;
 
 /**
  * WordReaderServiceImpl 类是 Word 附件读取服务的具体实现，
  * 该类实现了 IAttachmentReaderService 接口，主要负责处理 Word 格式附件的读取操作。
  */
-public class WordReaderServiceImpl implements IAttachmentReaderService {
+@Service
+public class WordReaderServiceImpl  extends BaseReaderServiceImpl {
 
     /**
      * 此方法用于读取指定 ID 的 Word 附件内容。
+     * 处理思路，解析出文档的菜单结构，返回每个章节的内容，然后再将每个章节内容整理成上下文会话
      *
-     * @param attachmentId 要读取的 Word 附件的唯一标识符。
+     * @param attachmentDto 要读取的 Word 附件的唯一标识符。
      * @return 返回读取到的 Word 附件内容，若读取失败或者附件无内容则返回 null。
      */
+    @SneakyThrows
     @Override
-    public String readAttachment(String attachmentId) {
-        return null;
+    public String readAttachment(FileAttachmentDto attachmentDto) {
+        // 解析word文档的目录结构
+        File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+
+        //加载包含目录的Word文档
+        Document doc = new Document();
+        doc.loadFromFile(file.getAbsolutePath()) ;
+
+        // 获取到所有内容，并去掉所有空格
+        return cleanText(doc.getText());
     }
+
 }
