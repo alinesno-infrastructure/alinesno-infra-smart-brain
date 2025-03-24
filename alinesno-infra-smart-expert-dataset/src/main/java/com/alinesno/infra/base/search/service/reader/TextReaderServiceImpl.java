28c09c2e120566610e268c23bfee5d4ad1317468
周健(James) <jamesAip@gmail.com>
2025-03-24 19:36:00 +0800

    feat: 实现 TextReaderServiceImpl 的 readAttachment 方法，支持读取文本附件内容并清理格式。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/TextReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/TextReaderServiceImpl.java
@@ -1,21 +1,35 @@
 package com.alinesno.infra.base.search.service.reader;
 
 import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 /**
  * TextReaderServiceImpl 类是文本附件读取服务的具体实现，
  * 该类实现了 IAttachmentReaderService 接口，主要负责处理文本格式附件的读取操作。
  */
-public class TextReaderServiceImpl implements IAttachmentReaderService {
+@Service
+public class TextReaderServiceImpl  extends BaseReaderServiceImpl {
 
     /**
      * 此方法用于读取指定 ID 的文本附件内容。
      *
-     * @param attachmentId 要读取的文本附件的唯一标识符。
+     * @param attachmentDto 要读取的文本附件的唯一标识符。
      * @return 返回读取到的文本附件内容，若读取失败或无内容则返回 null。
      */
+    @SneakyThrows
     @Override
-    public String readAttachment(String attachmentId) {
-        return null;
+    public String readAttachment(FileAttachmentDto attachmentDto) {
+        File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+        // 读取文件内容
+        if (file != null) {
+            String content = FileUtils.readFileToString(file, "UTF-8") ;
+            return cleanText(content);
+        }
+        return null ;
     }
 }
