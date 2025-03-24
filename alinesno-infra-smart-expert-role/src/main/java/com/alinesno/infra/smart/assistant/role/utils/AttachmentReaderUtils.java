31fbfb715a96d4c53022d8b4604234552777ed15
周健(James) <jamesAip@gmail.com>
2025-03-24 19:35:59 +0800

    feat:新增 AttachmentReaderUtils 工具类，用于读取和解析附件内容。

---
+++ alinesno-infra-smart-expert-role/src/main/java/com/alinesno/infra/smart/assistant/role/utils/AttachmentReaderUtils.java
@@ -0,0 +1,58 @@
+package com.alinesno.infra.smart.assistant.role.utils;
+
+import cn.hutool.extra.spring.SpringUtil;
+import com.alinesno.infra.base.search.service.reader.*;
+import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 附件内容读取工具
+ */
+@Component
+public class AttachmentReaderUtils {
+
+
+    /**
+     * 读取附件列表，并解析出每个附件的内容出来
+     * @param attachments
+     * @return
+     */
+    public List<FileAttachmentDto> readAttachmentList(List<FileAttachmentDto> attachments){
+
+        List<FileAttachmentDto> newAttachments = new ArrayList<>() ;
+
+        // 解析内容并过滤为空的数据
+        for(FileAttachmentDto attachment : attachments){
+
+            String fileType = attachment.getFileType() ;
+            IAttachmentReaderService attachmentReaderService = getAttachmentReaderService(fileType) ;
+            if(attachmentReaderService != null){
+                String fileContent = attachmentReaderService.readAttachment(attachment) ;
+
+                if(fileContent != null && !fileContent.isEmpty()){
+                    attachment.setFileContent(fileContent);
+                    newAttachments.add(attachment) ;
+                }
+            }
+        }
+
+        return newAttachments ;
+    }
+
+    public IAttachmentReaderService getAttachmentReaderService(String fileType) {
+        return switch (fileType) {
+            case "txt" -> SpringUtil.getBean(TextReaderServiceImpl.class);
+            case "md" -> SpringUtil.getBean(MarkdownReaderServiceImpl.class);
+            case "pdf" -> SpringUtil.getBean(PdfReaderServiceImpl.class);
+            case "doc", "docx" -> SpringUtil.getBean(WordReaderServiceImpl.class);
+            case "xls", "xlsx" -> SpringUtil.getBean(ExcelReaderServiceImpl.class);
+            case "png", "jpg", "jpeg" -> SpringUtil.getBean(ImageReaderServiceImpl.class);
+            default -> null;
+        };
+    }
+}
