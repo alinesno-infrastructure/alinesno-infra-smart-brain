ae9615490a3d0078a6b495ec0f76382c7a2aa5ed
王磊(Levi) <leviAip@gmail.com>
2025-03-25 22:27:54 +0800

    feat:新增 IndustryRoleDraftMapper 接口，继承 IBaseMapper 定义行业角色草稿数据映射。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/IndustryRoleDraftMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleDraftEntity;
+
+public interface IndustryRoleDraftMapper extends IBaseMapper<IndustryRoleDraftEntity> {
+
+}
