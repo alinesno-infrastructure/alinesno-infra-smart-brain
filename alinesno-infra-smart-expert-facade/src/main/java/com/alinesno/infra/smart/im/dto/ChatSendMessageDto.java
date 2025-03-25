11e5f84251c6106f5bc22d1651cbbee05e56662c
王磊(Levi) <leviAip@gmail.com>
2025-03-25 22:27:54 +0800

    feat:初始化 ChatSendMessageDto 数据传输对象，完善属性并设置默认值。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
@@ -3,15 +3,16 @@ package com.alinesno.infra.smart.im.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ChatSendMessageDto implements Serializable {
 
     private String message;
-    private List<Long> users; // Agent用户ID列表
-    private List<Long> businessIds ;  // 业务ID列表
-    private List<Long> fileIds ; // 文件ID列表
+    private List<Long> users = new ArrayList<>() ; // Agent用户ID列表
+    private List<Long> businessIds = new ArrayList<>() ;  // 业务ID列表
+    private List<Long> fileIds = new ArrayList<>(); // 文件ID列表
     private Long channelId;
 
     private Long accountId ; // 操作账号ID
