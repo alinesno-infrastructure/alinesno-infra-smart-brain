fb30a20d709655ab6ef38dff157c4e16e3016ab1
刘芳(Luna) <lunaAip@gmail.com>
2025-03-27 00:15:47 +0800

    feat: 增加 PdfReaderServiceImpl 方法参数，支持传入 UploadData 对象。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/PdfReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/PdfReaderServiceImpl.java
@@ -2,7 +2,7 @@ package com.alinesno.infra.base.search.service.reader;
 
 import com.agentsflex.core.document.Document;
 import com.agentsflex.document.parser.PdfBoxDocumentParser;
-import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -22,11 +22,12 @@ public class PdfReaderServiceImpl  extends BaseReaderServiceImpl {
      * 此方法用于根据给定的附件 ID 读取对应的 PDF 附件内容。
      *
      * @param attachmentDto 要读取的 PDF 附件的唯一标识符。
+     * @param uploadData
      * @return 返回读取到的 PDF 附件内容，若读取失败或无内容则返回 null。
      */
     @SneakyThrows
     @Override
-    public String readAttachment(FileAttachmentDto attachmentDto) {
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
 
         File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
 
