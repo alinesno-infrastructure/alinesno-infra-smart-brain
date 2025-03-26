7a15dfb366ab23e6ed8c6840566c6badd8529a35
刘芳(Luna) <lunaAip@gmail.com>
2025-03-27 00:15:47 +0800

    feat:新增 `readAttachment` 方法参数，支持传入 `UploadData` 对象以增强功能。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/MarkdownReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/MarkdownReaderServiceImpl.java
@@ -1,6 +1,6 @@
 package com.alinesno.infra.base.search.service.reader;
 
-import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
@@ -20,11 +20,12 @@ public class MarkdownReaderServiceImpl extends BaseReaderServiceImpl {
      * 该方法用于读取指定 ID 的 Markdown 附件内容。
      *
      * @param attachmentDto 要读取的 Markdown 附件的唯一标识符。
+     * @param uploadData
      * @return 返回读取到的 Markdown 附件内容，如果读取失败或者附件无内容则返回 null。
      */
     @SneakyThrows
     @Override
-    public String readAttachment(FileAttachmentDto attachmentDto) {
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
         File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
         // 读取文件内容
         if (file != null) {
