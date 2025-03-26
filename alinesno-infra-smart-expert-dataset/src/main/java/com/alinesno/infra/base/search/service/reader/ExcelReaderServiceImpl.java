cb0b34260eb0f41a149b167e8074e28fae4ae0c8
刘芳(Luna) <lunaAip@gmail.com>
2025-03-27 00:15:47 +0800

    feat: 修改 `ExcelReaderServiceImpl` 方法签名，新增 `UploadData` 参数支持。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
@@ -1,6 +1,6 @@
 package com.alinesno.infra.base.search.service.reader;
 
-import com.alinesno.infra.smart.assistant.service.IAttachmentReaderService;
+import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +16,11 @@ public class ExcelReaderServiceImpl  extends BaseReaderServiceImpl {
      * 此方法用于读取指定 ID 的 Excel 附件的内容。
      *
      * @param attachmentDto 要读取的 Excel 附件的唯一标识符。
+     * @param uploadData
      * @return 返回读取到的 Excel 附件内容，如果读取失败或者没有内容则返回 null。
      */
     @Override
-    public String readAttachment(FileAttachmentDto attachmentDto) {
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
         return null;
     }
 }
