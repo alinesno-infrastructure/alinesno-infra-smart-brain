7ef93733c970b6ec97d0dc1d99897ab4ce472248
王磊(Levi) <leviAip@gmail.com>
2025-03-27 00:15:47 +0800

    feat:新增图片识别相关字段，包括 `recognitionType` 和 `modelId`。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/UploadData.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/UploadData.java
@@ -9,8 +9,11 @@ import lombok.Data;
 public class UploadData {
 
     private boolean enable; // 是否启用
-    private String modelId;  // 多模态模型ID
     private int fileUploadLimit;  // 文件上传限制
+
+    // -->>>>>>>>>>>>> 图片识别处理 -->>>>>>>>>>>>
+    private String recognitionType; // 识别方式(ocr/大模型)
+    private String modelId;  // 多模态模型ID
     private int imageUploadLimit;  // 图片上传限制
 
 }
