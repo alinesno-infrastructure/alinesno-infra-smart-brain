d7b01db9e5346401f0b17a8d8e8fa65402844da8
王磊(Levi) <leviAip@gmail.com>
2025-03-27 00:15:47 +0800

    feat:新增 OCR 和视觉相关模型接口，扩展 ILLmAdapterService 功能。

--- alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/service/ILLmAdapterService.java
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/service/ILLmAdapterService.java
@@ -4,10 +4,14 @@ import com.agentsflex.core.image.ImageConfig;
 import com.agentsflex.core.image.ImageModel;
 import com.agentsflex.core.llm.Llm;
 import com.agentsflex.core.llm.LlmConfig;
+import com.agentsflex.core.ocr.OcrConfig;
+import com.agentsflex.core.ocr.OcrModel;
+import com.agentsflex.core.ocr.OcrResponse;
 import com.agentsflex.core.reranker.ReRanker;
 import com.agentsflex.core.reranker.ReRankerConfig;
 import com.agentsflex.core.speech.SpeechConfig;
 import com.agentsflex.core.speech.SpeechModel;
+import com.agentsflex.core.vision.BaseVision;
 
 /**
  * LLmAdapter 大模型适配器
@@ -46,5 +50,15 @@ public interface ILLmAdapterService {
      * @return
      */
     SpeechModel speechModel(String type ,  SpeechConfig config);
+
+    /**
+     * 获取OCR模型
+     */
+    OcrModel ocrModel(String type , OcrConfig config);
+
+    /**
+     * 获取视觉模型
+     */
+    Llm visionModel(String type , LlmConfig config)  ;
 
 }
