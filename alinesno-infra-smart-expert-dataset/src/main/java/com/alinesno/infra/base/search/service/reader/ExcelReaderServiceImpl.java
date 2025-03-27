4d932ea041226b5f742e5ddbe9c8d52b1d5df9e0
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 21:23:34 +0800

    feat:为 ExcelReaderServiceImpl 添加 @Scope("prototype") 注解，优化实例化行为。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/ExcelReaderServiceImpl.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.base.search.service.reader;
 
 import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
  * 它实现了 IAttachmentReaderService 接口，主要负责处理 Excel 附件的读取操作。
  * 该类提供了一个方法用于读取指定 Excel 附件的内容。
  */
+@Scope("prototype")
 @Service
 public class ExcelReaderServiceImpl  extends BaseReaderServiceImpl {
 
