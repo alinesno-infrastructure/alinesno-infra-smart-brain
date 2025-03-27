772af32c2ac1772f89e05bfe03b3120f2076ed38
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 21:23:33 +0800

    feat:新增 ScreenTypeEnums 枚举类，定义屏幕类型的枚举值及解析方法。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/core/enums/ScreenTypeEnums.java
@@ -0,0 +1,36 @@
+package com.alinesno.infra.smart.assistant.screen.core.enums;
+
+import lombok.Getter;
+
+/**
+ * ScreenTypeEnums 枚举类
+ */
+@Getter
+public enum ScreenTypeEnums {
+
+    LARGE_TEXT("large_text","大文本"),
+    EXAM("exam","培训考试"),
+    VIDEO_CLIP("video_clip","视频剪辑"),
+    LEADER_MODEL("leader_model","管理者");
+
+    private final String key;
+    private final String name;
+
+    private ScreenTypeEnums(String key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    /**
+     * 通过key获取到类型
+     */
+    public static ScreenTypeEnums getByKey(String key) {
+        for (ScreenTypeEnums type : values()) {
+            if (type.getKey().equals(key)) {
+                return type;
+            }
+        }
+        return null;
+    }
+
+}
