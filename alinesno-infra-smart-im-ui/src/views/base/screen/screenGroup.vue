d45fd5ccb3cd38adbc1136c87e64bd7d0aee8a77
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 21:23:33 +0800

    refactor: 优化创建场景对话框布局，调整表单结构并简化样式。

--- alinesno-infra-smart-im-ui/src/views/base/screen/screenGroup.vue
+++ alinesno-infra-smart-im-ui/src/views/base/screen/screenGroup.vue
@@ -1,255 +1,154 @@
 <template>
+  <!-- 创建多个企业场景 -->
+  <el-dialog v-model="centerDialogVisible" title="创建场景" width="1200" :append-to-body="true" align-center>
+    <div>
+      <div style="margin-bottom:10px">
+        请选择合适的场景，并创建场景。
+      </div>
+      <div :size="large" style="flex-wrap: wrap;align-items: center;gap: 10px;width: 100%;display: flex;justify-content: flex-start;">
+        <div  v-for="item in sceneList" :key="item.id" class="box-card" style="width: calc(20% - 10px);border-radius: 10px;" shadow="never">
+          <div style="padding: 5px;background: rgb(33 36 36);border-radius: 5px;gap:7px; text-align: left;display: flex;flex-direction: column;flex-wrap: nowrap;justify-content: center;align-items: flex-start;cursor: pointer;">
 
-  <el-dialog v-model="centerDialogVisible" title="创建场景" width="30%" :append-to-body="true" align-center>
-
-    <div style="height:420px;padding-top:20px;">
-        <el-form :model="form" :rules="rules" label-position="top" ref="userRef" label-width="80px">
-          <el-row>
-            <el-col :span="24" class="editor-after-div">
-              <el-form-item label="封面" prop="screenBanner">
-                  <el-upload
-                    :file-list="imageUrl"
-                    :action="upload.url + '?type=img&updateSupport=' + upload.updateSupport"
-                    list-type="picture-card"
-                    :auto-upload="true"
-                    :on-success="handleAvatarSuccess"
-                    :before-upload="beforeAvatarUpload"
-                    :headers="upload.headers"
-                    :disabled="upload.isUploading"
-                    :on-progress="handleFileUploadProgress"
-                  >
-                    <el-icon class="avatar-uploader-icon"><Plus /></el-icon>
-                  </el-upload>
-                </el-form-item>
-            </el-col>
-          </el-row>
-          <el-col :span="24">
-            <el-form-item label="场景类型" prop="screenType">
-              <el-radio-group v-model="form.screenType">
-                <el-radio v-for="item in screenTypes" 
-                    :disabled="!item.isAvailable"
-                    :key="item.key" 
-                    :label="item.key">{{ item.name }}</el-radio>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
-          <el-row>
-              <el-col :span="24">
-                <el-form-item label="场景名称" prop="screenName">
-                    <el-input size="large" v-model="form.screenName" placeholder="请输入场景名称" maxlength="30" />
-                </el-form-item>
-              </el-col>
-          </el-row>
-          <el-row>
-              <el-col :span="24">
-                <el-form-item label="备注" prop="screenDesc">
-                    <el-input size="large" v-model="form.screenDesc" placeholder="请输入备注"></el-input>
-                </el-form-item>
-              </el-col>
-          </el-row>
-
-        </el-form> 
-        <div class="dialog-footer">
-            <el-button type="primary" size="large" bg text @click="submitScreenForm" style="float:right;margin-top:20px" icon="Link">创建场景</el-button>
+            <div class="screen-banner">
+              <img :src="'http://data.linesno.com/icons/screen/'+item.id+'.webp'" style="width:100%;border-radius: 5px;;" />
+            </div>
+            <div class="screen-title" style="font-size:15px;font-weight: bold;">
+              <span>{{ item.sceneName }}</span>
+            </div>
+            <div class="screen-description" style="height:50px;">
+              {{ item.sceneDescription }}
+            </div>
+            <!-- <div class="screen-tags">
+              <el-button type="primary" text bg size="small">报表</el-button>
+              <el-button type="primary" text bg size="small">办公</el-button>
+            </div> -->
+            <div class="screen-info" style="
+    font-size: 12px;
+    float: right;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    padding-right:10px;
+    margin-bottom: 10px;">
+              <!-- <span>
+                使用次数1200 
+              </span> -->
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="dialog-footer">
+        <el-button type="primary" size="large" bg text @click="submitScreenForm" style="float:right;margin-top:20px" icon="Link">创建场景</el-button>
+      </div>
     </div>
-
-    <template #footer>
-      <span class="dialog-footer">
-      </span>
-    </template>
-
   </el-dialog>
-
 </template>
 
 <script setup>
 
-let { proxy } = getCurrentInstance();
-import {getToken} from "@/utils/auth";
-
-import {
-  createScreen, 
-} from '@/api/base/im/screen'
-
-import { reactive, ref , onMounted, nextTick } from 'vue'
-import { ElLoading } from 'element-plus'
-
-const centerDialogVisible = ref(false)
+const centerDialogVisible = ref(false);
 const router = useRouter();
+const large = 'large';
 
-const imageUrl = ref([])
-
-const screenTypes = ref([
-  { key: 'large_text', name: '文档编写', isAvailable: true },
-  { key: 'leader_model', name: '管理者', isAvailable: true },
-  { key: 'video_clip', name: '图片视频', isAvailable: true },
-  { key: 'exam', name: '试题生成', isAvailable: true }
-]);
-
-
-const data = reactive({
-  form: {
-    channelType: '2'
+const sceneList = [
+  {
+    id: 1,
+    sceneName: '长文本生成',
+    englishName: 'LongText',
+    sceneDescription: '主要用于合同、法律、招投标书编写等长文本场景',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
   },
-  rules: {
-    screenName: [{ required: true, message: "场景名称不能为空", trigger: "blur" }],
-    screenDesc: [{ required: true, message: "场景描述不能为空", trigger: "blur" }],
-    screenType: [{ required: true, message: "场景类型不能为空", trigger: "blur" }],
+  {
+    id: 2,
+    sceneName: '领导者场景',
+    englishName: 'LeaderModel',
+    sceneDescription: '主要用于管理者调动多个智能体去完成一个工作的场景，针对于这个场景进行结果的收集和进一步反馈',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
+  },
+  {
+    id: 3,
+    sceneName: '企业客服场景',
+    englishName: 'CustomerService',
+    sceneDescription: '一键导入企业官网或者上传知识库，自动生成企业客服角色【待定】',
+    integrationProgress: '待定',
+    isMature: '否',
+    bannerLink: ''
+  },
+  {
+    id: 4,
+    sceneName: '文档审核场景',
+    englishName: 'DocumentReview',
+    sceneDescription: '上传一份文档进行文档审核和修改建议，可以配置多个审核的Agent角色',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
+  },
+  {
+    id: 5,
+    sceneName: '文档阅读场景',
+    englishName: 'DocumentReading',
+    sceneDescription: '针对于大文档类型的阅读，归纳总结等，查询内容，或者提供对应的建议等 ',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
+  },
+  {
+    id: 6,
+    sceneName: '生成试卷场景',
+    englishName: 'ExamPaperGeneration',
+    sceneDescription: '根据培训情况，自动生成试卷内容，并导出Word内容',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
+  },
+  {
+    id: 7,
+    sceneName: '文案生成场景',
+    englishName: 'Copywriting',
+    sceneDescription: '可以根据所提供的模板，自动生成文案信息，比较年度报告、合作协议等复杂文件初稿【待定】',
+    integrationProgress: '待定',
+    isMature: '否',
+    bannerLink: ''
+  },
+  {
+    id: 8,
+    sceneName: '会议纪要场景',
+    englishName: 'MeetingMinutes',
+    sceneDescription: '自动生成会议纪要，生成专业报告并优化排版，根据上传的模板生成会议纪要',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
+  },
+  {
+    id: 9,
+    sceneName: '产品研究场景',
+    englishName: 'ProductResearch',
+    sceneDescription: '自动接入Git或者禅道等，获取到进度更新情况，并进行汇总归纳',
+    integrationProgress: '已集成',
+    isMature: '是',
+    bannerLink: ''
   }
-});
+];
 
-const { form, rules } = toRefs(data);
-
-/*** 应用导入参数 */
-const upload = reactive({
-  // 是否显示弹出层（应用导入）
-  open: false,
-  // 弹出层标题（应用导入）
-  title: "",
-  // 是否禁用上传
-  isUploading: false,
-  // 是否更新已经存在的应用数据
-  updateSupport: 0,
-  // 设置上传的请求头部
-  headers: {Authorization: "Bearer " + getToken()},
-  // 上传的地址
-  url: import.meta.env.VITE_APP_BASE_API + "/v1/api/infra/base/im/chat/importData", 
-  // 显示地址
-  display: import.meta.env.VITE_APP_BASE_API + "/v1/api/infra/base/im/chat/displayImage/" 
-});
-
-const reset = () => {
-  form.value = {
-    screenBanner: null , 
-    channelId: undefined,
-    screenName: undefined,
-    screenType: undefined,
-    screenDesc: undefined,
-  };
-  proxy.resetForm("userRef");
-}
-
-/** 设置ChannelId */
-function handleSetScreenId(screenId){
-
-  if(form.value.screenType === 'leader_model'){  // 领导模型
-    router.push({
-        path: '/screen/leaderModel',
-        query: { 'screenId': screenId}
-    })
-  }else if(form.value.screenType === 'exam'){
-    router.push({
-        path: '/screen/exam',
-        query: { 'screenId': screenId}
-    })
-  }else if(form.value.screenType === 'video_clip'){
-    router.push({
-        path: '/screen/mediaClip',
-        query: { 'screenId': screenId}
-    })
-  }else{  // 默认长文本模型
-    router.push({
-        path: '/screen/longText',
-        query: { 'screenId': screenId}
-    })
-  }
-}
-
-/** 图片上传成功 */
-const handleAvatarSuccess = (response, uploadFile) => {
-  // imageUrl.value = URL.createObjectURL(uploadFile.raw);
-  imageUrl.value = response.data ? response.data.split(',').map(url =>{return { url:upload.display + url }}):[];
-  form.value.screenBanner = response.data ;
-  console.log('form.icon = ' + form.value.screenBanner);
-};
-
-/** 图片上传之前 */
-const beforeAvatarUpload = (rawFile) => {
-  if (rawFile.size / 1024 / 1024 > 2) {
-    ElMessage.error('Avatar picture size can not exceed 2MB!');
-    return false;
-  }
-  return true;
-};
-
-/** 创建场景 */
-function submitScreenForm(){
-  proxy.$refs["userRef"].validate(valid => {
-    if (valid) {
-      const loading = ElLoading.service({
-        lock: true,
-        text: 'Loading',
-        background: 'rgba(0, 0, 0, 0.2)',
-      })
-      createScreen(form.value).then(response => {
-        proxy.$modal.msgSuccess("新增成功");
-        // loading.close();
-        centerDialogVisible.value = false ;
-        loading.close() ;
-
-        handleSetScreenId(response.data) ;
-      });
-    }
-  });
-}
-
-
-function handleOpenChannel(val){
-  imageUrl.value = [];
-  reset();
+const handleOpenChannel = (val) => {
   centerDialogVisible.value = val;
-}
+};
+
+const submitScreenForm = () => {
+  // 这里可以添加创建场景的逻辑
+  console.log('创建场景');
+};
 
 defineExpose({
   handleOpenChannel,
-})
-
+  submitScreenForm
+});
 </script>
 
 <style lang="scss" scoped>
-.channel-box-panel {
-
-  .channel-item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    padding: 10px;
-    position: relative;
-    background: #fafafa;
-    border-radius: 5px;
-  }
-
-  .channel-image {
-    width: 35px;
-    height: 35px;
-    border-radius: 50%;
-    margin-right: 10px;
-  }
-
-  .channel-text {
-    font-size: 14px;
-    font-weight: bold;
-    color: #333;
-  }
-
-  .channel-desc {
-    margin-left: 10px;
-    font-size: 13px;
-  }
-
-  .icon-btn{
-    margin-right: 10px;
-  }
-
-}
-
-.hidden-box {
-  display: none;
-}
-
-.show-box {
-  display: block;
-}
 
 </style>
