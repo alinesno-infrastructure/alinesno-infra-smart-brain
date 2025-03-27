b52f66dc138c37f6b47515f830e064176eece4e7
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 21:23:33 +0800

    feat:新增 ChapterMapper 接口，实现 ChapterEntity 的基础映射功能。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/scene/longtext/mapper/ChapterMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.screen.scene.longtext.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.screen.core.entity.ChapterEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChapterMapper extends IBaseMapper<ChapterEntity> {
+}
