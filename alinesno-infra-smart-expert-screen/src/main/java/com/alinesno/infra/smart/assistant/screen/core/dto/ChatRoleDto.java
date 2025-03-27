fd2baf01ce7e7df5b0aacaec9f1fdcda5abd32d6
李伟(Michael) <michaelAip@gmail.com>
2025-03-27 21:23:33 +0800

    feat:新增 ChatRoleDto 数据传输对象，包含场景ID、消息内容和角色ID等字段。

---
+++ alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/core/dto/ChatRoleDto.java
@@ -0,0 +1,24 @@
+package com.alinesno.infra.smart.assistant.screen.core.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import java.io.Serializable;
+
+/**
+ * 与角色交互的数据传输对象
+ */
+@Data
+public class ChatRoleDto implements Serializable {
+
+    @Min(value = 1, message = "screenId不能为空")
+    private long screenId ;  // 场景ID
+
+    @NotBlank(message = "message不能为空")
+    private String message ; // 消息内容
+
+    @Min(value = 1, message = "channelId不能为空")
+    private long roleId ; // 角色ID
+
+}
