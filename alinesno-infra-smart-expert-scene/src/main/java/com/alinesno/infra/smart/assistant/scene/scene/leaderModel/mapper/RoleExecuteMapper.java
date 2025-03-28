5220dc92f0b69ee6f4252245bd48941745f3db04
李娜(Allen) <allenAip@gmail.com>
2025-03-29 00:18:36 +0800

    feat:新增 RoleExecuteMapper 接口，实现角色执行实体的数据库操作映射。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/leaderModel/mapper/RoleExecuteMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.leaderModel.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.scene.core.entity.RoleExecuteEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RoleExecuteMapper extends IBaseMapper<RoleExecuteEntity> {
+}
