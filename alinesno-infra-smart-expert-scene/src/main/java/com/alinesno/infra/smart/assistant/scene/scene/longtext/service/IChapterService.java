4ae303d7a3e9bc01cd820b61f8af056c4223a22c
李娜(Allen) <allenAip@gmail.com>
2025-03-29 00:18:36 +0800

    feat:新增 IChapterService 接口，包含章节层级保存、树形结构获取及编辑者更新功能。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/service/IChapterService.java
@@ -0,0 +1,41 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.core.entity.ChapterEntity;
+import com.alinesno.infra.smart.scene.dto.ChatContentEditDto;
+import com.alinesno.infra.smart.scene.dto.TreeNodeDto;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IChapterService extends IBaseService<ChapterEntity> {
+
+    /**
+     * 根据层级结构保存章节及其父ID
+     *
+     * @param chapters 章节列表，包含章节信息
+     * @param parentId 父章节的ID，用于建立章节间的层级关系
+     * @param level    章节的层级，表示章节在层级结构中的深度
+     * @param sceneId
+     */
+    void saveChaptersWithHierarchy(List<TreeNodeDto> chapters, Long parentId, int level, long sceneId);
+
+    /**
+     * 获取章节的树形结构
+     *
+     * @return 返回一个表示章节层级结构的树形列表，每个节点包含章节信息
+     */
+    List<TreeNodeDto> getChapterTree(long sceneId);
+
+    /**
+     * 更新章节的编辑者
+     * @param dto
+     */
+    void updateChapterEditor(ChatContentEditDto dto);
+
+}
