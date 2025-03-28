2071903abffe3c0f113de10e6898fd42460276b7
李娜(Allen) <allenAip@gmail.com>
2025-03-29 00:18:35 +0800

    更新:alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/package-info.java

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * 场景工具类，集成场景如下:<br/>
- * - LongText: 长文本场景，主要用于合同、法律、招投标书等长文本场景
- * - LeaderModel: 领导者场景，主要用于管理者调动多个智能体去完成一个工作的场景，针对于这个场景进行结果的收集和进一步反馈
- */
-package com.alinesno.infra.smart.assistant.screen;
