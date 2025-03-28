3d97cd6f668b0ba1c06056c8a8cfa22440a53a7d
李娜(Allen) <allenAip@gmail.com>
2025-03-29 00:18:34 +0800

    更新:alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/scene/leader/mapper/RoleExecuteMapper.java

--- alinesno-infra-smart-expert-screen/src/main/java/com/alinesno/infra/smart/assistant/screen/scene/leader/mapper/RoleExecuteMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.smart.assistant.screen.scene.leader.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.screen.core.entity.RoleExecuteEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RoleExecuteMapper extends IBaseMapper<RoleExecuteEntity> {
-}
