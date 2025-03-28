6f5278536201df6ad32eb8d012837a0b5eba1b0a
李娜(Allen) <allenAip@gmail.com>
2025-03-29 00:18:37 +0800

    feat:新增 IRoleExecuteService 接口，包含任务保存、任务完成状态判断及任务内容获取等方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/leaderModel/service/IRoleExecuteService.java
@@ -0,0 +1,44 @@
+package com.alinesno.infra.smart.assistant.scene.scene.leaderModel.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.core.entity.RoleExecuteEntity;
+import com.alinesno.infra.smart.scene.dto.RoleTaskDto;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IRoleExecuteService extends IBaseService<RoleExecuteEntity> {
+
+    /**
+     * 更新任务状态
+     *
+     * @param sceneId
+     * @param tasks
+     */
+    void saveNewTasks(long sceneId, List<RoleTaskDto> tasks);
+
+    /**
+     * 是否当前频道的任务全部完成
+     * @param sceneId
+     * @return
+     */
+    boolean isAllTaskCompleted(Long sceneId);
+
+    /**
+     * 标识任务已经完成
+     * @param id
+     */
+    void finishTask(Long id);
+
+    /**
+     * 获取前一步任务的内容
+     * @param preRoleId
+     * @return
+     */
+    String getPreContent(String preRoleId , long sceneId);
+}
