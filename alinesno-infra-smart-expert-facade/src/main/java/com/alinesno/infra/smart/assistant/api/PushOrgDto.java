b56c86c8a539e52d3b7e493183271934887a0061
张婷(Tina) <tinaAip@gmail.com>
2025-03-30 00:15:00 +0800

    feat:新增 PushOrgDto 类中 pushType 字段，并添加非空校验注解。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/PushOrgDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/PushOrgDto.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.smart.assistant.api;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -11,4 +12,7 @@ public class PushOrgDto {
 
     @Min(value = 1, message = "组织ID不能为空")
     private long orgId;
+
+    @NotBlank(message = "推送方式不能为空")
+    private String pushType ;
 }
