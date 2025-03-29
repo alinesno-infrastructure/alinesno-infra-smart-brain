79165208150ccfa6a090d7aab82a3fe2bdd82ed5
张婷(Tina) <tinaAip@gmail.com>
2025-03-30 00:14:59 +0800

    feat:新增 im/store.js 文件，实现商店角色及类型相关接口封装。

---
+++ alinesno-infra-smart-im-ui/src/api/base/im/store.js
@@ -0,0 +1,42 @@
+import request from '@/utils/request'
+
+const prefix = '/v1/api/infra/base/im/store'
+
+const managerUrl = {
+    getAgentStoreType: prefix + '/getAgentStoreType',
+    getAgentStoreRole: prefix + '/getAgentStoreRole',
+    storeRoleList: prefix + '/storeRoleList'
+}
+
+// 查询商店角色列表 
+export function storeRoleList(query) {
+    return request({
+      url: managerUrl.storeRoleList ,
+      method: 'post',
+      params: query
+    })
+  }
+
+// 获取所有商店类型
+export function getAgentStoreType() {
+    return request({
+        url: managerUrl.getAgentStoreType,
+        headers: {
+            isEncrypt: true
+        },
+        method: 'get'
+    })
+}
+
+// 获取所有商店里面的角色
+export function getAgentStoreRole(page) {
+    return request({
+        url: managerUrl.getAgentStoreRole,
+        headers: {
+            isEncrypt: true
+        },
+        method: 'get',
+        params: page
+    })
+}
+    
