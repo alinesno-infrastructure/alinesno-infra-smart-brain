7a163e6f2047af2f463c965055d455cc31fac1cc
张婷(Tina) <tinaAip@gmail.com>
2025-03-30 00:14:59 +0800

    feat:新增场景管理相关接口封装，包含创建、查询、更新等功能。

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/assistant/scene.js
@@ -0,0 +1,185 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/scene/' ;
+var managerUrl = {
+  datatables : prefix +"datatables" ,
+  createUrl: prefix + 'add' ,
+  saveUrl: prefix + 'createScene' ,
+  updateUrl: prefix +"modify" ,
+  statusUrl: prefix +"changeStatus" ,
+  cleanUrl: prefix + "clean",
+  detailUrl: prefix +"detail",
+  removeUrl: prefix + "delete" ,
+  removeScene: prefix + "removeScene" ,
+  exportUrl: prefix + "exportExcel",
+  changeField: prefix + "changeField",
+  downloadFile: prefix + "downloadFile" ,
+  createScene: prefix + "createScene" ,
+  allMyScene: prefix + "allMyScene" ,
+  allPublicScene: prefix + "allPublicScene" ,
+  joinScene: prefix + "joinScene" ,
+  getDefaultSceneId: prefix + "getDefaultSceneId" ,
+  updateSceneAgent: prefix + "updateSceneAgent" ,
+  closeSceneSSE: "/v1/api/infra/base/im/sseSceneTask/closeSseConnect" ,
+  getFlowTaskNotice: "/v1/api/infra/base/im/chat/getFlowTaskNotice" ,
+}
+
+// 更新频道角色列表
+export function updateSceneAgent(screenId , rolesId) {
+  const data = {
+    screenId : screenId ,
+    rolesId : rolesId
+  }
+  return request({
+    url: managerUrl.updateSceneAgent , 
+    method: 'post',
+    data: data
+  })
+}
+
+// 获取任务实例完成通知
+export function getFlowTaskNotice(){
+  return request({
+    url: managerUrl.getFlowTaskNotice , 
+    method: 'get'
+  })
+}
+
+// 关闭频道SSE
+export function closeSceneSSE(scene , type){
+  return request({
+    url: managerUrl.closeSceneSSE + '?scene=' + scene + '&type=' + type, 
+    method: 'get'
+  })
+}
+
+// 获取默认频道 
+export function getDefaultSceneId(){
+  return request({
+    url: managerUrl.getDefaultSceneId , 
+    method: 'get'
+  })
+}
+
+// 加入频道
+export function joinScene(screenId){
+  return request({
+    url: managerUrl.joinScene + '?screenId=' + screenId , 
+    method: 'get'
+  })
+}
+
+// 新增频道
+export function createScene(data) {
+  return request({
+    url: managerUrl.createScene ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询频道列表
+export function listScene(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询所有频道详细
+export function allPublicScene() {
+  return request({
+    url: managerUrl.allPublicScene, 
+    method: 'get'
+  })
+}
+
+// 查询频道详细
+export function allMyScene() {
+  return request({
+    url: managerUrl.allMyScene , 
+    method: 'get'
+  })
+}
+
+// 查询频道详细
+export function getScene(id) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 新增频道
+export function addScene(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+
+
+// 修改频道
+export function updateScene(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除频道
+export function removeScene(id) {
+  return request({
+    url: managerUrl.removeScene+ '?screenId=' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
+
+// 删除频道
+export function delScene(id) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
+
+// 频道密码重置
+export function resetScenePwd(id, password) {
+  const data = {
+    id,
+    password
+  }
+  return request({
+    url: '/api/infra/base/starter/application/resetPwd',
+    method: 'put',
+    data: data
+  })
+}
+
+// 频道状态修改
+export function changeSceneStatus(id, status) {
+  const data = {
+    id,
+    status
+  }
+  return request({
+    url: '/api/infra/base/starter/application/changeStatus',
+    method: 'put',
+    data: data
+  })
+}
+
+// 查询部门下拉树结构
+export function deptTreeSelect() {
+  return request({
+    url: '/api/infra/base/starter/application/deptTree',
+    method: 'get'
+  })
+}
