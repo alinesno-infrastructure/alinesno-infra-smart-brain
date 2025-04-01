e1d675d3c258a7c4dffa9adf742f3699b953f6a6
李娜(Allen) <allenAip@gmail.com>
2025-04-02 00:11:59 +0800

    feat:新增 AgentSceneMapper 接口，继承 IBaseMapper 定义数据操作规范。

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/AgentSceneMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.AgentSceneEntity;
+
+public interface AgentSceneMapper extends IBaseMapper<AgentSceneEntity> {
+
+}
