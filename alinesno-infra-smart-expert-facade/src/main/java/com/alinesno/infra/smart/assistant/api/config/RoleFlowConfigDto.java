696556b3bc6c97ab9fcf10c911036261af985e88
张婷(Tina) <tinaAip@gmail.com>
2025-04-03 00:14:38 +0800

    feat:新增 RoleFlowConfigDto 类中多个字段，包括问候语问题、模型配置、知识库ID列表、选择工具数据、文件上传状态及相关数据、语音输入数据、猜测用户问题数据和语料搜索配置。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/RoleFlowConfigDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/api/config/RoleFlowConfigDto.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * 用于表示角色反应配置的数据传输对象（DTO）
  */
@@ -17,6 +19,18 @@ public class RoleFlowConfigDto {
     @NotBlank(message = "问候语不能为空")
     private String greeting;
 
+    // 问候语问题
+    private List<String> greetingQuestion ;
+
+    // 模型配置相关信息
+    private ModelConfig modelConfig;
+
+    // 知识库ID列表，用于关联相关知识数据
+    private List<String> knowledgeBaseIds;
+
+    // 选择工具数据列表，可能用于特定操作或查询的工具标识
+    private List<String> selectionToolsData;
+
     // 是否启用长期记忆功能
     private boolean longTermMemoryEnabled;
 
@@ -29,6 +43,12 @@ public class RoleFlowConfigDto {
     // 语音播放
     private boolean voicePlayStatus ;
 
+    // 文件上传
+    private boolean uploadStatus ;
+
+    // 文件上传相关数据
+    private UploadData uploadData ;
+
     // 语音输入相关数据配置
     private VoiceInputData voiceInputData;
 
@@ -37,5 +57,8 @@ public class RoleFlowConfigDto {
 
     // 猜测用户问题相关数据配置
     private GuessWhatYouAskData guessWhatYouAskData;
+
+    // 语料搜索配置
+    private DatasetSearchConfig datasetSearchConfig ;
 
 }
