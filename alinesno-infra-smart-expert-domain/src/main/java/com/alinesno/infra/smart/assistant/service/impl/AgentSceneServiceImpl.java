910c0041abf92938a214d19c48c0d03c6c80f5bb
张婷(Tina) <tinaAip@gmail.com>
2025-04-03 00:14:38 +0800

    feat: 实现 addRoleToScene 方法，支持为场景添加角色并优化逻辑判断

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/AgentSceneServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/AgentSceneServiceImpl.java
@@ -1,22 +1,95 @@
 package com.alinesno.infra.smart.assistant.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.mapper.AgentSceneMapper;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.constants.AgentConstants;
 import com.alinesno.infra.smart.im.entity.AgentSceneEntity;
 import com.alinesno.infra.smart.im.service.IAgentSceneService;
+import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
+import com.alinesno.infra.smart.scene.dto.ScreenAgent;
+import com.alinesno.infra.smart.scene.enums.SceneEnum;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Service
 public class AgentSceneServiceImpl extends IBaseServiceImpl<AgentSceneEntity, AgentSceneMapper> implements IAgentSceneService {
 
+    @Autowired
+    private IIndustryRoleService industryRoleService ;
+
     @Override
-    public void addRoleToScene(Long roleId, long sceneId, long sceneAgentId) {
+    public void addRoleToScene(Long roleId, long sceneId, long agentTypeId) {
+
+        log.debug("添加角色到场景中 roleId = {} , sceneId = {} ,agentTypeId = {} " , roleId , sceneId ,  agentTypeId);
+
         // 判断角色是否已经在场景里面
         if(roleId != 0){
+
+            LambdaQueryWrapper<AgentSceneEntity> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(AgentSceneEntity::getAgentId , roleId) ;
+            long count = count(wrapper) ;
+
             AgentSceneEntity agentSceneEntity = new AgentSceneEntity();
+
+            if(count > 0){
+                agentSceneEntity = getOne(wrapper) ;
+            }
+
+            SceneInfoDto sceneInfoDto = SceneEnum.getSceneInfoById(sceneId) ;
+            ScreenAgent sceneAgent = SceneEnum.getAgentById(agentTypeId) ;
+
             agentSceneEntity.setAgentId(roleId);
+
+            agentSceneEntity.setSceneId(sceneId);
+            agentSceneEntity.setSceneCode(sceneInfoDto.getCode());
+
+            agentSceneEntity.setAgentTypeId(agentTypeId);
+            agentSceneEntity.setAgentTypeCode(sceneAgent.getCode());
+
+            agentSceneEntity.setSortOrder((int) count);
+            agentSceneEntity.setRoleOrganizationId(AgentConstants.STORE_EMPTY_ORG_ID); // 发布到公共角色场景
+
+            saveOrUpdate(agentSceneEntity);
+
         }
+    }
+
+    @Override
+    public void offlineScene(Long roleId) {
+        // 将角色从场景中删除
+        if(roleId != 0){
+            LambdaQueryWrapper<AgentSceneEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(AgentSceneEntity::getAgentId, roleId);
+
+            remove(lambdaQueryWrapper);
+        }
+    }
+
+    @Override
+    public List<IndustryRoleEntity> getRoleBySceneIdAndAgentType(long sceneId, long agentTypeId, long orgId) {
+
+        if(sceneId != 0 && agentTypeId != 0){
+            LambdaQueryWrapper<AgentSceneEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+            lambdaQueryWrapper.eq(AgentSceneEntity::getSceneId, sceneId);
+            lambdaQueryWrapper.eq(AgentSceneEntity::getAgentTypeId, agentTypeId);
+
+            List<AgentSceneEntity> agentSceneEntities = list(lambdaQueryWrapper);
+
+            // 获取到AgentId
+            List<Long> agentIds = agentSceneEntities.stream().map(AgentSceneEntity::getAgentId).toList();
+            if(!agentIds.isEmpty()){
+                return industryRoleService.listByIds(agentIds);
+            }
+        }
+
+        return Collections.emptyList() ;
     }
 }
