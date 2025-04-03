faaa1808d7204ca1abbae4a28301cf7b4a2e80aa
赵阳(Yang) <yangAip@gmail.com>
2025-04-04 00:13:40 +0800

    fix: 修复发布到场景时参数传递不完整的问题，新增 `dto.getLlmModelId()` 参数。

--- alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/impl/ChannelPublishServiceImpl.java
+++ alinesno-infra-smart-expert-publish/src/main/java/com/alinesno/infra/smart/assistant/publish/service/impl/ChannelPublishServiceImpl.java
@@ -42,7 +42,7 @@ public class ChannelPublishServiceImpl extends IBaseServiceImpl<ChannelPublishEn
 
         // 如果是发布到场景，则将角色信息添加到AIP场景里面
         if(ChannelListEnums.AIP_AGENT_SCENE.getParamKey().equals(entity.getParamKey())){
-            agentSceneService.addRoleToScene(entity.getRoleId() , dto.getSceneId() , dto.getAgentTypeId()) ;
+            agentSceneService.addRoleToScene(entity.getRoleId() , dto.getSceneId() , dto.getAgentTypeId() , dto.getLlmModelId()) ;
         }
     }
 
