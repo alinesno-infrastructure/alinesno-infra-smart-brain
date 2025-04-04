492a3772816f6a50827f08158e2b5e6b17d654bd
周健(James) <jamesAip@gmail.com>
2025-04-05 00:12:38 +0800

    feat: 实现 ToolServiceImpl 的 toolSelection 方法，支持分页查询与权限过滤工具数据。

--- alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolServiceImpl.java
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/service/impl/ToolServiceImpl.java
@@ -3,12 +3,18 @@ package com.alinesno.infra.smart.assistant.plugin.service.impl;
 import com.alibaba.fastjson.JSON;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.smart.assistant.api.ToolDto;
 import com.alinesno.infra.smart.assistant.api.ToolRequestDto;
 import com.alinesno.infra.smart.assistant.entity.ToolEntity;
+import com.alinesno.infra.smart.assistant.enums.ModelDataScopeOptions;
 import com.alinesno.infra.smart.assistant.plugin.mapper.ToolMapper;
 import com.alinesno.infra.smart.assistant.plugin.tool.ToolExecutor;
 import com.alinesno.infra.smart.assistant.service.IToolService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +22,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 应用构建Service业务层处理
@@ -117,5 +121,38 @@ public class ToolServiceImpl extends IBaseServiceImpl<ToolEntity, ToolMapper> im
         }
 
         return null;
+    }
+
+    @Override
+    public TableDataInfo toolSelection(DatatablesPageBean page, PermissionQuery query) {
+
+        // TODO: 待处理分页的问题
+        int pageNow = page.getPageNum();
+        int pageSize = page.getPageSize();
+
+        LambdaQueryWrapper<ToolEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.setEntityClass(ToolEntity.class) ;
+        query.toWrapper(wrapper);
+
+        Page<ToolEntity> pageResult = new Page<>(pageNow, pageSize) ;
+        pageResult = this.page(pageResult, wrapper);
+
+        // 查询出公共的工具
+        LambdaQueryWrapper<ToolEntity> publicWrapper = new LambdaQueryWrapper<>();
+        publicWrapper.eq(ToolEntity::getToolPermission, ModelDataScopeOptions.PUBLIC.getValue());
+        List<ToolEntity> publicTools = this.list(publicWrapper);
+
+        if(publicTools == null){
+            publicTools = new ArrayList<>();
+        }
+
+        TableDataInfo tableDataInfo = new TableDataInfo();
+
+        publicTools.addAll(pageResult.getRecords())  ;
+        tableDataInfo.setRows(publicTools);
+
+        tableDataInfo.setTotal(pageResult.getTotal() + publicTools.size());
+
+        return tableDataInfo;
     }
 }
