624d34967b63433e5537da74c7c172650b70f14c
张婷(Tina) <tinaAip@gmail.com>
2025-04-06 00:10:47 +0800

    fix: 修改 ProductSidebar 组件中 "目录分类管理" 的描述为 "数据集分类"

--- alinesno-infra-smart-expert-ui/src/layout/components/ProductSidebar/index.vue
+++ alinesno-infra-smart-expert-ui/src/layout/components/ProductSidebar/index.vue
@@ -47,7 +47,7 @@ const menuItems = ref([
   { id: '5', icon: 'fa-solid fa-feather', link: '/prompt/smart/assistant/scene/index', desc: '场景列表' },
 
   { id: '18', icon: 'fa-solid fa-file-pdf', link: '/base/search/vectorData/index', desc: '数据集管理' },
-  { id: '14', icon: 'fa-brands fa-skype', link: '/base/search/catalog/index', desc: '目录分类管理' },
+  { id: '14', icon: 'fa-brands fa-skype', link: '/base/search/catalog/index', desc: '数据集分类' },
   
   { id: '6', icon: 'fa-solid fa-pen-nib', link: '/template/smart/assistant/plugin/index', desc: '工具管理' },
   { id: '7', icon: 'fa-solid fa-paper-plane', link: '/template/smart/assistant/secret/index', desc: '密钥管理' },
