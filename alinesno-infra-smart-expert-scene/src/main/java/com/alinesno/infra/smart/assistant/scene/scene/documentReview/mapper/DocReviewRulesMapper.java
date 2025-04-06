1ccce63d750296fda1a72b809a3d457e56e37395
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 DocReviewRulesMapper 接口，实现文档审核规则的数据映射。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewRulesMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewRulesEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocReviewRulesMapper extends IBaseMapper<DocReviewRulesEntity> {
+
+}
