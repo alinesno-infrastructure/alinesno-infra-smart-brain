48f279b4dfa44bdb223ea7bb5b9e4a0b943fa750
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 roleSelectPanel.vue 组件，支持选择角色并触发相关操作。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/roleSelectPanel.vue
@@ -0,0 +1,91 @@
+<template>
+    <div style="display: flex;gap: 20px; align-items: center;">
+
+        <el-button text bg type="primary" size="large" @click="configAgent()">选择角色</el-button>
+
+      <div style="display: flex;align-items: center;gap: 10px;font-size: 14px;">
+        <span>文档分析专员</span>
+        <img class="review-agent" :src="imagePathByPath(props.currentSeneInfo.analysisAgentEngineerEntity?.roleAvatar)" />
+      </div>
+
+      <div style="display: flex;align-items: center;gap: 10px;font-size: 14px;">
+        <span>文档审核专员</span>
+        <img class="review-agent" :src="imagePathByPath(props.currentSeneInfo.logicReviewerEngineerEntity?.roleAvatar)" />
+      </div>
+
+    </div>
+</template>
+
+<script setup>
+
+import TransferAgentPanel from '@/views/base/scene/common/transferAgent'
+
+const emit = defineEmits(['openExecuteHandle'])
+
+const props = defineProps({
+  currentSeneInfo: {
+      type: Object,
+      default: () => {
+          return {} 
+      }
+  }
+})
+
+// 选择Agent组件
+const transferAgentPanel = ref(null)
+const channelAgentList = ref([])
+
+/** 选择角色生成目录 */
+// function selectChaterGenerator(item) {
+    // chaterDialogVisible.value = true;
+    // console.log('selectChaterGenerator')
+    // console.log(' personCardRef.value = ' +  personCardRef.value)
+//     person.value = item ;
+//     person.value.email =  'zhangsan@example.com';
+// }
+
+/** 配置成员 */
+function configAgent(){
+
+    // configAgentDialogVisible.value = true ;
+    // console.log('configAgent')
+    // currentAgentConfigType.value = type 
+
+    // const title = type == 'chapter'?'选择大纲生成专员':'选择内容生成专员' ; 
+
+    // if(type === 'analysisAgent'){
+    //     channelAgentList.value = props.analysisAgentEngineers ;// currentSceneInfo.value.chapterEditors.map(item => item.id); 
+    // }else if(type === 'logicReviewer'){
+    //     channelAgentList.value = props.logicReviewerEngineers ; // currentSceneInfo.value.contentEditors.map(item => item.id); 
+    // }
+
+    // transferAgentPanel.value.handleOpendAgent(title , channelAgentList.value)
+
+    emit('openExecuteHandle') ; 
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+.review-agent{
+  width: 35px;
+  border-radius: 50%;
+}
+</style>
+
+<style>
+.el-transfer-panel {
+    width:350px;
+}
+
+.el-card__header {
+    border: 0px;
+}
+
+.chapter-tree-content .el-tree-node__content {
+    height: auto !important;
+}
+
+</style>
