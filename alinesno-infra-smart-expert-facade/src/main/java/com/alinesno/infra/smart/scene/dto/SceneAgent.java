2cb24b02cf5c175a1433ea9f384c14bd5b9053c3
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 SceneAgent 类，用于描述场景代理信息并支持数据处理模型配置。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/SceneAgent.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.smart.scene.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 场景代理类，用于描述屏幕代理的信息。
+ */
+@Data
+@NoArgsConstructor
+public class SceneAgent {
+
+    // 唯一标识符，改为 long 类型且不少于 8 位
+    private long id;
+    private String name;
+    private String code;
+    private String description;
+
+    // 是否需要数据处理模型
+    private boolean needDataModel = false;
+
+    public SceneAgent(long id, String name, String code, String description) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.description = description;
+    }
+
+    public SceneAgent(long id, String name, String code, String description, boolean needDataModel) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.description = description;
+        this.needDataModel = needDataModel;
+    }
+}
