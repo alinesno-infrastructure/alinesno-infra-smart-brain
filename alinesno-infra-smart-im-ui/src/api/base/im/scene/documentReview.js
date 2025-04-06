c40bc2a50eb288c952182c5d0b1091472f66332c
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增文档审查相关接口，包含获取场景详情与预览文档功能。

--- alinesno-infra-smart-im-ui/src/api/base/im/scene/documentReview.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/documentReview.js
@@ -2,4 +2,26 @@ import request from '@/utils/request'
 import { parseStrEmpty } from "@/utils/ruoyi";
 
 // 接口配置项
-var prefix = '/api/infra/smart/assistant/screen/' ;
+var prefix = '/api/infra/smart/assistant/scene/documentReview/' ;
+
+var managerUrl = {
+  getScene: prefix +"getScene",
+  getPreviewDocx: prefix +"getPreviewDocx",
+}
+
+// 获取预览文档
+export function getPreviewDocx(sceneId) {
+  return request({
+    url: managerUrl.getPreviewDocx + '?sceneId=' + parseStrEmpty(sceneId),
+    responseType: 'arraybuffer', // 显式声明返回二进制流
+    method: 'get'
+  })
+}
+
+// 查询场景详细
+export function getScene(id) {
+  return request({
+    url: managerUrl.getScene + '?id=' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
