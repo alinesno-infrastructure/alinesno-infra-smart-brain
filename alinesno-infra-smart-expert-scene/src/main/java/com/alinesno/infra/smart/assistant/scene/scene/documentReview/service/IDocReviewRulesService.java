a232e20a0467ee5acc9accf9e8553b65ab74926e
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 IDocReviewRulesService 接口，定义文档审核规则服务的基础操作。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewRulesService.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewRulesEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IDocReviewRulesService extends IBaseService<DocReviewRulesEntity> {
+
+}
