5f1cb0739a5aef5fd132b5b08fa42ff85fbe7319
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 IDocReviewSceneService 接口，包含初始化智能助手功能

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewSceneService.java
@@ -0,0 +1,21 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewSceneEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewInitDto;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IDocReviewSceneService extends IBaseService<DocReviewSceneEntity> {
+
+    /**
+     * 初始化智能助手
+     * @param dto
+     */
+    void initAgents(DocReviewInitDto dto);
+
+}
