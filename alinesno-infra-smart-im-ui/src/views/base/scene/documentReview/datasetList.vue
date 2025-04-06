e372f86c5e829cfc9fbc76a19c1cd2bf1ea4eaa0
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:37 +0800

    feat:新增 datasetList.vue 文件，实现文档审查场景下的数据集展示容器。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/datasetList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app-container docuemnt-dataset-container">
+        这是一个内嵌的网站
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/document-review.scss';
+</style>    
