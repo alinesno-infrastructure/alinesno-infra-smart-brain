9bcb90fde87dd23693cd09dfa8f857c41f93384a
张婷(Tina) <tinaAip@gmail.com>
2025-04-07 00:12:36 +0800

    feat: 实现 DocReviewSceneServiceImpl，初始化文档审核场景相关逻辑

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewSceneServiceImpl.java
@@ -0,0 +1,38 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewSceneEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewInitDto;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewSceneMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewSceneService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DocReviewSceneServiceImpl extends IBaseServiceImpl<DocReviewSceneEntity, DocReviewSceneMapper> implements IDocReviewSceneService {
+
+    @Override
+    public void initAgents(DocReviewInitDto dto) {
+
+        long sceneId = dto.getSceneId() ;
+
+        LambdaQueryWrapper<DocReviewSceneEntity> wrapper = new LambdaQueryWrapper<DocReviewSceneEntity>();
+        wrapper.eq(DocReviewSceneEntity::getSceneId, sceneId) ;
+        long count = count(wrapper) ;
+
+        DocReviewSceneEntity entity = new DocReviewSceneEntity() ;
+        if(count > 0){
+            entity = getOne(wrapper) ;
+        }
+
+        entity.setAnalysisAgentEngineer(dto.getAnalysisAgentEngineer());
+        entity.setLogicReviewerEngineer(dto.getLogicReviewerEngineer());
+        entity.setSceneId(sceneId);
+
+        saveOrUpdate(entity) ;
+
+    }
+
+}
