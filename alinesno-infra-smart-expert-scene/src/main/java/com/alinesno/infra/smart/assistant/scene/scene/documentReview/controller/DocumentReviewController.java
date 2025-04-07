487b5c77c423ab46718b7c41a7f7941cc8cdf7ad
王磊(Levi) <leviAip@gmail.com>
2025-04-08 00:12:16 +0800

    feat:新增 DocumentReviewController 功能，支持文档审核相关接口及工具集成。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/controller/DocumentReviewController.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/controller/DocumentReviewController.java
@@ -1,22 +1,33 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.controller;
 
+import cn.hutool.core.util.IdUtil;
+import com.alibaba.fastjson.JSONArray;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.facade.response.R;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
+import com.alinesno.infra.smart.assistant.adapter.service.CloudStorageConsumer;
+import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.assistant.scene.common.service.ISceneService;
 import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewSceneEntity;
 import com.alinesno.infra.smart.assistant.scene.core.entity.SceneEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRulesDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocSceneInfoDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewSceneService;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.tools.AnalysisTool;
 import com.alinesno.infra.smart.assistant.scene.scene.longtext.dto.LongTextSceneDto;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
 import com.alinesno.infra.smart.scene.enums.ContractTypeEnum;
 import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -24,11 +35,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
 
 /**
  * 文档审核场景控制
@@ -47,6 +59,15 @@ public class DocumentReviewController extends SuperController {
 
     @Autowired
     private IIndustryRoleService industryRoleService;
+
+    @Autowired
+    private CloudStorageConsumer storageConsumer ;
+
+    @Autowired
+    private AnalysisTool analysisTool;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
 
     /**
      * 获取合同类型列表
@@ -93,18 +114,33 @@ public class DocumentReviewController extends SuperController {
 
             docSceneInfoDto.setLogicReviewerEngineer(docReviewSceneEntity.getLogicReviewerEngineer());
             docSceneInfoDto.setLogicReviewerEngineerEntity(industryRoleService.getById(docReviewSceneEntity.getLogicReviewerEngineer()));
+
+            docSceneInfoDto.setContractType(docReviewSceneEntity.getContractType());
+            docSceneInfoDto.setReviewPosition(docReviewSceneEntity.getReviewPosition()) ;
+            docSceneInfoDto.setReviewListKnowledgeBase(docReviewSceneEntity.getReviewListKnowledgeBase());
+            docSceneInfoDto.setContractOverview(docReviewSceneEntity.getContractOverview());
+            docSceneInfoDto.setDocumentId(docReviewSceneEntity.getDocumentId());
+
+            docSceneInfoDto.setReviewList(docReviewSceneEntity.getReviewList());
+            docSceneInfoDto.setDocumentName(docReviewSceneEntity.getDocumentName());
+            docSceneInfoDto.setReviewListDtos(JSONArray.parseArray(docReviewSceneEntity.getReviewList(), DocReviewRulesDto.class));
         }
+
+        docSceneInfoDto.setContractTypes(ContractTypeEnum.getList());
 
         return AjaxResult.success("操作成功.", docSceneInfoDto);
     }
 
     @GetMapping("/getPreviewDocx")
-    public ResponseEntity<Resource> getPreviewDocx(@RequestParam String sceneId) {
+    public ResponseEntity<Resource> getPreviewDocx(@RequestParam long sceneId) {
 
-        String REMOTE_DOCX_URL = "http://data.linesno.com/test.docx";
+        DocReviewSceneEntity entity = docReviewSceneService.getBySceneId(sceneId);
+        String previewUrl = storageConsumer.getPreviewUrl(entity.getDocumentId()).getData();
+
+        log.debug("previewUrl= {}" , previewUrl);
 
         RestTemplate restTemplate = new RestTemplate();
-        byte[] fileBytes = restTemplate.getForObject(REMOTE_DOCX_URL, byte[].class);
+        byte[] fileBytes = restTemplate.getForObject(previewUrl, byte[].class);
 
         if (fileBytes == null) {
             // 文件下载失败，返回合适的错误信息
@@ -126,5 +162,55 @@ public class DocumentReviewController extends SuperController {
                 .headers(headers)
                 .body(resource);
     }
+
+    /**
+     * 文件上传
+     * @return
+     */
+    @SneakyThrows
+    @PostMapping("/importData")
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, long sceneId){
+
+        log.debug("sceneId = {}" , sceneId);
+
+        // 新生成的文件名称
+        String fileName = file.getOriginalFilename() ;
+        String fileSuffix = Objects.requireNonNull(fileName).substring(fileName.lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        log.debug("newFileName = {} , targetFile = {}" , newFileName , targetFile.getAbsoluteFile());
+
+        R<String> r = storageConsumer.upload(targetFile) ;
+
+        DocReviewSceneEntity docReviewSceneEntity = docReviewSceneService.getBySceneId(sceneId) ;
+
+        // 获取到文档的基础内容
+        String content =  analysisTool.analysisDocumentBaseContent(targetFile) ;
+
+        MessageTaskInfo taskInfo = new MessageTaskInfo() ;
+        taskInfo.setRoleId(docReviewSceneEntity.getAnalysisAgentEngineer());
+        taskInfo.setChannelId(sceneId);
+        taskInfo.setSceneId(sceneId);
+        taskInfo.setText(content);
+
+        // 优先获取到结果内容
+        WorkflowExecutionDto genContent  = new WorkflowExecutionDto() ;
+        analysisTool.handleChapterMessage(genContent , taskInfo);
+
+        // 更新Entity数据
+        docReviewSceneEntity.setContractOverview(genContent.getGenContent());
+        docReviewSceneEntity.setDocumentId(r.getData());
+        docReviewSceneEntity.setDocumentName(fileName);
+
+        docReviewSceneService.update(docReviewSceneEntity);
+
+        log.debug("ajaxResult= {}" , r);
+        return AjaxResult.success("上传成功." , r.getData()) ;
+    }
+
 }
 
