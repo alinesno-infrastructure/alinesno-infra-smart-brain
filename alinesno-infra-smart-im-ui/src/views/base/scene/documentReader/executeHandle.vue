1beca1e11488e32f926e7003ed0e876cd5e8a994
张婷(Tina) <tinaAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 executeHandle.vue 组件，支持智能体选择与表单交互功能。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReader/executeHandle.vue
@@ -0,0 +1,319 @@
+<template>
+  <div>
+    <el-dialog
+      v-model="dialogVisible"
+      width="800px"
+      title="请选择智能体"
+      @close="handleClose"
+    >
+      <template #default>
+        <div class="dialog-description">
+          由多个智能体协作，请根据需求选择大纲生成智能体和章节工程师，并输入内容生成相关信息。
+        </div>
+        <el-form ref="formRef" :model="formData" :rules="rules" size="large" :label-position="'top'">
+
+          <!-- 文档分析助手 -->
+          <el-form-item label="选择内容分析专员" prop="summaryAgentEngineer">
+            <el-scrollbar class="scrollbar">
+              <div class="custom-radio-group" >
+                <div
+                  v-for="engineer in summaryAgentEngineers"
+                  :key="engineer.id"
+                  class="custom-radio"
+                  :class="{ 'selected': formData.summaryAgentEngineer === engineer.id }"
+                  @click="selectSummaryAgent(engineer.id)"
+                >
+                  <div class="engineer-info">
+                    <img
+                      :src="imagePathByPath(engineer.roleAvatar)"
+                      alt="工程师头像"
+                      class="engineer-avatar"
+                    />
+                    <div class="engineer-details">
+                      <span class="engineer-name">
+                        {{ engineer.roleName }}
+                      </span>
+                      <span class="engineer-responsibilities">
+                        {{ engineer.responsibilities }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </el-scrollbar>
+          </el-form-item>
+
+
+          <!-- 文档审核助手 -->
+          <el-form-item label="选择案例查询专员" prop="caseQueryAgentEngineer">
+            <el-scrollbar class="scrollbar">
+              <div class="custom-radio-group" >
+                <div
+                  v-for="engineer in caseQueryAgentEngineers"
+                  :key="engineer.id"
+                  class="custom-radio"
+                  :class="{ 'selected': formData.caseQueryAgentEngineer === engineer.id }"
+                  @click="selectChapterEngineer(engineer.id)"
+                >
+                  <div class="engineer-info">
+                    <img
+                      :src="imagePathByPath(engineer.roleAvatar)"
+                      alt="工程师头像"
+                      class="engineer-avatar"
+                    />
+                    <div class="engineer-details">
+                      <span class="engineer-name">
+                        {{ engineer.roleName }}
+                      </span>
+                      <span class="engineer-responsibilities">
+                        {{ engineer.responsibilities }}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </el-scrollbar>
+          </el-form-item>
+
+        </el-form>
+      </template>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button size="large" @click="dialogVisible = false">取消</el-button>
+          <el-button size="large" type="primary" @click="handleConfirm">确认选择</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+import { nextTick, ref } from 'vue';
+
+// import { ElDialog, ElButton, ElInput, ElForm, ElFormItem, ElScrollbar, ElUpload } from 'element-plus';
+// import { listAll } from '@/api/base/im/user';
+
+import { initAgents } from '@/api/base/im/scene/documentReaderSceneInfo';
+
+const emit = defineEmits(['openChatBox' , 'handleGetScene']) ; 
+
+// const agentList = ref([]);
+const dialogVisible = ref(false);
+const formRef = ref(null);
+
+const summaryAgentEngineers = ref([]);
+const caseQueryAgentEngineers = ref([]);
+
+const formData = ref({
+  summaryAgentEngineer: null,
+  caseQueryAgentEngineer: null,
+  contentInput: ''
+});
+
+const currentSceneInfo = ref({});
+const uploadRef = ref();
+
+const submitUpload = () => {
+  uploadRef.value.submit();
+};
+
+const rules = ref({
+  summaryAgentEngineer: [
+    {
+      validator: (rule, value, callback) => {
+        if (value === null) {
+          callback(new Error('请选择一名大纲设计工程师'));
+        } else {
+          callback();
+        }
+      },
+      trigger: 'change'
+    }
+  ],
+  caseQueryAgentEngineer: [
+    {
+      validator: (rule, value, callback) => {
+        if (value === null) {
+          callback(new Error('请选择一名章节工程师'));
+        } else {
+          callback();
+        }
+      },
+      trigger: 'change'
+    }
+  ],
+  contentInput: [
+    {
+      validator: (rule, value, callback) => {
+        if (value.trim() === '') {
+          callback(new Error('内容生成输入不能为空'));
+        } else {
+          callback();
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+});
+
+const handleClose = () => {
+  console.log('对话框已关闭');
+};
+
+const handleConfirm = async() => {
+
+  const valid = await new Promise((resolve) => {
+      formRef.value.validate((valid) => {
+          resolve(valid);
+      });
+  });
+
+  if (valid) {
+      console.log('点击了确定按钮');
+      console.log('选中的大纲设计工程师:', formData.value.summaryAgentEngineer);
+      console.log('选中的章节工程师:', formData.value.caseQueryAgentEngineer);
+      console.log('内容生成输入:', formData.value.contentInput);
+      dialogVisible.value = false;
+
+      formData.value.sceneId = currentSceneInfo.value.id ;
+      
+      const response = await initAgents(formData.value) ;
+      console.log('response = ' + response)
+
+      emit('handleGetScene')
+      // emit('openChatBox' , formData.value.summaryAgentEngineer , formData.value.contentInput)
+
+  } else {
+      console.log('表单校验不通过');
+  }
+
+}
+
+const selectSummaryAgent = (id) => {
+  formData.value.summaryAgentEngineer = id;
+};
+
+const selectChapterEngineer = (id) => {
+  formData.value.caseQueryAgentEngineer = id;
+};
+
+// /** 根据场景id和类型获取到角色信息 */
+// const handleRoleBySceneIdAndAgentType = async () => {
+//   console.log('sceneId = ' + currentSceneInfo.value.sceneId + ' , agents = ' + currentSceneInfo.value.agents);
+
+//   if (currentSceneInfo.value.sceneId && currentSceneInfo.value.agents) {
+//     for (let i = 0; i < currentSceneInfo.value.agents.length; i++) {
+//       let item = currentSceneInfo.value.agents[i];
+
+//       const response = await getRoleBySceneIdAndAgentType(currentSceneInfo.value.sceneId, item.id);
+//       console.log('chapterEditor response = ' + response.data);
+
+//       if (item.code === 'analysisAgent') {
+//         const response = await getRoleBySceneIdAndAgentType(currentSceneInfo.value.sceneId, item.id);
+//         console.log('analysisAgent response = ' + response);
+//         summaryAgentEngineers.value = response.data;
+//       } else if (item.code === 'logicReviewer') {
+//         const response = await getRoleBySceneIdAndAgentType(currentSceneInfo.value.sceneId, item.id);
+//         console.log('logicReviewer response = ' + response);
+//         caseQueryAgentEngineer.value = response.data;
+//       }
+
+//     }
+//   }
+// };
+
+/** 打开弹出窗口 */
+const handleOpen = (sceneInfo , summaryAgentEngineersVal , caseQueryAgentEngineerVal) => {
+  dialogVisible.value = true;
+  currentSceneInfo.value = sceneInfo;
+  // handleRoleBySceneIdAndAgentType();
+
+  summaryAgentEngineers.value = summaryAgentEngineersVal ;
+  caseQueryAgentEngineers.value = caseQueryAgentEngineerVal ;
+};
+
+// nextTick(() => {
+//   handleOpen();
+// })
+
+defineExpose({
+  handleOpen
+});
+</script>
+
+<style lang="scss" scoped>
+$border-color: rgb(233, 235, 242) ; // #4c4d4f;
+$selected-border-color: #f5f5f5;
+$selected-bg-color: #262727;
+
+.dialog-description {
+  padding: 20px 0px;
+  padding-top: 10px;
+  font-size: 15px;
+}
+
+.scrollbar {
+  max-height: 150px;
+  width: 100%;
+}
+
+.custom-radio-group {
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  justify-content: flex-start;
+  flex-direction: row;
+
+  .custom-radio {
+    cursor: pointer;
+    padding: 10px;
+    border: 1px solid $border-color;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    width: 33%;
+
+    &.selected {
+      border-color: $selected-border-color;
+      background-color: $selected-bg-color;
+    }
+
+    .engineer-info {
+      display: flex;
+      align-items: center;
+
+      .engineer-avatar {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-right: 10px;
+      }
+
+      .engineer-details {
+        display: flex;
+        flex-direction: column;
+        line-height: 25px;
+
+        .engineer-name {
+          font-size: 15px;
+          font-weight: bold;
+        }
+
+        .engineer-responsibilities {
+          font-size: 13px;
+          color: rgb(85, 85, 85);
+          line-height: 20px;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      }
+    }
+  }
+}
+
+.dialog-footer {
+  margin-bottom: 20px;
+}
+</style>    
