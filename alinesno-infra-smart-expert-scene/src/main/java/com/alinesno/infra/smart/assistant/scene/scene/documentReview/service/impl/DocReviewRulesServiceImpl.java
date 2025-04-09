5a730ee821788e8dfaada04e05e566f951c127a3
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:28 +0800

    fix:修复 DocReviewRulesServiceImpl 中依赖包路径错误

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewRulesServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewRulesServiceImpl.java
@@ -1,9 +1,9 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewRulesEntity;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewRulesMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewRulesService;
+import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
