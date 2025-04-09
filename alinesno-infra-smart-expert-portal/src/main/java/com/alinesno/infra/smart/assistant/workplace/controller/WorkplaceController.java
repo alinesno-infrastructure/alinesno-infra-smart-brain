b4779a482832c4ea7a764d9311277d3f427ebdfa
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 WorkplaceController 控制器，支持工作台相关接口功能。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/controller/WorkplaceController.java
@@ -0,0 +1,106 @@
+package com.alinesno.infra.smart.assistant.workplace.controller;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceAddDto;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceResponseDto;
+import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceEntity;
+import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceSourceEnums;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceItemService;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/smart/assistant/workplace")
+public class WorkplaceController extends BaseController<WorkplaceEntity, IWorkplaceService> {
+
+    @Autowired
+    private IWorkplaceService service;
+
+    @Autowired
+    private IWorkplaceItemService workplaceItemService; ;
+
+    /**
+     * 获取BusinessLogEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @DataPermissionScope
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+
+        TableDataInfo tableDataInfo =  this.toPage(model, this.getFeign(), page);
+
+        List<?> list =  tableDataInfo.getRows() ;
+        if(CollectionUtil.isNotEmpty(list)){
+            List<WorkplaceResponseDto> dtos = CollectionUtil.newArrayList() ;
+            List<WorkplaceEntity> entities = (List<WorkplaceEntity>) list ;
+
+            for (WorkplaceEntity entity : entities) {
+                WorkplaceResponseDto dto = new WorkplaceResponseDto() ;
+                BeanUtils.copyProperties(entity , dto) ;
+
+                WorkplaceResponseDto workplaceItemDto = workplaceItemService.getWorkplaceItem(entity.getId()) ;
+                dto.setAgentsList(workplaceItemDto.getAgentsList());
+                dto.setChannelsList(workplaceItemDto.getChannelsList());
+                dto.setScenesList(workplaceItemDto.getScenesList());
+
+                dtos.add(dto) ;
+            }
+
+            tableDataInfo.setRows(dtos) ;
+        }
+
+        return tableDataInfo;
+    }
+
+    /**
+     * createChannel
+     * @return
+     */
+    @DataPermissionSave
+    @PostMapping("/createWorkplace")
+    public AjaxResult createWorkplace(@RequestBody @Validated WorkplaceAddDto dto){
+
+        Long workplaceId = service.createWorkplace(dto) ;
+        dto.setWorkplaceSource(WorkplaceSourceEnums.BACKEND.getCode());
+
+        return AjaxResult.success("操作成功" , workplaceId) ;
+    }
+
+    @Override
+    public IWorkplaceService getFeign() {
+        return this.service;
+    }
+}
+
