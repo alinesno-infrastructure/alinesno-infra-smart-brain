12a9f07cccdd6794c790ee4217cc0a69f1e10bc8
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:28 +0800

    fix:修正 RoleExecuteMapper 导入路径，替换旧包为新包

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/leaderModel/mapper/RoleExecuteMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/leaderModel/mapper/RoleExecuteMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.leaderModel.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.scene.core.entity.RoleExecuteEntity;
+import com.alinesno.infra.smart.scene.entity.RoleExecuteEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
