76c1e2ec89199129982ccca4022354ba72fb9e42
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 WorkplaceAddDto 类，用于工作台添加的数据传输对象。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/dto/WorkplaceAddDto.java
@@ -0,0 +1,43 @@
+package com.alinesno.infra.smart.assistant.workplace.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.im.entity.ChannelEntity;
+import com.alinesno.infra.smart.scene.entity.SceneEntity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 工作台添加DTO类
+ * 继承自BaseDto，用于处理工作台添加的相关数据传输
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class WorkplaceAddDto extends BaseDto {
+
+    // 工作台名称，不能为空
+    @NotBlank(message = "工作台名称不能为空")
+    private String name;
+
+    // 工作台描述，不能为空
+    @NotBlank(message = "工作台描述不能为空")
+    private String description;
+
+    // 工作台背景图，不能为空
+    @NotBlank(message = "工作台背景图不能为空")
+    private String backgroundImage;
+
+    // 工作台类型，不能为空
+    @NotBlank(message = "工作台类型不能为空")
+    private String workplaceType ;
+
+    // 工作台来源
+    private String workplaceSource ;
+
+    // 来源工作台ID
+    private long sourceWorkplaceId ;
+
+}
