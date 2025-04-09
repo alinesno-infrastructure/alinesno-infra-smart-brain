f3dbbba5a44743d6908207a21af169ed143d076a
张婷(Tina) <tinaAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 configScenePanel.vue 组件，支持频道配置弹窗功能。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workplace/configScenePanel.vue
@@ -0,0 +1,62 @@
+<template>
+    <el-dialog v-model="configDialogVisible" :title="workplaceConfigTitle" width="50%">
+
+        <!-- 底部 -->
+        <template #footer>
+          <div class="dialog-footer">
+            <el-button @click="configDialogVisible = false">关闭</el-button>
+            <el-button type="primary" @click="handleCloseConfig">确认</el-button>
+          </div>
+        </template>
+    </el-dialog>
+</template>
+
+<script setup name="Index">
+
+import {
+  listAllScene , 
+  listPublicScene
+} from '@/api/smart/assistant/scene';
+
+
+const configDialogVisible = ref(false)
+const workplaceConfigTitle = ref('频道配置')
+
+const openConfigDialog =() => {
+  configDialogVisible.value = true
+}
+
+const handleCloseConfig = () => {
+  configDialogVisible.value = false
+}
+
+/** 配置成员 */
+const configScene = async(row) =>{
+
+    configDialogVisible.value = true ;
+    workplaceConfigTitle.value = row.name + "场景" ; 
+
+    agentList.value = []
+
+    // const workplaceType = row.workplaceType;
+    // if(workplaceType== 'org'){
+    //   await handleListAllRole() ;
+    // }else if(workplaceType== 'public'){
+    //   await handleListPublicRole();
+    // }
+
+    // currentWorkplaceId.value = row.id ;
+    // workplaceAgentList.value = row.roles ;
+
+}
+
+defineExpose({ 
+  configScene
+})
+
+
+</script>
+
+<style lang="scss" scoped>
+// 自定义新式 
+</style>
