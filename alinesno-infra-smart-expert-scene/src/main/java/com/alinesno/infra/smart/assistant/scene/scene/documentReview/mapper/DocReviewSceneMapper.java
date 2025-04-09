725966b2ff2737deabc07c56561ace503faefed6
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:28 +0800

    fix:修复 DocReviewSceneMapper 导入路径错误，将旧包名替换为新包名。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewSceneMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewSceneMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.scene.core.entity.DocReviewSceneEntity;
+import com.alinesno.infra.smart.scene.entity.DocReviewSceneEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
