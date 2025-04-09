282fd6b1349a2e607f80460cb311b312dc72fdca
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat: 实现 DocReaderSceneServiceImpl，包含按场景ID获取实体及初始化代理的方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReader/service/impl/DocReaderSceneServiceImpl.java
@@ -0,0 +1,44 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReader.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReader.dto.DocReaderInitDto;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReader.mapper.DocReaderSceneMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReader.service.IDocReaderSceneService;
+import com.alinesno.infra.smart.scene.entity.DocReaderSceneEntity;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DocReaderSceneServiceImpl extends IBaseServiceImpl<DocReaderSceneEntity, DocReaderSceneMapper> implements IDocReaderSceneService {
+
+    @Override
+    public DocReaderSceneEntity getBySceneId(long sceneId) {
+
+        LambdaQueryWrapper<DocReaderSceneEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DocReaderSceneEntity::getSceneId, sceneId) ;
+
+        return getOne(wrapper) ;
+    }
+
+    @Override
+    public void initAgents(DocReaderInitDto dto) {
+        long sceneId = dto.getSceneId() ;
+
+        LambdaQueryWrapper<DocReaderSceneEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DocReaderSceneEntity::getSceneId, sceneId) ;
+        long count = count(wrapper) ;
+
+        DocReaderSceneEntity entity = new DocReaderSceneEntity() ;
+        if(count > 0){
+            entity = getOne(wrapper) ;
+        }
+
+        entity.setSummaryAgentEngineer(dto.getSummaryAgentEngineer());
+        entity.setCaseQueryAgentEngineer(dto.getCaseQueryAgentEngineer());
+        entity.setSceneId(sceneId);
+
+        saveOrUpdate(entity) ;
+    }
+}
