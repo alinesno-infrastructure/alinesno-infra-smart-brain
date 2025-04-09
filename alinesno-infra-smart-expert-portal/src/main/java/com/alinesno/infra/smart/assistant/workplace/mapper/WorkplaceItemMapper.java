222c784f99a6ddb62abfd2cd066d84bbbee09596
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 WorkplaceItemMapper 接口，实现数据映射功能。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/mapper/WorkplaceItemMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.workplace.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceItemEntity;
+
+public interface WorkplaceItemMapper extends IBaseMapper<WorkplaceItemEntity> {
+}
