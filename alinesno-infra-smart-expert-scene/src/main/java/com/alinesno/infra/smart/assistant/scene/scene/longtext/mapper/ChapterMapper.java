a29768285260d95ce049e06baa3f0232bee051f2
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:29 +0800

    fix:修复 ChapterMapper 文件中导入路径错误，更新为 com.alinesno.infra.smart.scene.entity.ChapterEntity

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/mapper/ChapterMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/mapper/ChapterMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.longtext.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.assistant.scene.core.entity.ChapterEntity;
+import com.alinesno.infra.smart.scene.entity.ChapterEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
