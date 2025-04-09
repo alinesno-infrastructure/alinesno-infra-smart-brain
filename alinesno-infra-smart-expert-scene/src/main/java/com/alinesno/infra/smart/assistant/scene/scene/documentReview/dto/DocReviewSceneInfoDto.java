907b9db4032a907361ad825700792bef213e5d50
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:26 +0800

    feat:新增 DocReviewSceneInfoDto 类，用于文档审核场景的信息传输。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/DocReviewSceneInfoDto.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto;
+
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 文档场景信息传输对象
+ * 用于文档审核场景中传输相关信息和配置
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocReviewSceneInfoDto extends SceneInfoDto {
+
+    private long sceneId;  // 场景ID
+    private String documentId;   // 文档ID
+    private String documentName; // 文档名称
+
+    private long analysisAgentEngineer; // 分析智能工程师ID
+    private IndustryRoleEntity analysisAgentEngineerEntity;  // 分析智能工程师实体
+
+    private long logicReviewerEngineer; // 逻辑审核工程师ID
+    private IndustryRoleEntity logicReviewerEngineerEntity;  // 逻辑审核工程师实体
+
+    private String contractType;  // 合同类型
+    private String reviewPosition;  // 审查立场
+    private String reviewListOption ;  // 审核列表选项
+    private String reviewList;  // 审核列表
+    private List<DocReviewRulesDto> reviewListDtos ;  // 审核列表
+    private String reviewListKnowledgeBase;  // 审核列表知识库
+    private String contractOverview;  // 合同概述
+
+    private List<Map<String , String>> contractTypes ;   // 合同类型列表，每项包含合同类型的有关信息
+}
