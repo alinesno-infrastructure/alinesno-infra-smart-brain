71345464626259f0d4f903e0f723bfdeb04c2682
梁明树(Nina) <ninaAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 documentReader.js 文件，实现获取预览文档和查询场景接口功能。

---
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/documentReader.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/scene/documentReader/' ;
+
+var managerUrl = {
+  getScene: prefix +"getScene",
+  getPreviewDocx: prefix +"getPreviewDocx",
+}
+
+// 获取预览文档
+export function getPreviewDocx(sceneId) {
+  return request({
+    url: managerUrl.getPreviewDocx + '?sceneId=' + parseStrEmpty(sceneId),
+    responseType: 'arraybuffer', // 显式声明返回二进制流
+    method: 'get'
+  })
+}
+
+// 查询场景详细
+export function getScene(id) {
+  return request({
+    url: managerUrl.getScene + '?id=' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
