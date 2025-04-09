fa12357ce9f5c02dc234d94ae91f5d4c20e82310
周健(James) <jamesAip@gmail.com>
2025-04-10 00:20:27 +0800

    feat:新增 SceneResponseDto 类，用于封装场景相关数据模型。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/SceneResponseDto.java
@@ -0,0 +1,79 @@
+package com.alinesno.infra.smart.scene.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SceneResponseDto extends BaseDto {
+
+    /**
+     * 场景ID，唯一标识场景的唯一标识符
+     */
+    private long sceneId;
+
+    /**
+     * 场景名称，用于标识特定的场景或页面
+     */
+    private String sceneName;
+
+    /**
+     * 场景横幅，通常是显示在场景顶部的图像或文字
+     */
+    private String sceneBanner;
+
+    /**
+     * 场景描述，对场景功能或内容的简要说明
+     */
+    private String sceneDesc;
+
+    /**
+     * 场景范围，定义场景 applicableTo 的范围，可以是全局、部门、用户等
+     */
+    private String sceneScope ;
+
+    /**
+     * 场景数量，表示与此场景相关的项目或元素的数量
+     */
+    private Integer sceneCount;
+
+    /**
+     * 文档类型，定义与场景相关的文档的类型
+     */
+    private String docType;
+
+    /**
+     * 知识ID，唯一标识与场景相关联的知识库条目
+     */
+    private String knowledgeId;
+
+    /**
+     * 排序顺序，决定场景在列表或菜单中的显示顺序
+     */
+    private Integer sortOrder;
+
+    /**
+     * 是否推荐，指示场景是否被推荐给用户
+     */
+    private Boolean isRecommend;
+
+    /**
+     * 场景类型，用于定义场景的功能或类型
+     */
+    private String sceneType;
+
+    /**
+     * 智能助手，包含多个智能助手，用于处理场景的智能操作
+     */
+    private List<SceneAgent> agents = new ArrayList<>();
+
+    /**
+     * 组织机构名称
+     */
+    private String orgName ;
+}
