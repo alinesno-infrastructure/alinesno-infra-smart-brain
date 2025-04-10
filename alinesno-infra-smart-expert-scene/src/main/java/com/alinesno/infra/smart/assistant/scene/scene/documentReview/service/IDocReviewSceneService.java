4ffe660f01b488f14c7077bc10f19aae93b2dee3
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:新增更新场景智能助手的方法 updateSceneAgents

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewSceneService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewSceneService.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewInitDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.DocReviewSceneEntity;
 
 /**
@@ -24,4 +25,10 @@ public interface IDocReviewSceneService extends IBaseService<DocReviewSceneEntit
      * @return
      */
     DocReviewSceneEntity getBySceneId(long sceneId);
+
+    /**
+     * 更新场景的智能助手
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
 }
