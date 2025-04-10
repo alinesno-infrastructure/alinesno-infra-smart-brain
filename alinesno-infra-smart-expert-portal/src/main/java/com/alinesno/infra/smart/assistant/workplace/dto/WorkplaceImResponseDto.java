e291d39c8b49f957a5710b97deeffba612ec59af
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:58 +0800

    feat:新增 WorkplaceImResponseDto 类，用于封装工作台相关信息。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/dto/WorkplaceImResponseDto.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.smart.assistant.workplace.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.im.entity.ChannelEntity;
+import com.alinesno.infra.smart.scene.entity.SceneEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 工作台响应DTO类
+ * 继承自BaseDto，用于工作台信息的传输和展示
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class WorkplaceImResponseDto extends BaseDto {
+
+    /**
+     * 工作台名称
+     */
+    private String name;
+
+    /**
+     * 工作台描述
+     */
+    private String description;
+
+    /**
+     * 工作台背景图片地址
+     */
+    private String backgroundImage;
+
+    /**
+     * 工作台类型
+     */
+    private String workplaceType;
+
+    /**
+     * 工作台来源
+     */
+    private String workplaceSource;
+
+    /**
+     * 来源工作台ID
+     */
+    private long sourceWorkplaceId;
+
+    /**
+     * 频道列表
+     */
+    private List<ChannelOrgDto> channelsList;
+
+    /**
+     * 智能助手列表
+     */
+    private List<IndustryRoleOrgDto> agentsList;
+
+    /**
+     * 场景列表
+     */
+    private List<SceneOrgDto> scenesList;
+}
