6cfaa0573f21d263819ba437d0fe9ea894235d35
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:58 +0800

    feat:新增 OrgWorkplaceMapper 接口，继承 IBaseMapper 定义数据映射操作。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/mapper/OrgWorkplaceMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.workplace.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.assistant.workplace.entity.OrgWorkplaceEntity;
+
+public interface OrgWorkplaceMapper extends IBaseMapper<OrgWorkplaceEntity> {
+}
