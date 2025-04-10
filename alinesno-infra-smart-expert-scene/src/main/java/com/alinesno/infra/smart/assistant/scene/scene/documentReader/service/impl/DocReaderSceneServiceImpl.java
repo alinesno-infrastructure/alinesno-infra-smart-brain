04656dcdb0e078e7cd354974a80f5890777e2d83
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:新增 updateSceneAgents 方法，支持更新场景代理信息

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReader/service/impl/DocReaderSceneServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReader/service/impl/DocReaderSceneServiceImpl.java
@@ -1,9 +1,11 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReader.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.common.utils.RoleUtils;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReader.dto.DocReaderInitDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReader.mapper.DocReaderSceneMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReader.service.IDocReaderSceneService;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.DocReaderSceneEntity;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +39,29 @@ public class DocReaderSceneServiceImpl extends IBaseServiceImpl<DocReaderSceneEn
 
         entity.setSummaryAgentEngineer(dto.getSummaryAgentEngineer());
         entity.setCaseQueryAgentEngineer(dto.getCaseQueryAgentEngineer());
+        entity.setSceneId(sceneId);
+
+        saveOrUpdate(entity) ;
+    }
+
+    @Override
+    public void updateSceneAgents(UpdateSceneAgentDto dto) {
+        long sceneId = dto.getSceneId() ;
+
+        LambdaQueryWrapper<DocReaderSceneEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DocReaderSceneEntity::getSceneId, sceneId) ;
+        long count = count(wrapper) ;
+
+        DocReaderSceneEntity entity = new DocReaderSceneEntity() ;
+        if(count > 0){
+            entity = getOne(wrapper) ;
+        }
+
+        long summaryAgentEngineer = RoleUtils.findSelectAgentIdByCode(dto , "summaryAgent") ;
+        long caseQueryAgentEngineer = RoleUtils.findSelectAgentIdByCode(dto , "caseQueryAgent") ;
+
+        entity.setSummaryAgentEngineer(summaryAgentEngineer) ;
+        entity.setCaseQueryAgentEngineer(caseQueryAgentEngineer) ;
         entity.setSceneId(sceneId);
 
         saveOrUpdate(entity) ;
