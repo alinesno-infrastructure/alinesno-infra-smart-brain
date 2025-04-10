8112749d90d3ab1c71a78b8aedeff296c70c51df
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:优化 createPortal.vue 组件，增加 loading 效果、完善模板样式及交互逻辑。

--- alinesno-infra-smart-im-ui/src/views/base/portal/createPortal.vue
+++ alinesno-infra-smart-im-ui/src/views/base/portal/createPortal.vue
@@ -5,7 +5,7 @@
     width="1024"
     :before-close="handleClose"
   >
-    <div>
+    <div v-loading="loading">
 
       <div style="padding: 5px;margin-bottom: 10px;">
         选择不同的工作台会形成不同的<b>AI智能体</b>工作组合，并且可以根据需求进行定制工作台。
@@ -26,13 +26,13 @@
         </el-form-item>
 
         <el-form-item label="工作台模板" prop="template">
-          <el-row>
+          <el-row style="width:100%">
             <el-col :span="6" v-for="(item, index) in workbenches" :key="index">
               <div
                 :style="{
                   background: selectedTemplateId === item.id ? '#e5f6ff' : '#fff',
                   border: selectedTemplateId === item.id ? '1px solid #1890ff' : '1px solid rgba(6, 7, 9, 0.1)',
-                  borderRadius: '5px',
+                  borderRadius: '8px',
                   cursor: 'pointer',
                   margin: '10px',
                   display: 'flex',
@@ -40,7 +40,7 @@
                 }"
                 @click="selectTemplate(item.id)"
               >
-                <img :src="'http://data.linesno.com/icons/screen/'+(index+1)+'.webp'" style="width:100%" />
+                <img :src="imagePathByPath(item.backgroundImage)" style="width: 100%;border-radius: 7px 7px 0px 0px;" />
                 <div style="display: flex;flex-direction: column;line-height: 1.4rem;gap: 5px;padding: 10px;">
                   <span
                     style="
@@ -63,15 +63,6 @@
                     {{ item.description }}
                   </div>
                 </div>
-                <!-- <div class="item-footer-panel" >
-                  <span>
-                    {{ item.team }} 
-                  </span>
-                  <span>
-                    启用 {{ item.usageCount }} 
-                  </span> 
-                </div>
-                  -->
               </div>
             </el-col>
           </el-row>
@@ -81,7 +72,7 @@
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="handleCancel" size="large">关闭</el-button>
-        <el-button type="primary" text bg size="large" @click="handleSubmit">
+        <el-button v-loading="submitLoading" type="primary" size="large" @click="handleSubmit">
           确认
         </el-button>
       </div>
@@ -93,7 +84,16 @@
 import { ref } from 'vue';
 import { ElMessage } from 'element-plus';
 
-const dialogVisible = ref(true);
+import {
+  listOrgWorkplace, 
+  useWorkplace,
+} from "@/api/base/im/workplace"
+
+const emit = defineEmits(['handleHasWorkplace']);
+
+const dialogVisible = ref(false);
+const loading = ref(false);
+const submitLoading = ref(false);
 const ruleFormRef = ref(null);
 const ruleForm = ref({
   name: '',
@@ -102,56 +102,7 @@ const ruleForm = ref({
 const formSize = ref('large');
 const selectedTemplateId = ref(null);
 
-const workbenches = ref([
-  {
-    id: 99,
-    name: "通用工作台",
-    status: 1,
-    description: "具备多种常用功能的通用型工作台，可用于不同场景下的任务管理、信息整合等操作",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  },
-  {
-    id: 1,
-    name: "校园社团工作台",
-    status: 1,
-    description: "针对于校园社团快速AI化管理工作台，集成社团能力",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  },
-  {
-    id: 2,
-    name: "产品运营工作台",
-    status: 0,
-    description: "助力电商商家高效运营的工作台，整合营销、订单管理等功能",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  },
-  {
-    id: 3,
-    name: "企业办公工作台",
-    status: 0,
-    description: "为企业员工提供便捷办公服务的工作台，集成文档管理、流程审批等能力",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  },
-  {
-    id: 10,
-    name: "媒体内容创作工作台",
-    status: 0,
-    description: "用于媒体从业者进行内容创作和发布的工作台，集成图文编辑、视频制作等能力",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  },
-  {
-    id: 11,
-    name: "售前部门工作台",
-    status: 0,
-    description: "服务于售前部门的工作台，可进行客户需求分析、方案制定、报价管理等操作",
-    team: "来自AIP团队",
-    usageCount: "8981"
-  }
-]);    
+const workbenches = ref([]);    
 
 const rules = ref({
   name: [
@@ -188,14 +139,49 @@ const handleSubmit = () => {
   ruleFormRef.value.validate((valid) => {
     if (valid) {
       // 这里可以添加提交表单数据的逻辑，比如发送请求到后端
-      ElMessage.success('表单提交成功');
-      dialogVisible.value = false;
+
+      submitLoading.value = true;
+
+      const data = {
+        workplaceName: ruleForm.value.name,
+        workplaceId: selectedTemplateId.value
+      }
+
+      useWorkplace(data).then(res => {
+        ElMessage.success('表单提交成功');
+        submitLoading.value = false;
+        dialogVisible.value = false;
+
+        emit("handleHasWorkplace")
+      }).catch(() => {
+        submitLoading.value = false;
+      })
+
     } else {
       // ElMessage.error('表单验证失败，请检查输入');
       return false;
     }
   });
-};
+}
+
+const handleListOrgWorkplace = () => {
+  loading.value = true;
+  listOrgWorkplace().then(res => {
+    workbenches.value = res.data;
+    loading.value = false;
+  })
+}
+
+const openSelectWorkplace = () => {
+  dialogVisible.value = true;
+  handleListOrgWorkplace();
+}
+
+// 提供给父类使用
+defineExpose({
+  openSelectWorkplace
+})
+
 </script>
 
 <style lang="scss" scoped>
@@ -206,6 +192,7 @@ const handleSubmit = () => {
   -webkit-line-clamp: 2;
   overflow: hidden;
   text-overflow: ellipsis;
+  height: 45px;
 }
 
 .item-footer-panel {
