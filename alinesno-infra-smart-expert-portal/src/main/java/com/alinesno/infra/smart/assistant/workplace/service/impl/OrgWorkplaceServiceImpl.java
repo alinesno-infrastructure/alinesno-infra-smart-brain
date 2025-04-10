88d6b134af198987229de075e595d5b2a842119e
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:58 +0800

    feat:实现 OrgWorkplaceServiceImpl 类，支持组织使用指定工作台的功能。

---
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/OrgWorkplaceServiceImpl.java
@@ -0,0 +1,103 @@
+package com.alinesno.infra.smart.assistant.workplace.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.workplace.dto.OrgWorkplaceDto;
+import com.alinesno.infra.smart.assistant.workplace.entity.OrgWorkplaceEntity;
+import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceEntity;
+import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceItemEntity;
+import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceSourceEnums;
+import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceType;
+import com.alinesno.infra.smart.assistant.workplace.mapper.OrgWorkplaceMapper;
+import com.alinesno.infra.smart.assistant.workplace.service.IOrgWorkplaceService;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceItemService;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class OrgWorkplaceServiceImpl extends IBaseServiceImpl<OrgWorkplaceEntity, OrgWorkplaceMapper> implements IOrgWorkplaceService {
+
+    @Autowired
+    private IWorkplaceService workplaceService ;
+
+    @Autowired
+    private IWorkplaceItemService workplaceItemService ;
+
+    /**
+     * 组织使用指定的工作台，处理过程
+     * 1. 通过workplaceId复制工作台和工作台关联的Item到当前组织下面，然后修改名称
+     * 2. 在OrgWorkplace表中添加一条记录，表示当前组织已经有使用的工作台
+     */
+    @Override
+    public void useWorkplace(OrgWorkplaceDto dto) {
+
+        WorkplaceEntity workplace = workplaceService.getById(dto.getWorkplaceId())  ;
+
+        // 保存新的用户工作台
+        WorkplaceEntity newWorkplace = new WorkplaceEntity() ;
+        BeanUtils.copyProperties(workplace, newWorkplace);
+
+        newWorkplace.setId(null);
+        newWorkplace.setName(dto.getWorkplaceName());
+        newWorkplace.setOrgId(dto.getOrgId());
+        newWorkplace.setOperatorId(dto.getOperatorId());
+        newWorkplace.setWorkplaceType(WorkplaceType.ORG.getId());
+
+        newWorkplace.setSourceWorkplaceId(workplace.getId());
+        newWorkplace.setWorkplaceSource(WorkplaceSourceEnums.FRONTEND.getCode());
+
+        workplaceService.save(newWorkplace) ;
+
+        // 复制各个ID信息
+        LambdaQueryWrapper<WorkplaceItemEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(WorkplaceItemEntity::getWorkplaceId, workplace.getId());
+
+        List<WorkplaceItemEntity> list = workplaceItemService.list(lambdaQueryWrapper);
+        if(!list.isEmpty()){
+           for(WorkplaceItemEntity item : list) {
+               item.setId(null);
+               item.setWorkplaceId(newWorkplace.getId());
+           }
+        }
+        workplaceItemService.saveBatch(list) ;
+
+        // 然后保存到OrgWorkplace表中
+        OrgWorkplaceEntity orgWorkplaceEntity = new OrgWorkplaceEntity() ;
+        orgWorkplaceEntity.setWorkplaceId(newWorkplace.getId());
+        orgWorkplaceEntity.setOrgId(dto.getOrgId());
+        save(orgWorkplaceEntity) ;
+    }
+
+    @Override
+    public Long isHasWorkplace(Long orgId) {
+
+        LambdaQueryWrapper<OrgWorkplaceEntity> query = Wrappers.lambdaQuery(OrgWorkplaceEntity.class) ;
+        query.eq(OrgWorkplaceEntity::getOrgId, orgId) ;
+
+        OrgWorkplaceEntity orgWorkplaceEntity = getOne(query);
+
+        return orgWorkplaceEntity == null ? null : orgWorkplaceEntity.getWorkplaceId() ;
+    }
+
+    @Override
+    public WorkplaceEntity getCurrentWorkplace(Long orgId) {
+
+        LambdaQueryWrapper<OrgWorkplaceEntity> query = Wrappers.lambdaQuery(OrgWorkplaceEntity.class) ;
+        query.eq(OrgWorkplaceEntity::getOrgId, orgId) ;
+
+        OrgWorkplaceEntity orgWorkplaceEntity = getOne(query);
+        if (orgWorkplaceEntity != null) {
+            return workplaceService.getById(orgWorkplaceEntity.getWorkplaceId()) ;
+        }
+
+        return null ;
+    }
+}
