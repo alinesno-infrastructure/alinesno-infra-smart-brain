4197b052da136d16e7f01e6a830b74eb47ef8c81
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:新增 `findSelectAgentIdByCode` 方法，用于根据代码查找指定 Agent 的 ID。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/RoleUtils.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/RoleUtils.java
@@ -3,6 +3,7 @@ package com.alinesno.infra.smart.assistant.scene.common.utils;
 import com.alinesno.infra.smart.assistant.api.IndustryRoleDto;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import org.springframework.beans.BeanUtils;
 
 import java.util.Arrays;
@@ -10,6 +11,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class RoleUtils {
+
+    public static long findSelectAgentIdByCode(UpdateSceneAgentDto dto, String code) {
+        for (UpdateSceneAgentDto.AgentDto agent : dto.getAgents()) {
+            if (agent.getCode().equals(code)) {
+                return agent.getSelectAgentId();
+            }
+        }
+        return -1; // 如果未找到，返回 -1
+    }
 
     // 查询编辑人员的公共方法
      public static List<IndustryRoleDto> getEditors(IIndustryRoleService roleService , String editorString) {
