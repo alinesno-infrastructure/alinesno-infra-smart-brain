7f367631ff756b7aae0f862148b744a6028e59f7
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:新增 IWorkplaceItemService 接口，包含更新工作台元素及按类型查询方法。

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/IWorkplaceItemService.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/IWorkplaceItemService.java
@@ -1,6 +1,8 @@
 package com.alinesno.infra.smart.assistant.workplace.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceImResponseDto;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceItemDto;
 import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceResponseDto;
 import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceItemEntity;
 
@@ -13,4 +15,18 @@ public interface IWorkplaceItemService extends IBaseService<WorkplaceItemEntity>
      */
     WorkplaceResponseDto getWorkplaceItem(Long id);
 
+    /**
+     * 更新工作台元素信息
+      * @param dto
+     * @return
+     */
+    boolean updateWorkplaceItem(WorkplaceItemDto dto);
+
+    /**
+     * 根据工作台ID和类型获取工作台元素信息
+     * @param workplaceId
+     * @param type
+     * @return
+     */
+    WorkplaceImResponseDto getWorkplaceItemByType(Long workplaceId, String type);
 }
