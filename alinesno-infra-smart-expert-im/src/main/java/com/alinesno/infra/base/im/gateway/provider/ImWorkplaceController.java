e54c4a0afa959a5c754fe1a8c1a64b1810757701
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:58 +0800

    feat:新增 ImWorkplaceController 类，实现工作台是否有配置以及获取当前工作台的功能。

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/gateway/provider/ImWorkplaceController.java
@@ -0,0 +1,120 @@
+package com.alinesno.infra.base.im.gateway.provider;
+
+import com.alinesno.infra.common.core.utils.StringUtils;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
+import com.alinesno.infra.smart.assistant.workplace.dto.OrgWorkplaceDto;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceImResponseDto;
+import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceResponseDto;
+import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceEntity;
+import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceItemTypeEnums;
+import com.alinesno.infra.smart.assistant.workplace.service.IOrgWorkplaceService;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceItemService;
+import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceService;
+import io.jsonwebtoken.lang.Assert;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 工作台配置管理
+ */
+@Slf4j
+@RestController
+@RequestMapping(value = "/v1/api/infra/base/im/workplace/")
+public class ImWorkplaceController {
+
+    @Autowired
+    private IWorkplaceService workplaceService;
+
+    @Autowired
+    private IOrgWorkplaceService orgWorkplaceService;
+
+    @Autowired
+    private IWorkplaceItemService workplaceItemService;
+
+    /**
+     * 是否有工作台
+     * @return
+     */
+    @GetMapping("/isHasWorkplace")
+    public AjaxResult isHasWorkplace() {
+        Long workplaceId = orgWorkplaceService.isHasWorkplace(CurrentAccountJwt.get().getOrgId()) ;
+        return AjaxResult.success("操作成功." , workplaceId);
+    }
+
+    /**
+     * 获取当前工作台
+     * @return
+     */
+    @GetMapping("/getCurrentWorkplace")
+    public AjaxResult getCurrentWorkplace(Long workplaceId) {
+
+        WorkplaceEntity entity = workplaceService.getById(workplaceId) ; // orgWorkplaceService.getCurrentWorkplace(CurrentAccountJwt.get().getOrgId()) ;
+
+        WorkplaceResponseDto dto = new WorkplaceResponseDto() ;
+        BeanUtils.copyProperties(entity , dto) ;
+
+//        WorkplaceResponseDto workplaceItemDto = workplaceItemService.getWorkplaceItem(entity.getId()) ;
+//        dto.setAgentsList(workplaceItemDto.getAgentsList());
+//        dto.setChannelsList(workplaceItemDto.getChannelsList());
+//        dto.setScenesList(workplaceItemDto.getScenesList());
+
+        return AjaxResult.success("操作成功." , dto);
+    }
+
+    /**
+     * 获取组织工作台，组织工作台为公共工作台和组织当前工作台
+     * @return
+     */
+    @GetMapping("/listOrgWorkplace")
+    public AjaxResult listOrgWorkplace() {
+        List<WorkplaceEntity> list =  workplaceService.listOrgWorkplace(CurrentAccountJwt.get().getOrgId()) ;
+        return AjaxResult.success(list);
+    }
+
+    /**
+     * 使用用户工作台
+     * @return
+     */
+    @DataPermissionSave
+    @PostMapping("/useWorkplace")
+    public AjaxResult useWorkplace(@RequestBody OrgWorkplaceDto dto) {
+        orgWorkplaceService.useWorkplace(dto) ;
+        return AjaxResult.success();
+    }
+
+
+    /**
+     * 获取到当前工作区的智能体、频道、场景
+     */
+    @GetMapping("/getWorkplaceItem")
+    public AjaxResult getWorkplaceItem(
+            @RequestParam Long workplaceId,
+            @RequestParam String type) {
+
+        // 获取数据
+        WorkplaceImResponseDto dto = workplaceItemService.getWorkplaceItemByType(workplaceId, type);
+        Assert.notNull(dto, "未找到对应的工作区项");
+
+        // 使用枚举进行类型匹配
+        WorkplaceItemTypeEnums itemType = WorkplaceItemTypeEnums.getByCode(type);
+        Assert.notNull(itemType, "无效的类型");
+
+        return switch (itemType) {
+            case AGENT ->
+                    AjaxResult.success(dto.getAgentsList() != null ? dto.getAgentsList() : Collections.emptyList());
+            case CHANNEL ->
+                    AjaxResult.success(dto.getChannelsList() != null ? dto.getChannelsList() : Collections.emptyList());
+            case SCENE ->
+                    AjaxResult.success(dto.getScenesList() != null ? dto.getScenesList() : Collections.emptyList());
+        };
+    }
+
+
+}
