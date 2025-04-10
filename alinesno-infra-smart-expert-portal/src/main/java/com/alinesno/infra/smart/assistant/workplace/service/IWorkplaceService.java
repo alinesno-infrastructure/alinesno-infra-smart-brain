2fcb37482148ebf71fc89ae745a34575d3d7546e
陈伟(Victor) <victorAip@gmail.com>
2025-04-11 00:14:59 +0800

    feat:新增查询组织工作台接口，支持获取组织当前及公共工作台数据。

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/IWorkplaceService.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/IWorkplaceService.java
@@ -4,6 +4,8 @@ import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceAddDto;
 import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceEntity;
 
+import java.util.List;
+
 public interface IWorkplaceService extends IBaseService<WorkplaceEntity> {
 
     /**
@@ -12,4 +14,12 @@ public interface IWorkplaceService extends IBaseService<WorkplaceEntity> {
      * @return
      */
     Long createWorkplace(WorkplaceAddDto dto);
+
+    /**
+     * 查询组织工作台，包括组织当前的工作台和公共的工作台
+     * @param orgId
+     * @return
+     */
+    List<WorkplaceEntity> listOrgWorkplace(Long orgId);
+
 }
