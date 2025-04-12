fe47b90cbc73cc6ac5c957a8d2dded82fe8f3d94
张婷(Tina) <tinaAip@gmail.com>
2025-04-13 00:13:52 +0800

    feat:新增 ChatSendMessageDto 类，支持消息发送相关字段及转义方法。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ChatSendMessageDto.java
@@ -1,19 +1,23 @@
 package com.alinesno.infra.smart.im.dto;
 
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class ChatSendMessageDto implements Serializable {
+public class ChatSendMessageDto extends BaseDto {
 
     private String message;
     private List<Long> users = new ArrayList<>() ; // Agent用户ID列表
     private List<Long> businessIds = new ArrayList<>() ;  // 业务ID列表
     private List<Long> fileIds = new ArrayList<>(); // 文件ID列表
     private Long channelId;
+    private String channelStreamId ;  // 频道流ID
 
     private Long accountId ; // 操作账号ID
     private String accountName; // 操作账号名称
@@ -22,4 +26,17 @@ public class ChatSendMessageDto implements Serializable {
 
     private String type;
 
+    /**
+     * 转换为权限实体
+     * @return
+     */
+    public MessageEntity toPowerMessageEntity() {
+        MessageEntity messageEntity = new MessageEntity();
+
+        messageEntity.setOrgId(this.getOrgId());
+        messageEntity.setDepartmentId(this.getDepartmentId());
+        messageEntity.setOperatorId(this.getOperatorId());
+
+        return messageEntity ;
+    }
 }
