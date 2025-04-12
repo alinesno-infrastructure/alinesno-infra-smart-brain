51bffadbf888fb359c39695e690bcba4184830aa
梁明树(Nina) <ninaAip@gmail.com>
2025-04-13 00:13:53 +0800

    fix: 移除 roleChat 接口中不必要的加密头部配置

--- alinesno-infra-smart-im-ui/src/api/base/im/roleChat.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/roleChat.js
@@ -89,9 +89,6 @@ export function getInfo(roleId) {
 export function chatRole(data , roleId) {
   return request({
     url: managerUrl.chatRole + '?roleId=' + parseStrEmpty(roleId),
-    headers: {
-      isEncrypt: true
-    },
     method: 'post',
     data: data
   })
