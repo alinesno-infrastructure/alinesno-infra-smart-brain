c769d2e019d9f77d1e17e16332538d1abb998692
张婷(Tina) <tinaAip@gmail.com>
2025-04-13 00:13:51 +0800

    feat:新增 LongTextSceneController 控制器，用于处理长文本场景相关请求。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/controller/LongTextSceneController.java
@@ -0,0 +1,126 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.smart.assistant.scene.common.utils.RoleUtils;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.dto.ChapterPromptContentRequestDto;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.dto.LongTextSceneDto;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.service.IChapterService;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.service.ILongTextSceneService;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
+import com.alinesno.infra.smart.scene.entity.LongTextSceneEntity;
+import com.alinesno.infra.smart.scene.entity.SceneEntity;
+import com.alinesno.infra.smart.scene.enums.SceneEnum;
+import com.alinesno.infra.smart.scene.service.ISceneService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.util.Assert;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/smart/assistant/scene/longText/")
+public class LongTextSceneController {
+
+    @Autowired
+    private ISceneService service;
+
+    @Autowired
+    private ILongTextSceneService longTextSceneService;
+
+    @Autowired
+    private IIndustryRoleService roleService ;
+
+    @Autowired
+    private IChapterService chapterService;
+
+    /**
+     * 通过Id获取到场景
+     *
+     * @return
+     */
+    @DataPermissionQuery
+    @GetMapping("/getScene")
+    public AjaxResult getScene(@RequestParam("id") long id , PermissionQuery query) {
+
+        Assert.isTrue(id > 0, "参数不能为空");
+
+        SceneEntity entity = service.getById(id);
+        if (entity == null) {
+            return AjaxResult.error("未找到对应的场景实体");
+        }
+
+        LongTextSceneDto dto = new LongTextSceneDto();
+        BeanUtils.copyProperties(entity, dto);
+
+        SceneInfoDto sceneInfoDto = SceneEnum.getSceneInfoByCode(entity.getSceneType());
+
+        dto.setAgents(sceneInfoDto.getAgents());
+        dto.setSceneId(sceneInfoDto.getId());
+
+        LongTextSceneEntity longTextSceneEntity = longTextSceneService.getBySceneId(id, query);
+        if(longTextSceneEntity != null){
+
+            dto.setChapterEditor(longTextSceneEntity.getChapterEditor());
+            dto.setContentEditor(longTextSceneEntity.getContentEditor());
+            dto.setGenStatus(longTextSceneEntity.getGenStatus());
+            dto.setChapterPromptContent(longTextSceneEntity.getChapterPromptContent());
+
+            dto.setChapterEditors(RoleUtils.getEditors(roleService , longTextSceneEntity.getChapterEditor())); // 查询出当前的章节编辑人员
+            dto.setContentEditors(RoleUtils.getEditors(roleService, longTextSceneEntity.getContentEditor())); // 查询出当前的内容编辑人员
+
+            dto.setChapterTree(chapterService.getChapterTree(entity.getId() , longTextSceneEntity.getId())); // 章节树信息
+        }
+
+        return AjaxResult.success("操作成功.", dto);
+    }
+
+    /**
+     * 更新chapterPromptContent内容
+     * @return
+     */
+    @DataPermissionQuery
+    @PostMapping("/updateChapterPromptContent")
+    public AjaxResult updateChapterPromptContent(@RequestBody @Validated ChapterPromptContentRequestDto dto , PermissionQuery query) {
+
+        LongTextSceneEntity sceneEntity = longTextSceneService.getBySceneId(dto.getSceneId() , query) ;
+        sceneEntity.setChapterPromptContent(dto.getPromptContent());
+
+        longTextSceneService.updateById(sceneEntity);
+        return AjaxResult.success("操作成功") ;
+    }
+
+    /**
+     * 更新生成状态
+     * @return
+     */
+    @DataPermissionQuery
+    @GetMapping("/updateSceneGenStatus")
+    public AjaxResult updateSceneGenStatus(@RequestParam("sceneId") long sceneId ,
+                                           @RequestParam("genStatus") int genStatus,
+                                           PermissionQuery query) {
+
+        LongTextSceneEntity sceneEntity = longTextSceneService.getBySceneId(sceneId , query) ;
+        sceneEntity.setGenStatus(genStatus);
+
+        longTextSceneService.updateById(sceneEntity);
+        return AjaxResult.success("操作成功") ;
+    }
+
+
+
+}
