dd6e323eaf42bce37908bcad481f2a84f1919d62
张婷(Tina) <tinaAip@gmail.com>
2025-04-13 00:13:53 +0800

    feat:新增 ChatRoleDto 类，支持场景ID、角色ID、消息内容等属性，并实现转为 MessageTaskInfo 的方法。

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/ChatRoleDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/ChatRoleDto.java
@@ -1,19 +1,25 @@
 package com.alinesno.infra.smart.scene.dto;
 
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.Min;
-import java.io.Serializable;
 
 /**
  * 与角色交互的数据传输对象
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class ChatRoleDto implements Serializable {
+public class ChatRoleDto extends BaseDto {
 
     @Min(value = 1, message = "sceneId不能为空")
     private long sceneId ;  // 场景ID
+
+    @NotBlank(message = "channelStreamId不能为空")
+    private String channelStreamId ;
 
     @NotBlank(message = "message不能为空")
     private String message ; // 消息内容
@@ -21,4 +27,13 @@ public class ChatRoleDto implements Serializable {
     @Min(value = 1, message = "channelId不能为空")
     private long roleId ; // 角色ID
 
+    public MessageTaskInfo toPowerMessageTaskInfo() {
+        MessageTaskInfo taskInfo = new MessageTaskInfo() ;
+
+        taskInfo.setOrgId(this.getOrgId());
+        taskInfo.setDepartmentId(this.getDepartmentId());
+        taskInfo.setOperatorId(this.getOperatorId());
+
+        return taskInfo ;
+    }
 }
