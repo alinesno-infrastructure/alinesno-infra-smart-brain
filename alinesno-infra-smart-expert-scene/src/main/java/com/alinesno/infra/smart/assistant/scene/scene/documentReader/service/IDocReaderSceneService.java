4fa1d4672034c3e864d5664ba8012fb3e2349ef4
张婷(Tina) <tinaAip@gmail.com>
2025-04-13 00:13:52 +0800

    feat:新增 IDocReaderSceneService 接口，包含获取场景角色列表功能

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReader/service/IDocReaderSceneService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReader/service/IDocReaderSceneService.java
@@ -1,9 +1,13 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReader.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReader.dto.DocReaderInitDto;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
 import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.DocReaderSceneEntity;
+
+import java.util.List;
 
 /**
  *
@@ -31,4 +35,11 @@ public interface IDocReaderSceneService extends IBaseService<DocReaderSceneEntit
      * @param dto
      */
     void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取场景角色列表
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
 }
