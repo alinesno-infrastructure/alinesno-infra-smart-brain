864f6baf9009f402030b4e1595f2804d8c99a450
张婷(Tina) <tinaAip@gmail.com>
2025-04-13 00:13:51 +0800

    feat:新增 ILongTextSceneService 接口，包含场景信息查询、AI代理更新及角色列表获取功能。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/service/ILongTextSceneService.java
@@ -0,0 +1,41 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
+import com.alinesno.infra.smart.scene.entity.LongTextSceneEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ILongTextSceneService extends IBaseService<LongTextSceneEntity> {
+
+    /**
+     * 根据场景ID获取场景信息
+     *
+     * @param id
+     * @param query
+     * @return
+     */
+    LongTextSceneEntity getBySceneId(long id, PermissionQuery query);
+
+    /**
+     * 更新场景的AI代理信息
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取场景的AI代理信息
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
+}
