4e6bf44a8d6a552c11d8937a6f6a6f27ad883deb
李伟(Michael) <michaelAip@gmail.com>
2025-04-14 00:11:02 +0800

    feat:扩展 addRoleToScene 方法，新增 sceneScope 和 orgId 参数，支持更细粒度的角色场景绑定逻辑。

--- alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/AgentSceneServiceImpl.java
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/service/impl/AgentSceneServiceImpl.java
@@ -26,7 +26,7 @@ public class AgentSceneServiceImpl extends IBaseServiceImpl<AgentSceneEntity, Ag
     private IIndustryRoleService industryRoleService ;
 
     @Override
-    public void addRoleToScene(Long roleId, long sceneId, long agentTypeId, long llmModelId) {
+    public void addRoleToScene(Long roleId, long sceneId, long agentTypeId, long llmModelId, String sceneScope, Long orgId) {
 
         log.debug("添加角色到场景中 roleId = {} , sceneId = {} ,agentTypeId = {} " , roleId , sceneId ,  agentTypeId);
 
@@ -58,6 +58,9 @@ public class AgentSceneServiceImpl extends IBaseServiceImpl<AgentSceneEntity, Ag
             agentSceneEntity.setRoleOrganizationId(AgentConstants.STORE_EMPTY_ORG_ID); // 发布到公共角色场景
 
             agentSceneEntity.setLlmModelId(llmModelId);
+            agentSceneEntity.setSceneScope(sceneScope);
+
+            agentSceneEntity.setOrgId(orgId);
 
             saveOrUpdate(agentSceneEntity);
 
@@ -79,9 +82,20 @@ public class AgentSceneServiceImpl extends IBaseServiceImpl<AgentSceneEntity, Ag
     public List<IndustryRoleEntity> getRoleBySceneIdAndAgentType(long sceneId, long agentTypeId, long orgId) {
 
         if(sceneId != 0 && agentTypeId != 0){
+//            LambdaQueryWrapper<AgentSceneEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+//            lambdaQueryWrapper.eq(AgentSceneEntity::getSceneId, sceneId);
+//            lambdaQueryWrapper.eq(AgentSceneEntity::getAgentTypeId, agentTypeId);
+
             LambdaQueryWrapper<AgentSceneEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
             lambdaQueryWrapper.eq(AgentSceneEntity::getSceneId, sceneId);
             lambdaQueryWrapper.eq(AgentSceneEntity::getAgentTypeId, agentTypeId);
+            lambdaQueryWrapper.and(wrapper ->
+                    wrapper.eq(AgentSceneEntity::getSceneScope, "public")
+                            .or(subWrapper ->
+                                    subWrapper.eq(AgentSceneEntity::getSceneScope, "org")
+                                            .eq(AgentSceneEntity::getOrgId, orgId)
+                            )
+            );
 
             List<AgentSceneEntity> agentSceneEntities = list(lambdaQueryWrapper);
 
