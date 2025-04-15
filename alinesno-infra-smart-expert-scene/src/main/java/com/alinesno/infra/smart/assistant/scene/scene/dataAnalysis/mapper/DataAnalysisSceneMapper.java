3a28709d3d28b078b0d4c8e195e57f0e5328de09
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:39 +0800

    feat:新增 DataAnalysisSceneMapper 接口，实现数据分析场景数据操作的基础映射

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/dataAnalysis/mapper/DataAnalysisSceneMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DataAnalysisSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 数据分析场景Mapper接口
+ */
+@Mapper
+public interface DataAnalysisSceneMapper extends IBaseMapper<DataAnalysisSceneEntity> {
+}    
