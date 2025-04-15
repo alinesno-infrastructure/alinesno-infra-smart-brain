37e5ee0ddd6201fdfb19358c3ce537e689b7c7db
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:40 +0800

    feat:新增 WorkplaceServiceImpl.deleteWorkplace 方法，支持批量删除工作区及其关联的中间表数据。

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/WorkplaceServiceImpl.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/WorkplaceServiceImpl.java
@@ -1,16 +1,20 @@
 package com.alinesno.infra.smart.assistant.workplace.service.impl;
 
+import cn.hutool.extra.spring.SpringUtil;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.smart.assistant.workplace.dto.WorkplaceAddDto;
+import com.alinesno.infra.smart.assistant.workplace.entity.OrgWorkplaceEntity;
 import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceEntity;
 import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceSourceEnums;
 import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceType;
 import com.alinesno.infra.smart.assistant.workplace.mapper.WorkplaceMapper;
+import com.alinesno.infra.smart.assistant.workplace.service.IOrgWorkplaceService;
 import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -47,6 +51,32 @@ public class WorkplaceServiceImpl extends IBaseServiceImpl<WorkplaceEntity, Work
         List<WorkplaceEntity> workplaceEntities = list(queryWrapper);
 
         return workplaceEntities == null ? Collections.emptyList() : workplaceEntities ;
+    }
+
+    @Override
+    public void deleteWorkplace(String ids) {
+        if (StringUtils.isBlank(ids)) {
+            return ;
+        }
+
+        String[] rowsId = ids.split(",");
+        Long[] longIds = new Long[rowsId.length];
+
+        for (int i = 0; i < rowsId.length; i++) {
+            longIds[i] = Long.parseLong(rowsId[i]);
+        }
+
+        if (rowsId.length > 0) {
+            deleteByIds(longIds);
+        }
+
+        IOrgWorkplaceService orgWorkplaceService = SpringUtil.getBean(IOrgWorkplaceService.class);
+
+        // 删除中间表数据
+        LambdaUpdateWrapper<OrgWorkplaceEntity> queryWrapper = new LambdaUpdateWrapper<>() ;
+        queryWrapper.in(OrgWorkplaceEntity::getWorkplaceId, (Object[]) longIds) ;
+
+        orgWorkplaceService.remove(queryWrapper) ;
     }
 
 }
