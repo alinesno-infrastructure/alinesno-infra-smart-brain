cb367473fef01db3eb715849c7222ff973540026
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:40 +0800

    feat: 实现模板参数格式更新方法及模板查询逻辑

--- alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/service/impl/TemplateServiceImpl.java
+++ alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/service/impl/TemplateServiceImpl.java
@@ -1,11 +1,16 @@
 package com.alinesno.infra.smart.assistant.template.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.response.R;
 import com.alinesno.infra.smart.assistant.adapter.service.CloudStorageConsumer;
+import com.alinesno.infra.smart.assistant.template.dto.TemplateParamJsonRequestDto;
 import com.alinesno.infra.smart.assistant.template.entity.TemplateEntity;
 import com.alinesno.infra.smart.assistant.template.mapper.TemplateMapper;
 import com.alinesno.infra.smart.assistant.template.service.ITemplateService;
+import com.alinesno.infra.smart.assistant.template.utils.TemplateProcessor;
+import com.alinesno.infra.smart.scene.enums.SceneScopeType;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.deepoove.poi.XWPFTemplate;
 import lombok.SneakyThrows;
@@ -18,6 +23,7 @@ import org.springframework.util.Assert;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -61,6 +67,17 @@ public class TemplateServiceImpl extends IBaseServiceImpl<TemplateEntity, Templa
     }
 
     /**
+     * 更新模板参数格式
+     * @param dto
+     */
+    @Override
+    public void updateTemplateParamFormat(TemplateParamJsonRequestDto dto) {
+       TemplateEntity templateEntity = getById(dto.getTemplateId()) ;
+       templateEntity.setParamFormat(dto.getJsonStr());
+       updateById(templateEntity) ;
+    }
+
+    /**
      * 生成word
      * @param dataMap
      * @param sourcePath
@@ -82,4 +99,29 @@ public class TemplateServiceImpl extends IBaseServiceImpl<TemplateEntity, Templa
         }
     }
 
+    @Override
+    public List<TemplateEntity> getTemplates(PermissionQuery query) {
+        LambdaQueryWrapper<TemplateEntity> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.and(wrapper -> wrapper
+                .eq(TemplateEntity::getOrgId, query.getOrgId())
+                .eq(TemplateEntity::getTemplateDataScope,SceneScopeType.ORG_SCENE.getValue()))
+        .or()
+        .eq(TemplateEntity::getTemplateDataScope, SceneScopeType.PUBLIC_SCENE.getValue());
+
+        return this.list(queryWrapper);
+    }
+
+    @Override
+    public String genTemplate(JSONObject dataObject, String templateId) {
+
+        TemplateEntity templateEntity = getById(templateId) ;
+
+        String templateUrl = storageConsumer.getPreviewUrl(templateEntity.getStorageFileId()).getData() ;
+        String docAbcPath = TemplateProcessor.processTemplateFromUrl(templateUrl, dataObject);
+
+        log.debug("docAbcPath = {}" , docAbcPath);
+
+        return storageConsumer.upload(new File(docAbcPath)).getData();
+    }
 }
