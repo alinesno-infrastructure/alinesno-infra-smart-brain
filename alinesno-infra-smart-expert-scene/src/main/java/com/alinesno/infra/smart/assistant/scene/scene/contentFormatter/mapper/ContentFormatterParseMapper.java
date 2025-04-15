1717fd121428c2b30de3d3f136f13ebe231b117c
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:39 +0800

    feat:新增 ContentFormatterParseMapper 接口，实现基础映射功能。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterParseMapper.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
+
+public interface ContentFormatterParseMapper extends IBaseMapper<ContentFormatterParseEntity> {
+}    
