42695467d7d92fd29888bd0ef39c275e2c2c6658
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:40 +0800

    feat:新增 ITemplateService 接口，包含更新模板参数格式、获取模板列表及生成模板等方法。

--- alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/service/ITemplateService.java
+++ alinesno-infra-smart-expert-template/src/main/java/com/alinesno/infra/smart/assistant/template/service/ITemplateService.java
@@ -1,8 +1,12 @@
 package com.alinesno.infra.smart.assistant.template.service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.template.dto.TemplateParamJsonRequestDto;
 import com.alinesno.infra.smart.assistant.template.entity.TemplateEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,4 +25,24 @@ public interface ITemplateService extends IBaseService<TemplateEntity> {
      */
     String parseTemplate(String templateKey , Map<String , Object> params) ;
 
+    /**
+     * 更新模板参数格式
+     * @param dto
+     */
+    void updateTemplateParamFormat(TemplateParamJsonRequestDto dto);
+
+    /**
+     * 获取到当前组织下的所有模板和公共的模板
+     * @param query
+     * @return
+     */
+    List<TemplateEntity> getTemplates(PermissionQuery query);
+
+    /**
+     * 生成模板
+     * @param dataObject
+     * @param templateId
+     * @return
+     */
+    String genTemplate(JSONObject dataObject, String templateId);
 }
