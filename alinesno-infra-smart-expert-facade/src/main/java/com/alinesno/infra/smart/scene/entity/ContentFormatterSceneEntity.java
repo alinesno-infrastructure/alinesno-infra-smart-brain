35254e91acbafcd84a25c8c224af553917c7fe73
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:39 +0800

    feat:新增 ContentFormatterSceneEntity 实体类，用于描述数据分析场景相关字段。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/ContentFormatterSceneEntity.java
@@ -0,0 +1,46 @@
+package com.alinesno.infra.smart.scene.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 数据分析场景实体类
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("content_formatter_scene")
+public class ContentFormatterSceneEntity extends InfraBaseEntity {
+
+    /**
+     * 场景ID
+     */
+    @TableField
+    @Column(name = "scene_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "场景ID")
+    private long sceneId;
+
+    /**
+     * 用于存储与数据分析相关的提示内容，可作为引导数据分析流程或操作的文本信息
+     */
+    @TableField
+    @Column(name = "content_prompt_content", type = MySqlTypeConstant.TEXT, comment = "用于存储与数据分析相关的提示内容，可作为引导数据分析流程或操作的文本信息")
+    private String contentPromptContent;
+
+    /**
+     * 记录负责模板提取的工程师姓名或标识，方便追溯责任人
+     */
+    @TableField
+    @Column(name = "template_extractor_engineer", type = MySqlTypeConstant.BIGINT, length = 32, comment = "记录负责模板提取的工程师姓名或标识，方便追溯责任人")
+    private String templateExtractorEngineer;
+
+    /**
+     * 生成状态(1生成菜单|0未生成)，默认未生成
+     */
+    @TableField
+    @Column(name = "gen_status", type = MySqlTypeConstant.VARCHAR, length = 32, comment = "生成状态(1生成菜单|0未生成)")
+    private Integer genStatus = 0;
+}
