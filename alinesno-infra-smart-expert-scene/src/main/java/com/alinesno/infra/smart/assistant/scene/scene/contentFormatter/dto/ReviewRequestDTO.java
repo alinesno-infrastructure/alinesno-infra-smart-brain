fd5cd8a61e5c47b81766c46c2acaa33a2e142cc5
王磊(Levi) <leviAip@gmail.com>
2025-04-15 20:16:38 +0800

    feat:新增 ReviewRequestDTO 数据传输对象，包含评审相关字段及校验规则。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/dto/ReviewRequestDTO.java
@@ -0,0 +1,39 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ReviewRequestDTO extends BaseDto {
+
+    @NotBlank(message = "reviewListOption不能为空")
+    private String reviewListOption;
+
+    @NotEmpty(message = "文档类型不能为空")
+    private List<String> documentType;
+
+    @NotBlank(message = "内容不能为空")
+    private String contentPromptContent ;
+
+    @NotEmpty(message = "业务场景不能为空")
+    private List<String> businessScenario;
+
+    @NotBlank(message = "模板不能为空")
+    private String templateId;
+
+    @NotNull(message = "场景不能为空")
+    private Long sceneId;
+
+    @NotNull(message = "角色不能为空")
+    private Long roleId ;
+
+    @NotNull(message = "流频道不能为空")
+    private Long channelStreamId;
+}    
