edf9cabc531b834b7b9b87dc359a3668c6acc80a
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:25 +0800

    feat:新增 DocReviewRulesService 接口，引入数据范围权限查询及规则组 DTO 支持。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewRulesService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewRulesService.java
@@ -1,7 +1,11 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRuleGroupDto;
 import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
+
+import java.util.List;
 
 /**
  *
@@ -10,5 +14,6 @@ import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
  * @version 1.0.0
  */
 public interface IDocReviewRulesService extends IBaseService<DocReviewRulesEntity> {
+
 
 }
