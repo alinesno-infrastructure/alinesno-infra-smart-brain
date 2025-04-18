86c7cb1d824ad0dd84df14aac1fc512bb22f0dad
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:新增 DocReviewAuditMapper 接口，实现文档审核相关实体的数据库操作映射。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewAuditMapper.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DocReviewAuditEntity;
+import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocReviewAuditMapper extends IBaseMapper<DocReviewAuditEntity> {
+
+}
