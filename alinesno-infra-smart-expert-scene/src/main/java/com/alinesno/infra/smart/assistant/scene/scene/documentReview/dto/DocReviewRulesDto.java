20d58d67d3a927e8fa359354401c9468d5db223e
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:新增 DocReviewRulesDto 类，包含规则名称、风险级别、规则内容等字段，并添加校验注解。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/DocReviewRulesDto.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/DocReviewRulesDto.java
@@ -1,40 +1,48 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto;
 
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 文档审核规则的DTO类
  * 用于文档审核场景中，定义了需要审核的文档所依据的规则信息
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class DocReviewRulesDto {
-
-    /**
-     * 规则ID
-     */
-    private long id ;
+public class DocReviewRulesDto extends BaseDto {
 
     /**
      * 规则名称
      * 用于标识审核规则的名称，便于理解和引用
      */
+    @NotBlank(message = "规则名称不能为空")
     private String ruleName;
 
     /**
-     * 规则级别
-     * 定义了审核规则的级别，可以用来区分规则的重要程度或适用范围
+     * 风险级别
      */
-    private String ruleLevel;
+    @NotBlank(message = "风险级别不能为空")
+    private String riskLevel;
 
     /**
      * 规则内容
      * 描述了审核规则的具体内容，是执行审核时的依据
      */
+    @NotBlank(message = "规则内容不能为空")
     private String ruleContent;
 
     /**
-     * 合同类型ID
-     * 关联了合同类型，使得审核规则能够针对不同类型的合同进行定制
+     * 关联的分组ID
      */
-    private String contractTypeId;
+    @NotNull(message = "关联的分组ID不能为空")
+    private Long groupId ;
+
+    /**
+     * 审核立场
+     */
+    @NotBlank(message = "审核立场不能为空")
+    private String reviewPosition ;
 }
