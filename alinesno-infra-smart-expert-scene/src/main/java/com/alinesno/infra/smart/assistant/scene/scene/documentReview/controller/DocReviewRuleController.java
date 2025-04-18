29431880730dce6a6aa05ca897b958ecaff7b650
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:新增 DocReviewRuleController 功能，支持数据权限注解及Excel解析工具集成。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/controller/DocReviewRuleController.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/controller/DocReviewRuleController.java
@@ -1,22 +1,38 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
-import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
-import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRulesDto;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewRuleGroupService;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewRulesService;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.tools.ExcelParserToMapPrinter;
 import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
-import jakarta.servlet.http.HttpServletRequest;
+import com.alinesno.infra.smart.scene.enums.ReviewPositionEnums;
+import com.alinesno.infra.smart.scene.enums.RiskLevelEnums;
+import io.jsonwebtoken.lang.Assert;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 处理与BusinessLogEntity相关的请求的Controller。
@@ -34,21 +50,75 @@ public class DocReviewRuleController extends BaseController<DocReviewRulesEntity
     @Autowired
     private IDocReviewRulesService service;
 
+    @Autowired
+    private IDocReviewRuleGroupService  ruleGroupService;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
+
     /**
-     * 获取BusinessLogEntity的DataTables数据。
-     *
-     * @param request HttpServletRequest对象。
-     * @param model Model对象。
-     * @param page DatatablesPageBean对象。
-     * @return 包含DataTables数据的TableDataInfo对象。
+     * 新增加规则
+     * @return
      */
     @DataPermissionScope
-    @ResponseBody
-    @PostMapping("/datatables")
-    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
-        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+    @PostMapping("/saveOrUpdateRule")
+    public AjaxResult addRule(@RequestBody @Validated DocReviewRulesDto dto) {
 
-        return this.toPage(model, this.getFeign(), page);
+        Assert.isTrue(RiskLevelEnums.contains(dto.getRiskLevel()) , "风险级别错误");
+        Assert.isTrue(ReviewPositionEnums.contains(dto.getReviewPosition()) , "审核立场错误");
+
+        log.debug("新增规则：{}", ToStringBuilder.reflectionToString(dto));
+
+        DocReviewRulesEntity entity = new DocReviewRulesEntity() ;
+        BeanUtils.copyProperties(dto , entity) ;
+
+        service.saveOrUpdate(entity);
+
+        return ok() ;
+    }
+
+
+    /**
+     * 文件上传
+     * @return
+     */
+    @DataPermissionQuery
+    @SneakyThrows
+    @PostMapping("/importData")
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, PermissionQuery query) {
+
+        String originalFilename = file.getOriginalFilename();
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(originalFilename).substring(originalFilename.lastIndexOf(".") + 1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath, newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        Map<String, List<Map<String, Object>>> result = ExcelParserToMapPrinter.parseExcelToMap(targetFile.getAbsolutePath());
+        ExcelParserToMapPrinter.printMap(result);
+
+        ruleGroupService.importExcelData(result , query) ;
+
+        return AjaxResult.success("审核规则导入成功");
+    }
+
+        /**
+         * 通过ids查询出规则列表
+         * @return
+         */
+    @DataPermissionQuery
+    @GetMapping("/listRuleByIds")
+    public AjaxResult listRuleByIds(@RequestParam String idsStr) {
+
+        // 使用 Stream API 分割字符串并转换为 Long 类型
+        List<Long> idsList = Arrays.stream(idsStr.split(","))
+                .map(Long::valueOf)
+                .collect(Collectors.toList());
+
+        return AjaxResult.success(service.listByIds(idsList)) ;
     }
 
     @Override
