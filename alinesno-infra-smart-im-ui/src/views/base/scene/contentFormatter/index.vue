85db14ac74071cdbf74c7d263881471d8e7e2578
张婷(Tina) <tinaAip@gmail.com>
2025-04-18 10:39:43 +0800

    feat:优化 contentFormatter 组件布局，重构输入框与表单样式，提升用户体验。

--- alinesno-infra-smart-im-ui/src/views/base/scene/contentFormatter/index.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/contentFormatter/index.vue
@@ -1,34 +1,62 @@
 <template>
-  <div class="content-formatter-container">
+    <div class="content-formatter-container">
 
-              <RoleSelectPanel 
-                  :currentSeneInfo="currentSceneInfo"
-                  @openExecuteHandle="openExecuteHandle"
-                  ref="roleSelectPanelRef" />
+        <RoleSelectPanel :currentSeneInfo="currentSceneInfo" @openExecuteHandle="openExecuteHandle"
+            ref="roleSelectPanelRef" />
 
-      <div class="main-content">
-          <div class="title-section">
-              <span class="title">内容排版，一键按模板格式</span>
-              <span class="description">运用先进算法，高效进行内容排版。输入文本后，即刻为您调整段落结构、字体格式等，根据模板进行排版，打造专业美观的内容呈现</span>
-          </div>
-          <div class="input-button-section">
-              <el-input type="textarea" 
-                  :rows="14" 
-                  v-model="promptText" 
-                  class="input-box" 
-                  size="large" 
-                  resize="none"
-                  placeholder="请输入您的数据及分析需求，生成对应的分析报告或进行数据处理"
-                  :prefix-icon="Search" />
-              <el-button type="primary" size="large" @click="generaterText()" class="send-button">
-                内容重新排版 &nbsp; <i class="fa-solid fa-paper-plane" style="font-size:15px"></i> 
-              </el-button>
-          </div>
-      </div>
-      <div class="review-footer-message">
-          <div class="footer-message">服务生成的所有内容均由人工智能模型生成，其生成内容的准确性和完整性无法保证，不能代表我们的态度和观点。</div>
-      </div>
-  </div>
+        <div class="main-content">
+            <div class="title-section">
+                <span class="title">内容排版，一键按模板格式</span>
+                <span class="description">高效进行内容排版，输入文本后，即刻为您调整段落结构、字体格式等，根据模板进行排版，打造专业美观的内容呈现</span>
+            </div>
+
+            <div>
+                <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules"
+                    style="display: flex;gap: 10px;flex-direction: column;" label-width="120px" :label-position="'top'"
+                    size="large">
+
+                    <div class="input-button-section">
+                        <el-form-item prop="promptText" style="width:100%">
+                            <el-input type="textarea" :rows="8" v-model="ruleForm.promptText" class="input-box" size="large"
+                                resize="none" placeholder="请输入您的数据及分析需求，生成对应的分析报告或进行数据处理" :prefix-icon="Search" />
+                        </el-form-item>
+                    </div>
+
+                    <div style="display: flex;flex-direction: row;gap: 10px;">
+
+                        <el-form-item prop="documentType" style="width:33%">
+                            <el-select v-model="ruleForm.documentType" multiple  collapse-tags collapse-tags-tooltip placeholder="请选择文书类型"
+                                style="width: 100%">
+                                <el-option v-for="option in documentOptions" :key="option.value" :border="false"
+                                    :value="option.value" :label="option.label" />
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item prop="businessScenario" style="width:33%">
+                            <el-select v-model="ruleForm.businessScenario" multiple collapse-tags collapse-tags-tooltip placeholder="请选择业务场景"
+                                style="width: 100%">
+                                <el-option v-for="option in businessOptions" :key="option.value" :border="false"
+                                    :value="option.value" :label="option.label" />
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item prop="templateId" style="width:33%">
+                            <el-select placeholder="选择排版模板" v-model="ruleForm.templateId">
+                                <el-option v-for="item in templateList" :key="item.id" :label="item.templateName"
+                                    :value="item.id">
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                    </div>
+                </el-form>
+            </div>
+
+            <el-button type="primary" size="large" @click="generaterText()" class="send-button">
+                内容重新排版 &nbsp; <i class="fa-solid fa-paper-plane" style="font-size:15px"></i>
+            </el-button>
+        </div>
+        <div class="review-footer-message">
+            <div class="footer-message">服务生成的所有内容均由人工智能模型生成，其生成内容的准确性和完整性无法保证，不能代表我们的态度和观点。</div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -36,57 +64,143 @@ import { ref } from 'vue';
 import { ElMessage } from 'element-plus';
 import RoleSelectPanel from '@/views/base/scene/common/roleSelectPanel'
 
-import { getScene , updateChapterPromptContent } from '@/api/base/im/scene/contentFormatter';
+import { getScene, updateChapterPromptContent } from '@/api/base/im/scene/contentFormatter';
 import SnowflakeId from "snowflake-id";
 
+import {
+    getTemplates,
+} from '@/api/base/im/scene/contentFormatter';
+
+const ruleFormRef = ref(null)
 const roleSelectPanelRef = ref(null)
 const snowflake = new SnowflakeId();
 const route = useRoute();
 const router = useRouter();
+const templateList = ref([]);
+
+// 定义表单数据
+const ruleForm = reactive({
+    promptText: '',
+    documentType: [],
+    businessScenario: [],
+    templateId: ''
+});
 
 const sceneId = ref(route.query.sceneId)
-const promptText = ref('');
+// const promptText = ref('');
+
+// 定义表单验证规则
+const rules = reactive({
+    promptText: [
+        { required: true, message: '请输入需要整理的文稿类型', trigger: 'blur' }
+    ],
+    documentType: [
+        { required: true, message: '请选择文稿类型', trigger: 'change' }
+    ],
+    businessScenario: [
+        { required: true, message: '请选择业务场景', trigger: 'change' }
+    ],
+    templateId: [
+        { required: true, message: '请选择排版模板', trigger: 'change' }
+    ]
+});
+
+const businessOptions = ref([
+    { value: 'government', label: '政务' },
+    { value: 'commerce', label: '商务' },
+    { value: 'ecommerce', label: '电商' },
+    { value: 'campus', label: '校园' },
+    { value: 'medical', label: '医疗' },
+    { value: 'education', label: '教育' },
+    { value: 'finance', label: '金融' },
+    { value: 'manufacturing', label: '制造' },
+    { value: 'media', label: '媒体' },
+    { value: 'entertainment', label: '娱乐' },
+    { value: 'transportation', label: '交通' },
+    { value: 'agriculture', label: '农业' },
+    { value: 'construction', label: '建筑' },
+    { value: 'energy', label: '能源' },
+    { value: 'tourism', label: '旅游' },
+    { value: 'service', label: '服务' }
+]);
+
+// 文书类型
+const documentOptions = ref([
+    { value: 'notice', label: '通知' },
+    { value: 'government_letter', label: '政务函' },
+    { value: 'red_head_document', label: '红头文件' },
+    { value: 'report', label: '报告' },
+    { value: 'memo', label: '备忘录' },
+    { value: 'contract', label: '合同' },
+    { value: 'agreement', label: '协议' },
+    { value: 'proposal', label: '提案' },
+    { value: 'announcement', label: '公告' },
+    { value: 'circular', label: '通报' },
+    { value: 'invitation', label: '邀请函' },
+    { value: 'declaration', label: '声明' },
+    { value: 'application', label: '申请书' },
+    { value: 'resume', label: '简历' }
+]);
 
 const list = [
-  { text: "分析某电商平台近三个月的销售数据并生成趋势报告" },
-  { text: "对公司员工的绩效数据进行清洗和统计分析" },
-  { text: "根据市场调研数据，撰写一份竞品分析报告" },
-  { text: "对某APP的用户行为数据进行聚类分析并给出优化建议" }
+    { text: "分析某电商平台近三个月的销售数据并生成趋势报告" },
+    { text: "对公司员工的绩效数据进行清洗和统计分析" },
+    { text: "根据市场调研数据，撰写一份竞品分析报告" },
+    { text: "对某APP的用户行为数据进行聚类分析并给出优化建议" }
 ];
 
 const currentSceneInfo = ref({
     sceneName: '文书生成',
 });
 
+const handleGetTemplates = () => {
+    getTemplates().then(res => {
+        // contractTypeList.value = res.data;
+        templateList.value = res.data;
+    })
+}
+
 const handleGetScene = () => {
     getScene(sceneId.value).then(res => {
         currentSceneInfo.value = res.data;
         // handleRoleBySceneIdAndAgentType();
 
-        if(!currentSceneInfo.value.templateExtractorEngineer){ // 选择配置角色
+        if (!currentSceneInfo.value.templateExtractorEngineer || !currentSceneInfo.value.contentReviewerEngineer) { // 选择配置角色
             roleSelectPanelRef.value.configAgent();
-            return ;
+            return;
         }
 
-        if(res.data.genStatus == 1){
-            jumpDataParser(); 
+        if (res.data.genStatus == 1) {
+            jumpDataParser();
         }
     })
 }
 
 const generaterText = () => {
-    if (!promptText.value) {
-        ElMessage.error('请输入内容');
+
+    const formEl = ruleFormRef.value;
+    if (!formEl) {
         return;
     }
-    ElMessage.success('生成成功');
 
-    updateChapterPromptContent({
-        sceneId: sceneId.value,
-        promptContent: promptText.value
-    }).then(res => {
-        jumpDataParser(); 
-    })
+
+    formEl.validate((valid) => {
+        if (valid) {
+            if (!ruleForm.promptText) {
+                ElMessage.error('请输入内容');
+                return;
+            }
+            ElMessage.success('生成成功');
+
+            updateChapterPromptContent({
+                sceneId: sceneId.value,
+                promptContent: ruleForm.promptText
+            }).then(res => {
+                jumpDataParser();
+            })
+        }
+    });
+
 
 }
 
@@ -96,19 +210,33 @@ const jumpDataParser = () => {
         path: '/scene/contentFormatter/contentParser',
         query: {
             sceneId: sceneId.value,
-            genStatus: false ,
-            channelStreamId: snowflake.generate() 
+            genStatus: false,
+            channelStreamId: snowflake.generate()
         }
     })
 }
 
 const handleExampleClick = (item) => {
-    promptText.value = item.text;
+    form.promptText.value = item.text;
     // generaterText();
 }
 
+// 存储表单数据到本地缓存
+const saveFormDataToLocalStorage = () => {
+    localStorage.setItem('ruleForm', JSON.stringify({
+        documentType: ruleForm.documentType,
+        businessScenario: ruleForm.businessScenario,
+        templateId: ruleForm.templateId
+    }));
+};
+
+watch([() => ruleForm.documentType, () => ruleForm.businessScenario, () => ruleForm.templateId], () => {
+    saveFormDataToLocalStorage();
+});
+
 onMounted(() => {
     handleGetScene();
+    handleGetTemplates();
 })
 
 </script>
@@ -122,7 +250,7 @@ onMounted(() => {
         width: 100%;
     }
 
-   .main-content {
+    .main-content {
         display: flex;
         flex-direction: column;
         padding-top: calc(10vh - 56px);
@@ -130,12 +258,12 @@ onMounted(() => {
         max-width: 1024px;
         margin: auto;
 
-       .title-section {
+        .title-section {
             display: flex;
             flex-direction: column;
             text-align: center;
 
-           .title {
+            .title {
                 color: #2C2C36;
                 font-weight: 600;
                 font-size: 28px;
@@ -143,7 +271,7 @@ onMounted(() => {
                 line-height: 40px;
             }
 
-           .description {
+            .description {
                 margin-top: 10px;
                 color: #8F91A8;
                 font-weight: 400;
@@ -152,24 +280,24 @@ onMounted(() => {
             }
         }
 
-       .input-button-section {
+        .input-button-section {
             display: flex;
             gap: 10px;
             position: relative;
             box-sizing: border-box;
             width: 100%;
             border-radius: 8px;
-            box-shadow: rgba(54, 54, 73, 0.06) 0px 12px 24px -16px, rgba(74, 80, 96, 0.12) 0px 12px 40px, rgba(44, 44, 54, 0.02) 0px 0px 1px;
+            // box-shadow: rgba(54, 54, 73, 0.06) 0px 12px 24px -16px, rgba(74, 80, 96, 0.12) 0px 12px 40px, rgba(44, 44, 54, 0.02) 0px 0px 1px;
             transition: 0.3s;
             background: rgb(255, 255, 255);
             padding: 10px !important;
             border: 1px solid rgb(232, 234, 242);
             margin-top: 30px;
-            margin-bottom: 30px;
+            margin-bottom: 20px;
             align-items: center;
             flex-direction: column;
 
-           .input-box {
+            .input-box {
                 width: 100%;
                 height: auto;
                 border: 0px !important;
@@ -177,10 +305,10 @@ onMounted(() => {
             }
         }
 
-       .example-section {
+        .example-section {
             padding: 0px 20px;
 
-           .example-title {
+            .example-title {
                 color: rgb(44, 44, 54);
                 font-size: 14px;
                 text-align: left;
@@ -189,11 +317,11 @@ onMounted(() => {
                 margin-bottom: 15px;
             }
 
-           .example-list {
+            .example-list {
                 display: flex;
                 flex-wrap: wrap;
 
-               .example-item {
+                .example-item {
                     position: relative;
                     display: flex;
                     gap: 8px;
@@ -211,6 +339,7 @@ onMounted(() => {
 
                     &:hover {
                         background: rgb(232 233 235);
+
                         .example-icon {
                             display: block;
                         }
@@ -219,10 +348,10 @@ onMounted(() => {
                     .example-icon {
                         display: none;
                         color: #585a73;
-                        font-size:12px;
+                        font-size: 12px;
                     }
 
-                   .example-text {
+                    .example-text {
                         flex: 1 1;
                         overflow: hidden;
                         color: #585a73;
@@ -237,7 +366,7 @@ onMounted(() => {
         }
     }
 
-   .review-footer-message {
+    .review-footer-message {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -246,7 +375,7 @@ onMounted(() => {
         padding: 12px 0px;
         text-align: center;
 
-       .footer-message {
+        .footer-message {
             margin-bottom: 4px;
             color: #C8CAD9;
             font-size: 12px;
@@ -254,12 +383,11 @@ onMounted(() => {
         }
     }
 }
-
 </style>
 
 
 <style>
-.content-formatter-container .el-textarea__inner{
+.content-formatter-container .el-textarea__inner {
     box-shadow: none !important;
 }
 </style>
