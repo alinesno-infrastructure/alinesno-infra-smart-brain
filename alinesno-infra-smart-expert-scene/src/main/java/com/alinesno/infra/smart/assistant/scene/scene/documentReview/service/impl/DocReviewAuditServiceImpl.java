b8a69f84635aec8e4fb9367e9f86d30f822e3f48
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:新增 DocReviewAuditServiceImpl 类，实现文档审核规则服务逻辑。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewAuditServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewRulesMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewRulesService;
+import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DocReviewAuditServiceImpl extends IBaseServiceImpl<DocReviewRulesEntity, DocReviewRulesMapper> implements IDocReviewRulesService {
+
+}
