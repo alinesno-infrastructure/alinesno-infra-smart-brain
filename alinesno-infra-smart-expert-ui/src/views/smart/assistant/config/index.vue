7026534a53a3c06e8d446f8d6ab7e5fba3cbbda3
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:重构全局配置页面，优化布局并提升用户体验

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/config/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/config/index.vue
@@ -1,78 +1,191 @@
 <template>
-  <div class="aip-config-container">
-    <el-tabs :tab-position="tabPosition" v-model="activeTab" @tab-click="handleTabClick">
-      <!-- 全局配置管理 -->
-      <el-tab-pane label="全局配置" name="globalConfig" value="globalConfig">
-        <template #label>
-          <span class="custom-tabs-label">
-            <i class="fa-solid fa-globe" />
-            <span>系统全局配置</span>
-          </span>
-        </template>
-        <GlobalConfigPanel />
-      </el-tab-pane>
-      <!-- 密钥配置管理 -->
-      <el-tab-pane label="密钥配置" name="secretConfig" value="secretConfig">
-        <template #label>
-          <span class="custom-tabs-label">
-            <i class="fa-solid fa-paper-plane" />
-            <span>全局密钥配置</span>
-          </span>
-        </template>
-        <GlobalSecretPanel />
-      </el-tab-pane>
-      <el-tab-pane label="模板配置" name="templateConfig" value="templateConfig">
-        <template #label>
-          <span class="custom-tabs-label">
-            <i class="fa-solid fa-file-pdf" />
-            <span>导出模板配置</span>
-          </span>
-        </template>
-        <TemplateConfigPanel />
-      </el-tab-pane>
-    </el-tabs>
+  <div class="app-container global-config-container">
+    <div class="global-section-title">
+      全局配置管理
+    </div>
+    <div class="config-setion">
+      <div class="section-title">
+        全局配置
+      </div>
+      <div>
+        <el-row :gutter="20">
+          <el-col v-for="(item, index) in globalConfigs" :span="8">
+            <router-link :to="item.link">
+              <div :key="index" class="config-item" >
+                <div class="config-icon">
+                  <i :class="item.icon" />
+                </div>
+                <div class="config-info">
+                  <span class="config-name">{{ item.configName }}</span>
+                  <span class="config-desc">{{ item.configDesc }}</span>
+                  <!-- <el-link :href="item.link" target="_blank" class="config-link">进入配置</el-link> -->
+                </div>
+              </div>
+            </router-link>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <div class="config-setion">
+      <div class="section-title">
+        场景配置
+      </div>
+      <div>
+        <el-row :gutter="20">
+          <el-col v-for="(item, index) in modelConfigs" :span="8">
+            <router-link :to="item.link">
+              <div :key="index" class="config-item">
+                <div class="config-icon">
+                  <i :class="item.icon" />
+                </div>
+                <div class="config-info">
+                  <span class="config-name">{{ item.configName }}</span>
+                  <span class="config-desc">{{ item.configDesc }}</span>
+                  <!-- <el-link :href="item.link" target="_blank" class="config-link">进入配置</el-link> -->
+                </div>
+              </div>
+            </router-link>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import GlobalConfigPanel from './globalConfig.vue';
-import GlobalSecretPanel from '@/views/smart/assistant/secret/index';
-import TemplateConfigPanel from '@/views/smart/assistant/template/index';
+<script setup name="Index">
+const globalConfigs = [
+  {
+    id: 1,
+    configName: "团队配置",
+    configDesc: "填写公司名称来明确团队所属的组织，还可以进行其他与团队标识相关的设置。",
+    icon: "fa-solid fa-users",
+    link: "/smart/assistant/config/globalConfig"
+  },
+  {
+    id: 1,
+    configName: "大模型配置",
+    configDesc: "系统可能会集成多个厂家的大模型以满足不同的业务需求" , 
+    icon: "fa-solid fa-microchip",
+    link: "/smart/assistant/llmModel/index"
+  },
+  {
+    id: 2,
+    configName: "密钥配置",
+    configDesc: "此配置项允许你设置全局密码，这些密码会被应用到系统脚本的各个相关环节。",
+    icon: "fa-solid fa-key",
+    link: "/template/smart/assistant/secret/index"
+  },
+  {
+    id: 1,
+    configName: "运行监控查看",
+    configDesc: "系统可能会集成多个厂家的大模型以满足不同的业务需求" , 
+    icon: "fa-solid fa-tv",
+    link: "/monitor/smart/assistant/analyse/index"
+  }
+];
 
-const tabPosition = ref('right');
-const activeTab = ref('globalConfig');
-
-// 从 localStorage 读取选中的 tab
-const loadSelectedTab = () => {
-  const storedTab = localStorage.getItem('selectedTab');
-  activeTab.value = storedTab || 'globalConfig';
-};
-
-// 处理 tab 点击事件，保存选中的 tab 到 localStorage
-const handleTabClick = (tab) => {
-  console.log('点击了标签：', tab);
-  console.log('activeTab.value', activeTab.value)
-  localStorage.setItem('selectedTab', activeTab.value);
-};
-
-onMounted(() => {
-  loadSelectedTab();
-});
+const modelConfigs = [
+  {
+    id: 3,
+    configName: "模板配置",
+    configDesc: "系统中存在多种类型的文件需要遵循一定的格式和规范，通过此配置项" , 
+    icon: "fa-solid fa-file",
+    link: "/template/smart/assistant/template/index"
+  },
+  {
+    id: 4,
+    configName: "文档审核清单",
+    configDesc: "此配置项允许你配置审核场景和对应的业务审核清单。",
+    icon: "fa-solid fa-list-check",
+    link: "/expert/smart/assistant/reviews/index"
+  },
+  {
+    id: 5,
+    configName: "审核规则库",
+    configDesc: "为了保证审核工作你可以在这个配置项中对审核规则进行管理" , 
+    icon: "fa-solid fa-ruler-combined",
+    link: "/expert/smart/assistant/reviews/ruleList"
+  }
+];
 </script>
 
 <style lang="scss" scoped>
-.aip-config-container {
-  padding: 10px;
+.global-config-container {
+  margin: auto;
+  width: 90%;
+  max-width: 1500px;
 
-  .tabs-container {
-    height: calc(100vh - 60px);
+  .global-section-title {
+    font-size: 22px;
+    padding-bottom: 20px;
+    color: #14141f;
   }
 
-  span.custom-tabs-label {
-    display: flex;
-    align-items: center;
-    gap: 3px;
+ .config-setion {
+    margin-bottom: 20px;
+
+   .section-title {
+      padding-top: 20px;
+      margin-bottom: 20px;
+      border-top: 1px solid rgb(244, 244, 248);
+      font-size: 15px;
+    }
+
+   .config-item {
+      display: flex;
+      padding: 15px;
+      border-radius: 8px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+      align-items: center;
+      background-color: #fafafa;
+      cursor: pointer;
+      gap: 10px;
+
+      &:hover {
+        background-color: #f5f5f5;
+      }
+
+     .config-icon {
+        font-size: 26px;
+        background: rgba(175,175,207,.225);
+        padding: 10px;
+        width: 55px;
+        height: 55px;
+        color: #3b5998;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+     .config-info {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        width: calc(100% - 60px);
+
+       .config-name {
+          font-size: 15px;
+        }
+
+       .config-desc {
+          font-size: 15px;
+          line-height: 1.2rem;
+          color: #888;
+        }
+
+       .config-link {
+          font-size: 15px;
+          color: #007bff;
+          text-decoration: none;
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
   }
 }
 </style>    
