83b6720ad58cdd677d9320df7d867fce9e218e79
张婷(Tina) <tinaAip@gmail.com>
2025-04-18 10:39:43 +0800

    feat:新增 DataAnalysisSceneController 控制器，支持数据权限注解及工具类引入。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/dataAnalysis/controller/DataAnalysisSceneController.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/dataAnalysis/controller/DataAnalysisSceneController.java
@@ -1,26 +1,37 @@
 package com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.controller;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.assistant.scene.common.utils.RoleUtils;
 import com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.dto.DataAnalysisSceneDto;
 import com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.dto.DataPromptContentRequestDto;
 import com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.service.IDataAnalysisPlanService;
 import com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.service.IDataAnalysisSceneService;
+import com.alinesno.infra.smart.assistant.scene.scene.dataAnalysis.tools.DataAnalysisFormatMessageTool;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.scene.dto.ChatRoleDto;
 import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
+import com.alinesno.infra.smart.scene.dto.TreeNodeDto;
 import com.alinesno.infra.smart.scene.entity.DataAnalysisSceneEntity;
+import com.alinesno.infra.smart.scene.entity.LongTextSceneEntity;
 import com.alinesno.infra.smart.scene.entity.SceneEntity;
 import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import com.alinesno.infra.smart.scene.service.ISceneService;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.BeanUtils;
@@ -30,6 +41,9 @@ import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.lang.exception.RpcServiceRuntimeException;
+import java.util.List;
 
 /**
  * 数据分析场景控制器
@@ -52,6 +66,9 @@ public class DataAnalysisSceneController extends BaseController<DataAnalysisScen
 
     @Autowired
     private ISceneService sceneService;
+
+    @Autowired
+    private DataAnalysisFormatMessageTool formatMessageTool;
 
     /**
      * 获取BusinessLogEntity的DataTables数据。
@@ -112,6 +129,48 @@ public class DataAnalysisSceneController extends BaseController<DataAnalysisScen
         return AjaxResult.success("操作成功.", dto);
     }
 
+
+    /**
+     * 与角色交互，获取到内容结果
+     */
+    @DataPermissionSave
+    @SneakyThrows
+    @PostMapping("/chatRole")
+    public AjaxResult chatRole(@RequestBody @Validated ChatRoleDto chatRole) {
+
+        MessageTaskInfo taskInfo = chatRole.toPowerMessageTaskInfo() ; // new MessageTaskInfo() ;
+
+        taskInfo.setRoleId(chatRole.getRoleId());
+        taskInfo.setChannelStreamId(chatRole.getChannelStreamId());
+        taskInfo.setChannelId(chatRole.getSceneId());
+        taskInfo.setSceneId(chatRole.getSceneId());
+        taskInfo.setText(chatRole.getMessage());
+
+        // 优先获取到结果内容
+        WorkflowExecutionDto genContent  = roleService.runRoleAgent(taskInfo) ;
+
+        // 获取到格式化的内容
+        formatMessageTool.handleChapterMessage(genContent , taskInfo);
+
+        // 解析得到代码内容
+        if(genContent.getCodeContent() !=null && !genContent.getCodeContent().isEmpty()){
+            String codeContent = genContent.getCodeContent().get(0).getContent() ;
+            JSONArray dataObject = JSONArray.parseArray(codeContent) ;
+
+            // 验证是否可以正常解析json
+            try{
+                List<TreeNodeDto> nodeDtos = JSON.parseArray(codeContent, TreeNodeDto.class);
+                log.debug("nodeDtos = {}", JSONUtil.toJsonPrettyStr(nodeDtos));
+            }catch (Exception e){
+                throw new RpcServiceRuntimeException("生成大纲格式不正确，请点击重新生成.") ;
+            }
+
+            return AjaxResult.success("操作成功" , dataObject) ;
+        }
+
+        return AjaxResult.success("操作成功" , genContent) ;
+    }
+
     /**
      * 更新chapterPromptContent内容
      * @return
@@ -144,7 +203,24 @@ public class DataAnalysisSceneController extends BaseController<DataAnalysisScen
         return AjaxResult.success("操作成功") ;
     }
 
+    /**
+     * 接收JSON数据并保存到数据库
+     * @param nodes JSON格式的章节列表
+     * @return 操作结果
+     */
+    @DataPermissionQuery
+    @PostMapping("/saveDataPlan")
+    public AjaxResult saveChapters(@RequestBody List<TreeNodeDto> nodes, @RequestParam("sceneId") long sceneId , PermissionQuery query) {
 
+        if(nodes == null || nodes.isEmpty()){
+            return error("参数错误") ;
+        }
+
+        DataAnalysisSceneEntity dataAnalysisSceneEntity = service.getBySceneId(sceneId , query) ;
+        dataAnalysisPlanService.saveChaptersWithHierarchy(nodes, null, 1 , sceneId , dataAnalysisSceneEntity.getId());
+
+        return ok() ;
+    }
 
     @Override
     public IDataAnalysisSceneService getFeign() {
