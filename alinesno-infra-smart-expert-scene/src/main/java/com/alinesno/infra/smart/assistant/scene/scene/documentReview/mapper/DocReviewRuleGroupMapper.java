01cedfcd1c6ff2c60207fd92f91b72c07239e182
陈伟(Victor) <victorAip@gmail.com>
2025-04-19 00:13:24 +0800

    feat:新增 DocReviewRuleGroupMapper 接口，实现文档审核规则组的数据映射。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewRuleGroupMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DocReviewRuleGroupEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DocReviewRuleGroupMapper extends IBaseMapper<DocReviewRuleGroupEntity> {
+
+}
