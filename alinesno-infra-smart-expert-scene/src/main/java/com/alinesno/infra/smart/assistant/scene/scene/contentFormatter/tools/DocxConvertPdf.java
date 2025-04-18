073405847dcc29850cb461aa296d26daad3f40c0
张婷(Tina) <tinaAip@gmail.com>
2025-04-18 10:39:43 +0800

    refactor: 注释掉 DocxConvertPdf 工具类代码以便后续逐步重构

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/tools/DocxConvertPdf.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/tools/DocxConvertPdf.java
@@ -1,23 +1,23 @@
-package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools;
-
-import com.documents4j.api.DocumentType;
-import com.documents4j.api.IConverter;
-import com.documents4j.job.LocalConverter;
-
-import java.io.*;
-
-public class DocxConvertPdf {
-
-    public static void convert(File inputWord , File outputFile){
-        try  {
-            InputStream docxInputStream = new FileInputStream(inputWord);
-            OutputStream outputStream = new FileOutputStream(outputFile);
-            IConverter converter = LocalConverter.builder().build();
-            converter.convert(docxInputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
-            outputStream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-}
+//package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools;
+//
+//import com.documents4j.api.DocumentType;
+//import com.documents4j.api.IConverter;
+//import com.documents4j.job.LocalConverter;
+//
+//import java.io.*;
+//
+//public class DocxConvertPdf {
+//
+//    public static void convert(File inputWord , File outputFile){
+//        try  {
+//            InputStream docxInputStream = new FileInputStream(inputWord);
+//            OutputStream outputStream = new FileOutputStream(outputFile);
+//            IConverter converter = LocalConverter.builder().build();
+//            converter.convert(docxInputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
+//            outputStream.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//}
