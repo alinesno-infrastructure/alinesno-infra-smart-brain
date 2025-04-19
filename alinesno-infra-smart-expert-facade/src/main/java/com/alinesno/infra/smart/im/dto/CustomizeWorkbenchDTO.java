986850c65e484b5845c76cd47ce3bbd4c4ad05c1
李伟(Michael) <michaelAip@gmail.com>
2025-04-20 00:10:47 +0800

    feat:新增 CustomizeWorkbenchDTO 类，包含工作台相关字段及校验规则。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/CustomizeWorkbenchDTO.java
@@ -0,0 +1,31 @@
+package com.alinesno.infra.smart.im.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 工作台数据传输对象
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomizeWorkbenchDTO extends BaseDto {
+
+    @NotBlank(message = "名称不能为空")
+    private String name;
+
+    @NotNull(message = "类型不能为空")
+    @Positive(message = "类型必须为正整数")
+    private Integer type;
+
+    @NotNull(message = "选择的智能体 ID 列表不能为空")
+    private List<String> selectAgentId;
+
+    @NotNull(message = "选择的场景 ID 列表不能为空")
+    private List<String> selectSceneId;
+}    
