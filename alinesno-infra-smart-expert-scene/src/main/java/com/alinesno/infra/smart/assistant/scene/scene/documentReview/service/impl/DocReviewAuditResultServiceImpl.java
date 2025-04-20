f189b390475b67d463d20f8736849f40310dd33b
周健(James) <jamesAip@gmail.com>
2025-04-21 00:12:21 +0800

    feat: 实现 DocReviewAuditResultServiceImpl，包含保存和查询审核结果的方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewAuditResultServiceImpl.java
@@ -0,0 +1,39 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.GenAuditResultDto;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewAuditResultMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewAuditResultService;
+import com.alinesno.infra.smart.scene.entity.DocReviewAuditResultEntity;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class DocReviewAuditResultServiceImpl extends IBaseServiceImpl<DocReviewAuditResultEntity, DocReviewAuditResultMapper> implements IDocReviewAuditResultService {
+
+    @Override
+    public void saveAuditResult(List<DocReviewAuditResultEntity> auditResultList, GenAuditResultDto dto) {
+        // 先删除sceneId下面的所有审核结果
+        LambdaUpdateWrapper<DocReviewAuditResultEntity> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(DocReviewAuditResultEntity::getSceneId, dto.getSceneId()) ;
+        wrapper.eq(DocReviewAuditResultEntity::getRuleId, dto.getRuleId()) ;
+        remove(wrapper) ;
+
+        saveBatch(auditResultList) ;
+    }
+
+    @Override
+    public List<DocReviewAuditResultEntity> getAuditResultByRuleId(long ruleId, long sceneId) {
+
+        LambdaQueryWrapper<DocReviewAuditResultEntity> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DocReviewAuditResultEntity::getRuleId, ruleId) ;
+        wrapper.eq(DocReviewAuditResultEntity::getSceneId, sceneId) ;
+
+        return list(wrapper) ;
+    }
+}
