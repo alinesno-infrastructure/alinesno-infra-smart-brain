85b67be3bbbb919294c61618cbb52c5c033357e5
周健(James) <jamesAip@gmail.com>
2025-04-21 00:12:21 +0800

    feat:新增 IDocReviewAuditResultService 接口，包含保存审核结果及按规则ID和场景ID查询审核结果的方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewAuditResultService.java
@@ -0,0 +1,32 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.GenAuditResultDto;
+import com.alinesno.infra.smart.scene.entity.DocReviewAuditResultEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IDocReviewAuditResultService extends IBaseService<DocReviewAuditResultEntity> {
+
+    /**
+     * 保存审核结果，会很清理当前sceneId下面的旧数据
+     *
+     * @param auditResultList
+     * @param dto
+     */
+    void saveAuditResult(List<DocReviewAuditResultEntity> auditResultList, GenAuditResultDto dto);
+
+    /**
+     * 根据规则ID和场景ID获取审核结果
+     * @param ruleId
+     * @param sceneId
+     * @return
+     */
+    List<DocReviewAuditResultEntity> getAuditResultByRuleId(long ruleId, long sceneId);
+}
