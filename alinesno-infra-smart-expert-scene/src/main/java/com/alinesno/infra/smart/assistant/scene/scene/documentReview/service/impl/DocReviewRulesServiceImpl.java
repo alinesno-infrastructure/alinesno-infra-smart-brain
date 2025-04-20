5e492d4a8daf24533f5fe2a1c763c86a7936a7f4
周健(James) <jamesAip@gmail.com>
2025-04-21 00:12:22 +0800

    fix: 修复 DocReviewRulesServiceImpl 类继承关系错误，由 IDocReviewAuditService 修改为 IDocReviewRulesService。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewRulesServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewRulesServiceImpl.java
@@ -1,86 +1,14 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewAuditDto;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRuleGroupDto;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRulesDto;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewAuditMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewRulesMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewAuditService;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewRulesService;
-import com.alinesno.infra.smart.scene.entity.DocReviewAuditEntity;
 import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-public class DocReviewRulesServiceImpl extends IBaseServiceImpl<DocReviewAuditEntity, DocReviewAuditMapper> implements IDocReviewAuditService {
+public class DocReviewRulesServiceImpl extends IBaseServiceImpl<DocReviewRulesEntity, DocReviewRulesMapper> implements IDocReviewRulesService {
 
-    @Autowired
-    private IDocReviewRulesService rulesService;
-
-    @Override
-    public DocReviewAuditDto queryAudit(Long id) {
-
-        DocReviewAuditDto auditDto = new DocReviewAuditDto();
-
-        DocReviewAuditEntity auditEntity = getBaseMapper().selectById(id);
-        BeanUtils.copyProperties(auditEntity, auditDto);
-
-        List<Long> ruleIds = getRuleIds(auditEntity) ;
-
-        if (ruleIds != null && !ruleIds.isEmpty()) {
-            List<DocReviewRulesEntity> rules = rulesService.listByIds(ruleIds);
-            if (rules != null && !rules.isEmpty()) {
-                auditDto.setRules(rules.stream()
-                        .map(r -> {
-                            DocReviewRulesDto rulesDto = new DocReviewRulesDto();
-                            BeanUtils.copyProperties(r, rulesDto);
-                            return rulesDto;
-                        }).toList()
-                );
-            }
-        }
-
-        return auditDto ;
-    }
-
-    @Override
-    public Long saveOrUpdateAudit(DocReviewAuditDto dto) {
-        DocReviewAuditEntity entity = new DocReviewAuditEntity() ;
-        BeanUtils.copyProperties(dto , entity) ;
-
-        // 处理id的问题
-        if (!dto.getAuditLists().isEmpty()) {
-            String ruleIds = dto.getAuditLists().stream()
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(","));
-            entity.setAuditList(ruleIds);
-        }
-
-        saveOrUpdate(entity);
-
-        return entity.getId();
-    }
-
-    public static List<Long> getRuleIds(DocReviewAuditEntity auditEntity) {
-        if (auditEntity == null || auditEntity.getAuditList() == null) {
-            return new ArrayList<>();
-        }
-        return Arrays.stream(auditEntity.getAuditList().split(","))
-                .map(String::trim)
-                .map(Long::parseLong)
-                .collect(Collectors.toList());
-    }
 }
