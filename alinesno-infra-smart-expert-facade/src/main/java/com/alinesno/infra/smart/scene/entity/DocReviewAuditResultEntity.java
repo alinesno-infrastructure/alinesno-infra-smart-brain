804fcc0608f548a019d18a05b7aaf594ece39f6b
周健(James) <jamesAip@gmail.com>
2025-04-21 00:12:21 +0800

    feat:新增 DocReviewAuditResultEntity 实体类，用于文档审核结果的数据库映射。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/DocReviewAuditResultEntity.java
@@ -0,0 +1,59 @@
+package com.alinesno.infra.smart.scene.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 文档审核结果实体类
+ */
+@EqualsAndHashCode(callSuper = true)
+@TableName("document_review_audit_result")
+@Data
+public class DocReviewAuditResultEntity extends InfraBaseEntity {
+
+    @TableField
+    @Column(name = "audit_id", type = MySqlTypeConstant.BIGINT , length = 32, comment = "审核清单ID")
+    private Long auditId;
+
+    @TableField
+    @Column(name = "rule_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "规则ID")
+    private Long ruleId;
+
+    @TableField
+    @Column(name = "scene_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "场景ID")
+    private Long sceneId;
+
+    @TableField
+    @Column(name = "audit_result", type = MySqlTypeConstant.VARCHAR, length = 255, comment = "审核结果")
+    private String auditResult;
+
+    @TableField
+    @Column(name = "audit_opinion", type = MySqlTypeConstant.VARCHAR, length = 255, comment = "审核意见")
+    private String auditOpinion;
+
+    @TableField
+    @Column(name = "audit_status", type = MySqlTypeConstant.INT, length = 2, comment = "审核状态")
+    private int auditStatus;
+
+    @TableField
+    @Column(name = "modification_reason", type = MySqlTypeConstant.VARCHAR, length = 512, comment = "修改原因")
+    private String modificationReason;
+
+    @TableField
+    @Column(name = "original_content", type = MySqlTypeConstant.VARCHAR, length = 512, comment = "原内容")
+    private String originalContent;
+
+    @TableField
+    @Column(name = "risk_level", type = MySqlTypeConstant.VARCHAR, length = 10, comment = "风险等级")
+    private String riskLevel;
+
+    @TableField
+    @Column(name = "suggested_content", type = MySqlTypeConstant.VARCHAR, length = 512, comment = "建议修改的内容")
+    private String suggestedContent;
+
+}
