91188951a83f935d8624cfe89a38787188ef4772
张婷(Tina) <tinaAip@gmail.com>
2025-04-21 00:12:22 +0800

    feat: 实现文档审核页面发起审核功能，支持根据不同场景生成审计结果并优化按钮交互逻辑。

--- alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/reviewChecklist.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/reviewChecklist.vue
@@ -19,8 +19,7 @@
                 </div>
             </el-scrollbar>
             <div class="button-section">
-                <!-- <el-button type="primary" text bg size="large">自定义审核规则</el-button> -->
-                <el-button type="primary" style="width:100%" size="large">
+                <el-button type="primary" style="width:100%" size="large" @click="submitAuditForm" >
                     <i class="fa-solid fa-rocket"></i> 发起文档审核
                 </el-button>
             </div>
@@ -29,30 +28,87 @@
 </template>
 
 <script setup>
+import { getScene, genAuditResult } from '@/api/base/im/scene/documentReview';
 
-import { getScene } from '@/api/base/im/scene/documentReview';
+import { useRoute } from 'vue-router';
+import { ElLoading, ElMessage } from 'element-plus';
+import { ref, onMounted } from 'vue';
 
+const emit = defineEmits(['handleStepClick' , 'genSingleChapterContent'])
+
+const streamLoading = ref(null);
 const route = useRoute();
+
 const currentSceneInfo = ref(null);
-const sceneId = ref(route.query.sceneId)
+const sceneId = ref(route.query.sceneId);
+const channelStreamId = ref(route.query.channelStreamId);
+const auditId = ref(null)
 
 const contractReviewList = ref([]);
-
 const checkList = ref([]);
 
 const handleGetScene = () => {
     getScene(sceneId.value).then(res => {
         currentSceneInfo.value = res.data;
-        contractReviewList.value = res.data.reviewListDtos ;
+        contractReviewList.value = res.data.reviewListDtos;
+
+        if(currentSceneInfo.value.reviewListOption === 'aigen'){
+            auditId.value = 0 ; 
+        }else if(currentSceneInfo.value.reviewListOption === 'dataset'){
+            auditId.value = currentSceneInfo.value.auditId ;
+        }
 
         // 将所有选项的 ruleName 赋值给 checkList 实现全选
         checkList.value = contractReviewList.value.map(item => item.id);
-    })
-}
+    });
+};
+
+const submitAuditForm = async () => {
+    // 获取所有选择的id
+    const selectedIds = checkList.value;
+    const totalCount = selectedIds.length;
+
+    // 开始生成
+    streamLoading.value = ElLoading.service({
+        lock: true,
+        background: 'rgba(255, 255, 255, 0.4)',
+        customClass: 'custom-loading'
+    });
+
+    emit('genSingleChapterContent', currentSceneInfo.value.logicReviewerEngineer);
+
+    // 遍历选择的id列表，依次调用genAuditResult方法
+    for (let index = 0; index < selectedIds.length; index++) {
+        const id = selectedIds[index];
+        const ruleName = contractReviewList.value.find(item => item.id === id).ruleName;
+        console.log('ruleName = ' + ruleName);
+
+        const currentCount = index + 1;
+        const remainingCount = totalCount - currentCount;
+        const text = `正在进行[${ruleName}]检查，当前是第${currentCount}条，还剩${remainingCount}条`;
+        streamLoading.value.setText(text);
+
+        const data = {
+            sceneId: sceneId.value,
+            channelStreamId: channelStreamId.value,
+            ruleId: id,
+            auditId: auditId.value,
+            roleId: currentSceneInfo.value.logicReviewerEngineer
+        };
+
+        const res = await genAuditResult(data);
+        console.log('res = ' + res);
+    }
+
+    streamLoading.value.close();
+
+    emit('handleStepClick', 3);
+};
 
 onMounted(() => {
     handleGetScene();
-})
+});
+
 </script>
 
 <style lang="scss" scoped>
