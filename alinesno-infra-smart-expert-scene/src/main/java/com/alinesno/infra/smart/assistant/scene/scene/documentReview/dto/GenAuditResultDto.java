9eb730061fd59c25bc98c974c688bee077ba9214
周健(James) <jamesAip@gmail.com>
2025-04-21 00:12:21 +0800

    feat:新增 GenAuditResultDto 类，用于封装审核结果相关字段及校验规则。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/GenAuditResultDto.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 进行审核
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class GenAuditResultDto extends BaseDto {
+
+    @NotNull(message = "场景ID不能为空")
+    private Long sceneId ;
+
+    @NotNull(message = "渠道流ID不能为空")
+    private Long channelStreamId ;
+
+    @NotNull(message = "规则ID不能为空")
+    private Long ruleId ;
+
+    @NotNull(message = "审核清单ID不能为空")
+    private Long auditId ;
+
+    @NotNull(message = "角色ID不能为空")
+    private Long roleId;
+}
