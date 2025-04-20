61aea985fc010caf9565bb86bb3655f9c8c892a7
林晓(Lily) <lilyAip@gmail.com>
2025-04-21 00:12:22 +0800

    feat:新增文档审核相关接口 genReviewListByDataset 和 getAuditList

--- alinesno-infra-smart-im-ui/src/api/base/im/scene/documentReviewSceneInfo.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/documentReviewSceneInfo.js
@@ -6,7 +6,26 @@ var prefix = '/api/infra/smart/assistant/scene/documentReview/sceneInfo/' ;
 
 var managerUrl = {
   initAgents: prefix +"initAgents",
-  genReviewList: prefix +"genReviewList"
+  genReviewList: prefix +"genReviewList" , 
+  getAuditList: prefix +"getAuditList" ,
+  genReviewListByDataset: prefix +"genReviewListByDataset" ,
+}
+
+// 生成文档审核场景信息
+export function genReviewListByDataset(data) {
+  return request({
+    url: managerUrl.genReviewListByDataset , 
+    method: 'post' , 
+    data: data
+  })
+}
+
+// 获取审核清单
+export function getAuditList() {
+  return request({
+    url: managerUrl.getAuditList , 
+    method: 'get' 
+  })
 }
 
 // 生成文档审核场景信息
