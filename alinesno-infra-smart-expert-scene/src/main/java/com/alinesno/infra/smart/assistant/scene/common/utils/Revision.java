4e60a22a64cb542abd1c89a8e608281de6affbde
李伟(Michael) <michaelAip@gmail.com>
2025-04-22 00:13:59 +0800

    feat:新增 Revision 类，用于表示文本修订建议，包含原始文本、建议文本、原因及作者等属性。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/Revision.java
@@ -0,0 +1,35 @@
+package com.alinesno.infra.smart.assistant.scene.common.utils;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 修订类，用于表示文本修订建议
+ */
+@NoArgsConstructor
+@Data
+public class Revision {
+
+    // 修改的原因
+    private String reason;
+    // 原始文本
+    String originalText;
+    // 建议的文本列表
+    String suggestedTexts;
+    // 修订的作者
+    String author;
+
+    /**
+     * 构造函数，用于创建修订对象
+     *
+     * @param originalText 原始文本
+     * @param suggestedTexts 建议的文本列表
+     */
+    public Revision(String originalText, String suggestedTexts, String reason) {
+        this.originalText = originalText;
+        this.suggestedTexts = suggestedTexts;
+        this.reason = reason;
+    }
+
+}
