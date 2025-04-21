b3554be1a310615f9f3797d6bef29573809e6339
李伟(Michael) <michaelAip@gmail.com>
2025-04-22 00:13:59 +0800

    feat: 引入 DocReviewSceneServiceImpl 相关依赖并添加 GenPdfTool 工具类支持

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewSceneServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/impl/DocReviewSceneServiceImpl.java
@@ -4,12 +4,15 @@ import com.alibaba.fastjson.JSONArray;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.scene.common.utils.GenPdfTool;
 import com.alinesno.infra.smart.assistant.scene.common.utils.RoleUtils;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.controller.ReviewListOptionEnum;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewInitDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRulesDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewSceneInfoDto;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper.DocReviewSceneMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewAuditResultService;
+import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewAuditService;
 import com.alinesno.infra.smart.assistant.scene.scene.documentReview.service.IDocReviewSceneService;
 import com.alinesno.infra.smart.assistant.scene.scene.longtext.dto.LongTextSceneDto;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
@@ -17,17 +20,22 @@ import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
 import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
 import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.DocReviewAuditResultEntity;
+import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
 import com.alinesno.infra.smart.scene.entity.DocReviewSceneEntity;
 import com.alinesno.infra.smart.scene.entity.SceneEntity;
 import com.alinesno.infra.smart.scene.enums.ContractTypeEnum;
 import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tools.ant.util.DateUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -37,6 +45,9 @@ public class DocReviewSceneServiceImpl extends IBaseServiceImpl<DocReviewSceneEn
 
     @Autowired
     private IIndustryRoleService roleService ;
+
+    @Autowired
+    private IDocReviewAuditService docReviewAuditService ;
 
     @Autowired
     private IDocReviewAuditResultService docReviewAuditResultService ;
@@ -194,6 +205,42 @@ public class DocReviewSceneServiceImpl extends IBaseServiceImpl<DocReviewSceneEn
         }
 
         return count ;
+    }
+
+    @Override
+    public String genMarkdownReport(long sceneId, PermissionQuery query, Long docReviewSceneId) {
+
+        DocReviewSceneEntity docReviewSceneEntity = getById(docReviewSceneId) ;
+
+        // 获取到所有的审核结果
+        List<DocReviewAuditResultEntity> auditResultList = docReviewAuditResultService.list(
+                new LambdaQueryWrapper<DocReviewAuditResultEntity>()
+                        .eq(DocReviewAuditResultEntity::getOrgId, query.getOrgId())
+                        .eq(DocReviewAuditResultEntity::getSceneId, sceneId)
+                        .eq(DocReviewAuditResultEntity::getDocReviewSceneId, docReviewSceneId)
+        );
+
+        // 审核markdown封面信息
+        String documentName = docReviewSceneEntity.getDocumentName() ;  // 审核文件名称
+        String auditOption = docReviewSceneEntity.getReviewListOption() ;  // 审核方式
+        String dateTime = DateUtils.format(docReviewSceneEntity.getAddTime(), DateUtils.ISO8601_DATETIME_PATTERN) ;  // 审核时间
+
+        // 规则清单
+        List<DocReviewRulesEntity> rules;
+        if(docReviewSceneEntity.getReviewListOption().equals(ReviewListOptionEnum.AIGEN.getValue())){  // 使用AI生成的审核清单
+            // 解析获取得到审核内容
+            rules = JSONArray.parseArray(docReviewSceneEntity.getReviewList(), DocReviewRulesEntity.class) ;
+        }else if(docReviewSceneEntity.getReviewListOption().equals(ReviewListOptionEnum.DATASET.getValue())) {  // 使用自定义的审核清单
+            // 以逗号分隔，获取规则 ID 列表
+            rules = docReviewAuditService.getRulesByAuditId(docReviewSceneEntity.getAuditId()) ;
+        } else {
+            rules = null;
+        }
+
+        // 整理在markdown格式
+        StringBuilder markdown = GenPdfTool.getGenDocReviewMarkdownReport(documentName, auditOption, dateTime, rules, auditResultList);
+
+        return markdown.toString();
     }
 
 
