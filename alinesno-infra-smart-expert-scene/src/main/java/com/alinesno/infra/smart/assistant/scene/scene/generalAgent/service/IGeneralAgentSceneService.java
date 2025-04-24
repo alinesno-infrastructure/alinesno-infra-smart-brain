933d9f31157cf1705944b62f21c8df7a101a1683
王磊(Levi) <leviAip@gmail.com>
2025-04-25 00:18:17 +0800

    feat:新增 IGeneralAgentSceneService 接口，包含场景代理相关操作方法定义。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/service/IGeneralAgentSceneService.java
@@ -0,0 +1,55 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.generalAgent.dto.GeneralAgentInitDto;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentSceneEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IGeneralAgentSceneService extends IBaseService<GeneralAgentSceneEntity> {
+
+    /**
+     * 根据场景ID获取场景信息
+     * @param sceneId
+     * @return
+     */
+    GeneralAgentSceneEntity getBySceneId(long sceneId , PermissionQuery query);
+
+    /**
+     * 初始化场景代理
+     * @param dto
+     */
+    void initAgents(GeneralAgentInitDto dto);
+
+    /**
+     * 更新场景代理
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取场景角色列表
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
+
+    /**
+     * 生成Markdown内容
+     * @param sceneId
+     * @param query
+     * @param generalAgentSceneId
+     * @return
+     */
+    String genMarkdownContent(long sceneId, PermissionQuery query, Long generalAgentSceneId);
+}
