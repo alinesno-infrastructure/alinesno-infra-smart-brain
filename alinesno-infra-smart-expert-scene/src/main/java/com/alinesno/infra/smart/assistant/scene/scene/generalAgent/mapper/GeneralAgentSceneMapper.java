8d1ce2806fa0c5d5081276a922b4ed6835bf05d1
王磊(Levi) <leviAip@gmail.com>
2025-04-25 00:18:17 +0800

    feat:新增 GeneralAgentSceneMapper 接口，继承 IBaseMapper 完成基础映射定义。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/mapper/GeneralAgentSceneMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface GeneralAgentSceneMapper extends IBaseMapper<GeneralAgentSceneEntity> {
+
+}
