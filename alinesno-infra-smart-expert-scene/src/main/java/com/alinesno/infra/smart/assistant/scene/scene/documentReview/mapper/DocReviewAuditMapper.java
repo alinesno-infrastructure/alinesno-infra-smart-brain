dfa971b4fb912f2048da6e76578563068b103488
王磊(Levi) <leviAip@gmail.com>
2025-04-25 00:18:17 +0800

    refactor: 移除未使用的 DocReviewRulesEntity 引用，优化代码依赖

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewAuditMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewAuditMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.scene.entity.DocReviewAuditEntity;
-import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
