538566fc23bf1d4481f7b2f4386c6895cd56ce9f
王磊(Levi) <leviAip@gmail.com>
2025-04-25 00:18:17 +0800

    refactor: 精简 IDocReviewRulesService 接口导入的依赖包

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewRulesService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/service/IDocReviewRulesService.java
@@ -1,11 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.service;
 
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto.DocReviewRuleGroupDto;
 import com.alinesno.infra.smart.scene.entity.DocReviewRulesEntity;
-
-import java.util.List;
 
 /**
  *
