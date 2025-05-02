374bbf6dddc768f7fb14894a62e972a26a27be52
张婷(Tina) <tinaAip@gmail.com>
2025-05-03 00:14:09 +0800

    feat:优化 WordParser 类，支持解析 docx 文件并移除对旧有 doc 格式的直接处理逻辑。

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/utils/parse/WordParser.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/utils/parse/WordParser.java
@@ -1,18 +1,11 @@
 package com.alinesno.infra.base.search.utils.parse;
 
+import com.alinesno.infra.base.search.service.reader.BaseReaderServiceImpl;
 import com.alinesno.infra.base.search.utils.TextParser;
+import com.spire.doc.Document;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.hwpf.extractor.WordExtractor;
-import org.apache.poi.ooxml.POIXMLDocument;
-import org.apache.poi.ooxml.extractor.POIXMLTextExtractor;
-import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,37 +15,59 @@ import java.util.List;
 @Slf4j
 public class WordParser extends TextParser {
 
-    //读取word文档中，doc后缀的文件
-    public static List<String> searchWordDoc(String filePath){
-        List<String> docList = new ArrayList<String>();
-        String content=null;
-        //读取字节流，读取文件路径
-        InputStream input = null;
+    /**
+     * 读取word文档中，docx后缀的文件
+     * @param filePath
+     * @return
+     */
+    public static List<String> searchWord(String filePath) {
+        List<String> docList = new ArrayList<>();
+        File tempFile = new File(filePath);
         try {
-            input = new FileInputStream(new File(filePath));
-            WordExtractor wex = new WordExtractor(input);
-            content = wex.getText();
-            docList.add(content);
+            Document doc = new Document();
+            doc.loadFromFile(tempFile.toString());
+            String content = doc.getText();
+
+            docList.add(BaseReaderServiceImpl.cleanText(content));
         } catch (Exception e) {
-            log.error("解析异常:{}" , e.getMessage());
+            log.error("解析异常:{}", e.getMessage());
         }
         return docList;
     }
 
+    //读取word文档中，doc后缀的文件
+    public static List<String> searchWordDoc(String filePath){
+//        List<String> docList = new ArrayList<String>();
+//        String content=null;
+//        //读取字节流，读取文件路径
+//        InputStream input = null;
+//        try {
+//            input = new FileInputStream(new File(filePath));
+//            WordExtractor wex = new WordExtractor(input);
+//            content = wex.getText();
+//            docList.add(content);
+//        } catch (Exception e) {
+//            log.error("解析异常:{}" , e.getMessage());
+//        }
+//        return docList;
+        return searchWord(filePath);
+    }
+
     public static List<String> searchWordDocX(String filePath){
-        //读取文件路径
-        OPCPackage opcPackage = null;
-        String content = null;
-        List<String> docxList = new ArrayList<String>();
-        try {
-            opcPackage = POIXMLDocument.openPackage(filePath);
-            XWPFDocument xwpf = new XWPFDocument(opcPackage);
-            POIXMLTextExtractor poiText = new XWPFWordExtractor(xwpf);
-            content = poiText.getText();
-            docxList.add(content);
-        } catch (IOException e) {
-            log.error("解析异常:{}" , e.getMessage());
-        }
-        return docxList;
+//        //读取文件路径
+//        OPCPackage opcPackage = null;
+//        String content = null;
+//        List<String> docxList = new ArrayList<String>();
+//        try {
+//            opcPackage = POIXMLDocument.openPackage(filePath);
+//            XWPFDocument xwpf = new XWPFDocument(opcPackage);
+//            POIXMLTextExtractor poiText = new XWPFWordExtractor(xwpf);
+//            content = poiText.getText();
+//            docxList.add(content);
+//        } catch (IOException e) {
+//            log.error("解析异常:{}" , e.getMessage());
+//        }
+//        return docxList;
+        return searchWord(filePath);
     }
 }
