ae9f9e68e9ca000217a40f7ae63f5bc121361212
张婷(Tina) <tinaAip@gmail.com>
2025-05-03 00:14:09 +0800

    feat:新增 AIPMcpTool 抽象类，作为连接AIP的工具库基类。

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/tool/AIPMcpTool.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.plugin.tool;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 连接AIP的工具库信息
+ */
+@Slf4j
+@EqualsAndHashCode(callSuper = true)
+@Data
+public abstract class AIPMcpTool extends Tool {
+
+}
