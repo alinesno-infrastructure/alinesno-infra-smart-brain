e9f1341575126c064799cf629d361e15f5f19a9a
林晓(Lily) <lilyAip@gmail.com>
2025-05-03 00:14:09 +0800

    feat:新增 EchartsTool 抽象类，作为图表显示工具的基础类。

---
+++ alinesno-infra-smart-expert-tool/src/main/java/com/alinesno/infra/smart/assistant/plugin/tool/EchartsTool.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.plugin.tool;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Echarts图片显示工具
+ */
+@Slf4j
+@EqualsAndHashCode(callSuper = true)
+@Data
+public abstract class EchartsTool extends Tool {
+
+}
