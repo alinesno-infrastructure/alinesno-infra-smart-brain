f44d6b033feba3a99d1a72c37880d9dc8ba75f81
张婷(Tina) <tinaAip@gmail.com>
2025-05-03 00:14:09 +0800

    feat:新增 IDatasetParseLogService 接口，定义数据集解析日志服务的基础操作。

---
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/IDatasetParseLogService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.base.search.service;
+
+import com.alinesno.infra.base.search.entity.DatasetParseLogEntity;
+import com.alinesno.infra.common.facade.services.IBaseService;
+
+public interface IDatasetParseLogService extends IBaseService<DatasetParseLogEntity> {
+}    
