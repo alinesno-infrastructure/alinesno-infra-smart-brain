783ca185f16afc61d8b59921bcaf2f649e6f26cc
周健(James) <jamesAip@gmail.com>
2025-05-06 00:17:30 +0800

    feat:新增 TestDatasetWebCrawler 测试类，用于模拟网页数据抓取功能。

---
+++ alinesno-infra-smart-expert-dataset/src/test/java/com/alinesno/infra/base/search/crew/TestDatasetWebCrawler.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.base.search.crew;
+
+import com.alinesno.infra.base.search.api.CrawlerDto;
+
+import java.util.List;
+
+public class TestDatasetWebCrawler {
+
+    public static void main(String[] args) {
+
+        CrawlerDto dto = new CrawlerDto();
+
+        String cssSelector = null ;
+
+        dto.setChannelStreamId("123");
+        dto.setAllowSpread(false);
+        dto.setMaxPage(10000);
+        dto.setBaseUrl("https://gjj.nanning.gov.cn");
+        dto.setCssSelector(cssSelector);
+
+        List<WebContentInfo> result = new DatasetWebCrawler().parseWebsite(dto.getBaseUrl(), dto.getCssSelector(), dto  , null);
+        for (WebContentInfo webContentInfo : result) {
+            System.out.println(webContentInfo.getTitle());
+            System.out.println(webContentInfo.getContent());
+            System.out.println("----------------------------------------");
+        }
+    }
+
+}
