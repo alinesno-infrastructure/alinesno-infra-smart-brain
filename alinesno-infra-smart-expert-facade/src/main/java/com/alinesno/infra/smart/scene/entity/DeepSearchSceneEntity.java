7754061bd276e8d320625f5d1d66d61fab5e4edb
李娜(Allen) <allenAip@gmail.com>
2025-05-09 00:16:29 +0800

    feat:新增 DeepSearchSceneEntity 实体类，包含场景ID、搜索规划者ID、执行规划者ID及生成状态等字段。

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/DeepSearchSceneEntity.java
@@ -0,0 +1,36 @@
+package com.alinesno.infra.smart.scene.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.alinesno.infra.smart.assistant.api.IndustryRoleDto;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+/**
+ * 深度搜索场景实体类
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DeepSearchSceneEntity extends InfraBaseEntity {
+
+    @TableField
+    @Column(name = "scene_id", type = MySqlTypeConstant.BIGINT, length = 32, comment = "场景ID")
+    private long sceneId;
+
+    @TableField
+    @Column(name = "search_planner_engineer", type = MySqlTypeConstant.BIGINT, length = 32, comment = "搜索规划者ID")
+    private Long searchPlannerEngineer ;
+
+    @TableField
+    @Column(name = "search_executor_engineer", type = MySqlTypeConstant.BIGINT, length = 32, comment = "执行规划者ID")
+    private Long searchExecutorEngineer; ;
+
+    @TableField("gen_status")
+    @Column(type = MySqlTypeConstant.INT, length = 1, comment = "生成状态(1已生成|0未生成)")
+    private Integer genStatus = 0 ;
+
+}
