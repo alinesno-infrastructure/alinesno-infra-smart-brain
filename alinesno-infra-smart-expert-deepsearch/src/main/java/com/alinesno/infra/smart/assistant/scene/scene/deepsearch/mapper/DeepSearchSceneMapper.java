ed260c598d36ea375a3d95d7baaebb7994e5a439
李娜(Allen) <allenAip@gmail.com>
2025-05-09 00:16:30 +0800

    feat:新增 DeepSearchSceneMapper 接口，用于数据分析规划相关操作。

---
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/mapper/DeepSearchSceneMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DeepSearchSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 数据分析规划 Mapper 接口
+ */
+@Mapper
+public interface DeepSearchSceneMapper extends IBaseMapper<DeepSearchSceneEntity> {
+}    
