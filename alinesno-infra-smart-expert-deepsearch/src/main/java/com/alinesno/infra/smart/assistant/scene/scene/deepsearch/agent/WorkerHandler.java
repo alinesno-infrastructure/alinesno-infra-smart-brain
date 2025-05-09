86fae4f87cbef9d9ef5125d15c81c350a6e3a122
李伟(Michael) <michaelAip@gmail.com>
2025-05-10 00:14:04 +0800

    feat:新增 WorkerHandler 中工具执行逻辑，完善 StepAction 状态与事件处理

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/agent/WorkerHandler.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/agent/WorkerHandler.java
@@ -1,6 +1,5 @@
 package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.agent;
 
-import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.util.IdUtil;
 import com.agentsflex.core.llm.ChatContext;
 import com.agentsflex.core.llm.Llm;
@@ -20,6 +19,7 @@ import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.prompt.WorkerPr
 import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.utils.FreemarkerUtil;
 import com.alinesno.infra.smart.deepsearch.dto.DeepSearchFlow;
 import com.alinesno.infra.smart.deepsearch.enums.StepActionEnums;
+import com.alinesno.infra.smart.deepsearch.enums.StepActionStatusEnums;
 import com.alinesno.infra.smart.utils.CodeBlockParser;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -62,8 +62,6 @@ public class WorkerHandler extends BaseHandler {
      */
     public String executeTask(DeepTaskBean task) {
 
-        maxLoopCount = 1 ;
-
         StringBuilder output = new StringBuilder();
 
         String answer = "";
@@ -81,14 +79,20 @@ public class WorkerHandler extends BaseHandler {
 
             WorkerResponseJson reactResponse = JSON.parseObject(codeContent.getContent(), WorkerResponseJson.class);
 
-//            DeepSearchFlow.Step toolStep = new DeepSearchFlow.Step() ;
-
             if (reactResponse.getTools() != null && !reactResponse.getTools().isEmpty()) {
                 for (WorkerResponseJson.Tool tool : reactResponse.getTools()) {
                     log.debug("正在执行工具名称：{}" , tool.getName());
 
                     DeepSearchFlow.StepAction stepActionDto = new DeepSearchFlow.StepAction();
                     stepActionDto.setActionId(IdUtil.getSnowflakeNextIdStr());
+
+                    stepActionDto.setActionType(StepActionEnums.TOOL.getActionType());
+                    stepActionDto.setThink(reactResponse.getThought()) ;
+                    stepActionDto.setStatus(StepActionStatusEnums.DOING.getKey());
+                    step.addAction(stepActionDto);
+                    getDeepSearchFlow().getSteps().removeIf(s -> s.getId().equals(step.getId())); // 更新step信息，通过id删除掉原来的step
+                    getDeepSearchFlow().getSteps().add(step);
+                    getStepEventUtil().eventStepMessage(getDeepSearchFlow(), getRole() , getTaskInfo());
 
                     String toolFullName = tool.getName() ;
                     stepActionDto.setActionType(StepActionEnums.TOOL.getActionType());
@@ -101,6 +105,7 @@ public class WorkerHandler extends BaseHandler {
                     getDeepSearchFlow().getSteps().add(step);
 
                     getStepEventUtil().eventStepMessage(getDeepSearchFlow(), getRole() , getTaskInfo());
+                    log.debug("正在执行工具名称：{}" , toolFullName);
 
                     ToolEntity toolEntity = getToolService().getToolScript(toolFullName , getRole().getSelectionToolsData()) ;
 
@@ -114,6 +119,7 @@ public class WorkerHandler extends BaseHandler {
                         toolOutput.append(String.format("当前执行次数[%s],使用工具[%s] \r\n执行结果:%s" , loopCount , toolFullName , executeToolOutput));
 
                         // 更新step信息，通过id删除掉原来的step
+                        stepActionDto.setStatus(StepActionStatusEnums.DONE.getKey());
                         stepActionDto.setResult(executeToolOutput + "");
                         getDeepSearchFlow().getSteps().removeIf(s -> s.getId().equals(step.getId()));
                         getDeepSearchFlow().getSteps().add(step);
@@ -127,13 +133,13 @@ public class WorkerHandler extends BaseHandler {
 
                     } catch (Exception e) {
                         log.error("执行工具异常：{}" , e.getMessage());
-
                         // 更新step信息，通过id删除掉原来的step
-//                        stepActionDto.setResult("执行工具异常:" + e.getMessage());
-//                        getDeepSearchFlow().getSteps().removeIf(s -> s.getId().equals(step.getId()));
-//                        getDeepSearchFlow().getSteps().add(step);
-//
-//                        getStepEventUtil().eventStepMessage(getDeepSearchFlow(), getRole() , getTaskInfo());
+                        stepActionDto.setStatus(StepActionStatusEnums.FAIL.getKey());
+                        stepActionDto.setResult(e.getMessage());
+                        getDeepSearchFlow().getSteps().removeIf(s -> s.getId().equals(step.getId()));
+                        getDeepSearchFlow().getSteps().add(step);
+
+                        getStepEventUtil().eventStepMessage(getDeepSearchFlow(), getRole() , getTaskInfo());
                     }
 
                 }
@@ -182,6 +188,7 @@ public class WorkerHandler extends BaseHandler {
                 stepActionDto.setActionType(StepActionEnums.ANALYSIS.getActionType());
                 stepActionDto.setResult(StringUtils.hasLength(message.getContent())?message.getContent():"");
                 stepActionDto.setThink(StringUtils.hasLength(message.getReasoningContent())?message.getReasoningContent():"");
+                stepActionDto.setStatus(StepActionStatusEnums.DOING.getKey()) ;
 
                 try {
                     boolean isEnd = false;
@@ -192,15 +199,16 @@ public class WorkerHandler extends BaseHandler {
 
                     step.addAction(stepActionDto);
 
+                    if (isEnd) {
+                        stepActionDto.setStatus(StepActionStatusEnums.DONE.getKey()) ;
+                        future.complete(outputStr.get());
+                    }
+
                     // 更新step信息，通过id删除掉原来的step
                     getDeepSearchFlow().getSteps().removeIf(s -> s.getId().equals(step.getId()));
                     getDeepSearchFlow().getSteps().add(step);
 
                     getStepEventUtil().eventStepMessage(getDeepSearchFlow(), getRole() , getTaskInfo());
-
-                    if (isEnd) {
-                        future.complete(outputStr.get());
-                    }
                 } catch (Exception e) {
                     // 处理发布事件时的异常
                     log.error(e.getMessage());
@@ -236,7 +244,7 @@ public class WorkerHandler extends BaseHandler {
     @NotNull
     private Map<String, Object> getStringObjectMap(DeepTaskBean task, StringBuilder toolOutput) {
         String envInfo = toolOutput.toString();
-        String planningDetail = getTaskStepMapStr();
+        String planningDetail = getPlanningDetail() ;
         String taskId = task.getId();
         String taskDescription = task.getTaskDesc();
 
@@ -245,29 +253,24 @@ public class WorkerHandler extends BaseHandler {
         params.put("planning_detail", planningDetail);
         params.put("task_id", taskId);
         params.put("task_description", taskDescription);
-        params.put("tool_info", String.join(",", Prompt.parsePlugins(getTools(), false)));
+        params.put("tool_info", getTools() != null? "没有工具可用" : String.join(",", Prompt.parsePlugins(getTools(), false)));
         params.put("current_time", Prompt.getCurrentTime());
         return params;
     }
 
-    /**
-     * 获取任务步骤处理成字符串形式
-     * @return
-     */
-    private String getTaskStepMapStr() {
-        if(CollectionUtil.isNotEmpty(getTaskStepMap())){
-            StringBuilder sb = new StringBuilder();
-            for(DeepTaskBean task : getTaskStepMap()){
+    private String getPlanningDetail() {
 
-                sb.append("顺序:").append(task.getOrder())
-                        .append(",").append("任务ID:").append(task.getId())
-                        .append(",").append("任务名称:").append(task.getTaskName())
-                        .append(",").append("任务描述:").append(task.getTaskDesc())
-                        .append("\r\n");
+        // 返回所有的任务规划列表
+        StringBuilder planningDetail = new StringBuilder();
 
-            }
-            return sb.toString();
+        for (DeepTaskBean task : taskStepMap) {
+            planningDetail.append("任务顺序:").append(task.getOrder()).append("\n")
+                    .append("任务ID:").append(task.getId()).append("\n")
+                    .append("任务名称:").append(task.getTaskName()).append("\n")
+                    .append("任务描述:").append(task.getTaskDesc()).append("\n")
+                    .append("\n");
         }
-        return null;
+
+        return planningDetail.toString();
     }
 }
