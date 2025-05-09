78a75081454e5f9a9c17c0c3dcfd25ebe2494bcf
李伟(Michael) <michaelAip@gmail.com>
2025-05-10 00:14:04 +0800

    feat:新增 DeepSearchController 的 updateChapterPromptContent 方法，支持更新章节提示内容并添加任务服务依赖。

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/controller/DeepSearchController.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/controller/DeepSearchController.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
@@ -8,15 +9,20 @@ import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import com.alinesno.infra.smart.assistant.adapter.service.CloudStorageConsumer;
 import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.IDeepSearchSceneService;
+import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.IDeepSearchTaskService;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
 import com.alinesno.infra.smart.deepsearch.dto.DeepSearchSceneDto;
+import com.alinesno.infra.smart.deepsearch.dto.DeepSearchSceneRequestDto;
 import com.alinesno.infra.smart.scene.dto.SceneDto;
 import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
 import com.alinesno.infra.smart.scene.entity.DeepSearchSceneEntity;
+import com.alinesno.infra.smart.scene.entity.DeepSearchTaskEntity;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentSceneEntity;
 import com.alinesno.infra.smart.scene.entity.SceneEntity;
 import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import com.alinesno.infra.smart.scene.service.ISceneService;
 import com.alinesno.infra.smart.utils.RoleUtils;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.BeanUtils;
@@ -50,6 +56,9 @@ public class DeepSearchController extends BaseController<DeepSearchSceneEntity, 
     @Autowired
     private CloudStorageConsumer storageConsumer ;
 
+    @Autowired
+    private IDeepSearchTaskService taskService ;
+
     /**
      * 保存场景
      */
@@ -66,6 +75,30 @@ public class DeepSearchController extends BaseController<DeepSearchSceneEntity, 
         return AjaxResult.success("操作成功.", sceneEntity.getId());
     }
 
+    @DataPermissionQuery
+    @PostMapping("/updateChapterPromptContent")
+    public AjaxResult updateChapterPromptContent(@RequestBody @Validated DeepSearchSceneRequestDto dto , PermissionQuery query) {
+
+        DeepSearchSceneEntity sceneEntity = service.getBySceneId(dto.getSceneId() , query) ;
+        sceneEntity.setPromptContent(dto.getPromptContent());
+
+        service.updateById(sceneEntity);
+
+        // 增加1个任务
+        DeepSearchTaskEntity taskEntity = new DeepSearchTaskEntity();
+
+        BeanUtil.copyProperties(query , taskEntity);
+        taskEntity.setSceneId(dto.getSceneId());
+        taskEntity.setDeepsearchSceneId(sceneEntity.getId());
+        taskEntity.setGenStatus(0);
+        taskEntity.setTaskName(dto.getPromptContent());
+        taskEntity.setPromptContent(dto.getPromptContent());
+        taskEntity.setSearchPlannerEngineer(sceneEntity.getSearchPlannerEngineer());
+
+        taskService.save(taskEntity);
+
+        return AjaxResult.success("操作成功" , taskEntity.getId()) ;
+    }
 
     /**
      * 通过Id获取到场景
@@ -93,13 +126,30 @@ public class DeepSearchController extends BaseController<DeepSearchSceneEntity, 
         if(deepSearchSceneEntity != null){
 
             dto.setSearchPlannerEngineer(deepSearchSceneEntity.getSearchPlannerEngineer());
-            dto.setSearchExecutorEngineer(deepSearchSceneEntity.getSearchExecutorEngineer());
+//            dto.setSearchExecutorEngineer(deepSearchSceneEntity.getSearchExecutorEngineer());
 
             dto.setSearchPlannerEngineers(RoleUtils.getEditors(roleService , String.valueOf(deepSearchSceneEntity.getSearchPlannerEngineer()))); // 查询出当前的数据分析编辑人员
-            dto.setSearchExecutorEngineers(RoleUtils.getEditors(roleService, String.valueOf(deepSearchSceneEntity.getSearchExecutorEngineer()))); // 查询出当前的内容编辑人员
+//            dto.setSearchExecutorEngineers(RoleUtils.getEditors(roleService, String.valueOf(deepSearchSceneEntity.getSearchExecutorEngineer()))); // 查询出当前的内容编辑人员
         }
 
         return AjaxResult.success("操作成功.", dto);
+    }
+
+    /**
+     * 查询出所有的DeepSearch场景
+     * @return
+     */
+    @DataPermissionQuery
+    @GetMapping("/listAllDeepsearchScene")
+    public AjaxResult listAll(PermissionQuery query , @RequestParam(required = true) String sceneId) {
+
+        LambdaQueryWrapper<DeepSearchTaskEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.setEntityClass(DeepSearchTaskEntity.class);
+        query.toWrapper(queryWrapper);
+        queryWrapper.eq(DeepSearchTaskEntity::getSceneId, sceneId);
+        queryWrapper.orderByDesc(DeepSearchTaskEntity::getAddTime);
+
+        return AjaxResult.success(taskService.list(queryWrapper)) ;
     }
 
     @Override
