baf6ba9f2d46fee2b426a64c8dc99737ac06065f
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:21 +0800

    feat:新增 datetimeFill.vue 组件，支持日期填空题的输入功能。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/datetimeFill.vue
@@ -0,0 +1,28 @@
+<template>
+    <questionContainer :question="question">
+        <div class="dateTime-question-wrapper">
+            <!-- {{ props.question }} -->
+            <el-input type="date" placeholder="选择日期" v-model="answer" size="large"></el-input>
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import { defineProps , ref } from 'vue'
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const answer = ref('')
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
