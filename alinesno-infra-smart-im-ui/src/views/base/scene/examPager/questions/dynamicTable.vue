618996e9ebc4b4c0e79d8d7090d509a2552af236
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增 dynamicTable.vue 组件，用于展示动态表格题目内容。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/dynamicTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <questionContainer :question="question">
+        <div class="dynamicTable-question-wrapper">
+            {{ props.question }}
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import { defineProps } from 'vue'
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
