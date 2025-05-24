f87969978679a4da97169ab1ab601434e90a6407
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增 examPagerGenResultPanel.vue 组件，包含分页容器与样式初始化。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/pagerGenResultPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pager-gen-result-panel">
+    <PagerContainer 
+      :showHeader="false"
+      ref="pagerContainerRef" />
+  </div>
+</template>
+
+<script setup>
+
+import PagerContainer from './components/PagerContainer.vue'
+
+const pagerContainerRef = ref(null)
+
+</script>
+
+<style lang="scss" scoped>
+.pager-gen-result-panel{
+  margin-bottom:20px;
+}
+</style>
