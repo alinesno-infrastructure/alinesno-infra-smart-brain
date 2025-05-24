bc80d4bdb91a8ffbef544cc7777bb40889c379f2
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增 matrixScale.vue 组件，包含基础模板结构和依赖引入。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/matrixScale.vue
@@ -0,0 +1,16 @@
+<template>
+    <questionContainer>
+        <div class="radio-question-wrapper">
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
