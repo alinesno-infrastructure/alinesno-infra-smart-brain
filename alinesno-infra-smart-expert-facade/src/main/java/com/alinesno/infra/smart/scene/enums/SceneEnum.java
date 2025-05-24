8d91be2763cdc802f8ff7530b9e02987d904e929
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增考试培训、PPT创作、文档审核及视频生成等场景枚举定义

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/enums/SceneEnum.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/enums/SceneEnum.java
@@ -20,6 +20,7 @@ public enum SceneEnum {
                     new SceneAgent(120000001L, "深度检索专员", "searchPlanner", "规划专员负责规划并确定问题的优先级，并确定问题的解决方式。")
             )
     )),
+    // 通用智能体
     GENERAL_AGENT(new SceneInfoDto(110000000L, "通用智能体", "generalAgent", "多行业的通用业务处理和结果展示场景，数据分析、行业分析、报告编写等", "已集成", "1", "", "fa-solid fa-cogs",
             List.of(
                     new SceneAgent(110000001L, "解决方案助手", "businessProcessor", "业务处理助手负责处理通用业务流程中的各类任务，为后续的结果展示提供准确的数据支持。" , true),
@@ -27,32 +28,55 @@ public enum SceneEnum {
                     new SceneAgent(110000003L, "业务展示助手", "dataViewer", "业务展示助手专注于将业务处理助手处理后的数据和结果，以清晰、直观且用户友好的方式呈现出来。")
             )
     )),
+    // 考试培训
+    EXAM_AGENT(new SceneInfoDto(130000000L, "考试培训", "examPager", "针对于考试培训场景，提供考试场景的智能助手", "已集成", "1", "", "fa-solid fa-graduation-cap",
+            List.of(
+                    // 出题专员、阅卷专员、组卷专员
+                    new SceneAgent(130000000L, "出题专员", "questionGenerator", "根据问题的类型和难度，自动生成试题。") ,
+                    new SceneAgent(130000001L, "阅卷专员", "answerChecker", "对学生的答案进行阅卷，给出正确与否的判断，并给出解析答案") ,
+                    new SceneAgent(130000002L, "组卷专员", "paperGenerator", "根据试题库和考试规则，自动生成试卷。")
+            )
+    )),
+    // PPT创作
+    PPT_CREATION(new SceneInfoDto(150000000L, "PPT创作", "pptCreation", "根据提供的文本内容，自动生成PPT内容，并导出为PPT文件", "已集成", "0", "", "fa-solid fa-file-powerpoint",
+            List.of(
+                    // PPT内容规划专员、PPT内容生成专员
+                    new SceneAgent(150000001L, "PPT内容规划专员", "pptPlanner", "根据提供的文本内容，自动生成PPT内容，并导出为PPT文件。") ,
+                    new SceneAgent(150000002L, "PPT内容生成专员", "pptGenerator", "根据提供的文本内容，自动生成PPT内容，并导出为PPT文件。")
+            )
+    )),
+    // 长文场景
     LONG_TEXT(new SceneInfoDto(10000000L, "文档编写", "longText", "主要用于论文、合同、法律、招投标书编写等长文本场景", "已集成", "1", "", "fa-solid fa-signature",
             List.of(
                     new SceneAgent(10000102L, "大纲编辑助手", "chapterEditor", "对长文本的大纲进行编辑和优化，使其逻辑更加清晰。" , true),
                     new SceneAgent(10000103L, "内容编写助手", "contentEditor", "根据规划和大纲，生成具体的文本内容。")
-            ))),
+    ))),
+    // 文档审核
     DOCUMENT_REVIEW(new SceneInfoDto(40000000L, "文档审核", "documentReview", "上传一份文档进行文档审核和修改建议，给出审核的意见和内容", "已集成", "1", "", "fa-solid fa-file-pen",
             List.of(
                     new SceneAgent(20000101L, "文档分析助手", "analysisAgent", "对合同类文档的审核，审核清单生成，还有分析合同目录内容等。" , true),
                     new SceneAgent(20000202L, "文档审核助手", "logicReviewer", "审核文档内容的逻辑是否合理，有无矛盾或不合理之处。")
-            ))),
+    ))),
+    // 视频生成
     VIDEO_GENERATION(new SceneInfoDto(100000000L, "短视频生成", "videoGeneration", "根据提供的文本内容，自动生成视频内容，并导出为 MP4 文件", "已集成", "0", "", "fa-solid fa-video",
             List.of(
                     new SceneAgent(70000701L, "视频脚本生成助手", "textParser", "对提供的文本内容进行解析，提取关键信息。"),
                     new SceneAgent(70000702L, "视频内容生成助手", "videoGenerator", "根据解析的文本内容，生成视频内容。")
-            ))),
+    ))),
+    // 文档阅读
     DOCUMENT_READER(new SceneInfoDto(50000000L, "文档阅读", "documentReader", "针对于大文档类型的阅读，归纳总结等，查询内容，或者提供对应的建议等 ", "已集成", "0", "", "fa-solid fa-book-open-reader",
             List.of(
                     new SceneAgent(30000301L, "内容分析助手", "summaryAgent", "对大文档的内容进行归纳总结，提取关键信息。"),
                     new SceneAgent(30000302L, "案例查询助手", "caseQueryAgent", "根据用户输入的关键词，在文档中查询相关案例内容。")
-            ))),
+    ))),
+    // 内容排版
     CONTENT_FORMATTER(new SceneInfoDto(70000000L, "内容排版", "contentFormatter", "根据给定的模板，自动生成内容，并优化排版", "已集成", "1", "", "fa-solid fa-file-lines",
             List.of(
                     new SceneAgent(40000401L, "内容提取助手", "templateExtractor", "从给定的内容中提取出模板信息。") ,
                     new SceneAgent(40000402L, "内容检查助手", "contentReviewer", "对提取的模块内容进行检查，确保模板的完整性和正确性。" , true)
             )
     )),
+    // 项目检索
     PRODUCT_RESEARCH(new SceneInfoDto(90000000L, "项目检索", "productResearch", "自动接入Git或者SVN等，获取到进度更新情况，并进行汇总归纳", "已集成", "0", "", "fa-solid fa-chart-line",
             Arrays.asList(
                     new SceneAgent(80000801L, "禅道进度提取助手", "progressExtractor", "从禅道系统中提取产品的进度信息。"),
