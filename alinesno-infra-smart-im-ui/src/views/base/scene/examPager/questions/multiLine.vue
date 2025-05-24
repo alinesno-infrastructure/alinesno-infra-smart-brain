a31848c8c12598a126b2460b8a4b0f895f693c42
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增多行输入题组件 multiLine.vue，支持展示和输入长文本答案。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/multiLine.vue
@@ -0,0 +1,29 @@
+<template>
+    <questionContainer :question="question">
+        <div class="multiLine-question-wrapper">
+            <!-- {{ props.question }} -->
+            <el-input v-model="answer" type="textarea" :rows="6" resize="none" placeholder="请输入内容" />
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import { defineProps , ref } from 'vue'
+
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const answer = ref('')
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
