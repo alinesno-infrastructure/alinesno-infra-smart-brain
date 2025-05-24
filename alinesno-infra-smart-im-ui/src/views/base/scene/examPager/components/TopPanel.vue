a6463e4352b417fd8331d0a56959212b76a6035b
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:22 +0800

    feat:新增 TopPanel 组件，包含返回、编辑、预览、保存和发布等功能按钮。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/components/TopPanel.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="top-panel">
+
+        <div style="display: flex; align-items: center; justify-content: space-between; border-bottom: 1px solid rgb(232, 232, 232); padding: 0px 24px; height: 45px;">
+            <div style="display: flex;align-items: center;">
+                <div style="display: flex;gap: 10px;align-items: center;">
+                    <el-button text bg type="primary" @click="handleBack()">
+                        <i class="fa-solid fa-arrow-left"></i>
+                    </el-button>
+                    <span>
+                        广西中医药大学2022级信息管理与信息系统专业
+                    </span>
+                    <span>
+                        <el-button icon="More" size="small" type="text" @click="handleMoreOptions" />
+                    </span>
+                </div>
+                <el-button  type="primary" text bg @click="handleMenu">
+                    <i class="fa-solid fa-file-pen"></i>&nbsp;编辑
+                </el-button>
+                <el-button  type="primary" text bg @click="handleMenu">
+                    <i class="fa-solid fa-eye"></i>&nbsp;预览
+                </el-button>
+            </div>
+
+            <div style="display: flex;flex-direction: row;align-items: center;align-content: center;">
+                <el-button  type="warning" text bg @click="handleSavePager()">
+                  <i class="fa-solid fa-floppy-disk"></i>&nbsp;保存 
+                </el-button>
+                <el-button  type="danger" text bg @click="handleMenu">
+                    <i class="fa-solid fa-share"></i>&nbsp;发布并分享
+                </el-button>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import { ref , defineEmits } from 'vue'
+const emit = defineEmits(['handleSavePager'])
+
+const router = useRouter()
+const route = useRoute()
+
+const sceneId = ref(route.query.sceneId)
+
+const handleSavePager = () => {
+    emit('handleSavePager')
+}
+
+const handleBack = () => {
+    console.log('返回')
+  const path = '/scene/examPager/pagerManager';
+  router.push({
+    path: path,
+    query: { 'sceneId': sceneId.value }
+  })
+}
+
+</script>
+
+<style lang="scss" scoped>
+.top-panel {
+    background: #fff;
+}
+</style>
