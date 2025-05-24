c2b01af216a8678e1b068be9eb68c042f29683c7
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:21 +0800

    feat:新增 descriptionPanel.vue 组件，支持题目描述输入框功能。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/descriptionPanel.vue
@@ -0,0 +1,34 @@
+<template>
+    <questionContainer :question="question">
+        <div class="descriptionPanel-question-wrapper">
+            <el-input 
+                type="textarea" 
+                placeholder="请输入内容" 
+                :rows="8"
+                resize="none"
+                v-model="answer" 
+                size="large">
+            </el-input>
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import { defineProps , ref } from 'vue'
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const answer = ref('')
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: false
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
