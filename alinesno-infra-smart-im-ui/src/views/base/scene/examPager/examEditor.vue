ac328d7651b5e99d63750dacb7c00594140f5643
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:21 +0800

    feat:新增 examEditor.vue 文件，实现考试编辑页面的基础布局，包含功能列表侧边栏和试卷视图主体。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/examEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="exam-pagercontainer">
+
+    <el-container style="height:calc(100vh - 40px );background-color: #fff;">
+
+      <el-aside width="280px" class="exam-pager-aside">
+        <FunctionList />
+      </el-aside>
+
+      <el-main class="exam-pager-main">
+        <ExamPagerView />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+import FunctionList from './functionList'
+import ExamPagerView from './examPagerView'
+
+</script>
+
+<style scoped lang="scss">
+</style>
