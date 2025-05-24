5e22905bccab640676a7d71c57c83c57cab5e019
李伟(Michael) <michaelAip@gmail.com>
2025-05-25 07:34:21 +0800

    feat:新增填空题组件 locationFill.vue，支持输入框绑定答案逻辑。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/locationFill.vue
@@ -0,0 +1,30 @@
+<template>
+    <questionContainer :question="question">
+        <div class="locationFill-question-wrapper">
+            <el-input 
+                v-model="answer" 
+                size="large"
+                placeholder="请输入内容"></el-input>
+        </div>
+    </questionContainer>
+</template>
+
+<script setup>
+
+import { defineProps , ref } from 'vue'
+import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const answer = ref('')
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
