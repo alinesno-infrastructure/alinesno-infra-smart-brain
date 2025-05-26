aca554e555eea627b3d80b162eca1d6bb59e54ff
李娜(Allen) <allenAip@gmail.com>
2025-05-27 00:12:49 +0800

    feat:新增 IExamPagerService 接口，实现获取试卷详情功能

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamPagerService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamPagerService.java
@@ -1,15 +1,8 @@
 package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service;
 
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
-import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
-import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
-
-import java.util.List;
 
 /**
  *
@@ -18,5 +11,12 @@ import java.util.List;
  * @version 1.0.0
  */
 public interface IExamPagerService extends IBaseService<ExamPagerEntity> {
+
+    /**
+     * 获取试卷详情
+     * @param id
+     * @return
+     */
+    ExamPaperDTO getPagerDetail(Long id);
 
 }
