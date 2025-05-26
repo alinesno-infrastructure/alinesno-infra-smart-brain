b8417ea339cc4863fee51742030eb597b929708d
陈伟(Victor) <victorAip@gmail.com>
2025-05-26 21:25:54 +0800

    feat:新增 IExamPagerService 接口，定义考试试卷服务的基础操作

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamPagerService.java
@@ -0,0 +1,22 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
+import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
+import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IExamPagerService extends IBaseService<ExamPagerEntity> {
+
+}
