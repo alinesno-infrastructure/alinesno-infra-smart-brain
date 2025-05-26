46defee03e878980082c4b86dc707d1fce5f761a
李娜(Allen) <allenAip@gmail.com>
2025-05-27 00:12:49 +0800

    feat:完善 ExamPagerServiceImpl，新增试卷详情获取逻辑并引入 JSONObject 支持。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamPagerServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamPagerServiceImpl.java
@@ -1,32 +1,20 @@
 package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.QuestionDTO;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper.ExamPagerMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper.ExamPagerSceneMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamPagerSceneService;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamPagerService;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionBankService;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionService;
-import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
-import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
-import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
-import com.alinesno.infra.smart.scene.entity.ExamQuestionBankEntity;
 import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
-import com.alinesno.infra.smart.utils.RoleUtils;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -36,4 +24,36 @@ import java.util.List;
 @Service
 public class ExamPagerServiceImpl extends IBaseServiceImpl<ExamPagerEntity , ExamPagerMapper> implements IExamPagerService {
 
+    @Autowired
+    private IExamQuestionService examQuestionService ;
+
+    @Override
+    public ExamPaperDTO getPagerDetail(Long id) {
+
+        ExamPaperDTO dto = new ExamPaperDTO() ;
+
+        // 获取到试卷信息
+        ExamPagerEntity entity = getById(id) ;
+        dto.setPagerName(entity.getTitle());
+
+        // 获取到试卷的题目信息
+        LambdaQueryWrapper<ExamQuestionEntity> waqWrapper = new LambdaQueryWrapper<>();
+        waqWrapper.eq(ExamQuestionEntity::getPagerId, id);
+
+        List<ExamQuestionEntity> questionList = examQuestionService.list(waqWrapper);
+
+        List<QuestionDTO> dtoList = questionList.stream().map(question -> {
+            QuestionDTO dtoInner = new QuestionDTO() ;
+
+            BeanUtils.copyProperties(question , dtoInner);
+            dtoInner.setAnswers(JSONObject.parseArray(question.getAnswers() , JSONObject.class));
+            dtoInner.setBlanks(JSONObject.parseArray(question.getBlanks() , JSONObject.class));
+
+            return dtoInner;
+        }).toList();
+
+        dto.setQuestionList(dtoList);
+
+        return dto;
+    }
 }
