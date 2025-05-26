cfe7ffb4f15f370cf21039e957f4674c14beed8b
梁明树(Nina) <ninaAip@gmail.com>
2025-05-27 00:12:49 +0800

    feat:新增 examPaperManager.js 文件，引入请求工具并初始化接口配置项。

---
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/examPaperManager.js
@@ -0,0 +1,8 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+// 接口配置项
+var prefix = '/api/infra/smart/assistant/scene/examPagerManager/' ;
+
+var managerUrl = {
+}
