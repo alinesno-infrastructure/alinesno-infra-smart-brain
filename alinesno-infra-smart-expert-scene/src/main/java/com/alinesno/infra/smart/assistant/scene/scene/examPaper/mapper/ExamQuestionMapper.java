f2c8f686247485a8bce3464d277230eac2de473d
陈伟(Victor) <victorAip@gmail.com>
2025-05-26 21:25:54 +0800

    feat:新增 ExamQuestionMapper 接口，继承 IBaseMapper 完成数据映射定义。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/mapper/ExamQuestionMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
+import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ExamQuestionMapper extends IBaseMapper<ExamQuestionEntity> {
+}
