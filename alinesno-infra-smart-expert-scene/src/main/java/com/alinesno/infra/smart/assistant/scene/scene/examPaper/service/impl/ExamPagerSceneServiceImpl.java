0f6ea41ed3a89648218f12f08f62b72d4c4617a0
陈伟(Victor) <victorAip@gmail.com>
2025-05-27 19:34:45 +0800

    refactor: 优化 ExamPagerSceneServiceImpl 类，移除未使用的依赖和服务方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamPagerSceneServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamPagerSceneServiceImpl.java
@@ -1,33 +1,22 @@
 package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.QuestionDTO;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper.ExamPagerSceneMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamPagerSceneService;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamPagerService;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionBankService;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionService;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
 import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
 import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
-import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
 import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
-import com.alinesno.infra.smart.scene.entity.ExamQuestionBankEntity;
-import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
 import com.alinesno.infra.smart.utils.RoleUtils;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -40,12 +29,6 @@ public class ExamPagerSceneServiceImpl extends IBaseServiceImpl<ExamPagerSceneEn
 
     @Autowired
     private IIndustryRoleService roleService ;
-
-    @Autowired
-    private IExamPagerService examPagerService;
-
-    @Autowired
-    private IExamQuestionBankService examQuestionBankService;
 
     @Autowired
     private IExamQuestionService examQuestionService;
@@ -109,100 +92,4 @@ public class ExamPagerSceneServiceImpl extends IBaseServiceImpl<ExamPagerSceneEn
         return getOne(wrapper) ;
     }
 
-    @Override
-    public void savePager(ExamPaperDTO dto) {
-
-        String pagerType = dto.getPagerType() ;
-
-        if("pager".equals(pagerType)){  // 保存到试卷
-            savePagerQuestion(dto);
-        }else if("banks".equals(pagerType)){  // 保存到题库
-            savePagerBanks(dto);
-        }
-
-    }
-
-    @Override
-    public List<ExamPagerEntity> pagerListByPage(DatatablesPageBean page, PermissionQuery query) {
-
-        page.setPageNum(0);
-        page.setPageSize(20);
-
-        Page<ExamPagerEntity> pageBean = new Page<>(page.getPageNum(), page.getPageSize());
-
-        LambdaQueryWrapper<ExamPagerEntity> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(ExamPagerEntity::getOrgId, query.getOrgId()) ;
-
-        pageBean = examPagerService.page(pageBean, wrapper);
-
-        return pageBean.getRecords();
-    }
-
-    /**
-     * 保存题库
-     * @param dto
-     */
-    private void savePagerBanks(ExamPaperDTO dto) {
-
-        String pagerName = dto.getPagerName() ;
-
-        ExamQuestionBankEntity examQuestionBankEntity = new ExamQuestionBankEntity();
-        examQuestionBankEntity.setBankName(pagerName);
-
-        examQuestionBankService.save(examQuestionBankEntity);
-
-        List<ExamQuestionEntity> questionList = new ArrayList<>() ;
-
-        List<QuestionDTO> list = dto.getQuestionList() ;
-        for(QuestionDTO questionDTO : list){
-            ExamQuestionEntity e = new ExamQuestionEntity() ;
-            BeanUtils.copyProperties(questionDTO , e);
-            e.setId(null);
-            e.setBankId(examQuestionBankEntity.getId());
-            e.setSortOrder(questionDTO.getOrder());
-
-            questionList.add(e);
-        }
-
-        examQuestionService.saveBatch(questionList);
-    }
-
-    /**
-     * 保存试卷
-     * @param dto
-     */
-    private void savePagerQuestion(ExamPaperDTO dto) {
-
-        String pagerName = dto.getPagerName() ;
-
-        ExamPagerEntity pagerEntity = new ExamPagerEntity();
-        BeanUtils.copyProperties(dto , pagerEntity);
-        pagerEntity.setTitle(pagerName);
-
-        examPagerService.save(pagerEntity);
-
-        List<ExamQuestionEntity> questionList = new ArrayList<>() ;
-
-        List<QuestionDTO> list = dto.getQuestionList() ;
-
-        for(QuestionDTO questionDTO : list){
-
-            ExamQuestionEntity e = new ExamQuestionEntity() ;
-
-            BeanUtils.copyProperties(dto , e);
-            BeanUtils.copyProperties(questionDTO , e);
-
-            e.setId(null);
-            e.setPagerId(pagerEntity.getId());
-
-            e.setAnswers(JSONObject.toJSONString(questionDTO.getAnswers()));
-            e.setBlanks(JSONObject.toJSONString(questionDTO.getBlanks()));
-            e.setSortOrder(questionDTO.getOrder());
-
-            questionList.add(e);
-        }
-
-        examQuestionService.saveBatch(questionList);
-
-    }
 }
