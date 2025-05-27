d9f9dcb346f83fe258d40918bc6c1e2edad86166
陈伟(Victor) <victorAip@gmail.com>
2025-05-27 19:34:45 +0800

    feat: 增加 currentPageInfo 数据传递，优化组件间交互逻辑

--- alinesno-infra-smart-im-ui/src/views/base/scene/examPager/examPagerView.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/examPagerView.vue
@@ -2,19 +2,25 @@
   <div class="exam-pager-container">
     <TopPanel 
       @handleSavePager="handleSavePager"
+      :currentPageInfo="currentPageInfo"
       ref="topPanelRef" />
 
     <el-row>
       <el-col :span="5">
         <LeftPanel 
           @handleAddQuestionCard="handleAddQuestionCard"
+          :currentPageInfo="currentPageInfo"
           ref="leftPanelRef" />
       </el-col>
       <el-col :span="14">
-        <PagerContainer ref="pagerContainerRef" />
+        <PagerContainer 
+          @setCurrentPageInfo="setCurrentPageInfo"
+          ref="pagerContainerRef" />
       </el-col>
       <el-col :span="5">
-        <RightPanel ref="rightPanelRef" />
+        <RightPanel 
+          :currentPageInfo="currentPageInfo"
+          ref="rightPanelRef" />
       </el-col>
     </el-row>
 
@@ -36,6 +42,7 @@ const topPanelRef = ref(null)
 const pagerContainerRef = ref(null)
 const leftPanelRef = ref(null)
 const rightPanelRef = ref(null)
+const currentPageInfo = ref(null)
 
 // 添加问题
 const handleAddQuestionCard = (newQuestionItem) => {
@@ -45,6 +52,10 @@ const handleAddQuestionCard = (newQuestionItem) => {
 // 保存试卷
 const handleSavePager  = () => {
   pagerContainerRef.value.handleSavePager()
+}
+
+const setCurrentPageInfo = (pageInfo) => {
+  currentPageInfo.value = pageInfo
 }
 
 </script>
