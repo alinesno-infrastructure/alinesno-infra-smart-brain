08a302a086bc4f6bc318c03316bad93e0b775278
张婷(Tina) <tinaAip@gmail.com>
2025-05-29 00:14:47 +0800

    feat:新增对右键菜单功能的支持，引入 contextMenu 指令。

--- alinesno-infra-smart-im-ui/src/directive/index.js
+++ alinesno-infra-smart-im-ui/src/directive/index.js
@@ -1,9 +1,11 @@
 import hasRole from './permission/hasRole'
 import hasPermi from './permission/hasPermi'
 import copyText from './common/copyText'
+import contextMenu from './common/contextMenu'
 
 export default function directive(app){
   app.directive('hasRole', hasRole)
   app.directive('hasPermi', hasPermi)
   app.directive('copyText', copyText)
+  app.directive('contextMenu', contextMenu)
 }
