051a69391adea61cb24515271e614d209d4894d4
王磊(Levi) <leviAip@gmail.com>
2025-05-29 00:14:47 +0800

    feat:新增 IPPTGeneratorSceneService 接口，包含更新场景代理、获取角色列表及根据场景ID获取场景信息等方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/service/IPPTGeneratorSceneService.java
@@ -0,0 +1,48 @@
+package com.alinesno.infra.smart.assistant.scene.scene.pptCreation.service;
+
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperUpdateDTO;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
+import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
+import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
+import com.alinesno.infra.smart.scene.entity.PPTGenerateSceneEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IPPTGeneratorSceneService extends IBaseService<PPTGenerateSceneEntity> {
+
+
+    /**
+     * 更新场景代理
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取角色列表
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
+
+    /**
+     * 根据场景ID获取场景信息
+     * @param sceneId
+     * @param query
+     * @return
+     */
+    PPTGenerateSceneEntity getBySceneId(long sceneId, PermissionQuery query);
+
+}
