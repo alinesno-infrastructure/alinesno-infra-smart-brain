ec80ccff49ee8b74bfa38a6c81333ab5da02db13
王磊(Levi) <leviAip@gmail.com>
2025-05-29 00:14:47 +0800

    feat:新增 PPTOutlineDto 类，用于封装PPT大纲信息并添加字段校验。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/dto/PPTOutlineDto.java
@@ -0,0 +1,36 @@
+package com.alinesno.infra.smart.assistant.scene.scene.pptCreation.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * PPTOutlineDto类用于封装PPT的大纲信息
+ * 该类标记了@Slf4j注解，用于日志记录
+ */
+@EqualsAndHashCode(callSuper = true)
+@Slf4j
+@Data
+public class PPTOutlineDto extends BaseDto {
+
+    /**
+     * PPT的唯一标识符
+     */
+    private Long pptId;
+
+    /**
+     * 场景的唯一标识符
+     */
+    @NotNull(message = "场景不能为空")
+    private Long sceneId ;
+
+    /**
+     * PPT的大纲内容
+     */
+    @NotNull(message = "大纲内容不能为空")
+    private String outline;
+
+}
+
