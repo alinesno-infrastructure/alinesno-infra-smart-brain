c13e7b8a1d70540e44846eb50149b0e012142774
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:57 +0800

    feat:新增 IProjectGitCommitService 接口，定义项目Git提交相关服务的基础方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectGitCommitService.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ProjectGitCommitEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IProjectGitCommitService extends IBaseService<ProjectGitCommitEntity> {
+}    
