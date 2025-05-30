2bd42005297e46552d25360e90a7f1622356797c
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 ArticleGenerateSceneMapper 接口，实现基础映射功能。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/mapper/ArticleGenerateSceneMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ArticleGenerateSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleGenerateSceneMapper extends IBaseMapper<ArticleGenerateSceneEntity> {
+}
