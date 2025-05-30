970c169ade0145ff0762bff0da243fbca24e258a
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 IArticleWriterSceneService 接口，包含更新场景代理、获取角色列表及根据场景ID获取场景信息等方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/service/IArticleWriterSceneService.java
@@ -0,0 +1,43 @@
+package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.service;
+
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
+import com.alinesno.infra.smart.scene.entity.ArticleGenerateSceneEntity;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IArticleWriterSceneService extends IBaseService<ArticleGenerateSceneEntity> {
+
+
+    /**
+     * 更新场景代理
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取角色列表
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
+
+    /**
+     * 根据场景ID获取场景信息
+     * @param sceneId
+     * @param query
+     * @return
+     */
+    ArticleGenerateSceneEntity getBySceneId(long sceneId, PermissionQuery query);
+
+}
