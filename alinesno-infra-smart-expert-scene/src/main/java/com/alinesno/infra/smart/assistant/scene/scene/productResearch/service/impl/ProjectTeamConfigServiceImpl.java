2f65aea1a9e67fb31b6959ef446be51c9a1429df
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 ProjectTeamConfigServiceImpl 类，实现团队配置服务逻辑。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/impl/ProjectTeamConfigServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper.ProjectTeamConfigMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.IProjectTeamConfigService;
+import com.alinesno.infra.smart.scene.entity.ProjectTeamConfigEntity;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProjectTeamConfigServiceImpl extends IBaseServiceImpl<ProjectTeamConfigEntity, ProjectTeamConfigMapper> implements IProjectTeamConfigService {
+}    
