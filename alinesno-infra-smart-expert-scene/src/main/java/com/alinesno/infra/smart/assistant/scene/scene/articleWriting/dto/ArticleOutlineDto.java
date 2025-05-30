2a6cd2592ef8534bc049046b435603051ffe789e
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 ArticleOutlineDto 类，用于封装文章大纲相关信息。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/dto/ArticleOutlineDto.java
@@ -0,0 +1,45 @@
+package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * PPTOutlineDto类用于封装PPT的大纲信息
+ * 该类标记了@Slf4j注解，用于日志记录
+ */
+@EqualsAndHashCode(callSuper = true)
+@Slf4j
+@Data
+public class ArticleOutlineDto extends BaseDto {
+
+    /**
+     * PPT的唯一标识符
+     */
+    private Long pptId;
+
+    @NotNull(message = "提示词不能为空")
+    private String promptText ;
+
+    /**
+     * 场景的唯一标识符
+     */
+    @NotNull(message = "场景不能为空")
+    private Long sceneId ;
+
+    /**
+     * PPT的大纲内容
+     */
+    @NotNull(message = "大纲内容不能为空")
+    private String outline;
+
+    /**
+     * PPT的配置信息
+     */
+    @NotNull(message = "PPT配置不能为空")
+    private ArticleConfigDto pptConfig;
+
+}
+
