6dcdc95794b67c31f0e57fe73b65dddec5f61ed4
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:57 +0800

    feat:新增 IProjectManagerService 接口，定义项目管理服务的基础操作。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectManagerService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ProjectManagerEntity;
+
+public interface IProjectManagerService extends IBaseService<ProjectManagerEntity> {
+}    
