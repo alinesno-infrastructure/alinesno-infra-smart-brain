80ead33b278b116f067adb36b6fedf2944a76217
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:57 +0800

    feat:新增 ProjectResearchSceneServiceImpl 类，实现产品研究场景服务逻辑。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/impl/ProjectResearchSceneServiceImpl.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper.ProjectResearchSceneMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.IProjectResearchSceneService;
+import com.alinesno.infra.smart.scene.entity.ProjectResearchSceneEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProjectResearchSceneServiceImpl extends IBaseServiceImpl<ProjectResearchSceneEntity, ProjectResearchSceneMapper> implements IProjectResearchSceneService {
+}    
