af8fe666b05f5a9a580b770492324b66aaaf931e
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:57 +0800

    feat:新增 ProjectResearchSceneMapper 接口，实现基础映射功能。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/mapper/ProjectResearchSceneMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ProjectGitCommitEntity;
+import com.alinesno.infra.smart.scene.entity.ProjectResearchSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectResearchSceneMapper extends IBaseMapper<ProjectResearchSceneEntity> {
+}    
