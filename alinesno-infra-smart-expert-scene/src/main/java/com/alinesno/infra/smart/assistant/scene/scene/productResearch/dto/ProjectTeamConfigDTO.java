f32e67ef952e968d802943e8bad11eba3e74bab3
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 ProjectTeamConfigDTO 类，继承 BaseDto 并支持后续扩展。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/dto/ProjectTeamConfigDTO.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ProjectTeamConfigDTO extends BaseDto {
+    // 可扩展额外字段
+}    
