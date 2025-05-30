6f1ae1510ed65a4d77be994469c065b6758816af
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:57 +0800

    feat:新增 ProjectTeamConfigMapper 接口，继承 IBaseMapper 完成基础映射定义。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/mapper/ProjectTeamConfigMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ProjectTeamConfigEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectTeamConfigMapper extends IBaseMapper<ProjectTeamConfigEntity> {
+}    
