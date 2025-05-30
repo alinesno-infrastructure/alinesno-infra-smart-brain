9deb3fa3267a6243d06be6a334285c78964ddf81
周健(James) <jamesAip@gmail.com>
2025-05-31 00:16:58 +0800

    feat:新增 IProjectTeamConfigService 接口，定义项目团队配置服务基础操作。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectTeamConfigService.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ProjectTeamConfigEntity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IProjectTeamConfigService extends IBaseService<ProjectTeamConfigEntity> {
+}    
