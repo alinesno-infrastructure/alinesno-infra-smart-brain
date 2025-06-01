e23b19173d3dd86a88cf488d10373d5130d84df8
李伟(Michael) <michaelAip@gmail.com>
2025-06-02 00:15:13 +0800

    feat:新增 IProjectResearchSceneService 接口，包含更新场景代理、获取角色列表及按场景ID获取场景信息等方法。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectResearchSceneService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectResearchSceneService.java
@@ -1,7 +1,35 @@
 package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
+import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
 import com.alinesno.infra.smart.scene.entity.ProjectResearchSceneEntity;
 
+import java.util.List;
+
 public interface IProjectResearchSceneService extends IBaseService<ProjectResearchSceneEntity> {
+
+    /**
+     * 更新场景代理
+     * @param dto
+     */
+    void updateSceneAgents(UpdateSceneAgentDto dto);
+
+    /**
+     * 获取角色列表
+     * @param dto
+     * @return
+     */
+    List<IndustryRoleEntity> getRoleList(RoleListRequestDto dto);
+
+    /**
+     * 根据场景ID获取场景信息
+     * @param sceneId
+     * @param query
+     * @return
+     */
+    ProjectResearchSceneEntity getBySceneId(long sceneId, PermissionQuery query);
+
 }    
