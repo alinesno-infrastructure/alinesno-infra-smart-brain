19ee16ff7497e76125f1b286e7acc1a6cfad5836
李伟(Michael) <michaelAip@gmail.com>
2025-06-02 00:15:13 +0800

    feat:新增导入Git项目功能，包括接口定义及导出方法实现

--- alinesno-infra-smart-im-ui/src/api/base/im/scene/productResearch.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/productResearch.js
@@ -2,6 +2,7 @@ import request from '@/utils/request'
 import { parseStrEmpty } from "@/utils/ruoyi";
 
 // 接口配置项
+var prefix = '/api/infra/smart/assistant/scene/productResearchScene/' ;
 
 var managerUrl = {
   // 大文本
@@ -9,7 +10,7 @@ var managerUrl = {
   processParagraph: prefix +"processParagraph",
   chatRole: prefix +"chatRole",
   getScene: prefix +"getScene",
-  updateChapterEditor: prefix + "updateChapterEditor" ,
+  updateChapterEditor: prefix + "updateChapterEditor" , 
   chatRoleSync: prefix +"chatRoleSync",
   updateChapterContentEditor: prefix +"updateChapterContentEditor",
   getChapterByRole: prefix +"getChapterByRole",
@@ -23,8 +24,8 @@ var managerUrl = {
   initAgents: prefix + "initAgents",
   getPreviewDocx: prefix +"getPreviewDocx",
   getPreviewUrl: prefix + "getPreviewUrl",
-  createScene: prefix + "createScene" ,
-  listAllDeepsearchScene: prefix + "listAllDeepsearchScene" ,
+  createScene: prefix + "createScene" , 
+  listAllDeepsearchScene: prefix + "listAllDeepsearchScene" , 
   getOutputPreviewUrl: prefix +"getOutputPreviewUrl",
   getOutputMarkdownContent: prefix +"getOutputMarkdownContent",
   getQuestionTypes: prefix +"getQuestionTypes",
@@ -34,6 +35,16 @@ var managerUrl = {
   getPagerDetail: prefix +"getPagerDetail",
   updatePagerQuestion: prefix +"updatePagerQuestion",
   savePPTOutline: prefix +"savePPTOutline",
+  importGitProject: prefix + "importGitProject",
+}
+
+// 导入项目
+export function importGitProject(data) {
+  return request({
+    url: managerUrl.importGitProject,
+    method: 'post',
+    data: data
+  })
 }
 
 // savePPTOutline
@@ -70,7 +81,7 @@ export function getPagerDetail(id) {
     url: managerUrl.getPagerDetail + '?id=' + parseStrEmpty(id),
     method: 'get'
   })
-}
+} 
 
 // 分页查询试卷
 export function pagerListByPage(data) {
@@ -81,7 +92,7 @@ export function pagerListByPage(data) {
   })
 }
 
-// 保存试卷问题类型
+// 保存试卷问题类型 
 export function savePagerQuestion(data) {
   return request({
     url: managerUrl.savePagerQuestion,
@@ -114,7 +125,7 @@ export function getOutputPreviewUrl(storageId) {
   })
 }
 
-// 获取到markdown内容
+// 获取到markdown内容 
 export function getOutputMarkdownContent(storageId) {
   return request({
     url: managerUrl.getOutputMarkdownContent + "?storageId=" + storageId,
@@ -172,7 +183,7 @@ export function genDataReport(sceneId) {
 // 更新场景状态
 export function updateSceneGenStatus(sceneId , genStatus) {
   return request({
-    url: managerUrl.updateSceneGenStatus + '?sceneId=' + parseStrEmpty(sceneId) + "&genStatus=" + genStatus ,
+    url: managerUrl.updateSceneGenStatus + '?sceneId=' + parseStrEmpty(sceneId) + "&genStatus=" + genStatus , 
     method: 'get'
   })
 }
@@ -197,7 +208,7 @@ export function chatPromptContent(data) {
 // 初始化Agent角色
 export function initAgents(data){
   return request({
-    url: managerUrl.initAgents ,
+    url: managerUrl.initAgents , 
     method: 'post',
     data: data
   })
@@ -211,168 +222,3 @@ export function getScene(id) {
     method: 'get'
   })
 }
-
-//// 获取到支持场景
-//export function supportScreen() {
-//  return request({
-//    url: managerUrl.supportScreen,
-//    method: 'get'
-//  })
-//}
-
-//// 运行任务
-//export function executeScreenTask(data,uId) {
-//  return request({
-//    url: managerUrl.executeScreenTask + "?uId=" + parseStrEmpty(uId) ,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 运行场景计划
-//export function leaderPlan(data) {
-//  return request({
-//    url: managerUrl.leaderPlan ,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 更新leader角色
-//export function updateLeaderRole(data) {
-//  return request({
-//    url: managerUrl.updateLeaderRole,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 上传文件到OSS
-//export function uploadOss(sceneId) {
-//  return request({
-//    url: managerUrl.uploadOss + '?sceneId=' + parseStrEmpty(sceneId),
-//    method: 'get',
-//  })
-//}
-//
-//// 场景列表
-//export function screenList(query) {
-//  return request({
-//    url: managerUrl.screenList,
-//    method: 'get',
-//    params: query
-//  })
-//}
-//
-//// 更新章节内容
-//export function updateChapterContent(data) {
-//  return request({
-//    url: managerUrl.updateChapterContent,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 内容片断修改
-//export function processParagraph(data) {
-//  return request({
-//    url: managerUrl.processParagraph,
-//    headers: {
-//      isEncrypt: false
-//    },
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 查询章节内容
-//export function getChapterContent(id) {
-//  return request({
-//    url: managerUrl.getChapterContent + '?chapterId=' + parseStrEmpty(id),
-//    method: 'get'
-//  })
-//}
-//
-//// 查询章节详细
-//export function getChapterByRole(roleId , sceneId) {
-//  return request({
-//    url: managerUrl.getChapterByRole + '?roleId=' + parseStrEmpty(roleId) + "&sceneId="+ parseStrEmpty(sceneId),
-//    method: 'get'
-//  })
-//}
-//
-//// 更新编辑人员
-//export function updateChapterContentEditor(data) {
-//  return request({
-//    url: managerUrl.updateChapterContentEditor,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//export function chatRoleSync(data) {
-//  return request({
-//    url: managerUrl.chatRoleSync ,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 与Role聊天
-//export function chatRole(data) {
-//  return request({
-//    url: managerUrl.chatRole ,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//// 保存章节
-//export function saveDataPlan(data , sceneId) {
-//  return request({
-//    url: managerUrl.saveDataPlan + "?sceneId=" + parseStrEmpty(sceneId) ,
-//    method: 'post',
-//    data: data
-//  })
-//}
-//
-//
-//// 查询场景详细
-//export function getScreen(id) {
-//  return request({
-//    url: managerUrl.getScreen + '?id=' + parseStrEmpty(id),
-//    method: 'get'
-//  })
-//}
-//
-//// 更新章节编辑人员
-//export function updateChapterEditor(id , editors , type) {
-//
-//  let data = {
-//    id: parseStrEmpty(id),
-//    editors: editors,
-//    type: type
-//  }
-//
-//  return request({
-//    url: managerUrl.updateChapterEditor ,
-//    headers: {
-//      isEncrypt: false
-//    },
-//    method: 'post',
-//    data: data
-//  })
-//
-//}
-//
-//// 创建场景
-//export function createScreen(data) {
-//  return request({
-//    url: managerUrl.createScreen,
-//    headers: {
-//      isEncrypt: false
-//    },
-//    method: 'post',
-//    data: data
-//  })
-//}
