6152aa687092f79ada17d9465bc592d90a1c1108
李伟(Michael) <michaelAip@gmail.com>
2025-06-02 00:15:13 +0800

    feat:新增 ProjectResearchSceneDTO 数据传输对象，包含场景ID、工程师信息及生成状态等字段。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/dto/ProjectResearchSceneDTO.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/dto/ProjectResearchSceneDTO.java
@@ -1,11 +1,28 @@
 package com.alinesno.infra.smart.assistant.scene.scene.productResearch.dto;
 
-import com.alinesno.infra.common.facade.base.BaseDto;
+import com.alinesno.infra.smart.assistant.api.IndustryRoleDto;
+import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.List;
+
+/**
+ *  项目跟进场景数据传输对象
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class ProjectResearchSceneDTO extends BaseDto {
-    // 可扩展额外字段
-}    
+public class ProjectResearchSceneDTO extends SceneInfoDto {
+
+    private long sceneId;  // 场景ID
+
+    private String processCollectorEngineer ;  //  进度采集工程师
+    private List<IndustryRoleDto>  processCollectorEngineerEntity;
+
+    private String progressAnalyzerEngineer ;  //  项目情况分析工程师
+    private List<IndustryRoleDto>  progressAnalyzerEngineerEntity;
+
+    private Integer genStatus = 0 ; // 生成状态
+    private String promptContent; // 提示内容
+
+}
