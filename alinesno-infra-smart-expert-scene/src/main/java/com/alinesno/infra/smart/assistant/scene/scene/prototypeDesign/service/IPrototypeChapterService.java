b7751b98718b54e0120b91fd03fef270cc3d988d
张婷(Tina) <tinaAip@gmail.com>
2025-06-03 00:15:27 +0800

    feat:新增 IPrototypeChapterService 接口，继承 IBaseService 定义原型章节服务规范。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/prototypeDesign/service/IPrototypeChapterService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.scene.scene.prototypeDesign.service;
+
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.PrototypeChapterEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IPrototypeChapterService extends IBaseService<PrototypeChapterEntity> {
+
+}
