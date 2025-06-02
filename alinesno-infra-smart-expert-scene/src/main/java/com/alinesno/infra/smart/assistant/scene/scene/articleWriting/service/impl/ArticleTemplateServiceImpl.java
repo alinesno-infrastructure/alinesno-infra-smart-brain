98756c34b08dc0e2f8f45cdb682689ad87de822b
张婷(Tina) <tinaAip@gmail.com>
2025-06-03 00:15:27 +0800

    feat: 实现 ArticleTemplateServiceImpl，提供按类型获取文章模板的方法

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/service/impl/ArticleTemplateServiceImpl.java
@@ -0,0 +1,44 @@
+package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.mapper.ArticleTemplateMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.service.IArticleTemplateService;
+import com.alinesno.infra.smart.scene.entity.ArticleTemplateEntity;
+import com.alinesno.infra.smart.scene.enums.SceneScopeType;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 试卷场景服务实现类
+ */
+@Slf4j
+@Service
+public class ArticleTemplateServiceImpl extends IBaseServiceImpl<ArticleTemplateEntity, ArticleTemplateMapper> implements IArticleTemplateService {
+
+    @Override
+    public List<ArticleTemplateEntity> getTemplateByType(PermissionQuery query, String typeCode) {
+
+        Long orgId = query.getOrgId();
+
+        // 创建查询条件构造器
+        LambdaQueryWrapper<ArticleTemplateEntity> wrapper = new LambdaQueryWrapper<>();
+
+        // 构建查询条件：articleTemplatePermission为public或者类型为org且orgId为查询条件中的orgId
+        wrapper.and(w -> w.eq(ArticleTemplateEntity::getArticleTemplatePermission , SceneScopeType.PUBLIC_SCENE.getValue())
+                .or()
+                .eq( ArticleTemplateEntity::getArticleTemplatePermission, SceneScopeType.ORG_SCENE.getValue())
+                .eq(ArticleTemplateEntity::getOrgId, orgId));
+
+        // 可选的类型过滤
+        if (typeCode != null && !typeCode.isEmpty()) {
+            wrapper.eq(ArticleTemplateEntity::getArticleTemplateType, typeCode);
+        }
+
+        // 执行查询并返回结果
+        return this.baseMapper.selectList(wrapper);
+    }
+}
