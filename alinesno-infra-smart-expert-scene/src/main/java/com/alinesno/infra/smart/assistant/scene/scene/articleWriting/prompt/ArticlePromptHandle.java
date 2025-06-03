c8ba637baedcf6194a608de94a498bb7de941238
李伟(Michael) <michaelAip@gmail.com>
2025-06-04 04:52:13 +0800

    feat: 增加 ArticlePromptHandle.generatorPrompt 方法的模板支持，支持传入 ArticleTemplateEntity 参数。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/prompt/ArticlePromptHandle.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/prompt/ArticlePromptHandle.java
@@ -1,32 +1,42 @@
 package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.prompt;
 
 import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.dto.ArticleGeneratorDTO;
+import com.alinesno.infra.smart.scene.entity.ArticleTemplateEntity;
 
 public class ArticlePromptHandle {
 
-    public static String generatorPrompt(ArticleGeneratorDTO dto) {
-        String str = ArticlePrompt.PROMPT_ARTICLE_ZH ;
+    public static String generatorPrompt(ArticleGeneratorDTO dto, ArticleTemplateEntity templateEntity) {
+
+        String templatePrompt = "";
+
+        if (templateEntity != null) {
+            templatePrompt = String.format("""
+                            %s
+                            ##限制:
+                            - 只返回文章内容，其它不相关内容不要返回
+                            - 严格按照模板格式返回
+                             """,
+                    templateEntity.getPrompt()
+            );
+        }
 
         String chatPrompt = """
-                ## 生成PPT需求
-                写作类型：%s
-                生成页数: %s
-                受众群体: %s
-                文章场景: %s
-                语句表达: %s
-
-                ##限制:
-                - 只返回文章内容，其它不相关内容不要返回
+                ## 其它说明
+                写作类型=%s
+                写作类型=%s
+                受众群体=%s
+                文章场景=%s
+                语句表达=%s
                 """;
 
         String chatPromptText = String.format(chatPrompt,
-                dto.getPromptText() ,
+                dto.getPromptText(),
                 dto.getPptConfig().getWritingType(),
                 dto.getPptConfig().getAudience(),
                 dto.getPptConfig().getScenario(),
                 dto.getPptConfig().getTone());
 
-        return str + chatPromptText;
+        return templatePrompt + chatPromptText;
     }
 
 }
