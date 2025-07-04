f57d40accb03f68d5ecfc08f8154d78b34c9a779
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:10 +0800

    feat:创建智能场景常量类SceneConstants，定义向量文档索引名称常量

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/constants/SceneConstants.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.scene.constants;
+
+/**
+ * 智能场景常量类
+ */
+public class SceneConstants {
+
+    public static final String ALINESNO_SCENE_PROJECT_VECTOR_DOCUMENT_INDEX_NAME = "alinesno_scene_project_vector_document";
+
+}
