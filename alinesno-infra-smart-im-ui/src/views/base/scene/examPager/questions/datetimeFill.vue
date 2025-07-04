5a031f8679011f768a8d8478387619dc8c0b2cb8
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:14 +0800

    feat:将日期输入改为使用el-date-picker组件并增加答案处理逻辑

--- alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/datetimeFill.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/datetimeFill.vue
@@ -1,18 +1,21 @@
 <template>
     <questionContainer :question="question">
         <div class="dateTime-question-wrapper">
-            <!-- {{ props.question }} -->
-            <el-input type="date" placeholder="选择日期" v-model="answer" size="large"></el-input>
+            <el-date-picker
+                v-model="answer"
+                type="date"
+                placeholder="选择日期"
+                size="large"
+                value-format="YYYY-MM-DD"
+                @change="handleAnswerChange"
+            />
         </div>
     </questionContainer>
 </template>
 
 <script setup>
-
-import { defineProps , ref } from 'vue'
+import { defineProps, ref, watch } from 'vue'
 import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
-
-const answer = ref('')
 
 const props = defineProps({
     question: {
@@ -21,8 +24,37 @@ const props = defineProps({
     }
 })
 
+const emit = defineEmits(['update-handleUpdateQuestion'])
+
+const answer = ref('')
+
+// 初始化答案
+if (props.question.answer && props.question.answer.length > 0) {
+    answer.value = props.question.answer[0] || ''
+}
+
+// 监听答案变化
+watch(answer, (newVal) => {
+    handleAnswerChange()
+})
+
+// 处理答案变化
+const handleAnswerChange = () => {
+    const updatedQuestion = {
+        ...props.question,
+        answer: [answer.value], // 日期答案以数组形式存储
+        userAnswer: answer.value
+    }
+    emit('update-handleUpdateQuestion', updatedQuestion)
+}
 </script>
 
 <style lang="scss" scoped>
-
+.dateTime-question-wrapper {
+    padding: 10px 0;
+    
+    .el-date-editor {
+        width: 100%;
+    }
+}
 </style>
