05f1539e1d8b385d44a7cb4966c4e942721c68d3
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:03 +0800

    feat:创建ILongTextTaskService接口，包含分页查询和获取任务进度方法。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/service/ILongTextTaskService.java
@@ -0,0 +1,34 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.dto.TaskProgressDto;
+import com.alinesno.infra.smart.scene.entity.LongTextTaskEntity;
+
+import java.util.List;
+
+/**
+ * 描述： 通用助手任务
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ILongTextTaskService extends IBaseService<LongTextTaskEntity> {
+
+
+    /**
+     * 分页查询
+     * @param page
+     * @param query
+     * @return
+     */
+    List<LongTextTaskEntity> pagerListByPage(DatatablesPageBean page, PermissionQuery query , Long sceneId);
+
+    /**
+     * 获取任务进度
+     * @param taskId
+     * @return
+     */
+    TaskProgressDto getProgress(Long taskId);
+}
