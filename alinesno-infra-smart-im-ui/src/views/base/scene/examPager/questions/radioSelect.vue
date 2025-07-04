2d16b2cbd3ff34121a4408442148e3ff654e9c47
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:14 +0800

    feat:优化单选组件，支持编辑状态和选项选中逻辑更新

--- alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/radioSelect.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/radioSelect.vue
@@ -1,5 +1,5 @@
 <template>
-    <questionContainer :question="question">
+    <questionContainer :question="question" :isQuestionEdit="props.isQuestionEdit">
         <div class="radio-question-wrapper"> 
 
             <div v-for="(item, index) in props.question.answers" 
@@ -7,12 +7,20 @@
                 :class="{ 'selected': selectedValue === index}"
                 @click="haddleSelectItem(item , index)"
                 class="radio-item">
-                    <input type="radio" :value="item" class="radio-item-input"> 
+                    <input type="radio" 
+                        :value="item" 
+                        :name="`radio-${props.question.id}`"
+                        :checked="item.selected" 
+                        @click.stop
+                        class="radio-item-input"> 
                     <div class="radio-item-content">
                         <label>{{ item.label }}</label>
-                        <EditableText style="width:100%" v-model="item.content" />
+                        <EditableText 
+                            style="width:100%" 
+                            :isQuestionEdit="props.isQuestionEdit"
+                            v-model="item.content" />
                     </div>
-                    <el-button v-if="selectedValue === index" type="text" @click="haddleDeleteAnswerItem(index)" class="delete-button">
+                    <el-button v-if="selectedValue === index && props.isQuestionEdit" type="text" @click="haddleDeleteAnswerItem(index)" class="delete-button">
                         删除
                     </el-button>
             </div>
@@ -34,15 +42,55 @@ const props = defineProps({
     question: {
         type: Object,
         required: true
+    }, 
+    isQuestionEdit: {
+        type: Boolean,
+        default: true,
+        required: false
     }
 })
 
 // 使用v-model绑定选中值
 const selectedValue = ref(-1)
 
+// 处理单选变化
+const handleRadioChange = (selectedItem) => {
+    // 更新所有选项的选中状态
+    props.question.answers.forEach(item => {
+        item.selected = (item.id === selectedItem.id)
+    })
+    
+    emitUpdatedQuestion()
+}
+
 // 处理选择项
-const haddleSelectItem = (item , index) => {
+const haddleSelectItem = (selectedItem  , index) => {
     selectedValue.value = index 
+
+     // 需要在非编辑状态下选择才会生效
+     if(!props.isQuestionEdit){
+        // 更新所有选项的选中状态
+        props.question.answers.forEach(item => {
+            console.log('item = ' + JSON.stringify(item))
+            console.log('selectItem = ' + JSON.stringify(selectedItem))
+            item.selected = (item.label === selectedItem.label)
+        })
+        
+        emitUpdatedQuestion()
+     }
+
+}
+
+// 发出更新事件
+const emitUpdatedQuestion = () => {
+    // 准备要更新的问题数据
+    const updatedQuestion = {
+        ...props.question,
+        answers: [...props.question.answers]
+    }
+    
+    // 触发更新事件
+    emit('update-handleUpdateQuestion', updatedQuestion)
 }
 
 // 删除问题指定答案
@@ -99,12 +147,17 @@ const haddleDeleteAnswerItem = async (index) => {
     }
 
     &.selected { 
-        border: 1px solid #2378ff;
-        font-weight: bold;
+        // border: 1px solid #2378ff;
+        // font-weight: bold;
+        border-color: #2378ff;
+        background-color: #f0f7ff;
+        box-shadow: 0 0 0 1px #2378ff;
     }
 
     &:hover { 
-        border: 1px solid #2378ff;
+        // border: 1px solid #2378ff;
+        // box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.07);
+        border-color: #2378ff;
         box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.07);
     }
 }
