903427ad7f636a202fe4d350e8c2e25880763ec0
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:11 +0800

    feat:创建IGeneralAgentTemplateGroupMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/mapper/IGeneralAgentTemplateGroupMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTemplateGroupEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface IGeneralAgentTemplateGroupMapper extends IBaseMapper<GeneralAgentTemplateGroupEntity> {
+}
