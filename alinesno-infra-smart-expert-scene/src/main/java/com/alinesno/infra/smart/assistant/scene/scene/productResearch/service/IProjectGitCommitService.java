533e95372f4fd0a01ab13b1cc4ed5586e8140adb
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:00 +0800

    更新:alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectGitCommitService.java

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/IProjectGitCommitService.java
+++
@@ -1,8 +0,0 @@
-package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.scene.entity.ProjectGitCommitEntity;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IProjectGitCommitService extends IBaseService<ProjectGitCommitEntity> {
-}    
