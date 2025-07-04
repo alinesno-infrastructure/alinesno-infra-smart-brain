ceaa372e6e442f6f06a735c5aaf0b1a257d9b7a4
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:04 +0800

    feat:创建PPT编辑页面，包含功能列表和智能AI PPT查看器组件

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/pptCreation/pptEditor.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="exam-pagercontainer">
+
+    <el-container style="height:calc(100vh - 40px );background-color: #fff;">
+
+      <el-aside width="80px" class="exam-pager-aside">
+        <FunctionList />
+      </el-aside>
+
+      <el-main class="exam-pager-main">
+        <SmartAIPPTViewer pptId="12345" @loaded="handleIframeLoad" @error="handleIframeError" />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+import FunctionList from './functionList'
+import SmartAIPPTViewer from './components/pptGenerator'
+const handleIframeLoad = () => {
+  console.log('iframe加载完成')
+}
+
+const handleIframeError = (error) => {
+  console.error('iframe加载出错', error)
+}
+</script>
+
+<style lang="scss" scoped>
+.exam-pager-card-container {
+  display: flex;
+  flex-grow: 1;
+  border-radius: 8px;
+}
+
+.exam-pager-card {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  overflow: hidden;
+  // padding: 12px 12px 16px;
+  border: 1px solid rgba(6, 7, 9, 0.1);
+  border-radius: 8px;
+  background-color: #fff;
+  cursor: pointer;
+  gap: 10px;
+  padding-bottom:10px;
+  transition: box-shadow 0.3s;
+
+  &:hover {
+    box-shadow: var(--coz-shadow-large);
+  }
+
+  .exam-pager-image-container {
+    position: relative;
+    width: 100%;
+    height: 10px;
+    // border-radius: 8px;
+    overflow: hidden;
+
+    .exam-pager-card-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+  }
+
+  .exam-pager-card-content {
+    margin-top: 10px;
+    padding: 0 4px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    width: calc(100%);
+    padding-left: 10px;
+
+    .scene-header {
+      display: flex;
+      align-items: flex-start;
+      gap: 8px;
+      margin: 8px 0px;
+      margin-top: 0px;
+      padding-left: 0px;
+      flex-direction: column;
+
+      .scene-title {
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 22px;
+        color: var(--coz-fg-primary);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      .scene-tag {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        padding: 2px 4px;
+        border-radius: 4px;
+        background-color: var(--coz-tag-primary);
+        color: var(--coz-fg-primary);
+        font-weight: 500;
+        font-size: 12px;
+        line-height: 16px;
+        min-height: 20px;
+        white-space: nowrap;
+        user-select: none;
+
+        svg {
+          width: 1em;
+          height: 1em;
+          fill: currentColor;
+        }
+
+        span {
+          margin-left: 2px;
+        }
+      }
+    }
+
+    .scene-author-info {
+      display: flex;
+      align-items: center;
+      gap: 4px;
+      margin-top: 4px;
+
+      .scene-avatar {
+        width: 14px;
+        height: 14px;
+        border-radius: 50%;
+      }
+
+      .scene-name,
+      .scene-username {
+        font-weight: 500;
+        font-size: 12px;
+        line-height: 16px;
+        color: var(--coz-fg-primary);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+
+    .scene-description {
+      margin-top: 8px;
+      margin-bottom: 8px;
+      font-size: 14px;
+      line-height: 20px;
+      color: var(--coz-fg-secondary);
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      height: 40px;
+      overflow: hidden;
+      display: -webkit-box;
+      display: -moz-box;
+      display: box;
+      -webkit-box-orient: vertical;
+      -moz-box-orient: vertical;
+      box-orient: vertical;
+      -webkit-line-clamp: 2;
+      -moz-line-clamp: 2;
+      line-clamp: 2;
+    }
+
+    .scene-footer {
+      margin-top: 4px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .scene-price {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 22px;
+        color: var(--coz-fg-primary);
+      }
+
+      .scene-stats {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        font-size: 12px;
+        line-height: 16px;
+        color: var(--coz-fg-secondary);
+      }
+    }
+  }
+}
+</style>
