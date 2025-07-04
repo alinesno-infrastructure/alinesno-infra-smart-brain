bac366438c57aab71603f067f8e2d3f3638c0bb6
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:16 +0800

    feat:定义TextRerankRequest类，包含文本重排序请求的属性和注释

--- alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/dto/TextRerankRequest.java
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/dto/TextRerankRequest.java
@@ -4,22 +4,36 @@ import lombok.Data;
 
 import java.util.List;
 
+/**
+ * 文本重排序请求类
+ * 用于表示文本重排序任务的请求数据
+ */
 @Data
 public class TextRerankRequest {
 
+    /**
+     * 模型名称，用于指定执行重排序任务所使用的模型
+     */
     private String model;
-    private Input input;
-    private Parameters parameters;
 
-    @Data
-    public static class Input {
-        private String query;
-        private List<String> documents;
-    }
+    /**
+     * 查询文本，是用户输入的搜索查询或问题
+     */
+    private String query;
 
-    @Data
-    public static class Parameters {
-        private boolean return_documents;
-        private int top_n;
-    }
+    /**
+     * 文档列表，包含待重排序的文档集合
+     */
+    private List<String> documents;
+
+    /**
+     * 是否返回文档内容，指示是否在结果中包含重排序后文档的完整内容
+     */
+    private boolean return_documents;
+
+    /**
+     * 返回的文档数量，指定重排序后返回的文档数目
+     */
+    private int top_n;
+
 }
