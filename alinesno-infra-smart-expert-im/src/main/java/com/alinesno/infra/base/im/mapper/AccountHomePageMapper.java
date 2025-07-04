db8f51bbe90aaec9529523a25c33fdc2fdde93f1
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:08 +0800

    feat: 创建AccountHomePageMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/AccountHomePageMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.AccountHomePageEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AccountHomePageMapper extends IBaseMapper<AccountHomePageEntity> {
+}
