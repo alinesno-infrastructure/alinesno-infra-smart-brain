9ff76bdef1120f7f28d22db6ce9b11d534457190
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:04 +0800

    feat:创建考试提交信息DTO类ExamSubmissionDto，包含考试ID、姓名、试卷代码和考生ID字段

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/dto/ExamSubmissionDto.java
@@ -0,0 +1,38 @@
+package com.alinesno.infra.smart.assistant.scene.common.examPaper.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 考试提交信息 DTO
+ */
+@Data
+@ToString
+public class ExamSubmissionDto {
+
+    /**
+     * 考试ID，不能为空
+     */
+    @NotBlank(message = "考试ID不能为空")
+    private String examId;
+
+    /**
+     * 考生姓名，不能为空
+     */
+    @NotBlank(message = "考生姓名不能为空")
+    private String name;
+
+    /**
+     * 试卷代码，不能为空
+     */
+    @NotBlank(message = "试卷代码不能为空")
+    private String paperCode;
+
+    /**
+     * 考生ID，不能为空
+     */
+    @NotBlank(message = "考生ID不能为空")
+    private String examineeId;
+
+}
