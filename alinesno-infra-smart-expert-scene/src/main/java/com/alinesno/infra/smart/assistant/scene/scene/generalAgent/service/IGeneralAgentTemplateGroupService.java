b659f81baa66baf20bd932022429563a149c365c
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:07 +0800

    feat:创建通用代理模板组服务接口IGeneralAgentTemplateGroupService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/service/IGeneralAgentTemplateGroupService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.service;
+
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTemplateGroupEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IGeneralAgentTemplateGroupService extends IBaseService<GeneralAgentTemplateGroupEntity> {
+
+}
