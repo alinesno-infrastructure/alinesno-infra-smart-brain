021c06552c276f4002ecf219064b5f7e25cd390b
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:07 +0800

    feat:创建文档审核任务Mapper接口

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewTaskMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DocReviewTaskEntity;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTaskEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 文档审核任务实体对象
+ */
+@Mapper
+public interface DocReviewTaskMapper extends IBaseMapper<DocReviewTaskEntity> {
+}    
