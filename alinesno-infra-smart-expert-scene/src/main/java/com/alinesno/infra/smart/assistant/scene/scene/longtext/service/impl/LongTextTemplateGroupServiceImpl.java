99d5f1058bd438a98ce458187c3c70ccf4dbe2ef
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:05 +0800

    feat:创建LongTextTemplateGroupServiceImpl类，实现通用助手任务服务

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/service/impl/LongTextTemplateGroupServiceImpl.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.mapper.LongTextTemplateGroupMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.service.ILongTextTemplateGroupService;
+import com.alinesno.infra.smart.scene.entity.LongTextTemplateGroupEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 通用助手任务服务实现类
+ */
+@Slf4j
+@Service
+public class LongTextTemplateGroupServiceImpl extends IBaseServiceImpl<LongTextTemplateGroupEntity, LongTextTemplateGroupMapper> implements ILongTextTemplateGroupService {
+
+
+}
