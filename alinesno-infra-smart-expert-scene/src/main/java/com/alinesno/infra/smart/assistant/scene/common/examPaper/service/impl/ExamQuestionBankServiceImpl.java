5b263353e10cb69efd25e70892dd6113d7fd2e28
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:11 +0800

    feat:创建试卷场景服务实现类ExamQuestionBankServiceImpl

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/service/impl/ExamQuestionBankServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.common.examPaper.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.common.examPaper.mapper.ExamQuestionBankMapper;
+import com.alinesno.infra.smart.assistant.scene.common.examPaper.service.IExamQuestionBankService;
+import com.alinesno.infra.smart.scene.entity.ExamQuestionBankEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 试卷场景服务实现类
+ */
+@Slf4j
+@Service
+public class ExamQuestionBankServiceImpl extends IBaseServiceImpl<ExamQuestionBankEntity , ExamQuestionBankMapper> implements IExamQuestionBankService {
+
+}
