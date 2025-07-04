a31bb1f2f0dccee534c42a7668dbc2424f7d1bbe
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:16 +0800

    fix:修改ChatContentEditDto新增场景id和任务id非空校验

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/ChatContentEditDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/dto/ChatContentEditDto.java
@@ -1,6 +1,7 @@
 package com.alinesno.infra.smart.scene.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -11,8 +12,11 @@ import java.util.List;
 @Data
 public class ChatContentEditDto {
 
-    @Min(value = 1, message = "场景id不能为空")
-    private long sceneId ; // 所属场景
+    @NotNull(message = "场景id不能为空")
+    private Long sceneId ; // 所属场景
+
+   @NotNull(message = "任务id不能为空")
+    private Long taskId ;
 
     private List<String> chapters; // 章节id
 
