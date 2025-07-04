64bea32c1c03fce836a897a76dd8e3ae6dbca3cd
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:10 +0800

    feat:创建ExamPagerSceneMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/mapper/ExamPagerSceneMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.common.examPaper.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ExamPagerSceneMapper extends IBaseMapper<ExamPagerSceneEntity> {
+}
