0aa05796996249e755fdd0c00dad5d58b721d9d7
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:16 +0800

    feat:优化文章提示生成逻辑，增加配置参数处理

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/prompt/ArticlePromptHandle.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/prompt/ArticlePromptHandle.java
@@ -1,42 +1,86 @@
 package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.prompt;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.dto.ArticleGeneratorDTO;
+import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.dto.ImageGeneratorDTO;
+import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.dto.ReWriterArticleGeneratorDTO;
 import com.alinesno.infra.smart.scene.entity.ArticleTemplateEntity;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
 public class ArticlePromptHandle {
 
     public static String generatorPrompt(ArticleGeneratorDTO dto, ArticleTemplateEntity templateEntity) {
 
-        String templatePrompt = "";
+        JSONObject articleConfig = dto.getArticleConfig();
 
-        if (templateEntity != null) {
-            templatePrompt = String.format("""
-                            %s
-                            ##限制:
-                            - 只返回文章内容，其它不相关内容不要返回
-                            - 严格按照模板格式返回
-                             """,
-                    templateEntity.getPrompt()
-            );
+        StringBuilder otherText = new StringBuilder(); // 将articleConfig转换成map，如果存在，则拼接成Key:value值说明
+        if(articleConfig != null){
+            for (String key : articleConfig.keySet()) {
+                Object value = articleConfig.get(key);
+                if (value != null && StringUtils.isNoneBlank(value+"")) {
+                    otherText.append(key).append(":").append(value).append("\r\n");
+                }
+            }
         }
 
-        String chatPrompt = """
-                ## 其它说明
-                写作类型=%s
-                写作类型=%s
-                受众群体=%s
-                文章场景=%s
-                语句表达=%s
-                """;
+        StringBuilder prompt = new StringBuilder() ;
+        String systemPrompt = templateEntity.getSystemPrompt() ;
 
-        String chatPromptText = String.format(chatPrompt,
-                dto.getPromptText(),
-                dto.getPptConfig().getWritingType(),
-                dto.getPptConfig().getAudience(),
-                dto.getPptConfig().getScenario(),
-                dto.getPptConfig().getTone());
+        if(StringUtils.isNoneBlank(systemPrompt)){
+            prompt.append(systemPrompt).append("\r\n");
+        }
 
-        return templatePrompt + chatPromptText;
+        if(StringUtils.isNoneBlank(dto.getPromptText())){
+            prompt.append(dto.getPromptText()).append("\r\n");
+        }
+
+        if (StringUtils.isNoneBlank(otherText)) {
+            prompt.append("其它要求:")
+                    .append(otherText);
+        }
+
+        return prompt.toString() ;
     }
 
+    /**
+     * 重写文章
+     *
+     * @param dto
+     * @param content
+     * @return
+     */
+    public static String generatorReWriterPrompt(ReWriterArticleGeneratorDTO dto, String content) {
+
+        String prompt = """
+                ##角色:你是一名文章修改和润色专家，请将下面的文章内容按要求重新润色，并返回文章内容。
+                ##原文章内容:%s
+                ##修改或者润色要求:%s
+                ##限制：
+                - 只返回润色之后的文章内容
+                - 返回markdown格式
+                """;
+
+        return String.format(prompt, content, dto.getPromptText());
+    }
+
+    /**
+     * 生成图片
+     * @param dto
+     * @param content
+     * @return
+     */
+    public static String generatorImagePrompt(ImageGeneratorDTO dto, String content) {
+        String prompt = """
+                ##角色:你作为一名AI图片生成模型，请将下面的内容生成图片，并返回图片。
+                ##内容:%s
+                ##要求:%s
+                """;
+        return String.format(prompt, content, dto.getPrompt());
+    }
 }
