3316c1707905ce0e5941e7979f61c7289bd1d6f5
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:17 +0800

    feat:在singleLine.vue中添加答案变化处理逻辑和样式调整

--- alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/singleLine.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/singleLine.vue
@@ -4,7 +4,8 @@
             <el-input 
                 v-model="answer" 
                 size="large"
-                placeholder="请输入内容"></el-input>
+                placeholder="请输入内容"
+                @change="handleAnswerChange"></el-input>
         </div>
     </questionContainer>
 </template>
@@ -13,6 +14,8 @@
 
 import { defineProps , ref } from 'vue'
 import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
+
+const emit = defineEmits(['update-handleUpdateQuestion'])
 
 const answer = ref('')
 
@@ -23,8 +26,35 @@ const props = defineProps({
     }
 })
 
+// 初始化答案
+if (props.question.answer && props.question.answer.length > 0) {
+    answer.value = props.question.answer[0] || ''
+}
+
+// 监听答案变化
+watch(answer, (newVal) => {
+    handleAnswerChange()
+})
+
+// 处理答案变化
+const handleAnswerChange = () => {
+    const updatedQuestion = {
+        ...props.question,
+        answer: [answer.value], // 填空题答案以数组形式存储，即使只有一个空
+        userAnswer: answer.value
+    }
+
+    console.log("更新后的question: " + JSON.stringify(updatedQuestion))
+
+    console.log("props.question: " + JSON.stringify(props.question))
+    emit('update-handleUpdateQuestion', updatedQuestion)
+}
+
+
 </script>
 
 <style lang="scss" scoped>
-
+.singleLine-question-wrapper {
+    padding: 10px 0;
+}
 </style>
