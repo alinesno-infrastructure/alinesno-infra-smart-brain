47c11e4ca1bda0aab2f2723a5b36b42284262df6
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:15 +0800

    feat:实现CvsReaderServiceImpl类的CSV文件读取功能

--- alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/CvsReaderServiceImpl.java
+++ alinesno-infra-smart-expert-dataset/src/main/java/com/alinesno/infra/base/search/service/reader/CvsReaderServiceImpl.java
@@ -2,8 +2,16 @@ package com.alinesno.infra.base.search.service.reader;
 
 import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.im.dto.FileAttachmentDto;
+import com.opencsv.CSVReader;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * CvsReaderServiceImpl 类是 CSV 附件读取服务的具体实现，
@@ -21,8 +29,33 @@ public class CvsReaderServiceImpl extends BaseReaderServiceImpl {
      * @param uploadData
      * @return 返回读取到的 CSV 附件内容，若读取失败或无内容则返回 null。
      */
+    @SneakyThrows
     @Override
     public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
-        return null;
+
+        File file ;
+        if(attachmentDto.getFile() != null){
+            file = attachmentDto.getFile() ;
+        }else{
+            file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+        }
+
+        if (file == null || !file.exists()) {
+            return null;
+        }
+
+        try (CSVReader reader = new CSVReader(new FileReader(file, StandardCharsets.UTF_8))) {
+            StringBuilder content = new StringBuilder();
+            String[] nextLine;
+
+            while ((nextLine = reader.readNext()) != null) {
+                content.append(Arrays.stream(nextLine)
+                                .map(s -> s == null ? "" : s)
+                                .collect(Collectors.joining(" | ")))
+                        .append("\n");
+            }
+
+            return content.toString();
+        }
     }
 }
