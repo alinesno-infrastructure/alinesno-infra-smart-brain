bfa57f687c116e74ddd55bd7811fb2df5a1168dc
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:12 +0800

    feat:添加多行输入框的答题逻辑和样式调整

--- alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/multiLine.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/examPager/questions/multiLine.vue
@@ -1,19 +1,20 @@
 <template>
     <questionContainer :question="question">
         <div class="multiLine-question-wrapper">
-            <!-- {{ props.question }} -->
-            <el-input v-model="answer" type="textarea" :rows="6" resize="none" placeholder="请输入内容" />
+            <el-input 
+                v-model="answer" 
+                type="textarea" 
+                :rows="6" 
+                resize="none" 
+                placeholder="请输入内容"
+                @change="handleAnswerChange" />
         </div>
     </questionContainer>
 </template>
 
 <script setup>
-
-import { defineProps , ref } from 'vue'
-
+import { defineProps, ref, watch } from 'vue'
 import questionContainer from '@/views/base/scene/examPager/common/questionContainer.vue'
-
-const answer = ref('')
 
 const props = defineProps({
     question: {
@@ -22,8 +23,37 @@ const props = defineProps({
     }
 })
 
+const emit = defineEmits(['update-handleUpdateQuestion'])
+
+const answer = ref('')
+
+// 初始化答案
+if (props.question.answer && props.question.answer.length > 0) {
+    answer.value = props.question.answer[0] || ''
+}
+
+// 监听答案变化
+watch(answer, (newVal) => {
+    handleAnswerChange()
+})
+
+// 处理答案变化
+const handleAnswerChange = () => {
+    const updatedQuestion = {
+        ...props.question,
+        answer: [answer.value], // 多行填空题答案也以数组形式存储
+        userAnswer: answer.value
+    }
+    emit('update-handleUpdateQuestion', updatedQuestion)
+}
 </script>
 
 <style lang="scss" scoped>
-
+.multiLine-question-wrapper {
+    padding: 10px 0;
+    
+    .el-textarea {
+        width: 100%;
+    }
+}
 </style>
