c83aeb20c9a3086951d78a2609eca6e2906b4df2
梁明树(Nina) <ninaAip@gmail.com>
2025-07-05 06:29:11 +0800

    feat: 添加可编辑标题组件EditableTitle.vue，支持点击编辑和保存功能。

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/longText/components/EditableTitle.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="editable-title-container">
+    <div v-if="!isEditing" class="title-display" @click="startEditing">
+      <el-popover
+        placement="top"
+        :width="Math.min(500, title.length * 10 + 50)"
+        trigger="hover"
+        :content="title"
+        :disabled="title.length <= 30"
+      >
+        <template #reference>
+          <div class="title-text">
+            {{ displayTitle }}
+          </div>
+        </template>
+      </el-popover>
+    </div>
+    
+    <el-input
+      v-else
+      ref="titleInput"
+      v-model="editTitle"
+      @blur="saveTitle"
+      @keyup.enter="saveTitle"
+      size="large"
+      class="title-input"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, nextTick } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:title'])
+
+const isEditing = ref(false)
+const editTitle = ref('')
+const titleInput = ref(null)
+
+const displayTitle = computed(() => {
+  return props.title.length > 20 ? props.title.substring(0, 20) + '...' : props.title
+})
+
+const startEditing = () => {
+  isEditing.value = true
+  editTitle.value = props.title
+  nextTick(() => {
+    titleInput.value?.focus()
+  })
+}
+
+const saveTitle = () => {
+  if (editTitle.value.trim() !== '') {
+    emit('update:title', editTitle.value.trim())
+  }
+  isEditing.value = false
+}
+</script>
+
+<style lang="scss" scoped>
+.editable-title-container {
+  margin: 0px 0;
+  width: 100% ;
+
+  .title-display {
+    cursor: pointer;
+    font-size: 17px;
+    font-weight: bold;
+    padding: 5px;
+    height: 40px;
+    line-height: 28px;
+    border-radius: 4px;
+    background-color: #f5f5f5;
+
+    &:hover {
+      background-color: #f5f5f5;
+    }
+  }
+
+  .title-input {
+    font-size: 17px;
+    font-weight: bold;
+
+    :deep(.el-input__inner) {
+      font-size: 17px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
