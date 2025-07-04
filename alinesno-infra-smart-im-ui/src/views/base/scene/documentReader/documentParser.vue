1d4adee96b0e84ec96f0fa10988cb0512a1b2c16
李伟(Michael) <michaelAip@gmail.com>
2025-07-05 06:29:14 +0800

    refactor:重构文档解析页面布局，优化界面结构和样式

--- alinesno-infra-smart-im-ui/src/views/base/scene/documentReader/documentParser.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReader/documentParser.vue
@@ -1,87 +1,64 @@
 <template>
-    <div class="app-container docuemnt-parser-container">
-        <el-row>
-            <el-col :span="12">
-                <div style="font-size: 16px;font-weight: bold;display: flex;align-items: center;gap:20px; justify-content: flex-start;">
-                    <el-button type="primary" text bg @click="onClickLeft()">
-                        <i class="fa-solid fa-arrow-left"></i>
-                    </el-button>
-                    <span>
-                        {{ currentSceneInfo?.documentName }}
-                    </span>
-                </div>
-                <div class="document-wrapper" v-loading="loadingDocument" >
-                    <el-scrollbar class="scrollable-area" style="height: calc(100vh - 140px);margin-top:20px; padding-right:0px">
-                        <div id="fileshow" class="content" style="width: 100%;height: 100vh;border-radius: 5px;" ref="contentRef"></div>
-                    </el-scrollbar>
-                </div>
-            </el-col>
-            <el-col :span="12" style="padding-left: 20px;border-left: 1px solid #262727;">
-                <div>
-                    <!--
-                    <el-steps class="mb-4" style="width: 100%;cursor: pointer;" :space="200" :active="currentActive" simple >
-                        <el-step @click="handleStepClick(1)" title="合同概览" :icon="Edit" />
-                        <el-step @click="handleStepClick(2)" title="审查清单" :icon="UploadFilled" />
-                        <el-step @click="handleStepClick(3)" title="审查结果" :icon="Picture" />
-                    </el-steps>
-                    -->
 
-                    <!-- <RoleChatPanel ref="roleChatPanelRef" /> -->
+    <div class="ppt-pager-container">
 
-                    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-                        <el-tab-pane name="first">
-                            <template #label>
+        <el-container style="height:calc(100vh - 40px );background-color: #fff;">
+
+            <el-aside width="280px" class="ppt-pager-aside">
+                <FunctionList />
+            </el-aside>
+
+            <el-main class="ppt-pager-main">
+                <div class="docuemnt-parser-container" style="padding: 10px">
+                    <el-row>
+                        <el-col :span="12">
+                            <div style="font-size: 16px;font-weight: bold;display: flex;align-items: center;gap:20px; justify-content: flex-start;">
+                                <el-button type="primary" text bg @click="onClickLeft()">
+                                    <i class="fa-solid fa-arrow-left"></i>
+                                </el-button>
                                 <span>
-                                    <i class="fa-solid fa-comment-dots"></i>&nbsp;阅读对话
+                                    {{ currentSceneInfo?.documentName }} 阅读分析
                                 </span>
-                            </template>
-                            <div>
-                                <RoleChatPanel ref="roleChatPanelRef" />
                             </div>
-                        </el-tab-pane>
-                        
-                        <!-- 
-                        <el-tab-pane name="second">
-                            <template #label>
-                                <span>
-                                    <i class="fa-solid fa-user-pen"></i> &nbsp;阅读笔记
-                                </span>
-                            </template>
-                            阅读笔记
-                        </el-tab-pane>
-                        <el-tab-pane label="相关案例" name="third">
-                            <template #label>
-                                <span>
-                                    <i class="fa-solid fa-clapperboard"></i>&nbsp;相关案例
-                                </span>
-                            </template>
-                            相关案例
-                        </el-tab-pane>
-                        <el-tab-pane name="four">
-                            <template #label>
-                                <span>
-                                    <i class="fa-solid fa-file-word"></i>&nbsp;相关文档
-                                </span>
-                            </template>
-                            相关文档
-                        </el-tab-pane> 
-                        -->
+                            <div class="document-wrapper" v-loading="loadingDocument">
+                                <el-scrollbar class="scrollable-area" style="height: calc(-140px + 100vh);margin-top: 20px;padding-right: 0px;background: #fafafa;border-radius: 5px;">
+                                    <div id="fileshow" class="content" style="width: 100%;height:auto ;border-radius: 5px;" ref="contentRef"></div>
+                                </el-scrollbar>
+                            </div>
+                        </el-col>
+                        <el-col :span="12" style="padding-left: 20px;">
+                            <!-- 配置和操作toolbar-->
+                            <div class="toolbar-container">
+                                <div class="reader-toolbar">
+                                    <div>
+                                        阅读角色
+                                    </div>
+                                    <div class="avatar-group">
+                                        <el-avatar 
+                                            v-for="item in avatarGroup" 
+                                            :key="item.id"
+                                            :size="30" 
+                                            :src="item.avatar">
+                                        </el-avatar>
+                                    </div>
+                                </div>
+                                <div>
+                                    <el-button type="primary" text bg @click="getDocxContent">
+                                        <i class="fa-solid fa-arrows-rotate"></i>&nbsp;刷新
+                                    </el-button>
+                                </div>
+                            </div>
 
-                    </el-tabs> 
+                            <!-- 聊天面板-->
+                            <div>
+                                <ReaderTabPanel ref="readerTabPanelRef" />
+                            </div>
+                        </el-col>
+                    </el-row>
+
                 </div>
-
-                <!-- 
-                <div>
-                    <el-collapse-transition>
-                        <DocumentOverview v-if="currentActive == 1" />
-                        <ReviewCheckList v-if="currentActive == 2" />
-                        <ReviewResult v-if="currentActive == 3" />
-                    </el-collapse-transition>
-                </div> 
-                -->
-
-            </el-col>
-        </el-row>
+            </el-main>
+        </el-container>
     </div>
 </template>
 
@@ -91,11 +68,8 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 const route = useRoute()
 
-// import DocumentOverview from '@/views/base/scene/documentReview/documentOverview.vue'
-// import ReviewCheckList  from '@/views/base/scene/documentReview/reviewChecklist.vue'
-// import ReviewResult  from '@/views/base/scene/documentReview/reviewResult.vue'
-
-import RoleChatPanel from '@/views/base/scene/common/chatPanel';
+import FunctionList from './functionList'
+import ReaderTabPanel from './readerTab'
 
 import axios from "axios";
 import { getScene } from '@/api/base/im/scene/documentReader';
@@ -106,59 +80,60 @@ const sceneId = ref(route.query.sceneId)
 const currentSceneInfo = ref({});
 const loadingDocument = ref(true)
 const contentRef = ref(null)
-const currentActive = ref(1)
+const readerTabPanelRef = ref(null)
 
-const activeName = ref('first')
-const roleChatPanelRef = ref(null)
+const avatarGroup = ref([
+    {
+        id: 1,
+        name: '张三',
+        avatar: 'http://staticok.oss-cn-hangzhou.aliyuncs.com/avatar-share/thumbnail-6046836d-7766-4bfd-a93d-85fd52d2b0e4.webp' , 
+    } , 
+    {
+        id: 2,
+        name: '李四',
+        avatar: 'http://alinesno-infra-smart-im-ui.beta.base.infra.linesno.com/prod-api/v1/api/infra/base/im/chat/displayImage/1851917403521929217' , 
+    } , 
+    {
+        id: 3,
+        name: '王五',
+        avatar: 'http://staticok.oss-cn-hangzhou.aliyuncs.com/avatar-share/thumbnail-d809e295-17bb-4247-9d3e-daafedefdf6e.webp' , 
+    }
+])
 
-const handleStepClick = (index) => {
-    console.log('index = ' + index);
-    currentActive.value = index
-}
-
-const getDocxContent = async() => {
+const getDocxContent = async () => {
     try {
         loadingDocument.value = true;
 
         const response = await getPreviewDocx(sceneId.value);
 
         console.log('response.data: ' + response); // 打印数据内容
-        renderAsync(response, contentRef.value , null  , {
-            inWrapper: true 
+        renderAsync(response, contentRef.value, null, {
+            inWrapper: true
         });
 
-        loadingDocument.value = false ;
+        loadingDocument.value = false;
 
         console.log('文档渲染成功');
     } catch (error) {
         console.error('获取或渲染文档失败', error);
     }
 }
- 
+
 const onClickLeft = () => {
-  // 判断历史记录中是否有回退
-  if (history.state?.back) {
-    router.back()
-  } else {
-    router.push('/')
-  }
+    // 判断历史记录中是否有回退
+    if (history.state?.back) {
+        router.back()
+    } else {
+        router.push('/')
+    }
 }
 
 const handleGetScene = () => {
     getScene(sceneId.value).then(res => {
         currentSceneInfo.value = res.data;
 
-        nextTick(() => {
-            roleChatPanelRef.value.openChatBoxWithRole(currentSceneInfo.value.summaryAgentEngineer) ; 
-        })
-
     })
 }
-
-// onMounted(() => {
-//     handleGetScene();
-//     getDocxContent()
-// })
 
 nextTick(() => {
     handleGetScene();
@@ -169,4 +144,179 @@ nextTick(() => {
 
 <style lang="scss" scoped>
 @import '@/assets/styles/document-review.scss';
+.ppt-pager-container {
+  background: #fff;
+  height: calc(100vh - 60px);
+
+  .review-footer {
+    padding: 10px;
+    font-size: 14px;
+    background: #fafafa;
+    border-radius: 8px;
+    text-align: left;
+    color: #555;
+    margin-top: 10px;
+}
+
+  .main-content {
+    display: flex;
+    flex-direction: column;
+    padding-top: calc(1vh);
+    margin: auto;
+    padding-left: 20px;
+    padding-right: 20px;
+
+    .example-result-section {
+      padding: 12px;
+      border-radius: 10px;
+      font-size: 14px;
+      text-align: left;
+      color: #585a73;
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      align-items: center;
+      gap: 10px;
+    }
+
+    .title-section {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+      align-items: flex-start;
+
+      .title {
+        color: #2C2C36;
+        font-weight: 600;
+        font-size: 28px;
+        margin-bottom: 10px;
+        line-height: 40px;
+      }
+
+      .description {
+        margin-top: 10px;
+        color: #8F91A8;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+      }
+    }
+
+    .input-button-section {
+      display: flex;
+      gap: 10px;
+      position: relative;
+      box-sizing: border-box;
+      width: 100%;
+      border-radius: 15px;
+      transition: 0.3s;
+      background: rgb(255, 255, 255);
+      padding: 10px !important;
+      border: 1px solid rgb(232, 234, 242);
+      margin-top: 30px;
+      margin-bottom: 10px;
+      align-items: flex-start;
+      flex-direction: column;
+
+      .input-box {
+        width: 100%;
+        height: 50px;
+        border: 0px !important;
+        margin-bottom: 0px;
+      }
+    }
+
+  }
+
+  .review-footer-message {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+    height: 36px;
+    padding: 12px 0px;
+    text-align: center;
+
+    .footer-message {
+      margin-bottom: 4px;
+      color: #C8CAD9;
+      font-size: 12px;
+      line-height: 12px;
+    }
+  }
+
+  .review-question-preview-title {
+    padding: 10px;
+    text-align: left;
+    font-weight: bold;
+    margin-left: 20px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    background: #fafafa;
+    color: #444;
+    font-size: 15px;
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .review-question-preview {
+    height: calc(100vh - 170px);
+    margin-left: 20px;
+    border-radius: 8px;
+    background: #fafafa;
+    border: 1px solid #e8eaf2;
+    overflow: hidden;
+  }
+
+}
+
+.ppt-pager-aside{
+  padding: 0px;
+  border-right: 1px solid #f2f3f7;
+  background: #fff;
+  margin-bottom: 0px;
+}
+
+.ppt-pager-main{
+  padding: 0px !important;
+}
+
+.pager-gen-result-panel{
+  margin-bottom:20px;
+  text-align: left;
+
+  .pager-container {
+    background-color: #fafafa;
+    margin: 10px 10px;
+    margin-right: 0px;
+    border-radius: 8px;
+    height: calc(100vh - 190px);
+    padding: 10px;
+    padding-left: 10px;
+    margin-left: 20px;
+    margin-bottom: 0px;
+  }
+}
+
+.toolbar-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .reader-toolbar {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+
+        .avatar-group{
+            display: flex;
+            align-items: center;
+            gap: 7px;
+        }
+    }
+
+}
 </style>
