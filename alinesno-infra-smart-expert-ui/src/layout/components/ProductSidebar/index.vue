791f11e054cc01b333a17302112bc8d6b1e76837
梁明树(Nina) <ninaAip@gmail.com>
2025-07-05 06:29:18 +0800

    feat:优化产品侧边栏菜单，添加图标和描述信息

--- alinesno-infra-smart-expert-ui/src/layout/components/ProductSidebar/index.vue
+++ alinesno-infra-smart-expert-ui/src/layout/components/ProductSidebar/index.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="siderbar">
     <el-menu default-active="1" class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
-      <el-menu-item index="1" @click="jumpTo">
+      <!-- <el-menu-item index="1" @click="jumpTo">
         <i class="fa-solid fa-computer"></i>
-      </el-menu-item>
+      </el-menu-item> -->
 
-      <el-tooltip effect="dark" :content="item.desc" v-for="item in menuItems" :key="item.id" placement="right">
-          <el-menu-item :index="item.id" @click="openServiceList(item.link)">
+      <!-- <el-tooltip effect="dark" :content="item.desc" v-for="item in menuItems" :key="item.id" placement="right"> -->
+          <el-menu-item 
+              :index="item.id" 
+              @click="openServiceList(item.link)" 
+              v-for="item in menuItems" 
+              :key="item.id" 
+              class="aip-menu-item">
             <i :class="item.icon"></i>
+            <span>
+              {{ item.desc }}
+            </span>
           </el-menu-item>
-      </el-tooltip>
+      <!-- </el-tooltip> -->
 
     </el-menu>
 
     <el-menu style="" class="el-menu-vertical acp-suggest" :collapse="isCollapse" @open="handleOpen" @close="handleClose">
-      <el-tooltip effect="dark" :content="item.desc" v-for="item in footerMenuItems" :key="item.id" placement="right">
-          <el-menu-item :index="item.id" @click="openServiceList(item.link)">
+      <!-- <el-tooltip effect="dark" :content="item.desc" v-for="item in footerMenuItems" :key="item.id" placement="right"> -->
+          <el-menu-item 
+            :index="item.id" 
+            v-for="item in footerMenuItems" 
+            :key="item.id" 
+            @click="openServiceList(item.link)" 
+            class="aip-menu-item">
             <i :class="item.icon"></i>
+            <span>
+              {{ item.desc }}
+            </span>
           </el-menu-item>
-      </el-tooltip>
+      <!-- </el-tooltip> -->
     </el-menu>
 
   </div>
@@ -35,21 +51,22 @@ const router = useRouter();
 // - 操作日志  /monitor/smart/assistant/apiRecord/index
 
 const footerMenuItems = ref([
-  { id: '13', icon: 'fa-solid fa-computer', link: '/smart/assistant/llmModel/index', desc: '模型管理'},
-  { id: '22', icon: 'fa-brands fa-wordpress',  link: '/monitor/smart/assistant/analyse/index', desc: '接口监控' },
+  { id: '13', icon: 'fa-solid fa-computer', link: '/smart/assistant/llmModel/index', desc: '模型'},
+  { id: '22', icon: 'fa-brands fa-wordpress',  link: '/monitor/smart/assistant/analyse/index', desc: '监控' },
 ])
 
 const menuItems = ref([
-  { id: '9', icon: 'fa-solid fa-ship', link: '/expert/smart/assistant/workplace/index', desc: '工作台管理' },
-  { id: '2', icon: 'fa-solid fa-masks-theater', link: '/expert/smart/assistant/role/index', desc: '专家管理' },
-  { id: '3', icon: 'fa-solid fa-file-pdf', link: '/expert/smart/assistant/roleCatalog/index', desc: '专家分类' },
-  { id: '4', icon: 'fa-solid fa-box', link: '/prompt/smart/assistant/channel/index', desc: '频道管理' },
-  { id: '5', icon: 'fa-solid fa-feather', link: '/prompt/smart/assistant/scene/index', desc: '场景列表' },
+  { id: '19', icon: 'fa-solid fa-computer', link: '/index', desc: '仪盘表' },
+  { id: '9', icon: 'fa-solid fa-ship', link: '/expert/smart/assistant/workplace/index', desc: '工作台' },
+  { id: '2', icon: 'fa-solid fa-masks-theater', link: '/expert/smart/assistant/role/index', desc: '智能体' },
+  { id: '3', icon: 'fa-solid fa-file-pdf', link: '/expert/smart/assistant/roleCatalog/index', desc: '类型' },
+  { id: '4', icon: 'fa-solid fa-box', link: '/prompt/smart/assistant/channel/index', desc: '频道' },
+  { id: '5', icon: 'fa-solid fa-feather', link: '/prompt/smart/assistant/scene/index', desc: '场景' },
 
-  { id: '18', icon: 'fa-solid fa-file-pdf', link: '/base/search/vectorData/index', desc: '数据集管理' },
-  { id: '14', icon: 'fa-brands fa-skype', link: '/base/search/catalog/index', desc: '数据集分类' },
+  { id: '18', icon: 'fa-solid fa-file-pdf', link: '/base/search/vectorData/index', desc: '数据集' },
+  { id: '14', icon: 'fa-brands fa-skype', link: '/base/search/catalog/index', desc: '分类' },
   
-  { id: '6', icon: 'fa-solid fa-screwdriver-wrench', link: '/template/smart/assistant/plugin/index', desc: '工具管理' },
+  { id: '6', icon: 'fa-solid fa-screwdriver-wrench', link: '/template/smart/assistant/plugin/index', desc: '工具' },
   // { id: '7', icon: 'fa-solid fa-paper-plane', link: '/template/smart/assistant/secret/index', desc: '密钥管理' },
 
 ]);
@@ -95,5 +112,22 @@ function openSmartService() {
   overflow: hidden;
   background-color: #fff;
   position: fixed;
+}
+
+.aip-menu-item {
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  line-height: 1.2rem;
+  padding-top: 0px;
+  margin: 5px;
+  border-radius: 10px;
+  justify-content: center;
+
+  span{
+    font-size:12px;
+    color: #888;
+  }
 }
 </style>
