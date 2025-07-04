9f92cc32aff8cdfff94b10836520430f8d7febc6
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:08 +0800

    feat:创建LongTextTemplateGroupMapper接口，用于操作长文本模板组数据

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/mapper/LongTextTemplateGroupMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.LongTextTemplateGroupEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LongTextTemplateGroupMapper extends IBaseMapper<LongTextTemplateGroupEntity> {
+}
