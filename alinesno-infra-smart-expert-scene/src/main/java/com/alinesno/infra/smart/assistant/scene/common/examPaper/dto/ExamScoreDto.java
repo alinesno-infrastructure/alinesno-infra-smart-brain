3d884df82e248ec180e1b41f9aa739d53596e194
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:07 +0800

    feat:创建考试成绩数据传输对象ExamScoreDto，包含基本属性和相关字段

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/dto/ExamScoreDto.java
@@ -0,0 +1,62 @@
+package com.alinesno.infra.smart.assistant.scene.common.examPaper.dto;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.common.facade.base.BaseDto;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+/**
+ * 考试成绩数据传输对象
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ExamScoreDto extends BaseDto {
+
+    private Long pagerId;
+
+    private Long sceneId;
+
+    private Long examInfoId;
+
+    private Long userId;
+
+    private String userName;
+
+    private String userCode;
+
+    private Integer score;
+
+    private Integer isPass;
+
+    private Integer usedTime;
+
+    private LocalDateTime submitTime;
+
+    private Long reviewerId;
+
+    private String reviewerName;
+
+    private LocalDateTime reviewTime;
+
+    private String remark;
+
+    private Integer isDeleted;
+
+    private Integer version;
+
+    private JSONObject answers; ;
+
+    private JSONArray questions ;
+
+    // 分析结果保存
+    private String analysisResult ;
+
+}
