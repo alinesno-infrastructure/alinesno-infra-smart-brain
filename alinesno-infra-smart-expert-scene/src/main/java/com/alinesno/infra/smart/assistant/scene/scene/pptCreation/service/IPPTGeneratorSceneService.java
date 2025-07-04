8cd73068b324563e79c7f862b661bda9775e86e3
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:17 +0800

    chore:清理导入依赖，优化PPT生成场景服务类

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/service/IPPTGeneratorSceneService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/service/IPPTGeneratorSceneService.java
@@ -2,15 +2,10 @@ package com.alinesno.infra.smart.assistant.scene.scene.pptCreation.service;
 
 
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperDTO;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamPaperUpdateDTO;
 import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
 import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
-import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
 import com.alinesno.infra.smart.scene.entity.PPTGenerateSceneEntity;
 
 import java.util.List;
