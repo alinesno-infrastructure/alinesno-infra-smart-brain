b0bb72605ffd8d8d262a00078a39daaaaaedb6eb
梁明树(Nina) <ninaAip@gmail.com>
2025-07-05 06:29:17 +0800

    feat:新增获取考生列表和试卷信息接口，优化导出试卷功能

--- alinesno-infra-smart-im-ui/src/api/base/im/scene/examPaperManager.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/examPaperManager.js
@@ -4,21 +4,42 @@ import { parseStrEmpty } from "@/utils/ruoyi";
 // 接口基础路径
 const BASE_URL = '/api/infra/smart/assistant/scene/examPagerManager';
 
-  /**
-   * 导出试卷为Word文档
-   * @param {number} pagerId 试卷ID
-   * @param {boolean} showAnswer 是否显示答案
-   * @returns {Promise} 返回文件下载流
-   */
+/**
+ * 获取考试的考生列表
+ */
+export function getExamineeList(examId) {
+  return request({
+    url: `${BASE_URL}/examineeList?examId=${parseStrEmpty(examId)}`,
+    method: 'get'
+  })
+}
+
+/**
+ * 获取到考试试卷信息
+ */
+export function getPaperInfo(pagerId) {
+  return request({
+    url: `${BASE_URL}/paperInfo?pagerId=${parseStrEmpty(pagerId)}`,
+    method: 'get'
+  })
+}
+
+
+/**
+ * 导出试卷为Word文档
+ * @param {number} pagerId 试卷ID
+ * @param {boolean} showAnswer 是否显示答案
+ * @returns {Promise} 返回文件下载流
+ */
 export function exportPaper(pagerId, showAnswer = false) {
-    return request({
-      url: `${BASE_URL}/export/${parseStrEmpty(pagerId)}`,
-      method: 'get',
-      params: { showAnswer },
-      responseType: 'blob' // 重要：指定响应类型为blob
-    })
-  } ;
-  
+  return request({
+    url: `${BASE_URL}/export/${parseStrEmpty(pagerId)}`,
+    method: 'get',
+    params: { showAnswer },
+    responseType: 'blob' // 重要：指定响应类型为blob
+  })
+};
+
 /**
  * 获取试卷导出URL
  * @param {number} pagerId 试卷ID
