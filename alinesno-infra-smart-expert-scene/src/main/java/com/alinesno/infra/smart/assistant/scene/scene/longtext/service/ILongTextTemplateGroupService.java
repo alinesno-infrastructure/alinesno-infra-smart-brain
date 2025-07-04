5694b433f8f5734efca2233e1bdc12af80018427
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:10 +0800

    feat:创建ILongTextTemplateGroupService接口，继承IBaseService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/service/ILongTextTemplateGroupService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.scene.scene.longtext.service;
+
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.LongTextTemplateGroupEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ILongTextTemplateGroupService extends IBaseService<LongTextTemplateGroupEntity> {
+
+}
