4bc57db67e506ac4601715f04924055db82f4026
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:06 +0800

    feat:创建GeneralAgentTaskMapper接口，用于数据分析规划数据操作

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/mapper/GeneralAgentTaskMapper.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTaskEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 数据分析规划 Mapper 接口
+ */
+@Mapper
+public interface GeneralAgentTaskMapper extends IBaseMapper<GeneralAgentTaskEntity> {
+}    
