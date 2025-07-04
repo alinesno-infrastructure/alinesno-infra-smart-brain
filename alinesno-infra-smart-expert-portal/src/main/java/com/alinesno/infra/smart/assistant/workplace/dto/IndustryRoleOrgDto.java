c8e9546d16c997ba5c8802d01e14dcb5a6d0adbe
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:12 +0800

    chore:注释掉IndustryRoleOrgDto类的定义和相关依赖

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/dto/IndustryRoleOrgDto.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/dto/IndustryRoleOrgDto.java
@@ -1,13 +1,13 @@
-package com.alinesno.infra.smart.assistant.workplace.dto;
-
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode(callSuper = true)
-@Data
-public class IndustryRoleOrgDto extends IndustryRoleEntity {
-   
-    private String orgName ;
-
-}
+//package com.alinesno.infra.smart.assistant.workplace.dto;
+//
+//import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//
+//@EqualsAndHashCode(callSuper = true)
+//@Data
+//public class IndustryRoleOrgDto extends IndustryRoleEntity {
+//
+//    private String orgName ;
+//
+//}
