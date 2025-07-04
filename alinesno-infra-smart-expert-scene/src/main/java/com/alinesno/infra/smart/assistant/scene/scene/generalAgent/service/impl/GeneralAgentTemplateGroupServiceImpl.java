63519e0e6425e43becc95ff9ebee96a514d86c80
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:04 +0800

    feat:创建通用助手任务服务实现类GeneralAgentTemplateGroupServiceImpl

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/service/impl/GeneralAgentTemplateGroupServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.generalAgent.mapper.IGeneralAgentTemplateGroupMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.generalAgent.service.IGeneralAgentTemplateGroupService;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTemplateGroupEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 通用助手任务服务实现类
+ */
+@Slf4j
+@Service
+public class GeneralAgentTemplateGroupServiceImpl extends IBaseServiceImpl<GeneralAgentTemplateGroupEntity, IGeneralAgentTemplateGroupMapper> implements IGeneralAgentTemplateGroupService {
+
+}
