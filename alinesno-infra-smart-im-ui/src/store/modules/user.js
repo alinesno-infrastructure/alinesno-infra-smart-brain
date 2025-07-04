d613957580ab94d2ae64ae59f7259b72a4c7d830
梁明树(Nina) <ninaAip@gmail.com>
2025-07-05 06:29:16 +0800

    feat:在user.js中新增获取和设置用户主页的方法

--- alinesno-infra-smart-im-ui/src/store/modules/user.js
+++ alinesno-infra-smart-im-ui/src/store/modules/user.js
@@ -1,4 +1,5 @@
 import { login, ssoLogout , logout, getInfo , goSsoAuthUrl , doLoginByTicket , isSsoLogin } from '@/api/login'
+import { getHomePage , setHomePage  } from '@/api/base/im/workplace'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import defAva from '@/assets/images/profile.jpg'
 
@@ -58,6 +59,28 @@ const useUserStore = defineStore(
           login(username, password, code, uuid).then(res => {
             setToken(res.token)
             this.token = res.token
+            resolve()
+          }).catch(error => {
+            reject(error)
+          })
+        })
+      },
+      // 获取用户主页
+      // 在 userStore 中
+      getHomePage() {
+        return new Promise((resolve, reject) => {
+          // 假设 getHomePage() 是API请求方法
+          getHomePage().then(response => {
+            resolve(response.data) // 返回实际的数据
+          }).catch(error => {
+            reject(error)
+          })
+        })
+      } ,
+      // 设置用户主页
+      setHomePage(data){
+        return new Promise((resolve, reject) => {
+          setHomePage(data).then(() => {
             resolve()
           }).catch(error => {
             reject(error)
