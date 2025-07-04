85ed34827c5ff711e446b89ec7c87dad1aa82f92
李娜(Allen) <allenAip@gmail.com>
2025-07-05 06:29:18 +0800

    feat:新增读取Excel数据并保存模板的方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/service/IArticleTemplateService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/articleWriting/service/IArticleTemplateService.java
@@ -3,9 +3,11 @@ package com.alinesno.infra.smart.assistant.scene.scene.articleWriting.service;
 
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.articleWriting.tools.ExcelData;
 import com.alinesno.infra.smart.scene.entity.ArticleTemplateEntity;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -23,4 +25,14 @@ public interface IArticleTemplateService extends IBaseService<ArticleTemplateEnt
      * @return
      */
     List<ArticleTemplateEntity> getTemplateByType(PermissionQuery query, String typeId);
+
+    /**
+     * 读取Excel数据，并保存模板
+     *
+     * @param dataList
+     * @param query
+     * @return
+     */
+    Map<String, Object> readExcel(List<ExcelData> dataList, PermissionQuery query);
+
 }
