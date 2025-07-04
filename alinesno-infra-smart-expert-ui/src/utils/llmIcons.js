60ce90e6eed50e2035c2788cb25e94e2ae986a9f
梁明树(Nina) <ninaAip@gmail.com>
2025-07-05 06:29:06 +0800

    feat:添加llmIcons工具函数，用于获取不同LLM和样式的图标路径

---
+++ alinesno-infra-smart-expert-ui/src/utils/llmIcons.js
@@ -0,0 +1,47 @@
+// utils/llmIcons.js (or you can put this in your component file)
+const getLlmIconPath = (providerCode) => {
+  const iconMap = {
+    'aip': 'aip.png',
+    'customModel': 'customModel.png',
+    'deepseek': 'deepseek.png',
+    'doubao': 'doubao.png',
+    'gitee': 'gitee.png',
+    'ollama': 'ollama.png',
+    'qwen': 'qwen.png',
+    'qwq': 'qwq.png',
+    'siliconflow': 'siliconflow.png'
+  };
+  
+  const fileName = iconMap[providerCode] || 'customModel.png';
+  return new URL(`/src/assets/icons/llm/${fileName}`, import.meta.url).href;
+};
+
+const getAipStyle = (type) => {
+  const iconMap = {
+    'dark': 'dark.png',
+    'light': 'light.png',
+  };
+  
+  const fileName = iconMap[type] || 'light.png';
+  return new URL(`/src/assets/icons/aip-style/${fileName}`, import.meta.url).href;
+};
+
+
+const getOutlineSvg = (type) => {
+  const iconMap = {
+    'iframe': 'iframe.svg',
+    'script': 'script.svg',
+    'offiaccount-copylink': 'offiaccount-copylink.png',
+    'aip_store_search': 'aip_store_search.jpg',
+    'example_banner': 'example_banner.png',
+  };
+  
+  const fileName = iconMap[type] || 'iframe.svg';
+  return new URL(`/src/assets/icons/outline/${fileName}`, import.meta.url).href;
+};
+
+export { 
+  getLlmIconPath,
+  getAipStyle , 
+  getOutlineSvg
+};
