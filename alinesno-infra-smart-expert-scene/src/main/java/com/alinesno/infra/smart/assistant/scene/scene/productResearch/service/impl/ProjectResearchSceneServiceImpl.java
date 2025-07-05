21a031e155f9167ab0b342495229065fee2496e3
陈伟(Victor) <victorAip@gmail.com>
2025-07-06 00:20:40 +0800

    fix:调整导入路径，修复ExamQuestionService的包路径错误。

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/impl/ProjectResearchSceneServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/service/impl/ProjectResearchSceneServiceImpl.java
@@ -3,7 +3,7 @@ package com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.i
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.scene.common.examPaper.service.IExamQuestionService;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionService;
 import com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper.ProjectResearchSceneMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.productResearch.service.IProjectResearchSceneService;
 import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
