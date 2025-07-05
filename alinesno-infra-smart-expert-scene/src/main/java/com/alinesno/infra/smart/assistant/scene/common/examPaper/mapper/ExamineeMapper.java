84f379430e08269b007675c978ee3d7675e86eb6
陈伟(Victor) <victorAip@gmail.com>
2025-07-06 00:20:37 +0800

    更新:alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/mapper/ExamineeMapper.java

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/examPaper/mapper/ExamineeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.smart.assistant.scene.common.examPaper.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ExamineeEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ExamineeMapper extends IBaseMapper<ExamineeEntity> {
-}
