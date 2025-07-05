d14ec255595b9749864ec7e9b365cf8118998070
陈伟(Victor) <victorAip@gmail.com>
2025-07-06 00:20:39 +0800

    feat:创建ExamQuestionController类，实现考试题目数据表格接口

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/controller/ExamQuestionController.java
@@ -0,0 +1,54 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamQuestionService;
+import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 处理与BusinessLogEntity相关的请求的Controller。
+ * 继承自BaseController类并实现IBusinessLogService接口。
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+@Slf4j
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/smart/assistant/scene/examQuestion")
+public class ExamQuestionController extends BaseController<ExamQuestionEntity, IExamQuestionService> {
+
+    @Autowired
+    private IExamQuestionService service;
+
+    /**
+     * 获取BusinessLogEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @DataPermissionScope
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    @Override
+    public IExamQuestionService getFeign() {
+        return this.service;
+    }
+}
