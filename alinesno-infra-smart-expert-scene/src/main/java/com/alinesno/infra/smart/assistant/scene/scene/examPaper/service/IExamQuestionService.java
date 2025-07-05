abbe1aaf1431a667007c0835a5332a4f540f7fb8
陈伟(Victor) <victorAip@gmail.com>
2025-07-06 00:20:38 +0800

    feat:创建考试题目服务接口IExamQuestionService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamQuestionService.java
@@ -0,0 +1,15 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
+import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
+
+/**
+ *
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IExamQuestionService extends IBaseService<ExamQuestionEntity> {
+
+}
