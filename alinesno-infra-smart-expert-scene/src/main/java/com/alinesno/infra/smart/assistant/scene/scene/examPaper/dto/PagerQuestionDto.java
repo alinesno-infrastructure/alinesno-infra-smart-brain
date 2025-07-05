e73ae6f75aee0de182d44806b5bb3c57da444af0
陈伟(Victor) <victorAip@gmail.com>
2025-07-06 00:20:38 +0800

    feat:创建PagerQuestionDto类，用于保存试卷问题数据

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/dto/PagerQuestionDto.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+
+/**
+ * PagerQuestionDto
+ * 保存试卷问题
+ */
+public class PagerQuestionDto extends BaseDto {
+
+}
