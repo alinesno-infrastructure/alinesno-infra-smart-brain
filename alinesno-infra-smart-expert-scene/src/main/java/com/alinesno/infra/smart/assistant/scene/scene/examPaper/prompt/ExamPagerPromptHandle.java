e0a60562116ed81b9f063ac6feee9f24c7d5cd49
李娜(Allen) <allenAip@gmail.com>
2025-07-09 00:18:20 +0800

    feat:新增考试试卷生成和阅卷提示方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/prompt/ExamPagerPromptHandle.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/prompt/ExamPagerPromptHandle.java
@@ -2,8 +2,18 @@ package com.alinesno.infra.smart.assistant.scene.scene.examPaper.prompt;
 
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamStructureItem;
 
+/**
+ * 考试试卷生成提示处理类
+ */
 public class ExamPagerPromptHandle {
 
+    /**
+     * 生成考试试卷生成提示
+     * @param chatContent
+     * @param difficultyLevel
+     * @param item
+     * @return
+     */
     public static String generatorPrompt(String chatContent  , String difficultyLevel , ExamStructureItem item){
         return String.format(ExamPagerPrompt.EXAM_PAPER_PROMPT_TEXT ,
                 chatContent ,
@@ -11,6 +21,15 @@ public class ExamPagerPromptHandle {
                 item.getType() + ":" + item.getTypeName() + ":" + item.getTypeDesc() ,
                 item.getTotalQuestion() ,
                 item.getScorePerQuestion()) ;
+    }
+
+    /**
+     * 生成考试试卷阅卷提示
+     * @param markdownContent
+     * @return
+     */
+    public static String generatorMarkPrompt(String markdownContent){
+        return String.format(ExamPagerPrompt.EXAM_MARK_PROMPT_TEXT , markdownContent) ;
     }
 
 }
