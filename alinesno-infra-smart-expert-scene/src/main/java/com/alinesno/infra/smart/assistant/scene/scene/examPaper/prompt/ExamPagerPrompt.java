e0d239542f3da221e462b1213a4fe720cb2c4045
李娜(Allen) <allenAip@gmail.com>
2025-07-09 00:18:20 +0800

    feat:新增考试评分提示文本常量EXAM_MARK_PROMPT_TEXT

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/prompt/ExamPagerPrompt.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/prompt/ExamPagerPrompt.java
@@ -20,4 +20,38 @@ public class ExamPagerPrompt {
            """ ;
 
 
+    public static final String EXAM_MARK_PROMPT_TEXT = """
+            请对下面的题目和用户的回答进行阅卷，以下为考试试卷和用户回答的答案：
+            %s
+                
+            ##任务
+            - 针对于每一道题目进行打分，并写出打分的理由
+            - 返回的结果需要格式化结果输出，并返回json格式方便应用解析:
+                ```json
+                [
+                    {
+                        "id":"问题的id" ,
+                        "score": "评分的分数",
+                        "maxScore": "题目满分" ,
+                        "comment":"评分的理由和原因"
+                    }
+                ]
+                ```
+                比如:
+                ```json
+                [
+                    {
+                        "id": "1941514712164405249"
+                        "score": 2,
+                        "maxScore": 2 ,
+                        "comment": "内容正确，符合培养要求，满分",
+                    },
+                    {
+                        "id": "1941514712168599553"
+                        "score": 1,
+                        "maxScore": 2 ,
+                        "comment": "选择不正确，答题不完善，1分",
+                    },
+                ]
+            """ ;
 }
