65571d95d5afe64d30a9b7072c5ea7ff9a3af821
李娜(Allen) <allenAip@gmail.com>
2025-07-09 00:18:20 +0800

    fix: 添加ExamMarkingDto导入，修复编译错误

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamInfoServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamInfoServiceImpl.java
@@ -4,6 +4,7 @@ import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamInfoPage;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamMarkingDto;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper.ExamInfoMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamInfoService;
 import com.alinesno.infra.smart.scene.entity.ExamInfoEntity;
@@ -52,4 +53,5 @@ public class ExamInfoServiceImpl extends IBaseServiceImpl<ExamInfoEntity, ExamIn
 
         return null;
     }
+
 }
