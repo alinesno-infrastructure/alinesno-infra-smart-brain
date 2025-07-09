476a60c64e3986e32c370b2058e1911030843874
陈伟(Victor) <victorAip@gmail.com>
2025-07-10 00:16:12 +0800

    feat:创建考生服务实现类ExamineeGroupServiceImpl

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/impl/ExamineeGroupServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper.ExamineeGroupMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.examPaper.service.IExamineeGroupService;
+import com.alinesno.infra.smart.scene.entity.ExamineeGroupEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 考生ServiceImpl类
+ */
+@Slf4j
+@Service
+public class ExamineeGroupServiceImpl extends IBaseServiceImpl<ExamineeGroupEntity, ExamineeGroupMapper> implements IExamineeGroupService {
+
+}
