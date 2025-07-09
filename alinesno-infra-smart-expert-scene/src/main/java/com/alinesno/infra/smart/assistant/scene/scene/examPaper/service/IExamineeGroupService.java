a379d1dae84312daac59b128e52269cd349ebad2
陈伟(Victor) <victorAip@gmail.com>
2025-07-10 00:16:12 +0800

    feat:创建考试人员服务接口IExamineeGroupService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamineeGroupService.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ExamineeGroupEntity;
+
+/**
+ * 考试人员服务接口
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface IExamineeGroupService extends IBaseService<ExamineeGroupEntity> {
+
+}
