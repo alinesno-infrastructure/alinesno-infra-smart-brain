a350a6ad98d1d95ebb9ae54338aede6fda956ede
李伟(Michael) <michaelAip@gmail.com>
2025-07-21 00:23:59 +0800

    feat:创建内容格式化容器组件ContentFormatterContainer.vue

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/contentFormatter/common/ContentFormatterContainer.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="content-formatter-container exam-pagercontainer">
+
+
+        <el-container style="height:calc(100vh - 40px );background-color: #fff;">
+
+            <el-aside width="80px" class="exam-pager-aside">
+                <FunctionList />
+            </el-aside>
+
+            <el-main class="exam-pager-main">
+                <!-- 主要内容显示 -->
+                <slot></slot>
+            </el-main>
+        </el-container>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { ElMessage } from 'element-plus';
+import FunctionList from './functionList'
+
+</script>
+
+<style lang="scss" scoped>
+.content-formatter-container {
+    background: #fff;
+    height: calc(100vh - 50px);
+
+    .send-button {
+        width: 100%;
+    }
+
+    .main-content {
+        display: flex;
+        flex-direction: column;
+        padding-top: calc(3vh - 56px);
+        text-align: center;
+        max-width: 1024px;
+        margin: auto;
+    }
+}
+</style>
