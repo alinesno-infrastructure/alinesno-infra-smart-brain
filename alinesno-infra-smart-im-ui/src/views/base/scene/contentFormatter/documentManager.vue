445dc6bfafa4644231d055a0e141a4f9776b8776
李伟(Michael) <michaelAip@gmail.com>
2025-07-21 00:23:59 +0800

    feat:创建文档管理界面组件，包含基础结构和引入依赖

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/contentFormatter/documentManager.vue
@@ -0,0 +1,19 @@
+<!-- 文档管理界面 -->
+<template>
+  <ContentFormatterContainer>
+    <div class="app-container">
+        文档管理界面
+    </div>
+  </ContentFormatterContainer>  
+</template>
+
+<script setup>
+
+import { onBeforeUnmount, ref, shallowRef, onMounted, nextTick } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+import ContentFormatterContainer from './common/ContentFormatterContainer';
+
+</script>
