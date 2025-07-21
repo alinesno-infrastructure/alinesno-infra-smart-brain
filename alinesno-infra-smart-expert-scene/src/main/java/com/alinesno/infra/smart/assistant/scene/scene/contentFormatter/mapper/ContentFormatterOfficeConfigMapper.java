1aba660df343c67712cbd62b0c2fccb6da25235d
张婷(Tina) <tinaAip@gmail.com>
2025-07-22 00:18:37 +0800

    feat:创建ContentFormatterOfficeConfigMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterOfficeConfigMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
+
+public interface ContentFormatterOfficeConfigMapper extends IBaseMapper<ContentFormatterOfficeConfigEntity> {
+}    
