5f717b9566be6d31ed9c13ee95f7e30dcb951ac2
张婷(Tina) <tinaAip@gmail.com>
2025-07-22 00:18:37 +0800

    feat:创建IContentFormatterOfficeConfigService接口，定义基础服务方法

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterOfficeConfigService.java
@@ -0,0 +1,12 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.dto.TreeNodeDto;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
+
+import java.util.List;
+
+public interface IContentFormatterOfficeConfigService extends IBaseService<ContentFormatterOfficeConfigEntity> {
+
+}
