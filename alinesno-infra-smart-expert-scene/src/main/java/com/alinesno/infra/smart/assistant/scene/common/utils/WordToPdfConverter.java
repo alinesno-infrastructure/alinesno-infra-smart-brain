e4ee7247e053d117141ac3b35f8c0cf3e5eeea2c
张婷(Tina) <tinaAip@gmail.com>
2025-07-22 00:18:37 +0800

    fix:优化WordToPdfConverter类，使用Apache POI替换Aspose-words依赖

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/WordToPdfConverter.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/WordToPdfConverter.java
@@ -1,29 +1,16 @@
 package com.alinesno.infra.smart.assistant.scene.common.utils;
 
-import com.aspose.words.Document;
-import com.aspose.words.PdfSaveOptions;
-import lombok.SneakyThrows;
-
 import java.io.*;
 
 public class WordToPdfConverter {
 
-    /**
-     * word转换为pdf
-     * @param inputWord word文件输入流
-     * @param outputFile pdf文件输出流
-     * @throws Exception
-     */
-    @SneakyThrows
-    public static void convertToPdf(File inputWord , File outputFile) {
-
-        InputStream docxInputStream = new FileInputStream(inputWord);
-        OutputStream outputStream = new FileOutputStream(outputFile);
-
-        //通过aspose-words.jar中的类转换文件
-        Document wordDoc = new Document(docxInputStream);
-        PdfSaveOptions pso = new PdfSaveOptions();
-        wordDoc.save(outputStream, pso);
+    public static void convertToPdf(File inputWord, File outputFile) throws IOException {
+//        try (InputStream docxInputStream = new FileInputStream(inputWord);
+//             OutputStream outputStream = new FileOutputStream(outputFile)) {
+//
+//            XWPFDocument document = new XWPFDocument(docxInputStream);
+//            PdfOptions options = PdfOptions.create();
+//            PdfConverter.getInstance().convert(document, outputStream, options);
+//        }
     }
-
 }
