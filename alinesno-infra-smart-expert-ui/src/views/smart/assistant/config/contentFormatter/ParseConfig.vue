a8f60aad4475c17ae3f80697f565d77ab5cb9478
张婷(Tina) <tinaAip@gmail.com>
2025-07-23 00:18:29 +0800

    feat:优化解析服务配置界面，调整按钮样式和对话框布局

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/config/contentFormatter/ParseConfig.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/config/contentFormatter/ParseConfig.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="parse-config">
-    <el-button type="primary" @click="addOfficeTool" class="mb-20">添加Office工具</el-button>
+    <el-button type="primary" text bg @click="addOfficeTool" icon="Plus" class="mb-20">添加解析服务</el-button>
 
-    <el-table :data="officeTools" border style="width: 100%">
+    <el-table :data="officeTools" style="width: 100%">
+      <!-- 序号 -->
+      <el-table-column type="index" label="序号" width="80" />
       <el-table-column prop="toolName" label="工具名称" width="180" />
       <el-table-column prop="toolPath" label="工具路径" />
       <el-table-column prop="requestToken" label="请求Token" width="200" />
@@ -11,16 +13,16 @@
           <el-tag :type="scopeTagType(row.dataScope)">{{ row.dataScope }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="操作" width="150">
+      <el-table-column label="操作" align="center" width="200">
         <template #default="{row}">
-          <el-button size="small" @click="editOfficeTool(row)">编辑</el-button>
-          <el-button size="small" type="danger" @click="deleteOfficeTool(row)">删除</el-button>
+          <el-button type="primary" text bg @click="editOfficeTool(row)">编辑</el-button>
+          <el-button type="danger" text bg @click="deleteOfficeTool(row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-dialog v-model="dialogVisible" :title="dialogTitle" width="50%">
-      <el-form :model="currentTool" label-width="120px">
+    <el-dialog v-model="dialogVisible" :title="dialogTitle" width="600">
+      <el-form :model="currentTool" size="large" label-width="100px">
         <el-form-item label="工具名称">
           <el-input v-model="currentTool.toolName" placeholder="请输入工具名称" />
         </el-form-item>
@@ -39,8 +41,8 @@
         </el-form-item>
       </el-form>
       <template #footer>
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="saveOfficeTool">保存</el-button>
+        <el-button size="large" @click="dialogVisible = false">取消</el-button>
+        <el-button size="large" type="primary" @click="saveOfficeTool">保存</el-button>
       </template>
     </el-dialog>
   </div>
