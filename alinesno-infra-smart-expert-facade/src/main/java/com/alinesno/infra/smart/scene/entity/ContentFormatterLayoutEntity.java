386e56db916bf47d93e09a3b42248d513f08b63e
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建内容格式化布局配置实体类ContentFormatterLayoutEntity

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/ContentFormatterLayoutEntity.java
@@ -0,0 +1,40 @@
+package com.alinesno.infra.smart.scene.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 内容格式化布局配置，针对于不同场景的内容格式化
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("content_formatter_layout")
+public class ContentFormatterLayoutEntity extends InfraBaseEntity {
+
+    // 排版的名称、描述、所属分组、配置内容（长文本）、排序
+    @TableField
+    @Column(comment = "排版名称" , length = 50 , type = MySqlTypeConstant.VARCHAR)
+    private String name;
+
+    @TableField
+    @Column(comment = "排版描述" , length = 256 , type = MySqlTypeConstant.VARCHAR)
+    private String layoutDesc;
+
+    @TableField
+    @Column(comment = "排版分组" , length = 50 , type = MySqlTypeConstant.VARCHAR)
+    private Long groupId;
+
+    @TableField
+    @Column(comment = "排版配置" , type = MySqlTypeConstant.TEXT)
+    private String layoutConfig;
+
+    @TableField
+    @Column(comment = "排版排序" , length = 11 , type = MySqlTypeConstant.INT)
+    private Integer sort;
+
+}
