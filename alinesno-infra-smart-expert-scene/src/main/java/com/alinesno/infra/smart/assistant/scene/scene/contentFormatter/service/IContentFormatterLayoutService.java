1fa68a97be2ed59228de7031a34fba499e8f3078
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建IContentFormatterLayoutService接口，继承IBaseService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutService.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+
+public interface IContentFormatterLayoutService extends IBaseService<ContentFormatterLayoutEntity> {
+
+}
