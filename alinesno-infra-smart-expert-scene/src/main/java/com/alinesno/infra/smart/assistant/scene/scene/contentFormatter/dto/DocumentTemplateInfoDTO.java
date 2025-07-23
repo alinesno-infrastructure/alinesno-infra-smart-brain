c639e0a10c56b4ea48938d1b3c39dc4c01032ee3
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建DocumentTemplateInfoDTO类，包含模板名称和描述字段

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/dto/DocumentTemplateInfoDTO.java
@@ -0,0 +1,22 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DocumentTemplateInfoDTO extends BaseDto {
+    /**
+     * 模板名称
+     */
+    @NotNull(message = "模板名称不能为空")
+    private String name;
+
+    /**
+     * 模板描述
+     */
+    @NotNull(message = "模板描述不能为空")
+    private String description;
+}
