e609e7356c69f963f92df33a02c4f7f998f6b077
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建ContentFormatterRuleServiceImpl类，实现规则服务逻辑。

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterRuleServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterOfficeConfigMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterRuleMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterOfficeConfigService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterRuleService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContentFormatterRuleServiceImpl extends IBaseServiceImpl<ContentFormatterRuleEntity, ContentFormatterRuleMapper> implements IContentFormatterRuleService {
+
+}
