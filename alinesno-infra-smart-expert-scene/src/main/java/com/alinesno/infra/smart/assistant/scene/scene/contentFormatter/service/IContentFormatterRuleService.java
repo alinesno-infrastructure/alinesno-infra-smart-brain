c4fcd67cfd7f9f83b1813c53c755e344e89cfb9e
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建内容格式化规则服务接口IContentFormatterRuleService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRuleService.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
+
+public interface IContentFormatterRuleService extends IBaseService<ContentFormatterRuleEntity> {
+
+}
