d4b143058f294ba862f106bcf15550ac3e8a8e3b
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建内容格式化分组配置实体类ContentFormatterLayoutGroupEntity

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/ContentFormatterLayoutGroupEntity.java
@@ -0,0 +1,44 @@
+package com.alinesno.infra.smart.scene.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 内容格式化分组配置
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("content_formatter_layout_group")
+public class ContentFormatterLayoutGroupEntity extends InfraBaseEntity {
+
+    // 分组名称、分组图标、排序
+
+    @TableField
+    @Column(comment = "分组名称" , length = 50 , type = MySqlTypeConstant.VARCHAR)
+    private String name;
+
+    @TableField
+    @Column(comment = "分组图标" , length = 128 , type = MySqlTypeConstant.VARCHAR)
+    private String icon;
+
+    @TableField
+    @Column(comment = "排序" , length = 11 , type = MySqlTypeConstant.INT)
+    private Integer sort;
+
+    public static ContentFormatterLayoutGroupEntity toPower(InfraBaseEntity entity) {
+        ContentFormatterLayoutGroupEntity template = new ContentFormatterLayoutGroupEntity();
+
+        template.setOrgId(entity.getOrgId());
+        template.setOperatorId(entity.getOperatorId());
+        template.setDepartmentId(entity.getDepartmentId());
+
+        return template;
+    }
+
+
+}
