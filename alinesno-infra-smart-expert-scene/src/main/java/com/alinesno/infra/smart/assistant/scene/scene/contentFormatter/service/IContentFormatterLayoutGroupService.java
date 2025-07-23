11cce6117095734eb6c71857d73693ae6e4b17e3
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat: 创建内容格式化布局组服务接口IContentFormatterLayoutGroupService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutGroupService.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutGroupEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+
+public interface IContentFormatterLayoutGroupService extends IBaseService<ContentFormatterLayoutGroupEntity> {
+
+}
