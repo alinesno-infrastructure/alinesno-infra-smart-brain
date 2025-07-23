60fe60503ecff6838dbf8876faee3725c139106d
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:新增Office配置服务接口及获取当前配置方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterOfficeConfigService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterOfficeConfigService.java
@@ -1,12 +1,16 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.scene.dto.TreeNodeDto;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
 
-import java.util.List;
-
+/**
+ * Office工具配置服务接口
+ */
 public interface IContentFormatterOfficeConfigService extends IBaseService<ContentFormatterOfficeConfigEntity> {
+
+    /**
+     * 获取当前配置
+     */
+    ContentFormatterOfficeConfigEntity getConfig(Long orgId) ;
 
 }
