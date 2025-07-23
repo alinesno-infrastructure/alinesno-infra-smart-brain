6adf3bafe1c2d0af31ef24451f987714943b6346
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建ContentFormatterRuleMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterRuleMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
+
+public interface ContentFormatterRuleMapper extends IBaseMapper<ContentFormatterRuleEntity> {
+}    
