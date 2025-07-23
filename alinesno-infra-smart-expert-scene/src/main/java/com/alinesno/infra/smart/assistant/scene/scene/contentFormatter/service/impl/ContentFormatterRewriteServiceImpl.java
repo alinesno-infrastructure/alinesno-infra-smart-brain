c0a6cd1a877e98756c2915c6d111327d220c3342
周健(James) <jamesAip@gmail.com>
2025-07-24 00:19:33 +0800

    feat:创建ContentFormatterRewriteServiceImpl类，实现内容格式化重写服务

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterRewriteServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterOfficeConfigMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterRewriteMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterOfficeConfigService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterRewriteService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterRewriteEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ContentFormatterRewriteServiceImpl extends IBaseServiceImpl<ContentFormatterRewriteEntity, ContentFormatterRewriteMapper> implements IContentFormatterRewriteService {
+
+}
