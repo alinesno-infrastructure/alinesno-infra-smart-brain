202f7ac3efdd0723b06e0f313c986a4c28c38c88
林晓(Lily) <lilyAip@gmail.com>
2025-07-25 00:20:09 +0800

    feat:新增内容格式化分组相关API接口

---
+++ alinesno-infra-smart-expert-ui/src/api/smart/scene/contentFormatterGroup.js
@@ -0,0 +1,47 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi"
+
+const prefix = '/api/infra/smart/assistant/scene/contentFormatterLayoutGroup/'
+
+// 查询分组列表
+export function listGroups(query) {
+  return request({
+    url: prefix + 'datatables',
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询分组详细
+export function getGroup(id) {
+  return request({
+    url: prefix + 'getGroup?id=' + parseStrEmpty(id),
+    method: 'get'
+  })
+}
+
+// 新增分组
+export function addGroup(data) {
+  return request({
+    url: prefix + 'addGroup',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改分组
+export function updateGroup(data) {
+  return request({
+    url: prefix + 'updateGroup',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除分组
+export function delGroup(id) {
+  return request({
+    url: prefix + 'delGroup?id=' + parseStrEmpty(id),
+    method: 'delete'
+  })
+}
