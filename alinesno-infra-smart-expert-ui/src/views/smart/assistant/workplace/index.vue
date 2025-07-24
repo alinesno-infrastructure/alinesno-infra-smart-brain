9eb9fd3aaf2c67a770b4b5bcc80e21664d80ca99
陈伟(Victor) <victorAip@gmail.com>
2025-07-25 00:20:10 +0800

    fix:修改工作台名称字体颜色为蓝色

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/workplace/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workplace/index.vue
@@ -80,7 +80,7 @@
           </el-table-column> 
           <el-table-column label="工作台名称" align="left" key="name" prop="name" v-if="columns[1].visible">
             <template #default="scope">
-                <div style="font-size: 15px;font-weight: 500;color: #3b5998;">
+                <div style="font-size: 15px;font-weight: 500;color: #1d75b0;">
                 {{ scope.row.name }}
                 </div>
                 <div style="font-size: 13px;color: #a5a5a5;">
