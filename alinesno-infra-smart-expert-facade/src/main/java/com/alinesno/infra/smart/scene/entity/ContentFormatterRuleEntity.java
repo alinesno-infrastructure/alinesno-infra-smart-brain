f1aea419c4aba3637bc68e982e87828d47fec39a
李娜(Allen) <allenAip@gmail.com>
2025-07-25 00:20:10 +0800

    feat:新增文档类型、适用范围和规则描述字段到ContentFormatterRuleEntity类

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/ContentFormatterRuleEntity.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/entity/ContentFormatterRuleEntity.java
@@ -16,7 +16,6 @@ import lombok.EqualsAndHashCode;
 @TableName("content_formatter_rule")
 public class ContentFormatterRuleEntity extends InfraBaseEntity {
 
-    // 规则名称、规则级别、规则内容、所属合同类型
     @TableField
     @Column(name = "rule_name", type = MySqlTypeConstant.VARCHAR, length = 128, comment = "规则名称")
     private String ruleName;
@@ -29,9 +28,19 @@ public class ContentFormatterRuleEntity extends InfraBaseEntity {
     @Column(name = "rule_content", type = MySqlTypeConstant.TEXT, comment = "规则内容")
     private String ruleContent;
 
-    // 审核立场
     @TableField
     @Column(name = "review_position", type = MySqlTypeConstant.VARCHAR, length = 128, comment = "审核立场")
     private String reviewPosition;
 
+    @TableField
+    @Column(name = "doc_type", type = MySqlTypeConstant.VARCHAR, length = 128, comment = "文档类型")
+    private String docType;
+
+    @TableField
+    @Column(name = "scope", type = MySqlTypeConstant.VARCHAR, length = 128, comment = "适用范围")
+    private String scope;
+
+    @TableField
+    @Column(name = "rule_description", type = MySqlTypeConstant.VARCHAR, length = 500, comment = "规则描述")
+    private String ruleDescription;
 }
