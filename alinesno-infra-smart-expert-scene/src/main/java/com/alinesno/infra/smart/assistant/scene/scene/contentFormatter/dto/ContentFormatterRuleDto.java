27ebdb6a24a97922e8434a64f8c1d99ff4105dc3
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:30 +0800

    fix:添加groupId字段的非空校验，注释掉docType字段的非空校验

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/dto/ContentFormatterRuleDto.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/dto/ContentFormatterRuleDto.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto;
 
 import com.alinesno.infra.common.facade.base.BaseDto;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -27,12 +28,15 @@ public class ContentFormatterRuleDto extends BaseDto {
     @NotBlank(message = "审核立场不能为空")
     private String reviewPosition;
 
-    @NotBlank(message = "文档类型不能为空")
-    private String docType;
+//    @NotBlank(message = "文档类型不能为空")
+//    private String docType;
 
     @NotBlank(message = "适用范围不能为空")
     private String scope;
 
     @Size(max = 500, message = "规则描述长度不能超过500个字符")
     private String ruleDescription;
+
+    @NotNull(message = "模板分组ID不能为空")
+    private Long groupId ;
 }
