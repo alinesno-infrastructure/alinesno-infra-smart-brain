6d262cffcb0abc7c4816d46908c2949548da3d01
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:29 +0800

    feat:添加读取Excel方法到IContentFormatterRuleService接口

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRuleService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRuleService.java
@@ -1,9 +1,22 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.ContentRuleExcelParser;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IContentFormatterRuleService extends IBaseService<ContentFormatterRuleEntity> {
+
+    /**
+     * 读取Excel
+     * @param templates
+     * @param query
+     * @return
+     */
+    Map<String, Object> readExcel(List<ContentRuleExcelParser.TemplateBean> templates, PermissionQuery query);
 
 }
