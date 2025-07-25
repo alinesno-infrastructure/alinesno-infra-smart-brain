cbf56b9b1da71092e0620af8ab956850bd109513
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:30 +0800

    fix:调整模型提供者的API地址为兼容模式v1

--- alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/enums/LlmModelProviderEnums.java
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/enums/LlmModelProviderEnums.java
@@ -35,12 +35,12 @@ import java.util.Map;
 public enum LlmModelProviderEnums {
 
     QWEN("阿里百炼(Qwen)", "qwen", List.of(
-            new ModelInfo(ModelTypeEnums.LARGE_LANGUAGE_MODEL, "https://dashscope.aliyuncs.com" , QwenLlmModelEnums.getAllModels()),
-            new ModelInfo(ModelTypeEnums.VECTOR_MODEL, "https://dashscope.aliyuncs.com" , QwenVectorModelEnums.getAllModels()),
+            new ModelInfo(ModelTypeEnums.LARGE_LANGUAGE_MODEL, "https://dashscope.aliyuncs.com/compatible-mode/v1" , QwenLlmModelEnums.getAllModels()),
+            new ModelInfo(ModelTypeEnums.VECTOR_MODEL, "https://dashscope.aliyuncs.com/compatible-mode/v1" , QwenVectorModelEnums.getAllModels()),
             new ModelInfo(ModelTypeEnums.RE_RANKING_MODEL, "https://dashscope.aliyuncs.com/api/v1/services/rerank/text-rerank/text-rerank" , QwenReRankerModelEnums.getAllModels()),
             new ModelInfo(ModelTypeEnums.SPEECH_RECOGNITION, "https://dashscope.aliyuncs.com/api/v1/services/audio/asr/transcription" , QwenSpeechRecognizeModelEnums.getAllModels()) ,
-            new ModelInfo(ModelTypeEnums.SPEECH_SYNTHESIS, "https://dashscope.aliyuncs.com" , QwenSpeechModelEnums.getAllModels() , 2) ,
-            new ModelInfo(ModelTypeEnums.VISION_MODEL, "https://dashscope.aliyuncs.com" , QwenVisionModelEnums.getAllModels()) ,
+            new ModelInfo(ModelTypeEnums.SPEECH_SYNTHESIS, "https://dashscope.aliyuncs.com/compatible-mode/v1" , QwenSpeechModelEnums.getAllModels() , 2) ,
+            new ModelInfo(ModelTypeEnums.VISION_MODEL, "https://dashscope.aliyuncs.com/compatible-mode/v1" , QwenVisionModelEnums.getAllModels()) ,
             new ModelInfo(ModelTypeEnums.IMAGE_GENERATION, "https://dashscope.aliyuncs.com/api/v1/services/aigc/text2image/image-synthesis" , QwenImageModelEnums.getAllModels())
     )),
 
