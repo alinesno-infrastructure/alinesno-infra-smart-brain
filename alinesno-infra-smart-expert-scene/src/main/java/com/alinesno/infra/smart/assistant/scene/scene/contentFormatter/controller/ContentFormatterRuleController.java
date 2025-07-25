795d565d679487c15167d95561bad90bc47c5c17
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:30 +0800

    feat:添加文件上传功能，支持导入审核规则数据

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/controller/ContentFormatterRuleController.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/controller/ContentFormatterRuleController.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
@@ -12,20 +13,29 @@ import com.alinesno.infra.common.facade.response.ResultCodeEnum;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto.ContentFormatterRuleDto;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterRuleService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.ContentRuleExcelParser;
+import com.alinesno.infra.smart.assistant.scene.scene.longtext.tools.RobustExcelParser;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 审核规则管理
@@ -39,6 +49,9 @@ public class ContentFormatterRuleController extends BaseController<ContentFormat
 
     @Autowired
     private IContentFormatterRuleService service;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
 
     /**
      * 获取BusinessLogEntity的DataTables数据。
@@ -107,6 +120,35 @@ public class ContentFormatterRuleController extends BaseController<ContentFormat
     public AjaxResult delRule(@RequestParam Long id) {
         service.removeById(id);
         return AjaxResult.success("删除成功");
+    }
+
+    /**
+     * 文件上传
+     * @return
+     */
+    @SneakyThrows
+    @DataPermissionQuery
+    @PostMapping("/importData")
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, String type , String updateSupport , PermissionQuery query){
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        log.debug("newFileName = {} , targetFile = {}" , newFileName , targetFile.getAbsoluteFile());
+
+        List<ContentRuleExcelParser.TemplateBean> templates = ContentRuleExcelParser.parseExcel(targetFile.getAbsolutePath()) ;
+        templates.forEach(System.out::println);
+
+        Map<String, Object> failResult = service.readExcel(templates , query) ;
+
+        FileUtils.forceDeleteOnExit(targetFile);
+
+        return AjaxResult.success("操作成功." , failResult);
     }
 
     @Override
