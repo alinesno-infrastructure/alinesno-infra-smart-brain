bc41b9b60e9499c496150fe8e1f641f3d0df3810
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:30 +0800

    feat:新增readExcel方法，用于读取Excel模板数据

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutService.java
@@ -1,9 +1,23 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.ContentLayoutExcelParser;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.ContentRuleExcelParser;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IContentFormatterLayoutService extends IBaseService<ContentFormatterLayoutEntity> {
+
+    /**
+     * 读取Excel
+     * @param templates
+     * @param query
+     * @return
+     */
+    Map<String, Object> readExcel(List<ContentLayoutExcelParser.LayoutBean> templates, PermissionQuery query);
 
 }
