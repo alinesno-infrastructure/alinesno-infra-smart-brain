8ca48095087eef0bee16af59aef17429eb02ba19
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:30 +0800

    feat:新增删除分组和查询组织分组方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutGroupService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterLayoutGroupService.java
@@ -1,9 +1,25 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutGroupEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+
+import java.util.List;
 
 public interface IContentFormatterLayoutGroupService extends IBaseService<ContentFormatterLayoutGroupEntity> {
 
+    /**
+     * 删除分组
+     *
+     * @param id
+     * @param groupType
+     */
+    void removeGroup(Long id, String groupType);
+
+    /**
+     * 查询组织分组
+     * @param query
+     * @return
+     */
+    List<ContentFormatterLayoutGroupEntity> listOrgGroup(PermissionQuery query);
 }
