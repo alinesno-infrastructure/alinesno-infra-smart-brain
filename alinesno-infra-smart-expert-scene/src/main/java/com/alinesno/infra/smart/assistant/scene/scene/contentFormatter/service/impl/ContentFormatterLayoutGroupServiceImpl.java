6e404484ac1f262f104495427862bfd6c07449c5
陈伟(Victor) <victorAip@gmail.com>
2025-07-26 00:19:29 +0800

    feat:新增ContentFormatterLayoutGroupServiceImpl类，添加布局组删除功能及相关依赖注入

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterLayoutGroupServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterLayoutGroupServiceImpl.java
@@ -1,17 +1,57 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.enums.GroupTypeEnums;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterLayoutGroupMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterOfficeConfigMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterLayoutMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterRuleMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterLayoutGroupService;
-import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterOfficeConfigService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutGroupEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
+import com.alinesno.infra.smart.scene.enums.SceneScopeType;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 public class ContentFormatterLayoutGroupServiceImpl extends IBaseServiceImpl<ContentFormatterLayoutGroupEntity, ContentFormatterLayoutGroupMapper> implements IContentFormatterLayoutGroupService {
 
+    @Autowired
+    private ContentFormatterRuleMapper ruleMapper;
+
+    @Autowired
+    private ContentFormatterLayoutMapper layoutMapper ;
+
+    @Override
+    public void removeGroup(Long id, String groupType) {
+        removeById(id) ;
+
+        if(GroupTypeEnums.LAYOUT.getValue().equals(groupType)){
+            layoutMapper.delete(new LambdaQueryWrapper<ContentFormatterLayoutEntity>()
+                    .eq(ContentFormatterLayoutEntity::getGroupId, id));
+        }else if(GroupTypeEnums.AUDIT.getValue().equals(groupType)){
+            // 删除分组下面的规则
+            ruleMapper.delete(new LambdaQueryWrapper<ContentFormatterRuleEntity>()
+                    .eq(ContentFormatterRuleEntity::getGroupId, id));
+        }
+
+    }
+
+    @Override
+    public List<ContentFormatterLayoutGroupEntity> listOrgGroup(PermissionQuery query) {
+
+        LambdaQueryWrapper<ContentFormatterLayoutGroupEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.setEntityClass(ContentFormatterLayoutGroupEntity.class) ;
+        queryWrapper.eq(ContentFormatterLayoutGroupEntity::getDataScope, SceneScopeType.ORG_SCENE.getValue())
+                .eq(ContentFormatterLayoutGroupEntity::getGroupType, GroupTypeEnums.LAYOUT.getValue())
+                .eq(ContentFormatterLayoutGroupEntity::getOrgId, query.getOrgId());
+
+        return list(queryWrapper) ;
+    }
 }
