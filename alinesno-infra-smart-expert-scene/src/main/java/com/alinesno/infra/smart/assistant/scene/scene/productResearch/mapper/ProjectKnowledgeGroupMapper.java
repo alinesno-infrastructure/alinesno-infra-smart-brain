07b05c89a85abffd4d515acf5e7b494834fa5706
李伟(Michael) <michaelAip@gmail.com>
2025-07-29 00:19:59 +0800

    更新:alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/mapper/ProjectKnowledgeGroupMapper.java

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/productResearch/mapper/ProjectKnowledgeGroupMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.smart.assistant.scene.scene.productResearch.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ProjectKnowledgeGroupEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ProjectKnowledgeGroupMapper extends IBaseMapper<ProjectKnowledgeGroupEntity> {
-}    
