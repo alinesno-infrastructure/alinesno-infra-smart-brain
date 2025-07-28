c583205c911b3f321e2db9ddfd549eec6f55d06c
李伟(Michael) <michaelAip@gmail.com>
2025-07-29 00:20:00 +0800

    feat:创建ProjectKnowledgeGroupMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/mapper/ProjectKnowledgeGroupMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.scene.common.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ProjectKnowledgeGroupEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProjectKnowledgeGroupMapper extends IBaseMapper<ProjectKnowledgeGroupEntity> {
+}    
