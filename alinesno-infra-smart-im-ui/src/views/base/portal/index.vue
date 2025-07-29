946cc62345ff1c33f8ebed166a18468a989b9a84
李伟(Michael) <michaelAip@gmail.com>
2025-07-30 00:18:37 +0800

    feat:优化最近使用列表和工作台标题显示逻辑

--- alinesno-infra-smart-im-ui/src/views/base/portal/index.vue
+++ alinesno-infra-smart-im-ui/src/views/base/portal/index.vue
@@ -6,14 +6,17 @@
                 <!-- 最近使用部分 -->
                 <div class="sidebar-section">
                     <div class="section-title">最近使用</div>
+                    <div class="none-tip">
+                        最近操作列表 ~~
+                    </div>
                     <div class="recent-items">
-                        <div v-for="item in items" :key="item" class="recent-item">
-                            <img class="item-icon" :src="item.icon" />
-                            <span>{{ item.title }}</span>
+                        <div v-for="item in items" :key="item.id" class="recent-item" @click="enterItem(item)">
+                            <img class="item-icon" :src="imagePathByPath(item.avator)" />
+                            <span>{{ item.name }}</span>
                         </div>
                     </div>
-                    <div class="none-tip" v-if="items.length == 0" >
-                        还没有收藏任何内容，点击⭐️按钮可将内容添加到这里~
+                    <div class="none-tip" v-if="items.length == 0">
+                        还没有操作任何内容， 最近操作可将内容添加到这里 ~~
                     </div>
                 </div>
 
@@ -29,13 +32,13 @@
                             <!-- 工作台标题 -->
                             <div class="workspace-header">
                                 <div class="workspace-title" @click="editTitle()">
-                                    <i class="fa-brands fa-slack"></i>  AIP智能体工作台 
+                                    <i class="fa-brands fa-slack"></i> {{ currentWorkplace.name }}
                                     <span class="title-edit">
                                         <i class="fa-solid fa-pen-nib"></i>
-                                    </span> 
+                                    </span>
                                 </div>
                                 <div class="workspace-description">
-                                   AIP 智能体工作台集成 AI 技术，可定制开发，支持多智能体协作，提升效率。 
+                                    {{ currentWorkplace.description }}
                                 </div>
                             </div>
 
@@ -44,37 +47,42 @@
                                 <div class="focus-title">我的收藏</div>
 
                                 <div class="focus-tags">
-                                    <el-check-tag type="primary" size="default">智能体</el-check-tag>
-                                    <el-check-tag type="success" size="default">场景</el-check-tag>
-                                    <el-check-tag type="warning" size="default">频道</el-check-tag>
+                                    <el-check-tag  :checked="filterTag === ''"  type="primary"  size="default" @click="handleTagClick('')" >全部</el-check-tag>
+                                    <el-check-tag  :checked="filterTag === 'scene'"  type="primary" size="default" @click="handleTagClick('scene')">场景</el-check-tag>
+                                    <el-check-tag  :checked="filterTag === 'agent'"  type="primary" size="default" @click="handleTagClick('agent')">智能体</el-check-tag>
+                                    <el-check-tag  :checked="filterTag === 'channel'"  type="primary" size="default" @click="handleTagClick('channel')" >频道</el-check-tag>
                                 </div>
 
                                 <!-- 智能体列表 -->
                                 <div class="agent-list">
                                     <div v-for="agent in agents" :key="agent.id" class="agent-card">
                                         <div class="agent-info">
-                                            <div class="agent-title">{{ agent.title }}</div>
+                                            <div class="agent-title" @click="enterItem(agent)">{{ agent.title }}</div>
                                             <div class="agent-description">{{ agent.description }}</div>
-                                            <div>
-                                                <el-check-tag disabled size="small">智能体</el-check-tag>
+                                            <div class="agent-type-tags" >
+                                                <el-check-tag checked="true" type="primary" size="small" v-if="agent.type === 'scene'">场景</el-check-tag>
+                                                <el-check-tag checked="true" type="danger" size="small" v-if="agent.type === 'agent'">智能体</el-check-tag>
+                                                <el-check-tag checked="true" type="warning" size="small" v-if="agent.type === 'channel'">频道</el-check-tag>
+                                                <el-check-tag type="danger" size="small" v-if="agent.sceneTypeName"> <i :class="agent.sceneIcon"></i>  {{ agent.sceneTypeName }} </el-check-tag>
                                             </div>
                                             <div class="agent-meta">
-                                                <img class="agent-avatar"
-                                                    src="https://p26-passport.byteacctimg.com/img/user-avatar/50e8e83154af3c08de64ff15d069d63a~300x300.image" />
-                                                <span>{{ agent.stats }}</span>
                                                 <span>{{ agent.editInfo }}</span>
                                                 <span>{{ agent.editTime }}</span>
+                                                <div class="agent-collect-remove" @click="handleUnCollectItem(agent)">
+                                                    <span><i class="fa-solid fa-eraser"></i> 取消</span>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="agent-image">
-                                            <img
-                                                src="http://alinesno-infra-smart-im-ui.beta.base.infra.linesno.com/prod-api/v1/api/infra/base/im/chat/displayImage/1940307040494858241" />
+                                            <img :src="imagePathByPath(agent.avatar)" />
                                         </div>
                                     </div>
                                     <div v-if="agents.length == 0" class="none-tip main-empty">
-                                        <el-empty description="你还没有收藏任务AI智能体和场景，请选择合适的场景和AI智能体进行⭐️收藏">
-                                            <el-button @click="toScene('/scene')" size="large" type="primary"> <i class="fa-solid fa-box"></i> &nbsp;进入场景市场</el-button>
-                                            <el-button @click="toScene('/store')" size="large" type="success"> <i class="fa-solid fa-sailboat"></i> &nbsp;进入智能体商店</el-button>
+                                        <el-empty description="你还没有收藏AI智能体和场景，请选择合适的场景和AI智能体进行⭐️收藏">
+                                            <el-button @click="toScene('/scene')" size="large" type="primary"> <i
+                                                    class="fa-solid fa-box"></i> &nbsp;进入场景市场</el-button>
+                                            <el-button @click="toScene('/store')" size="large" type="success"> <i
+                                                    class="fa-solid fa-sailboat"></i> &nbsp;进入智能体商店</el-button>
                                         </el-empty>
                                     </div>
                                 </div>
@@ -84,7 +92,7 @@
                 </el-scrollbar>
             </el-main>
         </el-container>
-        <CreatePortal ref="createPortalRef" @handleHasWorkplace="handleHasWorkplace" />
+        <CreatePortal ref="createPortalRef" @handleHasWorkplace="handleGetCurrentWorkplace" />
     </div>
 </template>
 
@@ -92,72 +100,136 @@
 
 import { ref } from 'vue'
 
+import {
+    isHasAccountWorkplace,
+    unCollectItem,
+    getFrequentItem,
+    getCurrentWorkplace
+} from "@/api/base/im/workplace"
+
+import SnowflakeId from "snowflake-id";
+const snowflake = new SnowflakeId();
+
 import CreatePortal from './createPortal'
 import UserIntegralPanel from './card/userIntegral'
 
+const { proxy } = getCurrentInstance();
 const router = useRouter();
 const createPortalRef = ref(null);
+const filterTag = ref('')
 
-const items = ref([
-    // {
-    //     id: 1,
-    //     icon: "http://alinesno-infra-smart-im-ui.beta.base.infra.linesno.com/prod-api/v1/api/infra/base/im/chat/displayImage/1861241150263382018",
-    //     title: "DeepSeek 能力增强版(副本)"
-    // },
-    // {
-    //     id: 2,
-    //     icon: "http://alinesno-infra-smart-im-ui.beta.base.infra.linesno.com/prod-api/v1/api/infra/base/im/chat/displayImage/1861241150263382018",
-    //     title: "信托产品小助手"
-    // },
-])
+const defaultDesc = ref('AI智能体工作台是融合多种先进技术的综合性工作平台，具备知识管理、内容生成、创作辅助等多种功能，功能包括多模型接入、知识管理、智能创作等功能，能够极大提升工作效率与创新能力，助力企业和个人在数字化时代实现高效、智能的工作模式 。')
+const currentWorkplace = ref({
+    name: 'AIP智能体运营工作台',
+    description: defaultDesc.value,
+})
+const workplaceId = ref('');
 
-const agents = ref([
-    // {
-    //     "title": "AI工程师的生成和服务",
-    //     "description": "只需在任务人一个操作中找到用户，它就能提供支持的工具和服务。",
-    //     "tag": "API工程师",
-    //     "stats": "99.6K+",
-    //     "editInfo": "桌面编辑",
-    //     "editTime": "00-25 17:40"
-    // },
-    // {
-    //     "title": "酒店知识通小账",
-    //     "description": "酒店知识通小账是一个专门推荐酒店相关知识的管理者。",
-    //     "tag": "软件工程师",
-    //     "stats": "99.6K+",
-    //     "editInfo": "桌面编辑",
-    //     "editTime": "00-23 13:58"
-    // }
-])
+const items = ref([])
+const agents = ref([])
 
-// const handleHasWorkplace = () => {
-//     isHasWorkplace().then(res => {
-//         if(!res.data){
-//             createPortalRef.value.openSelectWorkplace();
-//         }else{
-//             workplaceId.value = res.data;
-//             getCurrentWorkplace(workplaceId.value).then(ress => {
-//                 console.log('ress ='+ ress);
-//                 currentWorkplace.value = ress.data;
-//                 handleClick(activeCode.value)
-//             })
-//         }
-//     })
-// }
-
-const toScene = (path) => { 
-  router.push(path)
+const toScene = (path) => {
+    router.push(path)
 }
 
 const editTitle = () => {
-    createPortalRef.value.openSelectWorkplace();
+    createPortalRef.value.openSelectWorkplace(currentWorkplace.value);
+}
+
+// 标签点击处理方法
+const handleTagClick = (tag) => {
+
+    // 如果当前的 filterTag 和 tag 相同，则不处理
+    if (filterTag.value === tag) {
+        return;
+    }
+
+    filterTag.value = tag
+    handleGetCurrentWorkplace();
+}
+
+const handleHasWorkplace = () => {
+    isHasAccountWorkplace().then(res => {
+        if (!res.data) {
+            createPortalRef.value.openSelectWorkplace();
+        } else {
+            workplaceId.value = res.data;
+            handleGetCurrentWorkplace();
+        }
+    })
+}
+
+const handleGetCurrentWorkplace = () => { 
+    getCurrentWorkplace(workplaceId.value , filterTag.value).then(ress => {
+        console.log('ress =' + ress);
+        currentWorkplace.value = ress.data;
+        agents.value = ress.workplaceItems
+    })
+}
+
+// 取消收藏
+const handleUnCollectItem = (item) => {
+    const data = {
+        itemId: item.itemId,
+        type: item.type
+    }
+    unCollectItem(data).then(res => {
+        proxy.$modal.msgSuccess("取消收藏成功.");
+        handleHasWorkplace();
+    })
+}
+
+const handleGetFrequentItem = () => {
+    getFrequentItem().then(res => {
+        items.value = res.data;
+    })
+}
+
+// 进入指定的item
+const enterItem = (item) => {
+    const type = item.type;
+    const id = item.id; 
+
+    console.log('type = ' + type + ' , id = ' + id);
+
+    if(type === 'scene'){
+        enterScreen(item);
+    }else if(type === 'channel'){
+        enterChannel(item);
+    }else if(type === 'agent'){
+        handleRoleChat(item);
+    }
+
+}
+
+/** 进入长文本编辑界面 */
+function enterScreen(item) {
+    const path = '/scene/' + item.sceneType + '/index';
+    router.push({
+        path: path,
+        query: { 'sceneId': item.id }
+    })
+}
+
+function handleRoleChat(item) {
+    router.push({
+        path: '/single/agentChat',
+        query: { 'roleId': item.id, 'channelId': snowflake.generate() }
+    })
+}
+
+function enterChannel(item) {
+  router.push({
+    path: '/chat',
+    query: { 'channel': item.id }
+  })
 }
 
 onMounted(() => {
-    // createPortalRef.value.openSelectWorkplace();
-    // nextTick(() => {
-    //     handleHasWorkplace();
-    // });
+    nextTick(() => {
+        handleHasWorkplace();
+        handleGetFrequentItem();
+    });
 });
 
 </script>
@@ -198,6 +270,11 @@ body {
                     border-radius: 5px;
                     padding: 2px 5px;
 
+                    &:hover {
+                        background-color: #eaeaea;
+                        cursor: pointer;
+                    }
+
                     .item-icon {
                         width: 16px;
                         height: 16px;
@@ -211,6 +288,7 @@ body {
     .main-content {
         margin-top: 0;
         padding-top: 0;
+        padding-right: 0;
         background: #fafafa;
         padding-bottom: 0px;
         margin-bottom: 0px;
@@ -288,13 +366,21 @@ body {
                     justify-content: space-between;
                     box-sizing: revert;
 
+                    &:hover {
+                        .agent-collect-remove {
+                            display: block !important;
+                        }
+                    }
+
                     .agent-info {
                         display: flex;
                         flex-direction: column;
                         gap: 10px;
 
+
                         .agent-title {
-                            font-size: 15px;
+                            font-size: 16px;
+                            cursor: pointer;
                         }
 
                         .agent-description {
@@ -311,14 +397,16 @@ body {
                             font-size: 13px;
                             display: flex;
                             gap: 10px;
-                            margin-top:10px;
+                            margin-top: 10px;
                             align-items: center;
                             color: #777;
 
-                            .agent-avatar {
-                                width: 16px;
-                                height: 16px;
-                                border-radius: 50%;
+
+                            .agent-collect-remove {
+                                align-items: center;
+                                display: none;
+                                cursor: pointer;
+                                gap: 10px;
                             }
                         }
                     }
@@ -327,6 +415,8 @@ body {
                         width: 70px;
                         height: 70px;
                         border-radius: 8px;
+                        object-fit: cover;
+                        object-position: center;
                     }
                 }
             }
@@ -342,6 +432,11 @@ body {
 
     .main-empty {
         width: 100%;
+    }
+
+    .agent-type-tags {
+        display: flex;
+        gap: 5px;
     }
 
 }
