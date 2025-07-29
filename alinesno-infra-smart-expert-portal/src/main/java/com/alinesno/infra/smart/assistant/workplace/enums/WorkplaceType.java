f5586e246fd1f38dcbe3951ebb3bc2aa07f12755
李伟(Michael) <michaelAip@gmail.com>
2025-07-30 00:18:37 +0800

    feat:新增PRIVATE枚举值，定义个人工作台类型及描述

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/enums/WorkplaceType.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/enums/WorkplaceType.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 
 @Getter
 public enum WorkplaceType {
+
+    PRIVATE("private", "个人工作台", "fa-solid fa-user", "个人工作台只能选择个人智能体、个人频道、个人场景"),
     PUBLIC("public", "公开工作台", "fa-solid fa-globe", "公开工作台只能选择公共智能体、公开频道、公开场景"),
     ORG("org", "组织工作台", "fa-solid fa-truck-plane", "组织工作台可以选择公共和组织内的智能体、频道和场景");
 
