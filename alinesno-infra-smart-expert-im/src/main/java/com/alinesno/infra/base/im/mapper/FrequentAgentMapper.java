4e55fb83908e95672a93d5863353ae05481fff51
李伟(Michael) <michaelAip@gmail.com>
2025-07-30 00:18:37 +0800

    feat:创建FrequentAgentMapper接口，用于操作常用Agent记录

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/FrequentAgentMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.FrequentAgentEntity;
+
+/**
+ * 常用Agent记录Mapper接口
+ */
+public interface FrequentAgentMapper extends IBaseMapper<FrequentAgentEntity> {
+}
