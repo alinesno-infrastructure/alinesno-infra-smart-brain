cb78f1a9178b927a7de28e3d8f2306639a27b8ba
李伟(Michael) <michaelAip@gmail.com>
2025-07-30 00:18:38 +0800

    feat:新增设置和获取账户首页的方法

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IAccountHomePageService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IAccountHomePageService.java
@@ -8,7 +8,19 @@ import com.alinesno.infra.smart.im.entity.AccountHomePageEntity;
  */
 public interface IAccountHomePageService extends IBaseService<AccountHomePageEntity> {
 
+    /**
+     * 设置账户首页
+     * @param accountId
+     * @param homePage
+     * @param type
+     * @return
+     */
     boolean setHomePage(Long accountId, String homePage, String type);
 
+    /**
+     * 获取账户首页
+     * @param accountId
+     * @return
+     */
     AccountHomePageEntity getByAccountId(Long accountId);
 }
