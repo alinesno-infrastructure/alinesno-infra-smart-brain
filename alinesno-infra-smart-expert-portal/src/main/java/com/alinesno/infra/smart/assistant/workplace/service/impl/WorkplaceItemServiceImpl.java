6113a433b8b956d801e59bd895cba3384b4ca444
李伟(Michael) <michaelAip@gmail.com>
2025-07-30 00:18:37 +0800

    fix:修复获取工作区项方法，增加类型判断和数据处理逻辑

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/WorkplaceItemServiceImpl.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/WorkplaceItemServiceImpl.java
@@ -10,10 +10,15 @@ import com.alinesno.infra.smart.assistant.workplace.entity.WorkplaceItemEntity;
 import com.alinesno.infra.smart.assistant.workplace.enums.WorkplaceItemTypeEnums;
 import com.alinesno.infra.smart.assistant.workplace.mapper.WorkplaceItemMapper;
 import com.alinesno.infra.smart.assistant.workplace.service.IWorkplaceItemService;
+import com.alinesno.infra.smart.assistant.workplace.tools.DateFormatterUtils;
+import com.alinesno.infra.smart.im.dto.CollectItemDto;
+import com.alinesno.infra.smart.im.dto.CollectItemObjectDto;
 import com.alinesno.infra.smart.im.dto.IndustryRoleOrgDto;
 import com.alinesno.infra.smart.im.entity.ChannelEntity;
 import com.alinesno.infra.smart.im.service.IChannelService;
+import com.alinesno.infra.smart.scene.dto.SceneInfoDto;
 import com.alinesno.infra.smart.scene.entity.SceneEntity;
+import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import com.alinesno.infra.smart.scene.service.ISceneService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -21,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.lang.exception.RpcServiceRuntimeException;
 import java.util.*;
@@ -108,119 +114,43 @@ public class WorkplaceItemServiceImpl extends IBaseServiceImpl<WorkplaceItemEnti
         return true ;
     }
 
-//    @Override
-//    public WorkplaceImResponseDto getWorkplaceItemByType(Long workplaceId, String type) {
-//        WorkplaceImResponseDto dto =  new WorkplaceImResponseDto();
-//
-//        LambdaQueryWrapper<WorkplaceItemEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
-//        lambdaQueryWrapper.eq(WorkplaceItemEntity::getWorkplaceId, workplaceId);
-//
-//        List<WorkplaceItemEntity> list = list(lambdaQueryWrapper);
-//        if(list.isEmpty()){
-//            return dto ;
-//        }
-//
-//        // 主逻辑
-//        try {
-//            // 使用公共方法提取 agentIds、channelIds 和 sceneIds
-//            if(type.equals(WorkplaceItemTypeEnums.AGENT.getCode())){
-//                List<Long> agentIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.AGENT.getCode());
-//                List<IndustryRoleEntity> agentsList = agentIds.isEmpty() ? Collections.emptyList() : industryRoleService.listByIds(agentIds);
-//
-//                List<IndustryRoleOrgDto> agentsOrgList = new ArrayList<>();
-//                for (IndustryRoleEntity industryRoleEntity : agentsList) {
-//                    IndustryRoleOrgDto dto1 = new IndustryRoleOrgDto();
-//                    BeanUtils.copyProperties(industryRoleEntity, dto1);
-//
-//                    OrganizationDto org = organizationConsumer.findOrg(industryRoleEntity.getOrgId()).getData() ;
-//                    if(org != null){
-//                        dto1.setOrgName(org.getOrgName());
-//                    }
-//
-//                    agentsOrgList.add(dto1);
-//                }
-//
-//                dto.setAgentsList(agentsOrgList);
-//            }
-//
-//
-//            if(type.equals(WorkplaceItemTypeEnums.CHANNEL.getCode())){
-//                List<Long> channelIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.CHANNEL.getCode());
-//                List<ChannelEntity> channelList = channelIds.isEmpty() ? Collections.emptyList() : channelService.listByIds(channelIds);
-//
-//                List<ChannelOrgDto> channelOrgList = new ArrayList<>();
-//                for (ChannelEntity channelEntity : channelList) {
-//                    ChannelOrgDto dto1 = new ChannelOrgDto();
-//                    BeanUtils.copyProperties(channelEntity, dto1);
-//
-//                    dto1.setOrgName(organizationConsumer.findOrg(channelEntity.getOrgId()).getData().getOrgName());
-//                    channelOrgList.add(dto1);
-//                }
-//                dto.setChannelsList(channelOrgList);
-//            }
-//
-//            if(type.equals(WorkplaceItemTypeEnums.SCENE.getCode())){
-//                List<Long> sceneIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.SCENE.getCode());
-//                List<SceneEntity> sceneList = sceneIds.isEmpty() ? Collections.emptyList() : sceneService.listByIds(sceneIds);
-//
-//                List<SceneOrgDto> sceneOrgList = new ArrayList<>();
-//                for (SceneEntity sceneEntity : sceneList) {
-//                    SceneOrgDto dto1 = new SceneOrgDto();
-//                    BeanUtils.copyProperties(sceneEntity, dto1);
-//
-//                    dto1.setOrgName(organizationConsumer.findOrg(sceneEntity.getOrgId()).getData().getOrgName());
-//                    sceneOrgList.add(dto1);
-//                }
-//
-//                dto.setScenesList(sceneOrgList);
-//            }
-//        } catch (Exception e) {
-//            // 捕获异常并记录日志（具体日志实现可根据项目需求调整）
-//            log.error("Error occurred while processing item lists" , e);
-//            throw new RpcServiceRuntimeException("查询列表数据异常"); // 根据需求决定是否重新抛出异常
-//        }
-//
-//        return dto ;
-//    }
-
     @Override
-    public WorkplaceImResponseDto getWorkplaceItemByType(Long workplaceId, String type) {
-        WorkplaceImResponseDto dto = new WorkplaceImResponseDto();
+    public List<CollectItemObjectDto> getWorkplaceItemByType(Long workplaceId, String type) {
+        List<CollectItemObjectDto> collectItemObjectDtos = new ArrayList<>();
 
         LambdaQueryWrapper<WorkplaceItemEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         lambdaQueryWrapper.eq(WorkplaceItemEntity::getWorkplaceId, workplaceId);
+
+        if(StringUtils.hasText(type)){
+            lambdaQueryWrapper.eq(WorkplaceItemEntity::getItemType, type);
+        }
+
         List<WorkplaceItemEntity> list = list(lambdaQueryWrapper);
 
         if(list.isEmpty()){
-            return dto;
+            return collectItemObjectDtos ;
         }
 
         try {
             // 预收集所有需要的orgId
             Set<Long> orgIds = new HashSet<>();
 
-            if(type.equals(WorkplaceItemTypeEnums.AGENT.getCode())){
-                List<Long> agentIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.AGENT.getCode());
-                if(!agentIds.isEmpty()) {
-                    List<IndustryRoleEntity> agentsList = industryRoleService.listByIds(agentIds);
-                    agentsList.forEach(agent -> orgIds.add(agent.getOrgId()));
-                }
+            List<Long> agentItemIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.AGENT.getCode());
+            if(!agentItemIds.isEmpty()) {
+                List<IndustryRoleEntity> agentsList = industryRoleService.listByIds(agentItemIds);
+                agentsList.forEach(agent -> orgIds.add(agent.getOrgId()));
             }
 
-            if(type.equals(WorkplaceItemTypeEnums.CHANNEL.getCode())){
-                List<Long> channelIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.CHANNEL.getCode());
-                if(!channelIds.isEmpty()) {
-                    List<ChannelEntity> channelList = channelService.listByIds(channelIds);
-                    channelList.forEach(channel -> orgIds.add(channel.getOrgId()));
-                }
+            List<Long> channelItemIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.CHANNEL.getCode());
+            if(!channelItemIds.isEmpty()) {
+                List<ChannelEntity> channelList = channelService.listByIds(channelItemIds);
+                channelList.forEach(channel -> orgIds.add(channel.getOrgId()));
             }
 
-            if(type.equals(WorkplaceItemTypeEnums.SCENE.getCode())){
-                List<Long> sceneIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.SCENE.getCode());
-                if(!sceneIds.isEmpty()) {
-                    List<SceneEntity> sceneList = sceneService.listByIds(sceneIds);
-                    sceneList.forEach(scene -> orgIds.add(scene.getOrgId()));
-                }
+            List<Long> sceneItemIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.SCENE.getCode());
+            if(!sceneItemIds.isEmpty()) {
+                List<SceneEntity> sceneList = sceneService.listByIds(sceneItemIds);
+                sceneList.forEach(scene -> orgIds.add(scene.getOrgId()));
             }
 
             // 批量获取所有组织信息
@@ -230,54 +160,122 @@ public class WorkplaceItemServiceImpl extends IBaseServiceImpl<WorkplaceItemEnti
                 orgList.forEach(org -> orgMap.put(org.getId(), org));
             }
 
-            // 主逻辑处理
-            if(type.equals(WorkplaceItemTypeEnums.AGENT.getCode())){
-                List<Long> agentIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.AGENT.getCode());
-                List<IndustryRoleEntity> agentsList = agentIds.isEmpty() ? Collections.emptyList() : industryRoleService.listByIds(agentIds);
+            // 获取智能体信息
+            List<Long> agentIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.AGENT.getCode());
+            List<IndustryRoleEntity> agentsList = agentIds.isEmpty() ? Collections.emptyList() : industryRoleService.listByIds(agentIds);
 
-                List<IndustryRoleOrgDto> agentsOrgList = new ArrayList<>();
-                for (IndustryRoleEntity industryRoleEntity : agentsList) {
-                    IndustryRoleOrgDto dto1 = new IndustryRoleOrgDto();
-                    BeanUtils.copyProperties(industryRoleEntity, dto1);
-                    dto1.setOrgName(orgMap.getOrDefault(industryRoleEntity.getOrgId(), new OrganizationDto()).getOrgName());
-                    agentsOrgList.add(dto1);
-                }
-                dto.setAgentsList(agentsOrgList);
+            List<CollectItemObjectDto> agentsOrgList = new ArrayList<>();
+            for (IndustryRoleEntity industryRoleEntity : agentsList) {
+                CollectItemObjectDto dto1 = new CollectItemObjectDto();
+                BeanUtils.copyProperties(industryRoleEntity, dto1);
+                dto1.setOrgName(orgMap.getOrDefault(industryRoleEntity.getOrgId(), new OrganizationDto()).getOrgName());
+                dto1.setType(WorkplaceItemTypeEnums.AGENT.getCode());
+
+                dto1.setId(industryRoleEntity.getId());
+                dto1.setItemId(industryRoleEntity.getId());
+                dto1.setTitle(industryRoleEntity.getRoleName());
+                dto1.setAvatar(industryRoleEntity.getRoleAvatar());
+                dto1.setDescription(industryRoleEntity.getResponsibilities());
+                dto1.setEditInfo(orgMap.getOrDefault(industryRoleEntity.getOrgId(), new OrganizationDto()).getOrgName());
+                dto1.setEditTime(DateFormatterUtils.getRelativeTime(industryRoleEntity.getAddTime()));
+
+                agentsOrgList.add(dto1);
             }
+            collectItemObjectDtos.addAll(agentsOrgList);
 
-            if(type.equals(WorkplaceItemTypeEnums.CHANNEL.getCode())){
-                List<Long> channelIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.CHANNEL.getCode());
-                List<ChannelEntity> channelList = channelIds.isEmpty() ? Collections.emptyList() : channelService.listByIds(channelIds);
+            // 获取频道信息
+            List<Long> channelIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.CHANNEL.getCode());
+            List<ChannelEntity> channelList = channelIds.isEmpty() ? Collections.emptyList() : channelService.listByIds(channelIds);
 
-                List<ChannelOrgDto> channelOrgList = new ArrayList<>();
-                for (ChannelEntity channelEntity : channelList) {
-                    ChannelOrgDto dto1 = new ChannelOrgDto();
-                    BeanUtils.copyProperties(channelEntity, dto1);
-                    dto1.setOrgName(orgMap.getOrDefault(channelEntity.getOrgId(), new OrganizationDto()).getOrgName());
-                    channelOrgList.add(dto1);
-                }
-                dto.setChannelsList(channelOrgList);
+            List<CollectItemObjectDto> channelOrgList = new ArrayList<>();
+            for (ChannelEntity channelEntity : channelList) {
+                CollectItemObjectDto dto1 = new CollectItemObjectDto();
+                BeanUtils.copyProperties(channelEntity, dto1);
+                dto1.setType(WorkplaceItemTypeEnums.CHANNEL.getCode());
+
+                dto1.setId(channelEntity.getId());
+                dto1.setItemId(channelEntity.getId());
+                dto1.setOrgName(orgMap.getOrDefault(channelEntity.getOrgId(), new OrganizationDto()).getOrgName());
+                dto1.setTitle(channelEntity.getChannelName());
+                dto1.setAvatar(channelEntity.getIcon());
+                dto1.setDescription(channelEntity.getChannelDesc());
+                dto1.setEditInfo(orgMap.getOrDefault(channelEntity.getOrgId(), new OrganizationDto()).getOrgName());
+                dto1.setEditTime(DateFormatterUtils.getRelativeTime(channelEntity.getAddTime()));
+
+                channelOrgList.add(dto1);
             }
+            collectItemObjectDtos.addAll(channelOrgList);
 
-            if(type.equals(WorkplaceItemTypeEnums.SCENE.getCode())){
-                List<Long> sceneIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.SCENE.getCode());
-                List<SceneEntity> sceneList = sceneIds.isEmpty() ? Collections.emptyList() : sceneService.listByIds(sceneIds);
+            // 获取场景信息
+            List<Long> sceneIds = filterAndMapItemIds(list, WorkplaceItemTypeEnums.SCENE.getCode());
+            List<SceneEntity> sceneList = sceneIds.isEmpty() ? Collections.emptyList() : sceneService.listByIds(sceneIds);
 
-                List<SceneOrgDto> sceneOrgList = new ArrayList<>();
-                for (SceneEntity sceneEntity : sceneList) {
-                    SceneOrgDto dto1 = new SceneOrgDto();
-                    BeanUtils.copyProperties(sceneEntity, dto1);
-                    dto1.setOrgName(orgMap.getOrDefault(sceneEntity.getOrgId(), new OrganizationDto()).getOrgName());
-                    sceneOrgList.add(dto1);
+            List<CollectItemObjectDto> sceneOrgList = new ArrayList<>();
+            for (SceneEntity sceneEntity : sceneList) {
+                CollectItemObjectDto dto1 = new CollectItemObjectDto();
+                BeanUtils.copyProperties(sceneEntity, dto1);
+                dto1.setType(WorkplaceItemTypeEnums.SCENE.getCode());
+
+                dto1.setOrgName(orgMap.getOrDefault(sceneEntity.getOrgId(), new OrganizationDto()).getOrgName());
+
+                dto1.setId(sceneEntity.getId());
+                dto1.setItemId(sceneEntity.getId());
+                dto1.setTitle(sceneEntity.getSceneName());
+                dto1.setAvatar(sceneEntity.getSceneBanner());
+                dto1.setSceneType(sceneEntity.getSceneType());
+
+                SceneInfoDto sceneInfo = SceneEnum.getSceneInfoByCode(sceneEntity.getSceneType()) ;
+                if(sceneEntity.getSceneType() != null && sceneInfo != null){
+                    dto1.setSceneTypeName(sceneInfo.getSceneName());
+                    dto1.setSceneIcon(sceneInfo.getIcon());
                 }
-                dto.setScenesList(sceneOrgList);
+                dto1.setDescription(sceneEntity.getSceneDesc());
+                dto1.setEditInfo(orgMap.getOrDefault(sceneEntity.getOrgId(), new OrganizationDto()).getOrgName());
+                dto1.setEditTime(DateFormatterUtils.getRelativeTime(sceneEntity.getAddTime()));
+
+                sceneOrgList.add(dto1);
             }
+            collectItemObjectDtos.addAll(sceneOrgList);
         } catch (Exception e) {
             log.error("Error occurred while processing item lists", e);
             throw new RpcServiceRuntimeException("查询列表数据异常");
         }
 
-        return dto;
+        return collectItemObjectDtos;
+    }
+
+    @Override
+    public void addCollectItem(CollectItemDto dto, Long workplaceId) {
+
+        String type = dto.getType();
+
+        LambdaQueryWrapper<WorkplaceItemEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(WorkplaceItemEntity::getWorkplaceId, workplaceId)
+                .eq(WorkplaceItemEntity::getItemId, dto.getItemId())
+                .eq(WorkplaceItemEntity::getItemType, type);
+
+        // 如果已存在，则不处理
+        if (count(queryWrapper) > 0) {
+            throw new RpcServiceRuntimeException("收藏已存在，如果 ");
+        }
+
+        WorkplaceItemEntity entity = new WorkplaceItemEntity();
+        entity.setWorkplaceId(workplaceId);
+        entity.setItemId(dto.getItemId());
+        entity.setItemType(type);
+
+        save(entity);
+    }
+
+    @Override
+    public void removeCollectItem(CollectItemDto dto, Long workplaceId) {
+
+        LambdaUpdateWrapper<WorkplaceItemEntity> queryWrapper = new LambdaUpdateWrapper<>();
+        queryWrapper.eq(WorkplaceItemEntity::getWorkplaceId, workplaceId)
+                .eq(WorkplaceItemEntity::getItemId, dto.getItemId())
+                .eq(WorkplaceItemEntity::getItemType, dto.getType());
+
+        remove(queryWrapper);
     }
 
     // 提取公共方法，用于根据 itemType 筛选并映射 itemId
