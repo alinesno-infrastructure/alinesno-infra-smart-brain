2b8f9768b078aa0ca975769c267232047d01c9a3
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:16 +0800

    fix:删除不必要的导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/mapper/PPTManagerMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/mapper/PPTManagerMapper.java
@@ -1,8 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.pptCreation.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
 import com.alinesno.infra.smart.scene.entity.PPTManagerEntity;
 import org.apache.ibatis.annotations.Mapper;
 
