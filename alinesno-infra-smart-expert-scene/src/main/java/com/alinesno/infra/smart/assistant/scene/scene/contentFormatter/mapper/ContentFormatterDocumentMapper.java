5cc83c07153971f8e2a3ec1e5370126548425b52
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:13 +0800

    feat:创建ContentFormatterDocumentMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterDocumentMapper.java
@@ -0,0 +1,8 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterDocumentEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutGroupEntity;
+
+public interface ContentFormatterDocumentMapper extends IBaseMapper<ContentFormatterDocumentEntity> {
+}    
