ac489c5dca0085a129aefdd24c7e993835af214d
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:13 +0800

    feat:创建文档服务接口IContentFormatterDocumentService

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterDocumentService.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterDocumentEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
+
+import java.io.File;
+
+/**
+ * 文档服务接口
+ */
+public interface IContentFormatterDocumentService extends IBaseService<ContentFormatterDocumentEntity> {
+
+    /**
+     * 保存上传的文档
+     * @param fileName
+     * @param targetFile
+     * @param sceneId
+     * @param query
+     * @return
+     */
+    Long saveUploadDocument(String fileName, File targetFile, long sceneId, PermissionQuery query);
+
+    /**
+     * 创建新的文档
+     * @param sceneId
+     * @param query
+     * @return
+     */
+    Long saveNewDocument(Long sceneId, PermissionQuery query);
+
+}
