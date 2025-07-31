e51c3884e90b2067d186083b31e22afd8ac2967b
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:14 +0800

    refactor:移除不必要的导入和注释，简化接口文件

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/IDeepSearchTaskService.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/IDeepSearchTaskService.java
@@ -1,14 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service;
 
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
-import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
-import com.alinesno.infra.smart.scene.entity.DeepSearchSceneEntity;
 import com.alinesno.infra.smart.scene.entity.DeepSearchTaskEntity;
-
-import java.util.List;
 
 public interface IDeepSearchTaskService extends IBaseService<DeepSearchTaskEntity> {
 
