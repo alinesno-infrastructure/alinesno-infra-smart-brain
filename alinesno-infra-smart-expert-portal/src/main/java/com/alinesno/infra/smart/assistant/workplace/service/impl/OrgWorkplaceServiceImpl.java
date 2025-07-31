48d993e32d0281bbae1c14eaac82443a3a831af4
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:15 +0800

    fix:移除未使用的导入语句

--- alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/OrgWorkplaceServiceImpl.java
+++ alinesno-infra-smart-expert-portal/src/main/java/com/alinesno/infra/smart/assistant/workplace/service/impl/OrgWorkplaceServiceImpl.java
@@ -22,7 +22,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
