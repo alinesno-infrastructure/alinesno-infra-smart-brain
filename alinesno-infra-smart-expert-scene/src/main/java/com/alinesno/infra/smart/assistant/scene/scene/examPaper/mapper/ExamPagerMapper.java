e54e33ae661db4643f55678ff6a87ab1a5216882
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:15 +0800

    fix:删除ExamPagerMapper.java中多余的导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/mapper/ExamPagerMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/mapper/ExamPagerMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
