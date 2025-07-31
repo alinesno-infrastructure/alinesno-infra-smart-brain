9286848a88460e983415247be23ac27f3bb75f9a
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:14 +0800

    fix:移除对ContentFormatterOfficeConfigEntity的无效引用

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRuleService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRuleService.java
@@ -3,7 +3,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.ContentRuleExcelParser;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterRuleEntity;
 
 import java.util.List;
