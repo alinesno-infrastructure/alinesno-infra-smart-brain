52f4c5edea386b331467f019a80df0d993408984
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:16 +0800

    fix:删除ExamQuestionMapper.java中不必要的导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/mapper/ExamQuestionMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/mapper/ExamQuestionMapper.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.examPaper.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
 import com.alinesno.infra.smart.scene.entity.ExamQuestionEntity;
 import org.apache.ibatis.annotations.Mapper;
 
