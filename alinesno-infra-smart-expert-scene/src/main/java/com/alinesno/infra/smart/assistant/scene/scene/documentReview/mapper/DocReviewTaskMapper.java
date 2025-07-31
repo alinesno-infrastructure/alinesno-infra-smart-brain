a5426afe084a3b4bfc0be922347438de8554abe1
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:16 +0800

    fix:移除DocReviewTaskMapper.java中的无用导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewTaskMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/mapper/DocReviewTaskMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.documentReview.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.scene.entity.DocReviewTaskEntity;
-import com.alinesno.infra.smart.scene.entity.GeneralAgentTaskEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
