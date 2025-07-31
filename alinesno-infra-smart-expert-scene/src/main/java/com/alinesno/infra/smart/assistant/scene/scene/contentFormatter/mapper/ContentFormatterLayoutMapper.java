685dcbfbc18bcfd7231c8c9feceae4bfa0a33e93
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:16 +0800

    fix:移除ContentFormatterParseEntity导入依赖

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterLayoutMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterLayoutMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
 
 public interface ContentFormatterLayoutMapper extends IBaseMapper<ContentFormatterLayoutEntity> {
 }    
