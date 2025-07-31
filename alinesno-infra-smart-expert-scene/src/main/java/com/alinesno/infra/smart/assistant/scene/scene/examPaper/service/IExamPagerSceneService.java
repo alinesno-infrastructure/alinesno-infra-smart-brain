782f5b840a732105c3a23853b01d333104ca6110
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:14 +0800

    refactor:移除不必要的导入依赖

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamPagerSceneService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamPagerSceneService.java
@@ -1,12 +1,10 @@
 package com.alinesno.infra.smart.assistant.scene.scene.examPaper.service;
 
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
 import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
-import com.alinesno.infra.smart.scene.entity.ExamPagerEntity;
 import com.alinesno.infra.smart.scene.entity.ExamPagerSceneEntity;
 
 import java.util.List;
