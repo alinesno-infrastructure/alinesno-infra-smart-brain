26b73f38571b0d4a42562dc6f4027c08d34b1b77
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:16 +0800

    fix:移除不必要的导入语句ContentFormatterOfficeConfigEntity

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRewriteService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterRewriteService.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterRewriteEntity;
 
 public interface IContentFormatterRewriteService extends IBaseService<ContentFormatterRewriteEntity> {
