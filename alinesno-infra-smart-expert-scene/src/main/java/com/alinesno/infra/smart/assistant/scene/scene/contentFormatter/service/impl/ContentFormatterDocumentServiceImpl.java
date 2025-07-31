fd177fae73ac516d999e89b75c24ed03dd80d496
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:13 +0800

    feat:新增文档操作接口实现类ContentFormatterDocumentServiceImpl

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterDocumentServiceImpl.java
@@ -0,0 +1,101 @@
+package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.core.utils.DateUtils;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.consumer.SmartDocumentConsumer;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto.DocumentTemplateDTO;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.enums.DocumentSourceEnums;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.enums.GroupTypeEnums;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterDocumentMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterLayoutGroupMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterLayoutMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterRuleMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterDocumentService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterLayoutGroupService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterOfficeConfigService;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.tools.DocxHtmlFormatterUtils;
+import com.alinesno.infra.smart.scene.entity.*;
+import com.alinesno.infra.smart.scene.enums.SceneScopeType;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 描述:文档操作接口
+ */
+@Slf4j
+@Service
+public class ContentFormatterDocumentServiceImpl extends IBaseServiceImpl<ContentFormatterDocumentEntity, ContentFormatterDocumentMapper> implements IContentFormatterDocumentService {
+
+    @Autowired
+    private IContentFormatterOfficeConfigService officeConfigService;
+
+    @Override
+    public Long saveUploadDocument(String fileName, File targetFile, long sceneId, PermissionQuery query) {
+
+        // fileName去掉后缀
+        String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf("."));
+
+        log.info("上传文件:{}", fileName);
+
+        // 1. 获取办公工具配置
+        ContentFormatterOfficeConfigEntity officeConfig = officeConfigService.getConfig(query.getOrgId());
+        if (officeConfig == null) {
+            throw new RuntimeException("未找到机构[" + query.getOrgId() + "]的办公工具配置");
+        }
+
+        // 2. 初始化智能文档处理器
+        SmartDocumentConsumer smartDocumentConsumer = new SmartDocumentConsumer();
+        smartDocumentConsumer.configure(officeConfig.getToolPath(), officeConfig.getRequestToken());
+
+         String htmlContent =smartDocumentConsumer.convertToHtml(targetFile).getBody() ;
+
+        ContentFormatterDocumentEntity entity = new ContentFormatterDocumentEntity();
+
+        entity.setSceneId(sceneId);
+        entity.setDocumentName(fileNameWithoutExtension) ;
+        entity.setDocumentDesc(DocxHtmlFormatterUtils.htmlToText(htmlContent , 100));
+        entity.setDocumentContent(htmlContent);
+        entity.setDocumentSource(DocumentSourceEnums.UPLOAD.getCode());
+        entity.setLastSavedTime(new Date());
+
+        // 权限复制
+        entity.setOperatorId(query.getOperatorId());
+        entity.setOrgId(query.getOrgId());
+        entity.setDepartmentId(query.getDepartmentId());
+
+        save(entity) ;
+
+        return entity.getId() ;
+    }
+
+    @Override
+    public Long saveNewDocument(Long sceneId, PermissionQuery query) {
+        ContentFormatterDocumentEntity entity = new ContentFormatterDocumentEntity();
+
+        entity.setSceneId(sceneId);
+        entity.setDocumentName("新建文档_" + DateUtils.dateTimeNow());
+        entity.setDocumentSource(DocumentSourceEnums.NEW.getCode());
+        entity.setLastSavedTime(new Date());
+
+        // 权限复制
+        entity.setOperatorId(query.getOperatorId());
+        entity.setOrgId(query.getOrgId());
+        entity.setDepartmentId(query.getDepartmentId());
+
+        save(entity) ;
+
+        return entity.getId() ;
+    }
+
+
+
+
+}
