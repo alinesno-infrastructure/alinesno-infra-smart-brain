d79fdab18f4c961ea88410dc588ec878a48ac057
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:15 +0800

    fix:移除IExamInfoService.java中未使用的导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamInfoService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/examPaper/service/IExamInfoService.java
@@ -4,7 +4,6 @@ import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamInfoPage;
-import com.alinesno.infra.smart.assistant.scene.scene.examPaper.dto.ExamMarkingDto;
 import com.alinesno.infra.smart.scene.entity.ExamInfoEntity;
 
 /**
