79c04b1b4861241cc752e3a58032f6b063b5b0b4
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:14 +0800

    fix:移除未使用的导入和冗余代码

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/impl/DeepSearchTaskServiceImpl.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/impl/DeepSearchTaskServiceImpl.java
@@ -1,26 +1,11 @@
 package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.impl;
 
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
-import com.alinesno.infra.common.facade.datascope.PermissionQuery;
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper.DeepSearchSceneMapper;
 import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper.DeepSearchTaskMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.IDeepSearchSceneService;
 import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.IDeepSearchTaskService;
-import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
-import com.alinesno.infra.smart.scene.dto.RoleListRequestDto;
-import com.alinesno.infra.smart.scene.dto.UpdateSceneAgentDto;
-import com.alinesno.infra.smart.scene.entity.DeepSearchSceneEntity;
 import com.alinesno.infra.smart.scene.entity.DeepSearchTaskEntity;
-import com.alinesno.infra.smart.utils.RoleUtils;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * 深度搜索场景服务实现类
