c8d5b9a702fc8c00caf63c3393e14fab1dca681c
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:14 +0800

    fix:移除PPTGenerateSceneMapper.java中的无用导入语句

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/mapper/PPTGenerateSceneMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/pptCreation/mapper/PPTGenerateSceneMapper.java
@@ -2,7 +2,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.pptCreation.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.smart.scene.entity.PPTGenerateSceneEntity;
-import com.alinesno.infra.smart.scene.entity.PPTManagerEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
