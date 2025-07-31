e39479623fa9bc2fa9aa438cc628ef9bca5aa276
陈伟(Victor) <victorAip@gmail.com>
2025-08-01 05:08:15 +0800

    refactor:移除不必要的导入依赖

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterLayoutGroupMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterLayoutGroupMapper.java
@@ -1,9 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutEntity;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterLayoutGroupEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterParseEntity;
 
 public interface ContentFormatterLayoutGroupMapper extends IBaseMapper<ContentFormatterLayoutGroupEntity> {
 }    
