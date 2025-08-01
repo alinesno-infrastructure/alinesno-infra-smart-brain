db555a6b5ab167d72caf94554fef0b50cedddd93
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 00:19:20 +0800

    fix:调整按钮样式和新增推荐开关功能

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/index.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/role/index.vue
@@ -120,14 +120,15 @@
                   </div>
                   <div class="switch-buttons">
                     <!-- 
-                    <el-switch size="small" v-model="item.hasRecommended" :active-value="true" :inactive-value="false" active-text="推荐" @change="handleRecommended(item.id)"></el-switch> 
                     <el-switch size="small" v-model="item.hasSale" :active-value="1" :inactive-value="0" active-text="分享" :disabled="item.saleFromRoleId" @change="handleChangeSaleField('hasSale', item.hasSale, item.id)"></el-switch>
+                    <el-switch size="small" v-model="item.hasRecommended" :active-value="true" :inactive-value="false" active-text="推荐" @change="handleRecommended(item.id)"></el-switch> 
                     -->
-                    <el-button type="primary" size="small" :disabled="item.saleFromRoleId" text  @click="pushOrg(item)">
+
+                    <el-button type="primary" size="small" :disabled="item.saleFromRoleId" text bg @click="pushOrg(item)">
                       <i class="fa-solid fa-truck-fast"></i> 
                     </el-button>
 
-                    <el-button type="primary" text size="small" @click="editWelcomePage(item)">
+                    <el-button type="primary" text bg size="small" @click="editWelcomePage(item)">
                       <i class="fa-solid fa-computer"></i> 
                     </el-button>
 
