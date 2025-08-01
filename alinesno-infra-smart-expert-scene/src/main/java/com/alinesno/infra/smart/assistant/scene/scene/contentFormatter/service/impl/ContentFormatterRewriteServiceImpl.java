469ecd366aa2b221fe7bf9687968e9137ef6aa08
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 00:19:19 +0800

    refactor:重构ContentFormatterRewriteServiceImpl类，改为使用ContentFormatterConfigEntity并新增更新配置方法

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterRewriteServiceImpl.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/impl/ContentFormatterRewriteServiceImpl.java
@@ -1,14 +1,57 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.dto.ContentFormatterRewriteConfigDto;
 import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper.ContentFormatterRewriteMapper;
-import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterRewriteService;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterRewriteEntity;
+import com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service.IContentFormatterConfigService;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterConfigEntity;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class ContentFormatterRewriteServiceImpl extends IBaseServiceImpl<ContentFormatterRewriteEntity, ContentFormatterRewriteMapper> implements IContentFormatterRewriteService {
+public class ContentFormatterRewriteServiceImpl extends IBaseServiceImpl<ContentFormatterConfigEntity, ContentFormatterRewriteMapper> implements IContentFormatterConfigService {
+
+    @Override
+    public void updateRewriteConfig(ContentFormatterRewriteConfigDto dto) {
+
+        LambdaQueryWrapper<ContentFormatterConfigEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ContentFormatterConfigEntity::getSceneId, dto.getSceneId())
+                .eq(ContentFormatterConfigEntity::getOrgId , dto.getOrgId()) ;
+
+        ContentFormatterConfigEntity entity = getOne(queryWrapper);
+
+        if(entity == null) {
+            entity = new ContentFormatterConfigEntity() ;
+
+            entity.setSceneId(dto.getSceneId());
+            entity.setOrgId(dto.getOrgId());
+            entity.setOperatorId(dto.getOperatorId());
+            entity.setDepartmentId(dto.getDepartmentId());
+
+            entity.setRewriteConfig(JSONObject.toJSONString(dto.getRewriteConfig()));
+            entity.setDataScope(dto.getDataScope());
+        }
+
+        saveOrUpdate(entity);
+    }
+
+    @Override
+    public void initConfig(PermissionQuery query) {
+
+        LambdaQueryWrapper<ContentFormatterConfigEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ContentFormatterConfigEntity::getOrgId , query.getOrgId()) ;
+
+        ContentFormatterConfigEntity entity = getOne(queryWrapper);
+
+        entity.setOrgId(query.getOrgId());
+        entity.setOperatorId(query.getOperatorId());
+        entity.setDepartmentId(query.getDepartmentId());
+
+        saveOrUpdate(entity);
+    }
 
 }
