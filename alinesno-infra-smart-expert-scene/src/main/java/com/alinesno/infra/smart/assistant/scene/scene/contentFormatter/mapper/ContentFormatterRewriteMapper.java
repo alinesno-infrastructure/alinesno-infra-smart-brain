58ce18d8488d602dd4f9e62d51de2c0f7bc304af
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 00:19:19 +0800

    chore:修改ContentFormatterRewriteMapper的导入类为ContentFormatterConfigEntity

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterRewriteMapper.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/mapper/ContentFormatterRewriteMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterRewriteEntity;
+import com.alinesno.infra.smart.scene.entity.ContentFormatterConfigEntity;
 
-public interface ContentFormatterRewriteMapper extends IBaseMapper<ContentFormatterRewriteEntity> {
+public interface ContentFormatterRewriteMapper extends IBaseMapper<ContentFormatterConfigEntity> {
 }    
