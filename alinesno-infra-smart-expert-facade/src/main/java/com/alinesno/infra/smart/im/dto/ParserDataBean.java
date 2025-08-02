afc05efe1dc14f9a27969bb43fb95fb25377de68
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 22:54:10 +0800

    feat:创建ParserDataBean类，包含解析数据的相关属性和构造方法

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/dto/ParserDataBean.java
@@ -0,0 +1,37 @@
+package com.alinesno.infra.smart.im.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 解析数据
+ */
+@NoArgsConstructor
+@Data
+public class ParserDataBean {
+
+    private int sort ;  // 解析顺序
+    private String name ;   // 文件名称
+    private String type ; // 类型(知识库/链接/记忆/附件)
+    private String textLength ; // 文本长度
+    private String content ; // 内容
+
+    public ParserDataBean(String name, int sort) {
+        this.name = name;
+        this.sort = sort;
+    }
+
+    public ParserDataBean(String name, int sort , String type) {
+        this.name = name;
+        this.sort = sort;
+        this.type = type ;
+    }
+
+    public ParserDataBean(String name, int sort , String type , String content) {
+        this.name = name;
+        this.sort = sort;
+        this.type = type ;
+        this.content = content ;
+        this.textLength = String.valueOf(content.length());
+    }
+}
