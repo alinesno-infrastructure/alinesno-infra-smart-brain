de9205093d068ab505de5d6cd16d434a7c2fb379
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 22:54:11 +0800

    feat:新增知识库引擎常量 PREFIX_ASSISTANT_RAG

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/assistant/enums/AssistantConstants.java
@@ -1,8 +1,9 @@
 package com.alinesno.infra.smart.assistant.enums;
 
+/**
+ * 智能助手常量类
+ */
 public interface AssistantConstants {
-
-    String PREFIX_ASSISTANT = "Agent_" ;
 
     // 默认脚本处理
     String PREFIX_ASSISTANT_DEFAULT = "Agent_DefaultExpert" ;
@@ -10,14 +11,14 @@ public interface AssistantConstants {
     // groovy脚本流程引擎
     String PREFIX_ASSISTANT_SCRIPT = "Agent_ScriptExpert" ;
 
+    // 知识库引擎
+    String PREFIX_ASSISTANT_RAG = "Agent_RagExpert" ;
+
     // 流程脚本引擎
     String PREFIX_ASSISTANT_FLOW = "Agent_FlowExpert" ;
 
     // 流程脚本引擎
     String PREFIX_ASSISTANT_REACT = "Agent_ReActExpert" ;
-
-    // 携带外部知识库
-    String PREFIX_ASSISTANT_REACT_OUTSIDE = "Agent_ReActOutsideExpert" ;
 
     // 简单角色设定Prompt
     String PREFIX_ASSISTANT_SIMPLE = "Agent_SimpleExpert" ;
