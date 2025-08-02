8044e13d3da3b026ab8178556b1a16ea4951369a
陈伟(Victor) <victorAip@gmail.com>
2025-08-02 22:54:10 +0800

    feat:创建MessageReferenceMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-domain/src/main/java/com/alinesno/infra/smart/assistant/mapper/MessageReferenceMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.smart.assistant.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.AgentSceneEntity;
+import com.alinesno.infra.smart.im.entity.MessageReferenceEntity;
+
+public interface MessageReferenceMapper extends IBaseMapper<MessageReferenceEntity> {
+
+}
