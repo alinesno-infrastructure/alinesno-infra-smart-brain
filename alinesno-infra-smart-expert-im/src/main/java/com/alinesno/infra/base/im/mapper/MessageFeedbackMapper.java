52019abbe9af238a24d26a84819679b3f6de9e21
李伟(Michael) <michaelAip@gmail.com>
2025-08-06 00:17:51 +0800

    feat:创建MessageFeedbackMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-im/src/main/java/com/alinesno/infra/base/im/mapper/MessageFeedbackMapper.java
@@ -0,0 +1,9 @@
+package com.alinesno.infra.base.im.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.im.entity.AccountChannelEntity;
+import com.alinesno.infra.smart.im.entity.MessageFeedbackEntity;
+
+public interface MessageFeedbackMapper extends IBaseMapper<MessageFeedbackEntity> {
+    
+}
