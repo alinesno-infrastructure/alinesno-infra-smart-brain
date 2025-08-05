28c4906c7ecc9f3d6b6560e014d2bf2d2b9471e4
李伟(Michael) <michaelAip@gmail.com>
2025-08-06 00:17:51 +0800

    feat:创建IMessageFeedbackService接口，定义消息反馈方法

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IMessageFeedbackService.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.smart.im.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.brain.api.dto.PromptMessageDto;
+import com.alinesno.infra.smart.im.dto.*;
+import com.alinesno.infra.smart.im.entity.MessageEntity;
+import com.alinesno.infra.smart.im.entity.MessageFeedbackEntity;
+
+import java.util.List;
+
+public interface IMessageFeedbackService extends IBaseService<MessageFeedbackEntity> {
+
+    /**
+     * 消息反馈
+     * @param dto
+     */
+    void messageFeedback(MessageFeedbackDto dto , Long userId , String userName);
+}
