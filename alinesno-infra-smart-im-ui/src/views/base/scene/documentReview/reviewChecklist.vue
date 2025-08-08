fdad8b909377b3f95ce3509dc58cfde9f83dc2d5
李伟(Michael) <michaelAip@gmail.com>
2025-08-09 00:20:15 +0800

    feat:优化审查清单界面，增加全选取消功能和规则展示样式

--- alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/reviewChecklist.vue
+++ alinesno-infra-smart-im-ui/src/views/base/scene/documentReview/reviewChecklist.vue
@@ -1,26 +1,49 @@
 <template>
     <div class="review-checklist-container">
-        <div class="header">
+        <div class="review-checklist-header">
             审查清单：智能生成
         </div>
-        <div class="rule-section">
-            <div class="rule-title">
+        <div class="review-checklist-rule-section">
+            <div class="review-checklist-rule-title">
                 全部规则({{ contractReviewList.length }})
+                <div class="check-actions">
+                    <el-button size="small" type="primary"
+                        @click="selectAll"
+                        :disabled="checkList.length === contractReviewList.length"
+                    ><i class="fa-solid fa-list-check"
+                        
+                    ></i> &nbsp; 全选</el-button>
+                    <el-button size="small" type="warning"
+                        @click="cancelSelection"
+                        :disabled="checkList.length === 0"
+                    > <i class="fa-solid fa-bars"></i> &nbsp;  取消</el-button>
+                </div>
             </div>
-            <el-scrollbar class="scrollable-area">
-                <div class="checkbox-group-container">
-                    <el-checkbox-group v-model="checkList" style="display: flex;flex-direction: column;">
+            <el-scrollbar class="scrollable-area review-checklist-scrollable-area" >
+                <div class="review-checklist-checkbox-group">
+                    <el-checkbox-group v-model="checkList" size="large">
                         <el-checkbox v-for="(item , index) in contractReviewList" :key="item.id" :label="item.id" size="large">
-                            <template #default>
-                                {{index+1}}.{{item.ruleName}}
-                            </template>
+                            <div class="review-checklist-item">
+                                {{index+1 < 10?'0' + (index +1): index}}
+                                <div class="review-checklist-item-content">
+                                    <span class="review-checklist-item-title">
+                                        {{item.ruleName}}
+                                    </span>
+                                    <span class="review-checklist-item-desc">
+                                        {{ truncateString(item.ruleContent,38)}}
+                                    </span>
+                                </div>
+                            </div>
                         </el-checkbox>
                     </el-checkbox-group>
                 </div>
             </el-scrollbar>
-            <div class="button-section">
-                <el-button type="primary" style="width:100%" size="large" @click="submitAuditForm" >
-                    <i class="fa-solid fa-rocket"></i> 发起文档审核
+            <div class="review-checklist-button-section">
+                <el-button type="primary" size="large" @click="submitAuditForm">
+                    <i class="fa-solid fa-rocket"></i> 发起文档审核 
+                    <span style="font-weight:bold;" v-if="checkList.length > 0">  
+                        （选择审核规则{{checkList.length}}条）
+                    </span>
                 </el-button>
             </div>
         </div>
@@ -51,10 +74,19 @@ const auditId = ref(null)
 const contractReviewList = ref([]);
 const checkList = ref([]);
 
+const selectAll = () => { 
+  checkList.value = contractReviewList.value.map(item => item.id);
+  console.log('checkList.value = ' + checkList.value)
+};
+
+const cancelSelection = () => { 
+  checkList.value = [];
+};
+
 const handleGetScene = () => {
     getScene(sceneId.value , taskId.value).then(res => {
-        currentSceneInfo.value = res.data;
-        contractReviewList.value = res.data.reviewListDtos;
+        currentSceneInfo.value = res.data; 
+        contractReviewList.value = res.data.reviewListDtos || [] ;
 
         if(currentSceneInfo.value.reviewListOption === 'aigen'){
             auditId.value = 0 ; 
@@ -68,6 +100,12 @@ const handleGetScene = () => {
 };
 
 const submitAuditForm = async () => {
+
+    if(checkList.value.length == 0){
+        ElMessage.warning("发起审核之前请先选择审核规则")
+        return ; 
+    }
+
     // 获取所有选择的id
     const selectedIds = checkList.value;
     const totalCount = selectedIds.length;
@@ -117,12 +155,129 @@ const submitAuditForm = async () => {
     emit('handleStepClick', 3);
 };
 
+const setCurrentTaskInfo = (info) => {
+    currentSceneInfo.value = info; 
+        contractReviewList.value = info.reviewListDtos || [] ;
+
+        if(currentSceneInfo.value.reviewListOption === 'aigen'){
+            auditId.value = 0 ; 
+        }else if(currentSceneInfo.value.reviewListOption === 'dataset'){
+            auditId.value = currentSceneInfo.value.auditId ;
+        }
+}
+
 onMounted(() => {
     handleGetScene();
 });
+
+defineExpose({
+  setCurrentTaskInfo,
+  handleGetScene
+})
 
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/document-review.scss';
+
+.review-checklist-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+.review-checklist-header {
+    font-size: 15px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    margin-top: 10px;
+}
+
+.review-checklist-rule-section {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+}
+
+.review-checklist-rule-title {
+margin-bottom: 15px;
+    padding: 10px 10px;
+    border-radius: 5px;
+    background: #f5f5f5;
+    font-size: 14px;
+
+        display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.review-checklist-scrollable-area { 
+    margin-bottom: 20px;
+    height:calc(100vh - 290px);
+}
+
+.review-checklist-checkbox-group {
+    display: flex;
+    flex-direction: column;
+    
+    .el-checkbox-group {
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .el-checkbox {
+        height: auto;
+        margin-bottom: 10px;
+    }
+}
+
+.review-checklist-item {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    width: 100%;
+    flex-direction: row;
+}
+
+.review-checklist-item-content {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px 15px 10px;
+    border-radius: 5px;
+    background: #f5f5f5;
+}
+
+.review-checklist-item-title {
+    font-size: 15px;
+}
+
+.review-checklist-item-desc {
+    font-size: 13px;
+    color: #a5a5a5;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
+    max-width: 100%;
+}
+
+.review-checklist-button-section .el-button {
+    width: 100%;
+}
+
+.check-actions {
+    display: flex;
+    gap: 8px;
+    
+    .el-button {
+        padding: 6px 12px;
+        font-size: 12px;
+        
+        i {
+            margin-right: 4px;
+        }
+    }
+}
+
 </style>
