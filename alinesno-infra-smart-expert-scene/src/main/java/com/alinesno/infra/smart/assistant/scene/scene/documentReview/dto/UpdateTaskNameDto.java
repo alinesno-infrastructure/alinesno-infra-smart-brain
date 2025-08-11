6b2061283ea212edf172de3d20f1c90c74d16c04
张婷(Tina) <tinaAip@gmail.com>
2025-08-12 00:19:26 +0800

    feat:创建修改任务名称的DTO类UpdateTaskNameDto

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/UpdateTaskNameDto.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/**
+ * 修改文档名称的DTO
+ */
+@Data
+public class UpdateTaskNameDto {
+
+    @NotNull(message = "任务ID不能为空")
+    private Long taskId;
+
+    @NotBlank(message = "任务名称不能为空")
+    private String taskName ;
+
+}
