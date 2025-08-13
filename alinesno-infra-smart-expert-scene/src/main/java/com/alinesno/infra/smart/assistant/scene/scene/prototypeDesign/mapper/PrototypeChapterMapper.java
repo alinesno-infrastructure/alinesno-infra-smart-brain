dad63900802a99e5711f5578aa683c39eb2bf880
李娜(Allen) <allenAip@gmail.com>
2025-08-14 00:16:25 +0800

    更新:alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/prototypeDesign/mapper/PrototypeChapterMapper.java

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/prototypeDesign/mapper/PrototypeChapterMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alinesno.infra.smart.assistant.scene.scene.prototypeDesign.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.smart.scene.entity.PrototypeChapterEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PrototypeChapterMapper extends IBaseMapper<PrototypeChapterEntity> {
-}
