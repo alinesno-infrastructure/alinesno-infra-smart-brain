cc5774fd24b9009199cfda18fc5b442526524943
李娜(Allen) <allenAip@gmail.com>
2025-08-14 00:16:26 +0800

    feat:创建AgentPointAnnotation注解类，用于智能体积分相关配置

---
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/point/annotation/AgentPointAnnotation.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.smart.point.annotation;
+
+import com.alinesno.infra.smart.point.enums.ParamSource;
+
+import java.lang.annotation.*;
+
+/**
+ * 智能体积分注解，针对于智能体聊天类型
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface AgentPointAnnotation {
+
+    /**
+     * 智能体编号
+     * @return
+     */
+    String agentCode() default  "" ;
+
+    /**
+     * roleId参数获取形式(params从url中获取/body则从对象里面获取)、默认名称为roleId
+     *
+     * @return
+     */
+    ParamSource paramType() default ParamSource.PARAMS;
+
+    /**
+     * 获取参数名称
+     * @return
+     */
+    String paramName() default "roleId";
+}
