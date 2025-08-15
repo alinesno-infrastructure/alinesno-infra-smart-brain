27c350567f0928a153cc9ebf24596a109b6c6165
陈伟(Victor) <victorAip@gmail.com>
2025-08-16 00:18:05 +0800

    feat:创建通用助手提示类GeneralAgentPrompt，包含生成结果提示方法

---
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/generalAgent/prompt/GeneralAgentPrompt.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.smart.assistant.scene.scene.generalAgent.prompt;
+
+import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.scene.entity.GeneralAgentTemplateEntity;
+
+/**
+ * 通用助手提示
+ */
+public class GeneralAgentPrompt {
+
+    /**
+     * 生成结果提示词
+     *
+     * @param taskInfo
+     * @param template
+     * @return
+     */
+    public static String getResultPrompt(MessageTaskInfo taskInfo, GeneralAgentTemplateEntity template) {
+    }
+}
