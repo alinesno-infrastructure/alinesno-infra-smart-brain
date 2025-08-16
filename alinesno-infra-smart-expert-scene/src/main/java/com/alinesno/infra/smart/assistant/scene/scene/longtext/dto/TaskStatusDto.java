36e47c5cf0096d32e0679b02c1f81d8bb909c914
李伟(Michael) <michaelAip@gmail.com>
2025-08-16 13:05:13 +0800

    feat:在TaskStatusDto中新增任务状态相关字段

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/dto/TaskStatusDto.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/dto/TaskStatusDto.java
@@ -1,5 +1,8 @@
 package com.alinesno.infra.smart.assistant.scene.scene.longtext.dto;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 
 /**
@@ -12,5 +15,11 @@ public class TaskStatusDto {
     private Long taskId;
     private String status;
     private int progress;
+
+    // --->>>>>>>>>>>> 任务状态_start ---->>>>>>>>>>>>
+    private String gentContentStatus ;
+    private String genPlanStatus;
+    private String genResultStatus ;
+    // --->>>>>>>>>>>> 任务状态_end ---->>>>>>>>>>>>
 
 }
