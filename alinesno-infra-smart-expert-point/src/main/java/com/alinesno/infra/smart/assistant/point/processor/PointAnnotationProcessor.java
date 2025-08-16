a07b004dbe478034b6495ba296deac9fb07e113b
周健(James) <jamesAip@gmail.com>
2025-08-17 00:15:08 +0800

    feat:在PointAnnotationProcessor接口中新增orgId参数

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/PointAnnotationProcessor.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/PointAnnotationProcessor.java
@@ -2,6 +2,18 @@ package com.alinesno.infra.smart.assistant.point.processor;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+/**
+ * 拦截的工具类
+ */
 public interface PointAnnotationProcessor {
-    void process(HttpServletRequest request, Long userId) throws Exception;
+
+    /**
+     * 拦截解析用户操作积分
+     * @param request
+     * @param userId
+     * @param orgId
+     * @throws Exception
+     */
+    void process(HttpServletRequest request, Long userId , Long orgId) throws Exception;
+
 }
