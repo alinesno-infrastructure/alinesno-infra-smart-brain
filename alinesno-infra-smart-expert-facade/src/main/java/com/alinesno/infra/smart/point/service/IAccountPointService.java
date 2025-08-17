945a0d4fa022a884fd1c632a194b60ec80e4392c
李娜(Allen) <allenAip@gmail.com>
2025-08-18 00:18:00 +0800

    feat:在IAccountPointService接口中新增会话和场景任务相关方法

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/point/service/IAccountPointService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/point/service/IAccountPointService.java
@@ -5,6 +5,40 @@ package com.alinesno.infra.smart.point.service;
  */
 public interface IAccountPointService {
 
+    /**
+     * 是否开启积分
+     * @return
+     */
     boolean isOpenPoint();
+
+    /**
+     * 开始会话
+     */
+    void startSession(long userId , long orgId , long roleId);
+
+    /**
+     * 结束会话
+     */
+    void endSession(long userId , long orgId , long roleId);
+
+    /**
+     * 获取指定组织的活跃会话数
+     */
+    public long getOrgSessionCount(long orgId) ;
+
+    /**
+     * 开始场景任务
+     */
+    void startSceneTask(long userId , long orgId , long roleId , long taskId);
+
+    /**
+     * 结束场景任务
+     */
+    void endSceneTask(long userId , long orgId , long roleId , long taskId);
+
+    /**
+     * 获取指定组织的场景任务数
+     */
+    public long getOrgSceneTaskCount(long orgId) ;
 
 }
