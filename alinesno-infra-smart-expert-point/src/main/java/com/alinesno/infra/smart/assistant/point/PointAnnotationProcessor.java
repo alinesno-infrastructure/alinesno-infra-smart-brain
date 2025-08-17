c08e7d03e741c54f2b421379c86cd9195fd7d43c
李娜(Allen) <allenAip@gmail.com>
2025-08-18 00:17:59 +0800

    feat:创建积分拦截处理接口PointAnnotationProcessor

---
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/PointAnnotationProcessor.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.smart.assistant.point;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * 拦截的工具类
+ */
+public interface PointAnnotationProcessor {
+
+    /**
+     * 拦截解析用户操作积分
+     * @param request
+     * @param userId
+     * @param orgId
+     * @throws Exception
+     */
+    void process(HttpServletRequest request, Long userId , Long orgId) ;
+
+}
