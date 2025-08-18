4fb92796b77eff4c21f3d9abc1466d27707abe2f
李伟(Michael) <michaelAip@gmail.com>
2025-08-19 00:18:19 +0800

    feat:引入依赖服务并初始化Spring管理的Bean

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/PointProcessorParent.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/PointProcessorParent.java
@@ -1,7 +1,12 @@
 package com.alinesno.infra.smart.assistant.point;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alinesno.infra.common.web.log.utils.SpringUtils;
+import com.alinesno.infra.smart.assistant.adapter.AipPointConsumer;
+import com.alinesno.infra.smart.assistant.service.IIndustryRoleService;
 import com.alinesno.infra.smart.point.enums.ParamSource;
+import com.alinesno.infra.smart.point.service.IAccountPointService;
+import com.alinesno.infra.smart.scene.service.ISceneService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,6 +15,11 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public abstract class PointProcessorParent implements PointAnnotationProcessor {
+
+    protected IAccountPointService accountPointService = SpringUtils.getBean(IAccountPointService.class) ;
+    protected ISceneService sceneService = SpringUtils.getBean(ISceneService.class) ;
+    protected IIndustryRoleService roleService = SpringUtils.getBean(IIndustryRoleService.class) ;
+    protected AipPointConsumer aipPointConsumer = SpringUtils.getBean(AipPointConsumer.class) ;
 
     /**
      * 从请求中获取roleId
