b9e5b57d48f3df4b1768dbeaab3aaf850637d168
李伟(Michael) <michaelAip@gmail.com>
2025-08-19 00:18:19 +0800

    feat:增加agentMaxConcurrent配置，用于控制并发会话数限制

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/AgentPointProcessor.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/AgentPointProcessor.java
@@ -6,8 +6,10 @@ import com.alinesno.infra.smart.point.annotation.AgentPointAnnotation;
 import com.alinesno.infra.smart.point.enums.ParamSource;
 import com.alinesno.infra.smart.point.service.IAccountPointService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -16,18 +18,20 @@ public class AgentPointProcessor extends PointProcessorParent {
 
     private final AgentPointAnnotation agentPointAnnotation;
 
+    @Setter
+    private int agentMaxConcurrent ;
+
     public AgentPointProcessor(AgentPointAnnotation agentPointAnnotation) {
         this.agentPointAnnotation = agentPointAnnotation;
     }
 
     @Override
     public void process(HttpServletRequest request, Long userId, Long orgId) {
-        IAccountPointService accountPointService = SpringUtils.getBean(IAccountPointService.class) ;
 
         String roleId = getRoleIdFromRequest(request, agentPointAnnotation.paramName() , ParamSource.PARAMS) ;
         long chatCount = accountPointService.getOrgSessionCount(orgId) ;
 
         log.debug("处理AgentPoint注解 - userId: {} , roleId = {} , chatCount = {}", userId , roleId , chatCount);
-        Assert.isTrue(chatCount < 3 , "当前并发会话数已超组织限制.");
+        Assert.isTrue(chatCount < agentMaxConcurrent , "当前并发会话数已超组织限制,会话并发数:" + agentMaxConcurrent);
     }
 }
