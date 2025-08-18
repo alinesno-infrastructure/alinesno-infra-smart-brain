baa4c5f7d8623d14091c64b24bee9f1aaf72e6a6
李伟(Michael) <michaelAip@gmail.com>
2025-08-19 00:18:19 +0800

    feat:增加场景任务并发控制逻辑，限制组织场景任务数

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/ScenePointProcessor.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/ScenePointProcessor.java
@@ -1,16 +1,23 @@
 package com.alinesno.infra.smart.assistant.point.processor;
 
+import com.alinesno.infra.common.web.log.utils.SpringUtils;
 import com.alinesno.infra.smart.assistant.point.PointProcessorParent;
 import com.alinesno.infra.smart.point.annotation.ScenePointAnnotation;
 import com.alinesno.infra.smart.point.enums.ParamSource;
+import com.alinesno.infra.smart.point.service.IAccountPointService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 @Slf4j
 public class ScenePointProcessor extends PointProcessorParent {
 
     private final ScenePointAnnotation scenePointAnnotation ;
+
+    @Setter
+    private int sceneMaxConcurrent;
 
     public ScenePointProcessor(ScenePointAnnotation scenePointAnnotation) {
         this.scenePointAnnotation = scenePointAnnotation ;
@@ -18,7 +25,11 @@ public class ScenePointProcessor extends PointProcessorParent {
 
     @Override
     public void process(HttpServletRequest request, Long userId, Long orgId) {
-        String roleId = getRoleIdFromRequest(request, scenePointAnnotation.paramName() , ParamSource.PARAMS) ;
-        log.debug("处理ScenePoint注解:{}" , orgId);
+
+        String taskId = getRoleIdFromRequest(request, scenePointAnnotation.paramName() , ParamSource.PARAMS) ;
+        long sceneCount = accountPointService.getOrgSceneTaskCount(orgId)  ;
+
+        log.debug("处理ScenePoint注解 - userId: {} , taskId = {} , sceneCount = {}", userId , taskId , sceneCount);
+        Assert.isTrue(sceneCount < sceneMaxConcurrent , "当前场景任务数已超过组织限制,组织并发数:" + sceneMaxConcurrent);
     }
 }
