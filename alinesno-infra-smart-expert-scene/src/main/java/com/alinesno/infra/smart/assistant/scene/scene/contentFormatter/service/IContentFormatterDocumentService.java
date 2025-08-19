637411acd0a419220297fbb96c1aad7abd327aa1
王磊(Levi) <leviAip@gmail.com>
2025-08-20 00:17:55 +0800

    feat:新增saveUploadDocument方法参数storageId

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterDocumentService.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/contentFormatter/service/IContentFormatterDocumentService.java
@@ -3,7 +3,6 @@ package com.alinesno.infra.smart.assistant.scene.scene.contentFormatter.service;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.smart.scene.entity.ContentFormatterDocumentEntity;
-import com.alinesno.infra.smart.scene.entity.ContentFormatterOfficeConfigEntity;
 
 import java.io.File;
 
@@ -14,13 +13,15 @@ public interface IContentFormatterDocumentService extends IBaseService<ContentFo
 
     /**
      * 保存上传的文档
+     *
      * @param fileName
      * @param targetFile
      * @param sceneId
      * @param query
+     * @param storageId
      * @return
      */
-    Long saveUploadDocument(String fileName, File targetFile, long sceneId, PermissionQuery query);
+    Long saveUploadDocument(String fileName, File targetFile, long sceneId, PermissionQuery query, String storageId);
 
     /**
      * 创建新的文档
