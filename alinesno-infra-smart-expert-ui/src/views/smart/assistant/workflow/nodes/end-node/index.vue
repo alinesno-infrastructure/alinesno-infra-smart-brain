956ef4e9588119b90962e6842ebffd9ef6a23585
李伟(Michael) <michaelAip@gmail.com>
2025-09-23 21:52:32 +0800

    feat:添加结束节点组件，包含设置表单和Markdown编辑器功能。

---
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/workflow/nodes/end-node/index.vue
@@ -0,0 +1,146 @@
+<template>
+  <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
+
+    <!-- 节点设置部分 -->
+    <div class="node-settings">
+      <!-- 节点设置表单区域 -->
+      <div class="settings-form">
+        <el-form :model="formData" label-width="auto" label-position="top">
+
+          <el-form-item label="指定结束内容">
+            <div class="function-CodemirrorEditor mb-8" style="height: 120px;width:100%">
+              <ScriptEditorPanel ref="auditEditorRef" :lang="'markdown'" />
+              <div class="function-CodemirrorEditor__footer">
+                <el-button text @click="openCodemirrorDialog" style="background-color: transparent !important;" class="magnify">
+                  <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                </el-button>
+              </div>
+            </div>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+
+    <el-dialog
+      v-model="dialogVisible"
+      :title="'函数内容（Markdown）'"
+      append-to-body
+      width="800"
+    >
+      <template #header>
+        <div class="dialog-footer mt-24" style="display: flex;align-items: center; justify-content: space-between; ">
+          <div>
+            请输入指定回答内容
+          </div>
+          <div>
+            <el-button type="primary" size="large" text bg @click="confirmAndSave"> 确定保存 </el-button>
+          </div>
+        </div>
+      </template>
+      <ScriptEditorFullPanel ref="auditFullEditorRef" :lang="'markdown'" />
+    </el-dialog>
+
+  </FlowContainer>
+</template>
+
+<script setup>
+import { set } from 'lodash'
+import { ref, reactive } from 'vue'
+
+import FlowContainer from '@/views/smart/assistant/workflow/common/FlowContainer'
+import ScriptEditorPanel from '@/views/smart/assistant/workflow/components/ScriptEditor';
+import ScriptEditorFullPanel from '@/views/smart/assistant/workflow/components/NodeScriptEditor';
+
+// 定义组件接收的属性
+const props = defineProps({
+  properties: {
+    type: Object,
+    default: () => ({})
+  },
+  isSelected: {
+    type: Boolean,
+    default: false
+  },
+  nodeModel: {
+    type: Object
+  }
+});
+
+const auditEditorRef = ref(null)
+const auditFullEditorRef = ref(null)
+
+// 绑定选择框的值
+const dialogVisible = ref(false)
+const cloneContent = ref('')
+
+// 绑定选择框的值
+const chatDataCode = ref('')
+const value1 = ref('')
+
+const replayType = ref('text')
+
+// 表单数据对象
+const form = reactive({
+  replayType: 'text',  // 回复方式 
+  replayContent: '',
+})
+
+const formData = computed({
+  get: () => {
+    if (props.nodeModel.properties.node_data) {
+      return props.nodeModel.properties.node_data
+    } else {
+      set(props.nodeModel.properties, 'node_data', form)
+    }
+    return props.nodeModel.properties.node_data
+  },
+  set: (value) => {
+    set(props.nodeModel.properties, 'node_data', value)
+  }
+})
+
+// 确认保存
+function confirmAndSave() {
+  console.log('confirmAndSave')
+
+  chatDataCode.value = auditFullEditorRef.value.getRawScript()
+  auditEditorRef.value.setRawScript(chatDataCode.value)
+
+  console.log('auditFullEditorRef.value.getRawScript() = ' + auditFullEditorRef.value.getRawScript())
+  dialogVisible.value = false
+
+  // 配置
+  formData.value.replayContent = chatDataCode.value
+}
+
+function openCodemirrorDialog() {
+
+  console.log('auditEditorRef.value.getRawScript() = ' + auditEditorRef.value.getRawScript());
+
+  chatDataCode.value = auditEditorRef.value.getRawScript();
+  console.log('chatDatacode = ' + chatDataCode.value)
+  dialogVisible.value = true
+
+  nextTick(() => {
+    auditFullEditorRef.value.setRawScript(chatDataCode.value)
+  })
+}
+
+onMounted(() => {
+  if(formData.value.replayContent){
+    auditEditorRef.value.setRawScript(formData.value.replayContent)
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+.function-CodemirrorEditor__footer {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+}
+
+</style>
