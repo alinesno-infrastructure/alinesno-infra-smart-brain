c0771fdd7fa2c1a715b96af5ba61e0be8b410e8e
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:36 +0800

    fix:优化ScenePointProcessor处理逻辑，增加场景类型日志记录和并发控制校验

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/ScenePointProcessor.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/ScenePointProcessor.java
@@ -1,23 +1,16 @@
 package com.alinesno.infra.smart.assistant.point.processor;
 
-import com.alinesno.infra.common.web.log.utils.SpringUtils;
 import com.alinesno.infra.smart.assistant.point.PointProcessorParent;
 import com.alinesno.infra.smart.point.annotation.ScenePointAnnotation;
-import com.alinesno.infra.smart.point.enums.ParamSource;
-import com.alinesno.infra.smart.point.service.IAccountPointService;
+import com.alinesno.infra.smart.scene.enums.SceneEnum;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Slf4j
 public class ScenePointProcessor extends PointProcessorParent {
 
     private final ScenePointAnnotation scenePointAnnotation ;
-
-    @Setter
-    private int sceneMaxConcurrent;
 
     public ScenePointProcessor(ScenePointAnnotation scenePointAnnotation) {
         this.scenePointAnnotation = scenePointAnnotation ;
@@ -26,10 +19,13 @@ public class ScenePointProcessor extends PointProcessorParent {
     @Override
     public void process(HttpServletRequest request, Long userId, Long orgId) {
 
-        String taskId = getRoleIdFromRequest(request, scenePointAnnotation.paramName() , ParamSource.PARAMS) ;
+        String sceneId = getRoleIdFromRequest(request, scenePointAnnotation.paramName()) ;
+        SceneEnum sceneType = scenePointAnnotation.sceneCode() ;
         long sceneCount = accountPointService.getOrgSceneTaskCount(orgId)  ;
 
-        log.debug("处理ScenePoint注解 - userId: {} , taskId = {} , sceneCount = {}", userId , taskId , sceneCount);
-        Assert.isTrue(sceneCount < sceneMaxConcurrent , "当前场景任务数已超过组织限制,组织并发数:" + sceneMaxConcurrent);
+        int sceneMaxConcurrent = getIntegral(orgId).getSceneMaxConcurrency() ;
+
+        log.debug("处理ScenePoint注解 - userId: {} , taskId = {} , sceneCount = {} , sceneType = {}", userId , sceneId , sceneCount , sceneType.name());
+        Assert.isTrue(sceneCount < sceneMaxConcurrent  , "当前场景任务数已超过组织限制,组织并发数:" + sceneMaxConcurrent);
     }
 }
