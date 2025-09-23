771e8df44633b10dbe906d0197b3c3a73644580e
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:39 +0800

    fix:修复FormatMessageTool.java文件中的格式化逻辑和注释内容

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/tools/FormatMessageTool.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/longtext/tools/FormatMessageTool.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.IdUtil;
 import com.agentsflex.core.llm.Llm;
 import com.agentsflex.core.llm.LlmConfig;
 import com.agentsflex.core.message.AiMessage;
+import com.alinesno.infra.common.core.utils.StringUtils;
 import com.alinesno.infra.smart.assistant.adapter.service.ILLmAdapterService;
 import com.alinesno.infra.smart.assistant.api.WorkflowExecutionDto;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
@@ -65,9 +66,6 @@ public class FormatMessageTool {
          ##任务
          你要编写以下章节当中的指定内容，请联系上下文进行编写:
          
-         ## 所有章节
-         %s
-         
          ## 需要编写的章节
          %s:%s
          
@@ -78,7 +76,6 @@ public class FormatMessageTool {
              正确示例，返回markdown内容
              ###标题
              这个是标题的内容
-             
              错误示例，带有markdown代码
              ```markdown
              ###标题
@@ -98,7 +95,6 @@ public class FormatMessageTool {
 
         return String.format(formatContent ,
                 chapterPromptContent ,
-                allChapterContent ,
                 dto.getChapterTitle() ,
                 dto.getChapterDescription());
     }
@@ -112,30 +108,28 @@ public class FormatMessageTool {
          ##角色：你是一个专业的大纲创作智能体，非常善于针对大纲领域，创作一个超长文档的大纲，并且对大纲中每一部分的编写要求给出具体指导，目的是方便将创作任务拆分为多个部分，可以分配给不同的智能体去完成。
          ##任务：请你针对用户的需求还有对应知识内容，结合模板编写出针对于客户场景需求的的大纲规划，严格遵循下面的规则要求，调取你的专业领域数据，创作一份满足用户大纲，注意不是内容本身，而是大纲，具体内容将会有多个智能体按照你创作的大纲进行编写。
          
-         ##大纲模板格式要求
-         %s:%s
          %s
-          
          ##规则：
           1-在创作大纲的时候，请你主要【附件资料】为主，调取你这个领域的专业数据进行创作大纲。
           2-在创作大纲的时候，请你平均拆分为N个部分，未来将有N个不同的智能体按照你的大纲进行创作具体的文字内容。请你保证N个部分边界清晰，没有重叠，避免冲突。
           3-在你创作的大纲的N个部分的时候，每个部分需要给出一个标题，然后给出一个具体的编写这部分内容编写要求，目的是给下一步拆分后的创作智能体参考。
           4-在你创作的N个部分编写要求中，除了对这部分的内容进行描述，也就是创作什么具体的内容，编写要求要明确内容范围。
-          5-在编写要求中，请你根据用户的文字总数进行计算，对每一个部分创作具体内容时候要求的字数，给出要求。如果用户没有给出字数要求，请你按照每一标题内容要求在1000字以上。
-          6-最终请你下面的格式进行直接输出，不要输出其他信息，格式：
+          6-在编写要求时，如果是非叶子节点，不能与叶子节点要求重复，则要求对叶子节点进行总结，避免与叶子节点内容重复，不能与叶子节点的内容进行重复。
+          7-在编写要求中，请你根据用户的文字总数进行计算，对每一个部分叶子节点创作具体内容时候要求的字数，给出要求。如果用户没有给出字数要求，请你按照每一叶子节点标题内容要求在1000字以上。
+          8-最终请你下面的格式进行直接输出，不要输出其他信息，格式：
            ```json
           [
               {
                   "label": "第一部分大纲标题",
-                  "description": "第一部分编写要求",
+                  "description": "第一部分编写要求，如果是非叶子节点，则要求对叶子节点进行总结，避免与叶子节点内容重复",
                   "children": [
                       {
                           "label": "第一部分大纲子标题1",
-                          "description": "第一部分大纲子标题1编写要求"
+                          "description": "第一部分大纲子标题1编写要求，如果是非叶子节点，则要求对叶子节点进行总结，避免与叶子节点内容重复"
                           "children": [
                               {
                                   "label": "第一部分大纲子标题1子标题1",
-                                  "description": "第一部分大纲子标题1子标题1编写要求"
+                                  "description": "第一部分大纲子标题1子标题1编写要求，如果是非叶子节点，则要求对叶子节点进行总结，避免与叶子节点内容重复"
                                   "children": [
                                       {
                                           "label": "第一部分大纲子标题1子标题1子标题1",
@@ -168,11 +162,24 @@ public class FormatMessageTool {
          ##附件资料=%s
          """;
 
+        String templatePrompt = """
+               ##大纲模板格式要求
+               %s:%s
+               %s
+                """ ;
+
+       String templatePromptContent = StringUtils.EMPTY;
+       if(longTextTemplate != null) {
+          templatePromptContent = String.format(
+                   templatePrompt ,
+                   longTextTemplate.getName() ,
+                   longTextTemplate.getDescription() ,
+                   longTextTemplate.getPrompt()) ;
+       }
+
         taskInfo.setQueryText(taskInfo.getText());
         taskInfo.setText(String.format(formatContent ,
-                longTextTemplate.getName() ,
-                longTextTemplate.getDescription() ,
-                longTextTemplate.getPrompt() ,
+                templatePromptContent ,
                 taskInfo.getText() ,
                 taskInfo.getFullContent()));
 
