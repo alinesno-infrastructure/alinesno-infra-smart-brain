a32c6ec10a9f257ae9d181732f169c3b1526d65a
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:31 +0800

    feat:创建GEConditionVerifier类，实现条件验证功能

---
+++ alinesno-infra-smart-expert-workflow/src/main/java/com/alinesno/infra/smart/assistant/workflow/nodes/step/compare/GEConditionVerifier.java
@@ -0,0 +1,26 @@
+package com.alinesno.infra.smart.assistant.workflow.nodes.step.compare;
+
+import java.util.Collection;
+
+public class GEConditionVerifier implements ConditionVerifier {
+    @Override
+    public boolean isSupported(String compare) {
+        return compare.equals(CompareTypeEnums.GE.getValue());
+    }
+    @Override
+    public boolean verify(Object sourceValue, String targetValue) {
+        if (sourceValue instanceof Collection){
+            return ((Collection<?>) sourceValue).size()>= Integer.parseInt(targetValue);
+        }
+        if (sourceValue instanceof Float){
+            return (float) sourceValue>= Float.parseFloat(targetValue);
+        }
+        if (sourceValue instanceof Double){
+            return (double) sourceValue>= Double.parseDouble(targetValue);
+        }
+        if (sourceValue instanceof Integer){
+            return (int) sourceValue>= Integer.parseInt(targetValue);
+        }
+        return false;
+    }
+}
