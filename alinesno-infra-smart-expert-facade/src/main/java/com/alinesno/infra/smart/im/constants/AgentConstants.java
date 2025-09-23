ca1458002e79c98294b5d936e0206d9ae35311f4
林晓(Lily) <lilyAip@gmail.com>
2025-09-23 21:52:41 +0800

    feat:添加STEP_ERROR常量，表示节点错误状态

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/constants/AgentConstants.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/constants/AgentConstants.java
@@ -6,6 +6,7 @@ public interface AgentConstants {
     String STEP_START = "start" ; // 节点开始
     String STEP_PROCESS = "process" ; // 节点任务进行中
     String STEP_FINISH = "finish" ;  //  节点结束
+    String STEP_ERROR = "error" ;  // 节点错误
     String EMPTY_RESULT = "" ;  //  空结果
 
     long ORG_AGENT_STORE_TYPE_ID = 9527L ;  // 角色类型ID
