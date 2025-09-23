d75a003b366c93e28c5daed5316b3105d6127ea4
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:39 +0800

    fix:在消息发布方法中添加异常日志记录

--- alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/event/StreamMessagePublisher.java
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/event/StreamMessagePublisher.java
@@ -3,10 +3,15 @@ package com.alinesno.infra.smart.assistant.adapter.event;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
 import com.alinesno.infra.smart.im.event.StreamMessageEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+/**
+ * StreamMessagePublisher
+ */
+@Slf4j
 @Component
 public class StreamMessagePublisher {
 
@@ -14,13 +19,23 @@ public class StreamMessagePublisher {
     private ApplicationEventPublisher applicationEventPublisher;
 
     public void doStuffAndPublishAnEvent(final String message , IndustryRoleEntity role , MessageTaskInfo taskInfo, long bId) {
-        StreamMessageEvent customEvent = new StreamMessageEvent(this, message , role , taskInfo , bId , null);
-        applicationEventPublisher.publishEvent(customEvent);
+        try{
+            StreamMessageEvent customEvent = new StreamMessageEvent(this, message , role , taskInfo , bId , null);
+            applicationEventPublisher.publishEvent(customEvent);
+        }catch(Exception e){
+            log.error("消息通知渠道异常 error:{}" , e.getMessage());
+        }
     }
 
 
     public void doStuffAndPublishAnEvent(final String message , IndustryRoleEntity role , MessageTaskInfo taskInfo, long bId , long messageId) {
-        StreamMessageEvent customEvent = new StreamMessageEvent(this, message , role , taskInfo , bId , messageId);
-        applicationEventPublisher.publishEvent(customEvent);
+
+        try{
+            StreamMessageEvent customEvent = new StreamMessageEvent(this, message , role , taskInfo , bId , messageId);
+            applicationEventPublisher.publishEvent(customEvent);
+        }catch(Exception e){
+            log.error("消息通知渠道异常:messageId:{}, error:{}" , messageId , e.getMessage());
+        }
+
     }
 }
