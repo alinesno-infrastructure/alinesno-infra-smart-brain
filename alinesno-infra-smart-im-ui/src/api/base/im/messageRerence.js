2fd5e9c03560984708b267df9bae4b50d4cb9594
林晓(Lily) <lilyAip@gmail.com>
2025-09-24 00:14:37 +0800

    feat:添加导出消息为Word的功能接口和方法

--- alinesno-infra-smart-im-ui/src/api/base/im/messageRerence.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/messageRerence.js
@@ -5,7 +5,8 @@ import { parseStrEmpty } from "@/utils/ruoyi";
 // 接口配置项
 var prefix = '/api/infra/base/im/messageReference' ;
 var managerUrl = {
-   getByMessageId: prefix + '/getByMessageId'
+   getByMessageId: prefix + '/getByMessageId' , 
+   exportWord: prefix + '/exportWord'
 }
 
 // 查询引用
@@ -15,4 +16,18 @@ export function getByMessageId(messageId){
     method: 'get'
   })
 }
- 
+
+// 导出消息成word
+export function exportWord(content , messageId, documentName) {
+  return request({
+    url: managerUrl.exportWord ,
+    method: 'post' ,
+    data: {
+      content: content ,
+      messageId: messageId,
+      documentName: documentName
+    },
+    // 关键：告诉 axios 后端返回二进制
+    responseType: 'blob'
+  })
+}
