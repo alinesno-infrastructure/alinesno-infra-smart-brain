26ce8eed7cdca260642a762b01d0192f9f751438
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:34 +0800

    fix:移除IAgentStoreService.java中的无用导入语句RpcWrapper

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IAgentStoreService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IAgentStoreService.java
@@ -3,7 +3,6 @@ package com.alinesno.infra.smart.im.service;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.common.facade.wrapper.RpcWrapper;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
 import com.alinesno.infra.smart.im.entity.AgentStoreEntity;
 
