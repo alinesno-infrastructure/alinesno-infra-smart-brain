85f4c4315aa05f3b0d04577e662eadee6fbc46b1
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:28 +0800

    更新:alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IGenerateTaskHistoryService.java

--- alinesno-infra-smart-brain-facade/src/main/java/com/alinesno/infra/smart/brain/service/IGenerateTaskHistoryService.java
+++
@@ -1,7 +0,0 @@
-package com.alinesno.infra.smart.brain.service;
-
-import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.brain.entity.GenerateTaskHistoryEntity;
-
-public interface IGenerateTaskHistoryService extends IBaseService<GenerateTaskHistoryEntity> {
-}
