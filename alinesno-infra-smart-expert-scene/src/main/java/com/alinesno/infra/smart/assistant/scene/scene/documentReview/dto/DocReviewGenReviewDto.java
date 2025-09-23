40c710055c07fdf19f812a2ff159d6e1d39208b7
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:39 +0800

    docs:移除不必要的Min注解导入

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/DocReviewGenReviewDto.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/documentReview/dto/DocReviewGenReviewDto.java
@@ -1,7 +1,6 @@
 package com.alinesno.infra.smart.assistant.scene.scene.documentReview.dto;
 
 import com.alinesno.infra.common.facade.base.BaseDto;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
