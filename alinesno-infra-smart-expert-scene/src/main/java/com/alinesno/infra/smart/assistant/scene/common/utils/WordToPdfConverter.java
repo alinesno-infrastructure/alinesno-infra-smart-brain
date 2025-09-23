677a9545228bd398ab4944e64fbd0ef33f8120d0
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:34 +0800

    fix:添加必要的IO库导入，修复WordToPdfConverter类依赖问题

--- alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/WordToPdfConverter.java
+++ alinesno-infra-smart-expert-scene/src/main/java/com/alinesno/infra/smart/assistant/scene/common/utils/WordToPdfConverter.java
@@ -10,7 +10,10 @@ import org.docx4j.fonts.PhysicalFonts;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
