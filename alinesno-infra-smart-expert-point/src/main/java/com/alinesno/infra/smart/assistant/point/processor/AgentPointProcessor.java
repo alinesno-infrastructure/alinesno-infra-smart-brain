a0879af04efc797aa128ebefd5fd058152f1e369
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:37 +0800

    fix:修复AgentPointProcessor类中获取角色ID和并发数的逻辑

--- alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/AgentPointProcessor.java
+++ alinesno-infra-smart-expert-point/src/main/java/com/alinesno/infra/smart/assistant/point/processor/AgentPointProcessor.java
@@ -1,26 +1,15 @@
 package com.alinesno.infra.smart.assistant.point.processor;
 
-import com.alinesno.infra.common.web.log.utils.SpringUtils;
 import com.alinesno.infra.smart.assistant.point.PointProcessorParent;
 import com.alinesno.infra.smart.point.annotation.AgentPointAnnotation;
-import com.alinesno.infra.smart.point.enums.ParamSource;
-import com.alinesno.infra.smart.point.service.IAccountPointService;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Slf4j
 public class AgentPointProcessor extends PointProcessorParent {
 
     private final AgentPointAnnotation agentPointAnnotation;
-
-    @Setter
-    private int agentMaxConcurrent ;
 
     public AgentPointProcessor(AgentPointAnnotation agentPointAnnotation) {
         this.agentPointAnnotation = agentPointAnnotation;
@@ -29,10 +18,10 @@ public class AgentPointProcessor extends PointProcessorParent {
     @Override
     public void process(HttpServletRequest request, Long userId, Long orgId) {
 
-        String roleId = getRoleIdFromRequest(request, agentPointAnnotation.paramName() , ParamSource.PARAMS) ;
-        Assert.notNull(roleId, "无法获取到角色ID");
-
+        String roleId = getRoleIdFromRequest(request, agentPointAnnotation.paramName()) ;
         long chatCount = accountPointService.getOrgSessionCount(orgId) ;
+
+        int agentMaxConcurrent = getIntegral(orgId).getAgentMaxConcurrency() ;
 
         log.debug("处理AgentPoint注解 - userId: {} , roleId = {} , chatCount = {}", userId , roleId , chatCount);
         Assert.isTrue(chatCount < agentMaxConcurrent , "当前并发会话数已超组织限制,会话并发数:" + agentMaxConcurrent);
