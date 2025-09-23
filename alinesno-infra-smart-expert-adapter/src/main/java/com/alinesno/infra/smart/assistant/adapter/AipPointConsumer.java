2d365e9befc5f41b6ccca8956dbbaba3429d8b8e
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:34 +0800

    feat:新增多个积分相关接口，包括生成订单、查询订单及支付二维码等

--- alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/AipPointConsumer.java
+++ alinesno-infra-smart-expert-adapter/src/main/java/com/alinesno/infra/smart/assistant/adapter/AipPointConsumer.java
@@ -1,12 +1,9 @@
 package com.alinesno.infra.smart.assistant.adapter;
 
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.R;
-import com.alinesno.infra.smart.assistant.adapter.dto.PointSettingDto;
-import com.alinesno.infra.smart.assistant.adapter.dto.UserPointDto;
-import com.dtflys.forest.annotation.BaseRequest;
-import com.dtflys.forest.annotation.Get;
-import com.dtflys.forest.annotation.Post;
-import com.dtflys.forest.annotation.Var;
+import com.alinesno.infra.smart.assistant.adapter.dto.*;
+import com.dtflys.forest.annotation.*;
 
 import java.util.List;
 
@@ -20,15 +17,45 @@ import java.util.List;
 public interface AipPointConsumer {
 
     /**
-     * 获取当前用户积分
+     * 生成订单
      */
-    @Get(url = "/getUserPoint" , contentType = "application/json")
-    R<UserPointDto> getUserPoint(@Var("accountId") Long accountId);
+    @Post(url = "/api/infra/base/platform/pay/payOrder/createOrder" , contentType = "application/json")
+    R<String> createOrder(@JSONBody CreateOrderDto dto);
 
     /**
-     * 减积分
+     * 查询订单及支付二维码
      */
-    @Get(url = "/reducePoint" , contentType = "application/json")
-    R<Boolean> reducePoint(@Var("accountId") Long accountId, @Var("point") Integer point);
+    @Post(url = "/api/infra/base/platform/pay/payOrder/queryOrderWithQrcode" , contentType = "application/json")
+    R<PayOrderWithQrcodeDto> queryOrderWithQrcode(@Query("orderId") String orderId , @Query("wayPay") String wayPay);
+
+    /**
+     * 查询订单
+     */
+    @Post(url = "/api/infra/base/platform/pay/payOrder/queryOrder" , contentType = "application/json")
+    R<PayOrderWithQrcodeDto> queryOrder(@Query("orderId") String orderId) ;
+
+    /**
+     * 获取当前组织积分
+     */
+    @Get(url = "/api/base/platform/integral/org/getIntegral" , contentType = "application/json")
+    R<OrgPointDto> getIntegral(@Query("orgId") Long orgId);
+
+    /**
+     * 消耗积分
+     */
+    @Post(url = "/api/base/platform/integral/org/consumePoints" , contentType = "application/json")
+    R<String> consumePoints(@JSONBody ConsumePointsDto dto);
+
+    /**
+     * 查询所有积分套餐
+     */
+    @Get(url = "/api/base/platform/integral/org/activePackages" , contentType = "application/json")
+    R<List<PointPackageDto>> activePackages();
+
+    /**
+     * 分页获取组织积分明细
+     */
+    @Post(url = "/api/base/platform/integral/org/pointsDetail" , contentType = "application/json")
+    R<TableDataInfo> pagerIntegralDetail(@JSONBody PointsDetailQueryDto dto);
 
 }
