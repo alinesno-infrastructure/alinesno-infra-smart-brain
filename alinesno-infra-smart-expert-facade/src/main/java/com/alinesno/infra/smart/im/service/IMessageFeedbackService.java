cf36e1a19a309ddaf8f4bdd20d69b3e441d7b484
周健(James) <jamesAip@gmail.com>
2025-09-23 21:52:41 +0800

    refactor:调整IMessageFeedbackService.java的导入依赖

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IMessageFeedbackService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/im/service/IMessageFeedbackService.java
@@ -1,10 +1,8 @@
 package com.alinesno.infra.smart.im.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
-import com.alinesno.infra.smart.brain.api.dto.PromptMessageDto;
-import com.alinesno.infra.smart.im.dto.*;
-import com.alinesno.infra.smart.im.entity.MessageEntity;
+import com.alinesno.infra.smart.im.dto.MessageFeedbackDto;
+import com.alinesno.infra.smart.im.dto.RoleFeedbackStat;
 import com.alinesno.infra.smart.im.entity.MessageFeedbackEntity;
 
 import java.util.ArrayList;
