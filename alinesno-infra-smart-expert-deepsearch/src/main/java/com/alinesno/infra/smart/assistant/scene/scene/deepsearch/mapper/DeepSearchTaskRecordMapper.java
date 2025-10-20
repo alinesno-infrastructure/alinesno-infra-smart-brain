c0a2d6a93e8fdc0b96d64a2d2b3b82be7d446ae9
张婷(Tina) <tinaAip@gmail.com>
2025-10-21 00:14:10 +0800

    feat:创建深度搜索任务状态Mapper接口

---
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/mapper/DeepSearchTaskRecordMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DeepSearchTaskRecordEntity;
+
+/**
+ * 深度搜索任务状态Mapper接口
+ */
+public interface DeepSearchTaskRecordMapper extends IBaseMapper<DeepSearchTaskRecordEntity> {
+}
