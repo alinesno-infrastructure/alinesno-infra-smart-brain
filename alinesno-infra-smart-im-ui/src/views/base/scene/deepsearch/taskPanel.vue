5d0ac8f302d60cde70ca9e42e3f85139f591a31a
李伟(Michael) <michaelAip@gmail.com>
2025-10-20 22:01:00 +0800

    feat:创建深度搜索任务面板组件，包含任务列表和聊天面板

---
+++ alinesno-infra-smart-im-ui/src/views/base/scene/deepsearch/taskPanel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="deepsearch-app-container">
+    <el-container style="height:calc(100vh - 40px );background-color: #fff;">
+      <el-aside width="80px" class="deepsearch-app-aside">
+        <TaskListPanel 
+          @handleNewTask="handleNewTask"
+          @handleTaskClick="handleTaskClick" />
+      </el-aside>
+        <el-main class="deepsearch-app-main">
+          <ChatPanel />
+        </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+import TaskListPanel from './functionList.vue'
+import ChatPanel from './chatPanel.vue'
+
+const hasNwTask = ref(true)
+const taskItem = ref({})
+
+const handleTaskClick = (item) => {
+  console.log('item = ' + item.name);
+  hasNwTask.value = false 
+  taskItem.value = item ;
+}
+
+const handleNewTask = () => {
+  hasNwTask.value = true
+}
+
+</script>
+
+<style lang="scss" scoped>
+.deepsearch-app-container {
+  padding: 0px ;
+  background-color: #f5f5f5 !important;
+
+  .deepsearch-app-aside {
+    padding: 0px !important;
+    margin-bottom: 0px !important;
+    background-color: #f5f5f5 !important;
+    border-right: 1px solid #ebebeb !important;
+  }
+
+  .deepsearch-app-main {
+    padding: 0px !important;
+    margin-right: 10px;
+    height: calc(100vh - 40px);
+  }
+
+}
+</style>
