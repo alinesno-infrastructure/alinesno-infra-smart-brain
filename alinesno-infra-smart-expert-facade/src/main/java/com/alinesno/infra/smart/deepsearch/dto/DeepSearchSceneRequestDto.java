b2dfa33b37c695ad7a9c824cf482941475a4e49b
周健(James) <jamesAip@gmail.com>
2025-10-20 22:01:01 +0800

    feat:在DeepSearchSceneRequestDto中添加场景ID、提示内容、渠道流ID字段并增加非空校验

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/deepsearch/dto/DeepSearchSceneRequestDto.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/deepsearch/dto/DeepSearchSceneRequestDto.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.smart.deepsearch.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -11,11 +12,24 @@ public class DeepSearchSceneRequestDto {
     /**
      * 场景ID，用于标识特定的场景
      */
+    @NotNull(message = "场景ID不能为空")
     private Long sceneId ;
 
     /**
      * 提示内容，用于描述章节的提示信息
      */
+    @NotNull(message = "提示内容不能为空")
     private String promptContent ;
+
+    /**
+     * 渠道流ID，用于标识特定的渠道流
+     */
+    @NotNull(message = "渠道流ID不能为空")
+    private String channelStreamId ;
+
+    /**
+     * 附件信息，用于描述章节的附件信息
+     */
+    private String attachments;  // 附件信息
 
 }
