497d1202aa98dd37096e8c43df1e1f341fab832f
周健(James) <jamesAip@gmail.com>
2025-10-20 22:01:01 +0800

    feat:在BaseHandler中新增多个属性字段，支持深度检索功能

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/agent/BaseHandler.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/agent/BaseHandler.java
@@ -2,11 +2,16 @@ package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.agent;
 
 import com.alinesno.infra.smart.assistant.adapter.event.StreamMessagePublisher;
 import com.alinesno.infra.smart.assistant.api.ToolDto;
+import com.alinesno.infra.smart.assistant.api.config.ContextEngineeringData;
+import com.alinesno.infra.smart.assistant.api.config.UploadData;
 import com.alinesno.infra.smart.assistant.entity.IndustryRoleEntity;
+import com.alinesno.infra.smart.assistant.role.tools.ReActServiceTool;
+import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.utils.DeepsearchSummaryMessageTool;
 import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.utils.StepEventUtil;
 import com.alinesno.infra.smart.assistant.service.IToolService;
 import com.alinesno.infra.smart.deepsearch.dto.DeepSearchFlow;
 import com.alinesno.infra.smart.im.dto.MessageTaskInfo;
+import com.alinesno.infra.smart.scene.entity.DeepSearchTaskEntity;
 import lombok.Data;
 
 import java.util.List;
@@ -34,4 +39,14 @@ public abstract class BaseHandler {
     private List<ToolDto> tools ;  // 工具列表
 
     private String datasetKnowledgeDocument ; // 知识库文档内容
+
+    private ReActServiceTool reActServiceTool  ;
+
+    private DeepsearchSummaryMessageTool summaryMessageTool ;
+
+    private UploadData uploadData  ; // 上传数据
+
+    private ContextEngineeringData contextEngineeringData ; // 上下文工程
+
+    private DeepSearchTaskEntity deepSearchTask ; // 深度检索任务
 }
