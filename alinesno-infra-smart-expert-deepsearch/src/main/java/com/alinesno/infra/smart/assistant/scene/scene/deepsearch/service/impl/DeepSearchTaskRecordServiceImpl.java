a9cb957077bdf9fe30ddc4d2e8afd4aa75fc3463
张婷(Tina) <tinaAip@gmail.com>
2025-10-21 00:14:11 +0800

    feat:创建深度搜索任务记录服务实现类

---
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/impl/DeepSearchTaskRecordServiceImpl.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper.DeepSearchTaskRecordMapper;
+import com.alinesno.infra.smart.assistant.scene.scene.deepsearch.service.IDeepSearchTaskRecordService;
+import com.alinesno.infra.smart.scene.entity.DeepSearchTaskRecordEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 深度搜索状态服务实现类
+ */
+@Slf4j
+@Service
+public class DeepSearchTaskRecordServiceImpl extends IBaseServiceImpl<DeepSearchTaskRecordEntity, DeepSearchTaskRecordMapper> implements IDeepSearchTaskRecordService {
+
+}
