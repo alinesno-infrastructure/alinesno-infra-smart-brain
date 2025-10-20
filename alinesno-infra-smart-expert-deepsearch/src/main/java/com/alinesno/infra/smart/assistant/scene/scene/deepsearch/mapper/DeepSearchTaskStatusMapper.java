938373669a4e971d6a9e33b6d6393196488d115c
周健(James) <jamesAip@gmail.com>
2025-10-20 22:00:59 +0800

    feat:创建DeepSearchTaskStatusMapper接口，继承IBaseMapper

---
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/mapper/DeepSearchTaskStatusMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.smart.assistant.scene.scene.deepsearch.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.smart.scene.entity.DeepSearchTaskStatusEntity;
+
+/**
+ * 深度搜索任务状态Mapper接口
+ */
+public interface DeepSearchTaskStatusMapper extends IBaseMapper<DeepSearchTaskStatusEntity> {
+}
