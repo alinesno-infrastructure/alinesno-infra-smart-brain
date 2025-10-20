80ff20557a77618d9656f743632b85ec038024de
李伟(Michael) <michaelAip@gmail.com>
2025-10-20 22:01:01 +0800

    fix:优化频道创建界面，移除冗余代码并调整表单布局

--- alinesno-infra-smart-im-ui/src/views/base/chat/channelGroup.vue
+++ alinesno-infra-smart-im-ui/src/views/base/chat/channelGroup.vue
@@ -2,56 +2,7 @@
 
   <el-dialog v-model="centerDialogVisible" title="创建频道" width="30%" :append-to-body="true" align-center>
 
-    <!--
-    <div class="channel-box-panel" :class="showCreateBox?'show-box':'hidden-box'">
-
-      <ul style="margin: 0;padding: 0px;list-style: none;">
-        <li>
-          <div class="channel-item">
-            <img class="channel-image" src="http://data.linesno.com/icons/sepcialist/dataset_55.png" />
-            <div class="channel-text">
-              创建自己的频道
-            </div>
-            <el-button type="danger" style="position: absolute;right: 10px;" text bg @click="showCreateBox = false">
-              <i class="fa-solid fa-feather icon-btn"></i> 创建 
-            </el-button>
-          </div>
-        </li>
-      </ul>
-
-      <div style="font-size: 13px;margin-bottom: 10px;width: 100%;">
-        进入推荐频道 
-      </div>
-      
-      <el-scrollbar height="400px">
-        <ul style="margin: 0;padding: 0px;list-style: none;">
-          <li v-for="(item, index) in chatChannelTemplate" :key="index">
-            <div class="channel-item">
-
-              <img class="channel-image" :src="imagePath(item)" />
-
-              <div class="channel-text">
-                #{{ item.channelName }}
-              </div>
-              <div class="channel-desc">
-                {{ truncateString(item.channelDesc,15) }}
-              </div>
-              <el-button type="primary" @click="handleSetChannelId(item.id)" style="position: absolute;right: 10px;" text bg>
-                <i class="fa-solid fa-hammer icon-btn"></i> 进入
-              </el-button>
-            </div>
-          </li>
-        </ul>
-      </el-scrollbar>
-    </div>
-    -->
-
     <div style="height:481px;padding-top:20px;">
-        <!-- <el-page-header title="" style="margin-bottom: 30px;">
-          <template #content>
-            <span class="text-large font-600 mr-3"> 填写频道信息</span>
-          </template>
-        </el-page-header> -->
         <el-form :model="form" :rules="rules" label-position="top" ref="userRef" label-width="80px">
           <el-row>
             <el-col :span="24" class="editor-after-div">
@@ -90,10 +41,6 @@
           <el-row>
               <el-col :span="24">
                 <el-form-item label="频道类型" prop="channelType">
-                  <!-- <el-select size="large" v-model="form.channelType" placeholder="选择频道类型">
-                    <el-option label="组织频道" value="org" />
-                    <el-option label="个人频道" value="private" />
-                  </el-select> -->
 
                   <el-radio-group v-model="form.channelType" size="large">
                     <el-radio label="org">组织频道</el-radio>
@@ -102,15 +49,6 @@
 
                 </el-form-item>
               </el-col>
-              <!-- 
-              <el-col :span="1">
-              </el-col>
-              <el-col :span="11" v-if="form.channelType === '9'">
-                <el-form-item label="进入密钥" prop="channelKey">
-                    <el-input size="large" v-model="form.channelKey" placeholder="请输入进入密钥" maxlength="30" />
-                </el-form-item>
-              </el-col> 
-              -->
           </el-row>
 
         </el-form> 
