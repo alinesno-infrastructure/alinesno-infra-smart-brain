4f0a3d405d713683959a91a72846e977ed3c9d1a
林晓(Lily) <lilyAip@gmail.com>
2025-10-20 22:01:01 +0800

    feat:新增文件管理相关接口及方法

--- alinesno-infra-smart-im-ui/src/api/base/im/scene/productResearchDataset.js
+++ alinesno-infra-smart-im-ui/src/api/base/im/scene/productResearchDataset.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 import { parseStrEmpty } from "@/utils/ruoyi";
+import { get } from '@vueuse/core';
 
 /**
  * 数据库接口操作
@@ -17,15 +18,75 @@ var managerUrl = {
   listGroup: groupPrefix + "listGroup" ,
   saveOrUpdateRuleGroup : groupPrefix + "saveOrUpdateRuleGroup" ,
   removeGroup: groupPrefix + "deleteGroup" ,
+  listLlmMode: groupPrefix + "listLlmMode" ,
+  getGroupById: groupPrefix + "getGroupById" ,
 
   getAllArticleTemplateType: groupPrefix +"getAllArticleTemplate",
   getTemplateByType: groupPrefix +"getTemplateByType",
   getTemplateDetail: groupPrefix +"getTemplateDetail",
 
-  // 规则接口
+  // 文件接口
   saveOrUpdateRule: prefix + "saveOrUpdateRule" ,
   listRuleByIds: prefix + "listRuleByIds" ,
+  listKnowledge: prefix + 'datatables',
   removeRule: prefix + "delete" ,
+  renameKnowledge: prefix + "renameKnowledge" , 
+  deleteKnowledge: prefix + "deleteKnowledge"
+}
+
+/**
+ * 重命名文件
+ * @param {Object} data 重命名参数
+ * @returns {Promise} 重命名Promise
+ */ 
+export function renameKnowledge(data) {
+    return request({
+        url: managerUrl.renameKnowledge , 
+        method: 'post',
+        params: data // 或者用 data: data
+    });
+}
+
+/**
+ * 删除文件
+ * @param {Object} data 删除参数
+ * @returns {Promise} 删除Promise
+ */
+export function deleteKnowledge(data) {
+    return request({
+        url: managerUrl.deleteKnowledge, 
+        method: 'delete',
+        params: data
+    });
+}
+
+/**
+ * 查询考生列表
+ * @param {Object} query 查询参数
+ * @returns {Promise} 请求Promise
+ */
+export function listKnowledge(query) {
+  return request({
+    url: managerUrl.listKnowledge,
+    method: 'post',
+    params: query
+  })
+}
+
+// 通过分组id获取规则详细信息
+export function getGroupById(groupId) {
+  return request({
+    url: managerUrl.getGroupById + '?groupId=' + parseStrEmpty(groupId) ,
+    method: 'get'
+  })
+}
+
+// 列出所有模型列表
+export function listAllLlmModel(modelType) {
+    return request({
+        url: managerUrl.listLlmMode+'?modelType=' + parseStrEmpty(modelType) ,
+        method: 'get'
+    })
 }
 
 // 获取模板详情
@@ -95,9 +156,10 @@ export function saveOrUpdateRuleGroup(data) {
 }
 
 // 查询所有分组列表
-export function listGroup(sceneId) {
+export function listGroup(params) {
   return request({
-    url: managerUrl.listGroup + '?sceneId=' + parseStrEmpty(sceneId),
-    method: 'get'
+    url: managerUrl.listGroup , 
+    method: 'get' , 
+    params: params
   })
 }
