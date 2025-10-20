b3f6f6385e6a95d1fcf7d2c53d866084ec35c935
周健(James) <jamesAip@gmail.com>
2025-10-20 22:01:01 +0800

    feat:在IProjectKnowledgeService中新增existsByMd5AndGroupId、hasOngoingImport、renameKnowledge和deleteKnowledgeById方法

--- alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/service/IProjectKnowledgeService.java
+++ alinesno-infra-smart-expert-facade/src/main/java/com/alinesno/infra/smart/scene/service/IProjectKnowledgeService.java
@@ -15,4 +15,28 @@ public interface IProjectKnowledgeService extends IBaseService<ProjectKnowledgeE
      */
     void importData(ProjectKnowledgeEntity entity , Llm llm, EmbeddingOptions embeddingOptions);
 
+    /**
+     * 判断指定 group 下是否存在相同 md5 的记录（用于去重）
+     */
+    boolean existsByMd5AndGroupId(String md5, Long groupId);
+
+    /**
+     * 判断指定 group 是否存在处于“处理中”的导入（vector_status = 0）
+     */
+    boolean hasOngoingImport(Long groupId);
+
+    /**
+     * 重命名知识
+     * @param id
+     * @param newName
+     */
+    void renameKnowledge(Long id, String newName);
+
+    /**
+     * 删除知识
+     * @param id
+     * @throws Exception
+     */
+    void deleteKnowledgeById(Long id) throws Exception;
+
 }
