0906d77b50fac6c56e6b2b00d9eac58b9efcf8bb
周健(James) <jamesAip@gmail.com>
2025-10-23 00:15:36 +0800

    feat:新增更新任务名称和描述的方法updateTitleAndDescById

--- alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/IDeepSearchTaskService.java
+++ alinesno-infra-smart-expert-deepsearch/src/main/java/com/alinesno/infra/smart/assistant/scene/scene/deepsearch/service/IDeepSearchTaskService.java
@@ -5,4 +5,13 @@ import com.alinesno.infra.smart.scene.entity.DeepSearchTaskEntity;
 
 public interface IDeepSearchTaskService extends IBaseService<DeepSearchTaskEntity> {
 
+    /**
+     * 更新任务名称和描述
+     *
+     * @param taskEntity
+     * @param title
+     * @param desc
+     */
+    void updateTitleAndDescById(DeepSearchTaskEntity taskEntity, String title, String desc);
+
 }
