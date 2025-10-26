efe40af20291f6daf857ab78280c6fb3fa6b3ed3
陈伟(Victor) <victorAip@gmail.com>
2025-10-27 00:14:01 +0800

    fix:修复代码编辑器样式和引入依赖问题

--- alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/promptEditorPanel.vue
+++ alinesno-infra-smart-expert-ui/src/views/smart/assistant/llmModel/promptEditorPanel.vue
@@ -3,17 +3,12 @@
     <div style="
     display: flex;
     justify-content: space-between;
-    align-items: center;
-">
+    align-items: center;">
       <span style="font-size:14px;font-weight: bold;"> 人设与回复逻辑 </span>
-      <!-- <span> 
-        <el-button type="primary" text bg>优化</el-button>
-      </span> -->
     </div>
 
     <div style="margin-top:10px;" class="prompt-editor-content">
-      <code-mirror basic :lang="lang" v-model="codeVal" :style="'height:700px'" :theme="theme"
-      :extensions="extensions" />
+      <code-mirror basic :lang="lang" v-model="codeVal" :style="'height:700px'" :theme="theme" :extensions="extensions" />
     </div>
 
     <div style="display: flex; justify-content: flex-end;">
@@ -28,22 +23,10 @@
 
 import CodeMirror from 'vue-codemirror6'
 import { oneDark } from '@codemirror/theme-one-dark'
-
-// import { python } from '@codemirror/lang-python';
-// import { java } from '@codemirror/lang-java';
-
-// import { json } from '@codemirror/lang-json';
-// import { sql } from '@codemirror/lang-sql';
-// import { yaml } from '@codemirror/lang-yaml';
-
 import { EditorView } from '@codemirror/view';
 import { markdown } from '@codemirror/lang-markdown';
 
-// const router = useRouter();
 const { proxy } = getCurrentInstance();
-
-// const dialogVisible = ref(false)
-// const cloneContent = ref('')
 
 // 初始化
 let codeVal = ref('');
@@ -51,14 +34,6 @@ let codeVal = ref('');
 // const editorHeight = ref(props.height);
 
 const lang = markdown() ; 
-
-//  props.lang == 'python' ? python() :
-//   props.lang == 'json' ? json() :
-//     props.lang == 'yaml' ? yaml() :
-//       props.lang == 'sql' ? sql() :
-//         props.lang == 'markdown' ? markdown() :
-//           props.lang == 'java' ? java() :
-//             python();
 
 // 扩展
 const extensions = [
